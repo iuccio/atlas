f1f2585fc8ac6f318da45ae74e603719666422e0
atlas <TechSupport-ATLAS@sbb.ch>
2021-07-19 10:12:29 +0200

    ATLAS-5: Spring booz initialiser initial commit with:
    - spring-boot-starter-actuator
    - spring-boot-starter-data-jpa
    - spring-boot-starter-oauth2-resource-server
    - spring-boot-starter-security
    - spring-boot-starter-web
    - postgresql
    - lombok
    - spring-boot-starter-test
    - spring-security-test

---
+++ src/main/java/ch/sbb/atlas/AtlasBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.sbb.atlas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlasBackendApplication.class, args);
+	}
+
+}
