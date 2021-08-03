99580a8da2a87c0875e746cb5ebe60babf9ee63f
atlas <TechSupport-ATLAS@sbb.ch>
2021-08-03 15:23:10 +0200

    init flyway

---
+++ src/main/resources/db/migration/V0.0.1__init.sql
@@ -0,0 +1,12 @@
+CREATE TABLE CUSTOMERS (
+                           id int not null,
+                           firstname VARCHAR(255),
+                           lastname VARCHAR(255)
+);
+
+CREATE SEQUENCE CUSTOMERS_SEQUENCE START WITH 1000 INCREMENT BY 1;
+
+INSERT INTO CUSTOMERS (id,firstname, lastname) VALUES (nextval('CUSTOMERS_SEQUENCE'),'Antonio', 'Romano');
+INSERT INTO CUSTOMERS (id,firstname, lastname) VALUES (nextval('CUSTOMERS_SEQUENCE'),'Diego Armando', 'Maradona');
+INSERT INTO CUSTOMERS (id,firstname, lastname) VALUES (nextval('CUSTOMERS_SEQUENCE'),'Zinedine', 'Zidane');
+INSERT INTO CUSTOMERS (id,firstname, lastname) VALUES (nextval('CUSTOMERS_SEQUENCE'),'Valentino', 'Rossi');
