2274251d9d0d8fbeb089597e6c3eacfbe10a07ab
atlas <19825793+iuccio@users.noreply.github.com>
2021-08-04 12:58:25 +0200

    ATLAS-46: init atlas db

--- src/main/resources/db/migration/V0.0.1__init.sql
+++ src/main/resources/db/migration/V0.0.1__init.sql
@@ -1,12 +1,30 @@
-CREATE TABLE CUSTOMERS (
-                           id int not null,
-                           firstname VARCHAR(255),
-                           lastname VARCHAR(255)
+CREATE TABLE timetable_field_number_version
+(
+    id                              BIGINT NOT NULL PRIMARY KEY,
+    fpfnid                          VARCHAR(500),
+    name                            VARCHAR(500),
+    number                          VARCHAR(50),
+    swiss_timetable_field_number    VARCHAR(50),
+    creation_date                   TIMESTAMP,
+    creator                         VARCHAR(50),
+    edition_date                    TIMESTAMP,
+    editor                          VARCHAR(50),
+    valid_from                      TIMESTAMP,
+    valid_to                        TIMESTAMP,
+    business_organisation           BIGINT,
+    comment                         VARCHAR(250),
+    name_compact                    VARCHAR(50)
 );
 
-CREATE SEQUENCE CUSTOMERS_SEQUENCE START WITH 1000 INCREMENT BY 1;
+CREATE SEQUENCE timetable_field_number_version_seq START WITH 1000 INCREMENT BY 1;
 
-INSERT INTO CUSTOMERS (id,firstname, lastname) VALUES (nextval('CUSTOMERS_SEQUENCE'),'Antonio', 'Romano');
-INSERT INTO CUSTOMERS (id,firstname, lastname) VALUES (nextval('CUSTOMERS_SEQUENCE'),'Diego Armando', 'Maradona');
-INSERT INTO CUSTOMERS (id,firstname, lastname) VALUES (nextval('CUSTOMERS_SEQUENCE'),'Zinedine', 'Zidane');
-INSERT INTO CUSTOMERS (id,firstname, lastname) VALUES (nextval('CUSTOMERS_SEQUENCE'),'Valentino', 'Rossi');
+CREATE TABLE timetable_field_lines_relation
+(
+    id                              BIGINT NOT NULL PRIMARY KEY,
+    slnid                           VARCHAR(500),
+    CONSTRAINT fk_timetable_field_number_version
+        FOREIGN KEY (id)
+            REFERENCES timetable_field_number_version(id)
+);
+
+CREATE SEQUENCE timetable_field_lines_relation_seq START WITH 1000 INCREMENT BY 1;
