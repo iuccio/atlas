a54fd16ec9ca9567623a04bc3d63a9e16d6ae1d6
atlas <TechSupport-ATLAS@sbb.ch>
2021-08-06 17:41:13 +0200

    Improvements

--- src/main/java/ch/sbb/timetable/field/number/TimetableFieldNumberApplication.java
+++ src/main/java/ch/sbb/timetable/field/number/TimetableFieldNumberApplication.java
@@ -1,5 +1,8 @@
 package ch.sbb.timetable.field.number;
 
+import java.time.ZoneId;
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TimetableFieldNumberApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("Europe/Zurich")));
 		SpringApplication.run(TimetableFieldNumberApplication.class, args);
 	}
 
