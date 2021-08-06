e1d1afc505e6a12ead0f11ba8e2f1ef22d257c02
atlas <TechSupport-ATLAS@sbb.ch>
2021-08-06 15:13:26 +0200

    Extract Interface for Api

---
+++ src/main/java/ch/sbb/timetable/field/number/api/VersionApi.java
@@ -0,0 +1,39 @@
+package ch.sbb.timetable.field.number.api;
+
+import java.util.List;
+
+import org.springdoc.core.converters.models.PageableAsQueryParam;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import ch.sbb.timetable.field.number.model.VersionModel;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "timetable-field-numbers")
+public interface VersionApi {
+
+    @GetMapping
+    @PageableAsQueryParam
+    List<VersionModel> getVersions(@Parameter(hidden = true) Pageable pageable);
+
+    @GetMapping("/{id}")
+    VersionModel getVersion(@PathVariable Long id);
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    VersionModel createVersion(@RequestBody VersionModel newVersion);
+
+    @PutMapping({"/{id}"})
+    VersionModel updateVersion(@PathVariable Long id, @RequestBody VersionModel newVersion);
+
+    @DeleteMapping({"/{id}"})
+    void deleteVersion(@PathVariable Long id);
+}
--- src/main/java/ch/sbb/timetable/field/number/controller/VersionController.java
+++ src/main/java/ch/sbb/timetable/field/number/controller/VersionController.java
@@ -5,31 +5,21 @@ import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
-import org.springdoc.core.converters.models.PageableAsQueryParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import ch.sbb.timetable.field.number.api.VersionApi;
 import ch.sbb.timetable.field.number.entity.Version;
 import ch.sbb.timetable.field.number.model.VersionModel;
 import ch.sbb.timetable.field.number.repository.VersionRepository;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
-@Tag(name = "timetable-field-numbers")
-public class VersionController {
+public class VersionController implements VersionApi {
 
     private static final Supplier<ResponseStatusException> NOT_FOUND_EXCEPTION = () -> new ResponseStatusException(HttpStatus.NOT_FOUND);
 
@@ -42,27 +32,25 @@ public class VersionController {
         this.modelMapper = modelMapper;
     }
 
-    @GetMapping
-    @PageableAsQueryParam
-    public List<VersionModel> getVersions(@Parameter(hidden = true) Pageable pageable) {
+    @Override
+    public List<VersionModel> getVersions(Pageable pageable) {
         log.info("Load Versions using pageable={}", pageable);
         return versionRepository.findAll(pageable).stream().map(version -> modelMapper.map(version, VersionModel.class)).collect(Collectors.toList());
     }
 
-    @GetMapping("/{id}")
-    public VersionModel getVersion(@PathVariable Long id) {
+    @Override
+    public VersionModel getVersion(Long id) {
         return versionRepository.findById(id).map(this::toModel).orElseThrow(NOT_FOUND_EXCEPTION);
     }
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public VersionModel createVersion(@RequestBody VersionModel newVersion) {
+    @Override
+    public VersionModel createVersion(VersionModel newVersion) {
         Version createdVersion = versionRepository.save(modelMapper.map(newVersion, Version.class));
         return toModel(createdVersion);
     }
 
-    @PutMapping({"/{id}"})
-    public VersionModel updateVersion(@PathVariable Long id, @RequestBody VersionModel newVersion) {
+    @Override
+    public VersionModel updateVersion(Long id, VersionModel newVersion) {
         Version versionToUpdate = versionRepository.findById(id).orElseThrow(NOT_FOUND_EXCEPTION);
 
         versionToUpdate.setTtfnid(newVersion.getTtfnid());
@@ -79,8 +67,8 @@ public class VersionController {
         return toModel(versionToUpdate);
     }
 
-    @DeleteMapping({"/{id}"})
-    public void deleteTodo(@PathVariable Long id) {
+    @Override
+    public void deleteVersion(Long id) {
         if (!versionRepository.existsById(id)) {
             throw NOT_FOUND_EXCEPTION.get();
         }
