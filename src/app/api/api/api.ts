ed2bf9252ab038d3dbc388ea8f3a7d4c0fdaa08f
atlas <TechSupport-ATLAS@sbb.ch>
2021-08-17 14:48:05 +0200

    Let Openapi do its thing

---
+++ src/app/api/api/api.ts
@@ -0,0 +1,3 @@
+export * from './timetableFieldNumbers.service';
+import { TimetableFieldNumbersService } from './timetableFieldNumbers.service';
+export const APIS = [TimetableFieldNumbersService];
