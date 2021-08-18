0a7b845d8b1ac4b84b18199b832bb2f7ed356b67
atlas <19825793+iuccio@users.noreply.github.com>
2021-08-18 10:53:25 +0200

    Lets try to do the rest thing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../core/auth.service';
+import { TimetableFieldNumbersService, Version } from '../api';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  versions: Observable<Array<Version>> | undefined;
+
+  constructor(
+    private authService: AuthService,
+    private timetableFieldNumbersService: TimetableFieldNumbersService
+  ) {}
+
+  ngOnInit(): void {
+    this.getVersions();
+  }
+
   get loggedIn() {
     return this.authService.loggedIn;
   }
 
-  constructor(private authService: AuthService) {}
+  getVersions(): void {
+    this.versions = this.timetableFieldNumbersService.getVersions(0, 20);
+  }
 }
