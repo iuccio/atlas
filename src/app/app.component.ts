6ab85fbb7262a690fd5f4b4d0ad101dfb28a2c2b
atlas <TechSupport-ATLAS@sbb.ch>
2021-08-24 10:50:59 +0200

    add ngx languages, header and user components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from './core/auth.service';
 
 import { environment } from '../environments/environment';
 
 import packageJson from '../../package.json';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   version = packageJson.version;
   environmentLabel = environment.label;
 
   title = $localize`Timetable Field Number`;
-
-  get userName() {
-    return this.authService.claims?.name;
+  constructor(private translate: TranslateService) // private keepalive: KeepaliveService,
+  // public loadingSpinnerService: LoadingSpinnerService
+  {
+    //TODO: get lang from browser
+    this.translate.use('de');
+    // this.loadingSpinnerService.initLoadingSpinner();
   }
 
-  constructor(private authService: AuthService) {}
-
-  login() {
-    this.authService.login();
-  }
-
-  logout() {
-    return this.authService.logout();
+  ngOnInit(): void {
+    // this.keepalive.initialize();
+    console.log('app-component');
   }
 }
---
+++ src/app/core/components/user/user.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../auth.service';
+import { User } from '../../../model/user';
+import { Observable, of } from 'rxjs';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss'],
+})
+export class UserComponent implements OnInit {
+  user$: Observable<User | undefined> = of(undefined);
+
+  protected authenticated: boolean | undefined;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    console.log('user');
+    this.authenticated = this.authService.loggedIn;
+    this.user$ = of(this.authService.claims);
+  }
+
+  login(): void {
+    this.authService.login();
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
+}
