4fcb2d75c1e56242b6ec021facec70420ea58341
atlas <19825793+iuccio@users.noreply.github.com>
2021-08-25 15:10:23 +0200

    ATLAS-67: fix tests config

--- src/app/core/components/table/table.component.spec.ts
+++ src/app/core/components/table/table.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TableComponent } from './table.component';
+import { TranslateFakeLoader, TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { MaterialModule } from '../../module/material.module';
 
 describe('TableComponent', () => {
   let component: TableComponent;
@@ -9,6 +11,12 @@ describe('TableComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [TableComponent],
+      imports: [
+        MaterialModule,
+        TranslateModule.forRoot({
+          loader: { provide: TranslateLoader, useClass: TranslateFakeLoader },
+        }),
+      ],
     }).compileComponents();
   });
 
