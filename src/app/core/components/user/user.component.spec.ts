4b33173568c51916ef9625d03978afe10b0f7b7d
atlas <19825793+iuccio@users.noreply.github.com>
2021-08-31 16:29:02 +0200

    add tests

--- src/app/core/components/user/user.component.spec.ts
+++ src/app/core/components/user/user.component.spec.ts
@@ -14,6 +14,7 @@ describe('UserComponent', () => {
   const authServiceMock: Partial<AuthService> = {
     claims: { name: 'Test (ITC)', email: 'test@test.ch', roles: ['role1', 'role2', 'role3'] },
     logout: () => Promise.resolve(true),
+    login: () => Promise.resolve(true),
   };
 
   const userName = authServiceMock.claims!.name;
@@ -39,45 +40,100 @@ describe('UserComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('Component Rendering', () => {
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should render username on the title', () => {
+      expect(fixture.nativeElement.querySelector('button').title).toContain(expectedUserName);
+    });
+
+    it('should show user menu', () => {
+      fixture.detectChanges();
+      const usermenuOpenButton = fixture.debugElement.query(By.css('button'));
+      usermenuOpenButton.nativeElement.click();
+      fixture.detectChanges();
+
+      const usernameModal = fixture.debugElement.query(By.css('.user-info-modal')).nativeElement;
+      expect(usernameModal.querySelector('.user-name-modal').textContent).toContain(
+        expectedUserName
+      );
+
+      const userRolesModal = fixture.debugElement.query(By.css('#user-roles-modal')).nativeElement;
+      expect(userRolesModal.querySelector('.user-info-modal').textContent).toContain(
+        'PROFILE.YOUR_ROLES'
+      );
+      const userRoles = userRolesModal.querySelectorAll('mat-list>mat-list-item.mat-list-item');
+      expect(userRoles[0].textContent).toContain(authServiceMock.claims!.roles[0]);
+      expect(userRoles[1].textContent).toContain(authServiceMock.claims!.roles[1]);
+      expect(userRoles[2].textContent).toContain(authServiceMock.claims!.roles[2]);
+    });
+
+    it('should logout', () => {
+      const logoutSpy = spyOn(authServiceMock as AuthService, 'logout');
+
+      // Open user menu
+      const usermenuOpenButton = fixture.debugElement.query(By.css('button'));
+      usermenuOpenButton.nativeElement.click();
+      fixture.detectChanges();
+
+      // Logout
+      const logoutButton = fixture.debugElement.query(By.css('#logout'));
+      logoutButton.nativeElement.click();
+
+      expect(logoutSpy).toHaveBeenCalled();
+    });
+
+    it('should login', () => {
+      const loginSpy = spyOn(authServiceMock as AuthService, 'login');
+      component.isAuthenticated = false;
+      fixture.detectChanges();
+
+      // Logout
+      const logoutButton = fixture.debugElement.query(By.css('#login'));
+      logoutButton.nativeElement.click();
+
+      expect(loginSpy).toHaveBeenCalled();
+    });
   });
 
-  it('should render username on the title', () => {
-    expect(fixture.nativeElement.querySelector('button').title).toContain(expectedUserName);
-  });
+  describe('Component logic', () => {
+    it('should extract username', () => {
+      //when
+      component.extractUserName();
 
-  it('should show user menu', () => {
-    fixture.detectChanges();
-    const usermenuOpenButton = fixture.debugElement.query(By.css('button'));
-    usermenuOpenButton.nativeElement.click();
-    fixture.detectChanges();
+      //then
+      expect(component.userName).toBe('Test');
+    });
 
-    const usernameModal = fixture.debugElement.query(By.css('.user-info-modal')).nativeElement;
-    expect(usernameModal.querySelector('.user-name-modal').textContent).toContain(expectedUserName);
+    it('should return null when name is null', () => {
+      //given
+      component.user = undefined;
+      //when
+      component.extractUserName();
 
-    const userRolesModal = fixture.debugElement.query(By.css('#user-roles-modal')).nativeElement;
-    expect(userRolesModal.querySelector('.user-info-modal').textContent).toContain(
-      'PROFILE.YOUR_ROLES'
-    );
-    const userRoles = userRolesModal.querySelectorAll('mat-list>mat-list-item.mat-list-item');
-    expect(userRoles[0].textContent).toContain(authServiceMock.claims!.roles[0]);
-    expect(userRoles[1].textContent).toContain(authServiceMock.claims!.roles[1]);
-    expect(userRoles[2].textContent).toContain(authServiceMock.claims!.roles[2]);
-  });
+      //then
+      expect(component.userName).toBeUndefined();
+    });
 
-  it('should logout', () => {
-    const logoutSpy = spyOn(authServiceMock as AuthService, 'logout');
+    it('should be authenticate when user is not null', () => {
+      //when
+      component.authenticate();
 
-    // Open user menu
-    const usermenuOpenButton = fixture.debugElement.query(By.css('button'));
-    usermenuOpenButton.nativeElement.click();
-    fixture.detectChanges();
+      //then
+      expect(component.isAuthenticated).toBeTruthy();
+    });
 
-    // Logout
-    const logoutButton = fixture.debugElement.query(By.css('#logout'));
-    logoutButton.nativeElement.click();
+    it('should not be authenticate when user is null', () => {
+      //given
+      component.user = undefined;
 
-    expect(logoutSpy).toHaveBeenCalled();
+      //when
+      component.authenticate();
+
+      //then
+      expect(component.isAuthenticated).toBeFalse();
+    });
   });
 });
