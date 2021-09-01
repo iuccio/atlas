1e87e72fba325e2a1553b1d5ad263890c468a73d
atlas <19825793+iuccio@users.noreply.github.com>
2021-09-01 10:35:34 +0200

    ATLAS-67: show version and env on header

--- src/app/core/components/header/header.component.spec.ts
+++ src/app/core/components/header/header.component.spec.ts
@@ -35,13 +35,72 @@ describe('HeaderComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('Component Rendering', () => {
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should render header', () => {
+      expect(fixture.nativeElement.querySelector('#header').textContent).toContain('HOME.HEADER');
+    });
   });
 
-  it('should render header', () => {
-    expect(fixture.nativeElement.querySelector('h1.white.ms-3').textContent).toContain(
-      'HOME.HEADER'
-    );
+  describe('Component logic', () => {
+    it('should show label dev', () => {
+      //given
+      component.environmentLabel = 'dev';
+      //when
+      const result = component.showLabel();
+      //then
+      expect(result).toBeTruthy();
+    });
+
+    it('should show label int', () => {
+      //given
+      component.environmentLabel = 'int';
+      //when
+      const result = component.showLabel();
+      //then
+      expect(result).toBeTruthy();
+    });
+
+    it('should not show label different from dev or int', () => {
+      //given
+      component.environmentLabel = 'pro';
+      //when
+      const result = component.showLabel();
+      //then
+      expect(result).toBeFalsy();
+    });
+
+    it('should return dev class', () => {
+      //given
+      component.environmentLabel = 'dev';
+      //when
+      const result = component.getEnvLabelClass();
+      //then
+      expect(result['bg-primary']).toBeTruthy();
+      expect(result['bg-warning']).toBeFalsy();
+    });
+
+    it('should return int class', () => {
+      //given
+      component.environmentLabel = 'int';
+      //when
+      const result = component.getEnvLabelClass();
+      //then
+      expect(result['bg-warning']).toBeTruthy();
+      expect(result['bg-primary']).toBeFalsy();
+    });
+
+    it('should return class without color', () => {
+      //given
+      component.environmentLabel = 'prod';
+      //when
+      const result = component.getEnvLabelClass();
+      //then
+      expect(result['bg-warning']).toBeFalsy();
+      expect(result['bg-primary']).toBeFalsy();
+    });
   });
 });
