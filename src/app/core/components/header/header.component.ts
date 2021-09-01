945b93645c92de7473d79256bd363ca4de7e1a38
atlas <19825793+iuccio@users.noreply.github.com>
2021-09-01 10:35:34 +0200

    ATLAS-67: show version and env on header

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import packageJson from '../../../../../package.json';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-header',
@@ -6,9 +8,38 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  private readonly DEV: string = 'dev';
+  private readonly INT: string = 'int';
+
+  version: string = packageJson.version;
+  environmentLabel: string = environment.label;
+
   @Output() showSidebar = new EventEmitter<void>();
 
   toggle(): void {
     this.showSidebar.emit();
+  }
+
+  showLabel() {
+    return this.environmentLabel === this.DEV || this.environmentLabel === this.INT;
+  }
+
+  getEnvLabelClass() {
+    const envClass = {
+      badge: true,
+      'text-wrap': true,
+      'ms-2': true,
+      'bg-primary': false,
+      'bg-warning': false,
+    };
+    if (this.environmentLabel === this.DEV) {
+      envClass['bg-primary'] = true;
+      return envClass;
+    }
+    if (this.environmentLabel === this.INT) {
+      envClass['bg-warning'] = true;
+      return envClass;
+    }
+    return envClass;
   }
 }
