3db804509d3f9e8ccfcc017694c8b1938e7ee782
atlas <19825793+iuccio@users.noreply.github.com>
2021-09-03 15:46:10 +0200

    ATLAS-67: Try reactive ... pretty close

--- src/app/timetable-field-number-detail/timetable-field-number-detail.component.ts
+++ src/app/timetable-field-number-detail/timetable-field-number-detail.component.ts
@@ -2,35 +2,43 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TimetableFieldNumbersService, Version } from '../api';
 import { DetailWrapperController } from '../core/components/detail-wrapper/detail-wrapper-controller';
+import { TimetableFieldNumberDetailFormService } from './timetable-field-number-detail-form.service';
+import { Observable } from 'rxjs';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-timetable-field-number-detail',
   templateUrl: './timetable-field-number-detail.component.html',
   styleUrls: ['./timetable-field-number-detail.component.scss'],
 })
-export class TimetableFieldNumberDetailComponent extends DetailWrapperController {
-  version: Version = {};
-
+export class TimetableFieldNumberDetailComponent extends DetailWrapperController<Version> {
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
-    private timetableFieldNumberService: TimetableFieldNumbersService
+    private timetableFieldNumberService: TimetableFieldNumbersService,
+    public timetableFieldNumberDetailFormService: TimetableFieldNumberDetailFormService
   ) {
     super(activatedRoute);
   }
 
-  readRecord(): void {
-    this.timetableFieldNumberService
-      .getVersion(this.getId())
-      .subscribe((version) => (this.version = version));
+  readRecord(): Observable<Version> {
+    return this.timetableFieldNumberService.getVersion(this.getId());
+  }
+
+  getFormGroup(record: Version): FormGroup {
+    return this.timetableFieldNumberDetailFormService.buildVersionForm(record);
+  }
+
+  getTitle(record: Version): string | undefined {
+    return record.swissTimetableFieldNumber;
   }
 
   updateRecord(): void {
-    this.timetableFieldNumberService.updateVersion(this.getId(), this.version).subscribe();
+    this.timetableFieldNumberService.updateVersion(this.getId(), this.record).subscribe();
   }
 
   createRecord(): void {
-    this.timetableFieldNumberService.createVersion(this.version).subscribe();
+    this.timetableFieldNumberService.createVersion(this.record).subscribe();
   }
 
   deleteRecord(): void {
