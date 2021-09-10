00cf04f3831571c248a05d27649cd445ce6e6ab3
atlas <19825793+iuccio@users.noreply.github.com>
2021-09-10 11:45:27 +0200

    ATLAS-109: HTML Tests für Spinner

--- src/app/core/components/loading-spinner/loading-spinner.component.spec.ts
+++ src/app/core/components/loading-spinner/loading-spinner.component.spec.ts
@@ -1,23 +1,53 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoadingSpinnerComponent } from './loading-spinner.component';
+import { By } from '@angular/platform-browser';
+import { ChangeDetectionStrategy } from '@angular/core';
 
 describe('LoadingSpinnerComponent', () => {
   let component: LoadingSpinnerComponent;
   let fixture: ComponentFixture<LoadingSpinnerComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [LoadingSpinnerComponent],
-    }).compileComponents();
-  });
-
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [LoadingSpinnerComponent],
+    })
+      .overrideComponent(LoadingSpinnerComponent, {
+        set: { changeDetection: ChangeDetectionStrategy.Default },
+      })
+      .compileComponents();
+
     fixture = TestBed.createComponent(LoadingSpinnerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create spinning logo', () => {
+    component.isLoading = true;
+    fixture.detectChanges();
+
     expect(component).toBeTruthy();
+
+    const loadingSpinnerDiv = fixture.debugElement.query(By.css('.loading-spinner'));
+    expect(loadingSpinnerDiv).toBeTruthy();
+
+    expect(fixture.debugElement.query(By.css('.full-screen'))).toBeFalsy();
+  });
+
+  it('should create fullscreen spinning logo', () => {
+    component.isLoading = true;
+    component.fullScreen = true;
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.query(By.css('.full-screen'))).toBeTruthy();
+  });
+
+  it('should do nothing if not loading', () => {
+    component.isLoading = false;
+    fixture.detectChanges();
+
+    expect(component).toBeTruthy();
+
+    const loadingSpinnerDiv = fixture.debugElement.query(By.css('.loading-spinner'));
+    expect(loadingSpinnerDiv).toBeFalsy();
   });
 });
