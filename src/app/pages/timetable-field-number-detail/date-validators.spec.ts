9888017577631204276b4d4cc81966772c9910a2
atlas <19825793+iuccio@users.noreply.github.com>
2021-09-16 21:20:36 +0200

    ATLAS-114: code ref

--- src/app/pages/timetable-field-number-detail/date-validators.spec.ts
+++ src/app/pages/timetable-field-number-detail/date-validators.spec.ts
@@ -1,12 +1,12 @@
 import { DateValidators } from './date-validators';
 import { FormControl } from '@angular/forms';
-import moment from 'moment/moment';
-
 describe('Date Validator', () => {
   it('should return validation error when validFrom greater then validTo ', () => {
     //given
-    const validFromForm = new FormControl('12.12.2000');
-    const validToForm = new FormControl('12.12.1999');
+    const validFrom = new Date('12.12.2000');
+    const validTo = new Date('12.12.1999');
+    const validFromForm = new FormControl(validFrom);
+    const validToForm = new FormControl(validTo);
     //when
     DateValidators.validateDates(validFromForm, validToForm);
     //then
@@ -14,21 +14,21 @@ describe('Date Validator', () => {
     const dateRangeErrorValidFrom = validFromForm.errors?.['date_range_error'];
     expect(dateRangeErrorValidFrom).toBeDefined();
     expect(dateRangeErrorValidFrom.date).toBeDefined();
-    expect(dateRangeErrorValidFrom.date.validFrom).toBe('12.12.2000');
-    expect(dateRangeErrorValidFrom.date.validTo).toBe('12.12.1999');
+    expect(dateRangeErrorValidFrom.date.validFrom).toBe(validFrom);
+    expect(dateRangeErrorValidFrom.date.validTo).toBe(validTo);
 
     expect(validToForm.errors).toBeDefined();
     const dateRangeErrorValidTo = validFromForm.errors?.['date_range_error'];
     expect(dateRangeErrorValidTo).toBeDefined();
     expect(dateRangeErrorValidTo.date).toBeDefined();
-    expect(dateRangeErrorValidTo.date.validFrom).toBe('12.12.2000');
-    expect(dateRangeErrorValidTo.date.validTo).toBe('12.12.1999');
+    expect(dateRangeErrorValidTo.date.validFrom).toBe(validFrom);
+    expect(dateRangeErrorValidTo.date.validTo).toBe(validTo);
   });
 
   it('should remove validation error when validTo is greater then validFrom ', () => {
     //given
-    const validFrom = new Date(moment('2010-12-31 23:59:59').valueOf());
-    const validTo = new Date(moment('2011-12-31 23:59:59').valueOf());
+    const validFrom = new Date('2010-12-31');
+    const validTo = new Date('2011-12-31');
     const validFromForm = new FormControl(validFrom);
     const validToForm = new FormControl(validTo);
     validFromForm.setErrors({
@@ -55,8 +55,8 @@ describe('Date Validator', () => {
   });
   it('should return validation error when validFrom greater then validTo and dates have in the past validation error', () => {
     //given
-    const validFrom = new Date(moment('2010-12-31 23:59:59').valueOf());
-    const validTo = new Date(moment('2009-12-31 23:59:59').valueOf());
+    const validFrom = new Date('2010-12-31');
+    const validTo = new Date('2009-12-31 23:59:59');
     const validFromForm = new FormControl(validFrom);
     const validToForm = new FormControl(validTo);
     validFromForm.setErrors({
@@ -95,8 +95,8 @@ describe('Date Validator', () => {
 
   it('should successfully validate dates', () => {
     //given
-    const validFromForm = new FormControl('12.12.2000');
-    const validToForm = new FormControl('12.12.2001');
+    const validFromForm = new FormControl(new Date('12.12.2000'));
+    const validToForm = new FormControl(new Date('12.12.2001'));
     //when
     DateValidators.validateDates(validFromForm, validToForm);
     //then
