4a9eafe6d9f9c6166e8abc4cca7ba7d46aba99d6
atlas <TechSupport-ATLAS@sbb.ch>
2021-09-16 21:20:36 +0200

    ATLAS-114: code ref

--- src/app/pages/timetable-field-number-detail/date-validators.ts
+++ src/app/pages/timetable-field-number-detail/date-validators.ts
@@ -1,4 +1,4 @@
-import { AbstractControl, ValidatorFn } from '@angular/forms';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 export class DateValidators {
   static dateLessThan(validFrom: string, validTo: string): ValidatorFn {
@@ -11,39 +11,38 @@ export class DateValidators {
   }
 
   static validateDates(validFromForm: AbstractControl | null, validToForm: AbstractControl | null) {
-    if (validFromForm?.value !== null && validToForm?.value !== null) {
-      if (validFromForm?.value > validToForm?.value) {
-        const error = {
-          date_range_error: {
-            date: {
-              validFrom: validFromForm?.value,
-              validTo: validToForm?.value,
-            },
-          },
-        };
-        if (!validFromForm?.errors) {
-          validFromForm?.setErrors(error);
-        } else {
-          Object.assign(validFromForm?.errors, error);
-        }
-        if (!validToForm?.errors) {
-          validToForm?.setErrors(error);
-        } else {
-          Object.assign(validToForm?.errors, error);
-        }
-      } else {
-        if (validFromForm?.errors?.date_range_error) {
-          delete validFromForm.errors['date_range_error'];
-          if (Object.keys(validFromForm.errors).length === 0) {
-            validFromForm.setErrors(null);
-          }
-        }
-        if (validToForm?.errors?.date_range_error) {
-          delete validToForm.errors['date_range_error'];
-          if (Object.keys(validToForm.errors).length === 0) {
-            validToForm.setErrors(null);
-          }
-        }
+    const validFromValue = validFromForm?.value;
+    const validToValue = validToForm?.value;
+    if (validFromValue !== null && validToValue !== null && validFromValue > validToValue) {
+      const error: ValidationErrors = {
+        date_range_error: {
+          date: { validFrom: validFromValue, validTo: validToValue },
+        },
+      };
+      DateValidators.populateWithValidationErrors(validFromForm, error);
+      DateValidators.populateWithValidationErrors(validToForm, error);
+    } else {
+      DateValidators.clearValidationError(validFromForm);
+      DateValidators.clearValidationError(validToForm);
+    }
+  }
+
+  static populateWithValidationErrors(
+    controlForm: AbstractControl | null,
+    error: ValidationErrors
+  ) {
+    if (!controlForm?.errors) {
+      controlForm?.setErrors(error);
+    } else {
+      Object.assign(controlForm?.errors, error);
+    }
+  }
+
+  static clearValidationError(controlForm: AbstractControl | null) {
+    if (controlForm?.errors?.date_range_error) {
+      delete controlForm.errors['date_range_error'];
+      if (Object.keys(controlForm.errors).length === 0) {
+        controlForm.setErrors(null);
       }
     }
   }
