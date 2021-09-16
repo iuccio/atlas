9b17627488c8bcd69c769b6fa86f608980a071bb
atlas <19825793+iuccio@users.noreply.github.com>
2021-09-16 16:11:38 +0200

    ATLAS-114: add date validator tests

--- src/app/pages/timetable-field-number-detail/date-validators.ts
+++ src/app/pages/timetable-field-number-detail/date-validators.ts
@@ -5,42 +5,46 @@ export class DateValidators {
     return (c: AbstractControl): { [key: string]: boolean } | null => {
       const validFromForm = c.get(validFrom);
       const validToForm = c.get(validTo);
-      if (validFromForm?.value !== null && validToForm?.value !== null) {
-        if (validFromForm?.value > validToForm?.value) {
-          const error = {
-            date_range_error: {
-              date: {
-                validFrom: validFromForm?.value,
-                validTo: validToForm?.value,
-              },
+      DateValidators.validateDates(validFromForm, validToForm);
+      return null;
+    };
+  }
+
+  static validateDates(validFromForm: AbstractControl | null, validToForm: AbstractControl | null) {
+    if (validFromForm?.value !== null && validToForm?.value !== null) {
+      if (validFromForm?.value > validToForm?.value) {
+        const error = {
+          date_range_error: {
+            date: {
+              validFrom: validFromForm?.value,
+              validTo: validToForm?.value,
             },
-          };
-          if (!validFromForm?.errors) {
-            validFromForm?.setErrors(error);
-          } else {
-            Object.assign(validFromForm?.errors, error);
-          }
-          if (!validToForm?.errors) {
-            validToForm?.setErrors(error);
-          } else {
-            Object.assign(validToForm?.errors, error);
-          }
+          },
+        };
+        if (!validFromForm?.errors) {
+          validFromForm?.setErrors(error);
         } else {
-          if (validFromForm?.errors?.date_range_error) {
-            delete validFromForm.errors['date_range_error'];
-            if (Object.keys(validFromForm.errors).length === 0) {
-              validFromForm.setErrors(null);
-            }
+          Object.assign(validFromForm?.errors, error);
+        }
+        if (!validToForm?.errors) {
+          validToForm?.setErrors(error);
+        } else {
+          Object.assign(validToForm?.errors, error);
+        }
+      } else {
+        if (validFromForm?.errors?.date_range_error) {
+          delete validFromForm.errors['date_range_error'];
+          if (Object.keys(validFromForm.errors).length === 0) {
+            validFromForm.setErrors(null);
           }
-          if (validToForm?.errors?.date_range_error) {
-            delete validToForm.errors['date_range_error'];
-            if (Object.keys(validToForm.errors).length === 0) {
-              validToForm.setErrors(null);
-            }
+        }
+        if (validToForm?.errors?.date_range_error) {
+          delete validToForm.errors['date_range_error'];
+          if (Object.keys(validToForm.errors).length === 0) {
+            validToForm.setErrors(null);
           }
         }
       }
-      return null;
-    };
+    }
   }
 }
