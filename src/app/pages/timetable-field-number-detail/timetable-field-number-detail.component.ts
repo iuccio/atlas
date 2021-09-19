11eb1b460831184c5a8df497944c7d1a7bf92c25
atlas <19825793+iuccio@users.noreply.github.com>
2021-09-19 15:16:41 +0200

    ATLAS-114: smal clean up

--- src/app/pages/timetable-field-number-detail/timetable-field-number-detail.component.ts
+++ src/app/pages/timetable-field-number-detail/timetable-field-number-detail.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TimetableFieldNumbersService, Version } from '../../api';
 import { DetailWrapperController } from '../../core/components/detail-wrapper/detail-wrapper-controller';
-import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { formatDate } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ValidationError } from './validation-error';
 import moment from 'moment/moment';
 import { DateRangeValidator } from './date-range-validator';
@@ -21,6 +20,10 @@ export class TimetableFieldNumberDetailComponent
   TTFNID_PLACEHOLDER = 'ch:1:fpfnid:100000';
   VALID_TO_PLACEHOLDER = '31.12.2099';
   NAME_PLACEHOLDER = 'Grenze - Bad, Bahnhof - Basel SBB - Zürich HB - Chur';
+
+  DATE_PATTERN = 'DD.MM.yyyy';
+  MAX_LENGTH = 255;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
@@ -32,10 +35,6 @@ export class TimetableFieldNumberDetailComponent
 
   ngOnInit() {
     super.ngOnInit();
-  }
-
-  get f(): { [key: string]: AbstractControl } {
-    return this.form.controls;
   }
 
   get minDateValue(): Date {
@@ -74,17 +73,20 @@ export class TimetableFieldNumberDetailComponent
       {
         swissTimetableFieldNumber: [
           version.swissTimetableFieldNumber,
-          [Validators.required, Validators.maxLength(255)],
+          [Validators.required, Validators.maxLength(this.MAX_LENGTH)],
         ],
-        ttfnid: [version.ttfnid, [Validators.required, Validators.maxLength(255)]],
-        validFrom: [version.validFrom, [Validators.required, Validators.maxLength(255)]],
-        validTo: [version.validTo, [Validators.required, Validators.maxLength(255)]],
+        ttfnid: [version.ttfnid, [Validators.required, Validators.maxLength(this.MAX_LENGTH)]],
+        validFrom: [
+          version.validFrom,
+          [Validators.required, Validators.maxLength(this.MAX_LENGTH)],
+        ],
+        validTo: [version.validTo, [Validators.required, Validators.maxLength(this.MAX_LENGTH)]],
         businessOrganisation: [
           version.businessOrganisation,
-          [Validators.required, Validators.maxLength(255)],
+          [Validators.required, Validators.maxLength(this.MAX_LENGTH)],
         ],
-        number: [version.number, [Validators.required, Validators.maxLength(255)]],
-        name: [version.name, [Validators.required, Validators.maxLength(255)]],
+        number: [version.number, [Validators.required, Validators.maxLength(this.MAX_LENGTH)]],
+        name: [version.name, [Validators.required, Validators.maxLength(this.MAX_LENGTH)]],
         comment: [version.comment],
       },
       {
@@ -94,12 +96,12 @@ export class TimetableFieldNumberDetailComponent
   }
 
   getValidFromPlaceHolder() {
-    return formatDate(new Date(), 'dd.MM.yyyy', 'en-US');
+    return moment().format(this.DATE_PATTERN);
   }
 
   getValidation(inputForm: string) {
     const result: ValidationError[] = [];
-    const inputFormValidated = this.f[inputForm];
+    const inputFormValidated = this.form.controls[inputForm];
     const controlErrors = inputFormValidated?.errors;
     if (controlErrors) {
       Object.keys(controlErrors).forEach((keyError) => {
@@ -113,17 +115,17 @@ export class TimetableFieldNumberDetailComponent
   }
 
   displayDate(validationError: ValidationError) {
-    const pattern = 'DD.MM.yyyy';
-    if (validationError?.value['date']) {
-      const validFrom = validationError.value['date'].validFrom;
-      const validTo = validationError.value['date'].validTo;
+    const pattern = this.DATE_PATTERN;
+    if (validationError?.value.date) {
+      const validFrom = validationError.value.date.validFrom;
+      const validTo = validationError.value.date.validTo;
       return validFrom.format(pattern) + ' - ' + validTo.format(pattern);
     }
-    if (validationError?.value['min']) {
-      return validationError.value['min'].format(pattern);
+    if (validationError?.value.min) {
+      return validationError.value.min.format(pattern);
     }
-    if (validationError?.value['max']) {
-      return validationError.value['max'].format(pattern);
+    if (validationError?.value.max) {
+      return validationError.value.max.format(pattern);
     }
   }
 }
