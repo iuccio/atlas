de57c8c4732e2808c31cdd5f377842ea3608ed5f
atlas <19825793+iuccio@users.noreply.github.com>
2021-09-20 14:58:00 +0200

    ATLAS-116: add tests

--- src/app/core/notification/notification.service.ts
+++ src/app/core/notification/notification.service.ts
@@ -1,33 +1,34 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 import { TranslateService } from '@ngx-translate/core';
+import { NotificationParamMessage } from './notification-param-message';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NotificationService {
   SNACK_BAR_CONFIG: MatSnackBarConfig = {
-    duration: 3000,
+    duration: 5000,
     horizontalPosition: 'right',
     verticalPosition: 'top',
   };
 
   constructor(private snackBar: MatSnackBar, private translateService: TranslateService) {}
 
-  success(msg: string) {
-    this.notify(msg, 'success');
+  success(msg: string, param?: NotificationParamMessage) {
+    this.notify(msg, 'success', param);
   }
 
-  error(msg: string) {
-    this.notify(msg, 'error');
+  error(msg: string, param?: NotificationParamMessage) {
+    this.notify(msg, 'error', param);
   }
 
-  warning(msg: string) {
-    this.notify(msg, 'warning');
+  info(msg: string, param?: NotificationParamMessage) {
+    this.notify(msg, 'info', param);
   }
 
-  notify(msg: string, type: string) {
-    this.translateService.get(msg).subscribe((value) => {
+  notify(msg: string, type: string, param?: NotificationParamMessage) {
+    this.translateService.get(msg, param).subscribe((value) => {
       this.SNACK_BAR_CONFIG['panelClass'] = [type, 'notification'];
       this.snackBar.open(value, '', this.SNACK_BAR_CONFIG);
     });
