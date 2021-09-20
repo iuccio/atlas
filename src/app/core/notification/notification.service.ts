c41de7df4f3b9536c7bebc66fd0ac0ab8d26d7f7
atlas <TechSupport-ATLAS@sbb.ch>
2021-09-20 10:42:41 +0200

    ATLAS-116: add notification service

---
+++ src/app/core/notification/notification.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
+import { TranslateService } from '@ngx-translate/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NotificationService {
+  SNACK_BAR_CONFIG: MatSnackBarConfig = {
+    duration: 600000,
+    horizontalPosition: 'right',
+    verticalPosition: 'top',
+  };
+
+  constructor(private snackBar: MatSnackBar, private translateService: TranslateService) {}
+
+  success(msg: string) {
+    this.notify(msg, 'success');
+  }
+
+  error(msg: string) {
+    this.notify(msg, 'error');
+  }
+
+  warning(msg: string) {
+    this.notify(msg, 'warning');
+  }
+
+  notify(msg: string, type: string) {
+    this.translateService.get(msg).subscribe((value) => {
+      this.SNACK_BAR_CONFIG['panelClass'] = [type, 'notification'];
+      this.snackBar.open(value, '', this.SNACK_BAR_CONFIG);
+    });
+  }
+}
