683b91d45ac583f40e17cb29ed4ecf86b4652578
atlas <19825793+iuccio@users.noreply.github.com>
2021-09-20 21:27:03 +0200

    ATLAS-116: unsubscribe subscription to avoid memory leak

--- src/app/core/notification/notification.service.ts
+++ src/app/core/notification/notification.service.ts
@@ -1,12 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 import { TranslateService } from '@ngx-translate/core';
 import { NotificationParamMessage } from './notification-param-message';
+import { catchError, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
-export class NotificationService {
+export class NotificationService implements OnDestroy {
+  private ngUnsubscribe = new Subject<void>();
+
   SNACK_BAR_CONFIG: MatSnackBarConfig = {
     duration: 5000,
     horizontalPosition: 'right',
@@ -28,9 +32,23 @@ export class NotificationService {
   }
 
   notify(msg: string, type: string, param?: NotificationParamMessage) {
-    this.translateService.get(msg, param).subscribe((value) => {
-      this.SNACK_BAR_CONFIG['panelClass'] = [type, 'notification'];
-      this.snackBar.open(value, '', this.SNACK_BAR_CONFIG);
-    });
+    this.translateService
+      .get(msg, param)
+      .pipe(
+        takeUntil(this.ngUnsubscribe),
+        catchError((err) => {
+          console.log(err);
+          throw err;
+        })
+      )
+      .subscribe((value) => {
+        this.SNACK_BAR_CONFIG['panelClass'] = [type, 'notification'];
+        this.snackBar.open(value, '', this.SNACK_BAR_CONFIG);
+      });
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 }
