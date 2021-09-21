72a7d923c8196db787fee57344e5608f5a2943e6
atlas <TechSupport-ATLAS@sbb.ch>
2021-09-21 15:43:24 +0200

    ATLAS-115: Tests für Dialog

--- src/app/core/components/dialog/dialog.service.spec.ts
+++ src/app/core/components/dialog/dialog.service.spec.ts
@@ -1,17 +1,40 @@
 import { TestBed } from '@angular/core/testing';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { TranslateModule } from '@ngx-translate/core';
 import { DialogService } from './dialog.service';
+import { of } from 'rxjs';
+
+const dialogData = {
+  title: 'Question',
+  message: 'Do you want to be a rockstar?',
+};
 
 describe('DialogService', () => {
   let service: DialogService;
 
+  const dialogSpy = jasmine.createSpyObj('dialog', ['open']);
+
   beforeEach(() => {
-    TestBed.configureTestingModule({ imports: [TranslateModule.forRoot(), MatDialogModule] });
+    TestBed.configureTestingModule({
+      imports: [TranslateModule.forRoot()],
+      providers: [{ provide: MatDialog, useValue: dialogSpy }],
+    });
     service = TestBed.inject(DialogService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should open confirmation dialog and pass success value - true', () => {
+    dialogSpy.open.and.returnValue({ afterClosed: () => of(true) });
+
+    service.confirm(dialogData).subscribe((result) => expect(result).toBeTrue());
+
+    expect(dialogSpy.open).toHaveBeenCalled();
+  });
+
+  it('should open confirmation dialog and pass cancel value - false', () => {
+    dialogSpy.open.and.returnValue({ afterClosed: () => of(false) });
+
+    service.confirm(dialogData).subscribe((result) => expect(result).toBeFalse());
+
+    expect(dialogSpy.open).toHaveBeenCalled();
   });
 });
