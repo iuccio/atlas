82265b023d02418e4e6cbc315bac73a669df0bf9
atlas <19825793+iuccio@users.noreply.github.com>
2021-09-22 16:04:14 +0200

    ATLAS-115: More unit tests for detailwrapper

---
+++ src/app/core/components/detail-wrapper/detail-wrapper-controller.spec.ts
@@ -0,0 +1,164 @@
+import { DetailWrapperController } from './detail-wrapper-controller';
+import { OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Record } from './record';
+import { DialogService } from '../dialog/dialog.service';
+import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+
+describe('DetailWrapperController', () => {
+  const dummyController = jasmine.createSpyObj('controller', [
+    'backToOverview',
+    'createRecord',
+    'deleteRecord',
+    'updateRecord',
+  ]);
+  let record: Record;
+
+  class DummyWrapperController extends DetailWrapperController<Record> implements OnInit {
+    constructor() {
+      super(dialogService);
+    }
+
+    backToOverview(): void {
+      dummyController.backToOverview();
+    }
+
+    createRecord(): void {
+      dummyController.createRecord();
+    }
+
+    deleteRecord(): void {
+      dummyController.deleteRecord();
+    }
+
+    getFormGroup(value: Record): FormGroup {
+      return new FormBuilder().group({
+        value: [value.id],
+      });
+    }
+
+    getTitle(record: Record): string | undefined {
+      return record.id ? +record.id + '' : undefined;
+    }
+
+    readRecord(): Record {
+      return record;
+    }
+
+    updateRecord(): void {
+      dummyController.updateRecord();
+    }
+  }
+
+  let controller: DummyWrapperController;
+  const dialogServiceSpy = jasmine.createSpyObj(['confirm']);
+  let dialogService: DialogService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [{ provide: DialogService, useValue: dialogServiceSpy }],
+    });
+    dialogService = TestBed.inject(DialogService);
+  });
+
+  describe('existing record', () => {
+    beforeEach(() => {
+      record = { id: 1 };
+      controller = new DummyWrapperController();
+      controller.ngOnInit();
+    });
+
+    it('should create', () => {
+      expect(controller).toBeTruthy();
+    });
+
+    it('should initialize heading', () => {
+      expect(controller.heading).toBe('1');
+    });
+
+    it('should initialize form', () => {
+      expect(controller.isNewRecord()).toBeFalse();
+      expect(controller.isExistingRecord()).toBeTrue();
+      expect(controller.form.enabled).toBeFalse();
+    });
+
+    it('should toggle edit form', () => {
+      dialogServiceSpy.confirm.and.returnValue(of(true));
+
+      expect(controller.form.enabled).toBeFalse();
+
+      controller.toggleEdit();
+      expect(controller.form.enabled).toBeTrue();
+
+      controller.toggleEdit();
+      expect(controller.form.enabled).toBeFalse();
+    });
+
+    it('should ask for confirmation to cancel when dirty', () => {
+      dialogServiceSpy.confirm.and.returnValue(of(false));
+
+      controller.toggleEdit();
+      controller.form.markAsDirty();
+
+      controller.toggleEdit();
+      expect(controller.form.enabled).toBeTrue();
+    });
+
+    it('should update on save', () => {
+      controller.toggleEdit();
+      controller.form.markAsDirty();
+      controller.save();
+
+      expect(dummyController.updateRecord).toHaveBeenCalled();
+    });
+
+    it('should delete on confirm', () => {
+      dialogServiceSpy.confirm.and.returnValue(of(true));
+      controller.delete();
+
+      expect(dummyController.deleteRecord).toHaveBeenCalled();
+    });
+
+    it('should not delete on cancel', () => {
+      dialogServiceSpy.confirm.and.returnValue(of(false));
+      controller.delete();
+
+      expect(dummyController.deleteRecord).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('new record', () => {
+    beforeEach(() => {
+      record = {};
+      controller = new DummyWrapperController();
+      controller.ngOnInit();
+    });
+
+    it('should initialize heading', () => {
+      expect(controller.heading).toBeUndefined();
+    });
+
+    it('should initialize form', () => {
+      expect(controller.isNewRecord()).toBeTrue();
+      expect(controller.isExistingRecord()).toBeFalse();
+      expect(controller.form.enabled).toBeTrue();
+    });
+
+    it('should create on save', () => {
+      controller.form.markAsDirty();
+      controller.save();
+
+      expect(dummyController.createRecord).toHaveBeenCalled();
+    });
+
+    it('should go back to overview on cancel', () => {
+      dialogServiceSpy.confirm.and.returnValue(of(true));
+
+      controller.form.markAsDirty();
+      controller.toggleEdit();
+
+      expect(dummyController.backToOverview).toHaveBeenCalled();
+    });
+  });
+});
