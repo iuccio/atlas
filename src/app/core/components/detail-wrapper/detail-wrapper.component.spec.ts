78ef9ee6348e8b94c265684a6579f5f3a0b65840
atlas <19825793+iuccio@users.noreply.github.com>
2021-09-22 16:04:14 +0200

    ATLAS-115: More unit tests for detailwrapper

--- src/app/core/components/detail-wrapper/detail-wrapper.component.spec.ts
+++ src/app/core/components/detail-wrapper/detail-wrapper.component.spec.ts
@@ -11,10 +11,65 @@ describe('DetailWrapperComponent', () => {
   /*eslint-disable */
   let component: DetailWrapperComponent<any>;
   let fixture: ComponentFixture<DetailWrapperComponent<any>>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [DetailWrapperComponent],
+      imports: [
+        MaterialModule,
+        TranslateModule.forRoot({
+          loader: { provide: TranslateLoader, useClass: TranslateFakeLoader },
+        }),
+      ],
+    }).compileComponents();
+  });
+
+  function init(controller: DetailWrapperController<any>) {
+    fixture = TestBed.createComponent(DetailWrapperComponent);
+    component = fixture.componentInstance;
+    component.controller = controller;
+    component.canEdit = true;
+    fixture.detectChanges();
+  }
   /*eslint-enable */
 
+  describe('disabled', (dummyController = createDummyForm(false)) => {
+    beforeEach(() => init(dummyController));
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should switch from disabled to enabled', () => {
+      const editButton = fixture.debugElement.query(By.css('.bi-pencil-fill'));
+      editButton.nativeElement.click();
+
+      expect(dummyController.toggleEdit).toHaveBeenCalled();
+    });
+
+    it('should delete record', () => {
+      const editButton = fixture.debugElement.query(By.css('.bi-trash-fill'));
+      editButton.nativeElement.click();
+
+      expect(dummyController.delete).toHaveBeenCalled();
+    });
+  });
+
+  describe('enabled', (dummyController = createDummyForm(true)) => {
+    beforeEach(() => init(dummyController));
+
+    it('should save and disable form', () => {
+      const submitButton = fixture.debugElement.query(By.css('[type=submit]'));
+      submitButton.nativeElement.click();
+
+      expect(dummyController.save).toHaveBeenCalled();
+    });
+  });
+});
+
+function createDummyForm(enabledForm: boolean) {
   const form = jasmine.createSpyObj('form', ['enable', 'disable'], {
-    enabled: true,
+    enabled: enabledForm,
     dirty: true,
     valid: true,
   });
@@ -30,6 +85,7 @@ describe('DetailWrapperComponent', () => {
       'confirmLeave',
       'validateAllFormFields',
       'ngOnInit',
+      'delete',
     ],
     {
       heading: undefined,
@@ -44,41 +100,5 @@ describe('DetailWrapperComponent', () => {
   dummyController.toggleEdit.and.callFake(DetailWrapperController.prototype.toggleEdit);
   dummyController.confirmLeave.and.returnValue(of(true));
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [DetailWrapperComponent],
-      imports: [
-        MaterialModule,
-        TranslateModule.forRoot({
-          loader: { provide: TranslateLoader, useClass: TranslateFakeLoader },
-        }),
-      ],
-    }).compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DetailWrapperComponent);
-    component = fixture.componentInstance;
-    component.controller = dummyController;
-    component.canEdit = true;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
-  it('should switch from disabled to enabled', () => {
-    const editButton = fixture.debugElement.query(By.css('.bi-pencil-fill'));
-    editButton.nativeElement.click();
-
-    expect(dummyController.toggleEdit).toHaveBeenCalled();
-  });
-
-  it('should save and disable form', () => {
-    const submitButton = fixture.debugElement.query(By.css('[type=submit]'));
-    submitButton.nativeElement.click();
-
-    expect(dummyController.save).toHaveBeenCalled();
-  });
-});
+  return dummyController;
+}
