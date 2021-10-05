03e2dafa4b21b66c5eaee603d1e2f81c6ab50f25
atlas <19825793+iuccio@users.noreply.github.com>
2021-10-05 22:11:25 +0200

    ATLAS-106: add test scenario 1

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -41,3 +41,71 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('loginClientCredentials', () => {
+  console.log('alternative login');
+  return cy
+    .visit(`${Cypress.env('Host')}/`)
+    .request({
+      url: Cypress.env('authority') + '/oauth2/v2.0/token',
+      method: 'POST',
+      body: {
+        grant_type: 'password',
+        client_id: Cypress.env('clientId'),
+        client_secret: Cypress.env('clientSecret'),
+        scope: Cypress.env('apiScopes')[0],
+        username: Cypress.env('username'),
+        password: Cypress.env('password'),
+      },
+      form: true,
+    })
+    .then((response) => {
+      console.log(response);
+    });
+});
+
+Cypress.Commands.add('loginWithCredentials', () => {
+  Cypress.log({
+    name: 'loginViaAuth0',
+  });
+
+  const options = {
+    method: 'POST',
+    url: `https://login.microsoftonline.com/2cda5d11-f0ac-46b3-967d-af1b2e1bd01a/oauth2/token`,
+    body: {
+      grant_type: 'password',
+      username: 'fxatl_r@sbb.ch',
+      password: 'ATLAS%r3ader',
+      resource: 'http://localhost:4200',
+      scope: Cypress.env('apiScopes')[0],
+      client_id: Cypress.env('clientId'),
+      client_secret: Cypress.env('clientSecret'),
+    },
+    form: true,
+  };
+  cy.request(options);
+});
+
+Cypress.Commands.add('login', () => {
+  return cy
+    .visit(`${Cypress.env('Host')}`)
+    .request({
+      method: 'POST',
+      url: `https://login.microsoftonline.com/${Cypress.env('tenantId')}/oauth2/token`,
+      form: true,
+      body: {
+        grant_type: 'client_credentials',
+        client_id: Cypress.env('clientId'),
+        client_secret: Cypress.env('clientSecret'),
+      },
+    })
+    .then((response) => {
+      console.log(response);
+      const accessToken = response.body.access_token;
+      const expiresOn = response.body.expires_on;
+      sessionStorage.setItem('access_token', accessToken);
+      sessionStorage.setItem('auth_token', accessToken);
+      sessionStorage.setItem('expires_at', expiresOn);
+      sessionStorage.setItem('access_token_stored_at', expiresOn);
+    });
+});
