6052c7bc00764c7922ded8a02ca2dd42c036d7d1
atlas <TechSupport-ATLAS@sbb.ch>
2021-10-07 15:36:12 +0200

    ATLAS-106: fix login

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,3 +1,14 @@
 // Plugins enable you to tap into, modify, or extend the internal behavior of Cypress
 // For more info, visit https://on.cypress.io/plugins-api
 module.exports = (on, config) => {};
+
+declare namespace Cypress {
+  interface Chainable<Subject> {
+    /**
+     * Login with test user
+     * @example
+     * cy.login()
+     */
+    login(): Chainable<any>;
+  }
+}
