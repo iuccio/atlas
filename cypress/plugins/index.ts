4f4fe63c43c8a8c3dd1a82befa793ff055615f02
atlas <19825793+iuccio@users.noreply.github.com>
2021-10-11 21:04:11 +0200

    ATLAS-106: add cypress-failed-lod, improve doc

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,15 +1,13 @@
 // Plugins enable you to tap into, modify, or extend the internal behavior of Cypress
 // For more info, visit https://on.cypress.io/plugins-api
-module.exports = (on, config) => {};
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import cypress_failed_log from 'cypress-failed-log/src/failed';
 
-// eslint-disable-next-line @typescript-eslint/no-namespace
-declare namespace Cypress {
-  interface Chainable<Subject> {
-    /**
-     * Login with test user
-     * @example
-     * cy.login()
-     */
-    login(): Chainable<any>;
-  }
-}
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+export default (on, config) => {
+  on('task', {
+    failed: cypress_failed_log(),
+  });
+};
