316926b85908bed02f234033b80633e0fce03f5a
atlas <19825793+iuccio@users.noreply.github.com>
2021-10-12 11:02:08 +0200

    ATLAS-10: Tests ffor tables

--- src/app/pages/lidi/sublines/sublines.component.spec.ts
+++ src/app/pages/lidi/sublines/sublines.component.spec.ts
@@ -1,8 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { AuthService } from '../../../core/auth/auth.service';
-import { TimetableFieldNumbersService, VersionsContainer } from '../../../api/ttfn';
-import { MaterialModule } from '../../../core/module/material.module';
 import { TranslateFakeLoader, TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { of } from 'rxjs';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
@@ -10,19 +6,17 @@ import { TableComponent } from '../../../core/components/table/table.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { LoadingSpinnerComponent } from '../../../core/components/loading-spinner/loading-spinner.component';
+import { SublinesService, VersionsContainerSublineVersion } from '../../../api/lidi';
 import { SublinesComponent } from './sublines.component';
+import { CoreModule } from '../../../core/module/core.module';
 
-const authServiceMock: Partial<AuthService> = {
-  loggedIn: true,
-};
-
-const versionContainer: VersionsContainer = {
+const versionContainer: VersionsContainerSublineVersion = {
   versions: [
     {
       id: 1,
-      ttfnid: 'ttfnid',
-      name: 'name',
-      swissTimetableFieldNumber: 'asdf',
+      slnid: 'slnid',
+      shortName: 'name',
+      description: 'asdf',
       status: 'ACTIVE',
       validFrom: new Date('2021-06-01'),
       validTo: new Date('2029-06-01'),
@@ -31,27 +25,18 @@ const versionContainer: VersionsContainer = {
   totalCount: 1,
 };
 
-// With Mock
-// const timetableFieldNumberMockService : Partial<TimetableFieldNumbersService> = {
-//   getVersions(): Observable<any> {
-//     return of([versionContainer]);
-//   }
-// }
-
-describe('HomeComponent', () => {
+describe('SublinesComponent', () => {
   let component: SublinesComponent;
   let fixture: ComponentFixture<SublinesComponent>;
 
   // With Spy
-  const timetableFieldNumberService = jasmine.createSpyObj('timetableFieldNumbersService', [
-    'getVersions',
-  ]);
-  timetableFieldNumberService.getVersions.and.returnValue(of(versionContainer));
+  const sublinesService = jasmine.createSpyObj('linesService', ['getSublineVersions']);
+  sublinesService.getSublineVersions.and.returnValue(of(versionContainer));
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [SublinesComponent, TableComponent, LoadingSpinnerComponent],
       imports: [
-        MaterialModule,
+        CoreModule,
         HttpClientTestingModule,
         BrowserAnimationsModule,
         RouterModule.forRoot([]),
@@ -59,10 +44,7 @@ describe('HomeComponent', () => {
           loader: { provide: TranslateLoader, useClass: TranslateFakeLoader },
         }),
       ],
-      providers: [
-        { provide: AuthService, useValue: authServiceMock },
-        { provide: TimetableFieldNumbersService, useValue: timetableFieldNumberService },
-      ],
+      providers: [{ provide: SublinesService, useValue: sublinesService }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SublinesComponent);
@@ -71,9 +53,7 @@ describe('HomeComponent', () => {
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-    fixture.detectChanges();
-    expect(timetableFieldNumberService.getVersions).toHaveBeenCalled();
+    expect(sublinesService.getSublineVersions).toHaveBeenCalled();
 
     expect(component.sublineVersions.length).toBe(1);
     expect(component.totalCount$).toBe(1);
