14498db8baa117004707468147e3ae4fec02a521
atlas <TechSupport-ATLAS@sbb.ch>
2021-10-12 17:24:06 +0200

    ATLAS-10: add atlas home

--- src/app/pages/module/pages.module.ts
+++ src/app/pages/module/pages.module.ts
@@ -3,9 +3,10 @@ import { HomeComponent } from '../home/home.component';
 import { CoreModule } from '../../core/module/core.module';
 import { LidiModule } from '../lidi/lidi.module';
 import { TtfnModule } from '../ttfn/ttfn.module';
+import { AppRoutingModule } from '../../app-routing.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CoreModule, LidiModule, TtfnModule],
+  imports: [CoreModule, LidiModule, TtfnModule, AppRoutingModule],
 })
 export class PagesModule {}
--- src/app/pages/pages.ts
+++ src/app/pages/pages.ts
@@ -5,18 +5,21 @@ export class Pages {
     title: 'PAGES.HOME',
     path: '',
     icon: 'bi-house-fill',
+    description: '',
   };
 
   public static TTFN: Page = {
-    title: 'PAGES.TTFN',
+    title: 'PAGES.TTFN.TITLE',
     path: 'timetable-field-number',
     icon: 'bi-book-half',
+    description: 'PAGES.TTFN.DESCRIPTION',
   };
 
   public static LIDI: Page = {
-    title: 'PAGES.LIDI',
+    title: 'PAGES.LIDI.TITLE',
     path: 'line-directory',
     icon: 'bi-signpost-split',
+    description: 'PAGES.LIDI.DESCRIPTION',
   };
 
   public static pages: Page[] = [Pages.HOME, Pages.TTFN, Pages.LIDI];
