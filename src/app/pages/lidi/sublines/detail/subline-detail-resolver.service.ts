a0fb2d57880bbf2c3ddbd7f4c3829374e6abaccd
atlas <TechSupport-ATLAS@sbb.ch>
2021-10-15 11:45:25 +0200

    ATLAS-143: Formular for sublines

---
+++ src/app/pages/lidi/sublines/detail/subline-detail-resolver.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, Router } from '@angular/router';
+import { catchError, EMPTY, Observable, of } from 'rxjs';
+import { SublinesService, SublineVersion } from '../../../../api/lidi';
+import { Pages } from '../../../pages';
+
+@Injectable({ providedIn: 'root' })
+export class SublineDetailResolver implements Resolve<SublineVersion> {
+  constructor(private sublinesService: SublinesService, private router: Router) {}
+
+  resolve(route: ActivatedRouteSnapshot): Observable<SublineVersion> {
+    const idParameter = route.paramMap.get('id') || '';
+    return idParameter === 'add'
+      ? of({})
+      : this.sublinesService.getSublineVersion(parseInt(idParameter)).pipe(
+          catchError(() => {
+            this.router.navigate([Pages.LIDI.path]).then();
+            return EMPTY;
+          })
+        );
+  }
+}
---
+++ src/app/pages/lidi/sublines/detail/subline-detail.resolver.spec.ts
@@ -0,0 +1,50 @@
+import { TestBed } from '@angular/core/testing';
+import { ActivatedRouteSnapshot, convertToParamMap, RouterModule } from '@angular/router';
+import { of } from 'rxjs';
+import { SublinesService, SublineVersion } from '../../../../api/lidi';
+import { SublineDetailResolver } from './subline-detail-resolver.service';
+import StatusEnum = SublineVersion.StatusEnum;
+
+const version: SublineVersion = {
+  id: 1234,
+  slnid: 'slnid',
+  shortName: 'name',
+  description: 'asdf',
+  status: 'ACTIVE',
+  validFrom: new Date('2021-06-01'),
+  validTo: new Date('2029-06-01'),
+};
+
+describe('SublineDetailResolver', () => {
+  const sublinesServiceSpy = jasmine.createSpyObj('sublinesService', ['getSublineVersion']);
+  sublinesServiceSpy.getSublineVersion.and.returnValue(of(version));
+
+  let resolver: SublineDetailResolver;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterModule.forRoot([])],
+      providers: [
+        SublineDetailResolver,
+        { provide: SublinesService, useValue: sublinesServiceSpy },
+      ],
+    });
+    resolver = TestBed.inject(SublineDetailResolver);
+  });
+
+  it('should create', () => {
+    expect(resolver).toBeTruthy();
+  });
+
+  it('should get version from service to display', () => {
+    const mockRoute = { paramMap: convertToParamMap({ id: '1234' }) } as ActivatedRouteSnapshot;
+
+    const resolvedVersion = resolver.resolve(mockRoute);
+
+    resolvedVersion.subscribe((version) => {
+      expect(version.id).toBe(1234);
+      expect(version.status).toBe(StatusEnum.Active);
+      expect(version.slnid).toBe('slnid');
+    });
+  });
+});
