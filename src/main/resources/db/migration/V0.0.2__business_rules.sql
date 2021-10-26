f664bb947872d16005b4bbe4ce232ce5d14602a7
atlas <19825793+iuccio@users.noreply.github.com>
2021-10-26 11:32:23 +0200

    ATLAS-22: Update Entities with Business Rules

--- src/main/resources/db/migration/V0.0.2__business_rules.sql
+++ src/main/resources/db/migration/V0.0.2__business_rules.sql
@@ -12,6 +12,8 @@ ALTER TABLE line_version
 
 -- Mandatory for Line
 ALTER TABLE line_version
+    ALTER COLUMN swiss_line_number set NOT NULL;
+ALTER TABLE line_version
     ALTER COLUMN slnid set NOT NULL;
 ALTER TABLE line_version
     ALTER COLUMN status set NOT NULL;
@@ -33,6 +35,23 @@ ALTER TABLE line_version
 
 -- Mandatory for Subline
 ALTER TABLE subline_version
+    ALTER COLUMN swiss_subline_number set NOT NULL;
+ALTER TABLE subline_version
     ALTER COLUMN status set NOT NULL;
 ALTER TABLE subline_version
     ALTER COLUMN slnid set NOT NULL;
+ALTER TABLE subline_version
+    ALTER COLUMN type set NOT NULL;
+ALTER TABLE subline_version
+    ALTER COLUMN payment_type set NOT NULL;
+ALTER TABLE subline_version
+    ALTER COLUMN valid_from set NOT NULL;
+ALTER TABLE subline_version
+    ALTER COLUMN valid_to set NOT NULL;
+ALTER TABLE subline_version
+    ALTER COLUMN business_organisation set NOT NULL;
+
+ALTER TABLE subline_version
+    ALTER COLUMN creation_date set NOT NULL;
+ALTER TABLE subline_version
+    ALTER COLUMN edition_date set NOT NULL;
