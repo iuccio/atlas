e3f371cc06dbddc160a09cf55fc079a974a9c102
atlas <19825793+iuccio@users.noreply.github.com>
2021-11-03 16:37:08 +0100

    add tests and documentation for AtlasVersionable annotation

--- src/main/java/ch/sbb/timetable/field/number/versioning/annotation/AtlasVersionableProperty.java
+++ src/main/java/ch/sbb/timetable/field/number/versioning/annotation/AtlasVersionableProperty.java
@@ -6,10 +6,20 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Mark a property to be versioned. See {@link AtlasVersionable}.
+ */
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ ElementType.FIELD })
+@Target({ElementType.FIELD})
 public @interface AtlasVersionableProperty {
+
   String key() default "";
-  String[] relationsFields() default {};
+
   RelationType relationType() default RelationType.NONE;
+
+  /**
+   * Used to specify which field of the related Object ({@link RelationType#ONE_TO_MANY} or {@link RelationType#ONE_TO_ONE})
+   * must be versioned. See {@link AtlasVersionable}.
+   */
+  String[] relationsFields() default {};
 }
