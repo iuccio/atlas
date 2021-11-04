fe95ea071e8f1e1a5be7ae1a8f7725d891839d7f
atlas <19825793+iuccio@users.noreply.github.com>
2021-11-04 16:21:54 +0100

    ATLAS-32: Remove unused stuff

--- src/main/java/ch/sbb/timetable/field/number/service/UserService.java
+++ src/main/java/ch/sbb/timetable/field/number/service/UserService.java
@@ -1,7 +1,6 @@
 package ch.sbb.timetable.field.number.service;
 
 
-import java.util.Map;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 
@@ -11,12 +10,8 @@ public class UserService {
     return getAccessToken().getClaimAsString("sbbuid");
   }
 
-  public static Map<String, Object> getClaims() {
-    return getAccessToken().getClaims();
-  }
-
   private static Jwt getAccessToken() {
-    return ((Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+    return (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
   }
 
   private UserService() {
