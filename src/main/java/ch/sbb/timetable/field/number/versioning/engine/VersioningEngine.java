306afdbb44ea21fe571432dc7e6db0f27da2413b
atlas <TechSupport-ATLAS@sbb.ch>
2021-11-08 11:51:28 +0100

    ATÃLAS-133: code clean up

--- src/main/java/ch/sbb/timetable/field/number/versioning/engine/VersioningEngine.java
+++ src/main/java/ch/sbb/timetable/field/number/versioning/engine/VersioningEngine.java
@@ -9,7 +9,6 @@ import ch.sbb.timetable.field.number.versioning.model.VersionedObject;
 import ch.sbb.timetable.field.number.versioning.version.Versioning;
 import ch.sbb.timetable.field.number.versioning.version.VersioningWhenValidFromAndValidToAreNotEdited;
 import ch.sbb.timetable.field.number.versioning.version.VersioningWhenValidToAndValidFromAreEdited;
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
@@ -33,43 +32,19 @@ public class VersioningEngine {
     //Temporal sort objects versioning
     objectsToVersioning.sort(Comparator.comparing(o -> o.getVersionable().getValidFrom()));
 
-    List<VersionedObject> versionedObjects = new ArrayList<>();
-
     Versioning versioning;
-    //validFrom and validTo are not modified
     if (areValidToAndValidFromNotEdited(currentVersion, editedVersion)) {
       //update actual version
       log.info("ValidFrom and ValidTo are not edited.");
       versioning = new VersioningWhenValidFromAndValidToAreNotEdited();
       return versioning.applyVersioning(currentVersion, editedEntity, objectsToVersioning);
     }
-//
-//    //only validFrom is modified
-//    if (isOnlyValidFromEdited(currentVersion, editedVersion)) {
-//      log.info("Only ValidFrom is edited.");
-//      versioning = new VersioningWhenOnlyValidFromIsEdited();
-//      return versioning.applyVersioning(editedVersion, currentVersion, objectsToVersioning,
-//          editedEntity);
-//    }
-//
-//    //only validTo is modified
-//    if (isOnlyValidToEdited(currentVersion, editedVersion)) {
-//      //get all versions between actual.getValidFrom() and edited.getValidTo()
-//      log.info("Only ValidTo is edited.");
-//      versioning = new VersioningWhenOnlyValidToIsEdited();
-//      return versioning.applyVersioning(editedVersion, currentVersion, objectsToVersioning,
-//          editedEntity);
-//    }
-
-    //validFrom and validTo are modified
-    if (areValidFromAndValidToEdited(editedVersion)) {
+    else {
       log.info("ValidFrom and ValidTo are edited.");
       versioning = new VersioningWhenValidToAndValidFromAreEdited();
       return versioning.applyVersioning(editedVersion, currentVersion, objectsToVersioning,
           editedEntity);
     }
-
-    return versionedObjects;
   }
 
   public boolean areValidToAndValidFromNotEdited(Versionable currentVersion,
@@ -77,20 +52,6 @@ public class VersioningEngine {
     return (editedVersion.getValidFrom() == null && editedVersion.getValidTo() == null) || (
         currentVersion.getValidFrom().equals(editedVersion.getValidFrom())
             && currentVersion.getValidTo().equals(editedVersion.getValidTo()));
-  }
-
-  public boolean isOnlyValidFromEdited(Versionable currentVersion, Versionable editedVersion) {
-    return (editedVersion.getValidFrom() != null && (editedVersion.getValidTo() == null
-        || currentVersion.getValidTo().equals(editedVersion.getValidTo())));
-  }
-
-  public boolean isOnlyValidToEdited(Versionable currentVersion, Versionable editedVersion) {
-    return (editedVersion.getValidTo() != null && (editedVersion.getValidFrom() == null
-        || currentVersion.getValidFrom().equals(editedVersion.getValidFrom())));
-  }
-
-  public boolean areValidFromAndValidToEdited(Versionable editedVersion) {
-    return editedVersion.getValidFrom() != null || editedVersion.getValidTo() != null;
   }
 
 }
