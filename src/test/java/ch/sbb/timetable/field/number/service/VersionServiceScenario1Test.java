4aeae825d47bb9f8cf4f4aa19353881a34993a04
atlas <19825793+iuccio@users.noreply.github.com>
2021-11-09 15:35:53 +0100

    ATLAS-134: define scenario 1d

--- src/test/java/ch/sbb/timetable/field/number/service/VersionServiceScenario1Test.java
+++ src/test/java/ch/sbb/timetable/field/number/service/VersionServiceScenario1Test.java
@@ -217,4 +217,78 @@ public class VersionServiceScenario1Test extends BaseVersionServiceTest {
     assertThat(thirdTemporalVersion.getComment()).isNull();
   }
 
+  /**
+   * Szenario 1d
+   *
+   * NEU:                 |______________________|
+   * IST:      |----------|----------|----------|----------|----------|
+   * Version:        1          2          3          4         5
+   *
+   * RESULTAT: |----------|----------|----------|----------|----------|
+   * Version:        1          2          3          4         5         version 2 und 3 werden nur UPDATED
+   */
+  @Test
+  public void scenario1d() {
+    //given
+    version1.setName("SBB1");
+    version1 = versionRepository.save(version1);
+    version2.setName("SBB2");
+    version2 = versionRepository.save(version2);
+    version3.setName("SBB3");
+    version3 = versionRepository.save(version3);
+    version4.setName("SBB1");
+    version4 = versionRepository.save(version4);
+    version5.setName("SBB4");
+    version5 = versionRepository.save(version5);
+    Version editedVersion = new Version();
+    editedVersion.setName("SBB1");
+    editedVersion.setValidFrom(version2.getValidFrom());
+    editedVersion.setValidTo(version3.getValidTo());
+
+    //when
+    versionService.updateVersion(version2, editedVersion);
+    List<Version> result = versionRepository.getAllVersionsVersioned(version1.getTtfnid());
+
+    //then
+    assertThat(result).isNotNull();
+    assertThat(result.size()).isEqualTo(5);
+    result.sort(Comparator.comparing(Version::getValidFrom));
+
+    Version firstTemporalVersion = result.get(0);
+    assertThat(firstTemporalVersion.getValidFrom()).isEqualTo(LocalDate.of(2020, 1, 1));
+    assertThat(firstTemporalVersion.getValidTo()).isEqualTo(LocalDate.of(2021, 12, 31));
+    assertThat(firstTemporalVersion.getName()).isEqualTo("SBB1");
+    assertThat(firstTemporalVersion.getComment()).isNull();
+    assertThat(firstTemporalVersion.getLineRelations()).isEmpty();
+
+    Version secondTemporalVersion = result.get(1);
+    assertThat(secondTemporalVersion.getValidFrom()).isEqualTo(LocalDate.of(2022, 1, 1));
+    assertThat(secondTemporalVersion.getValidTo()).isEqualTo(LocalDate.of(2023, 12, 31));
+    assertThat(secondTemporalVersion.getName()).isEqualTo("SBB1");
+    assertThat(secondTemporalVersion.getComment()).isNull();
+    assertThat(secondTemporalVersion.getLineRelations()).isEmpty();
+
+    Version thirdTemporalVersion = result.get(2);
+    assertThat(thirdTemporalVersion.getValidFrom()).isEqualTo(LocalDate.of(2024, 1, 1));
+    assertThat(thirdTemporalVersion.getValidTo()).isEqualTo(LocalDate.of(2024, 12, 31));
+    assertThat(thirdTemporalVersion.getName()).isEqualTo("SBB1");
+    assertThat(thirdTemporalVersion.getComment()).isNull();
+    assertThat(thirdTemporalVersion.getLineRelations()).isEmpty();
+
+    Version fourthTemporalVersion = result.get(3);
+    assertThat(fourthTemporalVersion.getValidFrom()).isEqualTo(LocalDate.of(2025, 1, 1));
+    assertThat(fourthTemporalVersion.getValidTo()).isEqualTo(LocalDate.of(2025, 12, 31));
+    assertThat(fourthTemporalVersion.getName()).isEqualTo("SBB1");
+    assertThat(fourthTemporalVersion.getComment()).isNull();
+    assertThat(fourthTemporalVersion.getLineRelations()).isEmpty();
+
+    Version fifthTemporalVersion = result.get(4);
+    assertThat(fifthTemporalVersion.getValidFrom()).isEqualTo(LocalDate.of(2026, 1, 1));
+    assertThat(fifthTemporalVersion.getValidTo()).isEqualTo(LocalDate.of(2026, 12, 31));
+    assertThat(fifthTemporalVersion.getName()).isEqualTo("SBB4");
+    assertThat(fifthTemporalVersion.getComment()).isNull();
+    assertThat(fifthTemporalVersion.getLineRelations()).isEmpty();
+
+  }
+
 }
