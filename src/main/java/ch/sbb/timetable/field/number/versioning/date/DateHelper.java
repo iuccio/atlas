afa7b105782ae7abd6ff7a924958a2dec90b1d15
atlas <TechSupport-ATLAS@sbb.ch>
2021-11-15 09:41:48 +0100

    ATLAS-135: add DateHelper Tests

--- src/main/java/ch/sbb/timetable/field/number/versioning/date/DateHelper.java
+++ src/main/java/ch/sbb/timetable/field/number/versioning/date/DateHelper.java
@@ -1,6 +1,7 @@
 package ch.sbb.timetable.field.number.versioning.date;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public final class DateHelper {
 
@@ -9,6 +10,12 @@ public final class DateHelper {
   }
 
   public static boolean areDatesSequential(LocalDate current, LocalDate next){
+    if(Objects.isNull(current)){
+      throw new IllegalStateException("Current date is null");
+    }
+    if(Objects.isNull(next)){
+      throw new IllegalStateException("Next date is null");
+    }
     return current.plusDays(1).equals(next);
   }
 
