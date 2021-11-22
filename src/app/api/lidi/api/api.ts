930b0a7c05bac622b36b3232200c61d2e9705dee
atlas <TechSupport-ATLAS@sbb.ch>
2021-11-22 14:37:49 +0100

    ATLAS-189: Regenerate API

--- src/app/api/lidi/api/api.ts
+++
@@ -1,5 +0,0 @@
-export * from './lines.service';
-import { LinesService } from './lines.service';
-export * from './sublines.service';
-import { SublinesService } from './sublines.service';
-export const APIS = [LinesService, SublinesService];
