6d459aee83556fe6d55fd562b15445a4d9d4aead
atlas <19825793+iuccio@users.noreply.github.com>
2021-11-23 12:10:38 +0100

    ATLAS-185: updated business_rules migration

--- src/main/resources/db/migration/V0.0.7__business_rules.sql
+++ src/main/resources/db/migration/V0.0.7__business_rules.sql
@@ -1,23 +1,33 @@
 CREATE SEQUENCE ttfnid_seq START WITH 1000000 INCREMENT BY 1;
 
--- hope it has not already null values
+DELETE from timetable_field_number_version where number IS NULL;
 ALTER TABLE timetable_field_number_version
     alter column number SET not null;
 
+DELETE from timetable_field_number_version where status IS NULL;
 ALTER TABLE timetable_field_number_version
     ALTER COLUMN status set NOT NULL;
 
+DELETE from timetable_field_number_version where creation_date IS NULL;
 ALTER TABLE timetable_field_number_version
     ALTER COLUMN creation_date set NOT NULL;
+
+DELETE from timetable_field_number_version where edition_date IS NULL;
 ALTER TABLE timetable_field_number_version
     ALTER COLUMN edition_date set NOT NULL;
+
+DELETE from timetable_field_number_version where creator IS NULL;
 ALTER TABLE timetable_field_number_version
     ALTER COLUMN creator set NOT NULL;
+
+DELETE from timetable_field_number_version where editor IS NULL;
 ALTER TABLE timetable_field_number_version
     ALTER COLUMN editor set NOT NULL;
 
+DELETE from timetable_field_number_version where swiss_timetable_field_number IS NULL;
 ALTER TABLE timetable_field_number_version
     ALTER COLUMN swiss_timetable_field_number set NOT NULL;
 
+DELETE from timetable_field_number_version where ttfnid IS NULL;
 ALTER TABLE timetable_field_number_version
     ALTER COLUMN ttfnid set NOT NULL;
