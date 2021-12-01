e0f410796bd9e9e78efe896a4f598ca52e152699
atlas <19825793+iuccio@users.noreply.github.com>
2021-12-01 11:19:30 +0100

    ATLAS-211: implement get actual version in ttfh detail page

--- src/app/pages/ttfn/detail/timetable-field-number-detail.resolver.ts
+++ src/app/pages/ttfn/detail/timetable-field-number-detail.resolver.ts
@@ -3,25 +3,23 @@ import { ActivatedRouteSnapshot, Resolve, Router } from '@angular/router';
 import { catchError, EMPTY, Observable, of } from 'rxjs';
 import { TimetableFieldNumbersService, Version } from '../../../api';
 import { Pages } from '../../pages';
-import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
-export class TimetableFieldNumberDetailResolver implements Resolve<Version> {
+export class TimetableFieldNumberDetailResolver implements Resolve<Array<Version>> {
   constructor(
     private timetableFieldNumbersService: TimetableFieldNumbersService,
     private router: Router
   ) {}
 
-  resolve(route: ActivatedRouteSnapshot): Observable<Version> {
+  resolve(route: ActivatedRouteSnapshot): Observable<Array<Version>> {
     const idParameter = route.paramMap.get('id') || '';
     return idParameter === 'add'
-      ? of({} as Version)
+      ? of([])
       : this.timetableFieldNumbersService.getAllVersionsVersioned(idParameter).pipe(
           catchError(() => {
             this.router.navigate([Pages.TTFN.path]).then();
             return EMPTY;
-          }),
-          map((arr) => arr[0]) // TODO: remove once detail supports multiple
+          })
         );
   }
 }
