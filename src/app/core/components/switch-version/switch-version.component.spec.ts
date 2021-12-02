d533f0e544c77be1f50abfc7d1d8f8eeb713768f
atlas <19825793+iuccio@users.noreply.github.com>
2021-12-02 10:45:12 +0100

    ATLAS-211: fix test

--- src/app/core/components/switch-version/switch-version.component.spec.ts
+++ src/app/core/components/switch-version/switch-version.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SwitchVersionComponent } from './switch-version.component';
+import { TranslateFakeLoader, TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { Record } from '../detail-wrapper/record';
+import moment from 'moment';
 
 describe('SwitchVersionComponent', () => {
   let component: SwitchVersionComponent;
@@ -9,12 +12,25 @@ describe('SwitchVersionComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SwitchVersionComponent],
+      imports: [
+        TranslateModule.forRoot({
+          loader: { provide: TranslateLoader, useClass: TranslateFakeLoader },
+        }),
+      ],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SwitchVersionComponent);
     component = fixture.componentInstance;
+    const firstRecord: Record = {
+      id: 1,
+      validFrom: moment('1.1.2000', 'DD.MM.YYYY').toDate(),
+      validTo: moment('31.12.2000', 'DD.MM.YYYY').toDate(),
+    };
+    const records: Array<Record> = [firstRecord];
+    component.currentRecord = firstRecord;
+    component.records = records;
     fixture.detectChanges();
   });
 
