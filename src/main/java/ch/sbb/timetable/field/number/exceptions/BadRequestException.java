e700f7e153cb5f301f8d63ac44cc162348b3a9ea
atlas <19825793+iuccio@users.noreply.github.com>
2021-12-09 12:33:13 +0100

    ATLAS-184: sonar review

--- src/main/java/ch/sbb/timetable/field/number/exceptions/BadRequestException.java
+++ src/main/java/ch/sbb/timetable/field/number/exceptions/BadRequestException.java
@@ -1,17 +1,18 @@
 package ch.sbb.timetable.field.number.exceptions;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
-public class BadRequestException {
+@JsonIgnoreProperties(value = {"stackTrace"})
+public class BadRequestException extends RuntimeException {
 
   private final int status = HttpStatus.BAD_REQUEST.value();
   private final String error = HttpStatus.BAD_REQUEST.getReasonPhrase();
-  private final String message;
 
   public BadRequestException(String message) {
-    this.message = message;
+    super(message);
   }
 
 }
