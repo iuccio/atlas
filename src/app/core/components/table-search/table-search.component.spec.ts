4846ee15b3851b3fcc4dfe1fa4ba7b5d7aece67c
atlas <19825793+iuccio@users.noreply.github.com>
2021-12-10 17:39:13 +0100

    ATLAS-184: finished last open tasks (Status search, date search), TODO: unit-tests for table-search component and e2e-tests

--- src/app/core/components/table-search/table-search.component.spec.ts
+++ src/app/core/components/table-search/table-search.component.spec.ts
@@ -1,6 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TableSearchComponent } from './table-search.component';
+import { MaterialModule } from '../../module/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  TranslateFakeLoader,
+  TranslateLoader,
+  TranslateModule,
+  TranslatePipe,
+} from '@ngx-translate/core';
 
 describe('TableSearchComponent', () => {
   let component: TableSearchComponent;
@@ -9,6 +17,14 @@ describe('TableSearchComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [TableSearchComponent],
+      imports: [
+        MaterialModule,
+        BrowserAnimationsModule,
+        TranslateModule.forRoot({
+          loader: { provide: TranslateLoader, useClass: TranslateFakeLoader },
+        }),
+      ],
+      providers: [TranslatePipe],
     }).compileComponents();
   });
 
