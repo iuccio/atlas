7337abd25c7716acc128e8baa0ea4abbfcd59c5d
atlas <TechSupport-ATLAS@sbb.ch>
2021-12-20 14:45:33 +0100

    ATLAS-184: code review

--- src/main/java/ch/sbb/timetable/field/number/enumaration/Status.java
+++ src/main/java/ch/sbb/timetable/field/number/enumaration/Status.java
@@ -1,9 +1,9 @@
 package ch.sbb.timetable.field.number.enumaration;
 
 public enum Status {
-  ACTIVE, INACTIVE, NEEDS_REVIEW, IN_REVIEW, REVIEWED;
-
-  public boolean isStatus(String statusName) {
-    return this.name().equals(statusName);
-  }
+  ACTIVE,
+  INACTIVE,
+  NEEDS_REVIEW,
+  IN_REVIEW,
+  REVIEWED
 }
