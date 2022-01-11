0b522b2e30c25c01c68a3c13c6fddb6805b2f1fd
atlas <19825793+iuccio@users.noreply.github.com>
2022-01-11 16:01:42 +0100

    ATLAS-368: made web-mvc-config global

---
+++ src/main/java/ch/sbb/timetable/field/number/configuration/AtlasWebMvcConfig.java
@@ -0,0 +1,16 @@
+package ch.sbb.timetable.field.number.configuration;
+
+import java.util.Collection;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AtlasWebMvcConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addFormatters(FormatterRegistry registry) {
+    registry.removeConvertible(String.class, Collection.class);
+  }
+
+}
