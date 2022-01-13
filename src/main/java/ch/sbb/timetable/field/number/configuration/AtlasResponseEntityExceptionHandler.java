a16737b99af4d4ac27f39596c5f9f75ea7a7d419
atlas <TechSupport-ATLAS@sbb.ch>
2022-01-13 16:42:34 +0100

    ATLAS-291: Move PropertyReferenceException

--- src/main/java/ch/sbb/timetable/field/number/configuration/AtlasResponseEntityExceptionHandler.java
+++ src/main/java/ch/sbb/timetable/field/number/configuration/AtlasResponseEntityExceptionHandler.java
@@ -2,12 +2,15 @@ package ch.sbb.timetable.field.number.configuration;
 
 import ch.sbb.timetable.field.number.api.ErrorResponse;
 import ch.sbb.timetable.field.number.exceptions.AtlasException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.mapping.PropertyReferenceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@Slf4j
 @ControllerAdvice
 public class AtlasResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
@@ -17,4 +20,17 @@ public class AtlasResponseEntityExceptionHandler extends ResponseEntityException
         HttpStatus.valueOf(conflictException.getErrorResponse().getHttpStatus()));
   }
 
+  @ExceptionHandler(PropertyReferenceException.class)
+  public ResponseEntity<ErrorResponse> handleInvalidSort(PropertyReferenceException exception) {
+    log.warn("Pageable sort parameter is not valid.", exception);
+    return ResponseEntity.badRequest()
+                         .body(ErrorResponse.builder()
+                                            .httpStatus(HttpStatus.BAD_REQUEST.value())
+                                            .message(
+                                                "Supplied sort field " + exception.getPropertyName()
+                                                    + " not found on " + exception.getType()
+                                                                                  .getType()
+                                                                                  .getSimpleName())
+                                            .build());
+  }
 }
