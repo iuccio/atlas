a31ca23bdfa6dfe4c8e9c6e23791b5b194834abd
atlas <TechSupport-ATLAS@sbb.ch>
2022-01-13 16:03:59 +0100

    ATLAS-291: Antonio gefällt so

--- src/main/java/ch/sbb/timetable/field/number/api/ErrorResponse.java
+++ src/main/java/ch/sbb/timetable/field/number/api/ErrorResponse.java
@@ -2,12 +2,18 @@ package ch.sbb.timetable.field.number.api;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.text.MessageFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -23,7 +29,9 @@ public class ErrorResponse {
   private String message;
 
   @Schema(description = "List of error details")
-  private List<Detail> details;
+  @NotNull
+  @Builder.Default
+  private List<Detail> details = new ArrayList<>();
 
   @AllArgsConstructor
   @NoArgsConstructor
@@ -32,25 +40,71 @@ public class ErrorResponse {
   @Schema(name = "ErrorDetail")
   public static class Detail {
 
-    @Schema(description = "Field on which to display the error", example = "validFrom")
-    @NotNull
-    private String field;
-
-    @Schema(description = "Errorcode for UI", example = "LIDI.LINE.CONFLICT")
-    @NotNull
-    private String code;
-
     @Schema(description = "Errormessage in english for API purposes", example = "Resource not found")
     @NotNull
     private String message;
 
-    @Schema(description = "Parameters for messages")
+    @Schema(description = "Field on which to display the error", example = "validFrom")
     @NotNull
-    private List<String> parameters;
+    private String field;
+
+    @Schema(description = "DisplayInfo for messages")
+    @NotNull
+    private DisplayInfo displayInfo;
 
     public String getMessage() {
-      return MessageFormat.format(message, parameters.toArray());
+      return MessageFormat.format(message, displayInfo.getParameters().stream().map(Parameter::getValue).toArray());
     }
+  }
+
+  @AllArgsConstructor
+  @Getter
+  public static class DisplayInfo {
+
+    @Schema(description = "Errorcode for UI", example = "LIDI.LINE.CONFLICT")
+    @NotNull
+    private final String code;
+
+    @Schema(description = "Parameters for messages")
+    @NotNull
+    private final List<Parameter> parameters;
+
+    public static DisplayInfoBuilder builder() {
+      return new DisplayInfoBuilder();
+    }
+
+    public static class DisplayInfoBuilder {
+
+      private String code;
+      private final List<Parameter> parameters = new ArrayList<>();
+
+      public DisplayInfoBuilder code(String code) {
+        this.code = code;
+        return this;
+      }
+
+      public DisplayInfoBuilder with(String key, String value) {
+        this.parameters.add(new Parameter(key, value));
+        return this;
+      }
+
+      public DisplayInfoBuilder with(String key, LocalDate value) {
+        this.parameters.add(new Parameter(key, value.format(DateTimeFormatter.ISO_DATE)));
+        return this;
+      }
+
+      public DisplayInfo build() {
+        return new DisplayInfo(Objects.requireNonNull(code), parameters);
+      }
+    }
+  }
+
+  @RequiredArgsConstructor
+  @Getter
+  public static class Parameter {
+
+    private final String key;
+    private final String value;
   }
 
 }
--- src/main/java/ch/sbb/timetable/field/number/exceptions/ConflictException.java
+++ src/main/java/ch/sbb/timetable/field/number/exceptions/ConflictException.java
@@ -2,9 +2,9 @@ package ch.sbb.timetable.field.number.exceptions;
 
 import ch.sbb.timetable.field.number.api.ErrorResponse;
 import ch.sbb.timetable.field.number.api.ErrorResponse.Detail;
+import ch.sbb.timetable.field.number.api.ErrorResponse.DisplayInfo;
 import ch.sbb.timetable.field.number.entity.Version;
 import ch.sbb.timetable.field.number.entity.Version.Fields;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -31,50 +31,60 @@ public class ConflictException extends AtlasException {
     List<Detail> details = new ArrayList<>();
 
     List<Version> numberOverlappingVersion = overlappingVersions.stream()
-                                                                    .filter(i -> Objects.equals(
-                                                                        i.getNumber(),
-                                                                        newVersion.getNumber()))
-                                                                    .collect(Collectors.toList());
+                                                                .filter(i -> Objects.equals(
+                                                                    i.getNumber(),
+                                                                    newVersion.getNumber()))
+                                                                .collect(Collectors.toList());
     numberOverlappingVersion.forEach(version -> details.add(Detail.builder()
-                                                                    .code("TTFN.CONFLICT.NUMBER")
-                                                                    .field(Fields.number)
-                                                                    .message(
-                                                                        "Number {0} already taken from {1} to {2} by {3}")
-                                                                    .parameters(
-                                                                        List.of(
-                                                                            newVersion.getNumber(),
-                                                                            version.getValidFrom()
-                                                                                   .format(
-                                                                                       DateTimeFormatter.ISO_DATE),
-                                                                            version.getValidTo()
-                                                                                   .format(
-                                                                                       DateTimeFormatter.ISO_DATE),
-                                                                            version.getTtfnid()))
-                                                                    .build()));
+                                                                  .field(Fields.number)
+                                                                  .message(
+                                                                      "Number {0} already taken from {1} to {2} by {3}")
+                                                                  .displayInfo(
+                                                                      DisplayInfo.builder()
+                                                                                 .code(
+                                                                                     "TTFN.CONFLICT.NUMBER")
+                                                                                 .with(
+                                                                                     Fields.number,
+                                                                                     newVersion.getNumber())
+                                                                                 .with(
+                                                                                     Fields.validFrom,
+                                                                                     version.getValidFrom())
+                                                                                 .with(
+                                                                                     Fields.validTo,
+                                                                                     version.getValidTo())
+                                                                                 .with(
+                                                                                     Fields.ttfnid,
+                                                                                     version.getTtfnid())
+                                                                                 .build()
+                                                                  )
+                                                                  .build()));
 
     List<Version> swissTimetableFieldNumberOverlappingVersion = overlappingVersions.stream()
-                                                                                       .filter(
-                                                                                           i -> i.getSwissTimetableFieldNumber()
-                                                                                                 .equals(
-                                                                                                     newVersion.getSwissTimetableFieldNumber()))
-                                                                                       .collect(
-                                                                                           Collectors.toList());
+                                                                                   .filter(
+                                                                                       i -> i.getSwissTimetableFieldNumber()
+                                                                                             .equals(
+                                                                                                 newVersion.getSwissTimetableFieldNumber()))
+                                                                                   .collect(
+                                                                                       Collectors.toList());
     swissTimetableFieldNumberOverlappingVersion.forEach(
         version -> details.add(Detail.builder()
-                                     .code("TTFN.CONFLICT.SWISS_NUMBER")
+
                                      .field(Fields.swissTimetableFieldNumber)
                                      .message(
                                          "SwissTimetableFieldNumber {0} already taken from {1} to {2} by {3}")
-                                     .parameters(
-                                         List.of(
-                                             newVersion.getSwissTimetableFieldNumber(),
-                                             version.getValidFrom()
-                                                    .format(
-                                                        DateTimeFormatter.ISO_DATE),
-                                             version.getValidTo()
-                                                    .format(
-                                                        DateTimeFormatter.ISO_DATE),
-                                             version.getTtfnid()))
+                                     .displayInfo(
+                                         DisplayInfo.builder().code("TTFN.CONFLICT.SWISS_NUMBER")
+                                                    .with(Fields.swissTimetableFieldNumber,
+                                                        newVersion.getSwissTimetableFieldNumber())
+                                                    .with(
+                                                        Fields.validFrom,
+                                                        version.getValidFrom())
+                                                    .with(
+                                                        Fields.validTo,
+                                                        version.getValidTo())
+                                                    .with(Fields.ttfnid,
+                                                        version.getTtfnid())
+                                                    .build())
                                      .build()));
     return details;
   }
