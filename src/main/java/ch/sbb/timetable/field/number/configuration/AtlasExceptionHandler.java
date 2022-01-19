21778a3e88c9c00e63fcc111c307025edab7217d
atlas <TechSupport-ATLAS@sbb.ch>
2022-01-19 16:25:28 +0100

    ATLAS-291: Formatting and stuff

--- src/main/java/ch/sbb/timetable/field/number/configuration/AtlasExceptionHandler.java
+++ src/main/java/ch/sbb/timetable/field/number/configuration/AtlasExceptionHandler.java
@@ -5,11 +5,13 @@ import ch.sbb.timetable.field.number.api.ErrorResponse.Detail;
 import ch.sbb.timetable.field.number.api.ErrorResponse.DisplayInfo;
 import ch.sbb.timetable.field.number.exceptions.AtlasException;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.mapping.PropertyReferenceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -44,21 +46,7 @@ public class AtlasExceptionHandler {
       MethodArgumentNotValidException exception) {
     List<Detail> details = exception.getFieldErrors()
                                     .stream()
-                                    .map(fieldError -> Detail.builder()
-                                                             .field(fieldError.getField())
-                                                             .message(
-                                                                 "Value {0} rejected due to {1}")
-                                                             .displayInfo(DisplayInfo.builder()
-                                                                                     .code(
-                                                                                         "TTFN.CONSTRAINT")
-                                                                                     .with(
-                                                                                         "rejectedValue",
-                                                                                         String.valueOf(
-                                                                                             fieldError.getRejectedValue()))
-                                                                                     .with("cause",
-                                                                                         fieldError.getDefaultMessage())
-                                                                                     .build())
-                                                             .build())
+                                    .map(toErrorDetail())
                                     .collect(Collectors.toList());
     return ResponseEntity.badRequest()
                          .body(ErrorResponse.builder()
@@ -66,5 +54,18 @@ public class AtlasExceptionHandler {
                                             .message("Constraint for requestbody was violated")
                                             .details(details)
                                             .build());
+  }
+
+  private Function<FieldError, Detail> toErrorDetail() {
+    return fieldError -> Detail.builder()
+                               .field(fieldError.getField())
+                               .message("Value {0} rejected due to {1}")
+                               .displayInfo(DisplayInfo.builder()
+                                                       .code("TTFN.CONSTRAINT")
+                                                       .with("rejectedValue", String.valueOf(
+                                                           fieldError.getRejectedValue()))
+                                                       .with("cause",
+                                                           fieldError.getDefaultMessage()).build())
+                               .build();
   }
 }
