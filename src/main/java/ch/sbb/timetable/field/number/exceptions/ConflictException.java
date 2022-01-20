451a3fb7dcc28c284249e345402d0523ab6da4cc
atlas <19825793+iuccio@users.noreply.github.com>
2022-01-20 14:43:24 +0100

    ATLAS-357: use equalsIgnoreCase in ConflictException

--- src/main/java/ch/sbb/timetable/field/number/exceptions/ConflictException.java
+++ src/main/java/ch/sbb/timetable/field/number/exceptions/ConflictException.java
@@ -22,52 +22,52 @@ public class ConflictException extends AtlasException {
   @Override
   public ErrorResponse getErrorResponse() {
     return ErrorResponse.builder()
-                        .httpStatus(HttpStatus.CONFLICT.value())
-                        .message("A conflict occurred due to a business rule")
-                        .details(getErrorDetails())
-                        .build();
+        .httpStatus(HttpStatus.CONFLICT.value())
+        .message("A conflict occurred due to a business rule")
+        .details(getErrorDetails())
+        .build();
   }
 
   private List<Detail> getErrorDetails() {
     List<Detail> details = new ArrayList<>();
 
     overlappingVersions.stream()
-                       .filter(
-                           version -> Objects.equals(version.getNumber(), newVersion.getNumber()))
-                       .forEach(version -> details.add(toNumberOverlapDetail(version)));
+        .filter(
+            version -> Objects.equals(version.getNumber(), newVersion.getNumber()))
+        .forEach(version -> details.add(toNumberOverlapDetail(version)));
 
     overlappingVersions.stream()
-                       .filter(version -> version.getSwissTimetableFieldNumber()
-                                                 .equals(newVersion.getSwissTimetableFieldNumber()))
-                       .forEach(version -> details.add(
-                           toSwissTimetableFieldNumberOverlapDetail(version)));
+        .filter(version -> version.getSwissTimetableFieldNumber()
+            .equalsIgnoreCase(newVersion.getSwissTimetableFieldNumber()))
+        .forEach(version -> details.add(
+            toSwissTimetableFieldNumberOverlapDetail(version)));
 
     return details;
   }
 
   private Detail toSwissTimetableFieldNumberOverlapDetail(Version version) {
     return Detail.builder()
-                 .field(Fields.swissTimetableFieldNumber)
-                 .message("SwissTimetableFieldNumber {0} already taken from {1} to {2} by {3}")
-                 .displayInfo(DisplayInfo.builder().code(CODE_PREFIX + "SWISS_NUMBER")
-                                         .with(Fields.swissTimetableFieldNumber,
-                                             newVersion.getSwissTimetableFieldNumber())
-                                         .with(Fields.validFrom, version.getValidFrom())
-                                         .with(Fields.validTo, version.getValidTo())
-                                         .with(Fields.ttfnid, version.getTtfnid())
-                                         .build()).build();
+        .field(Fields.swissTimetableFieldNumber)
+        .message("SwissTimetableFieldNumber {0} already taken from {1} to {2} by {3}")
+        .displayInfo(DisplayInfo.builder().code(CODE_PREFIX + "SWISS_NUMBER")
+            .with(Fields.swissTimetableFieldNumber,
+                newVersion.getSwissTimetableFieldNumber())
+            .with(Fields.validFrom, version.getValidFrom())
+            .with(Fields.validTo, version.getValidTo())
+            .with(Fields.ttfnid, version.getTtfnid())
+            .build()).build();
   }
 
   private Detail toNumberOverlapDetail(Version version) {
     return Detail.builder()
-                 .field(Fields.number)
-                 .message("Number {0} already taken from {1} to {2} by {3}")
-                 .displayInfo(DisplayInfo.builder()
-                                         .code(CODE_PREFIX + "NUMBER")
-                                         .with(Fields.number, newVersion.getNumber())
-                                         .with(Fields.validFrom, version.getValidFrom())
-                                         .with(Fields.validTo, version.getValidTo())
-                                         .with(Fields.ttfnid, version.getTtfnid())
-                                         .build()).build();
+        .field(Fields.number)
+        .message("Number {0} already taken from {1} to {2} by {3}")
+        .displayInfo(DisplayInfo.builder()
+            .code(CODE_PREFIX + "NUMBER")
+            .with(Fields.number, newVersion.getNumber())
+            .with(Fields.validFrom, version.getValidFrom())
+            .with(Fields.validTo, version.getValidTo())
+            .with(Fields.ttfnid, version.getTtfnid())
+            .build()).build();
   }
 }
