29d25e8965683967437840b72a123437b5470f5c
atlas <TechSupport-ATLAS@sbb.ch>
2022-01-24 15:56:00 +0100

    ATLAS-23: impl subline assing to line conflict rule

--- src/main/java/ch/sbb/line/directory/repository/SublineVersionRepository.java
+++ src/main/java/ch/sbb/line/directory/repository/SublineVersionRepository.java
@@ -4,7 +4,9 @@ import ch.sbb.line.directory.entity.SublineVersion;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
