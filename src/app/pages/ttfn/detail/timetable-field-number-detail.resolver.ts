fc658e346c5411ee048f93e94a43e28a93a66df0
atlas <19825793+iuccio@users.noreply.github.com>
2022-02-02 17:05:44 +0100

    show warning when url with id not found

--- src/app/pages/ttfn/detail/timetable-field-number-detail.resolver.ts
+++ src/app/pages/ttfn/detail/timetable-field-number-detail.resolver.ts
@@ -3,23 +3,26 @@ import { ActivatedRouteSnapshot, Resolve, Router } from '@angular/router';
 import { catchError, EMPTY, Observable, of } from 'rxjs';
 import { TimetableFieldNumbersService, Version } from '../../../api';
 import { Pages } from '../../pages';
+import { NotificationService } from '../../../core/notification/notification.service';
 
 @Injectable({ providedIn: 'root' })
 export class TimetableFieldNumberDetailResolver implements Resolve<Array<Version>> {
-  constructor(
-    private timetableFieldNumbersService: TimetableFieldNumbersService,
-    private router: Router
-  ) {}
-
   resolve(route: ActivatedRouteSnapshot): Observable<Array<Version>> {
     const idParameter = route.paramMap.get('id') || '';
     return idParameter === 'add'
       ? of([])
       : this.timetableFieldNumbersService.getAllVersionsVersioned(idParameter).pipe(
-          catchError(() => {
+          catchError((error) => {
             this.router.navigate([Pages.TTFN.path]).then();
+            this.notificationService.error(error);
             return EMPTY;
           })
         );
   }
+
+  constructor(
+    private timetableFieldNumbersService: TimetableFieldNumbersService,
+    private notificationService: NotificationService,
+    private router: Router
+  ) {}
 }
