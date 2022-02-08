47b0248ca8acfeeed7425be3a6fc27bc06245e59
atlas <TechSupport-ATLAS@sbb.ch>
2022-02-08 14:10:24 +0100

    ATLAS-298: show warning

--- src/app/core/notification/notification.service.ts
+++ src/app/core/notification/notification.service.ts
@@ -9,12 +9,13 @@ import { DisplayInfo } from '../../api';
 import { NavigationStart, Router } from '@angular/router';
 import { HttpErrorResponse } from '@angular/common/http';
 
+const VERSIONING_NO_CHANGES_HTTP_STATUS = 520;
+
 @Injectable({
   providedIn: 'root',
 })
 export class NotificationService implements OnDestroy {
   private ngUnsubscribe = new Subject<void>();
-  showOnlyCode = false;
   displayCode = '';
   SNACK_BAR_CONFIG: MatSnackBarConfig = {
     horizontalPosition: 'right',
@@ -34,23 +35,47 @@ export class NotificationService implements OnDestroy {
   error(errorResponse: HttpErrorResponse, code?: string) {
     this.SNACK_BAR_CONFIG['duration'] = undefined;
     this.SNACK_BAR_CONFIG['panelClass'] = ['error', 'notification'];
-    if (code) {
-      this.showOnlyCode = true;
-      this.displayCode = code;
-    } else if (errorResponse.error?.details) {
-      if (errorResponse.status === 520) {
-        this.SNACK_BAR_CONFIG['panelClass'] = ['warning', 'notification'];
-      }
-      this.SNACK_BAR_CONFIG['data'] = errorResponse.error;
-    } else {
-      this.showOnlyCode = true;
-      this.displayCode = 'ERROR.GENERIC';
-    }
+    this.configureNotification(code, errorResponse);
     const errorSnackBar = this.snackBar.openFromComponent(
       ErrorNotificationComponent,
       this.SNACK_BAR_CONFIG
     );
     this.dismissOnNavigation(errorSnackBar);
+  }
+
+  private configureNotification(code: string | undefined, errorResponse: HttpErrorResponse) {
+    if (code) {
+      this.configureErrorCodeNotification(code);
+    } else if (errorResponse.error?.details) {
+      if (errorResponse.status === VERSIONING_NO_CHANGES_HTTP_STATUS) {
+        this.configureWarningNotification(errorResponse);
+      } else {
+        this.configureMultilineErrorNotification(errorResponse);
+      }
+    } else {
+      this.configureGenericErrorNotification();
+    }
+  }
+
+  private configureGenericErrorNotification() {
+    this.displayCode = 'ERROR.GENERIC';
+  }
+
+  private configureErrorCodeNotification(code: string) {
+    this.displayCode = code;
+  }
+
+  private configureMultilineErrorNotification(errorResponse: HttpErrorResponse) {
+    this.SNACK_BAR_CONFIG['data'] = errorResponse.error;
+  }
+
+  private configureWarningNotification(errorResponse: HttpErrorResponse) {
+    this.SNACK_BAR_CONFIG['panelClass'] = ['warning', 'notification'];
+    this.displayCode = errorResponse.error.details[0].displayInfo.code;
+  }
+
+  showOnlyCode() {
+    return !this.displayCode || this.displayCode.length === 0;
   }
 
   private dismissOnNavigation(errorSnackBar: MatSnackBarRef<ErrorNotificationComponent>) {
