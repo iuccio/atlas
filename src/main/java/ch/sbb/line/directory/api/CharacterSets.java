ed6d58809f214386d226832fd647b80c77e8c7f8
atlas <TechSupport-ATLAS@sbb.ch>
2022-02-08 08:54:22 +0100

    ATLAS-270: Charsets fdefinitions for LiDi

---
+++ src/main/java/ch/sbb/line/directory/api/CharacterSets.java
@@ -0,0 +1,13 @@
+package ch.sbb.line.directory.api;
+
+public final class CharacterSets {
+
+  public static final String ISO_8859_1 = "[\\u0000-\\u00ff]*";
+  public static final String NUMERIC = "[0-9]*";
+  public static final String NUMERIC_WITH_DOT = "[.0-9]*";
+  public static final String SID4PT = "[-.:_0-9a-zA-Z]*";
+
+  private CharacterSets() {
+    throw new IllegalStateException();
+  }
+}
