2f3c5fe8a481c1636d82d0484f8ac8c9e352f4e3
atlas <TechSupport-ATLAS@sbb.ch>
2022-02-10 18:42:24 +0100

    Fix date min,max, range error

--- src/app/core/validation/validation.service.spec.ts
+++ src/app/core/validation/validation.service.spec.ts
@@ -1,7 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ValidationService } from './validation.service';
-import { ValidationError } from './validation-error';
 import moment from 'moment/moment';
 
 describe('ValidationService', () => {
@@ -14,25 +13,6 @@ describe('ValidationService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
-  });
-
-  it('should add validation error', () => {
-    //given
-    const error = {
-      error: {
-        actual: '20.20.2000',
-        min: '21.20.2000',
-      },
-    };
-    //when
-    const result = service.getValidation(error);
-    //then
-    expect(result).toBeDefined();
-    expect(result.length).toBe(1);
-    expect(result[0].error).toBe('VALIDATION.ERROR');
-    expect(result[0].value).toBeDefined();
-    expect(result[0].value.actual).toBe('20.20.2000');
-    expect(result[0].value.min).toBe('21.20.2000');
   });
 
   it('should not add any validation error', () => {
@@ -74,13 +54,11 @@ describe('ValidationService', () => {
 
   it('should return validationError with validFrom and validTo', () => {
     ///given
-    const error: ValidationError = {
+    const error = {
       error: 'VALIDATION.DATE_RANGE_ERROR',
-      value: {
-        date: {
-          validFrom: moment('2000-10-09T22:00:00.000Z'),
-          validTo: moment('2000-09-08T22:00:00.000Z'),
-        },
+      date: {
+        validFrom: moment('2000-10-09T22:00:00.000Z'),
+        validTo: moment('2000-09-08T22:00:00.000Z'),
       },
     };
 
@@ -93,11 +71,9 @@ describe('ValidationService', () => {
 
   it('should return validationError with min', () => {
     ///given
-    const error: ValidationError = {
+    const error = {
       error: 'VALIDATION.DATE_RANGE_ERROR',
-      value: {
-        min: moment('2000-10-09T22:00:00.000Z'),
-      },
+      min: moment('2000-10-09T22:00:00.000Z'),
     };
 
     //when
@@ -109,11 +85,9 @@ describe('ValidationService', () => {
 
   it('should return validationError with max', () => {
     ///given
-    const error: ValidationError = {
+    const error = {
       error: 'VALIDATION.DATE_RANGE_ERROR',
-      value: {
-        max: moment('2000-10-09T22:00:00.000Z'),
-      },
+      max: moment('2000-10-09T22:00:00.000Z'),
     };
 
     //when
