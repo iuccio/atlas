57de1d86146e33603ea4bb59dcd62a7a489f5a3d
atlas <19825793+iuccio@users.noreply.github.com>
2022-02-10 14:31:53 +0100

    ATLAS-297: Default values for colors

--- src/app/core/text-input/with-default-value.directive.spec.ts
+++ src/app/core/text-input/with-default-value.directive.spec.ts
@@ -1,27 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Component, DebugElement } from '@angular/core';
-import { EmptyToNullDirective } from './empty-to-null';
 import { FormControl } from '@angular/forms';
 import { CoreModule } from '../module/core.module';
 import { By } from '@angular/platform-browser';
+import { WithDefaultValueDirective } from './with-default-value.directive';
+
+const keyUpEvent = new KeyboardEvent('keyup');
 
 @Component({
-  template: ` <input [formControl]="form" emptyToNull /> `,
+  template: ` <input [formControl]="form" withDefaultValue="defaultValue" /> `,
 })
 class TestComponent {
   form = new FormControl();
 }
 
-const keyUpEvent = new KeyboardEvent('keyup');
-
-describe('EmptyToNullDirective', () => {
+describe('WithDefaultValueDirective', () => {
   let component: TestComponent;
   let fixture: ComponentFixture<TestComponent>;
   let inputField: DebugElement;
 
   beforeEach(() => {
     fixture = TestBed.configureTestingModule({
-      declarations: [EmptyToNullDirective, TestComponent],
+      declarations: [WithDefaultValueDirective, TestComponent],
       imports: [CoreModule],
     }).createComponent(TestComponent);
 
@@ -31,13 +31,13 @@ describe('EmptyToNullDirective', () => {
     fixture.detectChanges();
   });
 
-  it('should convert empty string to null', () => {
+  it('should convert empty to default value', () => {
     component.form.setValue('');
 
     inputField.nativeElement.dispatchEvent(keyUpEvent);
     fixture.detectChanges();
 
-    expect(component.form.value).toBe(null);
+    expect(component.form.value).toBe('defaultValue');
   });
 
   it('should do nothing on given value', () => {
