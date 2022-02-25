f20e551fdf38e72f5c0cddf89669afd099320c73
atlas <TechSupport-ATLAS@sbb.ch>
2022-02-25 14:27:18 +0100

    ATLAS-411 - Leerzeichen von allen String-Feldern in Rest-Controller Models entfernen

---
+++ src/main/java/ch/sbb/line/directory/converter/TrimLeadingTrailingWhitespace.java
@@ -0,0 +1,26 @@
+package ch.sbb.line.directory.converter;
+
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdScalarDeserializer;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import java.io.IOException;
+import org.springframework.stereotype.Component;
+
+/**
+ * https://stackoverflow.com/questions/6852213/can-jackson-be-configured-to-trim-leading-trailing-whitespace-from-all-string-pr
+ */
+@Component
+public class TrimLeadingTrailingWhitespace extends SimpleModule {
+
+  public TrimLeadingTrailingWhitespace() {
+    addDeserializer(String.class, new StdScalarDeserializer<>(String.class) {
+      @Override
+      public String deserialize(JsonParser jsonParser, DeserializationContext ctx)
+          throws IOException {
+        return jsonParser.getValueAsString().trim();
+      }
+    });
+  }
+}
