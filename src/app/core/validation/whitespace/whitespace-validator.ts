3309987de290ce689861ba594c79007698d8527b
atlas <19825793+iuccio@users.noreply.github.com>
2022-03-03 15:47:25 +0100

    ATLAS-411: whitespace validator (inc. blank check)

---
+++ src/app/core/validation/whitespace/whitespace-validator.ts
@@ -0,0 +1,13 @@
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+
+export class WhitespaceValidator {
+  static notBlankNorLeadingOrTrailing(control: AbstractControl): ValidationErrors | null {
+    if (control.value?.length && control.value?.trim().length === 0) {
+      return { 'blank': control.value };
+    }
+    if (control.value?.startsWith(' ') || control.value?.endsWith(' ')) {
+      return { 'whitespaces': control.value };
+    }
+    return null;
+  };
+}
