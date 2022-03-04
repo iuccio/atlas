30fbff0233b23ebbb8e343a9391a3ad73372c0e1
atlas <19825793+iuccio@users.noreply.github.com>
2022-03-04 16:41:24 +0100

    ATLAS-516 - header and sidebar

--- frontend/src/app/core/components/header/header.component.ts
+++ frontend/src/app/core/components/header/header.component.ts
@@ -1,11 +1,11 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import packageJson from '../../../../../package.json';
 import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
   private readonly DEV: string = 'dev';
@@ -15,12 +15,6 @@ export class HeaderComponent {
 
   version: string = packageJson.version;
   environmentLabel: string = environment.label;
-
-  @Output() showSidebar = new EventEmitter<void>();
-
-  toggle(): void {
-    this.showSidebar.emit();
-  }
 
   showLabel() {
     return this.STAGES_WITH_LABEL.includes(this.environmentLabel);
@@ -33,7 +27,7 @@ export class HeaderComponent {
       'ms-2': true,
       'bg-primary': this.environmentLabel === this.DEV,
       'bg-secondary': this.environmentLabel === this.TEST,
-      'bg-warning': this.environmentLabel === this.INT,
+      'bg-warning': this.environmentLabel === this.INT
     };
   }
 }
