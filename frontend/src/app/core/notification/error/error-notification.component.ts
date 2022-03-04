1803f5b073d58353b4bc1546eb20710e48350685
atlas <19825793+iuccio@users.noreply.github.com>
2022-03-04 12:14:58 +0100

    ATLAS-516 - SCSS Files strukturiert

--- frontend/src/app/core/notification/error/error-notification.component.ts
+++ frontend/src/app/core/notification/error/error-notification.component.ts
@@ -6,11 +6,13 @@ import { NotificationService } from '../notification.service';
 @Component({
   selector: 'error-notification',
   templateUrl: './error-notification.component.html',
+  styleUrls: ['./error-notification.component.scss']
 })
 export class ErrorNotificationComponent {
   constructor(
     public snackBarRef: MatSnackBarRef<ErrorNotificationComponent>,
     public notificationService: NotificationService,
     @Inject(MAT_SNACK_BAR_DATA) public data: ErrorResponse
-  ) {}
+  ) {
+  }
 }
