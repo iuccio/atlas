0ad4b40e610d0f3c59d71552682596c6a9c08914
atlas <TechSupport-ATLAS@sbb.ch>
2022-03-06 14:42:57 +0100

    ATLAS-472: get coverad lines and lineVersions

--- line-directory/src/main/java/ch/sbb/line/directory/repository/LineRepository.java
+++ line-directory/src/main/java/ch/sbb/line/directory/repository/LineRepository.java
@@ -1,9 +1,14 @@
 package ch.sbb.line.directory.repository;
 
 import ch.sbb.line.directory.entity.Line;
+import ch.sbb.line.directory.enumaration.CoverageType;
+import ch.sbb.line.directory.enumaration.ModelType;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,4 +16,11 @@ public interface LineRepository extends JpaRepository<Line, String>,
     JpaSpecificationExecutor<Line> {
 
   Optional<Line> findAllBySlnid(String slnid);
+
+  @Query("SELECT l FROM line as l"
+      + " JOIN coverage as c "
+      + " ON l.slnid = c.slnid"
+      + " WHERE c.modelType = ch.sbb.line.directory.enumaration.ModelType.LINE"
+      + " AND c.coverageType = ch.sbb.line.directory.enumaration.CoverageType.COMPLETE")
+  List<Line> getAllCoveredLines();
 }
