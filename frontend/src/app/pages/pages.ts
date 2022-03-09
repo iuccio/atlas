e3b5f54de388bee9b42d3cab1c3149111f077892
atlas <19825793+iuccio@users.noreply.github.com>
2022-03-09 16:26:25 +0100

    ATLAS-517: implemented "startseite", TODO: cleanup, tests

--- frontend/src/app/pages/pages.ts
+++ frontend/src/app/pages/pages.ts
@@ -4,36 +4,42 @@ export class Pages {
   public static HOME: Page = {
     title: 'PAGES.HOME',
     path: '',
+    pathText: '',
     description: '',
   };
 
   public static TTFN: Page = {
     title: 'PAGES.TTFN.TITLE',
     path: 'timetable-field-number',
+    pathText: 'PAGES.TTFN.PATH_TEXT',
     description: 'PAGES.TTFN.DESCRIPTION',
   };
 
   public static TTFN_DETAIL: Page = {
     title: 'PAGES.DETAILS',
     path: ':id',
+    pathText: '',
     description: '',
   };
 
   public static LIDI: Page = {
     title: 'PAGES.LIDI.TITLE',
     path: 'line-directory',
+    pathText: 'PAGES.LIDI.PATH_TEXT',
     description: 'PAGES.LIDI.DESCRIPTION',
   };
 
   public static LINES: Page = {
     title: 'PAGES.DETAILS',
     path: 'lines',
+    pathText: '',
     description: '',
   };
 
   public static SUBLINES: Page = {
     title: 'PAGES.DETAILS',
     path: 'sublines',
+    pathText: '',
     description: '',
   };
 
