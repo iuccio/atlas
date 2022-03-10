dd9d2663e7e99a253dd77a7a242c97dc25ef27fd
atlas <19825793+iuccio@users.noreply.github.com>
2022-03-10 17:23:05 +0100

    ATLAS-549: Upgrade H2 - never ever never use with in h2 SQL scripts

--- line-directory/src/main/resources/db/migration/V0.0.9__add_ttfn.sql
+++ line-directory/src/main/resources/db/migration/V0.0.9__add_ttfn.sql
@@ -35,59 +35,36 @@ CREATE SEQUENCE ttfnid_seq START WITH 1000000 INCREMENT BY 1;
 
 --- Create View
 create or replace view timetable_field_number as
-with timetable_field_numbers_fields as (
-    with valid_today as (
-        select distinct on (ttfnid) swiss_timetable_field_number, description, status, ttfnid
-        from timetable_field_number_version
-        where valid_from <= current_timestamp
-          and current_timestamp <= valid_to
-    ),
-         valid_in_future as (
-             select distinct on (ttfnid) swiss_timetable_field_number,
+select *
+from (
+         select f.*, v.valid_from, v.valid_to
+         from (
+                  select swiss_timetable_field_number,
                                          description,
                                          status,
                                          ttfnid,
                                          valid_from as vf
+                  from (
+                           select distinct on (ttfnid) *
+                           from ((select distinct on (ttfnid) *
+                                  from timetable_field_number_version
+                                  where valid_from <= current_timestamp
+                                    and current_timestamp <= valid_to)
+                                 union all
+                                 (select distinct on (ttfnid) *
+                                  from timetable_field_number_version
+                                  where valid_from >= current_timestamp
+                                  order by ttfnid, valid_from asc)
+                                 union all
+                                 (select distinct on (ttfnid) *
+                                  from timetable_field_number_version
+                                  where valid_to <= current_timestamp
+                                  order by ttfnid, valid_to desc)) as ranked
+                       ) as chosen
+              ) f
+                  join (
+             select ttfnid, min(valid_from) as valid_from, max(valid_to) as valid_to
              from timetable_field_number_version
-             where valid_from >= current_timestamp
-             order by ttfnid, valid_from
-         ),
-         last_valid_in_past as (
-             select distinct on (ttfnid) swiss_timetable_field_number,
-                                         description,
-                                         status,
-                                         ttfnid,
-                                         valid_to as vt
-             from timetable_field_number_version
-             where valid_to <= current_timestamp
-             order by ttfnid, valid_to desc
-         ),
-         ranked_properties as (
-             select swiss_timetable_field_number, description, status, ttfnid
-             from valid_today
-             union all
-             select swiss_timetable_field_number, description, status, ttfnid
-             from valid_in_future
-             union all
-             select swiss_timetable_field_number, description, status, ttfnid
-             from last_valid_in_past
-         ),
-         chosen_one as (
-             select distinct on (ttfnid) *
-             from ranked_properties
-         )
-    select swiss_timetable_field_number, description, status, ttfnid
-    from chosen_one
-),
-     timetable_field_number_validity as (
-         select ttfnid, min(valid_from) as valid_from, max(valid_to) as valid_to
-         from timetable_field_number_version
-         group by ttfnid
-     ),
-     timetable_field_numbers as (
-         select f.*, v.valid_from, v.valid_to
-         from timetable_field_numbers_fields f
-                  join timetable_field_number_validity v on f.ttfnid = v.ttfnid
-     )
-select *
-from timetable_field_numbers
+             group by ttfnid
+         ) v on f.ttfnid = v.ttfnid
+     ) as timetable_field_numbers;
