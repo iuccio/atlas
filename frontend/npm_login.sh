32fabf977c2cc01649310b5b41a3cac5907c87bd
atlas <19825793+iuccio@users.noreply.github.com>
2022-03-15 16:00:55 +0100

    ATLAS-549: print log

--- frontend/npm_login.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "npm registry login"
-touch .npmrc
-echo "registry=https://bin.sbb.ch/artifactory/api/npm/npm/" >> .npmrc
-echo _auth = ${NPM_AUTH} >> .npmrc
-echo  email = antonio.romano@sbb.ch >> .npmrc
-echo always-auth = true >> .npmrc
-
