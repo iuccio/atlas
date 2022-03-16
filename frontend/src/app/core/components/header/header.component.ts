135ad0d8341357e1ad87b19cb77dd3cd07ee6ae2
atlas <19825793+iuccio@users.noreply.github.com>
2022-03-16 15:38:08 +0100

    ATLAS-588: implemented dynamic header title, TODO: unit- and e2e-tests

--- frontend/src/app/core/components/header/header.component.ts
+++ frontend/src/app/core/components/header/header.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import packageJson from '../../../../../package.json';
 import { environment } from '../../../../environments/environment';
+import { NavigationEnd, Router, ActivatedRouteSnapshot } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
   private readonly DEV: string = 'dev';
@@ -15,6 +18,21 @@ export class HeaderComponent {
 
   version: string = packageJson.version;
   environmentLabel: string = environment.label;
+  headerTitle$: Observable<string>;
+
+  constructor(private readonly router: Router) {
+    this.headerTitle$ = router.events.pipe(
+      filter((e) => e instanceof NavigationEnd),
+      map(() => this.getHeaderTitleForCurrentRoute(router.routerState.snapshot.root))
+    );
+  }
+
+  private getHeaderTitleForCurrentRoute(node: ActivatedRouteSnapshot): string {
+    if (!node.firstChild?.data.headerTitle) {
+      return node.data.headerTitle;
+    }
+    return this.getHeaderTitleForCurrentRoute(node.firstChild);
+  }
 
   showLabel() {
     return this.STAGES_WITH_LABEL.includes(this.environmentLabel);
@@ -27,7 +45,7 @@ export class HeaderComponent {
       'ms-2': true,
       'bg-primary': this.environmentLabel === this.DEV,
       'bg-secondary': this.environmentLabel === this.TEST,
-      'bg-warning': this.environmentLabel === this.INT
+      'bg-warning': this.environmentLabel === this.INT,
     };
   }
 }
--- frontend/src/app/core/model/page.ts
+++ frontend/src/app/core/model/page.ts
@@ -1,6 +1,7 @@
 export interface Page {
   title: string;
   titleMenu?: string;
+  headerTitle?: string;
   path: string;
   pathText: string;
   description: string;
