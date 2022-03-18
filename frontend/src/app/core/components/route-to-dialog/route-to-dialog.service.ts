ac081dc090441e3de71936101022518eac87544c
atlas <TechSupport-ATLAS@sbb.ch>
2022-03-18 13:41:28 +0100

    ATLAS-611: dialog with form components

---
+++ frontend/src/app/core/components/route-to-dialog/route-to-dialog.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RouteToDialogService {
+  private lastDialogRef?: MatDialogRef<any>;
+
+  hasDialog(): boolean {
+    return !!this.lastDialogRef;
+  }
+
+  getDialog(): MatDialogRef<any> {
+    return this.lastDialogRef!;
+  }
+
+  closeDialog(): void {
+    this.lastDialogRef?.close();
+  }
+
+  setDialogRef(dialogRef: MatDialogRef<any>) {
+    this.lastDialogRef = dialogRef;
+  }
+
+  clearDialogRer() {
+    this.lastDialogRef = undefined;
+  }
+}
