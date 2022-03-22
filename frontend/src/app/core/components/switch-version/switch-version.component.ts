a36248728711c7f8924d690966f74fd314549a6d
atlas <TechSupport-ATLAS@sbb.ch>
2022-03-22 09:57:24 +0100

    ATLAS-524: switch version component rebuild

--- frontend/src/app/core/components/switch-version/switch-version.component.ts
+++ frontend/src/app/core/components/switch-version/switch-version.component.ts
@@ -1,15 +1,25 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 import { Record } from '../detail-wrapper/record';
 import { DateService } from '../../date/date.service';
 import { Page } from '../../model/page';
 import { Pages } from '../../../pages/pages';
+import { Status } from '../../../api';
 
 @Component({
   selector: 'app-switch-version',
   templateUrl: './switch-version.component.html',
   styleUrls: ['./switch-version.component.scss'],
 })
-export class SwitchVersionComponent {
+export class SwitchVersionComponent implements AfterContentChecked {
   @Input() records!: Array<Record>;
   @Input() currentRecord!: Record;
   @Input() pageType!: Page;
@@ -17,24 +27,32 @@ export class SwitchVersionComponent {
   @Output() switchVersion = new EventEmitter<number>();
 
   currentIndex: number;
+  tableColumns = [
+    { translationKey: 'COMMON.VERSION_DESCRIPTION', identifier: 'description' },
+    { translationKey: 'COMMON.VALID_FROM', identifier: 'validFrom' },
+    { translationKey: 'COMMON.VALID_TO', identifier: 'validTo' },
+    { translationKey: 'COMMON.STATUS', identifier: 'status' },
+  ];
 
   constructor() {
     this.currentIndex = 0;
   }
 
-  displayPageTypeTitle() {
-    this.getCurrentIndex();
+  // TODO: ngOnChanges
+  ngAfterContentChecked() {
+    for (let i = 0; i < this.records.length; i++) {
+      if (this.hasGapToNextRecord(this.records[i])) {
+        this.records.splice(i + 1, 0, { placeholder: true });
+        i++;
+      }
+    }
+    this.records
+      .filter((item) => !item.placeholder)
+      .map((item, index) => (item.description = `Version ${index + 1}`));
+  }
 
-    if (this.pageType === Pages.TTFN) {
-      return 'TTFN.TTFN';
-    }
-    if (this.pageType === Pages.LINES) {
-      return 'LIDI.LINE.LINE';
-    }
-    if (this.pageType === Pages.SUBLINES) {
-      return 'LIDI.SUBLINE.SUBLINE';
-    }
-    return '';
+  get columnValues() {
+    return this.tableColumns.map((el) => el.identifier);
   }
 
   getStartDate() {
@@ -49,12 +67,25 @@ export class SwitchVersionComponent {
     return DateService.getDateFormatted(date);
   }
 
+  format(input: Date | undefined, column: { translationKey: string; identifier: string }) {
+    if (
+      this.tableColumns
+        .filter((el) => el.identifier === 'validFrom' || el.identifier === 'validTo')
+        .includes(column)
+    ) {
+      return this.formatDate(input);
+    }
+    return input;
+  }
+
   setCurrentRecord(clickedRecord: Record) {
+    if (clickedRecord.placeholder) return;
     this.currentIndex = this.getIndexOfRecord(clickedRecord);
     this.switchVersion.emit(this.currentIndex);
   }
 
   isCurrentRecord(record: Record): boolean {
+    this.getCurrentIndex();
     return this.currentIndex == this.getIndexOfRecord(record);
   }
 
