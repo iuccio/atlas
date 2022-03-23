028b1b389b25c2847508d6382423bf9fa3dcce11
atlas <TechSupport-ATLAS@sbb.ch>
2022-03-23 17:46:32 +0100

    ATLAS-591: Squash db migrations, rename type attributes

---
+++ line-directory/src/main/java/ch/sbb/line/directory/api/AtlasFieldLengths.java
@@ -0,0 +1,9 @@
+package ch.sbb.line.directory.api;
+
+public final class AtlasFieldLengths {
+
+  public static final int SMALL = 50;
+  public static final int MID = 255;
+  public static final int BUSINESS_IDS = 500;
+  public static final int COMMENTS = 1500;
+}
