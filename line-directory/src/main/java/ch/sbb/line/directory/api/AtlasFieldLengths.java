57b1fb6848b7895918fd42c16a9a024091427c26
atlas <19825793+iuccio@users.noreply.github.com>
2022-03-24 11:20:07 +0100

    ATLAS-591: Only for you Marek

--- line-directory/src/main/java/ch/sbb/line/directory/api/AtlasFieldLengths.java
+++ line-directory/src/main/java/ch/sbb/line/directory/api/AtlasFieldLengths.java
@@ -2,6 +2,10 @@ package ch.sbb.line.directory.api;
 
 public final class AtlasFieldLengths {
 
+  private AtlasFieldLengths() {
+    throw new IllegalStateException();
+  }
+
   public static final int SMALL = 50;
   public static final int MID = 255;
   public static final int BUSINESS_IDS = 500;
