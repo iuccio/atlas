824e74202ea69c8c984e0d01c2ada2aa8c0175cb
atlas <19825793+iuccio@users.noreply.github.com>
2022-03-25 12:12:46 +0100

    ATLAS-634: First go

---
+++ frontend/src/app/core/auth/role.ts
@@ -0,0 +1,8 @@
+export type Role = 'lidi-writer' | 'lidi-admin' | 'bo-writer' | 'bo-admin';
+
+export const Roles = {
+  LidiAdmin: 'lidi-admin' as Role,
+  LidiWriter: 'lidi-writer' as Role,
+  BoWriter: 'bo-writer' as Role,
+  BoAdmin: 'bo-admin' as Role,
+};
