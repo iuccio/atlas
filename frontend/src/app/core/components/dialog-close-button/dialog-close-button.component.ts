940ed687d750082ca13bd4e80b0ff64f3672f0f8
atlas <19825793+iuccio@users.noreply.github.com>
2022-04-06 13:59:09 +0200

    ATLAS-661: Weniger ist mehr

--- frontend/src/app/core/components/dialog-close-button/dialog-close-button.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-dialog-close-button',
-  templateUrl: './dialog-close-button.component.html',
-  styleUrls: ['./dialog-close-button.component.scss'],
-})
-export class DialogCloseButtonComponent {}
