d620aea664c5fca3cbbaad48016e0d9615337f31
atlas <TechSupport-ATLAS@sbb.ch>
2022-04-07 08:57:57 +0200

    ATLAS-522: implemented new subline form

---
+++ frontend/src/app/core/form-components/comment/comment.component.spec.ts
@@ -0,0 +1,31 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CommentComponent } from './comment.component';
+import { AppTestingModule } from '../../../app.testing.module';
+import { FormControl, FormGroup } from '@angular/forms';
+
+describe('CommentComponent', () => {
+  let component: CommentComponent;
+  let fixture: ComponentFixture<CommentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [CommentComponent],
+      imports: [AppTestingModule],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CommentComponent);
+    component = fixture.componentInstance;
+    component.formGroup = new FormGroup({
+      comment: new FormControl('test'),
+    });
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+    expect(component.formGroup.value).toEqual({ comment: 'test' });
+  });
+});
