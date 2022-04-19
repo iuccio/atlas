688419146b305900f7bf550ffd92fbacb447e6b0
atlas <19825793+iuccio@users.noreply.github.com>
2022-04-19 20:42:48 +0200

    ATLAS-688: create date-icon component

---
+++ frontend/src/app/core/components/date-icon/date-icon.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DateIconComponent } from './date-icon.component';
+
+describe('DateIconComponent', () => {
+  let component: DateIconComponent;
+  let fixture: ComponentFixture<DateIconComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [DateIconComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DateIconComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/core/components/date-icon/date-icon.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-date-icon',
+  templateUrl: './date-icon.component.html',
+  styleUrls: ['./date-icon.component.scss'],
+})
+export class DateIconComponent {
+  @Input() enabled!: boolean;
+
+  constructor() {
+    console.log(this.enabled);
+  }
+}
