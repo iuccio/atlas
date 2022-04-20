bccd6de5293d315c1d68b851af4f626f44e8b35d
atlas <19825793+iuccio@users.noreply.github.com>
2022-04-20 14:51:29 +0200

    ATLAS-688: create url component

---
+++ frontend/src/app/core/form-components/url/url.component.spec.ts
@@ -0,0 +1,32 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UrlComponent } from './url.component';
+import { AppTestingModule } from '../../../app.testing.module';
+import { FormControl, FormGroup } from '@angular/forms';
+import { InfoIconComponent } from '../../components/info-icon/info-icon.component';
+
+describe('UrlComponent', () => {
+  let component: UrlComponent;
+  let fixture: ComponentFixture<UrlComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [UrlComponent, InfoIconComponent],
+      imports: [AppTestingModule],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UrlComponent);
+    component = fixture.componentInstance;
+    component.formGroup = new FormGroup({
+      icon: new FormControl('https://www.sbb.ch'),
+    });
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+    expect(component.formGroup.value).toEqual({ icon: 'https://www.sbb.ch' });
+  });
+});
