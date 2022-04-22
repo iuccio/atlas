56b572475be0fdbe9110bd5984a03b014a59a081
atlas <19825793+iuccio@users.noreply.github.com>
2022-04-22 18:13:41 +0200

    rebased with master

--- frontend/src/app/core/components/side-nav/side-nav.component.ts
+++ frontend/src/app/core/components/side-nav/side-nav.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Page } from '../../model/page';
 import { Pages } from '../../../pages/pages';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, takeUntil, tap } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.scss'],
 })
-export class SideNavComponent {
-  pages: Page[] = Pages.pages;
+export class SideNavComponent implements OnDestroy {
+  readonly pages: Page[] = Pages.pages;
+  private readonly ngUnsubscribe = new Subject<void>();
+  private currentUrl = '/';
+
+  constructor(private readonly router: Router) {
+    this.router.events
+      .pipe(
+        takeUntil(this.ngUnsubscribe),
+        filter((event) => event instanceof NavigationEnd),
+        tap(() => (this.currentUrl = this.router.url))
+      )
+      .subscribe();
+  }
+
+  isRouteActive(path: string): boolean {
+    if (path.length === 0 && this.currentUrl === '/') return true;
+    else if (path.length > 0 && this.currentUrl.includes(path)) return true;
+    return false;
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.complete();
+  }
 }
