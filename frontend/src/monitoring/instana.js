7db95018b2b2369bd30c7c02126a03105ebf2fa7
atlas <19825793+iuccio@users.noreply.github.com>
2022-04-27 14:42:25 +0200

    Add Instana Keys for all stages

---
+++ frontend/src/monitoring/instana.js
@@ -0,0 +1,21 @@
+(function (s, t, a, n) {
+  s[t] ||
+    ((s[t] = a),
+    (n = s[a] =
+      function () {
+        n.q.push(arguments);
+      }),
+    (n.q = []),
+    (n.v = 2),
+    (n.l = 1 * new Date()));
+})(window, 'InstanaEumObject', 'ineum');
+
+const instanaKey =
+  typeof process === 'object' && process.env && process.env.INSTANA_KEY
+    ? process.env.INSTANA_KEY
+    : 'JxmTF5fGTEmLrJeyAiPtaA';
+console.log('Logging to instanaKey=' + instanaKey);
+
+ineum('reportingUrl', 'https://eum-green-saas.instana.io');
+ineum('key', instanaKey);
+ineum('trackSessions');
