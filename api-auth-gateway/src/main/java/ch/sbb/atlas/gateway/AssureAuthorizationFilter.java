b65601bbc2e339b1195b16046d8ffe10e87db3c0
atlas <19825793+iuccio@users.noreply.github.com>
2022-05-05 11:22:40 +0200

    ATLAS-574: Add additional gateway which authorizes unauthorized requests with client credentials

---
+++ api-auth-gateway/src/main/java/ch/sbb/atlas/gateway/AssureAuthorizationFilter.java
@@ -0,0 +1,91 @@
+package ch.sbb.atlas.gateway;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+/**
+ * Required dependencies:
+ * <dependency>
+ *  <groupId>org.springframework.security</groupId>
+ *  <artifactId>spring-security-oauth2-client</artifactId>
+ * </dependency>
+ * <dependency>
+ *  <groupId>org.springframework.boot</groupId>
+ *  <artifactId>spring-boot-starter-security</artifactId>
+ * </dependency>
+ */
+@RequiredArgsConstructor
+@Component
+public class AssureAuthorizationFilter extends AbstractGatewayFilterFactory<Object> {
+
+  private final ReactiveClientRegistrationRepository clientRegistrations;
+  private final ReactiveOAuth2AuthorizedClientService clientService;
+
+  public GatewayFilter apply() {
+    return apply((Object) null);
+  }
+
+  @Override
+  public GatewayFilter apply(Object config) {
+    return addAccessTokenIfNotPresent();
+  }
+
+  /**
+   * GatewayFilter to add an Authorization Header, if currently not set
+   */
+  private GatewayFilter addAccessTokenIfNotPresent() {
+    return (exchange, chain) -> getClientCredentialAccessToken().map(accessToken -> {
+      if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+        exchange.getRequest()
+                .mutate()
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                .build();
+      }
+      return exchange;
+    }).defaultIfEmpty(exchange).flatMap(chain::filter);
+  }
+
+  private Mono<String> getClientCredentialAccessToken() {
+    ReactiveOAuth2AuthorizedClientManager manager = new AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(
+        clientRegistrations, clientService);
+    return manager.authorize(getAuthorizateRequest())
+                  .map(oAuth2AuthorizedClient -> oAuth2AuthorizedClient.getAccessToken()
+                                                                       .getTokenValue());
+  }
+
+  /**
+   * OAuth2AuthorizeRequest to execute.
+   * Configure your application with spring in application.yml as follows:
+   *
+   * spring:
+   *  security:
+   *    oauth2:
+   *      client:
+   *        registration:
+   *          sbb:
+   *            client-id: 39ff0746-6345-4204-afd3-3e114e5ab715
+   *            client-secret: super-secret-text
+   *            authorization-grant-type: client_credentials
+   *            scope: api://87e6e634-6ba1-4e7a-869d-3348b4c3eafc/.default
+   *         provider:
+   *          sbb:
+   *            token-uri: https://login.microsoftonline.com/2cda5d11-f0ac-46b3-967d-af1b2e1bd01a/oauth2/v2.0/token
+   *
+   * The registrationId has to match the path in your settings.
+   */
+  private static OAuth2AuthorizeRequest getAuthorizateRequest() {
+    return OAuth2AuthorizeRequest.withClientRegistrationId("sbb")
+                                 .principal("unauthenticated")
+                                 .build();
+  }
+
+}
---
+++ api-auth-gateway/src/main/java/ch/sbb/atlas/gateway/AtlasApiAuthGatewayApplication.java
@@ -0,0 +1,16 @@
+package ch.sbb.atlas.gateway;
+
+import java.time.ZoneId;
+import java.util.TimeZone;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlasApiAuthGatewayApplication {
+
+  public static void main(String[] args) {
+    TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("Europe/Zurich")));
+    SpringApplication.run(AtlasApiAuthGatewayApplication.class, args);
+  }
+
+}
