2d222100c8746a0b3dec11dac057e8fcef742d20
atlas <TechSupport-ATLAS@sbb.ch>
2022-05-17 11:08:06 +0200

    ATLAS-236: add business-organisation-directory module

---
+++ business-organisation-directory/src/test/java/ch/sbb/business/organisation/directory/BusinessOrganisationDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.sbb.business.organisation.directory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessOrganisationDirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
