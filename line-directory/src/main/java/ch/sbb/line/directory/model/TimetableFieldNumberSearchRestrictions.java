c5eab59d30ad4db6ee8a1c29a9ee2188eb138911
atlas <TechSupport-ATLAS@sbb.ch>
2022-05-18 09:49:27 +0200

    ATLAS-731: Config in Restriction integrieren

--- line-directory/src/main/java/ch/sbb/line/directory/model/TimetableFieldNumberSearchRestrictions.java
+++ line-directory/src/main/java/ch/sbb/line/directory/model/TimetableFieldNumberSearchRestrictions.java
@@ -2,8 +2,11 @@ package ch.sbb.line.directory.model;
 
 import ch.sbb.atlas.model.Status;
 import ch.sbb.atlas.searching.SearchRestrictions;
+import ch.sbb.atlas.searching.SpecificationBuilder;
+import ch.sbb.line.directory.entity.Line;
 import ch.sbb.line.directory.entity.TimetableFieldNumber;
 import ch.sbb.line.directory.entity.TimetableFieldNumber_;
+import java.util.List;
 import javax.persistence.metamodel.SingularAttribute;
 import lombok.Getter;
 import lombok.ToString;
@@ -18,6 +21,19 @@ public class TimetableFieldNumberSearchRestrictions extends
   @Override
   protected SingularAttribute<TimetableFieldNumber, Status> getStatus() {
     return TimetableFieldNumber_.status;
+  }
+
+  @Override
+  protected SpecificationBuilder<TimetableFieldNumber> specificationBuilder() {
+    return SpecificationBuilder.<TimetableFieldNumber>builder()
+                               .stringAttributes(
+                                   List.of(TimetableFieldNumber_.swissTimetableFieldNumber,
+                                       TimetableFieldNumber_.description,
+                                       TimetableFieldNumber_.ttfnid, TimetableFieldNumber_.number,
+                                       TimetableFieldNumber_.businessOrganisation))
+                               .validFromAttribute(TimetableFieldNumber_.validFrom)
+                               .validToAttribute(TimetableFieldNumber_.validTo)
+                               .build();
   }
 
 }
