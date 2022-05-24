6c3b385c9a7baa5656890f1581ea84841e02efb8
atlas <TechSupport-ATLAS@sbb.ch>
2022-05-24 11:17:24 +0200

    ATLAS-263: add controller tests

--- business-organisation-directory/src/test/java/ch/sbb/business/organisation/directory/ApimYamlExtractionTest.java
+++ business-organisation-directory/src/test/java/ch/sbb/business/organisation/directory/ApimYamlExtractionTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import ch.sbb.atlas.model.controller.IntegrationTest;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -18,7 +19,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 @SpringBootTest
-// TODO: use integrationtest annotation
+@IntegrationTest
 @ActiveProfiles("integration-test")
 @AutoConfigureMockMvc(addFilters = false)
 @Slf4j
--- business-organisation-directory/src/test/java/ch/sbb/business/organisation/directory/controller/BusinessOrganisationControllerApiTest.java
+++ business-organisation-directory/src/test/java/ch/sbb/business/organisation/directory/controller/BusinessOrganisationControllerApiTest.java
@@ -1,6 +1,24 @@
 package ch.sbb.business.organisation.directory.controller;
 
+import static ch.sbb.business.organisation.directory.api.BusinessOrganisationVersionModel.Fields.abbreviationDe;
+import static ch.sbb.business.organisation.directory.api.BusinessOrganisationVersionModel.Fields.abbreviationEn;
+import static ch.sbb.business.organisation.directory.api.BusinessOrganisationVersionModel.Fields.abbreviationFr;
+import static ch.sbb.business.organisation.directory.api.BusinessOrganisationVersionModel.Fields.abbreviationIt;
+import static ch.sbb.business.organisation.directory.api.BusinessOrganisationVersionModel.Fields.businessTypes;
+import static ch.sbb.business.organisation.directory.api.BusinessOrganisationVersionModel.Fields.contactEnterpriseEmail;
+import static ch.sbb.business.organisation.directory.api.BusinessOrganisationVersionModel.Fields.descriptionDe;
+import static ch.sbb.business.organisation.directory.api.BusinessOrganisationVersionModel.Fields.descriptionEn;
+import static ch.sbb.business.organisation.directory.api.BusinessOrganisationVersionModel.Fields.descriptionFr;
+import static ch.sbb.business.organisation.directory.api.BusinessOrganisationVersionModel.Fields.descriptionIt;
+import static ch.sbb.business.organisation.directory.api.BusinessOrganisationVersionModel.Fields.organisationNumber;
+import static ch.sbb.business.organisation.directory.api.BusinessOrganisationVersionModel.Fields.types;
+import static ch.sbb.business.organisation.directory.api.BusinessOrganisationVersionModel.Fields.validFrom;
+import static ch.sbb.business.organisation.directory.api.BusinessOrganisationVersionModel.Fields.validTo;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import ch.sbb.atlas.model.Status;
@@ -21,6 +39,9 @@ public class BusinessOrganisationControllerApiTest extends BaseControllerApiTest
 
   @Autowired
   private BusinessOrganisationVersionRepository versionRepository;
+
+  @Autowired
+  private BusinessOrganisationController controller;
 
   private final BusinessOrganisationVersion version =
       BusinessOrganisationVersion.builder()
@@ -83,6 +104,199 @@ public class BusinessOrganisationControllerApiTest extends BaseControllerApiTest
         .content(mapper.writeValueAsString(model))
     ).andExpect(status().isCreated());
 
+  }
+
+  @Test
+  public void shouldGelBusinessOrganisationVersions() throws Exception {
+    //given
+    BusinessOrganisationVersionModel model =
+        BusinessOrganisationVersionModel.builder()
+                                        .sboid("ch:1:sboid:1000001")
+                                        .abbreviationDe("de1")
+                                        .abbreviationFr("fr1")
+                                        .abbreviationIt("it1")
+                                        .abbreviationEn("en1")
+                                        .descriptionDe("desc-de1")
+                                        .descriptionFr("desc-fr1")
+                                        .descriptionIt("desc-it1")
+                                        .descriptionEn("desc-en1")
+                                        .businessTypes(new HashSet<>(
+                                            Arrays.asList(BusinessType.RAILROAD, BusinessType.AIR,
+                                                BusinessType.SHIP)))
+                                        .contactEnterpriseEmail("mail1@mail.ch")
+                                        .organisationNumber(1234)
+                                        .status(Status.ACTIVE)
+                                        .validFrom(LocalDate.of(2001, 1, 1))
+                                        .validTo(LocalDate.of(2001, 12, 31))
+                                        .build();
+    controller.createBusinessOrganisationVersion(model);
+
+    //when and then
+    mvc.perform(get("/v1/business-organisations")
+       ).andExpect(status().isOk())
+       .andExpect(jsonPath("$[0]." + businessTypes,
+           containsInAnyOrder(BusinessType.RAILROAD.name(), BusinessType.AIR.name(),
+               BusinessType.SHIP.name())))
+       .andExpect(jsonPath("$[0]." + types, is("10|20|45")))
+       .andExpect(jsonPath("$[0]." + validFrom, is("2000-01-01")))
+       .andExpect(jsonPath("$[0]." + validTo, is("2000-12-31")))
+       .andExpect(jsonPath("$[0]." + organisationNumber, is(123)))
+       .andExpect(jsonPath("$[0]." + contactEnterpriseEmail, is("mail@mail.ch")))
+       .andExpect(jsonPath("$[0]." + descriptionDe, is("desc-de")))
+       .andExpect(jsonPath("$[0]." + descriptionFr, is("desc-fr")))
+       .andExpect(jsonPath("$[0]." + descriptionIt, is("desc-it")))
+       .andExpect(jsonPath("$[0]." + descriptionEn, is("desc-en")))
+       .andExpect(jsonPath("$[0]." + abbreviationDe, is("de")))
+       .andExpect(jsonPath("$[0]." + abbreviationFr, is("fr")))
+       .andExpect(jsonPath("$[0]." + abbreviationIt, is("it")))
+       .andExpect(jsonPath("$[0]." + abbreviationEn, is("en")))
+
+       .andExpect(jsonPath("$[1]." + businessTypes,
+           containsInAnyOrder(BusinessType.RAILROAD.name(), BusinessType.AIR.name(),
+               BusinessType.SHIP.name())))
+       .andExpect(jsonPath("$[1]." + types, is("10|20|45")))
+       .andExpect(jsonPath("$[1]." + validFrom, is("2001-01-01")))
+       .andExpect(jsonPath("$[1]." + validTo, is("2001-12-31")))
+       .andExpect(jsonPath("$[1]." + organisationNumber, is(1234)))
+       .andExpect(jsonPath("$[1]." + contactEnterpriseEmail, is("mail1@mail.ch")))
+       .andExpect(jsonPath("$[1]." + descriptionDe, is("desc-de1")))
+       .andExpect(jsonPath("$[1]." + descriptionFr, is("desc-fr1")))
+       .andExpect(jsonPath("$[1]." + descriptionIt, is("desc-it1")))
+       .andExpect(jsonPath("$[1]." + descriptionEn, is("desc-en1")))
+       .andExpect(jsonPath("$[1]." + abbreviationDe, is("de1")))
+       .andExpect(jsonPath("$[1]." + abbreviationFr, is("fr1")))
+       .andExpect(jsonPath("$[1]." + abbreviationIt, is("it1")))
+       .andExpect(jsonPath("$[1]." + abbreviationEn, is("en1")));
+  }
+
+  @Test
+  public void shouldNotCreateBusinessOrganisationVersionWhenRequiredAbbreviationDeFieldProvidedIsTooLong()
+      throws Exception {
+    //given
+    BusinessOrganisationVersionModel model =
+        BusinessOrganisationVersionModel.builder()
+                                        .sboid("ch:1:sboid:100000")
+                                        .abbreviationDe("de")
+                                        .abbreviationFr("fr-fr-fr-fr-fr-fr-fr")
+                                        .abbreviationIt("it")
+                                        .abbreviationEn("en")
+                                        .descriptionDe("desc-de")
+                                        .descriptionFr("desc-fr")
+                                        .descriptionIt("desc-it")
+                                        .descriptionEn("desc-en")
+                                        .businessTypes(new HashSet<>(
+                                            Arrays.asList(BusinessType.RAILROAD, BusinessType.AIR,
+                                                BusinessType.SHIP)))
+                                        .contactEnterpriseEmail("mail@mail.ch")
+                                        .organisationNumber(123)
+                                        .status(Status.ACTIVE)
+                                        .validFrom(LocalDate.of(2000, 1, 1))
+                                        .validTo(LocalDate.of(2000, 12, 31))
+                                        .build();
+
+    //when and then
+    mvc.perform(post("/v1/business-organisations/versions")
+           .contentType(contentType)
+           .content(mapper.writeValueAsString(model))
+       ).andExpect(status().isBadRequest())
+       .andExpect(jsonPath("$.status", is(400)))
+       .andExpect(jsonPath("$.message", is("Constraint for requestbody was violated")))
+       .andExpect(jsonPath("$.error", is("Method argument not valid error")))
+       .andExpect(jsonPath("$.details[0].message",
+           is("Value fr-fr-fr-fr-fr-fr-fr rejected due to size must be between 1 and 10")))
+       .andExpect(jsonPath("$.details[0].field", is("abbreviationFr")))
+       .andExpect(jsonPath("$.details[0].displayInfo.code", is("ERROR.CONSTRAINT")))
+       .andExpect(jsonPath("$.details[0].displayInfo.parameters[0].key", is("rejectedValue")))
+       .andExpect(
+           jsonPath("$.details[0].displayInfo.parameters[0].value", is("fr-fr-fr-fr-fr-fr-fr")))
+       .andExpect(jsonPath("$.details[0].displayInfo.parameters[1].key", is("cause")))
+       .andExpect(jsonPath("$.details[0].displayInfo.parameters[1].value",
+           is("size must be between 1 and 10")));
+  }
+
+
+  @Test
+  public void shouldNotUpdateBusinessOrganisationVersionWhenRequiredAbbreviationDeFieldNotProvided()
+      throws Exception {
+    //given
+    BusinessOrganisationVersionModel model =
+        BusinessOrganisationVersionModel.builder()
+                                        .sboid("ch:1:sboid:100000")
+                                        .abbreviationFr("fr")
+                                        .abbreviationIt("it")
+                                        .abbreviationEn("en")
+                                        .descriptionDe("desc-de")
+                                        .descriptionFr("desc-fr")
+                                        .descriptionIt("desc-it")
+                                        .descriptionEn("desc-en")
+                                        .businessTypes(new HashSet<>(
+                                            Arrays.asList(BusinessType.RAILROAD, BusinessType.AIR,
+                                                BusinessType.SHIP)))
+                                        .contactEnterpriseEmail("mail@mail.ch")
+                                        .organisationNumber(123)
+                                        .status(Status.ACTIVE)
+                                        .validFrom(LocalDate.of(2000, 1, 1))
+                                        .validTo(LocalDate.of(2000, 12, 31))
+                                        .build();
+
+    //when and then
+    mvc.perform(post("/v1/business-organisations/versions")
+           .contentType(contentType)
+           .content(mapper.writeValueAsString(model))
+       ).andExpect(status().isBadRequest())
+       .andExpect(jsonPath("$.status", is(400)))
+       .andExpect(jsonPath("$.message", is("Constraint for requestbody was violated")))
+       .andExpect(jsonPath("$.error", is("Method argument not valid error")))
+       .andExpect(
+           jsonPath("$.details[0].message", is("Value null rejected due to must not be null")))
+       .andExpect(jsonPath("$.details[0].field", is("abbreviationDe")))
+       .andExpect(jsonPath("$.details[0].displayInfo.code", is("ERROR.CONSTRAINT")))
+       .andExpect(jsonPath("$.details[0].displayInfo.parameters[0].key", is("rejectedValue")))
+       .andExpect(jsonPath("$.details[0].displayInfo.parameters[0].value", is("null")))
+       .andExpect(jsonPath("$.details[0].displayInfo.parameters[1].key", is("cause")))
+       .andExpect(jsonPath("$.details[0].displayInfo.parameters[1].value", is("must not be null")));
+  }
+
+  @Test
+  public void shouldNotUpdateBusinessOrganisationVersionWhenProvidedIdDoesNotExists()
+      throws Exception {
+    //given
+    BusinessOrganisationVersionModel model =
+        BusinessOrganisationVersionModel.builder()
+                                        .sboid("ch:1:sboid:100000")
+                                        .abbreviationDe("de")
+                                        .abbreviationFr("fr")
+                                        .abbreviationIt("it")
+                                        .abbreviationEn("en")
+                                        .descriptionDe("desc-de")
+                                        .descriptionFr("desc-fr")
+                                        .descriptionIt("desc-it")
+                                        .descriptionEn("desc-en")
+                                        .businessTypes(new HashSet<>(
+                                            Arrays.asList(BusinessType.RAILROAD, BusinessType.AIR,
+                                                BusinessType.SHIP)))
+                                        .contactEnterpriseEmail("mail@mail.ch")
+                                        .organisationNumber(123)
+                                        .status(Status.ACTIVE)
+                                        .validFrom(LocalDate.of(2000, 1, 1))
+                                        .validTo(LocalDate.of(2000, 12, 31))
+                                        .build();
+
+    //when and then
+    mvc.perform(post("/v1/business-organisations/versions/123456789")
+           .contentType(contentType)
+           .content(mapper.writeValueAsString(model))
+       ).andExpect(status().isNotFound())
+       .andExpect(jsonPath("$.status", is(404)))
+       .andExpect(jsonPath("$.message", is("Entity not found")))
+       .andExpect(jsonPath("$.error", is("Not found")))
+       .andExpect(jsonPath("$.details[0].message", is("Object with id 123456789 not found")))
+       .andExpect(jsonPath("$.details[0].field", is("id")))
+       .andExpect(jsonPath("$.details[0].displayInfo.code", is("ERROR.ENTITY_NOT_FOUND")))
+       .andExpect(jsonPath("$.details[0].displayInfo.parameters[0].key", is("field")))
+       .andExpect(jsonPath("$.details[0].displayInfo.parameters[0].value", is("id")))
+       .andExpect(jsonPath("$.details[0].displayInfo.parameters[1].key", is("value")))
+       .andExpect(jsonPath("$.details[0].displayInfo.parameters[1].value", is("123456789")));
   }
 
 
