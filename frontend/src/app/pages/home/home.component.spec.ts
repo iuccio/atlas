6888ccbeec957118b1cbebf862888b5daabe7413
atlas <19825793+iuccio@users.noreply.github.com>
2022-05-31 10:22:49 +0200

    ATLAS-242: Update to 3 cards

--- frontend/src/app/pages/home/home.component.spec.ts
+++ frontend/src/app/pages/home/home.component.spec.ts
@@ -32,8 +32,8 @@ describe('HomeComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should create 2 cards', () => {
+  it('should create 3 cards', () => {
     const cards = fixture.debugElement.queryAll(By.css('.card'));
-    expect(cards.length).toBeGreaterThan(1);
+    expect(cards.length).toBe(3);
   });
 });
