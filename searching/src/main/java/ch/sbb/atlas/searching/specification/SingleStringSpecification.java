a92a7c358a048a630afae450683cbc0ad065ee29
atlas <19825793+iuccio@users.noreply.github.com>
2022-05-31 11:39:42 +0200

    ATLAS-241: code clean up

--- searching/src/main/java/ch/sbb/atlas/searching/specification/SingleStringSpecification.java
+++ searching/src/main/java/ch/sbb/atlas/searching/specification/SingleStringSpecification.java
@@ -12,21 +12,24 @@ import org.springframework.data.jpa.domain.Specification;
 
 public class SingleStringSpecification<T> implements Specification<T> {
 
-    private final Optional<String> searchString;
-    private final SingularAttribute<T, String> stringAttribute;
+  private static final long serialVersionUID = 1;
 
-    public SingleStringSpecification(Optional<String> searchString, SingularAttribute<T, String> stringAttribute) {
-      this.searchString = Objects.requireNonNull(searchString);
-      this.stringAttribute = stringAttribute;
-    }
+  private final Optional<String> searchString;
+  private final SingularAttribute<T, String> stringAttribute;
 
-    @Override
-    public Predicate toPredicate(Root<T> root, CriteriaQuery<?> query,
-        CriteriaBuilder criteriaBuilder) {
-      if (searchString.isEmpty()) {
-        return criteriaBuilder.and();
-      }
-      return StringPredicates.likeIgnoreCase(criteriaBuilder, root.get(stringAttribute),
-          searchString.orElseThrow());
-    }
+  public SingleStringSpecification(Optional<String> searchString,
+      SingularAttribute<T, String> stringAttribute) {
+    this.searchString = Objects.requireNonNull(searchString);
+    this.stringAttribute = stringAttribute;
   }
+
+  @Override
+  public Predicate toPredicate(Root<T> root, CriteriaQuery<?> query,
+      CriteriaBuilder criteriaBuilder) {
+    if (searchString.isEmpty()) {
+      return criteriaBuilder.and();
+    }
+    return StringPredicates.likeIgnoreCase(criteriaBuilder, root.get(stringAttribute),
+        searchString.orElseThrow());
+  }
+}
