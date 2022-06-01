d12680252ed2d4c3382abed87977c56a12e80dbc
atlas <19825793+iuccio@users.noreply.github.com>
2022-06-01 11:11:00 +0200

    ATLAS-248: Move logic to service as per review

--- business-organisation-directory/src/main/java/ch/sbb/business/organisation/directory/service/BusinessOrganisationValidationService.java
+++ business-organisation-directory/src/main/java/ch/sbb/business/organisation/directory/service/BusinessOrganisationValidationService.java
@@ -4,6 +4,9 @@ import ch.sbb.business.organisation.directory.entity.BusinessOrganisationVersion
 import ch.sbb.business.organisation.directory.exception.BusinessOrganisationAbbreviationConflictException;
 import ch.sbb.business.organisation.directory.repository.BusinessOrganisationVersionRepository;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,11 +21,32 @@ public class BusinessOrganisationValidationService {
   }
 
   private void validateAbbreviationConflict(BusinessOrganisationVersion version) {
-    List<BusinessOrganisationVersion> overlaps = versionRepository.findAbbreviationOverlaps(
+    List<BusinessOrganisationVersion> overlaps = findAbbreviationOverlaps(
         version);
     if (!overlaps.isEmpty()) {
       throw new BusinessOrganisationAbbreviationConflictException(version, overlaps);
     }
+  }
+
+  List<BusinessOrganisationVersion> findAbbreviationOverlaps(
+      BusinessOrganisationVersion version) {
+    return Stream.of(
+                     versionRepository.findAllByValidToGreaterThanEqualAndValidFromLessThanEqualAndAbbreviationDeIgnoreCase(
+                                          version.getValidFrom(), version.getValidTo(), version.getAbbreviationDe())
+                                      .stream(),
+                     versionRepository.findAllByValidToGreaterThanEqualAndValidFromLessThanEqualAndAbbreviationFrIgnoreCase(
+                                          version.getValidFrom(), version.getValidTo(), version.getAbbreviationFr())
+                                      .stream(),
+                     versionRepository.findAllByValidToGreaterThanEqualAndValidFromLessThanEqualAndAbbreviationItIgnoreCase(
+                                          version.getValidFrom(), version.getValidTo(), version.getAbbreviationIt())
+                                      .stream(),
+                     versionRepository.findAllByValidToGreaterThanEqualAndValidFromLessThanEqualAndAbbreviationEnIgnoreCase(
+                                          version.getValidFrom(), version.getValidTo(), version.getAbbreviationEn())
+                                      .stream())
+                 .flatMap(Function.identity())
+                 .filter(i -> !i.getSboid().equals(version.getSboid()))
+                 .distinct()
+                 .collect(Collectors.toList());
   }
 
 }
