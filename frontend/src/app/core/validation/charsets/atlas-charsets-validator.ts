002f937c6cc0a11880a3a08a4b7227b839479c18
atlas <19825793+iuccio@users.noreply.github.com>
2022-06-09 15:30:29 +0200

    ATLAS-330: Nur 5 Zahlen sind erlaubt

--- frontend/src/app/core/validation/charsets/atlas-charsets-validator.ts
+++ frontend/src/app/core/validation/charsets/atlas-charsets-validator.ts
@@ -37,6 +37,10 @@ export class AtlasCharsetsValidator {
     );
   }
 
+  static fiveNumbers(control: AbstractControl): ValidationErrors | null {
+    return AtlasCharsetsValidator.validateAllowedCharacters(control, '[0-9]{1,5}', '0-9');
+  }
+
   private static validateAllowedCharacters(
     control: AbstractControl,
     pattern: string,
