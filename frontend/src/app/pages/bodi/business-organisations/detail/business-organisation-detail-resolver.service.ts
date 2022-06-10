0da0823afab77341eb6d0d6ea6714ed4e87fc82b
atlas <19825793+iuccio@users.noreply.github.com>
2022-06-10 11:18:23 +0200

    ATLAS-514: pr review (catchError with routing to resolvers)

--- frontend/src/app/pages/bodi/business-organisations/detail/business-organisation-detail-resolver.service.ts
+++ frontend/src/app/pages/bodi/business-organisations/detail/business-organisation-detail-resolver.service.ts
@@ -1,18 +1,30 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import { ActivatedRouteSnapshot, Resolve, Router } from '@angular/router';
+import { catchError, Observable, of } from 'rxjs';
 import { BusinessOrganisationsService, BusinessOrganisationVersion } from '../../../../api';
+import { Pages } from '../../../pages';
 
 @Injectable({ providedIn: 'root' })
 export class BusinessOrganisationDetailResolver
   implements Resolve<Array<BusinessOrganisationVersion>>
 {
-  constructor(private businessOrganisationsService: BusinessOrganisationsService) {}
+  constructor(
+    private readonly businessOrganisationsService: BusinessOrganisationsService,
+    private readonly router: Router
+  ) {}
 
   resolve(route: ActivatedRouteSnapshot): Observable<Array<BusinessOrganisationVersion>> {
     const idParameter = route.paramMap.get('id') || '';
     return idParameter === 'add'
       ? of([])
-      : this.businessOrganisationsService.getBusinessOrganisationVersions(idParameter);
+      : this.businessOrganisationsService.getBusinessOrganisationVersions(idParameter).pipe(
+          catchError(() =>
+            this.router
+              .navigate([Pages.BODI.path], {
+                state: { notDismissSnackBar: true },
+              })
+              .then(() => [])
+          )
+        );
   }
 }
--- frontend/src/app/pages/ttfn/detail/timetable-field-number-detail.resolver.ts
+++ frontend/src/app/pages/ttfn/detail/timetable-field-number-detail.resolver.ts
@@ -1,18 +1,30 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import { ActivatedRouteSnapshot, Resolve, Router } from '@angular/router';
+import { catchError, Observable, of } from 'rxjs';
 import { TimetableFieldNumbersService, TimetableFieldNumberVersion } from '../../../api';
+import { Pages } from '../../pages';
 
 @Injectable({ providedIn: 'root' })
 export class TimetableFieldNumberDetailResolver
   implements Resolve<Array<TimetableFieldNumberVersion>>
 {
-  constructor(private timetableFieldNumbersService: TimetableFieldNumbersService) {}
+  constructor(
+    private readonly timetableFieldNumbersService: TimetableFieldNumbersService,
+    private readonly router: Router
+  ) {}
 
   resolve(route: ActivatedRouteSnapshot): Observable<Array<TimetableFieldNumberVersion>> {
     const idParameter = route.paramMap.get('id') || '';
     return idParameter === 'add'
       ? of([])
-      : this.timetableFieldNumbersService.getAllVersionsVersioned(idParameter);
+      : this.timetableFieldNumbersService.getAllVersionsVersioned(idParameter).pipe(
+          catchError(() =>
+            this.router
+              .navigate([Pages.TTFN.path], {
+                state: { notDismissSnackBar: true },
+              })
+              .then(() => [])
+          )
+        );
   }
 }
