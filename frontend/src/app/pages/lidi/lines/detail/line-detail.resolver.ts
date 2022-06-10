b8502e718688c23825ed2c6a8109a156a6c4a031
atlas <19825793+iuccio@users.noreply.github.com>
2022-06-10 11:18:23 +0200

    ATLAS-514: pr review (catchError with routing to resolvers)

--- frontend/src/app/pages/lidi/lines/detail/line-detail.resolver.ts
+++ frontend/src/app/pages/lidi/lines/detail/line-detail.resolver.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import { ActivatedRouteSnapshot, Resolve, Router } from '@angular/router';
+import { catchError, Observable, of } from 'rxjs';
 import { LinesService, LineVersion } from '../../../../api';
+import { Pages } from '../../../pages';
 
 @Injectable({ providedIn: 'root' })
 export class LineDetailResolver implements Resolve<Array<LineVersion>> {
-  constructor(private linesService: LinesService) {}
+  constructor(private readonly linesService: LinesService, private readonly router: Router) {}
 
   resolve(route: ActivatedRouteSnapshot): Observable<Array<LineVersion>> {
     const idParameter = route.paramMap.get('id') || '';
-    return idParameter === 'add' ? of([]) : this.linesService.getLineVersions(idParameter);
+    return idParameter === 'add'
+      ? of([])
+      : this.linesService.getLineVersions(idParameter).pipe(
+          catchError(() =>
+            this.router
+              .navigate([Pages.LIDI.path], {
+                state: { notDismissSnackBar: true },
+              })
+              .then(() => [])
+          )
+        );
   }
 }
--- frontend/src/app/pages/lidi/sublines/detail/subline-detail.resolver.ts
+++ frontend/src/app/pages/lidi/sublines/detail/subline-detail.resolver.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import { ActivatedRouteSnapshot, Resolve, Router } from '@angular/router';
+import { catchError, Observable, of } from 'rxjs';
 import { SublinesService, SublineVersion } from '../../../../api';
+import { Pages } from '../../../pages';
 
 @Injectable({ providedIn: 'root' })
 export class SublineDetailResolver implements Resolve<Array<SublineVersion>> {
-  constructor(private sublinesService: SublinesService) {}
+  constructor(private readonly sublinesService: SublinesService, private readonly router: Router) {}
 
   resolve(route: ActivatedRouteSnapshot): Observable<Array<SublineVersion>> {
     const idParameter = route.paramMap.get('id') || '';
-    return idParameter === 'add' ? of([]) : this.sublinesService.getSublineVersion(idParameter);
+    return idParameter === 'add'
+      ? of([])
+      : this.sublinesService.getSublineVersion(idParameter).pipe(
+          catchError(() =>
+            this.router
+              .navigate([Pages.LIDI.path, Pages.SUBLINES.path], {
+                state: { notDismissSnackBar: true },
+              })
+              .then(() => [])
+          )
+        );
   }
 }
