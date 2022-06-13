868eeafbce6f84e6e2b4b94a030f78e835089f59
atlas <19825793+iuccio@users.noreply.github.com>
2022-06-13 17:47:16 +0200

    ATLAS-749: Drop views, use subselect

--- line-directory/src/main/java/ch/sbb/line/directory/entity/TimetableFieldNumber.java
+++ line-directory/src/main/java/ch/sbb/line/directory/entity/TimetableFieldNumber.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.FieldNameConstants;
 import org.hibernate.annotations.Immutable;
+import org.hibernate.annotations.Subselect;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -22,7 +23,8 @@ import org.hibernate.annotations.Immutable;
 @Builder
 @Immutable
 @FieldNameConstants
-@Entity(name = "timetable_field_number")
+@Entity
+@Subselect(TimetableFieldNumber.VIEW_DEFINITION)
 public class TimetableFieldNumber {
 
   @Id
@@ -45,4 +47,39 @@ public class TimetableFieldNumber {
   @Column(columnDefinition = "TIMESTAMP")
   private LocalDate validTo;
 
+  static final String VIEW_DEFINITION = "select *"
+      + "from ("
+      + "         select f.*, v.valid_from, v.valid_to"
+      + "         from ("
+      + "                  select swiss_timetable_field_number,"
+      + "                         number,"
+      + "                         description,"
+      + "                         status,"
+      + "                         ttfnid,"
+      + "                         business_organisation,"
+      + "                         valid_from as vf"
+      + "                  from ("
+      + "                           select distinct on (ttfnid) *"
+      + "                           from ((select distinct on (ttfnid) 1 as rank, *"
+      + "                                  from timetable_field_number_version"
+      + "                                  where valid_from <= current_timestamp"
+      + "                                    and current_timestamp <= valid_to)"
+      + "                                 union all"
+      + "                                 (select distinct on (ttfnid) 2 as rank, *"
+      + "                                  from timetable_field_number_version"
+      + "                                  where valid_from >= current_timestamp"
+      + "                                  order by ttfnid, valid_from)"
+      + "                                 union all"
+      + "                                 (select distinct on (ttfnid) 3 as rank, *"
+      + "                                  from timetable_field_number_version"
+      + "                                  where valid_to <= current_timestamp"
+      + "                                  order by ttfnid, valid_to desc)) as ranked order by ttfnid, rank"
+      + "                       ) as chosen"
+      + "              ) f"
+      + "                  join ("
+      + "             select ttfnid, min(valid_from) as valid_from, max(valid_to) as valid_to"
+      + "             from timetable_field_number_version"
+      + "             group by ttfnid"
+      + "         ) v on f.ttfnid = v.ttfnid"
+      + "     ) as timetable_field_numbers";
 }
