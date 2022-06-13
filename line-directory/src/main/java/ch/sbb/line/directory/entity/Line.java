7cf7b1630686b3bea19e1f87adb619640f92a5c6
atlas <19825793+iuccio@users.noreply.github.com>
2022-06-13 17:47:16 +0200

    ATLAS-749: Drop views, use subselect

--- line-directory/src/main/java/ch/sbb/line/directory/entity/Line.java
+++ line-directory/src/main/java/ch/sbb/line/directory/entity/Line.java
@@ -15,6 +15,7 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.FieldNameConstants;
 import org.hibernate.annotations.Immutable;
+import org.hibernate.annotations.Subselect;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -23,7 +24,8 @@ import org.hibernate.annotations.Immutable;
 @Builder
 @Immutable
 @FieldNameConstants
-@Entity(name = "line")
+@Entity
+@Subselect(Line.VIEW_DEFINITION)
 public class Line {
 
   private String swissLineNumber;
@@ -49,4 +51,39 @@ public class Line {
   @Column(columnDefinition = "TIMESTAMP")
   private LocalDate validTo;
 
+  static final String VIEW_DEFINITION = "select *"
+      + "from ("
+      + "         select f.*, v.valid_from, v.valid_to"
+      + "         from ("
+      + "                  select swiss_line_number,"
+      + "                         number,"
+      + "                         description,"
+      + "                         status,"
+      + "                         line_type,"
+      + "                         business_organisation,"
+      + "                         slnid"
+      + "                  from ("
+      + "                           select distinct on (slnid) *"
+      + "                           from ((select distinct on (slnid) 1 as rank, *"
+      + "                                  from line_version"
+      + "                                  where valid_from <= current_timestamp"
+      + "                                    and current_timestamp <= valid_to)"
+      + "                                 union all"
+      + "                                 (select distinct on (slnid) 2 as rank, *"
+      + "                                  from line_version"
+      + "                                  where valid_from >= current_timestamp"
+      + "                                  order by slnid, valid_from)"
+      + "                                 union all"
+      + "                                 (select distinct on (slnid) 3 as rank, *"
+      + "                                  from line_version"
+      + "                                  where valid_to <= current_timestamp"
+      + "                                  order by slnid, valid_to desc)) as ranked order by slnid, rank"
+      + "                       ) as chosen"
+      + "              ) f"
+      + "                  join ("
+      + "             select slnid, min(valid_from) as valid_from, max(valid_to) as valid_to"
+      + "             from line_version"
+      + "             group by slnid"
+      + "         ) v on f.slnid = v.slnid"
+      + "     ) as lines";
 }
--- line-directory/src/main/java/ch/sbb/line/directory/entity/Subline.java
+++ line-directory/src/main/java/ch/sbb/line/directory/entity/Subline.java
@@ -15,6 +15,7 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.FieldNameConstants;
 import org.hibernate.annotations.Immutable;
+import org.hibernate.annotations.Subselect;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -23,7 +24,8 @@ import org.hibernate.annotations.Immutable;
 @Builder
 @Immutable
 @FieldNameConstants
-@Entity(name = "subline")
+@Entity
+@Subselect(Subline.VIEW_DEFINITION)
 public class Subline {
 
   private String swissSublineNumber;
@@ -51,4 +53,73 @@ public class Subline {
   @Column(columnDefinition = "TIMESTAMP")
   private LocalDate validTo;
 
+  static final String VIEW_DEFINITION = "select *"
+      + "from ("
+      + "         select f.*, v.valid_from, v.valid_to"
+      + "         from ("
+      + "                  select swiss_subline_number,"
+      + "                         description,"
+      + "                         swiss_line_number,"
+      + "                         number,"
+      + "                         status,"
+      + "                         subline_type,"
+      + "                         business_organisation,"
+      + "                         slnid"
+      + "                  from ("
+      + "                           select distinct on (slnid) *"
+      + "                           from ((select distinct on (s.slnid) 1 as rank,"
+      + "                                                               s.swiss_subline_number,"
+      + "                                                               s.description,"
+      + "                                                               l.swiss_line_number,"
+      + "                                                               s.number,"
+      + "                                                               s.status,"
+      + "                                                               s.subline_type,"
+      + "                                                               s.business_organisation,"
+      + "                                                               s.slnid,"
+      + "                                                               s.valid_from,"
+      + "                                                               s.valid_to"
+      + "                                  from subline_version s"
+      + "                                           join (" + Line.VIEW_DEFINITION + ") l on s.mainline_slnid = l.slnid"
+      + "                                  where s.valid_from <= current_timestamp"
+      + "                                    and current_timestamp <= s.valid_to)"
+      + "                                 union all"
+      + "                                 (select distinct on (s.slnid) 2 as rank,"
+      + "                                                               s.swiss_subline_number,"
+      + "                                                               s.description,"
+      + "                                                               l.swiss_line_number,"
+      + "                                                               s.number,"
+      + "                                                               s.status,"
+      + "                                                               s.subline_type,"
+      + "                                                               s.business_organisation,"
+      + "                                                               s.slnid,"
+      + "                                                               s.valid_from,"
+      + "                                                               s.valid_to"
+      + "                                  from subline_version s"
+      + "                                           join (" + Line.VIEW_DEFINITION + ") l on s.mainline_slnid = l.slnid"
+      + "                                  where s.valid_from >= current_timestamp"
+      + "                                  order by s.slnid, s.valid_from)"
+      + "                                 union all"
+      + "                                 (select distinct on (s.slnid) 3 as rank,"
+      + "                                                               s.swiss_subline_number,"
+      + "                                                               s.description,"
+      + "                                                               l.swiss_line_number,"
+      + "                                                               s.number,"
+      + "                                                               s.status,"
+      + "                                                               s.subline_type,"
+      + "                                                               s.business_organisation,"
+      + "                                                               s.slnid,"
+      + "                                                               s.valid_from,"
+      + "                                                               s.valid_to"
+      + "                                  from subline_version s"
+      + "                                           join (" + Line.VIEW_DEFINITION + ") l on s.mainline_slnid = l.slnid"
+      + "                                  where s.valid_to <= current_timestamp"
+      + "                                  order by s.slnid, s.valid_to desc)) as ranked order by slnid, rank"
+      + "                       ) as chosen"
+      + "              ) f"
+      + "                  join ("
+      + "             select slnid, min(valid_from) as valid_from, max(valid_to) as valid_to"
+      + "             from subline_version"
+      + "             group by slnid"
+      + "         ) v on f.slnid = v.slnid"
+      + "     ) as sublines";
 }
