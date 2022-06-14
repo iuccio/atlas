4abe86a30afc8e59cd1a351ea04e590467e99fac
atlas <19825793+iuccio@users.noreply.github.com>
2022-06-14 18:55:26 +0200

    ATLAS-674: finished service worker implementation

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,7 @@
-import { ApplicationRef, Component } from '@angular/core';
-
+import { Component } from '@angular/core';
 import { LoadingSpinnerService } from './core/components/loading-spinner/loading-spinner.service';
 import { animate, style, transition, trigger } from '@angular/animations';
-import { SwUpdate } from '@angular/service-worker';
-import { MatDialog } from '@angular/material/dialog';
-import { DialogComponent } from './core/components/dialog/dialog.component';
-import { filter, first } from 'rxjs/operators';
-import { concat, interval } from 'rxjs';
+import { ServiceWorkerService } from './service-worker.service';
 
 @Component({
   selector: 'app-root',
@@ -17,62 +12,14 @@ import { concat, interval } from 'rxjs';
       transition('true <=> false', [style({ opacity: 0 }), animate(650, style({ opacity: 1 }))]),
     ]),
   ],
+  providers: [ServiceWorkerService],
 })
 export class AppComponent {
   constructor(
     public loadingSpinnerService: LoadingSpinnerService,
-    private readonly swUpdate: SwUpdate,
-    private readonly dialog: MatDialog,
-    private readonly appRef: ApplicationRef
+    private readonly _swService: ServiceWorkerService
   ) {
     loadingSpinnerService.initLoadingSpinner();
-
-    // TODO: service
-    if (swUpdate.isEnabled) {
-      const appIsStable$ = appRef.isStable.pipe(first((isStable) => isStable));
-      const checkForUpdateInterval$ = interval(60000);
-      const checkForUpdate$ = concat(appIsStable$, checkForUpdateInterval$);
-
-      checkForUpdate$.subscribe(() => swUpdate.checkForUpdate());
-
-      swUpdate.versionUpdates
-        .pipe(filter((versionEvent) => versionEvent.type === 'VERSION_READY'))
-        .subscribe(() => {
-          const dialogRef = dialog.open(DialogComponent, {
-            data: {
-              confirmText: 'DIALOG.RELOAD',
-              title: 'Newer version available',
-              message: 'There is a new version of the app available. You should reload the page',
-            },
-            panelClass: 'atlas-dialog-panel',
-            backdropClass: 'atlas-dialog-backdrop',
-          });
-
-          dialogRef.afterClosed().subscribe((result) => {
-            if (result) {
-              document.location.reload();
-            }
-          });
-        });
-      swUpdate.unrecoverable.subscribe((event) => {
-        const dialogRef = dialog.open(DialogComponent, {
-          data: {
-            message:
-              `Your version of the application is not recoverable because:\n${event.reason}\n\n` +
-              'Please reload the page. If this error occurs again, try with an hard refresh (Ctrl + F5)',
-            confirmText: 'DIALOG.RELOAD',
-            title: 'Unrecoverable version',
-          },
-          panelClass: 'atlas-dialog-panel',
-          backdropClass: 'atlas-dialog-backdrop',
-        });
-        dialogRef.afterClosed().subscribe((result) => {
-          if (result) {
-            document.location.reload();
-          }
-        });
-      });
-    }
   }
 }
 
---
+++ frontend/src/app/service-worker.service.ts
@@ -0,0 +1,50 @@
+import { ApplicationRef, Injectable } from '@angular/core';
+import { filter, first } from 'rxjs/operators';
+import { concat, interval } from 'rxjs';
+import { DialogComponent } from './core/components/dialog/dialog.component';
+import { SwUpdate } from '@angular/service-worker';
+import { MatDialog } from '@angular/material/dialog';
+
+@Injectable()
+export class ServiceWorkerService {
+  constructor(
+    private readonly appRef: ApplicationRef,
+    private readonly swUpdate: SwUpdate,
+    private readonly dialog: MatDialog
+  ) {
+    if (swUpdate.isEnabled) {
+      const appIsStable$ = appRef.isStable.pipe(first((isStable) => isStable));
+      const checkForUpdateInterval$ = interval(60000); // TODO: change to 5mins
+      const checkForUpdate$ = concat(appIsStable$, checkForUpdateInterval$);
+
+      checkForUpdate$.subscribe(() => swUpdate.checkForUpdate());
+
+      swUpdate.versionUpdates
+        .pipe(filter((versionEvent) => versionEvent.type === 'VERSION_READY'))
+        .subscribe(() => this.openSWDialog('SW_DIALOG.UPDATE_TITLE', 'SW_DIALOG.UPDATE_MESSAGE'));
+
+      swUpdate.unrecoverable.subscribe(() =>
+        this.openSWDialog('SW_DIALOG.UNRECOVERABLE_TITLE', 'SW_DIALOG.UNRECOVERABLE_MESSAGE')
+      );
+    }
+  }
+
+  private openSWDialog(titleTranslateKey: string, messageTranslateKey: string): void {
+    this.dialog
+      .open(DialogComponent, {
+        data: {
+          confirmText: 'DIALOG.RELOAD',
+          title: titleTranslateKey,
+          message: messageTranslateKey,
+        },
+        panelClass: 'atlas-dialog-panel',
+        backdropClass: 'atlas-dialog-backdrop',
+      })
+      .afterClosed()
+      .subscribe((result) => {
+        if (result) {
+          document.location.reload();
+        }
+      });
+  }
+}
