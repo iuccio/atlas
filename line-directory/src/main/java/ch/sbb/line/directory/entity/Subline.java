107a4a0b38c7c39c834b06c9211230fb3dc5427d
atlas <19825793+iuccio@users.noreply.github.com>
2022-06-15 13:31:18 +0200

    Revert "ATLAS-749: Drop views, use subselect"
    This reverts commit 868eeafbce6f84e6e2b4b94a030f78e835089f59.

--- line-directory/src/main/java/ch/sbb/line/directory/entity/Subline.java
+++ line-directory/src/main/java/ch/sbb/line/directory/entity/Subline.java
@@ -15,7 +15,6 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.FieldNameConstants;
 import org.hibernate.annotations.Immutable;
-import org.hibernate.annotations.Subselect;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -24,8 +23,7 @@ import org.hibernate.annotations.Subselect;
 @Builder
 @Immutable
 @FieldNameConstants
-@Entity
-@Subselect(Subline.VIEW_DEFINITION)
+@Entity(name = "subline")
 public class Subline {
 
   private String swissSublineNumber;
@@ -53,73 +51,4 @@ public class Subline {
   @Column(columnDefinition = "TIMESTAMP")
   private LocalDate validTo;
 
-  static final String VIEW_DEFINITION = "select *"
-      + "from ("
-      + "         select f.*, v.valid_from, v.valid_to"
-      + "         from ("
-      + "                  select swiss_subline_number,"
-      + "                         description,"
-      + "                         swiss_line_number,"
-      + "                         number,"
-      + "                         status,"
-      + "                         subline_type,"
-      + "                         business_organisation,"
-      + "                         slnid"
-      + "                  from ("
-      + "                           select distinct on (slnid) *"
-      + "                           from ((select distinct on (s.slnid) 1 as rank,"
-      + "                                                               s.swiss_subline_number,"
-      + "                                                               s.description,"
-      + "                                                               l.swiss_line_number,"
-      + "                                                               s.number,"
-      + "                                                               s.status,"
-      + "                                                               s.subline_type,"
-      + "                                                               s.business_organisation,"
-      + "                                                               s.slnid,"
-      + "                                                               s.valid_from,"
-      + "                                                               s.valid_to"
-      + "                                  from subline_version s"
-      + "                                           join (" + Line.VIEW_DEFINITION + ") l on s.mainline_slnid = l.slnid"
-      + "                                  where s.valid_from <= current_timestamp"
-      + "                                    and current_timestamp <= s.valid_to)"
-      + "                                 union all"
-      + "                                 (select distinct on (s.slnid) 2 as rank,"
-      + "                                                               s.swiss_subline_number,"
-      + "                                                               s.description,"
-      + "                                                               l.swiss_line_number,"
-      + "                                                               s.number,"
-      + "                                                               s.status,"
-      + "                                                               s.subline_type,"
-      + "                                                               s.business_organisation,"
-      + "                                                               s.slnid,"
-      + "                                                               s.valid_from,"
-      + "                                                               s.valid_to"
-      + "                                  from subline_version s"
-      + "                                           join (" + Line.VIEW_DEFINITION + ") l on s.mainline_slnid = l.slnid"
-      + "                                  where s.valid_from >= current_timestamp"
-      + "                                  order by s.slnid, s.valid_from)"
-      + "                                 union all"
-      + "                                 (select distinct on (s.slnid) 3 as rank,"
-      + "                                                               s.swiss_subline_number,"
-      + "                                                               s.description,"
-      + "                                                               l.swiss_line_number,"
-      + "                                                               s.number,"
-      + "                                                               s.status,"
-      + "                                                               s.subline_type,"
-      + "                                                               s.business_organisation,"
-      + "                                                               s.slnid,"
-      + "                                                               s.valid_from,"
-      + "                                                               s.valid_to"
-      + "                                  from subline_version s"
-      + "                                           join (" + Line.VIEW_DEFINITION + ") l on s.mainline_slnid = l.slnid"
-      + "                                  where s.valid_to <= current_timestamp"
-      + "                                  order by s.slnid, s.valid_to desc)) as ranked order by slnid, rank"
-      + "                       ) as chosen"
-      + "              ) f"
-      + "                  join ("
-      + "             select slnid, min(valid_from) as valid_from, max(valid_to) as valid_to"
-      + "             from subline_version"
-      + "             group by slnid"
-      + "         ) v on f.slnid = v.slnid"
-      + "     ) as sublines";
 }
