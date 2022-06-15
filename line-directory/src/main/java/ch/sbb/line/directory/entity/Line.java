543e4d6f25fa502328c9972c667833e18b25e03a
atlas <TechSupport-ATLAS@sbb.ch>
2022-06-15 13:31:18 +0200

    Revert "ATLAS-749: Drop views, use subselect"
    This reverts commit 1998197d6afd7dec26b7bb37f2a20c4c1a5c8795.

--- line-directory/src/main/java/ch/sbb/line/directory/entity/Line.java
+++ line-directory/src/main/java/ch/sbb/line/directory/entity/Line.java
@@ -15,7 +15,6 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.FieldNameConstants;
 import org.hibernate.annotations.Immutable;
-import org.hibernate.annotations.Subselect;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -24,8 +23,7 @@ import org.hibernate.annotations.Subselect;
 @Builder
 @Immutable
 @FieldNameConstants
-@Entity
-@Subselect(Line.VIEW_DEFINITION)
+@Entity(name = "line")
 public class Line {
 
   private String swissLineNumber;
@@ -51,39 +49,4 @@ public class Line {
   @Column(columnDefinition = "TIMESTAMP")
   private LocalDate validTo;
 
-  static final String VIEW_DEFINITION = "select *"
-      + "from ("
-      + "         select f.*, v.valid_from, v.valid_to"
-      + "         from ("
-      + "                  select swiss_line_number,"
-      + "                         number,"
-      + "                         description,"
-      + "                         status,"
-      + "                         line_type,"
-      + "                         business_organisation,"
-      + "                         slnid"
-      + "                  from ("
-      + "                           select distinct on (slnid) *"
-      + "                           from ((select distinct on (slnid) 1 as rank, *"
-      + "                                  from line_version"
-      + "                                  where valid_from <= current_timestamp"
-      + "                                    and current_timestamp <= valid_to)"
-      + "                                 union all"
-      + "                                 (select distinct on (slnid) 2 as rank, *"
-      + "                                  from line_version"
-      + "                                  where valid_from >= current_timestamp"
-      + "                                  order by slnid, valid_from)"
-      + "                                 union all"
-      + "                                 (select distinct on (slnid) 3 as rank, *"
-      + "                                  from line_version"
-      + "                                  where valid_to <= current_timestamp"
-      + "                                  order by slnid, valid_to desc)) as ranked order by slnid, rank"
-      + "                       ) as chosen"
-      + "              ) f"
-      + "                  join ("
-      + "             select slnid, min(valid_from) as valid_from, max(valid_to) as valid_to"
-      + "             from line_version"
-      + "             group by slnid"
-      + "         ) v on f.slnid = v.slnid"
-      + "     ) as lines";
 }
