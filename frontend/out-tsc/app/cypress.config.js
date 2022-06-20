0a99e9f442b8139546ff8a55675ba78d8582e9f7
atlas <19825793+iuccio@users.noreply.github.com>
2022-06-20 10:01:12 +0200

    unregister service-worker before each suite

---
+++ frontend/out-tsc/app/cypress.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'cypress';
+import cypress_failed_log from 'cypress-failed-log/src/failed';
+import cypress_high_resolution from 'cypress-high-resolution';
+export default defineConfig({
+    videosFolder: 'cypress/videos',
+    screenshotsFolder: 'cypress/screenshots',
+    fixturesFolder: 'cypress/fixtures',
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    videoCompression: false,
+    env: {
+        resolution: 'high',
+    },
+    e2e: {
+        // We've imported your old cypress plugins here.
+        // You may want to clean this up later by importing these.
+        setupNodeEvents(on, config) {
+            on('task', {
+                failed: cypress_failed_log(),
+            });
+            cypress_high_resolution(on, config);
+        },
+        baseUrl: 'http://localhost:4200',
+    },
+});
+//# sourceMappingURL=cypress.config.js.map
