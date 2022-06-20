b749a856a116d13449d6604c833a1d0a13e391f2
atlas <19825793+iuccio@users.noreply.github.com>
2022-06-20 16:57:25 +0200

    ATLAS-498: added bo-tu relation table migration

---
+++ business-organisation-directory/src/main/resources/db/migration/V0.0.2__bo_tc_reference.sql
@@ -0,0 +1,16 @@
+create table business_organisation_transport_company_link
+(
+    id integer unique not null primary key,
+    transport_company_id smallint not null,
+    sboid varchar(32) not null,
+    valid_from date not null,
+    valid_to date not null,
+    creator varchar(50) not null ,
+    creation_date timestamp not null,
+    editor varchar(50) not null,
+    edition_date timestamp not null,
+    foreign key (sboid) REFERENCES business_organisation_version (sboid),
+    foreign key (transport_company_id) references transport_company (id)
+);
+
+CREATE SEQUENCE business_organisation_transport_company_link_seq START WITH 1000 INCREMENT BY 1;
