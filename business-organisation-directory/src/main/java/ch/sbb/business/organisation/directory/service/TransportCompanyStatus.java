cbf9d38c730ab3f1e3d589f1ad04da1709e15eef
atlas <19825793+iuccio@users.noreply.github.com>
2022-06-21 17:08:10 +0200

    ATLAS-250: Update API and Tests

--- business-organisation-directory/src/main/java/ch/sbb/business/organisation/directory/service/TransportCompanyStatus.java
+++ business-organisation-directory/src/main/java/ch/sbb/business/organisation/directory/service/TransportCompanyStatus.java
@@ -1,6 +1,5 @@
 package ch.sbb.business.organisation.directory.service;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -11,22 +10,16 @@ import lombok.RequiredArgsConstructor;
 public enum TransportCompanyStatus {
 
   // Betreiber
-  @JsonProperty("1")
   OPERATOR,
   // Aktuell
-  @JsonProperty("2")
   CURRENT,
   // Aufsicht
-  @JsonProperty("3")
   SUPERVISION,
   // Betriebsteil
-  @JsonProperty("4")
   OPERATING_PART,
   // Liquidiert
-  @JsonProperty("5")
   LIQUIDATED,
   // Inaktiv
-  @JsonProperty("6")
   INACTIVE
 
 }
