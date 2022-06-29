0df198a3a4df9ee81b895356d20944ea5b0a32c9
atlas <TechSupport-ATLAS@sbb.ch>
2022-06-29 16:21:49 +0200

    ATLAS-773: add tests and docu

--- mail/src/main/java/ch/sbb/mail/model/MailNotification.java
+++ mail/src/main/java/ch/sbb/mail/model/MailNotification.java
@@ -7,7 +7,6 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.apache.commons.lang3.ArrayUtils;
 
 @Data
