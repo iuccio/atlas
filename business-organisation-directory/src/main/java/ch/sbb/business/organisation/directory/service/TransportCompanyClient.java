5c242ae7dc32900916554f5bf12b3f6fc9df0959
atlas <TechSupport-ATLAS@sbb.ch>
2022-06-29 11:20:55 +0200

    ATLAS-747: POC Crd import per soap

--- business-organisation-directory/src/main/java/ch/sbb/business/organisation/directory/service/TransportCompanyClient.java
+++ business-organisation-directory/src/main/java/ch/sbb/business/organisation/directory/service/TransportCompanyClient.java
@@ -4,7 +4,7 @@ import feign.Response;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "bavClient", url = "${external.url.bav}")
+@FeignClient(name = "bavClient", url = "${external.bav.url}")
 interface TransportCompanyClient {
   @GetMapping(value = "/Pages/Export/Export.aspx?identifier=g8DwS5k4", produces = "text/csv")
   Response getTransportCompanies();
