5381b6ec58f3f11f740e97e48201f19d1ddb9204
atlas <19825793+iuccio@users.noreply.github.com>
2022-06-29 16:21:49 +0200

    ATLAS-773: add tests and docu

--- mail/src/main/java/ch/sbb/mail/api/MailApiV1.java
+++ mail/src/main/java/ch/sbb/mail/api/MailApiV1.java
@@ -7,13 +7,29 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+/**
+ * @deprecated
+ * "This REST API will be deleted when ATLAS will integrate Kafka for the Service to Service cominication"
+ */
 @Tag(name = "Mails")
 @RequestMapping("v1/mail")
+@Deprecated(forRemoval = true)
 public interface MailApiV1 {
 
+  /**
+   * @deprecated
+   * "This REST API will be deleted when ATLAS will integrate Kafka for the Service to Service cominication"
+   */
+  @Deprecated(forRemoval = true)
   @PostMapping(value = "simple")
   ResponseEntity<?> sendEmail(@RequestBody MailNotification mailNotification);
 
+  /**
+   * @deprecated
+   * "This REST API will be deleted when ATLAS will integrate Kafka for the Service to Service cominication"
+   */
+  @Deprecated(forRemoval = true)
   @PostMapping(value = "html")
   ResponseEntity<?> sendEmailInHtml(@RequestBody MailNotification mailNotification);
 
