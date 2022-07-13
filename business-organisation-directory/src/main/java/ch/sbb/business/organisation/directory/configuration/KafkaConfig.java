d6342ebbab3de937c12d4660d930a6838d7e34ee
atlas <19825793+iuccio@users.noreply.github.com>
2022-07-13 15:03:45 +0200

    ATLAS-776: Service2Service Kommunikation lokal über Kafka handeln

---
+++ business-organisation-directory/src/main/java/ch/sbb/business/organisation/directory/configuration/KafkaConfig.java
@@ -0,0 +1,32 @@
+package ch.sbb.business.organisation.directory.configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+@Configuration
+@Slf4j
+@RequiredArgsConstructor
+public class KafkaConfig {
+
+  private final KafkaProperties kafkaProperties;
+
+  @Bean
+  public ProducerFactory<String, Object> producerFactory() {
+    Map<String, Object> props = new HashMap<>(kafkaProperties.buildProducerProperties());
+    return new DefaultKafkaProducerFactory<>(props);
+  }
+
+  @Bean
+  public KafkaTemplate<String, Object> kafkaTemplate() {
+    return new KafkaTemplate<>(producerFactory());
+  }
+
+}
