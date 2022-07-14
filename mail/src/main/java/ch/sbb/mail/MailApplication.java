2f9efda9289dadfa44c453bef8b8dee8dc6c3755
atlas <19825793+iuccio@users.noreply.github.com>
2022-07-14 15:40:55 +0200

    ATLAS-776: configure mail application with kafka on all stages

--- mail/src/main/java/ch/sbb/mail/MailApplication.java
+++ mail/src/main/java/ch/sbb/mail/MailApplication.java
@@ -1,13 +1,41 @@
 package ch.sbb.mail;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MailApplication {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+    setupTruststore();
     SpringApplication.run(MailApplication.class, args);
   }
 
+  private static void setupTruststore() throws IOException {
+    Path truststore = Files.createTempFile("truststore", ".p12");
+    Files.copy(Objects.requireNonNull(MailApplication.class.getClassLoader()
+                                                           .getResourceAsStream(
+                                                               "kafka/" + getTruststoreFileName()
+                                                                   + ".p12")), truststore,
+        StandardCopyOption.REPLACE_EXISTING);
+    System.setProperty("KAFKA_TRUSTSTORE_LOCATION", truststore.toUri().toString());
+  }
+
+  private static String getTruststoreFileName() {
+    String truststoreFileName = "truststore-test";
+
+    String profilesActive = System.getenv("SPRING_PROFILES_ACTIVE");
+    if ("int".equals(profilesActive)) {
+      truststoreFileName = "truststore-inte";
+    }
+    if ("prod".equals(profilesActive)) {
+      truststoreFileName = "truststore-prod";
+    }
+    return truststoreFileName;
+  }
 }
