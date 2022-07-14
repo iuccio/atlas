da5e9514be56ce71b5fead7c3de6a1dca627fed9
atlas <19825793+iuccio@users.noreply.github.com>
2022-07-14 16:34:45 +0200

    ATLAS-776: configure bo with kafka

--- business-organisation-directory/src/main/java/ch/sbb/business/organisation/directory/service/MailClient.java
+++ business-organisation-directory/src/main/java/ch/sbb/business/organisation/directory/service/MailClient.java
@@ -3,6 +3,7 @@ package ch.sbb.business.organisation.directory.service;
 import ch.sbb.atlas.model.mail.MailNotification;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
@@ -16,9 +17,13 @@ public class MailClient {
 
   private final KafkaTemplate<String, Object> kafkaTemplate;
 
+  @Value("${spring.kafka.atlas.mail.topic}")
+  private String mailTopic;
+
   public void produceMailNotification(MailNotification mailNotification) {
+
     ListenableFuture<SendResult<String, Object>> future =
-        kafkaTemplate.send("atlas.mail", "mail", mailNotification);
+        kafkaTemplate.send(mailTopic, "mail", mailNotification);
 
     future.addCallback(new ListenableFutureCallback<>() {
 
