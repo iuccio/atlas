aa6efbb56510105eabc2ea8f0b87b91661e895ad
atlas <TechSupport-ATLAS@sbb.ch>
2022-07-15 13:54:09 +0200

    ATLAS-776: Ref

--- business-organisation-directory/src/main/java/ch/sbb/business/organisation/directory/BusinessOrganisationDirectoryApplication.java
+++ business-organisation-directory/src/main/java/ch/sbb/business/organisation/directory/BusinessOrganisationDirectoryApplication.java
@@ -1,14 +1,10 @@
 package ch.sbb.business.organisation.directory;
 
 import ch.sbb.atlas.model.configuration.AtlasExceptionHandler;
+import ch.sbb.atlas.model.service.KafkaTruststorePreparation;
 import ch.sbb.atlas.versioning.service.VersionableService;
 import ch.sbb.atlas.versioning.service.VersionableServiceImpl;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 import java.time.ZoneId;
-import java.util.Objects;
 import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,36 +17,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class BusinessOrganisationDirectoryApplication {
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
     TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("Europe/Zurich")));
-    setupTruststore();
+    KafkaTruststorePreparation.setupTruststore(BusinessOrganisationDirectoryApplication.class.getClassLoader());
     SpringApplication.run(BusinessOrganisationDirectoryApplication.class, args);
-  }
-
-  private static void setupTruststore() throws IOException {
-    Path truststore = Files.createTempFile("truststore", ".p12");
-    Files.copy(
-        Objects.requireNonNull(BusinessOrganisationDirectoryApplication.class.getClassLoader()
-                                                                             .getResourceAsStream(
-                                                                                 "kafka/"
-                                                                                     + getTruststoreFileName()
-                                                                                     + ".p12")),
-        truststore,
-        StandardCopyOption.REPLACE_EXISTING);
-    System.setProperty("KAFKA_TRUSTSTORE_LOCATION", truststore.toUri().toString());
-  }
-
-  private static String getTruststoreFileName() {
-    String truststoreFileName = "truststore-test";
-
-    String profilesActive = System.getenv("SPRING_PROFILES_ACTIVE");
-    if ("int".equals(profilesActive)) {
-      truststoreFileName = "truststore-inte";
-    }
-    if ("prod".equals(profilesActive)) {
-      truststoreFileName = "truststore-prod";
-    }
-    return truststoreFileName;
   }
 
   @Bean
