bf056724cd3292d8b4118a1dec091d43f45ef516
atlas <TechSupport-ATLAS@sbb.ch>
2022-08-22 15:53:26 +0200

    ATLAS-753: pr review

--- user-administration/src/main/java/ch/sbb/atlas/user/administration/enums/UserAccountStatus.java
+++ user-administration/src/main/java/ch/sbb/atlas/user/administration/enums/UserAccountStatus.java
@@ -1,5 +1,15 @@
 package ch.sbb.atlas.user.administration.enums;
 
 public enum UserAccountStatus {
-  ACTIVE,INACTIVE,DELETED
+  ACTIVE,INACTIVE,DELETED;
+
+  public static UserAccountStatus getUserAccountStatusFromBoolean(Boolean accountStatus) {
+    if (accountStatus == null) {
+      return null;
+    }
+    if (accountStatus) {
+      return UserAccountStatus.ACTIVE;
+    }
+    return UserAccountStatus.INACTIVE;
+  }
 }
