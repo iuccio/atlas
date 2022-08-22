bf1243f4d91a29898dd569a8790d0048cc9f0365
atlas <19825793+iuccio@users.noreply.github.com>
2022-08-22 15:53:26 +0200

    ATLAS-753: pr review

--- user-administration/src/main/java/ch/sbb/atlas/user/administration/models/UserModel.java
+++ user-administration/src/main/java/ch/sbb/atlas/user/administration/models/UserModel.java
@@ -1,18 +1,20 @@
 package ch.sbb.atlas.user.administration.models;
 
 import ch.sbb.atlas.user.administration.enums.UserAccountStatus;
+import com.microsoft.graph.models.User;
+import lombok.Builder;
 
+@Builder
 public record UserModel(String sbbUserId, String lastName, String firstName, String mail,
                         UserAccountStatus accountStatus, String displayName) {
-
-  public static UserAccountStatus getUserAccountStatusFromBoolean(Boolean accountStatus) {
-    if (accountStatus == null){
-      return null;
-    }
-    if (accountStatus) {
-      return UserAccountStatus.ACTIVE;
-    }
-    return UserAccountStatus.INACTIVE;
+  public static UserModel toModel(User user) {
+    return UserModel.builder()
+                    .sbbUserId(user.onPremisesSamAccountName)
+                    .lastName(user.surname)
+                    .firstName(user.givenName)
+                    .mail(user.mail)
+                    .accountStatus(UserAccountStatus.getUserAccountStatusFromBoolean(user.accountEnabled))
+                    .displayName(user.displayName)
+                    .build();
   }
-
 }
