d87c8eaa295f6ae4a4363f22117861ddeccf0ae1
atlas <19825793+iuccio@users.noreply.github.com>
2022-08-30 09:03:10 +0200

    ATLAS-770: add scheduling service

---
+++ scheduling/src/main/java/ch/sbb/scheduling/SchedulingApplication.java
@@ -0,0 +1,17 @@
+package ch.sbb.scheduling;
+
+import java.time.ZoneId;
+import java.util.TimeZone;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class SchedulingApplication {
+
+  public static void main(String[] args) {
+    TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("Europe/Zurich")));
+    SpringApplication.run(SchedulingApplication.class, args);
+  }
+}
---
+++ scheduling/src/main/java/ch/sbb/scheduling/config/OAuthClientCredentialsFeignManager.java
@@ -0,0 +1,75 @@
+package ch.sbb.scheduling.config;
+
+import java.util.Collection;
+import java.util.Collections;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+
+@Slf4j
+@RequiredArgsConstructor
+public class OAuthClientCredentialsFeignManager {
+
+
+  private final OAuth2AuthorizedClientManager manager;
+  private final ClientRegistration clientRegistration;
+
+  public String getAccessToken() {
+    OAuth2AuthorizeRequest oAuth2AuthorizeRequest =
+        OAuth2AuthorizeRequest
+            .withClientRegistrationId(clientRegistration.getRegistrationId())
+            .principal(createPrincipal())
+            .build();
+    OAuth2AuthorizedClient client = manager.authorize(oAuth2AuthorizeRequest);
+    if (client == null) {
+      throw new IllegalStateException(
+          "client credentials flow on " + clientRegistration.getRegistrationId()
+              + " failed, client is null");
+    }
+    return client.getAccessToken().getTokenValue();
+  }
+
+  private Authentication createPrincipal() {
+    return new Authentication() {
+      @Override
+      public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptySet();
+      }
+
+      @Override
+      public Object getCredentials() {
+        return null;
+      }
+
+      @Override
+      public Object getDetails() {
+        return null;
+      }
+
+      @Override
+      public Object getPrincipal() {
+        return this;
+      }
+
+      @Override
+      public boolean isAuthenticated() {
+        return false;
+      }
+
+      @Override
+      public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+      }
+
+      @Override
+      public String getName() {
+        return clientRegistration.getClientId();
+      }
+    };
+  }
+
+}
