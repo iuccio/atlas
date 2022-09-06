ee895fb8b8efed98b0a156a407266d82710b5a9d
atlas <19825793+iuccio@users.noreply.github.com>
2022-09-06 11:56:06 +0200

    ATLAS-826: add subline scheduling

--- line-directory/src/test/java/ch/sbb/line/directory/service/export/LineVersionExportServiceTest.java
+++ line-directory/src/test/java/ch/sbb/line/directory/service/export/LineVersionExportServiceTest.java
@@ -1,15 +1,19 @@
 package ch.sbb.line.directory.service.export;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 import ch.sbb.atlas.amazon.service.AmazonService;
 import ch.sbb.atlas.amazon.service.FileService;
 import ch.sbb.atlas.model.exception.ExportException;
+import ch.sbb.line.directory.LineTestData;
+import ch.sbb.line.directory.entity.LineVersion;
 import ch.sbb.line.directory.repository.LineVersionRepository;
-import java.io.IOException;
+import java.io.File;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -26,7 +30,6 @@ class LineVersionExportServiceTest {
   @Mock
   private AmazonService amazonService;
 
-
   private LineVersionExportService lineVersionExportService;
 
   @BeforeEach
@@ -36,26 +39,76 @@ class LineVersionExportServiceTest {
         lineVersionRepository);
   }
 
-
   @Test
-  void shouldThrowExportExceptionWhenPutCsvFile() throws IOException {
+  public void shouldGetFullVersionsCsv() {
     //given
-    when(amazonService.putFile(any(), eq("line"))).thenThrow(IOException.class);
+    LineVersion lineVersion1 = LineTestData.lineVersionBuilder().build();
+    LineVersion lineVersion2 = LineTestData.lineVersionBuilder()
+                                           .validFrom(LocalDate.of(2022, 1, 1))
+                                           .validTo(LocalDate.of(2022, 12, 31))
+                                           .description("desc2")
+                                           .build();
+    List<LineVersion> lineVersions = List.of(lineVersion1, lineVersion2);
+    when(lineVersionRepository.getFullLineVersions()).thenReturn(lineVersions);
     //when
-
-    assertThatExceptionOfType(ExportException.class).isThrownBy(
-        () -> lineVersionExportService.putCsvFile(any()));
+    File result = lineVersionExportService.getFullVersionsCsv();
+    //then
+    assertThat(result).isNotNull();
+    assertThat(result.getName()).isNotNull();
+    assertThat(result.getName()).contains("full_");
+    result.delete();
   }
 
   @Test
-  void shouldThrowExportExceptionWhenPutZipFile() throws IOException {
+  public void shouldGetActualVersionsCsv() {
     //given
-    when(amazonService.putZipFile(any(), eq("line"))).thenThrow(IOException.class);
+    LineVersion lineVersion1 = LineTestData.lineVersionBuilder().build();
+    LineVersion lineVersion2 = LineTestData.lineVersionBuilder()
+                                           .validFrom(LocalDate.of(2022, 1, 1))
+                                           .validTo(LocalDate.of(2022, 12, 31))
+                                           .description("desc2")
+                                           .build();
+    List<LineVersion> lineVersions = List.of(lineVersion1, lineVersion2);
+    when(lineVersionRepository.getActualLineVersions(LocalDate.now())).thenReturn(lineVersions);
+    //when
+    File result = lineVersionExportService.getActualVersionsCsv();
+    //then
+    assertThat(result).isNotNull();
+    assertThat(result.getName()).isNotNull();
+    assertThat(result.getName()).contains("actual_date_");
+    result.delete();
+  }
+
+  @Test
+  public void shouldGetFutureTimetableVersionsCsv() {
+    //given
+    LineVersion lineVersion1 = LineTestData.lineVersionBuilder().build();
+    LineVersion lineVersion2 = LineTestData.lineVersionBuilder()
+                                           .validFrom(LocalDate.of(2022, 1, 1))
+                                           .validTo(LocalDate.of(2022, 12, 31))
+                                           .description("desc2")
+                                           .build();
+    List<LineVersion> lineVersions = List.of(lineVersion1, lineVersion2);
+    when(lineVersionRepository.getActualLineVersions(LocalDate.now())).thenReturn(lineVersions);
+    //when
+    File result = lineVersionExportService.getFutureTimetableVersionsCsv();
+    //then
+    assertThat(result).isNotNull();
+    assertThat(result.getName()).isNotNull();
+    assertThat(result.getName()).contains("future_timetable_");
+    result.delete();
+  }
+
+  @Test
+  void shouldThrowExportExceptionWhenDirDoesNotExists() {
+    //given
+    when(fileService.getDir()).thenReturn("./test/export");
+    when(lineVersionRepository.getActualLineVersions(LocalDate.now())).thenReturn(
+        Collections.emptyList());
     //when
 
     assertThatExceptionOfType(ExportException.class).isThrownBy(
-        () -> lineVersionExportService.putZipFile(any()));
+        () -> lineVersionExportService.getFutureTimetableVersionsCsv());
   }
-
 
 }
