ad4b792d0163bb344e403029e8ec27e888220844
atlas <TechSupport-ATLAS@sbb.ch>
2022-09-06 10:05:08 +0200

    ATLAS-827: need another rebase

---
+++ user-administration-security/src/main/java/ch/sbb/atlas/user/administration/security/UserPermissionHolder.java
@@ -0,0 +1,10 @@
+package ch.sbb.atlas.user.administration.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserPermissionHolder {
+
+  private Map<String, U>
+
+}
