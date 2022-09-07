81f562048d5cd86079dc5c7d3c7416246ef8f5ce
atlas <TechSupport-ATLAS@sbb.ch>
2022-09-07 14:06:13 +0200

    ATLAS-821: wip user create

--- frontend/src/app/pages/user-administration/user-administration.module.ts
+++ frontend/src/app/pages/user-administration/user-administration.module.ts
@@ -3,9 +3,19 @@ import { UserAdministrationOverviewComponent } from './overview/user-administrat
 import { UserAdministrationRoutingModule } from './user-administration-routing.module';
 import { CoreModule } from '../../core/module/core.module';
 import { FormModule } from '../../core/module/form.module';
+import { UserAdministrationDetailComponent } from './detail/user-administration-detail.component';
+import { UserAdministrationEditComponent } from './edit/user-administration-edit.component';
+import { UserAdministrationCreateComponent } from './create/user-administration-create.component';
+import { UserAdministrationApplicationConfigComponent } from './application-config/user-administration-application-config.component';
 
 @NgModule({
-  declarations: [UserAdministrationOverviewComponent],
+  declarations: [
+    UserAdministrationOverviewComponent,
+    UserAdministrationDetailComponent,
+    UserAdministrationEditComponent,
+    UserAdministrationCreateComponent,
+    UserAdministrationApplicationConfigComponent,
+  ],
   imports: [UserAdministrationRoutingModule, CoreModule, FormModule],
 })
 export class UserAdministrationModule {}
