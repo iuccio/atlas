2e181c5e425e975541e66c3865a831c8ed33595a
atlas <19825793+iuccio@users.noreply.github.com>
2022-09-07 14:06:13 +0200

    ATLAS-821: wip user create

---
+++ frontend/src/app/pages/user-administration/edit/user-administration-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit',
+  templateUrl: './user-administration-edit.component.html',
+  styleUrls: ['./user-administration-edit.component.scss'],
+})
+export class UserAdministrationEditComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
