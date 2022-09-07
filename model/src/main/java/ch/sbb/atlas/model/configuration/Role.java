bbecdf05cca97c3942dfae278391019c1845450d
atlas <19825793+iuccio@users.noreply.github.com>
2022-09-07 16:17:24 +0200

    ATLAS-827: Error handling for forbidden in API and frontend

---
+++ model/src/main/java/ch/sbb/atlas/model/configuration/Role.java
@@ -0,0 +1,10 @@
+package ch.sbb.atlas.model.configuration;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Role {
+
+  public static final String ATLAS_ADMIN =  "atlas-admin";
+
+}
