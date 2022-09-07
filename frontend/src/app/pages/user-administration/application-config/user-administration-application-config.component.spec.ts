bdcabb64cd891bb8855e1ebe99b1914f68f96f76
atlas <19825793+iuccio@users.noreply.github.com>
2022-09-07 14:06:13 +0200

    ATLAS-821: wip user create

---
+++ frontend/src/app/pages/user-administration/application-config/user-administration-application-config.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserAdministrationApplicationConfigComponent } from './user-administration-application-config.component';
+
+describe('ApplicationConfigComponent', () => {
+  let component: UserAdministrationApplicationConfigComponent;
+  let fixture: ComponentFixture<UserAdministrationApplicationConfigComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [UserAdministrationApplicationConfigComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(UserAdministrationApplicationConfigComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/pages/user-administration/edit/user-administration-edit.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserAdministrationEditComponent } from './user-administration-edit.component';
+
+describe('EditComponent', () => {
+  let component: UserAdministrationEditComponent;
+  let fixture: ComponentFixture<UserAdministrationEditComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [UserAdministrationEditComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(UserAdministrationEditComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
