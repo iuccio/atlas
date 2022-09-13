5cbcd3643b6a6be75ae32010e3a5110955e21a96
atlas <19825793+iuccio@users.noreply.github.com>
2022-09-13 20:05:43 +0200

    ATLAS-847: small changes

--- business-organisation-directory/src/main/java/ch/sbb/business/organisation/directory/configuration/AtlasWebMvcConfig.java
+++ business-organisation-directory/src/main/java/ch/sbb/business/organisation/directory/configuration/AtlasWebMvcConfig.java
@@ -1,12 +1,15 @@
 package ch.sbb.business.organisation.directory.configuration;
 
+import ch.sbb.atlas.base.service.model.configuration.CorrelationIdFilterConfig;
 import java.util.Collection;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Import(CorrelationIdFilterConfig.class)
 public class AtlasWebMvcConfig implements WebMvcConfigurer {
 
   @Override
@@ -17,6 +20,6 @@ public class AtlasWebMvcConfig implements WebMvcConfigurer {
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     registry.addResourceHandler("/static/**")
-            .addResourceLocations("classpath:/static/");
+        .addResourceLocations("classpath:/static/");
   }
 }
