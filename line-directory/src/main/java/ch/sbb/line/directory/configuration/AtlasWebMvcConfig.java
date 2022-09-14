edcfa1b46a11382064cea7edcb3238d9adc10ef2
atlas <19825793+iuccio@users.noreply.github.com>
2022-09-14 10:20:23 +0200

    ATLAS-847: changes after review

--- line-directory/src/main/java/ch/sbb/line/directory/configuration/AtlasWebMvcConfig.java
+++ line-directory/src/main/java/ch/sbb/line/directory/configuration/AtlasWebMvcConfig.java
@@ -1,15 +1,12 @@
 package ch.sbb.line.directory.configuration;
 
-import ch.sbb.atlas.base.service.model.configuration.CorrelationIdFilterConfig;
 import java.util.Collection;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@Import(CorrelationIdFilterConfig.class)
 public class AtlasWebMvcConfig implements WebMvcConfigurer {
 
   @Override
