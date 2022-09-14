b9370eabc0e6beafc813dedcbee89b61a0ad3e37
atlas <19825793+iuccio@users.noreply.github.com>
2022-09-14 13:53:19 +0200

    ATLAS-827: Reset MailListener

--- mail/src/main/java/ch/sbb/mail/service/MailListener.java
+++ mail/src/main/java/ch/sbb/mail/service/MailListener.java
@@ -6,23 +6,19 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.annotation.PartitionOffset;
-import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
-@KafkaListener(topics = "${spring.kafka.atlas.mail.topic}", groupId = "${spring.kafka.atlas.mail.groupId}",
-    topicPartitions = @TopicPartition(topic = "${spring.kafka.atlas.mail.topic}",
-        partitionOffsets = @PartitionOffset(partition = "0", initialOffset = "0")))
+@KafkaListener(topics = "${spring.kafka.atlas.mail.topic}", groupId = "${spring.kafka.atlas.mail.groupId}")
 public class MailListener {
 
   private final MailService mailService;
 
   @KafkaHandler
   public void receiveMailSendingRequest(@Valid MailNotification mailNotification) {
-    log.info("Receiving all mails, current={}", mailNotification);
+    mailService.sendEmailWithHtmlTemplate(mailNotification);
   }
 
 }
