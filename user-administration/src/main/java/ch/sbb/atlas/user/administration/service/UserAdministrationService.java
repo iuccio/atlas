1cfdbb0d1644d0bf0384a1a089fce3c90be1ad6e
atlas <TechSupport-ATLAS@sbb.ch>
2022-09-15 12:47:29 +0200

    ATLAS-821: backend finished with tests

--- user-administration/src/main/java/ch/sbb/atlas/user/administration/service/UserAdministrationService.java
+++ user-administration/src/main/java/ch/sbb/atlas/user/administration/service/UserAdministrationService.java
@@ -2,13 +2,13 @@ package ch.sbb.atlas.user.administration.service;
 
 import ch.sbb.atlas.user.administration.api.UserPermissionCreateModel;
 import ch.sbb.atlas.user.administration.entity.UserPermission;
+import ch.sbb.atlas.user.administration.exception.UserPermissionConflictException;
 import ch.sbb.atlas.user.administration.enumeration.ApplicationRole;
 import ch.sbb.atlas.user.administration.enumeration.ApplicationType;
 import ch.sbb.atlas.user.administration.enumeration.UserAccountStatus;
 import ch.sbb.atlas.user.administration.models.UserModel;
 import ch.sbb.atlas.user.administration.models.UserPermissionModel;
 import ch.sbb.atlas.user.administration.repository.UserPermissionRepository;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -27,8 +27,6 @@ public class UserAdministrationService {
 
   private final UserPermissionRepository userPermissionRepository;
 
-  private final GraphApiService graphApiService;
-
   public Page<String> getUserPage(Pageable pageable) {
     return userPermissionRepository.findAllDistinctSbbUserId(pageable);
   }
@@ -37,41 +35,11 @@ public class UserAdministrationService {
     return userPermissionRepository.findBySbbUserId(sbbUserId);
   }
 
-  public void validateUserPermissionCreation(UserPermissionCreateModel user) {
+  public void validatePermissionExistence(UserPermissionCreateModel user) {
     boolean exists = userPermissionRepository.existsBySbbUserIdIgnoreCase(user.getSbbUserId());
     if (exists) {
-      throw new ValidationException("User already exists");
+      throw new UserPermissionConflictException(user.getSbbUserId());
     }
-
-    List<UserModel> resolvedUsers = graphApiService.resolveUsers(List.of(user.getSbbUserId()));
-    if (resolvedUsers.get(0).getAccountStatus() == UserAccountStatus.DELETED) {
-      throw new ValidationException("User does not exist");
-    }
-
-    // application unique
-    Set<ApplicationType> applicationTypesInPermissions = new HashSet<>();
-    user.getPermissions()
-        .forEach(permission -> applicationTypesInPermissions.add(permission.getApplication()));
-    if (applicationTypesInPermissions.size() != user.getPermissions().size()) {
-      throw new ValidationException("Duplicate application types");
-    }
-
-    // only sboids when writer role
-    user.getPermissions().forEach(permission -> {
-      if (permission.getRole() != ApplicationRole.WRITER && permission.getSboids().size() > 0) {
-        throw new ValidationException("No sboids permitted when not writer role");
-      }
-    });
-
-    user.getPermissions()
-        .forEach(permission -> {
-          Set<String> sboidsInPermission = new HashSet<>(permission.getSboids());
-          if (sboidsInPermission.size() != permission.getSboids().size()) {
-            throw new ValidationException("Duplicate sboids in permission");
-          }
-        });
-
-    // TODO: maybe sboids exists
   }
 
   public List<UserPermission> save(List<UserPermission> userPermissions) {
