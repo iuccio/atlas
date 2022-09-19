d18dc9d349e1c0a32035fc3478ee4426f4ac7e86
atlas <TechSupport-ATLAS@sbb.ch>
2022-09-19 15:28:25 +0200

    ATLAS-821: review - backend

--- user-administration/src/main/java/ch/sbb/atlas/user/administration/api/UserAdministrationApiV1.java
+++ user-administration/src/main/java/ch/sbb/atlas/user/administration/api/UserAdministrationApiV1.java
@@ -1,6 +1,7 @@
 package ch.sbb.atlas.user.administration.api;
 
 import ch.sbb.atlas.base.service.model.api.Container;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import javax.validation.Valid;
@@ -19,21 +20,25 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @RequestMapping("/v1/users")
 public interface UserAdministrationApiV1 {
 
-  @GetMapping
-  @PageableAsQueryParam
-  Container<UserModel> getUsers(@Parameter(hidden = true) Pageable pageable);
+    @GetMapping
+    @PageableAsQueryParam
+    @Operation(description = "Retrieve Overview for all the managed Users")
+    Container<UserModel> getUsers(@Parameter(hidden = true) Pageable pageable);
 
-  @GetMapping("{userId}")
-  UserModel getUser(@PathVariable String userId);
+    @GetMapping("{userId}")
+    @Operation(description = "Retrieve User Information for a given user")
+    UserModel getUser(@PathVariable String userId);
 
   @GetMapping("current")
   UserModel getCurrentUser();
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(description = "Register a user with given permissions")
     UserModel createUserPermission(@RequestBody @Valid UserPermissionCreateModel user);
 
     @PutMapping
+    @Operation(description = "Update the user permissions of a user")
     UserModel updateUserPermissions(@RequestBody @Valid UserPermissionCreateModel editedPermissions);
 
 }
