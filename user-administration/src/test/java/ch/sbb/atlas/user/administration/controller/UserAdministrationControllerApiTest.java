fa873fd370f46f7a4e8b014b177e22f8d767eb66
atlas <TechSupport-ATLAS@sbb.ch>
2022-09-20 13:02:14 +0200

    ATLAS-733: Update readme

--- user-administration/src/test/java/ch/sbb/atlas/user/administration/controller/UserAdministrationControllerApiTest.java
+++ user-administration/src/test/java/ch/sbb/atlas/user/administration/controller/UserAdministrationControllerApiTest.java
@@ -13,7 +13,6 @@ import ch.sbb.atlas.user.administration.repository.UserPermissionRepository;
 import ch.sbb.atlas.user.administration.service.UserPermissionDistributor;
 import java.util.List;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
