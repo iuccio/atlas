d3d853271ac99f79232ab0d8170bb41f01f1a0f7
atlas <TechSupport-ATLAS@sbb.ch>
2022-09-20 16:03:09 +0200

    ATLAS-821: frontend test fixes

--- frontend/src/app/pages/user-administration/edit/user-administration-edit.component.spec.ts
+++ frontend/src/app/pages/user-administration/edit/user-administration-edit.component.spec.ts
@@ -1,10 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserAdministrationEditComponent } from './user-administration-edit.component';
-import { AppTestingModule } from '../../../app.testing.module';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { TranslatePipe } from '@ngx-translate/core';
+import {
+  TranslateFakeLoader,
+  TranslateLoader,
+  TranslateModule,
+  TranslatePipe,
+} from '@ngx-translate/core';
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../../core/module/material.module';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-close',
@@ -16,21 +23,29 @@ describe('UserAdministrationEditComponent', () => {
   let component: UserAdministrationEditComponent;
   let fixture: ComponentFixture<UserAdministrationEditComponent>;
 
+  const dialogMock = {
+    close: () => {
+      // Mock implementation
+    },
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [UserAdministrationEditComponent, MockDialogCloseComponent],
-      imports: [AppTestingModule],
-      providers: [
-        TranslatePipe,
-        {
-          provide: MAT_DIALOG_DATA,
-          useValue: { user: undefined },
-        },
+      imports: [
+        TranslateModule.forRoot({
+          loader: { provide: TranslateLoader, useClass: TranslateFakeLoader },
+        }),
+        MaterialModule,
+        RouterTestingModule,
+        HttpClientTestingModule,
       ],
+      providers: [TranslatePipe, { provide: MatDialogRef, useValue: dialogMock }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(UserAdministrationEditComponent);
     component = fixture.componentInstance;
+    component.user = {};
     fixture.detectChanges();
   });
 
