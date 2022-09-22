ad66667b9f0098a5ebefc4b8b7ee3eb57178881d
atlas <TechSupport-ATLAS@sbb.ch>
2022-09-22 11:41:29 +0200

    wip: benutzerverwaltung rework

---
+++ frontend/src/app/pages/user-administration/user-select/user-select-format.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { UserSelectFormatPipe } from './user-select-format.pipe';
+
+describe('UserSelectFormatPipe', () => {
+  it('create an instance', () => {
+    const pipe = new UserSelectFormatPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
