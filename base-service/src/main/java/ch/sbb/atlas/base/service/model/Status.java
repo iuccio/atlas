9a5e748d55b24020dabbb397cfd98e009557b53f
atlas <19825793+iuccio@users.noreply.github.com>
2022-10-07 09:35:33 +0200

    ATLAS-825: Add docu link

--- base-service/src/main/java/ch/sbb/atlas/base/service/model/Status.java
+++ base-service/src/main/java/ch/sbb/atlas/base/service/model/Status.java
@@ -2,6 +2,9 @@ package ch.sbb.atlas.base.service.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
+/**
+ * See https://confluence.sbb.ch/display/ATLAS/Status
+ */
 @Schema(enumAsRef = true)
 public enum Status {
 
