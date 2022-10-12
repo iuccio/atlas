414ebcb025178f09a93a8ca6360179b6416717c4
atlas <TechSupport-ATLAS@sbb.ch>
2022-10-12 13:44:11 +0200

    ATLAS-925: Atlas button - the beginning

---
+++ frontend/src/app/core/components/button/atlas-button.type.ts
@@ -0,0 +1,8 @@
+export const AtlasButtonType = {
+  CREATE: 'create' as AtlasButtonType,
+  CREATE_CHECKING_PERMISSION: 'createCheckingPermission' as AtlasButtonType,
+  FOOTER: 'footer' as AtlasButtonType,
+  EDIT: 'edit' as AtlasButtonType,
+};
+
+export type AtlasButtonType = 'createCheckingPermission' | 'create' | 'footer' | 'edit';
