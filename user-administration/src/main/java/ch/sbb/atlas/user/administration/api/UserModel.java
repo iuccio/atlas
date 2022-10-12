963e57f76db80d1709ffc09ee74161461ed1c1ce
atlas <19825793+iuccio@users.noreply.github.com>
2022-10-12 11:03:07 +0200

    ATLAS-820: pr review

--- user-administration/src/main/java/ch/sbb/atlas/user/administration/api/UserModel.java
+++ user-administration/src/main/java/ch/sbb/atlas/user/administration/api/UserModel.java
@@ -19,57 +19,57 @@ import lombok.experimental.FieldNameConstants;
 @Schema(name = "User")
 public class UserModel {
 
-    @Schema(description = "SBB User Id", example = "u111111")
-    private String sbbUserId;
+  @Schema(description = "SBB User Id", example = "u111111")
+  private String sbbUserId;
 
-    @Schema(description = "User lastname")
-    private String lastName;
+  @Schema(description = "User lastname", example = "Mustermann")
+  private String lastName;
 
-    @Schema(description = "User firstname")
-    private String firstName;
+  @Schema(description = "User firstname", example = "Max")
+  private String firstName;
 
-    @Schema(description = "User E-Mail address", example = "example@sbb.ch")
-    private String mail;
+  @Schema(description = "User E-Mail address", example = "example@sbb.ch")
+  private String mail;
 
-    @Schema(description = "User display name (azure)", example = "Example User (IT-PTR-CEN2-YPT)")
-    private String displayName;
+  @Schema(description = "User display name (azure)", example = "Example User (IT-PTR-CEN2-YPT)")
+  private String displayName;
 
-    @Schema(description = "User account status", example = "ACTIVE")
-    private UserAccountStatus accountStatus;
+  @Schema(description = "User account status", example = "ACTIVE")
+  private UserAccountStatus accountStatus;
 
-    @Schema(description = "User permissions")
-    private Set<UserPermissionModel> permissions;
+  @Schema(description = "User permissions")
+  private Set<UserPermissionModel> permissions;
 
-    public UserAdministrationModel toKafkaModel() {
-        Set<UserAdministrationPermissionModel> permissionModels = getPermissions().stream()
-                .map(
-                        permission -> UserAdministrationPermissionModel.builder()
-                                .application(
-                                        permission.getApplication())
-                                .role(
-                                        permission.getRole())
-                                .sboids(
-                                        new HashSet<>(permission.getSboids()))
-                                .build())
-                .collect(
-                        Collectors.toSet());
-        return UserAdministrationModel.builder()
-                .sbbUserId(getSbbUserId())
-                .permissions(permissionModels)
-                .build();
-    }
+  public UserAdministrationModel toKafkaModel() {
+    Set<UserAdministrationPermissionModel> permissionModels = getPermissions().stream()
+        .map(
+            permission -> UserAdministrationPermissionModel.builder()
+                .application(
+                    permission.getApplication())
+                .role(
+                    permission.getRole())
+                .sboids(
+                    new HashSet<>(permission.getSboids()))
+                .build())
+        .collect(
+            Collectors.toSet());
+    return UserAdministrationModel.builder()
+        .sbbUserId(getSbbUserId())
+        .permissions(permissionModels)
+        .build();
+  }
 
-    public static UserModel userToModel(User user) {
-        return UserModel.builder()
-                .sbbUserId(user.onPremisesSamAccountName)
-                .lastName(user.surname)
-                .firstName(user.givenName)
-                .mail(user.mail)
-                .accountStatus(
-                        UserAccountStatus.getUserAccountStatusFromBoolean(user.accountEnabled))
-                .displayName(user.displayName)
-                .permissions(Collections.emptySet())
-                .build();
-    }
+  public static UserModel userToModel(User user) {
+    return UserModel.builder()
+        .sbbUserId(user.onPremisesSamAccountName)
+        .lastName(user.surname)
+        .firstName(user.givenName)
+        .mail(user.mail)
+        .accountStatus(
+            UserAccountStatus.getUserAccountStatusFromBoolean(user.accountEnabled))
+        .displayName(user.displayName)
+        .permissions(Collections.emptySet())
+        .build();
+  }
 
 }
