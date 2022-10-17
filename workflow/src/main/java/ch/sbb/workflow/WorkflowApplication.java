3fd68a33a118be5914612cfd2828a325b3e7e73a
atlas <19825793+iuccio@users.noreply.github.com>
2022-10-17 11:37:37 +0200

    ATLAS-823: init Workflow Service

---
+++ workflow/src/main/java/ch/sbb/workflow/WorkflowApplication.java
@@ -0,0 +1,17 @@
+package ch.sbb.workflow;
+
+import ch.sbb.atlas.kafka.KafkaTruststorePreparation;
+import java.time.ZoneId;
+import java.util.TimeZone;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowApplication {
+
+  public static void main(String[] args) {
+    TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("Europe/Zurich")));
+    KafkaTruststorePreparation.setupTruststore();
+    SpringApplication.run(WorkflowApplication.class, args);
+  }
+}
---
+++ workflow/src/main/resources/db/migration/V0.0.1__init-wf.sql
@@ -0,0 +1,40 @@
+CREATE TABLE person
+(
+    id            BIGINT    NOT NULL PRIMARY KEY,
+    first_name    VARCHAR(50),
+    last_name     VARCHAR(50),
+    funtion       VARCHAR(50),
+    mail          VARCHAR(255),
+    creation_date TIMESTAMP NOT NULL,
+    edition_date  TIMESTAMP NOT NULL
+
+);
+
+CREATE SEQUENCE person_seq START WITH 1000 INCREMENT BY 1;
+
+
+CREATE TABLE workflow
+(
+    id                      BIGINT    NOT NULL PRIMARY KEY,
+    business_object_type_id VARCHAR(50),
+    business_object_id      VARCHAR(50),
+    workflow_type           VARCHAR(50),
+    swiss_id                VARCHAR(50),
+    description             VARCHAR(50),
+    status                  VARCHAR(50),
+    workflow_comment        VARCHAR(50),
+    check_comment           VARCHAR(50),
+    client_id               BIGINT,
+    examinant_id            BIGINT,
+    creation_date           TIMESTAMP NOT NULL,
+    edition_date            TIMESTAMP NOT NULL,
+    CONSTRAINT fk_client
+        FOREIGN KEY (client_id)
+            REFERENCES person (id),
+    CONSTRAINT fk_examinant
+        FOREIGN KEY (examinant_id)
+            REFERENCES person (id)
+);
+
+CREATE SEQUENCE workflow_seq START WITH 1000 INCREMENT BY 1;
+
