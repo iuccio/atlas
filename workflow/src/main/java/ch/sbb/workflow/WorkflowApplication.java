714a7e8f03a69b7fb9dc6a63341ca0b8b365b400
atlas <19825793+iuccio@users.noreply.github.com>
2022-10-17 16:47:36 +0200

    ATLAS-834: add rest-doc tests

--- workflow/src/main/java/ch/sbb/workflow/WorkflowApplication.java
+++ workflow/src/main/java/ch/sbb/workflow/WorkflowApplication.java
@@ -1,10 +1,12 @@
 package ch.sbb.workflow;
 
+import ch.sbb.atlas.base.service.model.configuration.AtlasExceptionHandler;
 import ch.sbb.atlas.kafka.KafkaTruststorePreparation;
 import java.time.ZoneId;
 import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WorkflowApplication {
@@ -14,4 +16,10 @@ public class WorkflowApplication {
     KafkaTruststorePreparation.setupTruststore();
     SpringApplication.run(WorkflowApplication.class, args);
   }
+
+  @Bean
+  public AtlasExceptionHandler atlasExceptionHandler() {
+    return new AtlasExceptionHandler();
+  }
+
 }
--- workflow/src/main/java/ch/sbb/workflow/api/WorkflowApiV1.java
+++ workflow/src/main/java/ch/sbb/workflow/api/WorkflowApiV1.java
@@ -1,13 +1,17 @@
 package ch.sbb.workflow.api;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import javax.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Tag(name = "Lines")
 @RequestMapping("v1/workflows")
@@ -20,5 +24,8 @@ public interface WorkflowApiV1 {
   List<WorkflowModel> getWorkflows();
 
   @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "201")})
   WorkflowModel createWorkflow(@RequestBody @Valid WorkflowModel newWorkflow);
 }
