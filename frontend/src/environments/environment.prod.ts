fb496355f65670344d1aff0a3f4d94dd19ecb63b
atlas <TechSupport-ATLAS@sbb.ch>
2022-10-24 20:59:51 +0200

    ATLAS-834: configure workflow deploy for dev,test,int, exclude prod

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -33,6 +33,7 @@ const authConfig: AuthConfig = {
 
 export const environment: Environment = {
   production: process.env.PRODUCTION !== 'false',
+  workflowEnabled: process.env.WORKFLOW_ENABLED !== 'false',
   label: process.env.ENVIRONMENT_LABEL!,
   appVersion: process.env.APP_VERSION!,
   atlasApiUrl: process.env.ATLAS_API_URL!,
