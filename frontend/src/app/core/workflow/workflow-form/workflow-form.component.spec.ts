4cb293bffaf986ca890f0690a155587700d8e263
atlas <19825793+iuccio@users.noreply.github.com>
2022-11-01 16:38:14 +0100

    ATLAS-881: fix tests

--- frontend/src/app/core/workflow/workflow-form/workflow-form.component.spec.ts
+++ frontend/src/app/core/workflow/workflow-form/workflow-form.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WorkflowFormComponent } from './workflow-form.component';
+import { AppTestingModule } from '../../../app.testing.module';
+import { FormControl, FormGroup } from '@angular/forms';
+import { WorkflowFormGroup } from '../workflow-form-group';
 
 describe('WorkflowFormComponent', () => {
   let component: WorkflowFormComponent;
@@ -9,10 +12,17 @@ describe('WorkflowFormComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [WorkflowFormComponent],
+      imports: [AppTestingModule],
     }).compileComponents();
-
     fixture = TestBed.createComponent(WorkflowFormComponent);
     component = fixture.componentInstance;
+    component.formGroup = new FormGroup<WorkflowFormGroup>({
+      comment: new FormControl(''),
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+      function: new FormControl(''),
+      mail: new FormControl(''),
+    });
     fixture.detectChanges();
   });
 
--- frontend/src/app/core/workflow/workflow.component.spec.ts
+++ frontend/src/app/core/workflow/workflow.component.spec.ts
@@ -1,6 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WorkflowComponent } from './workflow.component';
+import { AppTestingModule } from '../../app.testing.module';
+import { AuthService } from '../auth/auth.service';
+import { Workflow } from '../../api';
+import WorkflowTypeEnum = Workflow.WorkflowTypeEnum;
+
+const authServiceMock: Partial<AuthService> = {
+  claims: {
+    name: 'Test (ITC)',
+    email: 'test@test.ch',
+    sbbuid: 'e123456',
+    roles: ['lidi-admin', 'lidi-writer'],
+  },
+};
 
 describe('WorkflowComponent', () => {
   let component: WorkflowComponent;
@@ -8,10 +21,25 @@ describe('WorkflowComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [AppTestingModule],
       declarations: [WorkflowComponent],
+      providers: [{ provide: AuthService, useValue: authServiceMock }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(WorkflowComponent);
+    fixture.componentInstance.workflow = {
+      businessObjectId: 123,
+      swissId: 'ch:slnid:0001',
+      workflowType: WorkflowTypeEnum.Line,
+      workflowComment: 'comment',
+      description: 'description',
+      client: {
+        firstName: 'Marek',
+        lastName: 'Hamsik',
+        mail: '@b.ch',
+        personFunction: 'function',
+      },
+    };
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
