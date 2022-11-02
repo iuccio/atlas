e90ccd89b018b3e9c6c08ce375042abc26a6c742
atlas <TechSupport-ATLAS@sbb.ch>
2022-11-02 15:45:15 +0100

    ATLAS-881: code clean up

--- frontend/src/app/core/workflow/line-record.ts
+++
@@ -1,6 +0,0 @@
-import { Record } from '../components/base-detail/record';
-import { LineVersionWorkflow } from '../../api';
-
-export interface LineRecord extends Record {
-  lineVersionWorkflows?: Set<LineVersionWorkflow>;
-}
