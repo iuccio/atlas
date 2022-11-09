c6c89a61cb45e4d80f4531cc0c100c807a62619a
atlas <TechSupport-ATLAS@sbb.ch>
2022-11-09 09:47:39 +0100

    ATLAS-927: Update auf fxatlsy und fxatlka

---
+++ business-organisation-directory/src/main/resources/db/migration/V0.0.6__creator_editor_update.sql
@@ -0,0 +1,5 @@
+update business_organisation_version set editor = 'fxatlsy' where lower(editor) in ('u236171', 'didok');
+update transport_company_relation set editor = 'fxatlsy' where lower(editor) in ('u236171', 'didok');
+
+update business_organisation_version set creator = 'fxatlsy' where lower(creator) in ('u236171', 'didok');
+update transport_company_relation set creator = 'fxatlsy' where lower(creator) in ('u236171', 'didok');
