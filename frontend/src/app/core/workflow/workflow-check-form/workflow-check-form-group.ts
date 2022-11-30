7ce1c3ae92c286c67a117e9ca95d31d1f5d9cbf1
atlas <TechSupport-ATLAS@sbb.ch>
2022-11-30 15:17:02 +0100

    ATLAS-836: Update to dynamic form, make mail optional

--- frontend/src/app/core/workflow/workflow-check-form/workflow-check-form-group.ts
+++
@@ -1,8 +0,0 @@
-import { FormControl } from '@angular/forms';
-
-export interface WorkflowCheckFormGroup {
-  comment: FormControl<string | null | undefined>;
-  firstName: FormControl<string | null | undefined>;
-  lastName: FormControl<string | null | undefined>;
-  function: FormControl<string | null | undefined>;
-}
--- frontend/src/app/core/workflow/workflow-check-form/workflow-check-form.component.ts
+++ frontend/src/app/core/workflow/workflow-check-form/workflow-check-form.component.ts
@@ -5,11 +5,11 @@ import { AtlasCharsetsValidator } from '../../validation/charsets/atlas-charsets
 import { Subject } from 'rxjs';
 import { UserAdministrationService, WorkflowService } from '../../../api';
 import { takeUntil } from 'rxjs/operators';
-import { WorkflowCheckFormGroup } from './workflow-check-form-group';
 import { Router } from '@angular/router';
 import { NotificationService } from '../../notification/notification.service';
 import { AuthService } from '../../auth/auth.service';
 import { ValidationService } from '../../validation/validation.service';
+import { WorkflowFormGroup } from '../workflow-form-group';
 
 @Component({
   selector: 'app-workflow-check-form',
@@ -18,7 +18,7 @@ import { ValidationService } from '../../validation/validation.service';
 export class WorkflowCheckFormComponent implements OnInit {
   @Input() workflowId: number | undefined;
 
-  formGroup: FormGroup<WorkflowCheckFormGroup> = new FormGroup<WorkflowCheckFormGroup>({
+  formGroup: FormGroup<WorkflowFormGroup> = new FormGroup<WorkflowFormGroup>({
     comment: new FormControl('', [
       AtlasFieldLengthValidator.comments,
       AtlasCharsetsValidator.iso88591,
