09087c55b93e82c35af35ddb721579ade330de96
atlas <19825793+iuccio@users.noreply.github.com>
2022-12-02 09:15:59 +0100

    Revert "ATLAS-836: Update to dynamic form, make mail optional"
    This reverts commit a234678e0dd1d71b6157701c591715c8cf03d1bf.

--- frontend/src/app/core/workflow/workflow-check-form/workflow-check-form-group.ts
+++ frontend/src/app/core/workflow/workflow-check-form/workflow-check-form-group.ts
@@ -1,9 +1,8 @@
 import { FormControl } from '@angular/forms';
 
-export interface WorkflowFormGroup {
+export interface WorkflowCheckFormGroup {
   comment: FormControl<string | null | undefined>;
   firstName: FormControl<string | null | undefined>;
   lastName: FormControl<string | null | undefined>;
   function: FormControl<string | null | undefined>;
-  mail: FormControl<string | null | undefined>;
 }
