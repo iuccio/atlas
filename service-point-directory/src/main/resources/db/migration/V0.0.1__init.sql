06a6b312b98f5474d81293f82104d600bad1b9d8
atlas <19825793+iuccio@users.noreply.github.com>
2022-12-02 14:20:07 +0100

    ATLAS-851 - db schema script

---
+++ service-point-directory/src/main/resources/db/migration/V0.0.1__init.sql
@@ -0,0 +1,59 @@
+-----------------------------------------------------------------------------------------
+-- Service Point
+-----------------------------------------------------------------------------------------
+
+CREATE TABLE service_point_version
+(
+    id                   BIGINT      NOT NULL PRIMARY KEY,
+    number_short         INTEGER     NOT NULL,
+    uic_country_code     INTEGER     NOT NULL,
+    designation_long     VARCHAR(50) NULL,
+    designation_official VARCHAR(30) NOT NULL,
+    abbreviation         VARCHAR(6) NULL,
+    statusDiDok3         INTEGER     NOT NULL,
+    number               INTEGER     NOT NULL,
+    check_digit          INTEGER     NOT NULL,
+    said                 VARCHAR(38) NULL,
+    has_geolocation      BOOLEAN NULL DEFAULT FALSE,
+    valid_from           DATE        NOT NULL,
+    valid_to             DATE        NOT NULL,
+    creation_date        TIMESTAMP   NOT NULL,
+    creator              VARCHAR(50) NOT NULL,
+    edition_date         TIMESTAMP   NOT NULL,
+    editor               VARCHAR(50) NOT NULL,
+    version              BIGINT      NOT NULL DEFAULT 0
+);
+
+CREATE SEQUENCE service_point_version_seq START WITH 1000 INCREMENT BY 1;
+
+CREATE TABLE service_point_geolocation_version
+(
+    id                      BIGINT      NOT NULL PRIMARY KEY,
+    service_point_version   BIGINT      NOT NULL,
+    source_spatial_ref      INTEGER     NOT NULL,
+    e_lv03                  INTEGER NULL,
+    n_lv03                  INTEGER NULL,
+    e_lv95                  INTEGER NULL,
+    n_lv95                  INTEGER NULL,
+    e_wgs84                 INTEGER NULL,
+    n_wgs84                 INTEGER NULL,
+    height                  INTEGER NULL,
+    country_code            VARCHAR(2) NULL,
+    swiss_municipality_name VARCHAR(100) NULL,
+    swiss_canton_fso_number INTEGER NULL,
+    swiss_canton_name       VARCHAR(100) NULL,
+    swiss_canton_number     INTEGER NULL,
+    swiss_district_name     VARCHAR(100) NULL,
+    swiss_district_number   INTEGER NULL,
+    swiss_locality_name     VARCHAR(255) NULL,
+    creation_date           TIMESTAMP   NOT NULL,
+    creator                 VARCHAR(50) NOT NULL,
+    edition_date            TIMESTAMP   NOT NULL,
+    editor                  VARCHAR(50) NOT NULL,
+    CONSTRAINT fk_service_point_version
+        FOREIGN KEY (service_point_version)
+            REFERENCES service_point_version (version)
+);
+
+CREATE SEQUENCE service_point_geolocation_version_seq START WITH 1000 INCREMENT BY 1;
+
