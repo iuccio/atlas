50d55c566bf9f46fb394e5260899f025233a21c2
atlas <19825793+iuccio@users.noreply.github.com>
2022-12-02 09:34:23 +0100

    ATLAS-849 - ServicePointDirectory http requests

---
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/config/KafkaConfig.java
@@ -0,0 +1,35 @@
+package ch.sbb.atlas.servicepointdirectory.config;
+
+import ch.sbb.atlas.kafka.AtlasKafkaConfiguration;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+@Configuration
+@ComponentScan
+@Import(AtlasKafkaConfiguration.class)
+@RequiredArgsConstructor
+public class KafkaConfig {
+
+  private final KafkaProperties kafkaProperties;
+
+  @Bean
+  public ProducerFactory<String, Object> producerFactory() {
+    Map<String, Object> props = new HashMap<>(kafkaProperties.buildProducerProperties());
+    return new DefaultKafkaProducerFactory<>(props);
+  }
+
+  @Bean
+  public KafkaTemplate<String, Object> kafkaTemplate() {
+    return new KafkaTemplate<>(producerFactory());
+  }
+
+}
