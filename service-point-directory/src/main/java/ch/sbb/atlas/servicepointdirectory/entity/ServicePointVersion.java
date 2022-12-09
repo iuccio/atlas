6d278a50dff64e422c625abc689ae83d0725d443
atlas <19825793+iuccio@users.noreply.github.com>
2022-12-09 18:45:02 +0100

    ATLAS-851: Implement first entities

---
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/entity/ServicePointVersion.java
@@ -0,0 +1,89 @@
+package ch.sbb.atlas.servicepointdirectory.entity;
+
+import ch.sbb.atlas.base.service.model.api.AtlasFieldLengths;
+import ch.sbb.atlas.base.service.model.entity.BaseVersion;
+import ch.sbb.atlas.base.service.versioning.annotation.AtlasVersionable;
+import ch.sbb.atlas.base.service.versioning.annotation.AtlasVersionableProperty;
+import ch.sbb.atlas.base.service.versioning.model.Versionable;
+import ch.sbb.atlas.user.administration.security.BusinessOrganisationAssociated;
+import lombok.*;
+import lombok.experimental.FieldNameConstants;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@FieldNameConstants
+@Entity(name = "service_point_version")
+@AtlasVersionable
+public class ServicePointVersion extends BaseVersion implements Versionable,
+    BusinessOrganisationAssociated {
+
+  private static final String VERSION_SEQ = "service_point_version_seq";
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = VERSION_SEQ)
+  @SequenceGenerator(name = VERSION_SEQ, sequenceName = VERSION_SEQ, allocationSize = 1, initialValue = 1000)
+  private Long id;
+
+  @NotNull
+  @AtlasVersionableProperty
+  private Integer number;
+
+  @NotNull
+  @AtlasVersionableProperty
+  private Integer checkDigit;
+
+  @NotNull
+  @AtlasVersionableProperty
+  private Integer numberShort;
+
+  @OneToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "uic_country_code", referencedColumnName = "uic_920_14")
+  private UicCountry uicCountry;
+
+  @NotBlank
+  @Size(max = AtlasFieldLengths.LENGTH_50)
+  @AtlasVersionableProperty
+  private String designationLong;
+
+  @NotBlank
+  @Size(max = AtlasFieldLengths.LENGTH_30)
+  @AtlasVersionableProperty
+  private String designationOfficial;
+
+  @NotBlank
+  @Size(max = AtlasFieldLengths.LENGTH_6)
+  @AtlasVersionableProperty
+  private String abbreviation;
+
+  @NotNull
+  @AtlasVersionableProperty
+  private Integer statusDidok3;
+
+  @AtlasVersionableProperty
+  private boolean hasGeolocation;
+
+  @NotNull
+  @Column(columnDefinition = "DATE")
+  private LocalDate validFrom;
+
+  @NotNull
+  @Column(columnDefinition = "DATE")
+  private LocalDate validTo;
+
+  @NotBlank
+  @Size(max = AtlasFieldLengths.LENGTH_50)
+  @AtlasVersionableProperty
+  private String businessOrganisation;
+
+}
