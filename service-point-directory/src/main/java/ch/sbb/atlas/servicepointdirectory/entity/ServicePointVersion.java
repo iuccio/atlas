777cd3e4ab976ef9f09bc57ab551fefdb789d83c
atlas <TechSupport-ATLAS@sbb.ch>
2022-12-12 17:17:07 +0100

    ATLAS-851: make use of them enums

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/entity/ServicePointVersion.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/entity/ServicePointVersion.java
@@ -5,6 +5,9 @@ import ch.sbb.atlas.base.service.model.entity.BaseVersion;
 import ch.sbb.atlas.base.service.versioning.annotation.AtlasVersionable;
 import ch.sbb.atlas.base.service.versioning.annotation.AtlasVersionableProperty;
 import ch.sbb.atlas.base.service.versioning.model.Versionable;
+import ch.sbb.atlas.servicepointdirectory.converter.CategoryConverter;
+import ch.sbb.atlas.servicepointdirectory.enumeration.Category;
+import ch.sbb.atlas.servicepointdirectory.enumeration.OperatingPointType;
 import ch.sbb.atlas.user.administration.security.BusinessOrganisationAssociated;
 import lombok.*;
 import lombok.experimental.FieldNameConstants;
@@ -28,75 +31,86 @@ import java.util.Set;
 @Entity(name = "service_point_version")
 @AtlasVersionable
 public class ServicePointVersion extends BaseVersion implements Versionable,
-    BusinessOrganisationAssociated {
+        BusinessOrganisationAssociated {
 
-  private static final String VERSION_SEQ = "service_point_version_seq";
+    private static final String VERSION_SEQ = "service_point_version_seq";
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = VERSION_SEQ)
-  @SequenceGenerator(name = VERSION_SEQ, sequenceName = VERSION_SEQ, allocationSize = 1, initialValue = 1000)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = VERSION_SEQ)
+    @SequenceGenerator(name = VERSION_SEQ, sequenceName = VERSION_SEQ, allocationSize = 1, initialValue = 1000)
+    private Long id;
 
-  @NotNull
-  @AtlasVersionableProperty
-  private Integer number;
+    @NotNull
+    @AtlasVersionableProperty
+    private Integer number;
 
-  @NotNull
-  @AtlasVersionableProperty
-  private Integer checkDigit;
+    @NotNull
+    @AtlasVersionableProperty
+    private Integer checkDigit;
 
-  @NotNull
-  @AtlasVersionableProperty
-  private Integer numberShort;
+    @NotNull
+    @AtlasVersionableProperty
+    private Integer numberShort;
 
-  @NotNull
-  @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-  @JoinColumn(name = "uic_country_code", referencedColumnName = "uic_920_14")
-  private UicCountry uicCountry;
+    @NotNull
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "uic_country_code", referencedColumnName = "uic_920_14")
+    private UicCountry uicCountry;
 
-  @NotBlank
-  @Size(max = AtlasFieldLengths.LENGTH_50)
-  @AtlasVersionableProperty
-  private String designationLong;
+    @NotBlank
+    @Size(max = AtlasFieldLengths.LENGTH_50)
+    @AtlasVersionableProperty
+    private String designationLong;
 
-  @NotBlank
-  @Size(max = AtlasFieldLengths.LENGTH_30)
-  @AtlasVersionableProperty
-  private String designationOfficial;
+    @NotBlank
+    @Size(max = AtlasFieldLengths.LENGTH_30)
+    @AtlasVersionableProperty
+    private String designationOfficial;
 
-  @NotBlank
-  @Size(max = AtlasFieldLengths.LENGTH_6)
-  @AtlasVersionableProperty
-  private String abbreviation;
+    @NotBlank
+    @Size(max = AtlasFieldLengths.LENGTH_6)
+    @AtlasVersionableProperty
+    private String abbreviation;
 
-  @NotNull
-  @AtlasVersionableProperty
-  private Integer statusDidok3;
+    @NotNull
+    @AtlasVersionableProperty
+    private Integer statusDidok3;
 
-  @AtlasVersionableProperty
-  private boolean hasGeolocation;
+    @AtlasVersionableProperty
+    private boolean hasGeolocation;
 
-  @NotNull
-  @Column(columnDefinition = "DATE")
-  private LocalDate validFrom;
+    @NotNull
+    @Column(columnDefinition = "DATE")
+    private LocalDate validFrom;
 
-  @NotNull
-  @Column(columnDefinition = "DATE")
-  private LocalDate validTo;
+    @NotNull
+    @Column(columnDefinition = "DATE")
+    private LocalDate validTo;
 
-  @NotBlank
-  @Size(max = AtlasFieldLengths.LENGTH_50)
-  @AtlasVersionableProperty
-  private String businessOrganisation;
+    @NotBlank
+    @Size(max = AtlasFieldLengths.LENGTH_50)
+    @AtlasVersionableProperty
+    private String businessOrganisation;
 
-  @OneToOne(cascade = CascadeType.ALL, mappedBy = "servicePointVersion")
-  private ServicePointGeolocation servicePointGeolocation;
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "servicePointVersion")
+    private ServicePointGeolocation servicePointGeolocation;
 
-  @Builder.Default
-  @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-  @JoinTable(
-          name = "service_point_category",
-          joinColumns = @JoinColumn(name = "service_point_version_id"),
-          inverseJoinColumns = @JoinColumn(name = "category_id"))
-  private Set<Category> categories = new HashSet<>();
+    @AtlasVersionableProperty
+    @ElementCollection(targetClass = Category.class, fetch = FetchType.EAGER)
+    @Convert(converter = CategoryConverter.class)
+    private Set<Category> categories;
+
+    @Enumerated(EnumType.STRING)
+    private OperatingPointType operatingPointType;
+
+    public boolean isOperatingPoint() {
+        return operatingPointType != null;
+    }
+
+    public Set<Category> getCategories() {
+        if (categories == null) {
+            return new HashSet<>();
+        }
+        return categories;
+    }
 }
