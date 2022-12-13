29063c079ca782acd8a9954dcb1fd10904be6e68
atlas <TechSupport-ATLAS@sbb.ch>
2022-12-13 17:07:28 +0100

    ATLAS-851: implement stop place

---
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/enumeration/MeansOfTransport.java
@@ -0,0 +1,34 @@
+package ch.sbb.atlas.servicepointdirectory.enumeration;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Schema(enumAsRef = true)
+@Getter
+@RequiredArgsConstructor
+public enum MeansOfTransport {
+
+    TRAIN(0, "Z", "Zug", "Zug", "Train", "Train", "Treno"),
+    COACH(1, "B", "Bus", "Bus", "Bus", "Coach", "Bus"),
+    TRAM(2, "T", "Tram", "Tram", "Tram", "Tram", "Tram"),
+    BOAT(3, "S", "Schiff", "Schiff", "Bateau", "Boat", "Nave"),
+    CABLE_CAR(4, "L", "Kabinenbahn", "Kabinenbahn", "Télécabine", "Cable Car", "Cabinovia"),
+    CHAIRLIFT(5, "E", "Sesselbahn", "Sesselbahn", "Télésiège", "Chairlift", "Seggiovia"),
+    CABLE_RAILWAY(6, "N", "Standseilbahn", "Standseilbahn", "Funiculaire", "Cable Railway", "Funicolare"),
+    RACK_RAILWAY(7, "H", "Zahnradbahn", "Zahnradbahn", "Chemin de fer à crémaillère", "Rack Railway", "Ferrovia a cremagliera"),
+    METRO(8, "M", "Metro", "Metro", "Métro", "Metro", "Metro"),
+    ELEVATOR(9, "A", "Aufzug", "Aufzug", "Ascenseur", "Elevator", "Ascensore"),
+    UNKNOWN(10, "U", "Unbekannt", "Unbekannt", "Inconnu", "Unknown", "Sconosciute"),
+
+    ;
+
+    private final Integer rank;
+    private final String code;
+    private final String name;
+    private final String nameDe;
+    private final String nameFr;
+    private final String nameEn;
+    private final String nameIt;
+
+}
