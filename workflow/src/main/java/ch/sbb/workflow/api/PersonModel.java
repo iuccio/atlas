3a97f2e41b74dcf4f632dfb9859a3439c2e1fe46
atlas <TechSupport-ATLAS@sbb.ch>
2022-12-13 10:30:31 +0100

    ATLAS-1038: Fix various Workflow Issues

--- workflow/src/main/java/ch/sbb/workflow/api/PersonModel.java
+++ workflow/src/main/java/ch/sbb/workflow/api/PersonModel.java
@@ -25,19 +25,16 @@ public class PersonModel {
   @Schema(description = "Firstname", example = "John")
   @Pattern(regexp = AtlasCharacterSetsRegex.ISO_8859_1)
   @Size(min = 1, max = AtlasFieldLengths.LENGTH_50)
-  @NotBlank
   private String firstName;
 
   @Schema(description = "Second", example = "Doe")
   @Size(min = 1, max = AtlasFieldLengths.LENGTH_50)
   @Pattern(regexp = AtlasCharacterSetsRegex.ISO_8859_1)
-  @NotBlank
   private String lastName;
 
   @Schema(description = "Person Function", example = "Officer")
   @Pattern(regexp = AtlasCharacterSetsRegex.ISO_8859_1)
   @Size(min = 1, max = AtlasFieldLengths.LENGTH_50)
-  @NotBlank
   private String personFunction;
 
   @Schema(description = "Object creation date", example = "01.01.2000", accessMode = AccessMode.READ_ONLY)
