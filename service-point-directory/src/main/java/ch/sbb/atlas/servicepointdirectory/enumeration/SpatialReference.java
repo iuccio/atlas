9d50bc0e693794a6df2d20d5ddba398bc6e69c1c
atlas <19825793+iuccio@users.noreply.github.com>
2022-12-16 13:12:53 +0100

    ATLAS-851: Spatial WKID nummer

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/enumeration/SpatialReference.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/enumeration/SpatialReference.java
@@ -1,14 +1,18 @@
 package ch.sbb.atlas.servicepointdirectory.enumeration;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @Schema(enumAsRef = true)
 public enum SpatialReference {
 
-  WGS84WEB,
-  LV95,
-  LV03,
-  WGS84,
+  WGS84WEB(3857),
+  LV95(2056),
+  LV03(21781),
+  WGS84(4326),
 
   ;
+
+  private final Integer wkid;
 }
