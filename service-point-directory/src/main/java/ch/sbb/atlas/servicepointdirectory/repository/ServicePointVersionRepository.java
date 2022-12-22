c6882f3f04c33f3ea5d2e72017e38943a797b50d
atlas <19825793+iuccio@users.noreply.github.com>
2022-12-22 14:39:30 +0100

    ATLAs-851 - fix import of service points

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/repository/ServicePointVersionRepository.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/repository/ServicePointVersionRepository.java
@@ -1,12 +1,15 @@
 package ch.sbb.atlas.servicepointdirectory.repository;
 
 import ch.sbb.atlas.servicepointdirectory.entity.ServicePointVersion;
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ServicePointVersionRepository extends
     PagingAndSortingRepository<ServicePointVersion, Long> {
+
+  List<ServicePointVersion> findAllByHasGeolocation(Boolean hasGeolocation, Pageable pageable);
 
 }
