a49b5ac5d892e05f6ee72e7309a4fa5668f8a6bb
atlas <19825793+iuccio@users.noreply.github.com>
2022-12-22 20:57:44 +0100

    ATLAs-851 - performance tests

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/repository/ServicePointVersionRepository.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/repository/ServicePointVersionRepository.java
@@ -2,12 +2,13 @@ package ch.sbb.atlas.servicepointdirectory.repository;
 
 import ch.sbb.atlas.servicepointdirectory.entity.ServicePointVersion;
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ServicePointVersionRepository extends
-    PagingAndSortingRepository<ServicePointVersion, Long> {
+    JpaRepository<ServicePointVersion, Long> {
 
   List<ServicePointVersion> findAllByNumber(Integer number);
 
