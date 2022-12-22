8c7a1b3d6a71b0b9df3a31ab172068015d301e44
atlas <TechSupport-ATLAS@sbb.ch>
2022-12-22 21:00:19 +0100

    ATLAs-851 - performance tests

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/repository/LoadingPointRepository.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/repository/LoadingPointRepository.java
@@ -2,11 +2,10 @@ package ch.sbb.atlas.servicepointdirectory.repository;
 
 import ch.sbb.atlas.servicepointdirectory.entity.LoadingPointVersion;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LoadingPointRepository extends
-    PagingAndSortingRepository<LoadingPointVersion, Long> {
+    JpaRepository<LoadingPointVersion, Long> {
 
 }
