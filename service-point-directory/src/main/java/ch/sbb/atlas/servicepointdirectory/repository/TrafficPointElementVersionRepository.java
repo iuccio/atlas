f1aa2bd7c2d5026da0fe68833c3e624b79975551
atlas <19825793+iuccio@users.noreply.github.com>
2022-12-22 21:00:19 +0100

    ATLAs-851 - performance tests

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/repository/TrafficPointElementVersionRepository.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/repository/TrafficPointElementVersionRepository.java
@@ -2,12 +2,12 @@ package ch.sbb.atlas.servicepointdirectory.repository;
 
 import ch.sbb.atlas.servicepointdirectory.entity.TrafficPointElementVersion;
 import java.util.List;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TrafficPointElementVersionRepository extends
-    PagingAndSortingRepository<TrafficPointElementVersion, Long> {
+    JpaRepository<TrafficPointElementVersion, Long> {
 
   List<TrafficPointElementVersion> findAllByServicePointNumber(Integer servicePointNumber);
 
