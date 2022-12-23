42a9b006e8ad8e022cb07c8e6b2d6684a3f5aab7
atlas <TechSupport-ATLAS@sbb.ch>
2022-12-23 14:12:26 +0100

    ATLAS-851 - service point numbers

---
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/repository/ServicePointNumberRepository.java
@@ -0,0 +1,11 @@
+package ch.sbb.atlas.servicepointdirectory.repository;
+
+import ch.sbb.atlas.servicepointdirectory.entity.ServicePointNumber;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServicePointNumberRepository extends
+    JpaRepository<ServicePointNumber, Long> {
+
+}
