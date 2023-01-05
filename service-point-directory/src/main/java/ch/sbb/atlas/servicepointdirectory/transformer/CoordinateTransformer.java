c466c468da41d57b3c1b9d5434dd54371788114b
atlas <19825793+iuccio@users.noreply.github.com>
2023-01-05 14:57:13 +0100

    ATLAS: coordinate transformer cleanup

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/transformer/CoordinateTransformer.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/transformer/CoordinateTransformer.java
@@ -4,6 +4,7 @@ import ch.sbb.atlas.servicepointdirectory.enumeration.SpatialReference;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Stream;
+import lombok.NonNull;
 import org.locationtech.proj4j.CRSFactory;
 import org.locationtech.proj4j.CoordinateReferenceSystem;
 import org.locationtech.proj4j.CoordinateTransform;
@@ -17,36 +18,47 @@ public class CoordinateTransformer {
   private final CoordinateTransformFactory ctFactory = new CoordinateTransformFactory();
   private final Map<SpatialReference, CoordinateReferenceSystem> referenceSystemMap =
       new HashMap<>();
+  private final Map<String, CoordinateTransform> coordinateTransformers = new HashMap<>();
 
   public CoordinateTransformer() {
     final CRSFactory crsFactory = new CRSFactory();
-    Stream.of(SpatialReference.values()).forEach(sr -> {
-      referenceSystemMap.put(sr, crsFactory.createFromName("epsg:" + sr.getWellKnownId()));
-    });
+    Stream.of(SpatialReference.values())
+          .forEach(sr -> referenceSystemMap
+              .put(sr, crsFactory.createFromName("epsg:" + sr.getWellKnownId())));
   }
 
-  public CoordinatePair transform(CoordinatePair coordinatePair,
-      SpatialReference targetSpatialReference) {
-    if (coordinatePair == null) {
-      throw new IllegalArgumentException("coordinatePair");
-    }
-    final SpatialReference sourceSpatialReference = coordinatePair.getSpatialReference();
+  /**
+   * Transform source coordinate into given target spatial reference.
+   *
+   * @param sourceCoordinate       Source coordinate.
+   * @param targetSpatialReference Target spatial reference system.
+   * @return New transformed coordinate.
+   */
+  public CoordinatePair transform(
+      @NonNull CoordinatePair sourceCoordinate,
+      @NonNull SpatialReference targetSpatialReference) {
+    final SpatialReference sourceSpatialReference = sourceCoordinate.getSpatialReference();
     if (sourceSpatialReference == null) {
-      throw new IllegalArgumentException("coordinatePair.sourceSpatialReference");
+      throw new NullPointerException("sourceCoordinate.sourceSpatialReference");
     }
-    if (targetSpatialReference == null) {
-      throw new IllegalArgumentException("targetSpatialReference");
-    }
-
-    final CoordinateTransform cTransform = ctFactory.createTransform(
-        referenceSystemMap.get(sourceSpatialReference),
-        referenceSystemMap.get(targetSpatialReference));
     final ProjCoordinate result = new ProjCoordinate();
-    cTransform.transform(new ProjCoordinate(coordinatePair.getEast(), coordinatePair.getNorth()),
-        result);
+    findTransformer(
+        referenceSystemMap.get(sourceSpatialReference),
+        referenceSystemMap.get(targetSpatialReference))
+        .transform(
+            new ProjCoordinate(sourceCoordinate.getEast(), sourceCoordinate.getNorth()),
+            result);
 
     return CoordinatePair.builder().north(result.y).east(result.x).build();
 
+  }
+
+  private CoordinateTransform findTransformer(
+      CoordinateReferenceSystem source,
+      CoordinateReferenceSystem target) {
+    final String mappingName = source.getName() + target.getName();
+    return coordinateTransformers.computeIfAbsent(mappingName,
+        t -> ctFactory.createTransform(source, target));
   }
 }
 
