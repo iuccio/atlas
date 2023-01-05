02e176eadfd129deef70ee58c163500961830e54
atlas <19825793+iuccio@users.noreply.github.com>
2023-01-05 12:38:29 +0100

    ATLAS: coordinate transformer

---
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/transformer/SpatialReferenceTransformer.java
@@ -0,0 +1,48 @@
+package ch.sbb.atlas.servicepointdirectory.transformer;
+
+import ch.sbb.atlas.servicepointdirectory.enumeration.SpatialReference;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+import org.locationtech.proj4j.CRSFactory;
+import org.locationtech.proj4j.CoordinateReferenceSystem;
+import org.locationtech.proj4j.CoordinateTransform;
+import org.locationtech.proj4j.CoordinateTransformFactory;
+import org.locationtech.proj4j.ProjCoordinate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpatialReferenceTransformer {
+
+  private final CoordinateTransformFactory ctFactory = new CoordinateTransformFactory();
+  private final Map<SpatialReference, CoordinateReferenceSystem> referenceSystemMap =
+      new HashMap<>();
+
+  public SpatialReferenceTransformer() {
+    final CRSFactory crsFactory = new CRSFactory();
+    Stream.of(SpatialReference.values()).forEach(sr -> {
+      referenceSystemMap.put(sr, crsFactory.createFromName("epsg:" + sr.getWellKnownId()));
+    });
+  }
+
+  public CoordinatePair transform(SpatialReference sourceSpatialReference,
+      SpatialReference targetSpatialReference, CoordinatePair coordinatePair) {
+
+    final CoordinateTransform cTransform = ctFactory.createTransform(
+        referenceSystemMap.get(sourceSpatialReference),
+        referenceSystemMap.get(targetSpatialReference));
+    final ProjCoordinate result = new ProjCoordinate();
+    cTransform.transform(new ProjCoordinate(coordinatePair.getEast(), coordinatePair.getNorth()),
+        result);
+
+    return CoordinatePair.builder().north(result.y).east(result.x).build();
+
+  }
+
+  public CoordinatePair transform(SpatialReference sourceSpatialReference,
+      SpatialReference targetSpatialReference, double east, double north) {
+    return transform(sourceSpatialReference, targetSpatialReference,
+        CoordinatePair.builder().east(east).north(north).build());
+  }
+}
+
