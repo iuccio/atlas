9bd0e95e7eba734977fffeeb86c96ef5807dc365
atlas <TechSupport-ATLAS@sbb.ch>
2023-01-11 16:39:54 +0100

    ATLAS-914: import: CsvService with tests

---
+++ import-service-point/src/main/java/ch/sbb/importservice/service/CsvService.java
@@ -0,0 +1,174 @@
+package ch.sbb.importservice.service;
+
+import ch.sbb.atlas.base.service.amazon.service.AmazonService;
+import ch.sbb.atlas.base.service.imports.DidokCsvMapper;
+import ch.sbb.atlas.base.service.imports.ServicePointCsvModel;
+import com.fasterxml.jackson.databind.MappingIterator;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class CsvService {
+
+  private static final String SERVICEPOINT_DIDOK_DIR_NAME = "servicepoint_didok";
+  private static final String CSV_DELIMITER = ";";
+
+  private final AmazonService amazonService;
+
+  @Setter
+  private LocalDate matchingDate = LocalDate.now();
+
+  //@PostConstruct
+  void test() throws IOException {
+    File importFile = downloadImportFileFromToday("DIDOK3_DIENSTSTELLEN_ALL_V_3_");
+    List<ServicePointCsvModel> servicePointCsvModels = getCsvModelsToUpdate(importFile, ServicePointCsvModel.class);
+    log.info("servicePointCsvModels size: {}", servicePointCsvModels.size());
+    //"DIDOK3_LADESTELLEN_",
+    //"DIDOK3_VERKEHRSPUNKTELEMENTE_ALL_V_1_"
+  }
+
+  // line by line comparison
+  /*public void loadCsvFilesForImport() throws IOException {
+    for (String csvImportFilePrefix : csvImportFilePrefixes) {
+      String csvImportFilePrefixToday = attachTodayDate(csvImportFilePrefix);
+      File importFileToday = downloadImportFileWithPrefix(csvImportFilePrefixToday);
+      String csvImportFilePrefixYesterday = attachYesterdayDate(csvImportFilePrefix);
+      File importFileYesterday = downloadImportFileWithPrefix(csvImportFilePrefixYesterday);
+      long mismatch = Files.mismatch(importFileToday.toPath(), importFileYesterday.toPath());
+      log.info("Mismatch: " + mismatch);
+      if (mismatch != -1) {
+        try (BufferedReader bufferedReaderFileToday = new BufferedReader(new FileReader(importFileToday));
+            BufferedReader bufferedReaderFileYesterday = new BufferedReader(new FileReader(importFileYesterday))) {
+          List<Integer> mismatchedLineNumbers = new ArrayList<>();
+          String lineFileToday, lineFileYesterday;
+          int lineNumber = 1;
+          while ((lineFileYesterday = bufferedReaderFileYesterday.readLine()) != null) {
+            lineFileToday = bufferedReaderFileToday.readLine();
+            if (lineFileToday == null || !lineFileToday.equals(lineFileYesterday)) {
+              mismatchedLineNumbers.add(lineNumber);
+            }
+            lineNumber++;
+          }
+          log.info("Line numbers: " + mismatchedLineNumbers);
+        }
+      }
+    }
+  }*/
+
+  public File downloadImportFileFromToday(String csvImportFilePrefix) throws IOException {
+    String csvImportFilePrefixToday = attachTodayDate(csvImportFilePrefix);
+    return downloadImportFileWithPrefix(csvImportFilePrefixToday);
+  }
+
+  public <T> List<T> getCsvModelsToUpdate(File importFile, Class<T> type) throws IOException {
+    try (BufferedReader bufferedReader = new BufferedReader(new FileReader(importFile))) {
+      String headerLine = skipUntilHeaderLine(bufferedReader);
+      int editedAtColumnIndex = getColumnIndexOfEditedAt(headerLine);
+      List<String> mismatchedLines = getMismatchedLines(bufferedReader, editedAtColumnIndex);
+      log.info("Mismatched lines count: " + mismatchedLines.size());
+      // parse mismatched lines
+      if (mismatchedLines.isEmpty()) {
+        return Collections.emptyList();
+      }
+      List<String> csvLinesToProcess = new ArrayList<>();
+      csvLinesToProcess.add(headerLine);
+      csvLinesToProcess.addAll(mismatchedLines);
+      MappingIterator<T> mappingIterator = DidokCsvMapper.CSV_MAPPER.readerFor(type)
+          .with(DidokCsvMapper.CSV_SCHEMA)
+          .readValues(String.join("\n", csvLinesToProcess));
+      return mapObjects(mappingIterator);
+    }
+  }
+
+  private String skipUntilHeaderLine(BufferedReader bufferedReader) throws IOException {
+    final String HASHTAG = "#";
+    String line;
+    while ((line = bufferedReader.readLine()) != null) {
+      if (!line.contains(HASHTAG)) {
+        return line;
+      }
+    }
+    throw new RuntimeException("Not found header line in csv");
+  }
+
+  private int getColumnIndexOfEditedAt(String headerLine) {
+    final String EDITED_AT_COLUMN_NAME = "GEAENDERT_AM";
+    String[] attributes = headerLine.split(CSV_DELIMITER);
+    int editedAtColumnIndex = Arrays.asList(attributes).indexOf(EDITED_AT_COLUMN_NAME);
+    if (editedAtColumnIndex == -1) {
+      throw new RuntimeException("Not found %s index".formatted(EDITED_AT_COLUMN_NAME));
+    }
+    return editedAtColumnIndex;
+  }
+
+  private List<String> getMismatchedLines(BufferedReader bufferedReader, int editedAtColumnIndex) throws IOException {
+    List<String> mismatchedLines = new ArrayList<>();
+    String line;
+    while ((line = bufferedReader.readLine()) != null) {
+      String[] splittedLine = line.split(CSV_DELIMITER);
+      LocalDate lastEditionDate = LocalDateTime.parse(splittedLine[editedAtColumnIndex],
+          DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss") // TODO: mby error handling
+      ).toLocalDate();
+      if (lastEditionDate.isEqual(matchingDate)) {
+        mismatchedLines.add(line);
+      }
+    }
+    return mismatchedLines;
+  }
+
+  private <T> List<T> mapObjects(MappingIterator<T> mappingIterator) {
+    List<T> mappedObjects = new ArrayList<>();
+    while (mappingIterator.hasNext()) {
+      mappedObjects.add(mappingIterator.next());
+    }
+    return mappedObjects;
+  }
+
+  private File downloadImportFileWithPrefix(String csvImportFilePrefix) throws IOException {
+    List<String> foundImportFileKeys = amazonService.getS3ObjectKeysFromPrefix(SERVICEPOINT_DIDOK_DIR_NAME, csvImportFilePrefix);
+    String fileKeyToDownload = handleImportFileKeysResult(foundImportFileKeys);
+    File download = amazonService.pullFile(fileKeyToDownload);
+    log.info("Name: " + download.getName() + ", size: " + download.length() + " bytes");
+    return download;
+  }
+
+  private String handleImportFileKeysResult(List<String> importFileKeys) {
+    if (importFileKeys.isEmpty()) {
+      throw new RuntimeException("[IMPORT]: Not found file on S3");
+    } else if (importFileKeys.size() > 1) {
+      throw new RuntimeException("[IMPORT]: Found more than 1 file to download on S3");
+    }
+    return importFileKeys.get(0);
+  }
+
+  private String attachTodayDate(String csvImportFilePrefix) {
+    LocalDate today = LocalDate.now();
+    return csvImportFilePrefix + replaceHyphensWithUnderscores(today.toString());
+  }
+
+  private String attachYesterdayDate(String csvImportFilePrefix) {
+    LocalDate yesterday = LocalDate.now().minusDays(1);
+    return csvImportFilePrefix + replaceHyphensWithUnderscores(yesterday.toString());
+  }
+
+  private String replaceHyphensWithUnderscores(String input) {
+    return input.replaceAll("-", "");
+  }
+
+}
---
+++ import-service-point/src/test/java/ch/sbb/importservice/service/CsvServiceTest.java
@@ -0,0 +1,88 @@
+package ch.sbb.importservice.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.openMocks;
+
+import ch.sbb.atlas.base.service.amazon.service.AmazonService;
+import ch.sbb.atlas.base.service.imports.ServicePointCsvModel;
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+public class CsvServiceTest {
+
+  private CsvService csvService;
+
+  @Mock
+  private AmazonService amazonService;
+
+  @BeforeEach
+  void setUp() {
+    openMocks(this);
+    csvService = new CsvService(amazonService);
+  }
+
+  @Test
+  void test_downloadShouldNotFoundFile() {
+    String today = LocalDate.now().toString().replaceAll("-", "");
+    when(amazonService.getS3ObjectKeysFromPrefix(eq("servicepoint_didok"), eq("PREFIX_" + today))).thenReturn(
+        Collections.emptyList());
+
+    String exMessage =
+        assertThrows(RuntimeException.class, () -> csvService.downloadImportFileFromToday("PREFIX_")).getLocalizedMessage();
+    verify(amazonService).getS3ObjectKeysFromPrefix(eq("servicepoint_didok"), eq("PREFIX_" + today));
+    assertThat(exMessage).isEqualTo("[IMPORT]: Not found file on S3");
+  }
+
+  @Test
+  void test_downloadShouldFindMoreThanOneFile() {
+    String today = LocalDate.now().toString().replaceAll("-", "");
+    when(amazonService.getS3ObjectKeysFromPrefix(eq("servicepoint_didok"), eq("PREFIX_" + today)))
+        .thenReturn(List.of("file1", "file2"));
+
+    String exMessage =
+        assertThrows(RuntimeException.class, () -> csvService.downloadImportFileFromToday("PREFIX_")).getLocalizedMessage();
+    verify(amazonService).getS3ObjectKeysFromPrefix(eq("servicepoint_didok"), eq("PREFIX_" + today));
+    assertThat(exMessage).isEqualTo("[IMPORT]: Found more than 1 file to download");
+  }
+
+  @Test
+  void test_downloadShouldWork() throws IOException {
+    String today = LocalDate.now().toString().replaceAll("-", "");
+    when(amazonService.getS3ObjectKeysFromPrefix(eq("servicepoint_didok"), eq("PREFIX_" + today))).thenReturn(List.of("file"));
+    when(amazonService.pullFile(eq("file"))).thenReturn(new File("file"));
+
+    File file = csvService.downloadImportFileFromToday("PREFIX_");
+    verify(amazonService).getS3ObjectKeysFromPrefix(eq("servicepoint_didok"), eq("PREFIX_" + today));
+    verify(amazonService).pullFile(eq("file"));
+    assertThat(file.getName()).isEqualTo("file");
+  }
+
+  @Test
+  void test_getCsvModelsToUpdate_shouldReturnEmptyList() throws IOException {
+    csvService.setMatchingDate(LocalDate.of(2023, 1, 1));
+    File csv = new File(getClass().getClassLoader().getResource("DIENSTSTELLEN_V3_IMPORT.csv").getFile());
+    List<ServicePointCsvModel> csvModelsToUpdate = csvService.getCsvModelsToUpdate(csv, ServicePointCsvModel.class);
+    assertThat(csvModelsToUpdate).hasSize(0);
+  }
+
+  @Test
+  void test_getCsvModelsToUpdate_shouldReturnOneMismatchedCsvModel() throws IOException {
+    csvService.setMatchingDate(LocalDate.of(2020, 6, 9));
+    File csv = new File(getClass().getClassLoader().getResource("DIENSTSTELLEN_V3_IMPORT.csv").getFile());
+    List<ServicePointCsvModel> csvModelsToUpdate = csvService.getCsvModelsToUpdate(csv, ServicePointCsvModel.class);
+    assertThat(csvModelsToUpdate).hasSize(1);
+    ServicePointCsvModel csvModel = csvModelsToUpdate.get(0);
+    assertThat(csvModel.getNummer()).isEqualTo(1542);
+  }
+
+}
