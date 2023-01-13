89e5f44612ad678bf67f03ea09281ab0b1eaf708
atlas <19825793+iuccio@users.noreply.github.com>
2023-01-13 18:00:01 +0100

    ATLAS-815: add search queryparams and way to specification

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/repository/ServicePointVersionRepository.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/repository/ServicePointVersionRepository.java
@@ -4,11 +4,12 @@ import ch.sbb.atlas.servicepointdirectory.entity.ServicePointVersion;
 import ch.sbb.atlas.servicepointdirectory.model.ServicePointNumber;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ServicePointVersionRepository extends
-    JpaRepository<ServicePointVersion, Long> {
+    JpaRepository<ServicePointVersion, Long>, JpaSpecificationExecutor<ServicePointVersion> {
 
   List<ServicePointVersion> findAllByNumberOrderByValidFrom(ServicePointNumber number);
 
--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/service/servicepoint/ServicePointService.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/service/servicepoint/ServicePointService.java
@@ -2,13 +2,13 @@ package ch.sbb.atlas.servicepointdirectory.service.servicepoint;
 
 import ch.sbb.atlas.servicepointdirectory.entity.ServicePointVersion;
 import ch.sbb.atlas.servicepointdirectory.model.ServicePointNumber;
+import ch.sbb.atlas.servicepointdirectory.model.ServicePointSearchRestrictions;
 import ch.sbb.atlas.servicepointdirectory.repository.ServicePointVersionRepository;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,8 +18,9 @@ public class ServicePointService {
 
   private final ServicePointVersionRepository servicePointVersionRepository;
 
-  public Page<ServicePointVersion> findAll(Pageable pageable) {
-    return servicePointVersionRepository.findAll(pageable);
+  public Page<ServicePointVersion> findAll(ServicePointSearchRestrictions servicePointSearchRestrictions) {
+    return servicePointVersionRepository.findAll(servicePointSearchRestrictions.getSpecification(),
+        servicePointSearchRestrictions.getPageable());
   }
 
   public List<ServicePointVersion> findServicePoint(ServicePointNumber servicePointNumber) {
