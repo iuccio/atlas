c0d9563f316a1ce9c979f932dd54cdb97d5dda7f
atlas <19825793+iuccio@users.noreply.github.com>
2023-01-16 11:02:42 +0100

    ATLAS-533: geoData Tile Layer - validAt date

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/controller/ServicePointsGeoDataController.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/controller/ServicePointsGeoDataController.java
@@ -2,6 +2,7 @@ package ch.sbb.atlas.servicepointdirectory.controller;
 
 import ch.sbb.atlas.servicepointdirectory.geodata.protobuf.VectorTile;
 import ch.sbb.atlas.servicepointdirectory.geodata.service.ServicePointGeoDataService;
+import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -19,10 +20,8 @@ public class ServicePointsGeoDataController {
 
   @GetMapping(value = "/v1/geodata/servicepoints/{z}/{x}/{y}.pbf", produces = {
       "application/x-protobuf"})
-  public VectorTile.Tile getServicePointsGeoData(
-      @PathVariable("z") Integer z,
-      @PathVariable("x") Integer x,
-      @PathVariable("y") Integer y) {
-    return servicePointGeoDataService.getGeoData(z, x, y);
+  public VectorTile.Tile getServicePointsGeoData(@PathVariable("z") Integer z,
+      @PathVariable("x") Integer x, @PathVariable("y") Integer y) {
+    return servicePointGeoDataService.getGeoData(z, x, y, LocalDate.now());
   }
 }
