c98731b58086a77163c17d004cc570e0412c72da
atlas <19825793+iuccio@users.noreply.github.com>
2023-01-18 09:47:37 +0100

    ATLAS-914: code clean up

--- import-service-point/src/main/java/ch/sbb/importservice/batch/LoadingPointApiWriter.java
+++ import-service-point/src/main/java/ch/sbb/importservice/batch/LoadingPointApiWriter.java
@@ -1,50 +1,25 @@
 package ch.sbb.importservice.batch;
 
 import ch.sbb.atlas.base.service.imports.servicepoint.loadingpoint.LoadingPointCsvModel;
-import ch.sbb.importservice.entitiy.ImportProcessItem;
-import ch.sbb.importservice.repository.ImportProcessedItemRepository;
-import ch.sbb.importservice.service.SePoDiClientService;
 import feign.Response;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.StepExecution;
-import org.springframework.batch.core.annotation.BeforeStep;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
-public class LoadingPointApiWriter implements ItemWriter<LoadingPointCsvModel> {
-
-  @Autowired
-  private SePoDiClientService sePoDiClientService;
-
-  @Autowired
-  private ImportProcessedItemRepository importProcessedItemRepository;
-
-  private StepExecution stepExecution;
-
-  @BeforeStep
-  public void getStepExecutionData(StepExecution stepExecution) {
-    this.stepExecution = stepExecution;
-  }
+public class LoadingPointApiWriter extends BaseApiWriter implements ItemWriter<LoadingPointCsvModel> {
 
   @Override
   public void write(List<? extends LoadingPointCsvModel> loadingPointCsvModels) {
     Long stepExecutionId = stepExecution.getId();
     for (LoadingPointCsvModel loadingPointCsvModel : loadingPointCsvModels) {
       Response response = sePoDiClientService.getServicePoints(loadingPointCsvModel.getNumber());
-      ImportProcessItem importProcessItem = ImportProcessItem.builder()
-          .itemNumber(loadingPointCsvModel.getNumber())
-          .stepExecutionId(stepExecutionId)
-          .jobExecutionName(stepExecution.getJobExecution().getJobInstance().getJobName())
-          .responseStatus(response.status())
-          .responseMessage(response.reason())
-          .build();
-      importProcessedItemRepository.save(importProcessItem);
+      saveItemProcessed(stepExecutionId, loadingPointCsvModel.getNumber(), response);
       int status = response.status();
       log.info("Response status {}", status);
     }
   }
+
 }
--- import-service-point/src/main/java/ch/sbb/importservice/batch/ServicePointApiWriter.java
+++ import-service-point/src/main/java/ch/sbb/importservice/batch/ServicePointApiWriter.java
@@ -1,48 +1,22 @@
 package ch.sbb.importservice.batch;
 
 import ch.sbb.atlas.base.service.imports.servicepoint.servicepoint.ServicePointCsvModel;
-import ch.sbb.importservice.entitiy.ImportProcessItem;
-import ch.sbb.importservice.repository.ImportProcessedItemRepository;
-import ch.sbb.importservice.service.SePoDiClientService;
 import feign.Response;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.StepExecution;
-import org.springframework.batch.core.annotation.BeforeStep;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
-public class ServicePointApiWriter implements ItemWriter<ServicePointCsvModel> {
-
-  @Autowired
-  private SePoDiClientService sePoDiClientService;
-
-  @Autowired
-  private ImportProcessedItemRepository importProcessedItemRepository;
-
-  private StepExecution stepExecution;
-
-  @BeforeStep
-  public void getStepExecutionData(StepExecution stepExecution) {
-    this.stepExecution = stepExecution;
-  }
+public class ServicePointApiWriter extends BaseApiWriter implements ItemWriter<ServicePointCsvModel> {
 
   @Override
   public void write(List<? extends ServicePointCsvModel> servicePoints) {
     Long stepExecutionId = stepExecution.getId();
     for (ServicePointCsvModel servicePoint : servicePoints) {
       Response response = sePoDiClientService.getServicePoints(servicePoint.getDidokCode());
-      ImportProcessItem importProcessItem = ImportProcessItem.builder()
-          .itemNumber(servicePoint.getDidokCode())
-          .stepExecutionId(stepExecutionId)
-          .jobExecutionName(stepExecution.getJobExecution().getJobConfigurationName())
-          .responseStatus(response.status())
-          .responseMessage(response.reason())
-          .build();
-      importProcessedItemRepository.save(importProcessItem);
+      saveItemProcessed(stepExecutionId, servicePoint.getNummer(), response);
       int status = response.status();
       log.info("Response status {}", status);
     }
