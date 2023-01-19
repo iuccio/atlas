fea46b37cffde89ce8f470cc58692c55d838085c
atlas <TechSupport-ATLAS@sbb.ch>
2023-01-19 11:08:54 +0100

    ATLAS-533 - mnaplibre ma

--- frontend/src/app/pages/sepodi/overview/sepodi-overview.component.ts
+++ frontend/src/app/pages/sepodi/overview/sepodi-overview.component.ts
@@ -1,5 +1,31 @@
 import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { Map } from 'maplibre-gl';
+import { Map, StyleSpecification } from 'maplibre-gl';
+
+const MAP_STYLE: StyleSpecification = {
+  version: 8,
+  sources: {
+    osm: {
+      type: 'raster',
+      tiles: [
+        'https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/default/current/3857/{z}/{x}/{y}.jpeg',
+      ],
+      tileSize: 256,
+      attribution: '&copy; OpenStreetMap Contributors',
+      maxzoom: 19,
+      bounds: [5.140242, 45.3981812, 11.47757, 48.230651],
+    },
+  },
+  layers: [
+    {
+      id: 'osm',
+      type: 'raster',
+      source: 'osm',
+      paint: {
+        'raster-opacity': 0.5,
+      },
+    },
+  ],
+};
 
 @Component({
   selector: 'app-sepodi-overview',
@@ -17,7 +43,7 @@ export class SepodiOverviewComponent implements AfterViewInit, OnDestroy {
 
     this.map = new Map({
       container: this.mapContainer.nativeElement,
-      style: 'https://demotiles.maplibre.org/style.json', // style URL
+      style: MAP_STYLE,
       center: [initialState.lng, initialState.lat],
       zoom: initialState.zoom,
     });
