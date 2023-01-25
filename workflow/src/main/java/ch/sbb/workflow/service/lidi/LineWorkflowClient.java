19b355aa44040d5964332d868dd9637f0e9f4b48
atlas <19825793+iuccio@users.noreply.github.com>
2023-01-25 17:22:55 +0100

    ATLAS-984: actually call gateway

--- workflow/src/main/java/ch/sbb/workflow/service/lidi/LineWorkflowClient.java
+++ workflow/src/main/java/ch/sbb/workflow/service/lidi/LineWorkflowClient.java
@@ -4,7 +4,7 @@ import ch.sbb.atlas.api.line.workflow.LineWorkflowApi;
 import ch.sbb.workflow.config.TokenPassingFeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "lineWorkflowClient", url = "localhost:8888", path = "line-directory", configuration =
+@FeignClient(name = "lineWorkflowClient", url = "${atlas.client.gateway.url}", path = "line-directory", configuration =
     TokenPassingFeignConfig.class)
 public interface LineWorkflowClient extends LineWorkflowApi {
 
