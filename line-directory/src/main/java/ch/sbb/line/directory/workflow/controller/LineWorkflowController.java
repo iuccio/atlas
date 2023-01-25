3893caac06140614f1b1cbd0c1a310e0702f96a7
atlas <19825793+iuccio@users.noreply.github.com>
2023-01-25 16:22:49 +0100

    ATLAS-984: seperate module for api interface

--- line-directory/src/main/java/ch/sbb/line/directory/workflow/controller/LineWorkflowController.java
+++ line-directory/src/main/java/ch/sbb/line/directory/workflow/controller/LineWorkflowController.java
@@ -1,8 +1,8 @@
 package ch.sbb.line.directory.workflow.controller;
 
+import ch.sbb.atlas.api.line.workflow.LineWorkflowApi;
+import ch.sbb.atlas.api.line.workflow.LineWorkflowEvent;
 import ch.sbb.atlas.base.service.model.workflow.WorkflowStatus;
-import ch.sbb.line.directory.workflow.api.LineWorkflowEvent;
-import ch.sbb.line.directory.workflow.api.LineWorkflowApi;
 import ch.sbb.line.directory.workflow.service.LineWorkflowProcessingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
