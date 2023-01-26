d06fd702b0697b862ba83ecd60474250910b8271
atlas <19825793+iuccio@users.noreply.github.com>
2023-01-26 12:39:43 +0100

    ATLAS-533 - cleanup

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/controller/ServicePointsGeoDataController.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/controller/ServicePointsGeoDataController.java
@@ -2,11 +2,14 @@ package ch.sbb.atlas.servicepointdirectory.controller;
 
 import ch.sbb.atlas.servicepointdirectory.geodata.protobuf.VectorTile;
 import ch.sbb.atlas.servicepointdirectory.geodata.service.ServicePointGeoDataService;
+import io.swagger.v3.oas.annotations.Parameter;
 import java.time.LocalDate;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,7 +22,9 @@ public class ServicePointsGeoDataController {
   @GetMapping(value = "/v1/geodata/service-points/{z}/{x}/{y}.pbf", produces = {
       "application/x-protobuf"})
   public VectorTile.Tile getServicePointsGeoData(@PathVariable("z") Integer z,
-      @PathVariable("x") Integer x, @PathVariable("y") Integer y) {
-    return servicePointGeoDataService.getGeoData(z, x, y, LocalDate.now());
+      @PathVariable("x") Integer x, @PathVariable("y") Integer y,
+      @RequestParam("validAtDate") Optional<LocalDate> validAtDate) {
+    return servicePointGeoDataService.getGeoData(z, x, y, validAtDate.orElse(LocalDate.now())
+    );
   }
 }
