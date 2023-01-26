9681b92872710da294f2e8852c50b42b7e348902
atlas <19825793+iuccio@users.noreply.github.com>
2023-01-26 12:46:01 +0100

    ATLAS-533 - cleanup

--- service-point-directory/src/main/resources/db/migration/V0.0.2__.sql
+++ service-point-directory/src/main/resources/db/migration/V0.0.2__.sql
@@ -1,7 +1,15 @@
+-----------------------------------------------------------------------------------------
+-- Service Point Version Geolocation
+-----------------------------------------------------------------------------------------
+
 CREATE INDEX service_point_version_geolocation_spatial_reference_index
     ON service_point_version_geolocation (spatial_reference);
 CREATE INDEX service_point_version_geolocation_east_north_index
     ON service_point_version_geolocation (east, north);
+
+-----------------------------------------------------------------------------------------
+-- Service Point Version - DIENSTSTELLEN_FC
+-----------------------------------------------------------------------------------------
 
 CREATE INDEX service_point_version_valid_from_to_index
     ON service_point_version (valid_from, valid_to);
