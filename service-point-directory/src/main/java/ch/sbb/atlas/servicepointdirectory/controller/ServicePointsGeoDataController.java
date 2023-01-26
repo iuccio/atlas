75506ec640213ebff53387b0500992ee64cc22f2
atlas <19825793+iuccio@users.noreply.github.com>
2023-01-26 15:02:25 +0100

    ATLAS-533 - cleanup

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/controller/ServicePointsGeoDataController.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/controller/ServicePointsGeoDataController.java
@@ -2,7 +2,6 @@ package ch.sbb.atlas.servicepointdirectory.controller;
 
 import ch.sbb.atlas.servicepointdirectory.geodata.protobuf.VectorTile;
 import ch.sbb.atlas.servicepointdirectory.geodata.service.ServicePointGeoDataService;
-import io.swagger.v3.oas.annotations.Parameter;
 import java.time.LocalDate;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
