6f155edacbd169868279f76701917ef836074e76
atlas <19825793+iuccio@users.noreply.github.com>
2023-01-27 16:26:40 +0100

    ATLAS-533: Move config to package, cleanup

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/config/ProtobufConfig.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/config/ProtobufConfig.java
@@ -1,11 +1,11 @@
-package ch.sbb.atlas.servicepointdirectory.converter;
+package ch.sbb.atlas.servicepointdirectory.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
 
 @Configuration
-public class ProtobufConverter {
+public class ProtobufConfig {
 
     @Bean
     ProtobufHttpMessageConverter protobufHttpMessageConverter() {
