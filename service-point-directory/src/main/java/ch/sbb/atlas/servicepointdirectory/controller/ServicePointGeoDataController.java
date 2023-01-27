640ff94e9ce289fe23a4d567980541dbd4555932
atlas <19825793+iuccio@users.noreply.github.com>
2023-01-27 16:01:09 +0100

    ATLAS-533: Ref

---
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/controller/ServicePointGeoDataController.java
@@ -0,0 +1,22 @@
+package ch.sbb.atlas.servicepointdirectory.controller;
+
+import ch.sbb.atlas.servicepointdirectory.api.ServicePointGeoDataApiV1;
+import ch.sbb.atlas.servicepointdirectory.geodata.protobuf.VectorTile;
+import ch.sbb.atlas.servicepointdirectory.geodata.service.ServicePointGeoDataService;
+import java.time.LocalDate;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class ServicePointGeoDataController implements ServicePointGeoDataApiV1 {
+
+  private final ServicePointGeoDataService servicePointGeoDataService;
+
+  public VectorTile.Tile getServicePointsGeoData(Integer z, Integer x, Integer y, Optional<LocalDate> validAtDate) {
+    return servicePointGeoDataService.getGeoData(z, x, y, validAtDate.orElse(LocalDate.now()));
+  }
+}
