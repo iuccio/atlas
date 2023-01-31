7c6d5afac6785cca4adfa43771ecb6da11044f5d
atlas <19825793+iuccio@users.noreply.github.com>
2023-01-31 15:28:51 +0100

    ATLAS-914: remove duplication

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/service/loadingpoint/LoadingPointCsvModel.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/service/loadingpoint/LoadingPointCsvModel.java
@@ -1,7 +1,7 @@
 package ch.sbb.atlas.servicepointdirectory.service.loadingpoint;
 
+import ch.sbb.atlas.base.service.imports.servicepoint.BaseDidokCsvModel;
 import ch.sbb.atlas.base.service.imports.servicepoint.deserializer.NumericBooleanDeserializer;
-import ch.sbb.atlas.servicepointdirectory.service.BaseDidokCsvModel;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
