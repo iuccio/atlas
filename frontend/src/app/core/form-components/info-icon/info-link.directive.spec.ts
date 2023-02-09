badf292e8f8e8827311ca36faaf181b7d450d9e1
atlas <19825793+iuccio@users.noreply.github.com>
2023-02-09 13:39:28 +0100

    ATLAS-992: finished tooltip updates for ttfn and lines

---
+++ frontend/src/app/core/form-components/info-icon/info-link.directive.spec.ts
@@ -0,0 +1,51 @@
+import { InfoLinkDirective } from './info-link.directive';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
+import SpyObj = jasmine.SpyObj;
+import { of } from 'rxjs';
+
+@Component({
+  template: `<span infoLink infoLinkTranslationKey="TEST_TRANSLATION_KEY">Test</span>`,
+})
+class TestComponent {}
+
+describe('InfoLinkDirective', () => {
+  let fixture: ComponentFixture<TestComponent>;
+  let translateServiceSpy: SpyObj<TranslateService>;
+
+  beforeEach(() => {
+    translateServiceSpy = jasmine.createSpyObj<TranslateService>('TranslateServiceMock', ['get']);
+    translateServiceSpy.get = jasmine.createSpy().and.returnValue(of('https://atlas.test.ch'));
+    fixture = TestBed.configureTestingModule({
+      declarations: [InfoLinkDirective, TestComponent],
+      providers: [{ provide: TranslateService, useValue: translateServiceSpy }],
+    }).createComponent(TestComponent);
+
+    fixture.detectChanges();
+  });
+
+  it('should add atlas-info-link class to component host element', () => {
+    const elementsWithInfoLinkDirective = fixture.debugElement.queryAll(
+      By.directive(InfoLinkDirective)
+    );
+    expect(elementsWithInfoLinkDirective).toHaveSize(1);
+    expect(elementsWithInfoLinkDirective[0].classes['atlas-info-link']).toBeTrue();
+  });
+
+  it('should handle click event', () => {
+    const elementsWithInfoLinkDirective = fixture.debugElement.queryAll(
+      By.directive(InfoLinkDirective)
+    );
+    expect(elementsWithInfoLinkDirective).toHaveSize(1);
+
+    spyOn(window, 'open');
+    spyOn(console, 'error');
+    elementsWithInfoLinkDirective[0].nativeElement.click();
+    fixture.detectChanges();
+    expect(translateServiceSpy.get).toHaveBeenCalledOnceWith('TEST_TRANSLATION_KEY');
+    expect(window.open).toHaveBeenCalledOnceWith('https://atlas.test.ch', '_blank');
+    expect(console.error).not.toHaveBeenCalled();
+  });
+});
