c65ad2147a728716a582e84c90bdfaa44c932055
atlas <TechSupport-ATLAS@sbb.ch>
2023-02-09 13:39:28 +0100

    ATLAS-992: finished tooltip updates for ttfn and lines

--- frontend/src/app/core/form-components/info-icon/info-icon.component.ts
+++ frontend/src/app/core/form-components/info-icon/info-icon.component.ts
@@ -6,5 +6,5 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./info-icon.component.scss'],
 })
 export class InfoIconComponent {
-  @Input() infoTitle!: string;
+  @Input() infoTitle = '';
 }
