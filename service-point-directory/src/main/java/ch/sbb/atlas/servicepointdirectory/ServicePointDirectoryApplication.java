1107a0e527dd48b7693ba1ffb354bd51bb166765
atlas <19825793+iuccio@users.noreply.github.com>
2023-02-13 15:15:34 +0100

    Cleanup Bean initialization

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/ServicePointDirectoryApplication.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/ServicePointDirectoryApplication.java
@@ -1,14 +1,10 @@
 package ch.sbb.atlas.servicepointdirectory;
 
-import ch.sbb.atlas.base.service.model.configuration.AtlasExceptionHandler;
-import ch.sbb.atlas.base.service.versioning.service.VersionableService;
-import ch.sbb.atlas.base.service.versioning.service.VersionableServiceImpl;
 import ch.sbb.atlas.kafka.KafkaTruststorePreparation;
 import java.time.ZoneId;
 import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServicePointDirectoryApplication {
@@ -17,16 +13,6 @@ public class ServicePointDirectoryApplication {
     TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("Europe/Zurich")));
     KafkaTruststorePreparation.setupTruststore();
     SpringApplication.run(ServicePointDirectoryApplication.class, args);
-  }
-
-  @Bean
-  public AtlasExceptionHandler atlasExceptionHandler() {
-    return new AtlasExceptionHandler();
-  }
-
-  @Bean
-  public VersionableService versionableService() {
-    return new VersionableServiceImpl();
   }
 
 }
