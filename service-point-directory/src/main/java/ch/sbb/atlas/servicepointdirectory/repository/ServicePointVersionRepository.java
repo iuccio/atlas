0dd6deb41e2757ff098e34734e87fa908dba5478
atlas <19825793+iuccio@users.noreply.github.com>
2023-02-14 09:57:37 +0100

    ATLAS-533: complete rebase

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/repository/ServicePointVersionRepository.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/repository/ServicePointVersionRepository.java
@@ -1,12 +1,8 @@
 package ch.sbb.atlas.servicepointdirectory.repository;
 
-import ch.sbb.atlas.base.service.imports.servicepoint.enumeration.SpatialReference;
 import ch.sbb.atlas.servicepointdirectory.entity.ServicePointVersion;
 import ch.sbb.atlas.servicepointdirectory.model.ServicePointNumber;
-import java.time.LocalDate;
 import java.util.List;
-import org.locationtech.jts.geom.Envelope;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -18,44 +14,5 @@ public interface ServicePointVersionRepository extends JpaRepository<ServicePoin
   List<ServicePointVersion> findAllByNumberOrderByValidFrom(ServicePointNumber number);
 
   boolean existsByNumber(ServicePointNumber servicePointNumber);
-  
-  static Specification<ServicePointVersion> coordinatesBetween(
-      SpatialReference spatialReference,
-      Envelope envelope) {
-    return spatialReferenceEquals(spatialReference)
-        .and(eastBetween(envelope.getMinX(), envelope.getMaxX())
-            .and(northBetween(envelope.getMinY(), envelope.getMaxY()))
-        );
-  }
-
-  static Specification<ServicePointVersion> validAtDate(LocalDate date) {
-    return validFromBeforeDate(date).and(validToAfterDate(date));
-  }
-
-  static Specification<ServicePointVersion> validFromBeforeDate(LocalDate date) {
-    return (root, query, criteriaBuilder) -> criteriaBuilder.lessThanOrEqualTo(
-        root.get("validFrom"), date);
-  }
-
-  static Specification<ServicePointVersion> validToAfterDate(LocalDate date) {
-    return (root, query, criteriaBuilder) -> criteriaBuilder.greaterThanOrEqualTo(
-        root.get("validTo"), date);
-  }
-
-  static Specification<ServicePointVersion> spatialReferenceEquals(
-      SpatialReference spatialReference) {
-    return (root, query, criteriaBuilder) -> criteriaBuilder.equal(
-        root.get("servicePointGeolocation").get("spatialReference"), spatialReference);
-  }
-
-  static Specification<ServicePointVersion> eastBetween(Double west, Double east) {
-    return (root, query, criteriaBuilder) -> criteriaBuilder.between(
-        root.get("servicePointGeolocation").get("east"), west, east);
-  }
-
-  static Specification<ServicePointVersion> northBetween(Double south, Double north) {
-    return (root, query, criteriaBuilder) -> criteriaBuilder.between(
-        root.get("servicePointGeolocation").get("north"), south, north);
-  }
 
 }
--- service-point-directory/src/test/java/ch/sbb/atlas/servicepointdirectory/repository/ServicePointGeolocationTest.java
+++ service-point-directory/src/test/java/ch/sbb/atlas/servicepointdirectory/repository/ServicePointGeolocationTest.java
@@ -5,10 +5,10 @@ import static ch.sbb.atlas.servicepointdirectory.model.TestData.testServicePoint
 import static ch.sbb.atlas.servicepointdirectory.repository.ServicePointGeolocationRepository.coordinatesBetween;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import ch.sbb.atlas.base.service.imports.servicepoint.enumeration.SpatialReference;
 import ch.sbb.atlas.base.service.model.controller.IntegrationTest;
 import ch.sbb.atlas.servicepointdirectory.entity.ServicePointVersion;
 import ch.sbb.atlas.servicepointdirectory.entity.geolocation.ServicePointGeoData;
-import ch.sbb.atlas.servicepointdirectory.enumeration.SpatialReference;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
