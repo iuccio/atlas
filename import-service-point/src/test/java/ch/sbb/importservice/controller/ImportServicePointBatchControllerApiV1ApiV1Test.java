fd519133dba033a7acb09a17034804e73715a898
atlas <19825793+iuccio@users.noreply.github.com>
2023-02-16 11:17:49 +0100

    ATLAS-956: fix importController registration

--- import-service-point/src/test/java/ch/sbb/importservice/controller/ImportServicePointBatchControllerApiV1ApiV1Test.java
+++ import-service-point/src/test/java/ch/sbb/importservice/controller/ImportServicePointBatchControllerApiV1ApiV1Test.java
@@ -22,7 +22,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-public class ImportServicePointBatchControllerApiV1Test extends BaseControllerApiTest {
+public class ImportServicePointBatchControllerApiV1ApiV1Test extends BaseControllerApiTest {
 
   @MockBean
   private SePoDiClient sePoDiClient;
