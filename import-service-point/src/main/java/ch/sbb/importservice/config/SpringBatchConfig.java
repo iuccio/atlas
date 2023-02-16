0809bc0d11fc028f2167736f911e5d45150ba171
atlas <19825793+iuccio@users.noreply.github.com>
2023-02-16 11:17:49 +0100

    ATLAS-956: fix importController registration

--- import-service-point/src/main/java/ch/sbb/importservice/config/SpringBatchConfig.java
+++ import-service-point/src/main/java/ch/sbb/importservice/config/SpringBatchConfig.java
@@ -20,7 +20,6 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
@@ -34,7 +33,6 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
-@EnableBatchProcessing
 @AllArgsConstructor
 @Slf4j
 public class SpringBatchConfig {
