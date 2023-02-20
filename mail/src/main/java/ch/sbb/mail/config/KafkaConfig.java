4c6eeefa29606c4507037fc87751f420f68f16c1
atlas <19825793+iuccio@users.noreply.github.com>
2023-02-20 11:26:42 +0100

    ATLAS-956: trace kafka

--- mail/src/main/java/ch/sbb/mail/config/KafkaConfig.java
+++ mail/src/main/java/ch/sbb/mail/config/KafkaConfig.java
@@ -1,5 +1,6 @@
 package ch.sbb.mail.config;
 
+import ch.sbb.atlas.kafka.SharedKafkaConfig;
 import java.util.HashMap;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
@@ -7,12 +8,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
 @Configuration
 @Slf4j
 @RequiredArgsConstructor
+@Import(SharedKafkaConfig.class)
 public class KafkaConfig {
 
   private final KafkaProperties kafkaProperties;
--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/config/KafkaConfig.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/config/KafkaConfig.java
@@ -1,17 +1,20 @@
 package ch.sbb.atlas.servicepointdirectory.config;
 
+import ch.sbb.atlas.kafka.SharedKafkaConfig;
 import java.util.HashMap;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
 @Configuration
 @RequiredArgsConstructor
+@Import(SharedKafkaConfig.class)
 public class KafkaConfig {
 
   private final KafkaProperties kafkaProperties;
