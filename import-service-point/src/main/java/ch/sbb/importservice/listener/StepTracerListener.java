a0b30602bf0e60583bb1053bce6e6e5f861fc8cb
atlas <19825793+iuccio@users.noreply.github.com>
2023-02-21 14:04:52 +0100

    ATLAS-956: sonar update

--- import-service-point/src/main/java/ch/sbb/importservice/listener/StepTracerListener.java
+++ import-service-point/src/main/java/ch/sbb/importservice/listener/StepTracerListener.java
@@ -1,5 +1,6 @@
 package ch.sbb.importservice.listener;
 
+import io.micrometer.tracing.Span;
 import io.micrometer.tracing.Tracer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,9 +17,12 @@ public class StepTracerListener implements StepExecutionListener {
 
   @Override
   public void beforeStep(StepExecution stepExecution) {
-    String traceId = tracer.currentSpan().context().traceId();
-    stepExecution.getExecutionContext().put("traceId", traceId);
-    log.info("Putting TraceId to stepExecution: {}", traceId);
+    Span currentSpan = tracer.currentSpan();
+    if (currentSpan != null) {
+      String traceId = currentSpan.context().traceId();
+      stepExecution.getExecutionContext().put("traceId", traceId);
+      log.info("Putting TraceId to stepExecution: {}", traceId);
+    }
   }
 
 }
