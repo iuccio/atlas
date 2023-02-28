9f0c9d90d242720c40012acdd0b652ed85740cbf
atlas <19825793+iuccio@users.noreply.github.com>
2023-02-28 08:16:48 +0100

    ATLAS-1115: unset field background when disabled, code clean up

--- frontend/src/app/core/module/material.module.ts
+++ frontend/src/app/core/module/material.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule } from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
