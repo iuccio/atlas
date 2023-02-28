9e733d737fa89d1442692693d9bc9a462544a210
atlas <19825793+iuccio@users.noreply.github.com>
2023-02-28 09:38:41 +0100

    ATLAS-1018: Carve out some more functionality

--- timetable-hearing/src/main/java/ch/sbb/atlas/timetable/hearing/repository/TimetableHearingStatementRepository.java
+++ timetable-hearing/src/main/java/ch/sbb/atlas/timetable/hearing/repository/TimetableHearingStatementRepository.java
@@ -1,11 +1,12 @@
 package ch.sbb.atlas.timetable.hearing.repository;
 
 import ch.sbb.atlas.timetable.hearing.entity.TimetableHearingStatement;
-import ch.sbb.atlas.timetable.hearing.entity.TimetableHearingYear;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TimetableHearingStatementRepository extends JpaRepository<TimetableHearingStatement, Long> {
+public interface TimetableHearingStatementRepository extends JpaRepository<TimetableHearingStatement, Long>,
+    JpaSpecificationExecutor<TimetableHearingStatement> {
 
 }
