6f94668e0f7b14f5ead46fae46992e0805ef325a
atlas <19825793+iuccio@users.noreply.github.com>
2023-02-28 13:11:54 +0100

    ATLAS-1115: mat dialog

--- frontend/src/app/core/components/dialog/dialog.component.ts
+++ frontend/src/app/core/components/dialog/dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, ViewEncapsulation } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { DialogData } from './dialog.data';
 
@@ -6,7 +6,6 @@ import { DialogData } from './dialog.data';
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
   styleUrls: ['dialog.component.scss'],
-  encapsulation: ViewEncapsulation.None,
 })
 export class DialogComponent {
   constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) {}
