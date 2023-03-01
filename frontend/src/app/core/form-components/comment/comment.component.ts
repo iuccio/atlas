a6095cf81d1df099dc43e5f92a9e44056479f249
atlas <19825793+iuccio@users.noreply.github.com>
2023-03-01 16:29:44 +0100

    ATLAS-1115: migrate comment component

--- frontend/src/app/core/form-components/comment/comment.component.ts
+++ frontend/src/app/core/form-components/comment/comment.component.ts
@@ -4,6 +4,7 @@ import { FormGroup } from '@angular/forms';
 @Component({
   selector: 'form-comment',
   templateUrl: './comment.component.html',
+  styleUrls: ['./comment.component.scss'],
 })
 export class CommentComponent {
   @Input() formGroup!: FormGroup;
