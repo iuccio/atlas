6943fa84cf7720547b23f3d26ba99cc134de1136
atlas <19825793+iuccio@users.noreply.github.com>
2023-03-03 09:26:05 +0100

    ATLAS-1018: Hack - safe transport company data redundently

--- line-directory/src/main/java/ch/sbb/line/directory/mapper/TimeTableHearingStatementMapper.java
+++ line-directory/src/main/java/ch/sbb/line/directory/mapper/TimeTableHearingStatementMapper.java
@@ -2,8 +2,6 @@ package ch.sbb.line.directory.mapper;
 
 import ch.sbb.atlas.api.timetable.hearing.TimetableHearingStatementModel;
 import ch.sbb.line.directory.entity.TimetableHearingStatement;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.stream.Collectors;
 import lombok.experimental.UtilityClass;
 
@@ -11,20 +9,24 @@ import lombok.experimental.UtilityClass;
 public class TimeTableHearingStatementMapper {
 
   public static TimetableHearingStatement toEntity(TimetableHearingStatementModel statementModel) {
-    return TimetableHearingStatement.builder()
+    TimetableHearingStatement timetableHearingStatement = TimetableHearingStatement.builder()
         .id(statementModel.getId())
         .statementStatus(statementModel.getStatementStatus())
         .timetableYear(statementModel.getTimetableYear())
         .ttfnid(statementModel.getTtfnid())
         .swissCanton(statementModel.getSwissCanton())
         .stopPlace(statementModel.getStopPlace())
-        .responsibleTransportCompanies(new HashSet<>(statementModel.getResponsibleTransportCompanies()))
         .statementSender(StatementSenderMapper.toEntity(statementModel.getStatementSender()))
         .statement(statementModel.getStatement())
         .documents(statementModel.getDocuments().stream().map(StatementDocumentMapper::toEntity).collect(Collectors.toSet()))
         .justification(statementModel.getJustification())
         .version(statementModel.getEtagVersion())
         .build();
+    timetableHearingStatement.setResponsibleTransportCompanies(
+        statementModel.getResponsibleTransportCompanies().stream()
+            .map(transportCompany -> ResponsibleTransportCompanyMapper.toEntity(transportCompany, timetableHearingStatement))
+            .collect(Collectors.toSet()));
+    return timetableHearingStatement;
   }
 
   public static TimetableHearingStatementModel toModel(TimetableHearingStatement statement) {
@@ -35,7 +37,8 @@ public class TimeTableHearingStatementMapper {
         .ttfnid(statement.getTtfnid())
         .swissCanton(statement.getSwissCanton())
         .stopPlace(statement.getStopPlace())
-        .responsibleTransportCompanies(new ArrayList<>(statement.getResponsibleTransportCompanies()))
+        .responsibleTransportCompanies(
+            statement.getResponsibleTransportCompanies().stream().map(ResponsibleTransportCompanyMapper::toModel).toList())
         .statementSender(StatementSenderMapper.toModel(statement.getStatementSender()))
         .statement(statement.getStatement())
         .documents(statement.getDocuments().stream().map(StatementDocumentMapper::toModel).toList())
