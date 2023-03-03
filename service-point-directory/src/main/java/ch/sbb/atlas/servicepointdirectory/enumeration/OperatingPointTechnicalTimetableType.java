1cc6fa1780a88978af0db9bec163c50fca37876b
atlas <19825793+iuccio@users.noreply.github.com>
2023-03-03 13:15:27 +0100

    ATLAS-1141: Fix typo

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/enumeration/OperatingPointTechnicalTimetableType.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/enumeration/OperatingPointTechnicalTimetableType.java
@@ -43,7 +43,7 @@ public enum OperatingPointTechnicalTimetableType implements CodeAndDesignations 
   INTERSECTION(11, true, "Ausweiche", "Évitement", "Incrocio", "Ausweiche", "technischerfahrplan", "Ausw", "évit", "incr", null),
   CONNECTING_POINT(9, true, "Anschlusspunkt", "Point de raccordement", "Punto di raccordo ", "Anschlusspunkt",
       "technischerfahrplan", "Apt", "prc", "prc", null),
-  LANGE_CHANGE(8, true, "Spurwechsel", "Diagonale d'échange", "Cambio binario ", "Spurwechsel", "technischerfahrplan", "Spw",
+  LANE_CHANGE(8, true, "Spurwechsel", "Diagonale d'échange", "Cambio binario ", "Spurwechsel", "technischerfahrplan", "Spw",
       "diag", "c bin", null),
   BRANCH(7, true, "Abzweigung, Verzweigung, Spaltweiche", "Bifurcation", "Diramazione", "Abzweigung, Verzweigung, Spaltweiche ",
       "technischerfahrplan", "Vzw", "bif", "drm", null),
