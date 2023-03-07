755673e5d5197e5853f3db75f75b2136a8715550
atlas <19825793+iuccio@users.noreply.github.com>
2023-03-07 15:53:29 +0100

    ATLAS-1115: update error-field component

--- frontend/src/app/core/form-components/atlas-field-error/atlas-field-error.component.ts
+++ frontend/src/app/core/form-components/atlas-field-error/atlas-field-error.component.ts
@@ -1,74 +1,43 @@
-import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
-import { concat, debounceTime, EMPTY, Observable, of } from 'rxjs';
-import { FormGroup, ValidationErrors } from '@angular/forms';
-import { map, tap } from 'rxjs/operators';
+import { Component, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { AtlasFieldCustomError } from './atlas-field-custom-error';
+import { ValidationService } from '../../validation/validation.service';
 
 @Component({
   selector: 'app-atlas-field-error',
   templateUrl: './atlas-field-error.component.html',
   styleUrls: ['./atlas-field-error.component.scss'],
 })
-export class AtlasFieldErrorComponent implements OnInit {
+export class AtlasFieldErrorComponent {
   @Input() controlName!: string;
   @Input() form: FormGroup = new FormGroup({});
   @Input() customError!: AtlasFieldCustomError;
-  hasError: Observable<boolean> = EMPTY;
-  errorTranslationKeyToShow = '';
-  validationErrors: ValidationErrors | null | undefined;
 
-  constructor(private cdref: ChangeDetectorRef) {}
+  constructor(private validationService: ValidationService) {}
 
-  get isTouched(): boolean {
-    return !!this.form.get(this.controlName)?.touched;
-  }
-
-  get isDirty(): boolean {
-    return !!this.form.get(this.controlName)?.dirty;
-  }
-
-  ngAfterContentChecked() {
-    this.cdref.detectChanges();
-  }
-
-  ngOnInit() {
-    const formControl = this.form.get(this.controlName);
-    if (formControl) {
-      this.hasError = concat(
-        // // start value
-        of(formControl.invalid),
-
-        // on value changes
-        formControl.valueChanges.pipe(
-          debounceTime(300),
-          map(() => this.hasErrors())
-        )
-      ).pipe(
-        tap((hasError) => {
-          if (hasError) {
-            this.errorTranslationKeyToShow = this.getFirstErrorTranslationKey();
-          }
-        })
-      );
+  get errors() {
+    const formField = this.form.get(this.controlName);
+    let validationErrors = this.getValidationErrors();
+    if (validationErrors) {
+      if ((validationErrors['required'] && formField?.touched) || !validationErrors['required']) {
+        return this.validationService.getValidation(validationErrors || null);
+      }
     }
+    return null;
   }
 
-  hasErrors(): boolean {
-    const formControl = this.form.get(this.controlName);
-    return !formControl ? false : formControl.invalid;
-  }
-
-  getFirstErrorTranslationKey(): string {
-    this.validationErrors = this.form.get(this.controlName)?.errors;
-
-    if (this.customError && this.validationErrors) {
-      if (this.validationErrors[this.customError.errorKey]) {
+  get error() {
+    let validationErrors = this.getValidationErrors();
+    if (validationErrors) {
+      if (this.customError && validationErrors[this.customError.errorKey]) {
         return this.customError.translationKey;
       }
     }
+    return null;
+  }
 
-    return this.validationErrors
-      ? `VALIDATION_ATLAS_FORM_COMPONENT.${Object.keys(this.validationErrors)[0].toUpperCase()}`
-      : '';
+  private getValidationErrors() {
+    const formField = this.form.get(this.controlName);
+    return formField?.errors;
   }
 }
