71f5ad3ecc3c91b0dfcb195ccdd7dead346c9a38
atlas <19825793+iuccio@users.noreply.github.com>
2023-03-15 21:39:44 +0100

    ATLAS-1005: impl tth overview

--- frontend/src/app/pages/tth/tth.module.ts
+++ frontend/src/app/pages/tth/tth.module.ts
@@ -3,9 +3,10 @@ import { CoreModule } from '../../core/module/core.module';
 import { TthRoutingModule } from './tth-routing.module';
 import { FormModule } from '../../core/module/form.module';
 import { TimetableHearingOverviewComponent } from './overview/timetable-hearing-overview.component';
+import { CantonCardComponent } from './overview/canton-card/canton-card.component';
 
 @NgModule({
-  declarations: [TimetableHearingOverviewComponent],
+  declarations: [TimetableHearingOverviewComponent, CantonCardComponent],
   imports: [CoreModule, TthRoutingModule, FormModule],
 })
 export class TthModule {}
