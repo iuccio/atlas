fb3687a4c77a2b16c41f00189044128bd6cf8b78
atlas <19825793+iuccio@users.noreply.github.com>
2023-03-16 16:52:19 +0100

    ATLAS-1005: add translations, add tests

--- frontend/src/app/pages/tth/overview/timetable-hearing-overview.component.spec.ts
+++ frontend/src/app/pages/tth/overview/timetable-hearing-overview.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TimetableHearingOverviewComponent } from './timetable-hearing-overview.component';
+import { By } from '@angular/platform-browser';
+import { CantonCardComponent } from './canton-card/canton-card.component';
+import { TthModule } from '../tth.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 describe('TimetableHearingOverviewComponent', () => {
   let component: TimetableHearingOverviewComponent;
@@ -8,7 +12,8 @@ describe('TimetableHearingOverviewComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [TimetableHearingOverviewComponent],
+      declarations: [TimetableHearingOverviewComponent, CantonCardComponent],
+      imports: [TthModule, TranslateModule.forRoot()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TimetableHearingOverviewComponent);
@@ -18,5 +23,10 @@ describe('TimetableHearingOverviewComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create 27 canton cards', () => {
+    const cards = fixture.debugElement.queryAll(By.css('.card'));
+    expect(cards.length).toBe(27);
   });
 });
