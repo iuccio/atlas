1140d8f367a410308fa8c5d1b55d625fba7da7ff
atlas <19825793+iuccio@users.noreply.github.com>
2023-03-21 21:48:48 +0100

    ATLAS-1006: small ref

--- frontend/src/app/pages/tth/tth.module.ts
+++ frontend/src/app/pages/tth/tth.module.ts
@@ -5,7 +5,7 @@ import { FormModule } from '../../core/module/form.module';
 import { TimetableHearingOverviewComponent } from './overview/timetable-hearing-overview.component';
 import { CantonCardComponent } from './overview/canton-card/canton-card.component';
 import { TimetableHearingOverviewDetailComponent } from './timetable-hearing-overview-detail/timetable-hearing-overview-detail.component';
-import { TimetableHearingOverviewTabComponent } from './timetable-hearing-overview-tab/timetable-hearing-overview-tab/timetable-hearing-overview-tab.component';
+import { TimetableHearingOverviewTabComponent } from './timetable-hearing-overview-tab/timetable-hearing-overview-tab.component';
 
 @NgModule({
   declarations: [
