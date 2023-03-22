ae894b9f4b3d6f074edca66993f4e55026c25741
atlas <19825793+iuccio@users.noreply.github.com>
2023-03-22 10:57:06 +0100

    ATLAS-1006: add tab-heading component

---
+++ frontend/src/app/pages/tth/timetable-hearing-overview-tab/timetable-hearing-overview-tab-heading/timetable-hearing-overview-tab-heading.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from '@angular/core';
+import { TimetableHearingYear } from '../../../../api';
+import { Pages } from '../../../pages';
+
+@Component({
+  selector: 'app-timetable-hearing-overview-tab-heading',
+  templateUrl: './timetable-hearing-overview-tab-heading.component.html',
+  styleUrls: ['./timetable-hearing-overview-tab-heading.component.scss'],
+})
+export class TimetableHearingOverviewTabHeadingComponent {
+  @Input() cantonShort!: string;
+  @Input() foundTimetableHearingYear!: TimetableHearingYear;
+  @Input() hearingPlan!: string;
+
+  isHearingPlanActual() {
+    return this.hearingPlan === Pages.TTH_ACTUAL.path;
+  }
+
+  isHearingPlanPlanned() {
+    return this.hearingPlan === Pages.TTH_PLANNED.path;
+  }
+
+  isHearingPlanArchived() {
+    return this.hearingPlan === Pages.TTH_ARCHIVED.path;
+  }
+}
