2c1c00d559dc5a723d68a8090948077917774187
atlas <TechSupport-ATLAS@sbb.ch>
2023-03-23 14:13:44 +0100

    ATLAS-1006: get active/planned/archived statements

--- line-directory/src/main/java/ch/sbb/line/directory/repository/TimetableHearingYearRepository.java
+++ line-directory/src/main/java/ch/sbb/line/directory/repository/TimetableHearingYearRepository.java
@@ -1,13 +1,14 @@
 package ch.sbb.line.directory.repository;
 
-import ch.sbb.line.directory.entity.TimetableHearingYear;
 import ch.sbb.atlas.api.timetable.hearing.enumeration.HearingStatus;
+import ch.sbb.line.directory.entity.TimetableHearingYear;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TimetableHearingYearRepository extends JpaRepository<TimetableHearingYear, Long>, JpaSpecificationExecutor<TimetableHearingYear> {
+public interface TimetableHearingYearRepository extends JpaRepository<TimetableHearingYear, Long>,
+    JpaSpecificationExecutor<TimetableHearingYear> {
 
   default boolean hearingActive() {
     return existsByHearingStatus(HearingStatus.ACTIVE);
