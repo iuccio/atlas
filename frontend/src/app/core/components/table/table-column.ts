c4dbcbc0f2a4b71f3097ffbd24b00ea9d308ad92
atlas <19825793+iuccio@users.noreply.github.com>
2023-03-27 21:43:43 +0200

    ATLAS-1006: add dropdown to table component

--- frontend/src/app/core/components/table/table-column.ts
+++ frontend/src/app/core/components/table/table-column.ts
@@ -1,12 +1,22 @@
 export interface TableColumn<TYPE> {
   headerTitle: string;
   value?: keyof TYPE & string;
-  callback?: Function;
+  dropdown?: ColumnDropDown;
+  callback?: (...args: any[]) => any;
   valuePath?: string;
   columnDef?: string;
   formatAsDate?: boolean;
   translate?: {
     withPrefix?: string;
     withKey?: string;
+  };
+}
+
+export interface ColumnDropDown {
+  options: string[];
+  changeSelectionCallback: (...args: any[]) => any;
+  selectedOption: string;
+  translate: {
+    withPrefix: string;
   };
 }
