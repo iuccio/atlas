19a2c4ed45001b7194a6e0048f863ec6dc561a08
atlas <19825793+iuccio@users.noreply.github.com>
2023-03-27 11:42:11 +0200

    ATLAS-963: Prep frontend

--- user-administration/src/test/java/ch/sbb/atlas/user/administration/service/ClientCredentialAdministrationServiceTest.java
+++ user-administration/src/test/java/ch/sbb/atlas/user/administration/service/ClientCredentialAdministrationServiceTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import ch.sbb.atlas.api.user.administration.CantonPermissionRestrictionModel;
 import ch.sbb.atlas.api.user.administration.ClientCredentialPermissionCreateModel;
 import ch.sbb.atlas.api.user.administration.PermissionModel;
+import ch.sbb.atlas.api.user.administration.SboidPermissionRestrictionModel;
 import ch.sbb.atlas.kafka.model.SwissCanton;
 import ch.sbb.atlas.kafka.model.user.admin.ApplicationRole;
 import ch.sbb.atlas.kafka.model.user.admin.ApplicationType;
@@ -66,5 +67,90 @@ public class ClientCredentialAdministrationServiceTest {
     assertThat(savedPermission.getId()).isNotNull();
     assertThat(savedPermission.getPermissionRestrictions()).hasSize(1);
     assertThat(savedPermission.getPermissionRestrictions().iterator().next().getId()).isNotNull();
+  }
+
+  @Test
+  void shouldUpdateClientCredentialsWithRestrictions() {
+    // Given
+    ClientCredentialPermissionCreateModel client = ClientCredentialPermissionCreateModel.builder()
+        .clientCredentialId("18746f30-7978-48b5-b19b-0f871fb12e67")
+        .alias("Atlas Frontend Dev")
+        .permissions(List.of(PermissionModel.builder()
+            .role(ApplicationRole.SUPERVISOR)
+            .application(ApplicationType.TIMETABLE_HEARING)
+            .permissionRestrictions(List.of(new CantonPermissionRestrictionModel(SwissCanton.BERN)))
+            .build()))
+        .build();
+    clientCredentialAdministrationService.create(client);
+
+    ClientCredentialPermissionCreateModel clientUpdate = ClientCredentialPermissionCreateModel.builder()
+        .clientCredentialId("18746f30-7978-48b5-b19b-0f871fb12e67")
+        .alias("Atlas Frontend Dev")
+        .permissions(List.of(PermissionModel.builder()
+            .role(ApplicationRole.WRITER)
+            .application(ApplicationType.TIMETABLE_HEARING)
+            .permissionRestrictions(List.of(new CantonPermissionRestrictionModel(SwissCanton.URI),
+                new CantonPermissionRestrictionModel(SwissCanton.JURA)))
+            .build()))
+        .build();
+
+    // When
+    clientCredentialAdministrationService.update(clientUpdate.getClientCredentialId(), clientUpdate);
+    List<ClientCredentialPermission> savedPermissions =
+        clientCredentialAdministrationService.getClientCredentialPermission(
+            clientUpdate.getClientCredentialId());
+    assertThat(savedPermissions).isNotEmpty();
+    ClientCredentialPermission savedPermission = savedPermissions.get(0);
+    assertThat(savedPermission.getRole()).isEqualTo(ApplicationRole.WRITER);
+    assertThat(savedPermission.getPermissionRestrictions()).hasSize(2);
+  }
+
+  @Test
+  void shouldUpdateClientCredentialsWithAdditionalRestrictions() {
+    // Given
+    ClientCredentialPermissionCreateModel client = ClientCredentialPermissionCreateModel.builder()
+        .clientCredentialId("18746f30-7978-48b5-b19b-0f871fb12e67")
+        .alias("Atlas Frontend Dev")
+        .permissions(List.of(PermissionModel.builder()
+            .role(ApplicationRole.SUPERVISOR)
+            .application(ApplicationType.TIMETABLE_HEARING)
+            .permissionRestrictions(List.of(new CantonPermissionRestrictionModel(SwissCanton.BERN)))
+            .build()))
+        .build();
+    clientCredentialAdministrationService.create(client);
+
+    ClientCredentialPermissionCreateModel clientUpdate = ClientCredentialPermissionCreateModel.builder()
+        .clientCredentialId("18746f30-7978-48b5-b19b-0f871fb12e67")
+        .alias("Atlas Frontend Dev")
+        .permissions(List.of(PermissionModel.builder()
+                .role(ApplicationRole.WRITER)
+                .application(ApplicationType.TIMETABLE_HEARING)
+                .permissionRestrictions(List.of(new CantonPermissionRestrictionModel(SwissCanton.URI),
+                    new CantonPermissionRestrictionModel(SwissCanton.JURA)))
+                .build(),
+            PermissionModel.builder()
+                .role(ApplicationRole.WRITER)
+                .application(ApplicationType.LIDI)
+                .permissionRestrictions(List.of(new SboidPermissionRestrictionModel("ch:1:sboid:134123"),
+                    new SboidPermissionRestrictionModel("ch:1:sboid:234211")))
+                .build()))
+        .build();
+
+    // When
+    clientCredentialAdministrationService.update(clientUpdate.getClientCredentialId(), clientUpdate);
+    List<ClientCredentialPermission> savedPermissions =
+        clientCredentialAdministrationService.getClientCredentialPermission(
+            clientUpdate.getClientCredentialId());
+    assertThat(savedPermissions).isNotEmpty();
+
+    ClientCredentialPermission savedPermission = savedPermissions.get(0);
+    assertThat(savedPermission.getRole()).isEqualTo(ApplicationRole.WRITER);
+    assertThat(savedPermission.getApplication()).isEqualTo(ApplicationType.TIMETABLE_HEARING);
+    assertThat(savedPermission.getPermissionRestrictions()).hasSize(2);
+
+    savedPermission = savedPermissions.get(1);
+    assertThat(savedPermission.getRole()).isEqualTo(ApplicationRole.WRITER);
+    assertThat(savedPermission.getApplication()).isEqualTo(ApplicationType.LIDI);
+    assertThat(savedPermission.getPermissionRestrictions()).hasSize(2);
   }
 }
