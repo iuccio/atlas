9b9e673acca9ec4b5f6be38ccac055c2cbedcdca
atlas <TechSupport-ATLAS@sbb.ch>
2023-03-28 17:40:01 +0200

    ATLAS-1120: Small changes before merge to master

--- line-directory/src/test/java/ch/sbb/line/directory/helper/PdfFiles.java
+++ line-directory/src/test/java/ch/sbb/line/directory/helper/PdfFiles.java
@@ -6,18 +6,17 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.experimental.UtilityClass;
 import org.apache.commons.io.IOUtils;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
+@UtilityClass
 public final class PdfFiles {
 
-  public static List<MultipartFile> multipartFiles = multipartFiles();
+  public static final List<MultipartFile> MULTIPART_FILES = multipartFiles();
 
-  public static List<File> files = getFiles();
-
-  private PdfFiles() {
-  }
+  public static final List<File> FILES = getFiles();
 
   private static MultipartFile getMultipartFile(String pathName) {
     File file = new File(pathName);
@@ -37,31 +36,21 @@ public final class PdfFiles {
 
   private static List<MultipartFile> multipartFiles() {
     List<MultipartFile> multipartFiles = new ArrayList<>();
-    MultipartFile multipartFile = getMultipartFile("src/test/resources/pdf/dummy.pdf");
-    MultipartFile multipartFile1 = getMultipartFile("src/test/resources/pdf/dummy1.pdf");
-    MultipartFile multipartFile2 = getMultipartFile("src/test/resources/pdf/dummy2.pdf");
-    MultipartFile multipartFile3 = getMultipartFile("src/test/resources/pdf/dummy3.pdf");
-    multipartFiles.add(multipartFile);
-    multipartFiles.add(multipartFile1);
-    multipartFiles.add(multipartFile2);
-    multipartFiles.add(multipartFile3);
+    multipartFiles.add(getMultipartFile("src/test/resources/pdf/dummy.pdf"));
+    multipartFiles.add(getMultipartFile("src/test/resources/pdf/dummy1.pdf"));
+    multipartFiles.add(getMultipartFile("src/test/resources/pdf/dummy2.pdf"));
+    multipartFiles.add(getMultipartFile("src/test/resources/pdf/dummy3.pdf"));
     return multipartFiles;
   }
 
   private static List<File> getFiles() {
     List<File> files = new ArrayList<>();
-    File file = new File("src/test/resources/pdf/dummy.pdf");
-    File file1 = new File("src/test/resources/pdf/dummy1.pdf");
-    File file2 = new File("src/test/resources/pdf/dummy2.pdf");
-    File file3 = new File("src/test/resources/pdf/dummy3.pdf");
-    File file4 = new File("src/test/resources/pdf/test.txt");
-    File file5 = new File("src/test/resources/pdf/test1.txt");
-    files.add(file);
-    files.add(file1);
-    files.add(file2);
-    files.add(file3);
-    files.add(file4);
-    files.add(file5);
+    files.add(new File("src/test/resources/pdf/dummy.pdf"));
+    files.add(new File("src/test/resources/pdf/dummy1.pdf"));
+    files.add(new File("src/test/resources/pdf/dummy2.pdf"));
+    files.add(new File("src/test/resources/pdf/dummy3.pdf"));
+    files.add(new File("src/test/resources/pdf/test.txt"));
+    files.add(new File("src/test/resources/pdf/test1.txt"));
     return files;
   }
 }
