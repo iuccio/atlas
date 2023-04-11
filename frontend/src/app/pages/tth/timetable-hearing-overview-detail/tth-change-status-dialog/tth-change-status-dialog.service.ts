b45a1ccb037c5276b6b252334659e4ca4482cc47
atlas <TechSupport-ATLAS@sbb.ch>
2023-04-11 18:05:24 +0200

    ATLAS-1014: impl changeStatus dialog

---
+++ frontend/src/app/pages/tth/timetable-hearing-overview-detail/tth-change-status-dialog/tth-change-status-dialog.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { TthChangeStatusDialogComponent } from './tth-change-status-dialog.component';
+import { ColumnDropDownEvent } from '../../../../core/components/table/column-drop-down-event';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TthChangeStatusDialogService {
+  private changeStatusDialog?: MatDialogRef<TthChangeStatusDialogComponent>;
+
+  constructor(private dialog: MatDialog) {}
+
+  onClick(changedStatus: ColumnDropDownEvent): Observable<boolean> {
+    this.changeStatusDialog = this.dialog.open(TthChangeStatusDialogComponent, {
+      data: {
+        title: 'TTH.DIALOG.STATUS_CHANGE',
+        message: 'TODO',
+        cancelText: 'TTH.DIALOG.BACK',
+        confirmText: 'TTH.DIALOG.STATUS_CHANGE',
+        id: changedStatus.value.id,
+        ths: changedStatus.value,
+      },
+      panelClass: 'atlas-dialog-panel',
+      backdropClass: 'atlas-dialog-backdrop',
+    });
+
+    return this.changeStatusDialog.afterClosed().pipe(map((value) => (value ? value : true)));
+  }
+
+  closeConfirmDialog(): void {
+    this.changeStatusDialog?.close();
+    this.changeStatusDialog = undefined;
+  }
+}
