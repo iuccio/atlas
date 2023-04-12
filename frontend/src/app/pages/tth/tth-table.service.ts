63013acb0ebb4cd8f64ce59eff9821aa0d6325ac
atlas <19825793+iuccio@users.noreply.github.com>
2023-04-12 14:05:18 +0200

    ATLAS-772: pr update: tth-table-service without router dependency

--- frontend/src/app/pages/tth/tth-table.service.ts
+++ frontend/src/app/pages/tth/tth-table.service.ts
@@ -1,40 +1,16 @@
-import { Injectable, OnDestroy } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { TableService } from '../../core/components/table/table.service';
-import { NavigationEnd, Router } from '@angular/router';
-import { filter } from 'rxjs/operators';
-import { Pages } from '../pages';
-import { Subscription } from 'rxjs';
+import { Page } from '../../core/model/page';
 
 @Injectable()
-export class TthTableService extends TableService implements OnDestroy {
-  private readonly tabRouteRegex: RegExp = new RegExp(
-    `(${Pages.TTH_ACTIVE.path}|${Pages.TTH_PLANNED.path}|${Pages.TTH_ARCHIVED.path})`
-  );
-  private oldTabPath: string | null;
-  private navigationEndSubscription: Subscription;
+export class TthTableService extends TableService {
+  private _activeTabPage?: Page;
 
-  constructor(private readonly router: Router) {
-    super();
-    this.oldTabPath = this.getTabPathFromRegexMatches(this.router.url.match(this.tabRouteRegex));
-    this.navigationEndSubscription = this.router.events
-      .pipe(
-        filter((routerEvent): routerEvent is NavigationEnd => routerEvent instanceof NavigationEnd)
-      )
-      .subscribe((navigationEnd) => {
-        // compare newTabPath to oldTabPath: if different => reset table settings
-        const matches: RegExpMatchArray | null = navigationEnd.urlAfterRedirects.match(
-          this.tabRouteRegex
-        );
-        const newTabPath: string | null = this.getTabPathFromRegexMatches(matches);
-        if (this.oldTabPath !== newTabPath) {
-          this.resetTableSettings();
-          this.oldTabPath = newTabPath;
-        }
-      });
-  }
-
-  ngOnDestroy(): void {
-    this.navigationEndSubscription.unsubscribe();
+  set activeTabPage(page: Page) {
+    if (this._activeTabPage !== page) {
+      this.resetTableSettings();
+      this._activeTabPage = page;
+    }
   }
 
   private resetTableSettings(): void {
@@ -42,9 +18,5 @@ export class TthTableService extends TableService implements OnDestroy {
     this.pageIndex = 0;
     this.sortActive = '';
     this.sortDirection = 'asc';
-  }
-
-  private getTabPathFromRegexMatches(matches: RegExpMatchArray | null): string | null {
-    return matches ? matches['0'] : null;
   }
 }
