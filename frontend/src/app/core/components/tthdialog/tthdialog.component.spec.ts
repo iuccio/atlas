7223de62c6244a8e7efeea505e5c7b52a6af9177
atlas <TechSupport-ATLAS@sbb.ch>
2023-04-12 10:51:06 +0200

    ATLAS-1012: Add missing tests.

--- frontend/src/app/core/components/tthdialog/tthdialog.component.spec.ts
+++ frontend/src/app/core/components/tthdialog/tthdialog.component.spec.ts
@@ -3,16 +3,17 @@ import { AppTestingModule } from '../../../app.testing.module';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { By } from '@angular/platform-browser';
 import { TthDialogComponent } from './tthdialog.component';
-import { TimetableHearingYear } from '../../../api';
+import {
+  ContainerTimetableHearingYear,
+  TimetableHearingService,
+  TimetableHearingYear,
+} from '../../../api';
 import moment from 'moment/moment';
+import { of } from 'rxjs';
 
-// const mockTimetableHearingService = jasmine.createSpyObj(
-//   'timetableHearingService',
-//   ['getHearingYears']
-//   // {
-//   //   statusChoices: [ 'ACTIVE', 'PLANNED' ]
-//   //   }
-// );
+const mockTimetableHearingService = jasmine.createSpyObj('timetableHearingService', [
+  'getHearingYears',
+]);
 
 describe('TthDialogComponent', () => {
   let tthDialogComponent: TthDialogComponent;
@@ -47,20 +48,24 @@ describe('TthDialogComponent', () => {
     return [currentTTHY, nextTTHY, tthyIn2Years, tthIn3Years];
   };
 
+  const hearingContainer: ContainerTimetableHearingYear = {
+    objects: getTimetableHearingYears(),
+    totalCount: 4,
+  };
+
   function getCurrentYear() {
     return new Date().getFullYear();
   }
 
   beforeEach(async () => {
-    // mockTimetableHearingService.getHearingYears.and.returnValue(of(getTimetableHearingYears));
-    // mockTimetableHearingService.getHearingYears.withArgs('statusChoices').and.returnValue(of(getTimetableHearingYears));
+    mockTimetableHearingService.getHearingYears.and.returnValue(of(hearingContainer));
     await TestBed.configureTestingModule({
       declarations: [TthDialogComponent],
       imports: [AppTestingModule],
       providers: [
         { provide: MAT_DIALOG_DATA, useValue: { title: 'Title' } },
         { provide: MatDialogRef, useValue: {} },
-        // { provide: TimetableHearingService, useValue: mockTimetableHearingService },
+        { provide: TimetableHearingService, useValue: mockTimetableHearingService },
       ],
     }).compileComponents();
   });
@@ -90,23 +95,23 @@ describe('TthDialogComponent', () => {
     expect(matDialogActions.nativeElement).toBeTruthy();
   });
 
+  it('should get year options and default year selection', () => {
+    const currentYear = getCurrentYear();
+    tthDialogComponent.initOverviewOfferedYears();
+    expect(tthDialogComponent.YEAR_OPTIONS).toEqual([
+      currentYear + 4,
+      currentYear + 5,
+      currentYear + 6,
+      currentYear + 7,
+      currentYear + 8,
+    ]);
+    expect(tthDialogComponent.defaultYearSelection).toEqual(currentYear + 4);
+  });
+
   it('should get active year', () => {
     const timetableHearingYears: TimetableHearingYear[] = getTimetableHearingYears();
     expect(tthDialogComponent.getActiveYear(timetableHearingYears)).toEqual(getCurrentYear());
   });
-
-  // it('new test', () => {
-  //   const currentYear = getCurrentYear();
-  //   tthDialogComponent.initOverviewOfferedYears();
-  //   expect(tthDialogComponent.YEAR_OPTIONS).toEqual([
-  //     currentYear + 4,
-  //     currentYear + 5,
-  //     currentYear + 6,
-  //     currentYear + 7,
-  //     currentYear + 8,
-  //   ]);
-  //   expect(tthDialogComponent.defaultYearSelection).toEqual(currentYear+4)
-  // });
 
   it('should get planned years', () => {
     const timetableHearingYears: TimetableHearingYear[] = getTimetableHearingYears();
