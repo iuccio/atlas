1f75648d67799193e88eb165977832faef1ffcd3
atlas <19825793+iuccio@users.noreply.github.com>
2023-04-21 09:27:53 +0200

    ATLAS-1170: Add tests for property resolving

--- line-directory/src/test/java/ch/sbb/line/directory/controller/TimetableHearingStatementControllerTest.java
+++ line-directory/src/test/java/ch/sbb/line/directory/controller/TimetableHearingStatementControllerTest.java
@@ -1,10 +1,13 @@
 package ch.sbb.line.directory.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import ch.sbb.atlas.amazon.service.FileService;
+import ch.sbb.atlas.api.timetable.hearing.TimetableHearingStatementRequestParams;
 import ch.sbb.line.directory.service.hearing.ResponsibleTransportCompaniesResolverService;
 import ch.sbb.line.directory.service.hearing.TimetableFieldNumberResolverService;
 import ch.sbb.line.directory.service.hearing.TimetableHearingStatementService;
@@ -16,6 +19,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.context.MessageSource;
+import org.springframework.web.server.ResponseStatusException;
 
 public class TimetableHearingStatementControllerTest {
 
@@ -50,5 +54,28 @@ public class TimetableHearingStatementControllerTest {
     timetableHearingStatementController.getResponsibleTransportCompanies("ttfnid", 2024L);
 
     verify(responsibleTransportCompaniesResolverService).getResponsibleTransportCompanies("ttfnid", LocalDate.of(2024, 1, 1));
+  }
+
+  @Test
+  void shouldRequireTimetableHearingYear() {
+    TimetableHearingStatementRequestParams params = TimetableHearingStatementRequestParams.builder()
+        .build();
+
+    ResponseStatusException exception = assertThrows(ResponseStatusException.class,
+        () -> timetableHearingStatementController.getStatementsAsCsv("de", params));
+
+    assertThat(exception.getReason()).isEqualTo("timetableHearingYear is mandatory here");
+  }
+
+  @Test
+  void shouldRequireLanguage() {
+    TimetableHearingStatementRequestParams params = TimetableHearingStatementRequestParams.builder()
+        .timetableHearingYear(2023L)
+        .build();
+
+    ResponseStatusException exception = assertThrows(ResponseStatusException.class,
+        () -> timetableHearingStatementController.getStatementsAsCsv("en", params));
+
+    assertThat(exception.getReason()).isEqualTo("Language must be either de,fr,it");
   }
 }
