57eca3a923168d2551a193b5c631a6a87b7a07c2
atlas <19825793+iuccio@users.noreply.github.com>
2023-04-24 10:49:13 +0200

    ATLAS-1180: wait for permissions to be loaded

--- frontend/src/app/core/auth/guards/timetable-hearing-guard.service.ts
+++ frontend/src/app/core/auth/guards/timetable-hearing-guard.service.ts
@@ -1,17 +1,28 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, Router, UrlTree } from '@angular/router';
+import { inject, Injectable } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { filter, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
-export class TimetableHearingGuard implements CanActivate {
+export class TimetableHearingGuard {
   constructor(private readonly authService: AuthService, private readonly router: Router) {}
 
-  canActivate(): boolean | UrlTree {
-    if (this.authService.mayAccessTimetableHearing()) {
-      return true;
-    }
-    return this.router.parseUrl('/');
+  canActivate() {
+    return this.authService.permissionsLoaded.pipe(
+      filter((loaded) => loaded),
+      map(() => {
+        if (this.authService.mayAccessTimetableHearing()) {
+          return true;
+        } else {
+          return this.router.parseUrl('/');
+        }
+      })
+    );
   }
 }
+
+export const canActivateTimetableHearing: CanActivateFn = () => {
+  return inject(TimetableHearingGuard).canActivate();
+};
