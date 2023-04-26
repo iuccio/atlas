336fb7c579dab3be1cf2c0e877f8cc74f4fe0bf9
atlas <TechSupport-ATLAS@sbb.ch>
2023-04-26 17:02:55 +0200

    ATLAS-1016: Initial fileupload component

---
+++ frontend/src/app/core/components/file-upload/file/file-size.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'fileSize',
+})
+export class FileSizePipe implements PipeTransform {
+  transform(size: number): string {
+    return this.formatBytes(size, 2);
+  }
+
+  formatBytes(bytes: number, decimals: number) {
+    if (bytes === 0) {
+      return '0 Bytes';
+    }
+    const k = 1024;
+    const dm = decimals <= 0 ? 0 : decimals || 2;
+
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+  }
+}
