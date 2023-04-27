9490d0a7570d07c91d4b4d215d51501e923d5c41
atlas <TechSupport-ATLAS@sbb.ch>
2023-04-27 11:04:02 +0200

    ATLAS-1009: reset checkbox, fix table pagination

--- frontend/src/app/core/components/table/table.service.ts
+++ frontend/src/app/core/components/table/table.service.ts
@@ -4,36 +4,42 @@ import { SortDirection } from '@angular/material/sort';
 @Injectable()
 export class TableService {
   private _pageSize = 10;
-  private _pageIndex = 0;
-
-  // sort
-  private _sortActive = '';
-  private _sortDirection: SortDirection = 'asc';
 
   get pageSize(): number {
     return this._pageSize;
   }
+
   set pageSize(pageSize: number) {
     this._pageSize = pageSize;
   }
 
+  private _pageIndex = 0;
+
   get pageIndex(): number {
     return this._pageIndex;
   }
+
   set pageIndex(pageIndex: number) {
     this._pageIndex = pageIndex;
   }
 
+  // sort
+  private _sortActive = '';
+
   get sortActive(): string {
     return this._sortActive;
   }
+
   set sortActive(activeSort: string) {
     this._sortActive = activeSort;
   }
 
+  private _sortDirection: SortDirection = 'asc';
+
   get sortDirection(): SortDirection {
     return this._sortDirection;
   }
+
   set sortDirection(direction: SortDirection) {
     this._sortDirection = direction;
   }
