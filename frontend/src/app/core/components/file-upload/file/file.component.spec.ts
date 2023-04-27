a9aaa0f37cb419f30e1f1c526038fd898009a408
atlas <TechSupport-ATLAS@sbb.ch>
2023-04-27 11:38:44 +0200

    ATLAS-1016: Fileupload component tests

--- frontend/src/app/core/components/file-upload/file/file.component.spec.ts
+++ frontend/src/app/core/components/file-upload/file/file.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FileComponent } from './file.component';
 import { AppTestingModule } from '../../../../app.testing.module';
+import { FileSizePipe } from '../file-size/file-size.pipe';
 
 describe('FileComponent', () => {
   let component: FileComponent;
@@ -9,7 +10,7 @@ describe('FileComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [FileComponent],
+      declarations: [FileComponent, FileSizePipe],
       imports: [AppTestingModule],
     }).compileComponents();
   });
@@ -17,6 +18,11 @@ describe('FileComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(FileComponent);
     component = fixture.componentInstance;
+    component.file = {
+      name: 'filename.pdf',
+      size: 10,
+      type: 'application/pdf',
+    } as File;
     fixture.detectChanges();
   });
 
