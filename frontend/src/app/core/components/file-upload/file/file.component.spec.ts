ed320d5079a83219950c852837dc15788f341ec7
atlas <19825793+iuccio@users.noreply.github.com>
2023-04-28 10:53:33 +0200

    ATLAS-1016: frontend tests

--- frontend/src/app/core/components/file-upload/file/file.component.spec.ts
+++ frontend/src/app/core/components/file-upload/file/file.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FileComponent } from './file.component';
 import { AppTestingModule } from '../../../../app.testing.module';
 import { FileSizePipe } from '../file-size/file-size.pipe';
+import { By } from '@angular/platform-browser';
 
 describe('FileComponent', () => {
   let component: FileComponent;
@@ -23,12 +24,32 @@ describe('FileComponent', () => {
       size: 10,
       type: 'application/pdf',
     } as File;
+    component.downloadEnabled = true;
+    component.deleteEnabled = true;
     fixture.detectChanges();
   });
 
-  describe('Component Rendering', () => {
-    it('should create', () => {
-      expect(component).toBeTruthy();
-    });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should trigger delete', () => {
+    const trashIcon = fixture.debugElement.query(By.css('.bi-trash'));
+    expect(trashIcon).toBeTruthy();
+
+    spyOn(component.fileDeleted, 'emit');
+
+    trashIcon.nativeElement.click();
+    expect(component.fileDeleted.emit).toHaveBeenCalled();
+  });
+
+  it('should trigger download', () => {
+    const downloadIcon = fixture.debugElement.query(By.css('.bi-download'));
+    expect(downloadIcon).toBeTruthy();
+
+    spyOn(component.downloadFile, 'emit');
+
+    downloadIcon.nativeElement.click();
+    expect(component.downloadFile.emit).toHaveBeenCalled();
   });
 });
