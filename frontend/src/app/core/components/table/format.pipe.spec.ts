935141f7042232ea0d3cdad37c6d61c63e6bb19b
atlas <19825793+iuccio@users.noreply.github.com>
2023-05-04 20:14:19 +0200

    ATLAS-1155: implemented feature

---
+++ frontend/src/app/core/components/table/format.pipe.spec.ts
@@ -0,0 +1,50 @@
+import { FormatPipe } from './format.pipe';
+import SpyObj = jasmine.SpyObj;
+import { TranslatePipe } from '@ngx-translate/core';
+import { TableColumn } from './table-column';
+
+describe('FormatPipe', () => {
+  let translatePipeMock: SpyObj<TranslatePipe>;
+  let formatPipe: FormatPipe;
+
+  beforeEach(() => {
+    translatePipeMock = jasmine.createSpyObj<TranslatePipe>('TranslatePipeSpy', ['transform']);
+    formatPipe = new FormatPipe(translatePipeMock);
+  });
+
+  it('create an instance', () => {
+    expect(formatPipe).toBeTruthy();
+  });
+
+  it('should format as Date', () => {
+    const date = new Date(2023, 0, 1);
+    const tableColumn: TableColumn<object> = { formatAsDate: true } as TableColumn<object>;
+    const formatted = formatPipe.transform(date, tableColumn);
+    expect(formatted).toEqual('01.01.2023');
+  });
+
+  it('should translate withPrefix', () => {
+    const value = 'test';
+    const tableColumn: TableColumn<object> = {
+      translate: {
+        withPrefix: 'prefix.',
+      },
+    } as TableColumn<object>;
+    formatPipe.transform(value, tableColumn);
+    expect(translatePipeMock.transform).toHaveBeenCalledOnceWith('prefix.test');
+  });
+
+  it('should call column callback', () => {
+    const value = 'test';
+    const tableColumn = jasmine.createSpyObj('TableColumn', ['callback']);
+
+    formatPipe.transform(value, tableColumn);
+    expect(tableColumn.callback).toHaveBeenCalledOnceWith('test');
+  });
+
+  it('should only return value when no condition matches', () => {
+    const value = 'test';
+    const formatted = formatPipe.transform(value, {} as TableColumn<object>);
+    expect(formatted).toEqual('test');
+  });
+});
---
+++ frontend/src/app/core/components/table/mouse-over-title.directive.spec.ts
@@ -0,0 +1,8 @@
+import { MouseOverTitleDirective } from './mouse-over-title.directive';
+
+describe('MouseOverTitleDirective', () => {
+  it('should create an instance', () => {
+    const directive = new MouseOverTitleDirective();
+    expect(directive).toBeTruthy();
+  });
+});
