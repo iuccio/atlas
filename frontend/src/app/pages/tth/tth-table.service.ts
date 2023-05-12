c28e5ca52bcb3d530bfbe7f9360b2fc92d785a0b
atlas <19825793+iuccio@users.noreply.github.com>
2023-05-12 13:20:38 +0200

    ATLAS-1258: quick fix for table settings in fplA

--- frontend/src/app/pages/tth/tth-table.service.ts
+++ frontend/src/app/pages/tth/tth-table.service.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@angular/core';
 import { TableService } from '../../core/components/table/table.service';
 import { Page } from '../../core/model/page';
+import {
+  copyOverviewDetailFilterConfig,
+  OverviewDetailTableFilterConfigType,
+} from './overview-detail/overview-detail-table-filter-config';
 
 @Injectable()
 export class TthTableService extends TableService {
   private _activeTabPage?: Page;
+  private _overviewDetailFilterConfig: OverviewDetailTableFilterConfigType =
+    copyOverviewDetailFilterConfig();
+
+  get overviewDetailFilterConfig() {
+    return this._overviewDetailFilterConfig;
+  }
 
   set activeTabPage(page: Page) {
     if (this._activeTabPage !== page) {
       this.resetTableSettings();
+      this._overviewDetailFilterConfig = copyOverviewDetailFilterConfig();
       this._activeTabPage = page;
     }
   }
