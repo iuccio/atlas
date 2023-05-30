b083ccec4e692af76e8613929a3fda83a12abecc
atlas <19825793+iuccio@users.noreply.github.com>
2023-05-30 15:22:41 +0200

    ATLAS-1096: Introduce superclass for versioned date params

--- base-atlas/src/main/java/ch/sbb/atlas/api/bodi/BusinessOrganisationVersionRequestParams.java
+++ base-atlas/src/main/java/ch/sbb/atlas/api/bodi/BusinessOrganisationVersionRequestParams.java
@@ -1,10 +1,8 @@
 package ch.sbb.atlas.api.bodi;
 
-import ch.sbb.atlas.api.AtlasApiConstants;
+import ch.sbb.atlas.api.model.VersionedObjectDateRequestParams;
 import ch.sbb.atlas.model.Status;
 import io.swagger.v3.oas.annotations.Parameter;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -14,7 +12,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.Singular;
 import lombok.ToString;
-import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
 @Setter
@@ -22,7 +19,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 @AllArgsConstructor
 @Builder
 @ToString
-public class BusinessOrganisationVersionRequestParams {
+public class BusinessOrganisationVersionRequestParams extends VersionedObjectDateRequestParams {
 
   @Parameter(description = "Search criteria strings will be looked up in specific columns")
   @Singular(value = "searchCriteria", ignoreNullCollections = true)
@@ -35,25 +32,5 @@ public class BusinessOrganisationVersionRequestParams {
   @Parameter(description = "Status based restriction", example = "85")
   @Singular(ignoreNullCollections = true)
   private List<Status> statusChoices = new ArrayList<>();
-
-  @Parameter(description = "ValidOn. Date format: " + AtlasApiConstants.DATE_FORMAT_PATTERN)
-  @DateTimeFormat(pattern = AtlasApiConstants.DATE_FORMAT_PATTERN)
-  private LocalDate validOn;
-
-  @Parameter(description = "[fromDate] >= validFrom. Date format: " + AtlasApiConstants.DATE_FORMAT_PATTERN)
-  @DateTimeFormat(pattern = AtlasApiConstants.DATE_FORMAT_PATTERN)
-  private LocalDate fromDate;
-
-  @Parameter(description = "[toDate] <= validTo. Date format: " + AtlasApiConstants.DATE_FORMAT_PATTERN)
-  @DateTimeFormat(pattern = AtlasApiConstants.DATE_FORMAT_PATTERN)
-  private LocalDate toDate;
-
-  @Parameter(description = "creationDate >= [createdAfter]. DateTime format: " + AtlasApiConstants.DATE_TIME_FORMAT_PATTERN)
-  @DateTimeFormat(pattern = AtlasApiConstants.DATE_TIME_FORMAT_PATTERN)
-  private LocalDateTime createdAfter;
-
-  @Parameter(description = "editionDate >= [modifiedAfter]. DateTime format: " + AtlasApiConstants.DATE_TIME_FORMAT_PATTERN)
-  @DateTimeFormat(pattern = AtlasApiConstants.DATE_TIME_FORMAT_PATTERN)
-  private LocalDateTime modifiedAfter;
 
 }
