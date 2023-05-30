f17529a954b18675cbb028ba4d3d86f2652a7ccb
atlas <TechSupport-ATLAS@sbb.ch>
2023-05-30 15:22:41 +0200

    ATLAS-1096: Introduce superclass for versioned date params

---
+++ base-atlas/src/main/java/ch/sbb/atlas/api/model/VersionedObjectDateRequestParams.java
@@ -0,0 +1,30 @@
+package ch.sbb.atlas.api.model;
+
+import ch.sbb.atlas.api.AtlasApiConstants;
+import io.swagger.v3.oas.annotations.Parameter;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class VersionedObjectDateRequestParams {
+
+  @Parameter(description = "ValidOn. Date format: " + AtlasApiConstants.DATE_FORMAT_PATTERN)
+  @DateTimeFormat(pattern = AtlasApiConstants.DATE_FORMAT_PATTERN)
+  private LocalDate validOn;
+
+  @Parameter(description = "[fromDate] >= validFrom. Date format: " + AtlasApiConstants.DATE_FORMAT_PATTERN)
+  @DateTimeFormat(pattern = AtlasApiConstants.DATE_FORMAT_PATTERN)
+  private LocalDate fromDate;
+
+  @Parameter(description = "[toDate] <= validTo. Date format: " + AtlasApiConstants.DATE_FORMAT_PATTERN)
+  @DateTimeFormat(pattern = AtlasApiConstants.DATE_FORMAT_PATTERN)
+  private LocalDate toDate;
+
+  @Parameter(description = "creationDate >= [createdAfter]. DateTime format: " + AtlasApiConstants.DATE_TIME_FORMAT_PATTERN)
+  @DateTimeFormat(pattern = AtlasApiConstants.DATE_TIME_FORMAT_PATTERN)
+  private LocalDateTime createdAfter;
+
+  @Parameter(description = "editionDate >= [modifiedAfter]. DateTime format: " + AtlasApiConstants.DATE_TIME_FORMAT_PATTERN)
+  @DateTimeFormat(pattern = AtlasApiConstants.DATE_TIME_FORMAT_PATTERN)
+  private LocalDateTime modifiedAfter;
+}
