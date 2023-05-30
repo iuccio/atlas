66ff19e4cd98f02343bc9b2415a4c38ebb97520a
atlas <TechSupport-ATLAS@sbb.ch>
2023-05-30 15:32:32 +0200

    ATLAS-1096: fix compilation

--- base-atlas/src/main/java/ch/sbb/atlas/api/model/VersionedObjectDateRequestParams.java
+++ base-atlas/src/main/java/ch/sbb/atlas/api/model/VersionedObjectDateRequestParams.java
@@ -4,8 +4,18 @@ import ch.sbb.atlas.api.AtlasApiConstants;
 import io.swagger.v3.oas.annotations.Parameter;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 import org.springframework.format.annotation.DateTimeFormat;
 
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
 public class VersionedObjectDateRequestParams {
 
   @Parameter(description = "ValidOn. Date format: " + AtlasApiConstants.DATE_FORMAT_PATTERN)
