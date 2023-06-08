c003675730b21b6973c7b8b34bd83ad99cb9887b
atlas <19825793+iuccio@users.noreply.github.com>
2023-06-08 10:56:39 +0200

    ATLAS-1026: Validation für BusinessOrganisation

---
+++ line-directory/src/main/java/ch/sbb/line/directory/service/TimetableFieldNumberValidationService.java
@@ -0,0 +1,41 @@
+package ch.sbb.line.directory.service;
+
+import ch.sbb.atlas.business.organisation.service.SharedBusinessOrganisationService;
+import ch.sbb.atlas.kafka.model.Status;
+import ch.sbb.line.directory.entity.TimetableFieldNumberVersion;
+import ch.sbb.line.directory.exception.TimetableFieldNumberConflictException;
+import ch.sbb.line.directory.repository.TimetableFieldNumberVersionRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TimetableFieldNumberValidationService {
+
+
+  private final TimetableFieldNumberVersionRepository versionRepository;
+  private final SharedBusinessOrganisationService sharedBusinessOrganisationService;
+
+  public void validatePreSave(TimetableFieldNumberVersion newVersion) {
+    validateNoOverlapsOnNumberAndSttfn(newVersion);
+    sharedBusinessOrganisationService.validateSboidExists(newVersion.getBusinessOrganisation());
+  }
+
+  private void validateNoOverlapsOnNumberAndSttfn(TimetableFieldNumberVersion newVersion) {
+    List<TimetableFieldNumberVersion> overlappingVersions = getOverlapsOnNumberAndSttfn(newVersion);
+    if (!overlappingVersions.isEmpty()) {
+      throw new TimetableFieldNumberConflictException(newVersion, overlappingVersions);
+    }
+  }
+
+  private List<TimetableFieldNumberVersion> getOverlapsOnNumberAndSttfn(TimetableFieldNumberVersion version) {
+    String ttfnid = version.getTtfnid() == null ? "" : version.getTtfnid();
+    return versionRepository.getAllByNumberOrSwissTimetableFieldNumberWithValidityOverlap(
+            version.getNumber(), version.getSwissTimetableFieldNumber().toLowerCase(),
+            version.getValidFrom(), version.getValidTo(), ttfnid).stream()
+        .filter(i -> i.getStatus() != Status.REVOKED)
+        .toList();
+  }
+
+}
