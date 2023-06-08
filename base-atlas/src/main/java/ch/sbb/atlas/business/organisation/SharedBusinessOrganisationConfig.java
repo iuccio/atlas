23007ae3669e073341019e37003ebe03d5f0225b
atlas <19825793+iuccio@users.noreply.github.com>
2023-06-08 16:17:21 +0200

    ATLAS-1294: Try to fix context

--- base-atlas/src/main/java/ch/sbb/atlas/business/organisation/SharedBusinessOrganisationConfig.java
+++ base-atlas/src/main/java/ch/sbb/atlas/business/organisation/SharedBusinessOrganisationConfig.java
@@ -1,10 +1,19 @@
 package ch.sbb.atlas.business.organisation;
 
+import ch.sbb.atlas.business.organisation.repository.SharedBusinessOrganisationVersionRepository;
+import ch.sbb.atlas.business.organisation.service.SharedBusinessOrganisationConsumer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
 public class SharedBusinessOrganisationConfig {
+
+  @Bean
+  public SharedBusinessOrganisationConsumer sharedBusinessOrganisationConsumer(
+      SharedBusinessOrganisationVersionRepository sharedBusinessOrganisationVersionRepository) {
+    return new SharedBusinessOrganisationConsumer(sharedBusinessOrganisationVersionRepository);
+  }
 
 }
--- base-atlas/src/main/java/ch/sbb/atlas/business/organisation/service/SharedBusinessOrganisationConsumer.java
+++ base-atlas/src/main/java/ch/sbb/atlas/business/organisation/service/SharedBusinessOrganisationConsumer.java
@@ -3,25 +3,21 @@ package ch.sbb.atlas.business.organisation.service;
 import ch.sbb.atlas.business.organisation.entity.SharedBusinessOrganisationVersion;
 import ch.sbb.atlas.business.organisation.repository.SharedBusinessOrganisationVersionRepository;
 import ch.sbb.atlas.kafka.model.business.organisation.SharedBusinessOrganisationUpdate;
-import ch.sbb.atlas.kafka.model.business.organisation.UpdateAction;
 import ch.sbb.atlas.kafka.model.business.organisation.SharedBusinessOrganisationVersionModel;
+import ch.sbb.atlas.kafka.model.business.organisation.UpdateAction;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-@Component
 @RequiredArgsConstructor
 @Slf4j
 @Transactional
-@KafkaListener(topics = "${kafka.atlas.business.organisation.topic}", groupId = "${kafka.atlas.business.organisation.groupId}")
 public class SharedBusinessOrganisationConsumer {
 
   private final SharedBusinessOrganisationVersionRepository sharedBusinessOrganisationVersionRepository;
 
-  @KafkaHandler
+  @KafkaListener(topics = "${kafka.atlas.business.organisation.topic}", groupId = "${kafka.atlas.business.organisation.groupId}")
   public void readBusinessOrganisationsFromKafka(SharedBusinessOrganisationUpdate modelUpdate) {
     if (modelUpdate.getAction() == UpdateAction.SAVE) {
       sharedBusinessOrganisationVersionRepository.save(toEntity(modelUpdate.getModel()));
