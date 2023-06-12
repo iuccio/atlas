6fc3246734cfeaf7fa7c0a8f3e8c8b5c058a6675
atlas <19825793+iuccio@users.noreply.github.com>
2023-06-12 14:14:46 +0200

    ATLAS-1294: Start tc consumer in lidi

--- line-directory/src/main/java/ch/sbb/line/directory/configuration/AtlasConfig.java
+++ line-directory/src/main/java/ch/sbb/line/directory/configuration/AtlasConfig.java
@@ -4,13 +4,14 @@ import ch.sbb.atlas.amazon.service.FileService;
 import ch.sbb.atlas.amazon.service.FileServiceImpl;
 import ch.sbb.atlas.business.organisation.SharedBusinessOrganisationConfig;
 import ch.sbb.atlas.configuration.handler.AtlasExceptionHandler;
+import ch.sbb.atlas.transport.company.SharedTransportCompanyConfig;
 import ch.sbb.atlas.versioning.service.VersionableService;
 import ch.sbb.atlas.versioning.service.VersionableServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-@Import(SharedBusinessOrganisationConfig.class)
+@Import({SharedBusinessOrganisationConfig.class, SharedTransportCompanyConfig.class})
 @Configuration
 public class AtlasConfig {
 
