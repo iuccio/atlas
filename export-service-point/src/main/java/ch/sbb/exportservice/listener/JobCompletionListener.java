8d4e6af62245b684d0583322efb81c9fa19e8f43
atlas <19825793+iuccio@users.noreply.github.com>
2023-06-19 12:16:05 +0200

    ATLAS-1291: send only failure notification

--- export-service-point/src/main/java/ch/sbb/exportservice/listener/JobCompletionListener.java
+++ export-service-point/src/main/java/ch/sbb/exportservice/listener/JobCompletionListener.java
@@ -3,10 +3,9 @@ package ch.sbb.exportservice.listener;
 import ch.sbb.atlas.kafka.model.mail.MailNotification;
 import ch.sbb.exportservice.service.MailNotificationService;
 import ch.sbb.exportservice.service.MailProducerService;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
@@ -28,28 +27,16 @@ public class JobCompletionListener implements JobExecutionListener {
 
   @Override
   public void afterJob(JobExecution jobExecution) {
-    StepExecution stepExecution = jobExecution.getStepExecutions().stream().findFirst().get();
-    if (ExitStatus.COMPLETED.equals(jobExecution.getExitStatus())) {
-      sendSuccessfullyNotification(stepExecution);
-    }
     if (ExitStatus.FAILED.equals(jobExecution.getExitStatus())) {
-      sendUnsuccessffulyNotification(stepExecution);
+      StepExecution failedStepExecution = jobExecution.getStepExecutions().stream()
+          .filter(stepExecution -> stepExecution.getStatus() == BatchStatus.FAILED).findFirst().get();
+      sendUnsuccessffulyNotification(failedStepExecution);
     }
   }
 
   private void sendUnsuccessffulyNotification(StepExecution stepExecution) {
     String jobName = getJobName(stepExecution);
     MailNotification mailNotification = mailNotificationService.buildMailErrorNotification(jobName, stepExecution);
-    mailProducerService.produceMailNotification(mailNotification);
-  }
-
-  private void sendSuccessfullyNotification(StepExecution stepExecution) {
-    String jobName = getJobName(stepExecution);
-
-    //TODO
-    List<String> exportedFiles = new ArrayList<>();
-    MailNotification mailNotification = mailNotificationService.buildMailSuccessNotification(jobName, exportedFiles,
-        stepExecution);
     mailProducerService.produceMailNotification(mailNotification);
   }
 
