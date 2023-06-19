36aedcc1ec0af48ff027dad84cc0d8fa4fd86f2e
atlas <19825793+iuccio@users.noreply.github.com>
2023-06-19 12:10:31 +0200

    ATLAS-1073: wip: traffic point import

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/repository/TrafficPointElementVersionRepository.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/repository/TrafficPointElementVersionRepository.java
@@ -2,6 +2,7 @@ package ch.sbb.atlas.servicepointdirectory.repository;
 
 import ch.sbb.atlas.servicepointdirectory.entity.TrafficPointElementVersion;
 import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Repository;
 public interface TrafficPointElementVersionRepository extends
     JpaRepository<TrafficPointElementVersion, Long>, JpaSpecificationExecutor<TrafficPointElementVersion> {
 
+  @EntityGraph(attributePaths = {TrafficPointElementVersion.Fields.trafficPointElementGeolocation})
   List<TrafficPointElementVersion> findAllBySloidOrderByValidFrom(String sloid);
 
   boolean existsBySloid(String sloid);
