73cfd4841f76c2332a7f235e401f0f819f45852e
atlas <19825793+iuccio@users.noreply.github.com>
2023-06-19 14:49:04 +0200

    ATLAS-1294: Update tests

--- base-atlas/src/main/java/ch/sbb/atlas/transport/company/SharedTransportCompanyConfig.java
+++ base-atlas/src/main/java/ch/sbb/atlas/transport/company/SharedTransportCompanyConfig.java
@@ -2,23 +2,23 @@ package ch.sbb.atlas.transport.company;
 
 import ch.sbb.atlas.transport.company.repository.TransportCompanySharingDataAccessor;
 import ch.sbb.atlas.transport.company.service.SharedTransportCompanyConsumer;
-import ch.sbb.atlas.transport.company.service.SharedTransportCompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SharedTransportCompanyConfig {
 
-  @Bean
-  public SharedTransportCompanyConsumer sharedTransportCompanyConsumer(
-      TransportCompanySharingDataAccessor transportCompanySharingDataAccessor) {
-    return new SharedTransportCompanyConsumer(transportCompanySharingDataAccessor);
+  private final TransportCompanySharingDataAccessor transportCompanySharingDataAccessor;
+
+  @Autowired
+  public SharedTransportCompanyConfig(TransportCompanySharingDataAccessor transportCompanySharingDataAccessor) {
+    this.transportCompanySharingDataAccessor = transportCompanySharingDataAccessor;
   }
 
   @Bean
-  public SharedTransportCompanyService sharedTransportCompanyService(
-      TransportCompanySharingDataAccessor transportCompanySharingDataAccessor) {
-    return new SharedTransportCompanyService(transportCompanySharingDataAccessor);
+  public SharedTransportCompanyConsumer sharedTransportCompanyConsumer() {
+    return new SharedTransportCompanyConsumer(this.transportCompanySharingDataAccessor);
   }
 
 }
