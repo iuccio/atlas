5bb7330adb5a1a095bf9bfc3ada4bd48d4c19d78
atlas <19825793+iuccio@users.noreply.github.com>
2023-06-20 13:37:09 +0200

    ATLAS-1291: upload json and gzip

--- export-service-point/src/main/java/ch/sbb/exportservice/model/ServicePointExportType.java
+++ export-service-point/src/main/java/ch/sbb/exportservice/model/ServicePointExportType.java
@@ -1,8 +1,10 @@
 package ch.sbb.exportservice.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Schema(enumAsRef = true)
 @Getter
 @RequiredArgsConstructor
 public enum ServicePointExportType {
