5eab9a27b4a7ecf5e2cc4c96b550d59769979c45
atlas <TechSupport-ATLAS@sbb.ch>
2023-06-26 12:52:20 +0200

    ATLAS-1283: Small changes after rebase.

--- base-atlas/src/main/java/ch/sbb/atlas/api/bodi/BusinessOrganisationApiV1.java
+++ base-atlas/src/main/java/ch/sbb/atlas/api/bodi/BusinessOrganisationApiV1.java
@@ -10,10 +10,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import java.net.URL;
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Optional;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springdoc.core.converters.models.PageableAsQueryParam;
 import org.springframework.data.domain.Pageable;
@@ -22,14 +18,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 @Tag(name = "Business Organisations")
 @RequestMapping("v1/business-organisations")
