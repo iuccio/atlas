a1256b08049dafdb3e3e58a7c4ef0cbce1b223ff
atlas <19825793+iuccio@users.noreply.github.com>
2023-06-27 17:28:05 +0200

    ATLAS-1270: added validator method for comment and justification section

--- frontend/src/app/core/components/base-detail/base-detail-controller.ts
+++ frontend/src/app/core/components/base-detail/base-detail-controller.ts
@@ -115,6 +115,11 @@ export abstract class BaseDetailController<TYPE extends Record> implements OnIni
   }
 
   save() {
+    const validateOnSave = true;
+    console.log('this form ', this.form);
+    if (this.form.get('comment')) {
+      ValidationService.checkWhitespaceErrors([this.form.get('comment')!], validateOnSave);
+    }
     ValidationService.validateForm(this.form);
     this.switchedIndex = undefined;
     if (this.form.valid) {
