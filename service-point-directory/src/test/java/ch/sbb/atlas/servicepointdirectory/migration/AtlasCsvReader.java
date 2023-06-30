1307dbae51cca54257a6c44b2c72b48b1e4fab0f
atlas <19825793+iuccio@users.noreply.github.com>
2023-06-30 15:23:03 +0200

    Implement didok-code and validity check on csv basis

---
+++ service-point-directory/src/test/java/ch/sbb/atlas/servicepointdirectory/migration/AtlasCsvReader.java
@@ -0,0 +1,27 @@
+package ch.sbb.atlas.servicepointdirectory.migration;
+
+import ch.sbb.atlas.servicepointdirectory.service.DidokCsvMapper;
+import com.fasterxml.jackson.databind.MappingIterator;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class AtlasCsvReader {
+
+  public static List<ServicePointVersionCsvModel> parseAtlasServicePoints(InputStream inputStream)
+      throws IOException {
+    MappingIterator<ServicePointVersionCsvModel> mappingIterator = DidokCsvMapper.CSV_MAPPER.readerFor(
+        ServicePointVersionCsvModel.class).with(DidokCsvMapper.CSV_SCHEMA).readValues(new InputStreamReader(inputStream,
+        "UTF-8"));
+    List<ServicePointVersionCsvModel> servicePoints = new ArrayList<>();
+
+    while (mappingIterator.hasNext()) {
+      servicePoints.add(mappingIterator.next());
+    }
+    return servicePoints;
+  }
+}
