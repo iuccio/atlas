a1836a284c4811f4f8f9f718b9ad44229618213e
atlas <19825793+iuccio@users.noreply.github.com>
2023-07-12 15:32:38 +0200

    ATLAS-1329: Add missing countries, map eurostat codes to isocode

---
+++ service-point-directory/src/main/resources/db/migration/V0.0.12__rename_enum_gb.sql
@@ -0,0 +1,2 @@
+update service_point_version set country='GREAT_BRITAIN' where country='NORTHERN_IRELAND';
+update service_point_version_geolocation set country='GREAT_BRITAIN' where country='NORTHERN_IRELAND';
