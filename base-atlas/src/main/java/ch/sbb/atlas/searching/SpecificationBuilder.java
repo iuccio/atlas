bc8449aa52a14c1944b245ca852f1ed296f7b18c
atlas <19825793+iuccio@users.noreply.github.com>
2023-07-18 07:45:52 +0200

    ATLAS-1079 improved solution for filtering data

--- base-atlas/src/main/java/ch/sbb/atlas/searching/SpecificationBuilder.java
+++ base-atlas/src/main/java/ch/sbb/atlas/searching/SpecificationBuilder.java
@@ -1,26 +1,14 @@
 package ch.sbb.atlas.searching;
 
-import ch.sbb.atlas.api.servicepoint.TrafficPointElementVersionModel;
 import ch.sbb.atlas.searching.specification.BooleanSpecification;
 import ch.sbb.atlas.searching.specification.EnumSpecification;
 import ch.sbb.atlas.searching.specification.InSpecification;
 import ch.sbb.atlas.searching.specification.SearchCriteriaSpecification;
 import ch.sbb.atlas.searching.specification.SingleStringSpecification;
 import ch.sbb.atlas.searching.specification.ValidOnSpecification;
-import ch.sbb.atlas.servicepoint.ServicePointNumber;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Join;
-import jakarta.persistence.criteria.JoinType;
-import jakarta.persistence.criteria.Predicate;
-import jakarta.persistence.criteria.Root;
 import jakarta.persistence.metamodel.SingularAttribute;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import lombok.Builder;
 import org.springframework.data.jpa.domain.Specification;
@@ -37,42 +25,6 @@ public class SpecificationBuilder<T> {
     return new SearchCriteriaSpecification<>(searchCriteria, stringAttributes);
   }
 
-  public Specification searchCriteriaSpecificationTrafficPoint(Map<String, List<String>> searchCriteria) {
-    return (root, query, builder) -> {
-      List<Predicate> predicates = new ArrayList<>();
-
-      //TODO: Errorhandling & Clean Code
-      for (Map.Entry<String, List<String>> entry : searchCriteria.entrySet()) {
-        String key = entry.getKey();
-        List<String> values = entry.getValue();
-        List<Predicate> valuePredicates = new ArrayList<>();
-
-        for (String value : values) {
-          if (LocalDate.class.isAssignableFrom(root.get(key).getJavaType())){
-            LocalDate date = LocalDate.parse(value);
-            if (key.equals("validFrom")){
-              valuePredicates.add(builder.greaterThanOrEqualTo(root.get(key), date));
-            }
-            if (key.equals("validTo")) {
-              valuePredicates.add(builder.lessThanOrEqualTo(root.get(key), date));
-            }
-          }
-          else if(LocalDateTime.class.isAssignableFrom(root.get(key).getJavaType())){
-            LocalDateTime dateTime = LocalDateTime.parse(value);
-            valuePredicates.add(builder.greaterThanOrEqualTo(root.get(key), dateTime));
-          }
-          else{
-            valuePredicates.add(builder.equal(root.get(key), value));
-          }
-        }
-
-        Predicate valuePredicate = builder.or(valuePredicates.toArray(new Predicate[0]));
-        predicates.add(valuePredicate);
-      }
-
-      return builder.and(predicates.toArray(new Predicate[0]));
-    };
-  }
   public <V> Specification<T> enumSpecification(List<V> enumRestrictions,
       SingularAttribute<T, V> enumAttribute) {
     return new EnumSpecification<>(enumRestrictions, enumAttribute);
