4761718f2c221640f36c153dbd89ea97faffaee1
atlas <19825793+iuccio@users.noreply.github.com>
2023-07-20 17:00:11 +0200

    Reintroduce didok3 status only to db and import

---
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/model/ServicePointStatus.java
@@ -0,0 +1,32 @@
+package ch.sbb.atlas.servicepointdirectory.model;
+
+import java.util.Arrays;
+import java.util.Objects;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ServicePointStatus {
+
+  TO_BE_REQUESTED(0, "Zu beantragen", "A demander", "Da richiedere", "To be requested"),
+  REQUESTED(1, "Angefragt", "Demandé", "Richiesto", "Requested"),
+  PLANNED(2, "Geplant", "Prévu", "Pianificato", "Planned"),
+  IN_OPERATION(3, "In Betrieb", "En fonctionnement", "In funzione", "In operation"),
+  TERMINATED(4, "Terminiert", "Terminé", "Terminato", "Terminated"),
+  IN_POST_OPERATIONAL_PHASE(5, "Post operationale Phase", "En phase post-opératoire", "In fase post-operativa",
+      "In post-operational phase"),
+  HISTORICAL(6, "Historisch", "Historique", "Storico", "Historical"),
+  UNKNOWN(7, "Unbekannt", "Inconnu", "Sconosciuto", "unknown");
+
+  private final Integer id;
+  private final String designationDe;
+  private final String designationFr;
+  private final String designationIt;
+  private final String designationEn;
+
+  public static ServicePointStatus from(Integer id) {
+    return Arrays.stream(ServicePointStatus.values())
+        .filter(servicePointStatus -> Objects.equals(servicePointStatus.getId(), id)).findFirst().orElse(null);
+  }
+}
