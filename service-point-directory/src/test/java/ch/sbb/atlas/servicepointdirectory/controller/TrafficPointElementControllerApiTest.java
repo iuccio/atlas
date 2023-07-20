6b9ba506384081f6b75b68c33f06b5e88665e6d3
atlas <19825793+iuccio@users.noreply.github.com>
2023-07-20 15:21:10 +0200

    ATLAS-1085: Adjustments based on PR comments.

--- service-point-directory/src/test/java/ch/sbb/atlas/servicepointdirectory/controller/TrafficPointElementControllerApiTest.java
+++ service-point-directory/src/test/java/ch/sbb/atlas/servicepointdirectory/controller/TrafficPointElementControllerApiTest.java
@@ -313,11 +313,11 @@ public class TrafficPointElementControllerApiTest extends BaseControllerApiTest 
     MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.put("/v1/traffic-point-elements/" + savedTrafficPointElementVersionModel.getId())
                     .contentType(contentType)
                     .content(mapper.writeValueAsString(newTrafficPointElementVersionModel)))
-            .andExpect(status().isInternalServerError()).andReturn();
+            .andExpect(status().isBadRequest()).andReturn();
 
     // then
     ErrorResponse errorResponse = mapper.readValue(mvcResult.getResponse().getContentAsString(), ErrorResponse.class);
-    assertThat(errorResponse.getStatus()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+    assertThat(errorResponse.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     assertThat(errorResponse.getMessage()).isEqualTo("Sloid for provided id: ch:1:sloid:1400015:0:310240 and sloid in the request body: ch:1:sloid:1400015:0:310241 are not equal.");
 
   }
