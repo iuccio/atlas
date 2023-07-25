25eb207e66ea8d365779ecfa6219798a9e7af5c2
atlas <19825793+iuccio@users.noreply.github.com>
2023-07-25 22:23:29 +0200

    ATLAS-1079: WIP for get traffic points.

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/repository/SharedBusinessOrganisationVersionRepository.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/repository/SharedBusinessOrganisationVersionRepository.java
@@ -8,8 +8,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface SharedBusinessOrganisationVersionRepository extends JpaRepository<SharedBusinessOrganisationVersion, Long>,
     BusinessOrganisationVersionSharingDataAccessor {
 
+  @Override
   boolean existsBySboid(String sboid);
 
+  @Override
   default void save(SharedBusinessOrganisationVersionModel model) {
     SharedBusinessOrganisationVersion sharedBusinessOrganisationVersion = new SharedBusinessOrganisationVersion();
     sharedBusinessOrganisationVersion.setPropertiesFromModel(model);
