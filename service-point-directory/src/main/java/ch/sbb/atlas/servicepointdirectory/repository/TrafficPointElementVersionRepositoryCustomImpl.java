dd517188fdefcf12bf73b55d710554a5afc60418
atlas <19825793+iuccio@users.noreply.github.com>
2023-07-26 16:24:38 +0200

    ATLAS-1079: Add working example with limit and offset.

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/repository/TrafficPointElementVersionRepositoryCustomImpl.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/repository/TrafficPointElementVersionRepositoryCustomImpl.java
@@ -7,11 +7,15 @@ import ch.sbb.atlas.servicepointdirectory.entity.TrafficPointElementVersion;
 import ch.sbb.atlas.servicepointdirectory.entity.geolocation.TrafficPointElementGeolocation;
 import ch.sbb.atlas.servicepointdirectory.service.trafficpoint.TrafficPointElementRequestParams;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
 
 @Repository
 @RequiredArgsConstructor
@@ -20,48 +24,20 @@ public class TrafficPointElementVersionRepositoryCustomImpl implements TrafficPo
     private final NamedParameterJdbcTemplate jdbcTemplate;
 
     @Override
-    public List<TrafficPointElementVersion> blaBloBlu2(TrafficPointElementRequestParams trafficPointElementRequestParams) {
+    public Page<TrafficPointElementVersion> blaBloBlu2(TrafficPointElementRequestParams trafficPointElementRequestParams, Pageable pageable) {
         MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
+        String query = getQueryString(trafficPointElementRequestParams, mapSqlParameterSource, pageable);
 
-        String query = """
-                SELECT DISTINCT
-                    trp.id,
-                    trp.sloid,
-                    trp.parent_sloid,
-                    trp.designation,
-                    trp.designation_operational,
-                    trp.traffic_point_element_type,
-                    trp.length,
-                    trp.boarding_area_height,
-                    trp.compass_direction,
-                    trp.service_point_number,
-                    trp.valid_from,
-                    trp.valid_to,
-                    trp.traffic_point_geolocation_id,
-                    trp.creation_date,
-                    trp.creator,
-                    trp.edition_date,
-                    trp.editor,
-                    trp.version,
-                    tpevg.*
-                FROM
-                    traffic_point_element_version trp
-                LEFT JOIN
-                    service_point_version spv ON trp.service_point_number = spv.number
-                LEFT JOIN traffic_point_element_version_geolocation tpevg ON tpevg.id = trp.traffic_point_geolocation_id
-                where 1=1
-                """;
+        Integer count = getCount(query, mapSqlParameterSource);
 
-        if (!trafficPointElementRequestParams.getBusinessOrganisations().isEmpty()) {
-            query += " and spv.business_organisation in (:sboids)";
-            mapSqlParameterSource.addValue("sboids", trafficPointElementRequestParams.getBusinessOrganisations());
+        query += " ORDER BY sloid";
+        if (pageable != null && pageable.isPaged()) {
+            query += " LIMIT :pageSize OFFSET :offset";
+            mapSqlParameterSource.addValue("pageSize", pageable.getPageSize());
+            mapSqlParameterSource.addValue("offset", pageable.getOffset());
         }
-//        if (!trafficPointElementRequestParams.getServicePointNumberShort().isEmpty()) {
-//            query += " and spv.number_short in (:shorts)";
-//        }
 
-
-        return jdbcTemplate.query(query, mapSqlParameterSource,(rs, rowNum) -> {
+        List<TrafficPointElementVersion> elements = jdbcTemplate.query(query, mapSqlParameterSource, (rs, rowNum) -> {
             return TrafficPointElementVersion.builder()
                     .id(rs.getLong("id"))
                     .designation(rs.getString("designation"))
@@ -93,6 +69,58 @@ public class TrafficPointElementVersionRepositoryCustomImpl implements TrafficPo
                     .editor(rs.getString("editor"))
                     .version(rs.getInt("version"))
                     .build();
-        } );
+        });
+
+
+        return new PageImpl<>(elements, pageable, count);
+
+    }
+
+    private Integer getCount(String query, MapSqlParameterSource mapSqlParameterSource) {
+        String countQuery = "select count (*) from (" + query + ") as foo";
+        return jdbcTemplate.queryForObject(countQuery, mapSqlParameterSource, Integer.class);
+    }
+
+    private static String getQueryString(TrafficPointElementRequestParams trafficPointElementRequestParams,
+                                         MapSqlParameterSource mapSqlParameterSource,
+                                         Pageable pageable) {
+        String query = """
+                SELECT DISTINCT
+                    trp.id,
+                    trp.sloid,
+                    trp.parent_sloid,
+                    trp.designation,
+                    trp.designation_operational,
+                    trp.traffic_point_element_type,
+                    trp.length,
+                    trp.boarding_area_height,
+                    trp.compass_direction,
+                    trp.service_point_number,
+                    trp.valid_from,
+                    trp.valid_to,
+                    trp.traffic_point_geolocation_id,
+                    trp.creation_date,
+                    trp.creator,
+                    trp.edition_date,
+                    trp.editor,
+                    trp.version,
+                    tpevg.*
+                FROM
+                    traffic_point_element_version trp
+                LEFT JOIN
+                    service_point_version spv ON trp.service_point_number = spv.number
+                LEFT JOIN traffic_point_element_version_geolocation tpevg ON tpevg.id = trp.traffic_point_geolocation_id
+                WHERE 1=1
+                """;
+
+        if (!trafficPointElementRequestParams.getBusinessOrganisations().isEmpty()) {
+            query += " AND spv.business_organisation IN (:sboids)";
+            mapSqlParameterSource.addValue("sboids", trafficPointElementRequestParams.getBusinessOrganisations());
+        }
+//        if (!trafficPointElementRequestParams.getServicePointNumberShort().isEmpty()) {
+//            query += " and spv.number_short in (:shorts)";
+//        }
+
+        return query;
     }
 }
