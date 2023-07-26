5f9c56d5e4b6d8af7d0c401a773cf513a5b5d91c
atlas <19825793+iuccio@users.noreply.github.com>
2023-07-26 16:24:38 +0200

    ATLAS-1079: Add working example with limit and offset.

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/repository/TrafficPointElementVersionRepositoryCustom.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/repository/TrafficPointElementVersionRepositoryCustom.java
@@ -2,13 +2,13 @@ package ch.sbb.atlas.servicepointdirectory.repository;
 
 import ch.sbb.atlas.servicepointdirectory.entity.TrafficPointElementVersion;
 import ch.sbb.atlas.servicepointdirectory.service.trafficpoint.TrafficPointElementRequestParams;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface TrafficPointElementVersionRepositoryCustom {
 
-    List<TrafficPointElementVersion> blaBloBlu2(TrafficPointElementRequestParams trafficPointElementRequestParams);
+    Page<TrafficPointElementVersion> blaBloBlu2(TrafficPointElementRequestParams trafficPointElementRequestParams, Pageable pageable);
 
 }
