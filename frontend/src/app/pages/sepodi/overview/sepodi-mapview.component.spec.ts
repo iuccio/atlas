595f9543c29b38a4c822243b0b95fabd319c2633
atlas <TechSupport-ATLAS@sbb.ch>
2023-08-03 15:23:12 +0200

    ATLAS-869: some styling

--- frontend/src/app/pages/sepodi/overview/sepodi-mapview.component.spec.ts
+++ frontend/src/app/pages/sepodi/overview/sepodi-mapview.component.spec.ts
@@ -1,21 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SepodiOverviewComponent } from './sepodi-overview.component';
+import { SepodiMapviewComponent } from './sepodi-mapview.component';
 import { AuthService } from '../../../core/auth/auth.service';
 
 const authService: Partial<AuthService> = {};
 
 describe('SepodiOverviewComponent', () => {
-  let component: SepodiOverviewComponent;
-  let fixture: ComponentFixture<SepodiOverviewComponent>;
+  let component: SepodiMapviewComponent;
+  let fixture: ComponentFixture<SepodiMapviewComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [SepodiOverviewComponent],
+      declarations: [SepodiMapviewComponent],
       providers: [{ provide: AuthService, useValue: authService }],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(SepodiOverviewComponent);
+    fixture = TestBed.createComponent(SepodiMapviewComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
