579e9484881e305ee851e9cb3a988042f4f4091c
atlas <19825793+iuccio@users.noreply.github.com>
2023-08-03 14:33:06 +0200

    ATLAS-869: Add map style switch

--- frontend/src/app/pages/sepodi/map/map.component.ts
+++ frontend/src/app/pages/sepodi/map/map.component.ts
@@ -1,15 +1,7 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  EventEmitter,
-  OnDestroy,
-  Output,
-  ViewChild,
-} from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 import { Map } from 'maplibre-gl';
-import { GeoJsonProperties } from 'geojson';
 import { MapService } from './map.service';
+import { MAP_STYLES, MapStyle } from './map-options.service';
 
 @Component({
   selector: 'atlas-map',
@@ -17,6 +9,10 @@ import { MapService } from './map.service';
   styleUrls: ['./map.component.scss'],
 })
 export class MapComponent implements AfterViewInit, OnDestroy {
+  availableMapStyles = MAP_STYLES;
+  currentMapStyle!: MapStyle;
+  showMapStyleSelection = false;
+
   map!: Map;
 
   @ViewChild('map')
@@ -26,9 +22,20 @@ export class MapComponent implements AfterViewInit, OnDestroy {
 
   ngAfterViewInit() {
     this.map = this.mapService.initMap(this.mapContainer.nativeElement);
+    this.currentMapStyle = this.mapService.currentMapStyle;
   }
 
   ngOnDestroy() {
     this.map.remove();
+  }
+
+  toggleStyleSelection() {
+    this.showMapStyleSelection = !this.showMapStyleSelection;
+  }
+
+  switchToStyle(style: MapStyle) {
+    this.mapService.switchToStyle(style);
+    this.toggleStyleSelection();
+    this.currentMapStyle = this.mapService.currentMapStyle;
   }
 }
