88f5aa9586ca1c2870f51e3a27698faf090f2e10
atlas <19825793+iuccio@users.noreply.github.com>
2023-08-03 15:23:12 +0200

    ATLAS-869: some styling

--- frontend/src/app/pages/sepodi/sepodi.module.ts
+++ frontend/src/app/pages/sepodi/sepodi.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { SepodiRoutingModule } from './sepodi-routing.module';
-import { SepodiOverviewComponent } from './overview/sepodi-overview.component';
+import { SepodiMapviewComponent } from './overview/sepodi-mapview.component';
 import { MapComponent } from './map/map.component';
 import { ServicePointSidePanelComponent } from './service-point-side-panel/service-point-side-panel.component';
 import { CoreModule } from '../../core/module/core.module';
@@ -17,7 +17,7 @@ import { GeographyComponent } from './geography/geography.component';
 
 @NgModule({
   declarations: [
-    SepodiOverviewComponent,
+    SepodiMapviewComponent,
     ServicePointSidePanelComponent,
     ServicePointDetailComponent,
     AreasDetailComponent,
