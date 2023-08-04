e0bf1994eb18003d949d7ff12a821fb05b415ca6
atlas <TechSupport-ATLAS@sbb.ch>
2023-08-04 09:57:57 +0200

    ATLAS-869: Bring tests to the green side

--- frontend/src/app/pages/sepodi/service-point-side-panel/service-point-side-panel.component.spec.ts
+++ frontend/src/app/pages/sepodi/service-point-side-panel/service-point-side-panel.component.spec.ts
@@ -1,18 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ServicePointSidePanelComponent } from './service-point-side-panel.component';
+import { ActivatedRoute } from '@angular/router';
+import { AppTestingModule } from '../../../app.testing.module';
+import { DisplayDatePipe } from '../../../core/pipe/display-date.pipe';
+import { FormatServicePointNumber } from '../number-pipe/service-point-number.pipe';
+import { of } from 'rxjs';
+import { BERN_WYLEREGG } from '../service-point-test-data';
 import { AuthService } from '../../../core/auth/auth.service';
+import { MockAtlasButtonComponent } from '../../../app.testing.mocks';
 
 const authService: Partial<AuthService> = {};
 
-describe('SepodiOverviewComponent', () => {
+describe('ServicePointSidePanelComponent', () => {
   let component: ServicePointSidePanelComponent;
   let fixture: ComponentFixture<ServicePointSidePanelComponent>;
 
+  const activatedRouteMock = { data: of({ servicePoint: [BERN_WYLEREGG] }) };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ServicePointSidePanelComponent],
-      providers: [{ provide: AuthService, useValue: authService }],
+      declarations: [
+        ServicePointSidePanelComponent,
+        DisplayDatePipe,
+        FormatServicePointNumber,
+        MockAtlasButtonComponent,
+      ],
+      imports: [AppTestingModule],
+      providers: [
+        { provide: AuthService, useValue: authService },
+        { provide: ActivatedRoute, useValue: activatedRouteMock },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ServicePointSidePanelComponent);
