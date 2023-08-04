daa9bbd4d8357e488af9130cf508ae76c03dca74
atlas <TechSupport-ATLAS@sbb.ch>
2023-08-04 13:53:44 +0200

    ATLAS-869: Fix change detection error

--- frontend/src/app/pages/sepodi/mapview/sepodi-mapview.component.ts
+++ frontend/src/app/pages/sepodi/mapview/sepodi-mapview.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { GeoJsonProperties } from 'geojson';
 import { Router } from '@angular/router';
 import { Pages } from '../../pages';
@@ -9,14 +9,39 @@ import { MapService } from '../map/map.service';
   templateUrl: './sepodi-mapview.component.html',
   styleUrls: ['./sepodi-mapview.component.scss'],
 })
-export class SepodiMapviewComponent {
+export class SepodiMapviewComponent implements AfterViewInit {
+  @ViewChild('detailContainer') detailContainer!: ElementRef<HTMLElement>;
+
+  private routeActive = false;
+
   constructor(private router: Router, private mapService: MapService) {
     this.mapService.selectedElement.subscribe((selectedPoint) =>
       this.servicePointClicked(selectedPoint)
     );
   }
 
+  ngAfterViewInit() {
+    this.styleDetailContainer();
+  }
+
   servicePointClicked($event: GeoJsonProperties) {
     this.router.navigate([Pages.SEPODI.path, Pages.SERVICE_POINTS.path, $event!.number]).then();
+  }
+
+  setRouteActive(value: boolean) {
+    this.routeActive = value;
+    this.styleDetailContainer();
+  }
+
+  private styleDetailContainer() {
+    if (this.detailContainer) {
+      const detailContainerDiv = this.detailContainer.nativeElement;
+      if (this.routeActive) {
+        detailContainerDiv.classList.add('side-panel-open');
+      } else {
+        detailContainerDiv.classList.remove('side-panel-open');
+        detailContainerDiv.style.width = 'unset';
+      }
+    }
   }
 }
