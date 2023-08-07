44a8e453b3350ae3fa249ebc4de3f56de90eced5
atlas <19825793+iuccio@users.noreply.github.com>
2023-08-07 13:00:30 +0200

    ATLAS-869: Improve test coverage

--- frontend/src/app/pages/sepodi/map/map.component.spec.ts
+++ frontend/src/app/pages/sepodi/map/map.component.spec.ts
@@ -1,10 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MapComponent } from './map.component';
-import { AuthService } from '../../../core/auth/auth.service';
 import { AppTestingModule } from '../../../app.testing.module';
+import { MAP_STYLES } from './map-options.service';
+import { MapService } from './map.service';
+import { Map } from 'maplibre-gl';
 
-const authService: Partial<AuthService> = {};
+const mapSpy = jasmine.createSpyObj<Map>(['once']);
+const mapService = jasmine.createSpyObj<MapService>(['initMap', 'switchToStyle', 'removeMap']);
+mapService.initMap.and.returnValue(mapSpy);
 
 describe('MapComponent', () => {
   let component: MapComponent;
@@ -14,7 +18,7 @@ describe('MapComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [MapComponent],
       imports: [AppTestingModule],
-      providers: [{ provide: AuthService, useValue: authService }],
+      providers: [{ provide: MapService, useValue: mapService }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MapComponent);
@@ -24,5 +28,23 @@ describe('MapComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should open map style selection', () => {
+    expect(component.showMapStyleSelection).toBeFalse();
+
+    component.openStyleSelection();
+    expect(component.showMapStyleSelection).toBeTrue();
+  });
+
+  it('should switch map style selection via service', () => {
+    mapService.switchToStyle.and.callFake((i) => i);
+
+    const newStyle = MAP_STYLES[1];
+    component.switchToStyle(newStyle);
+
+    expect(mapService.switchToStyle).toHaveBeenCalled();
+    expect(component.currentMapStyle).toEqual(newStyle);
+    expect(component.showMapStyleSelection).toBeFalse();
   });
 });
