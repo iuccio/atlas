a631ced6406fc0356114a47ae01d84bc6d055f1b
atlas <TechSupport-ATLAS@sbb.ch>
2023-08-14 16:12:36 +0200

    ATLAS-1087: add some controller test

--- service-point-directory/src/test/java/ch/sbb/atlas/servicepointdirectory/controller/LoadingPointControllerApiTest.java
+++ service-point-directory/src/test/java/ch/sbb/atlas/servicepointdirectory/controller/LoadingPointControllerApiTest.java
@@ -1,32 +1,41 @@
 package ch.sbb.atlas.servicepointdirectory.controller;
 
+import ch.sbb.atlas.model.controller.BaseControllerApiTest;
+import ch.sbb.atlas.servicepoint.ServicePointNumber;
+import ch.sbb.atlas.servicepointdirectory.ServicePointTestData;
+import ch.sbb.atlas.servicepointdirectory.entity.LoadingPointVersion;
+import ch.sbb.atlas.servicepointdirectory.entity.LoadingPointVersion.Fields;
+import ch.sbb.atlas.servicepointdirectory.entity.ServicePointVersion;
+import ch.sbb.atlas.servicepointdirectory.entity.geolocation.LoadingPointGeolocation;
+import ch.sbb.atlas.servicepointdirectory.repository.LoadingPointVersionRepository;
+import ch.sbb.atlas.servicepointdirectory.repository.ServicePointVersionRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import static ch.sbb.atlas.imports.servicepoint.enumeration.SpatialReference.LV95;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import ch.sbb.atlas.model.controller.BaseControllerApiTest;
-import ch.sbb.atlas.servicepoint.ServicePointNumber;
-import ch.sbb.atlas.servicepointdirectory.entity.LoadingPointVersion;
-import ch.sbb.atlas.servicepointdirectory.entity.LoadingPointVersion.Fields;
-import ch.sbb.atlas.servicepointdirectory.entity.geolocation.LoadingPointGeolocation;
-import ch.sbb.atlas.servicepointdirectory.repository.LoadingPointVersionRepository;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-
 public class LoadingPointControllerApiTest extends BaseControllerApiTest {
 
   private final LoadingPointVersionRepository repository;
+
+  private final ServicePointVersionRepository servicePointVersionRepository;
   private LoadingPointVersion loadingPointVersion;
 
+  private ServicePointVersion servicePointVersion;
+
   @Autowired
-  public LoadingPointControllerApiTest(LoadingPointVersionRepository repository) {
+  public LoadingPointControllerApiTest(LoadingPointVersionRepository repository, ServicePointVersionRepository servicePointVersionRepository) {
     this.repository = repository;
+    this.servicePointVersionRepository = servicePointVersionRepository;
   }
 
   @BeforeEach
@@ -43,13 +52,15 @@ public class LoadingPointControllerApiTest extends BaseControllerApiTest {
         .editionDate(LocalDateTime.of(2018, 6, 28, 11, 48, 56))
         .build();
 
+    servicePointVersion = servicePointVersionRepository.save(ServicePointTestData.createAbroadServicePointVersion());
+
     LoadingPointVersion loadingPointVersion = LoadingPointVersion
         .builder()
         .number(4201)
         .designation("Piazzale")
         .designationLong("Piazzaleee")
         .connectionPoint(false)
-        .servicePointNumber(ServicePointNumber.of(83017186))
+        .servicePointNumber(servicePointVersion.getNumber())
         .validFrom(LocalDate.of(2018, 6, 28))
         .validTo(LocalDate.of(2099, 12, 31))
         .creator("fs45117")
@@ -67,15 +78,18 @@ public class LoadingPointControllerApiTest extends BaseControllerApiTest {
   @AfterEach
   void cleanUpDb() {
     repository.deleteAll();
+    servicePointVersionRepository.deleteAll();
   }
 
   @Test
   void shouldGetLoadingPoint() throws Exception {
-    mvc.perform(get("/v1/loading-points/83017186/4201")).andExpect(status().isOk())
+    int servicePointNumber = servicePointVersion.getNumber().getValue();
+    Integer number = loadingPointVersion.getNumber();
+    mvc.perform(get("/v1/loading-points/"+servicePointNumber+"/"+ number)).andExpect(status().isOk())
         .andExpect(jsonPath("$[0]." + Fields.id, is(loadingPointVersion.getId().intValue())))
         .andExpect(jsonPath("$[0]." + Fields.number, is(4201)))
         .andExpect(jsonPath("$[0]." + Fields.connectionPoint, is(false)))
-        .andExpect(jsonPath("$[0].servicePointNumber.number", is(8301718)))
+        .andExpect(jsonPath("$[0].servicePointNumber.number", is(ServicePointNumber.ofNumberWithoutCheckDigit(servicePointNumber).getNumber())))
         .andExpect(jsonPath("$[0].hasGeolocation", is(true)))
         .andExpect(jsonPath("$[0].loadingPointGeolocation.lv95.north", is(1116455.883)))
         .andExpect(jsonPath("$[0].creationDate", is("2017-12-04T13:11:03")))
@@ -84,9 +98,10 @@ public class LoadingPointControllerApiTest extends BaseControllerApiTest {
 
   @Test
   void shouldGetLoadingPointVersions() throws Exception {
-    mvc.perform(get("/v1/loading-points")).andExpect(status().isOk())
-        .andExpect(jsonPath("$.objects[0]." + Fields.id, is(loadingPointVersion.getId().intValue())))
-        .andExpect(jsonPath("$.totalCount", is(1)));
+    mvc.perform(get("/v1/loading-points"))
+         .andExpect(status().isOk())
+         .andExpect(jsonPath("$.totalCount", is(1)))
+        .andExpect(jsonPath("$.objects[0]." + Fields.id, is(loadingPointVersion.getId().intValue())));
   }
 
   @Test
