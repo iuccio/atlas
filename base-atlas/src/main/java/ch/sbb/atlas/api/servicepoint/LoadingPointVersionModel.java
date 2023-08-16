9d1839dc74e62645fad0c16c3de6f0e4b1bfcd5b
atlas <19825793+iuccio@users.noreply.github.com>
2023-08-16 14:08:25 +0200

    ATLAS-1087: expose servicePointSloid on API

--- base-atlas/src/main/java/ch/sbb/atlas/api/servicepoint/LoadingPointVersionModel.java
+++ base-atlas/src/main/java/ch/sbb/atlas/api/servicepoint/LoadingPointVersionModel.java
@@ -10,13 +10,14 @@ import io.swagger.v3.oas.annotations.media.Schema.AccessMode;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldNameConstants;
 import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -50,11 +51,23 @@ public class LoadingPointVersionModel extends BaseVersionModel implements DatesV
   @Valid
   private ServicePointNumber servicePointNumber;
 
+  @Size(min = 1, max = AtlasFieldLengths.LENGTH_500)
+  @Schema(description = "Unique code for locations that is used in customer information. The structure is described in the "
+          + "“Swiss Location ID” specification, chapter 4.2. The document is available here. "
+          + "https://transportdatamanagement.ch/standards/", example = "ch:1:sloid:18771")
+  private String servicePointSloid;
+
   private GeolocationBaseReadModel loadingPointGeolocation;
+
   @NotNull
   private LocalDate validFrom;
+
   @NotNull
   private LocalDate validTo;
+
+  public String getServicePointSloid(){
+    return ServicePointNumber.calculateSloid(this.servicePointNumber);
+  }
 
   @JsonInclude
   @Schema(description = "LoadingPoint has a Geolocation")
