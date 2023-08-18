841b152879157d3a154a7364ffb4ea916273f6b4
atlas <TechSupport-ATLAS@sbb.ch>
2023-08-18 11:11:45 +0200

    ATLAS-1364: Add coordinate constraints

--- base-atlas/src/main/java/ch/sbb/atlas/api/servicepoint/GeolocationBaseCreateModel.java
+++ base-atlas/src/main/java/ch/sbb/atlas/api/servicepoint/GeolocationBaseCreateModel.java
@@ -1,7 +1,12 @@
 package ch.sbb.atlas.api.servicepoint;
 
+import static ch.sbb.atlas.api.AtlasApiConstants.ATLAS_LV_MAX_DIGITS;
+import static ch.sbb.atlas.api.AtlasApiConstants.ATLAS_WGS84_MAX_DIGITS;
+
 import ch.sbb.atlas.imports.servicepoint.enumeration.SpatialReference;
+import ch.sbb.atlas.math.DoubleOperations;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -31,5 +36,18 @@ public class GeolocationBaseCreateModel implements TransformableGeolocation {
 
   @Schema(description = "Height of the coordinate point", example = "540.20000")
   private Double height;
+
+  @AssertTrue(message = "Max Fractions exceeded. LV03 and LV95 max. 5 digits. WGS84 and WGS84WEB max. 11 digits.")
+  boolean isSpatialReferenceFractionValid() {
+    if (getSpatialReference() == null || getNorth() == null || getEast() == null) {
+      return false;
+    }
+    return switch (getSpatialReference()) {
+      case LV03, LV95 ->
+          Math.max(DoubleOperations.getFractions(getNorth()), DoubleOperations.getFractions(getEast())) <= ATLAS_LV_MAX_DIGITS;
+      case WGS84, WGS84WEB ->
+          Math.max(DoubleOperations.getFractions(getNorth()), DoubleOperations.getFractions(getEast())) <= ATLAS_WGS84_MAX_DIGITS;
+    };
+  }
 
 }
