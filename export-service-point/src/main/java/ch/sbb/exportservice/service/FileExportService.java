139e6eea1431510655b9b53752808f3c0b4107cb
atlas <19825793+iuccio@users.noreply.github.com>
2023-08-22 16:19:03 +0200

    ATLAS-1343: Remove commented out code.

--- export-service-point/src/main/java/ch/sbb/exportservice/service/FileExportService.java
+++ export-service-point/src/main/java/ch/sbb/exportservice/service/FileExportService.java
@@ -7,7 +7,6 @@ import ch.sbb.atlas.amazon.service.FileService;
 import ch.sbb.atlas.api.AtlasApiConstants;
 import ch.sbb.exportservice.model.ExportExtensionFileType;
 import ch.sbb.exportservice.model.ExportType;
-import java.nio.file.Files;
 import ch.sbb.exportservice.model.SpExportFileName;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,7 +14,6 @@ import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBo
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -23,8 +21,6 @@ import java.time.format.DateTimeFormatter;
 @RequiredArgsConstructor
 public class FileExportService {
 
-  private static final int OUT_BUFFER = 4096;
-  private static final int IN_BUFFER = 1024;
   private final AmazonService amazonService;
 
   private final FileService fileService;
@@ -33,66 +29,9 @@ public class FileExportService {
     return fileService.streamingJsonFile(exportType, exportFileName, amazonService, null, getBaseFileName(exportType, exportFileName));
   }
 
-//  public StreamingResponseBody streamingJsonFile(ExportType exportType, SpExportFileName exportFileName) {
-//    String fileToDownload = getJsonFileToDownload(exportType,exportFileName);
-//    try {
-//      File file = amazonService.pullFile(AmazonBucket.EXPORT, fileToDownload);
-//      byte[] bytes = decompressGzipToBytes(file.toPath());
-//      InputStream inputStream = new ByteArrayInputStream(bytes);
-//      return writeOutputStream(file, inputStream);
-//    } catch (IOException e) {
-//      throw new FileException(e);
-//    }
-//  }
-
   public StreamingResponseBody streamGzipFile(ExportType exportType, SpExportFileName exportFileName) {
     return fileService.streamingGzipFile(exportType, exportFileName, amazonService, null, getBaseFileName(exportType, exportFileName));
   }
-
-//  public StreamingResponseBody streamingGzipFile(ExportType exportType, SpExportFileName exportFileName) {
-//    String fileToDownload = getJsonFileToDownload(exportType, exportFileName);
-//    try {
-//      File file = amazonService.pullFile(AmazonBucket.EXPORT, fileToDownload);
-//      InputStream inputStream = new FileInputStream(file);
-//      return writeOutputStream(file, inputStream);
-//    } catch (IOException e) {
-//      throw new FileException(e);
-//    }
-//  }
-
-//  private StreamingResponseBody writeOutputStream(File file, InputStream inputStream) {
-//    return outputStream -> {
-//      int len;
-//      byte[] data = new byte[OUT_BUFFER];
-//      while ((len = inputStream.read(data, 0, data.length)) != -1) {
-//        outputStream.write(data, 0, len);
-//      }
-//      inputStream.close();
-//      file.delete();
-//    };
-//  }
-//
-//  private String getJsonFileToDownload(ExportType exportType, SpExportFileName exportFileName) {
-//    return exportFileName.getBaseDir()
-//        + "/"
-//        + exportType.getDir()
-//        + "/"
-//        + getBaseFileName(exportType, exportFileName)
-//        + ".json.gz";
-//  }
-
-//  byte[] decompressGzipToBytes(Path source) throws IOException {
-//    ByteArrayOutputStream output = new ByteArrayOutputStream();
-//    try (GZIPInputStream gis = new GZIPInputStream(
-//        new FileInputStream(source.toFile()))) {
-//      byte[] buffer = new byte[IN_BUFFER];
-//      int len;
-//      while ((len = gis.read(buffer)) > 0) {
-//        output.write(buffer, 0, len);
-//      }
-//    }
-//    return output.toByteArray();
-//  }
 
   public void exportFile(File file, ExportType exportType, SpExportFileName exportFileName, ExportExtensionFileType exportExtensionFileType) {
     String pathDirectory = exportFileName.getBaseDir() + "/" + exportType.getDir();
