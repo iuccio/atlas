bfaf6b1c4aef4240971f2da726dd81fbd001146a
atlas <19825793+iuccio@users.noreply.github.com>
2023-08-23 12:12:47 +0200

    ATLAS-1343: Increase test coverage.

--- base-atlas/src/main/java/ch/sbb/atlas/export/BaseExportService.java
+++ base-atlas/src/main/java/ch/sbb/atlas/export/BaseExportService.java
@@ -113,7 +113,7 @@ public abstract class BaseExportService<T extends BaseVersion> {
 
     File csvFile = createFile(exportType, ".csv");
 
-    List<VersionCsvModel> versionCsvModels = convertToCsvModel(versions);
+    List<? extends VersionCsvModel> versionCsvModels = convertToCsvModel(versions);
 
     ObjectWriter objectWriter = getObjectWriter();
     return CsvExportWriter.writeToFile(csvFile, versionCsvModels, objectWriter);
@@ -123,7 +123,7 @@ public abstract class BaseExportService<T extends BaseVersion> {
 
     File jsonFile = createFile(exportType, ".json");
 
-    List<BaseVersionModel> versionJsonModels = convertToJsonModel(versions);
+    List<? extends BaseVersionModel> versionJsonModels = convertToJsonModel(versions);
 
     ObjectMapper objectMapper = new ObjectMapper();
     objectMapper.registerModule(new JavaTimeModule());
