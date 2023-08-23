87ca348fe26a1f4a110426c998330723b44e6737
atlas <19825793+iuccio@users.noreply.github.com>
2023-08-23 10:26:16 +0200

    ATLAS-533: derive type from data

---
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/entity/geolocation/ServicePointType.java
@@ -0,0 +1,9 @@
+package ch.sbb.atlas.servicepointdirectory.entity.geolocation;
+
+public enum ServicePointType {
+  OPERATING_POINT_TECHNICAL,
+  STOP_POINT_AND_FREIGHT_SERVICE_POINT,
+  STOP_POINT,
+  FREIGHT_SERVICE_POINT,
+  SERVICE_POINT
+}
