d879832edb4e98caa9cf11cbaa36cc834bfa55cd
atlas <19825793+iuccio@users.noreply.github.com>
2023-08-23 17:28:23 +0200

    ATLAS-1395: added missing column to trapo csv export

--- export-service-point/src/main/java/ch/sbb/exportservice/processor/TrafficPointElementVersionCsvProcessor.java
+++ export-service-point/src/main/java/ch/sbb/exportservice/processor/TrafficPointElementVersionCsvProcessor.java
@@ -12,55 +12,57 @@ import java.time.format.DateTimeFormatter;
 
 @Slf4j
 public class TrafficPointElementVersionCsvProcessor extends BaseServicePointProcessor implements
-        ItemProcessor<TrafficPointElementVersion, TrafficPointVersionCsvModel> {
+    ItemProcessor<TrafficPointElementVersion, TrafficPointVersionCsvModel> {
 
-    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern(AtlasApiConstants.DATE_FORMAT_PATTERN);
-    private static final DateTimeFormatter LOCAL_DATE_FORMATTER = DateTimeFormatter.ofPattern(AtlasApiConstants.DATE_TIME_FORMAT_PATTERN);
+  private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern(AtlasApiConstants.DATE_FORMAT_PATTERN);
+  private static final DateTimeFormatter LOCAL_DATE_FORMATTER = DateTimeFormatter.ofPattern(
+      AtlasApiConstants.DATE_TIME_FORMAT_PATTERN);
 
-    @Override
-    public TrafficPointVersionCsvModel process(TrafficPointElementVersion version) {
-        BusinessOrganisation servicePointBusinessOrganisation = version.getServicePointBusinessOrganisation();
-        TrafficPointVersionCsvModel.TrafficPointVersionCsvModelBuilder builder = TrafficPointVersionCsvModel.builder()
-                .sloid(version.getSloid())
-                .numberShort(version.getServicePointNumber().getNumberShort())
-                .number(version.getServicePointNumber().getNumber())
-                .uicCountryCode(version.getServicePointNumber().getUicCountryCode())
-                .validFrom(DATE_FORMATTER.format(version.getValidFrom()))
-                .validTo(DATE_FORMATTER.format(version.getValidTo()))
-                .designation(version.getDesignation())
-                .designationOperational(version.getDesignationOperational())
-                .length(version.getLength())
-                .boardingAreaHeight(version.getBoardingAreaHeight())
-                .compassDirection(version.getCompassDirection())
-                .parentSloid(version.getParentSloid())
-                .trafficPointElementType(version.getTrafficPointElementType().name())
-                .designationOfficial(version.getServicePointDesignationOfficial())
-                .creationDate(LOCAL_DATE_FORMATTER.format(version.getCreationDate()))
-                .editionDate(LOCAL_DATE_FORMATTER.format(version.getEditionDate()))
-                .parentSloidServicePoint(version.getParentSloidServicePoint())
-                .servicePointBusinessOrganisation(servicePointBusinessOrganisation.getBusinessOrganisation())
-                .servicePointBusinessOrganisationAbbreviationDe(servicePointBusinessOrganisation.getBusinessOrganisationAbbreviationDe())
-                .servicePointBusinessOrganisationAbbreviationFr(servicePointBusinessOrganisation.getBusinessOrganisationAbbreviationFr())
-                .servicePointBusinessOrganisationAbbreviationIt(servicePointBusinessOrganisation.getBusinessOrganisationAbbreviationIt())
-                .servicePointBusinessOrganisationAbbreviationEn(servicePointBusinessOrganisation.getBusinessOrganisationAbbreviationEn())
-                .servicePointBusinessOrganisationDescriptionDe(servicePointBusinessOrganisation.getBusinessOrganisationDescriptionDe())
-                .servicePointBusinessOrganisationDescriptionFr(servicePointBusinessOrganisation.getBusinessOrganisationDescriptionFr())
-                .servicePointBusinessOrganisationDescriptionIt(servicePointBusinessOrganisation.getBusinessOrganisationDescriptionIt())
-                .servicePointBusinessOrganisationDescriptionEn(servicePointBusinessOrganisation.getBusinessOrganisationDescriptionEn());
-        buildGeolocation(version, builder);
-        return builder.build();
+  @Override
+  public TrafficPointVersionCsvModel process(TrafficPointElementVersion version) {
+    BusinessOrganisation servicePointBusinessOrganisation = version.getServicePointBusinessOrganisation();
+    TrafficPointVersionCsvModel.TrafficPointVersionCsvModelBuilder builder = TrafficPointVersionCsvModel.builder()
+        .sloid(version.getSloid())
+        .numberShort(version.getServicePointNumber().getNumberShort())
+        .number(version.getServicePointNumber().getNumber())
+        .uicCountryCode(version.getServicePointNumber().getUicCountryCode())
+        .validFrom(DATE_FORMATTER.format(version.getValidFrom()))
+        .validTo(DATE_FORMATTER.format(version.getValidTo()))
+        .designation(version.getDesignation())
+        .designationOperational(version.getDesignationOperational())
+        .length(version.getLength())
+        .boardingAreaHeight(version.getBoardingAreaHeight())
+        .compassDirection(version.getCompassDirection())
+        .parentSloid(version.getParentSloid())
+        .trafficPointElementType(version.getTrafficPointElementType().name())
+        .designationOfficial(version.getServicePointDesignationOfficial())
+        .creationDate(LOCAL_DATE_FORMATTER.format(version.getCreationDate()))
+        .editionDate(LOCAL_DATE_FORMATTER.format(version.getEditionDate()))
+        .parentSloidServicePoint(version.getParentSloidServicePoint())
+        .servicePointBusinessOrganisation(servicePointBusinessOrganisation.getBusinessOrganisation())
+        .servicePointBusinessOrganisationNumber(servicePointBusinessOrganisation.getBusinessOrganisationNumber())
+        .servicePointBusinessOrganisationAbbreviationDe(servicePointBusinessOrganisation.getBusinessOrganisationAbbreviationDe())
+        .servicePointBusinessOrganisationAbbreviationFr(servicePointBusinessOrganisation.getBusinessOrganisationAbbreviationFr())
+        .servicePointBusinessOrganisationAbbreviationIt(servicePointBusinessOrganisation.getBusinessOrganisationAbbreviationIt())
+        .servicePointBusinessOrganisationAbbreviationEn(servicePointBusinessOrganisation.getBusinessOrganisationAbbreviationEn())
+        .servicePointBusinessOrganisationDescriptionDe(servicePointBusinessOrganisation.getBusinessOrganisationDescriptionDe())
+        .servicePointBusinessOrganisationDescriptionFr(servicePointBusinessOrganisation.getBusinessOrganisationDescriptionFr())
+        .servicePointBusinessOrganisationDescriptionIt(servicePointBusinessOrganisation.getBusinessOrganisationDescriptionIt())
+        .servicePointBusinessOrganisationDescriptionEn(servicePointBusinessOrganisation.getBusinessOrganisationDescriptionEn());
+    buildGeolocation(version, builder);
+    return builder.build();
+  }
 
+  private void buildGeolocation(TrafficPointElementVersion version,
+      TrafficPointVersionCsvModel.TrafficPointVersionCsvModelBuilder builder) {
+    GeolocationBaseReadModel geolocation = toModel(version.getTrafficPointElementGeolocation());
+    if (geolocation != null) {
+      builder.lv95East(geolocation.getLv95().getEast())
+          .lv95North(geolocation.getLv95().getNorth())
+          .wgs84East(geolocation.getWgs84().getEast())
+          .wgs84North(geolocation.getWgs84().getNorth())
+          .height(geolocation.getHeight());
     }
-
-    private void buildGeolocation(TrafficPointElementVersion version, TrafficPointVersionCsvModel.TrafficPointVersionCsvModelBuilder builder) {
-        GeolocationBaseReadModel geolocation = toModel(version.getTrafficPointElementGeolocation());
-        if(geolocation != null) {
-          builder.lv95East(geolocation.getLv95().getEast())
-                  .lv95North(geolocation.getLv95().getNorth())
-                  .wgs84East(geolocation.getWgs84().getEast())
-                  .wgs84North(geolocation.getWgs84().getNorth())
-                  .height(geolocation.getHeight());
-        }
-    }
+  }
 
 }
