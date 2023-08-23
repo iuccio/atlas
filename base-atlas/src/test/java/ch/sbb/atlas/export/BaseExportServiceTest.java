810b6739d84e75558c0024c983235c104e000f9f
atlas <19825793+iuccio@users.noreply.github.com>
2023-08-23 12:12:47 +0200

    ATLAS-1343: Increase test coverage.

--- base-atlas/src/test/java/ch/sbb/atlas/export/BaseExportServiceTest.java
+++ base-atlas/src/test/java/ch/sbb/atlas/export/BaseExportServiceTest.java
@@ -1,5 +1,10 @@
 package ch.sbb.atlas.export;
 
+import ch.sbb.atlas.api.model.BaseVersionModel;
+import ch.sbb.atlas.export.enumeration.ExportType;
+import ch.sbb.atlas.export.model.VersionCsvModel;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -7,9 +12,12 @@ import org.mockito.Mockito;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 
 public class BaseExportServiceTest {
@@ -22,7 +30,7 @@ public class BaseExportServiceTest {
             Mockito.CALLS_REAL_METHODS);
 
     @BeforeEach
-    public void setUp() throws IOException {
+    void setUp() throws IOException {
         someUrl = new URL("http://example.com");
         file = new File("demo.txt");
     }
@@ -118,6 +126,39 @@ public class BaseExportServiceTest {
         assertEquals("http://example.com", urls.get(0).toString());
         assertEquals("http://example.com", urls.get(1).toString());
         assertEquals("http://example.com", urls.get(2).toString());
+    }
+
+    @Test
+    void shouldCreateJsonFile() throws IOException {
+        File jsonFile = new File("demo.json");
+        List<BaseVersionModel> baseVersionModelList = new ArrayList<>();
+        BaseExportService bes = Mockito.spy(baseExportService);
+
+        Mockito.doReturn(baseVersionModelList).when(bes).convertToJsonModel(any());
+        Mockito.doReturn(jsonFile).when(bes).createFile(any(), any());
+
+        File jsonFileResult = bes.createJsonFile(baseVersionModelList, ExportType.FULL);
+        assertNotNull(jsonFileResult);
+        assertEquals(jsonFile, jsonFileResult);
+        Files.delete(jsonFile.toPath());
+    }
+
+    @Test
+    void shouldCreateCsvFile() throws IOException {
+        File csvFile = new File("demo.csv");
+        List<VersionCsvModel> versionCsvModels = new ArrayList<>();
+        BaseExportService bes = Mockito.spy(baseExportService);
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectWriter writer = mapper.writer();
+
+        Mockito.doReturn(versionCsvModels).when(bes).convertToCsvModel(any());
+        Mockito.doReturn(csvFile).when(bes).createFile(any(), any());
+        Mockito.doReturn(writer).when(bes).getObjectWriter();
+
+        File csvFileResult = bes.createCsvFile(versionCsvModels, ExportType.FULL);
+        assertNotNull(csvFileResult);
+        assertEquals(csvFile, csvFileResult);
+        Files.delete(csvFile.toPath());
     }
 
 }
