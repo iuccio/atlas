c2e05d6c4571f4312deee03a9daf53e11b69bf9c
atlas <19825793+iuccio@users.noreply.github.com>
2023-08-28 09:44:24 +0200

    ATLAS-1365 added zoom & home button to map

--- frontend/src/app/pages/sepodi/map/map.component.ts
+++ frontend/src/app/pages/sepodi/map/map.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
-import { Map } from 'maplibre-gl';
+import maplibregl, { Map } from 'maplibre-gl';
 import { MapService } from './map.service';
 import { MAP_STYLES, MapStyle } from './map-options.service';
 
@@ -22,6 +22,8 @@ export class MapComponent implements AfterViewInit, OnDestroy {
 
   ngAfterViewInit() {
     this.map = this.mapService.initMap(this.mapContainer.nativeElement);
+    this.map.dragRotate.disable();
+    this.map.touchZoomRotate.disableRotation();
     this.currentMapStyle = this.mapService.currentMapStyle;
   }
 
@@ -39,5 +41,27 @@ export class MapComponent implements AfterViewInit, OnDestroy {
   switchToStyle(style: MapStyle) {
     this.currentMapStyle = this.mapService.switchToStyle(style);
     this.showMapStyleSelection = false;
+  }
+
+  zoomIn() {
+    const currentZoom = this.map.getZoom();
+    let newZoom = currentZoom + 0.75;
+    this.map.zoomTo(newZoom, { duration: 500 });
+  }
+
+  zoomOut() {
+    const currentZoom = this.map.getZoom();
+    let newZoom = currentZoom - 0.75;
+    this.map.zoomTo(newZoom, { duration: 500 });
+  }
+
+  goHome() {
+    const swissLongLat = [8.2275, 46.8182];
+
+    this.map.flyTo({
+      center: swissLongLat as maplibregl.LngLatLike,
+      zoom: 7.25,
+      speed: 0.8,
+    });
   }
 }
