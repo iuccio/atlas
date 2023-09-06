9f3d74eeafee959e087945b44f00f7385a29864d
atlas <19825793+iuccio@users.noreply.github.com>
2023-09-06 14:27:31 +0200

    ATLAS-908: transform coordinates on switch

--- frontend/src/app/pages/sepodi/geography/geography.component.ts
+++ frontend/src/app/pages/sepodi/geography/geography.component.ts
@@ -4,6 +4,10 @@ import { CoordinatePair, SpatialReference } from '../../../api';
 import { GeographyFormGroup } from './geography-form-group';
 import { CoordinateTransformationService } from './coordinate-transformation.service';
 import { Subscription } from 'rxjs';
+import { MatRadioChange } from '@angular/material/radio';
+
+export const LV95_MAX_DIGITS = 5;
+export const WGS84_MAX_DIGITS = 11;
 
 @Component({
   selector: 'sepodi-geography',
@@ -12,6 +16,9 @@ import { Subscription } from 'rxjs';
 export class GeographyComponent implements OnInit, OnDestroy {
   @Input() disabled = false;
   @Input() formGroup!: FormGroup<GeographyFormGroup>;
+
+  readonly LV95_MAX_DIGITS = LV95_MAX_DIGITS;
+  readonly WGS84_MAX_DIGITS = WGS84_MAX_DIGITS;
 
   transformedCoordinatePair?: CoordinatePair;
   private spatialReferenceSubscription!: Subscription;
@@ -58,5 +65,28 @@ export class GeographyComponent implements OnInit, OnDestroy {
       east: Number(this.formGroup.value.east!),
       north: Number(this.formGroup.value.north!),
     };
+  }
+
+  switchSpatialReference($event: MatRadioChange) {
+    const newReference: SpatialReference = $event.value;
+    const transformedCoordinatePair = this.coordinateTransformationService.transform(
+      this.currentCoordinates,
+      this.transformedSpatialReference,
+      newReference
+    );
+    this.formGroup.controls.east.setValue(
+      Number(
+        transformedCoordinatePair.east.toFixed(
+          newReference == SpatialReference.Lv95 ? this.LV95_MAX_DIGITS : this.WGS84_MAX_DIGITS
+        )
+      )
+    );
+    this.formGroup.controls.north.setValue(
+      Number(
+        transformedCoordinatePair.north.toFixed(
+          newReference == SpatialReference.Lv95 ? this.LV95_MAX_DIGITS : this.WGS84_MAX_DIGITS
+        )
+      )
+    );
   }
 }
