cb43374155f548612502e14204298ae321457372
atlas <19825793+iuccio@users.noreply.github.com>
2023-09-06 14:56:50 +0200

    ATLAS-916: improve sp search

---
+++ frontend/src/app/pages/sepodi/search-service-point/split-service-point-number.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { SplitServicePointNumberPipe } from './split-service-point-number.pipe';
+
+describe('SplitServicePointNumberPipe', () => {
+  it('create an instance', () => {
+    const pipe = new SplitServicePointNumberPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
