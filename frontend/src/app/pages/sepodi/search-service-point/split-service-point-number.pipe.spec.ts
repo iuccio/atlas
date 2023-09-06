070e9338d5f6b918ad46db8844848bdbfb5e940e
atlas <TechSupport-ATLAS@sbb.ch>
2023-09-06 22:02:32 +0200

    ATLAS-916: add tests

--- frontend/src/app/pages/sepodi/search-service-point/split-service-point-number.pipe.spec.ts
+++ frontend/src/app/pages/sepodi/search-service-point/split-service-point-number.pipe.spec.ts
@@ -5,4 +5,19 @@ describe('SplitServicePointNumberPipe', () => {
     const pipe = new SplitServicePointNumberPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should split without highlight', () => {
+    const pipe = new SplitServicePointNumberPipe();
+    expect(pipe.transform('8507000')).toBe('85 07000');
+  });
+
+  it('should split with highlight', () => {
+    const pipe = new SplitServicePointNumberPipe();
+    expect(pipe.transform('850<b>7000</b>')).toBe('85 0<b>7000</b>');
+  });
+
+  it('should split at beginning with highlight', () => {
+    const pipe = new SplitServicePointNumberPipe();
+    expect(pipe.transform('<b>85</b>07000')).toBe('<b>85 </b>07000');
+  });
 });
