4119a086e5e0fcb033a0b8e795f7fd65845217c3
atlas <19825793+iuccio@users.noreply.github.com>
2023-09-06 18:04:06 +0200

    ATLAS-1409: Dont allow same servicepointnumber

---
+++ service-point-directory/src/test/java/ch/sbb/atlas/servicepointdirectory/controller/ServicePointControllerTest.java
@@ -0,0 +1,53 @@
+package ch.sbb.atlas.servicepointdirectory.controller;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import ch.sbb.atlas.api.servicepoint.CreateServicePointVersionModel;
+import ch.sbb.atlas.servicepointdirectory.entity.ServicePointVersion;
+import ch.sbb.atlas.servicepointdirectory.exception.ServicePointNumberAlreadyExistsException;
+import ch.sbb.atlas.servicepointdirectory.service.servicepoint.ServicePointFotCommentService;
+import ch.sbb.atlas.servicepointdirectory.service.servicepoint.ServicePointImportService;
+import ch.sbb.atlas.servicepointdirectory.service.servicepoint.ServicePointService;
+import java.time.LocalDate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class ServicePointControllerTest {
+
+  @Mock
+  private ServicePointService servicePointService;
+  @Mock
+  private ServicePointFotCommentService servicePointFotCommentService;
+  @Mock
+  private ServicePointImportService servicePointImportService;
+
+  private ServicePointController servicePointController;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+    servicePointController = new ServicePointController(servicePointService, servicePointFotCommentService,
+        servicePointImportService);
+
+    when(servicePointService.save(any())).then(i -> i.getArgument(0, ServicePointVersion.class));
+  }
+
+  @Test
+  void shouldReturnConflictExceptionWhenNumberAlreadyUsed() {
+    when(servicePointService.isServicePointNumberExisting(any())).thenReturn(true);
+    CreateServicePointVersionModel servicePointVersionModel = CreateServicePointVersionModel.builder()
+        .numberWithoutCheckDigit(8507000)
+        .designationOfficial("Bern")
+        .businessOrganisation("ch:1:sboid:5846489645")
+        .validFrom(LocalDate.of(2022, 1, 1))
+        .validTo(LocalDate.of(2022, 12, 31))
+        .build();
+
+    assertThrows(ServicePointNumberAlreadyExistsException.class,
+        () -> servicePointController.createServicePoint(servicePointVersionModel));
+  }
+}
