604666f8efa66c2f64d3ce390fb8fb859806b531
atlas <19825793+iuccio@users.noreply.github.com>
2023-09-13 15:06:30 +0200

    ATLAS-1473: fix versioning

--- business-organisation-directory/src/main/java/ch/sbb/business/organisation/directory/service/BusinessOrganisationService.java
+++ business-organisation-directory/src/main/java/ch/sbb/business/organisation/directory/service/BusinessOrganisationService.java
@@ -62,6 +62,7 @@ public class BusinessOrganisationService {
   public void updateBusinessOrganisationVersion(
       BusinessOrganisationVersion currentVersion, BusinessOrganisationVersion editedVersion) {
     versionRepository.incrementVersion(currentVersion.getSboid());
+    editedVersion.setSboid(currentVersion.getSboid());
     if (editedVersion.getVersion() != null && !currentVersion.getVersion()
         .equals(editedVersion.getVersion())) {
       throw new StaleObjectStateException(BusinessOrganisationVersion.class.getSimpleName(),
@@ -99,4 +100,6 @@ public class BusinessOrganisationService {
   public void syncAllBusinessOrganisations() {
     versionRepository.findAll().forEach(businessOrganisationDistributor::saveToDistributedServices);
   }
+
+//  private BusinessOrganisationVersion copyBusinessOrganisationVersion(BusinessOrganisationVersion businessOrganisationVersion)
 }
