06de8798d2543a6bd230d61c46316dc23e4b47bd
atlas <TechSupport-ATLAS@sbb.ch>
2023-09-13 16:36:54 +0200

    ATLAS-1472: Ref

--- business-organisation-directory/src/main/java/ch/sbb/business/organisation/directory/service/BusinessOrganisationAmazonService.java
+++ business-organisation-directory/src/main/java/ch/sbb/business/organisation/directory/service/BusinessOrganisationAmazonService.java
@@ -2,42 +2,43 @@ package ch.sbb.business.organisation.directory.service;
 
 import ch.sbb.atlas.amazon.service.AmazonBucket;
 import ch.sbb.atlas.amazon.service.AmazonService;
+import ch.sbb.atlas.amazon.service.BaseAmazonFileStreamingService;
+import ch.sbb.atlas.amazon.service.FileService;
 import ch.sbb.atlas.api.AtlasApiConstants;
 import ch.sbb.atlas.export.enumeration.ExportType;
 import ch.sbb.business.organisation.directory.service.export.BusinessOrganisationExportFileName;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
-public class BusinessOrganisationAmazonService {
+public class BusinessOrganisationAmazonService extends BaseAmazonFileStreamingService {
 
-  private final AmazonService amazonService;
+  public BusinessOrganisationAmazonService(AmazonService amazonService, FileService fileService) {
+    super(amazonService, fileService);
+  }
 
   public StreamingResponseBody streamJsonFile(ExportType exportType) {
     String fileToStream = getFileToStream(exportType);
-    return amazonService.streamFile(AmazonBucket.EXPORT, fileToStream, true);
+    return streamFileAndDecompress(AmazonBucket.EXPORT, fileToStream);
   }
 
   public StreamingResponseBody streamGzipFile(ExportType exportType) {
     String fileToStream = getFileToStream(exportType);
-    return amazonService.streamFile(AmazonBucket.EXPORT, fileToStream, false);
+    return streamFile(AmazonBucket.EXPORT, fileToStream);
   }
 
   private String getFileToStream(ExportType exportType) {
     return BusinessOrganisationExportFileName.BUSINESS_ORGANISATION_VERSION.getBaseDir() + "/" +
-        getFileName(exportType);
+        getFileName(exportType) + ".json.gz";
   }
 
   public String getFileName(ExportType exportType) {
     return exportType.getFileTypePrefix() + BusinessOrganisationExportFileName.BUSINESS_ORGANISATION_VERSION.getFileName() + "_" +
-        DateTimeFormatter.ofPattern(AtlasApiConstants.DATE_FORMAT_PATTERN).format(LocalDate.now())
-        + ".json.gz";
+        DateTimeFormatter.ofPattern(AtlasApiConstants.DATE_FORMAT_PATTERN).format(LocalDate.now());
   }
 
 }
