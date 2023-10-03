0192114a14ec928783d249ea172d7e31575fd773
atlas <19825793+iuccio@users.noreply.github.com>
2023-10-03 10:30:04 +0200

    ATLAS-1347: init reference point

---
+++ prm-directory/src/main/java/ch/sbb/prm/directory/mapper/ReferencePointVersionMapper.java
@@ -0,0 +1,29 @@
+package ch.sbb.prm.directory.mapper;
+
+import ch.sbb.prm.directory.controller.ReferencePointVersionModel;
+import ch.sbb.prm.directory.entity.ReferencePointVersion;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ReferencePointVersionMapper {
+
+  public static ReferencePointVersionModel toModel(ReferencePointVersion version){
+    return ReferencePointVersionModel.builder()
+        .id(version.getId())
+        .sloid(version.getSloid())
+        .parentServicePointSloid(version.getParentServicePointSloid())
+        .number(version.getNumber())
+        .validFrom(version.getValidFrom())
+        .validTo(version.getValidTo())
+        .designation(version.getDesignation())
+        .mainReferencePoint(version.isMainReferencePoint())
+        .referencePointType(version.getReferencePointType())
+        .creator(version.getCreator())
+        .creationDate(version.getCreationDate())
+        .editor(version.getEditor())
+        .editionDate(version.getEditionDate())
+        .etagVersion(version.getVersion())
+        .build();
+  }
+
+}
---
+++ prm-directory/src/test/java/ch/sbb/prm/directory/repository/ReferencePointRepositoryTest.java
@@ -0,0 +1,44 @@
+package ch.sbb.prm.directory.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import ch.sbb.atlas.model.controller.IntegrationTest;
+import ch.sbb.prm.directory.ReferencePointTestData;
+import ch.sbb.prm.directory.entity.ReferencePointVersion;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+@IntegrationTest
+@Transactional
+class ReferencePointRepositoryTest {
+
+  private final ReferencePointRepository referencePointRepository;
+
+  @Autowired
+  ReferencePointRepositoryTest(ReferencePointRepository referencePointRepository) {
+    this.referencePointRepository = referencePointRepository;
+  }
+
+  @BeforeEach()
+  void initDB() {
+    referencePointRepository.save(ReferencePointTestData.getReferencePointVersion());
+  }
+
+  @AfterEach
+  void tearDown() {
+    referencePointRepository.deleteAll();
+  }
+
+  @Test
+  void shouldReturnStopPlaces() {
+    //when
+   List<ReferencePointVersion> result = referencePointRepository.findAll();
+   //then
+   assertThat(result).hasSize(1);
+  }
+
+}
