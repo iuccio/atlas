8014f2de7e2071f1803f5fdc43e9b7d0e0ae4111
atlas <TechSupport-ATLAS@sbb.ch>
2023-10-03 10:30:04 +0200

    ATLAS-1347: init reference point

---
+++ prm-directory/src/main/java/ch/sbb/prm/directory/controller/ReferencePointController.java
@@ -0,0 +1,21 @@
+package ch.sbb.prm.directory.controller;
+
+import ch.sbb.prm.directory.mapper.ReferencePointVersionMapper;
+import ch.sbb.prm.directory.service.ReferencePointService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class ReferencePointController implements ReferencePointApiV1 {
+
+  private final ReferencePointService referencePointService;
+
+  @Override
+  public List<ReferencePointVersionModel> getReferencePoints() {
+    return referencePointService.getAllReferencePoints().stream().map(ReferencePointVersionMapper::toModel).sorted().toList();
+  }
+}
---
+++ prm-directory/src/test/java/ch/sbb/prm/directory/ReferencePointTestData.java
@@ -0,0 +1,25 @@
+package ch.sbb.prm.directory;
+
+import ch.sbb.atlas.servicepoint.ServicePointNumber;
+import ch.sbb.prm.directory.entity.ReferencePointVersion;
+import ch.sbb.prm.directory.enumeration.ReferencePointType;
+import java.time.LocalDate;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ReferencePointTestData {
+
+  public static ReferencePointVersion getReferencePointVersion(){
+    return ReferencePointVersion.builder()
+        .sloid("ch:1.sloid:12345:1")
+        .number(ServicePointNumber.ofNumberWithoutCheckDigit(1234567))
+        .validFrom(LocalDate.of(2000, 1, 1))
+        .validTo(LocalDate.of(2000, 12, 31))
+        .designation("designation")
+        .mainReferencePoint(true)
+        .parentServicePointSloid("ch:1.sloid:12345")
+        .referencePointType(ReferencePointType.PLATFORM)
+        .build();
+  }
+
+}
