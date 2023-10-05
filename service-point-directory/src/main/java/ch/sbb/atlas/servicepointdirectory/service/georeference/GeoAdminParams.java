41abe3873b7f1607008729f2f19b3999b349375a
atlas <19825793+iuccio@users.noreply.github.com>
2023-10-05 16:02:33 +0200

    Fix code smells

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/service/georeference/GeoAdminParams.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/service/georeference/GeoAdminParams.java
@@ -6,19 +6,24 @@ import lombok.Data;
 @Data
 public class GeoAdminParams {
 
-    private final Integer sr;
-    private final String geometry;
-    private final String layers;
+  private final Integer sr;
+  private final String geometry;
+  private final String layers;
 
-    private final String geometryType = "esriGeometryPoint";
-    private final String imageDisplay = "0,0,0";
-    private final String mapExtent = "0,0,0,0";
-    private final Integer tolerance = 0;
-    private final boolean returnGeometry = false;
+  private final String geometryType;
+  private final String imageDisplay;
+  private final String mapExtent;
+  private final Integer tolerance;
+  private final boolean returnGeometry;
 
-    public GeoAdminParams(CoordinatePair coordinatePair) {
-        this.sr = coordinatePair.getSpatialReference().getWellKnownId();
-        this.geometry = coordinatePair.getEast() + "," + coordinatePair.getNorth();
-        this.layers = Layers.getLayersParam();
-    }
+  public GeoAdminParams(CoordinatePair coordinatePair) {
+    this.sr = coordinatePair.getSpatialReference().getWellKnownId();
+    this.geometry = coordinatePair.getEast() + "," + coordinatePair.getNorth();
+    this.layers = Layers.getLayersParam();
+    geometryType = "esriGeometryPoint";
+    imageDisplay = "0,0,0";
+    mapExtent = "0,0,0,0";
+    tolerance = 0;
+    returnGeometry = false;
+  }
 }
