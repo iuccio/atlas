41e83d5781e4c62c9fd1045e37996f06a88cc6ff
atlas <TechSupport-ATLAS@sbb.ch>
2023-10-05 10:49:37 +0200

    ATLAS-1347: create informationDesk with relation

--- prm-directory/src/main/java/ch/sbb/prm/directory/service/InformationDeskService.java
+++ prm-directory/src/main/java/ch/sbb/prm/directory/service/InformationDeskService.java
@@ -1,21 +1,35 @@
 package ch.sbb.prm.directory.service;
 
 import ch.sbb.prm.directory.entity.InformationDeskVersion;
+import ch.sbb.prm.directory.enumeration.ReferencePointElementType;
 import ch.sbb.prm.directory.repository.InformationDeskRepository;
+import ch.sbb.prm.directory.repository.ReferencePointRepository;
+import ch.sbb.prm.directory.repository.RelationRepository;
+import ch.sbb.prm.directory.repository.StopPlaceRepository;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@RequiredArgsConstructor
 @Service
 @Transactional
-public class InformationDeskService {
+public class InformationDeskService extends BaseRelationConnectionService<InformationDeskVersion>{
 
   private final InformationDeskRepository informationDeskRepository;
 
+  public InformationDeskService(InformationDeskRepository informationDeskRepository, StopPlaceRepository stopPlaceRepository,
+      RelationRepository relationRepository, ReferencePointRepository referencePointRepository) {
+    super(stopPlaceRepository,relationRepository,referencePointRepository);
+    this.informationDeskRepository = informationDeskRepository;
+  }
+
   public List<InformationDeskVersion> getAllInformationDesks() {
    return informationDeskRepository.findAll();
+  }
+
+  public void createInformationDesk(InformationDeskVersion version) {
+    checkStopPlaceExists(version.getParentServicePointSloid());
+    createRelation(version, ReferencePointElementType.INFORMATION_DESK);
+    informationDeskRepository.save(version);
   }
 
 }
