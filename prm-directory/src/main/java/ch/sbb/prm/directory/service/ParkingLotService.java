d481994579c7bd55fe2bbc464fd4410e0cda44ec
atlas <TechSupport-ATLAS@sbb.ch>
2023-10-05 10:58:36 +0200

    ATLAS-1347: create parkingLot with relation

--- prm-directory/src/main/java/ch/sbb/prm/directory/service/ParkingLotService.java
+++ prm-directory/src/main/java/ch/sbb/prm/directory/service/ParkingLotService.java
@@ -1,21 +1,36 @@
 package ch.sbb.prm.directory.service;
 
+import static ch.sbb.prm.directory.enumeration.ReferencePointElementType.PARKING_LOT;
+
 import ch.sbb.prm.directory.entity.ParkingLotVersion;
 import ch.sbb.prm.directory.repository.ParkingLotRepository;
+import ch.sbb.prm.directory.repository.ReferencePointRepository;
+import ch.sbb.prm.directory.repository.RelationRepository;
+import ch.sbb.prm.directory.repository.StopPlaceRepository;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@RequiredArgsConstructor
 @Service
 @Transactional
-public class ParkingLotService {
+public class ParkingLotService extends BaseRelationConnectionService<ParkingLotVersion>{
 
   private final ParkingLotRepository parkingLotRepository;
 
+  public ParkingLotService(ParkingLotRepository parkingLotRepository, StopPlaceRepository stopPlaceRepository,
+      RelationRepository relationRepository, ReferencePointRepository referencePointRepository) {
+    super(stopPlaceRepository,relationRepository,referencePointRepository);
+    this.parkingLotRepository = parkingLotRepository;
+  }
+
   public List<ParkingLotVersion> getAllParkingLots() {
    return parkingLotRepository.findAll();
+  }
+
+  public void createParkingLot(ParkingLotVersion version) {
+    checkStopPlaceExists(version.getParentServicePointSloid());
+    createRelation(version, PARKING_LOT);
+    parkingLotRepository.save(version);
   }
 
 }
