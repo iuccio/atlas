eb718240accfaf23ca684d152f1b9ab9c5d58463
atlas <19825793+iuccio@users.noreply.github.com>
2023-10-06 20:46:22 +0200

    ATLAS-1347: create Toilet from API

--- prm-directory/src/main/java/ch/sbb/prm/directory/mapper/ToiletVersionMapper.java
+++ prm-directory/src/main/java/ch/sbb/prm/directory/mapper/ToiletVersionMapper.java
@@ -1,14 +1,16 @@
 package ch.sbb.prm.directory.mapper;
 
-import ch.sbb.prm.directory.controller.model.ToiletVersionModel;
+import ch.sbb.atlas.servicepoint.ServicePointNumber;
+import ch.sbb.prm.directory.controller.model.create.CreateToiletVersionModel;
+import ch.sbb.prm.directory.controller.model.read.ReadToiletVersionModel;
 import ch.sbb.prm.directory.entity.ToiletVersion;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class ToiletVersionMapper {
 
-  public static ToiletVersionModel toModel(ToiletVersion version){
-    return ToiletVersionModel.builder()
+  public static ReadToiletVersionModel toModel(ToiletVersion version){
+    return ReadToiletVersionModel.builder()
         .id(version.getId())
         .sloid(version.getSloid())
         .parentServicePointSloid(version.getParentServicePointSloid())
@@ -23,6 +25,24 @@ public class ToiletVersionMapper {
         .editor(version.getEditor())
         .editionDate(version.getEditionDate())
         .etagVersion(version.getVersion())
+        .build();
+  }
+
+  public static ToiletVersion toEntity(CreateToiletVersionModel model){
+    return ToiletVersion.builder()
+        .id(model.getId())
+        .sloid(model.getSloid())
+        .parentServicePointSloid(model.getParentServicePointSloid())
+        .number(ServicePointNumber.ofNumberWithoutCheckDigit(model.getNumberWithoutCheckDigit()))
+        .validFrom(model.getValidFrom())
+        .validTo(model.getValidTo())
+        .designation(model.getDesignation())
+        .info(model.getInfo())
+        .wheelchairToilet(model.getWheelchairToilet())
+        .creator(model.getCreator())
+        .creationDate(model.getCreationDate())
+        .editor(model.getEditor())
+        .editionDate(model.getEditionDate())
         .build();
   }
 
