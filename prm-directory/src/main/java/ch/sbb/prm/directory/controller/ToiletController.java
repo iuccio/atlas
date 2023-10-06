eb32baf9b6a9a746d2fb30674e94776c76f6f655
atlas <19825793+iuccio@users.noreply.github.com>
2023-10-06 20:46:22 +0200

    ATLAS-1347: create Toilet from API

--- prm-directory/src/main/java/ch/sbb/prm/directory/controller/ToiletController.java
+++ prm-directory/src/main/java/ch/sbb/prm/directory/controller/ToiletController.java
@@ -1,7 +1,9 @@
 package ch.sbb.prm.directory.controller;
 
 import ch.sbb.prm.directory.api.ToiletApiV1;
-import ch.sbb.prm.directory.controller.model.ToiletVersionModel;
+import ch.sbb.prm.directory.controller.model.create.CreateToiletVersionModel;
+import ch.sbb.prm.directory.controller.model.read.ReadToiletVersionModel;
+import ch.sbb.prm.directory.entity.ToiletVersion;
 import ch.sbb.prm.directory.mapper.ToiletVersionMapper;
 import ch.sbb.prm.directory.service.ToiletService;
 import java.util.List;
@@ -17,7 +19,14 @@ public class ToiletController implements ToiletApiV1 {
   private final ToiletService toiletService;
 
   @Override
-  public List<ToiletVersionModel> getToilets() {
+  public List<ReadToiletVersionModel> getToilets() {
     return toiletService.getAllToilets().stream().map(ToiletVersionMapper::toModel).sorted().toList();
   }
+
+  @Override
+  public ReadToiletVersionModel createToiletVersion(CreateToiletVersionModel toiletVersionModel) {
+    ToiletVersion toiletVersion = toiletService.createToilet(ToiletVersionMapper.toEntity(toiletVersionModel));
+    return ToiletVersionMapper.toModel(toiletVersion);
+  }
+
 }
--- prm-directory/src/test/java/ch/sbb/prm/directory/ToiletTestData.java
+++ prm-directory/src/test/java/ch/sbb/prm/directory/ToiletTestData.java
@@ -1,6 +1,7 @@
 package ch.sbb.prm.directory;
 
 import ch.sbb.atlas.servicepoint.ServicePointNumber;
+import ch.sbb.prm.directory.controller.model.create.CreateToiletVersionModel;
 import ch.sbb.prm.directory.entity.ToiletVersion;
 import ch.sbb.prm.directory.enumeration.StandardAttributeType;
 import java.time.LocalDate;
@@ -20,7 +21,19 @@ public class ToiletTestData {
         .wheelchairToilet(StandardAttributeType.NO)
         .info("Additional information")
         .build();
+  }
 
+  public static CreateToiletVersionModel getCreateToiletVersionModel(){
+    return CreateToiletVersionModel.builder()
+        .sloid("ch:1.sloid:12345:1")
+        .numberWithoutCheckDigit(8507000)
+        .validFrom(LocalDate.of(2001, 1, 1))
+        .validTo(LocalDate.of(2001, 12, 31))
+        .parentServicePointSloid("ch:1.sloid:12345")
+        .designation("Designation")
+        .wheelchairToilet(StandardAttributeType.NO)
+        .info("Additional information")
+        .build();
   }
 
 }
