3fc81b56fb9b6ad5b588e43b99f3e807300e6c53
atlas <19825793+iuccio@users.noreply.github.com>
2023-10-10 11:32:31 +0200

    ATLAS-1402: add versioning to stop place

--- prm-directory/src/main/java/ch/sbb/prm/directory/service/StopPlaceService.java
+++ prm-directory/src/main/java/ch/sbb/prm/directory/service/StopPlaceService.java
@@ -1,10 +1,14 @@
 package ch.sbb.prm.directory.service;
 
+import ch.sbb.atlas.versioning.consumer.ApplyVersioningDeleteByIdLongConsumer;
+import ch.sbb.atlas.versioning.model.VersionedObject;
+import ch.sbb.atlas.versioning.service.VersionableService;
 import ch.sbb.prm.directory.entity.StopPlaceVersion;
 import ch.sbb.prm.directory.exception.StopPlaceDoesNotExistsException;
 import ch.sbb.prm.directory.repository.StopPlaceRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.StaleObjectStateException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class StopPlaceService {
 
   private final StopPlaceRepository stopPlaceRepository;
+  private final VersionableService versionableService;
 
   public List<StopPlaceVersion> getAllStopPlaces() {
    return stopPlaceRepository.findAll();
@@ -28,4 +33,22 @@ public class StopPlaceService {
   public StopPlaceVersion createStopPlace(StopPlaceVersion stopPlaceVersion) {
     return stopPlaceRepository.saveAndFlush(stopPlaceVersion);
   }
+
+  public StopPlaceVersion updateStopPlaceVersion(StopPlaceVersion currentVersion, StopPlaceVersion editedVersion){
+    stopPlaceRepository.incrementVersion(currentVersion.getNumber());
+    if (editedVersion.getVersion() != null && !currentVersion.getVersion().equals(editedVersion.getVersion())) {
+      throw new StaleObjectStateException(StopPlaceVersion.class.getSimpleName(), "version");
+    }
+
+    editedVersion.setSloid(currentVersion.getSloid());
+
+    List<StopPlaceVersion> existingDbVersions = stopPlaceRepository.findAllByNumberOrderByValidFrom(
+        currentVersion.getNumber());
+    List<VersionedObject> versionedObjects = versionableService.versioningObjectsDeletingNullProperties(currentVersion,
+        editedVersion, existingDbVersions);
+    versionableService.applyVersioning(StopPlaceVersion.class, versionedObjects,
+        this::createStopPlace, new ApplyVersioningDeleteByIdLongConsumer(stopPlaceRepository));
+    return currentVersion;
+  }
+
 }
