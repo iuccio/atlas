b20b15a2abc25602e620bf79002fbe7d8dd70719
atlas <19825793+iuccio@users.noreply.github.com>
2023-10-11 14:43:26 +0200

    ATLAS-1402: add versioning for parkingLot

--- prm-directory/src/main/java/ch/sbb/prm/directory/service/ParkingLotService.java
+++ prm-directory/src/main/java/ch/sbb/prm/directory/service/ParkingLotService.java
@@ -2,11 +2,17 @@ package ch.sbb.prm.directory.service;
 
 import static ch.sbb.prm.directory.enumeration.ReferencePointElementType.PARKING_LOT;
 
+import ch.sbb.atlas.servicepoint.ServicePointNumber;
+import ch.sbb.atlas.versioning.consumer.ApplyVersioningDeleteByIdLongConsumer;
+import ch.sbb.atlas.versioning.model.VersionedObject;
+import ch.sbb.atlas.versioning.service.VersionableService;
 import ch.sbb.prm.directory.entity.ParkingLotVersion;
 import ch.sbb.prm.directory.enumeration.ReferencePointElementType;
 import ch.sbb.prm.directory.repository.ParkingLotRepository;
 import ch.sbb.prm.directory.repository.ReferencePointRepository;
 import java.util.List;
+import java.util.Optional;
+import org.hibernate.StaleObjectStateException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,10 +22,13 @@ public class ParkingLotService extends RelatableService<ParkingLotVersion> {
 
   private final ParkingLotRepository parkingLotRepository;
 
+  private final VersionableService versionableService;
+
   public ParkingLotService(ParkingLotRepository parkingLotRepository, StopPlaceService stopPlaceService,
-      RelationService relationService, ReferencePointRepository referencePointRepository) {
+      RelationService relationService, ReferencePointRepository referencePointRepository, VersionableService versionableService) {
     super(stopPlaceService,relationService,referencePointRepository);
     this.parkingLotRepository = parkingLotRepository;
+    this.versionableService = versionableService;
   }
 
   @Override
@@ -33,7 +42,38 @@ public class ParkingLotService extends RelatableService<ParkingLotVersion> {
 
   public ParkingLotVersion createParkingLot(ParkingLotVersion version) {
     createRelation(version);
+    return save(version);
+  }
+
+  private ParkingLotVersion save(ParkingLotVersion version) {
     return parkingLotRepository.saveAndFlush(version);
   }
 
+  public ParkingLotVersion updateParkingLotVersion(ParkingLotVersion currentVersion, ParkingLotVersion editedVersion){
+    checkStaleObjectIntegrity(currentVersion, editedVersion);
+    editedVersion.setSloid(currentVersion.getSloid());
+    editedVersion.setNumber(currentVersion.getNumber());
+    List<ParkingLotVersion> existingDbVersions = parkingLotRepository.findAllByNumberOrderByValidFrom(
+        currentVersion.getNumber());
+    List<VersionedObject> versionedObjects = versionableService.versioningObjectsDeletingNullProperties(currentVersion,
+        editedVersion, existingDbVersions);
+    versionableService.applyVersioning(ParkingLotVersion.class, versionedObjects,
+        this::save, new ApplyVersioningDeleteByIdLongConsumer(parkingLotRepository));
+    return currentVersion;
+  }
+
+  private void checkStaleObjectIntegrity(ParkingLotVersion currentVersion, ParkingLotVersion editedVersion) {
+    parkingLotRepository.incrementVersion(currentVersion.getNumber());
+    if (editedVersion.getVersion() != null && !currentVersion.getVersion().equals(editedVersion.getVersion())) {
+      throw new StaleObjectStateException(ParkingLotVersion.class.getSimpleName(), "version");
+    }
+  }
+
+  public List<ParkingLotVersion> findAllByNumberOrderByValidFrom(ServicePointNumber number) {
+    return parkingLotRepository.findAllByNumberOrderByValidFrom(number);
+  }
+
+  public Optional<ParkingLotVersion> getPlatformVersionById(Long id) {
+    return parkingLotRepository.findById(id);
+  }
 }
--- prm-directory/src/test/java/ch/sbb/prm/directory/ParkingLotTestData.java
+++ prm-directory/src/test/java/ch/sbb/prm/directory/ParkingLotTestData.java
@@ -3,6 +3,7 @@ package ch.sbb.prm.directory;
 import ch.sbb.atlas.servicepoint.ServicePointNumber;
 import ch.sbb.prm.directory.controller.model.parkinglot.CreateParkingLotVersionModel;
 import ch.sbb.prm.directory.entity.ParkingLotVersion;
+import ch.sbb.prm.directory.entity.ParkingLotVersion.ParkingLotVersionBuilder;
 import ch.sbb.prm.directory.enumeration.BooleanOptionalAttributeType;
 import java.time.LocalDate;
 import lombok.experimental.UtilityClass;
@@ -22,6 +23,45 @@ public class ParkingLotTestData {
         .placesAvailable(BooleanOptionalAttributeType.YES)
         .prmPlacesAvailable(BooleanOptionalAttributeType.TO_BE_COMPLETED)
         .build();
+  }
+
+  public static ParkingLotVersionBuilder<?, ?> builderVersion1(){
+    return ParkingLotVersion.builder()
+        .sloid("ch:1.sloid:12345:1")
+        .number(ServicePointNumber.ofNumberWithoutCheckDigit(1234567))
+        .validFrom(LocalDate.of(2000, 1, 1))
+        .validTo(LocalDate.of(2000, 12, 31))
+        .parentServicePointSloid("ch:1.sloid:12345")
+        .designation("Designation")
+        .info("Additional information")
+        .placesAvailable(BooleanOptionalAttributeType.YES)
+        .prmPlacesAvailable(BooleanOptionalAttributeType.TO_BE_COMPLETED);
+  }
+
+  public static ParkingLotVersionBuilder<?, ?> builderVersion2(){
+    return ParkingLotVersion.builder()
+        .sloid("ch:1.sloid:12345:1")
+        .number(ServicePointNumber.ofNumberWithoutCheckDigit(1234567))
+        .validFrom(LocalDate.of(2001, 1, 1))
+        .validTo(LocalDate.of(2002, 12, 31))
+        .parentServicePointSloid("ch:1.sloid:12345")
+        .designation("Designation wrong")
+        .info("Additional information")
+        .placesAvailable(BooleanOptionalAttributeType.YES)
+        .prmPlacesAvailable(BooleanOptionalAttributeType.TO_BE_COMPLETED);
+  }
+
+  public static ParkingLotVersionBuilder<?, ?> builderVersion3(){
+    return ParkingLotVersion.builder()
+        .sloid("ch:1.sloid:12345:1")
+        .number(ServicePointNumber.ofNumberWithoutCheckDigit(1234567))
+        .validFrom(LocalDate.of(2003, 1, 1))
+        .validTo(LocalDate.of(2003, 12, 31))
+        .parentServicePointSloid("ch:1.sloid:12345")
+        .designation("Designation ok")
+        .info("Additional information")
+        .placesAvailable(BooleanOptionalAttributeType.YES)
+        .prmPlacesAvailable(BooleanOptionalAttributeType.TO_BE_COMPLETED);
   }
 
   public static CreateParkingLotVersionModel getCreateParkingLotVersionModel(){
