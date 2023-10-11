e0c24485c6e8e453bd0332917776e6d467852db2
atlas <19825793+iuccio@users.noreply.github.com>
2023-10-11 11:27:44 +0200

    ATLAS-1495: add checkdigit to csvs

--- export-service-point/src/main/java/ch/sbb/exportservice/writer/CsvTrafficPointElementVersionWriter.java
+++ export-service-point/src/main/java/ch/sbb/exportservice/writer/CsvTrafficPointElementVersionWriter.java
@@ -10,7 +10,7 @@ public class CsvTrafficPointElementVersionWriter extends BaseCsvWriter<TrafficPo
   @Override
   String[] getCsvHeader() {
     return new String[]{Fields.sloid, Fields.numberShort, Fields.uicCountryCode,
-        Fields.number, Fields.validFrom, Fields.validTo, Fields.designation,
+        Fields.number, Fields.checkDigit, Fields.validFrom, Fields.validTo, Fields.designation,
         Fields.designationOperational, Fields.length, Fields.boardingAreaHeight, Fields.compassDirection, Fields.parentSloid,
         Fields.trafficPointElementType, Fields.lv95East, Fields.lv95North, Fields.wgs84East, Fields.wgs84North,
         Fields.height, Fields.creationDate, Fields.editionDate, Fields.parentSloidServicePoint, Fields.designationOfficial,
