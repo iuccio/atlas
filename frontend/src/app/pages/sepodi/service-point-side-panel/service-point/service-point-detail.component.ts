6c6f8b9af4b9afaf8dc9ae994bd688e92323fd9a
atlas <TechSupport-ATLAS@sbb.ch>
2023-10-12 17:14:27 +0200

    ATLAS-875: Populate bp-k master when bp-s checked.

--- frontend/src/app/pages/sepodi/service-point-side-panel/service-point/service-point-detail.component.ts
+++ frontend/src/app/pages/sepodi/service-point-side-panel/service-point/service-point-detail.component.ts
@@ -432,12 +432,12 @@ export class ServicePointDetailComponent implements OnInit, OnDestroy, DetailFor
       this.form.controls.operatingPointRouteNetwork.setValue(true);
       this.form.controls.operatingPointKilometer.setValue(true);
       this.form.controls.operatingPointKilometer.disabled;
-      // this.form.controls.operatingPointKilometerMaster.setValue(this.selectedVersion.number.number);
+      this.form.controls.operatingPointKilometerMaster.setValue(this.selectedVersion.number.number);
       this.disableBpk = true;
     } else {
       this.form.controls.operatingPointRouteNetwork.setValue(false);
       this.form.controls.operatingPointKilometer.setValue(false);
-      // this.form.controls.operatingPointKilometerMaster.setValue(undefined);
+      this.form.controls.operatingPointKilometerMaster.setValue(undefined);
       this.disableBpk = false;
     }
   }
@@ -445,10 +445,10 @@ export class ServicePointDetailComponent implements OnInit, OnDestroy, DetailFor
   setOperatingPointKilometer(select: MatCheckboxChange) {
     if (select.checked) {
       this.form.controls.operatingPointKilometer.setValue(true);
-      // this.form.controls.operatingPointKilometerMaster.setValue(this.selectedVersion.number.number);
+      this.form.controls.operatingPointKilometerMaster.setValue(this.selectedVersion.number.number);
     } else {
       this.form.controls.operatingPointKilometer.setValue(false);
-      // this.form.controls.operatingPointKilometerMaster.setValue(undefined);
+      this.form.controls.operatingPointKilometerMaster.setValue(undefined);
     }
   }
 }
