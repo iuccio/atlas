98d4c972bb537aac1e6fa8c7af13629555f4f0e4
atlas <19825793+iuccio@users.noreply.github.com>
2023-10-17 15:34:25 +0200

    ATLAS-1421: filter sequential equals and equals versions

--- import-service-point/src/main/java/ch/sbb/importservice/service/csv/StopPlaceCsvService.java
+++ import-service-point/src/main/java/ch/sbb/importservice/service/csv/StopPlaceCsvService.java
@@ -1,14 +1,16 @@
 package ch.sbb.importservice.service.csv;
 
+import static java.util.Comparator.comparing;
+
 import ch.sbb.atlas.imports.prm.BasePrmCsvModel;
 import ch.sbb.atlas.imports.prm.stopplace.StopPlaceCsvModel;
 import ch.sbb.atlas.imports.prm.stopplace.StopPlaceCsvModelContainer;
+import ch.sbb.atlas.versioning.date.DateHelper;
 import ch.sbb.importservice.service.FileHelperService;
 import ch.sbb.importservice.service.JobHelperService;
 import ch.sbb.importservice.utils.JobDescriptionConstants;
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -26,6 +28,7 @@ public class StopPlaceCsvService extends CsvService<StopPlaceCsvModel> {
   protected StopPlaceCsvService(FileHelperService fileHelperService, JobHelperService jobHelperService) {
     super(fileHelperService, jobHelperService);
   }
+
   @Override
   protected CsvFileNameModel defineCsvFileName() {
     return CsvFileNameModel.builder().fileName(PRM_STOP_PLACES_FILE_NAME).addDateToPostfix(true).build();
@@ -51,30 +54,49 @@ public class StopPlaceCsvService extends CsvService<StopPlaceCsvModel> {
     List<StopPlaceCsvModel> activeStopPlaces = filterActiveStopPlaces(stopPlaceCsvModels);
     Map<Integer, List<StopPlaceCsvModel>> stopPlaceGroupedByDidokCode = activeStopPlaces.stream()
         .collect(Collectors.groupingBy(StopPlaceCsvModel::getDidokCode));
-    List<StopPlaceCsvModelContainer> servicePointCsvModelContainers = new ArrayList<>();
+    List<StopPlaceCsvModelContainer> stopPlaceCsvModelContainers = new ArrayList<>();
     stopPlaceGroupedByDidokCode.forEach((key, value) -> {
       StopPlaceCsvModelContainer servicePointCsvModelContainer = StopPlaceCsvModelContainer.builder()
           .didokCode(key)
           .stopPlaceCsvModels(value)
           .build();
       value.sort(Comparator.comparing(BasePrmCsvModel::getValidFrom));
-      servicePointCsvModelContainers.add(servicePointCsvModelContainer);
+      stopPlaceCsvModelContainers.add(servicePointCsvModelContainer);
     });
-    return servicePointCsvModelContainers;
+
+    mergeSequentialEqualsVersions(stopPlaceCsvModelContainers);
+    mergeEqualsVersions(stopPlaceCsvModelContainers);
+    return stopPlaceCsvModelContainers;
+  }
+
+  private void mergeSequentialEqualsVersions(List<StopPlaceCsvModelContainer> servicePointCsvModelContainers) {
+    log.info("Starting checking sequential equals StopPlace versions...");
+    List<Integer> mergedDidokNumbers = new ArrayList<>();
+    servicePointCsvModelContainers.forEach(
+        container -> container.setStopPlaceCsvModels(mergeSequentialEqualsStopPlaceVersions(container.getStopPlaceCsvModels(),
+            mergedDidokNumbers)));
+    log.info("Total Merged sequential StopPlace versions {}", mergedDidokNumbers.size());
+    log.info("Merged StopPlace Didok numbers {}", mergedDidokNumbers);
+  }
+
+  private void mergeEqualsVersions(List<StopPlaceCsvModelContainer> servicePointCsvModelContainers) {
+    log.info("Starting checking equals StopPlace versions...");
+    List<Integer> mergedDidokNumbers = new ArrayList<>();
+    servicePointCsvModelContainers.forEach(
+        container -> container.setStopPlaceCsvModels(mergeEqualsStopPlaceVersions(container.getStopPlaceCsvModels(),
+            mergedDidokNumbers)));
+    log.info("Total Merged equals StopPlace versions {}", mergedDidokNumbers.size());
+    log.info("Merged equals StopPlace Didok numbers {}", mergedDidokNumbers);
   }
 
   private static List<StopPlaceCsvModel> filterActiveStopPlaces(List<StopPlaceCsvModel> models) {
     List<StopPlaceCsvModel> stopPlaceCsvModels = removeWrongMeansOfTransportCode(models);
     log.info("Found and removed {} StopPlace versions with [~0~] TRANSPORTATION_MEANS code.",
-        models.size()-stopPlaceCsvModels.size());
+        models.size() - stopPlaceCsvModels.size());
     List<StopPlaceCsvModel> activeStopPlaceVersions = getActiveStopPlaceVersions(stopPlaceCsvModels);
     log.info("Found and removed {} inactive (STATUS=0) StopPlace versions.",
-        stopPlaceCsvModels.size() - activeStopPlaceVersions.size() );
-    List<StopPlaceCsvModel> distinctActiveStopPlaceCsvModels = removeDuplicatedVersions(activeStopPlaceVersions);
-    log.info("Found and removed {} duplicated StopPlace versions.",
-        activeStopPlaceVersions.size() - distinctActiveStopPlaceCsvModels.size() );
-    log.info("Found {} StopPlace versions to be imported.",distinctActiveStopPlaceCsvModels.size());
-    return distinctActiveStopPlaceCsvModels;
+        stopPlaceCsvModels.size() - activeStopPlaceVersions.size());
+    return activeStopPlaceVersions;
   }
 
   private static List<StopPlaceCsvModel> getActiveStopPlaceVersions(List<StopPlaceCsvModel> stopPlaceCsvModels) {
@@ -82,12 +104,71 @@ public class StopPlaceCsvService extends CsvService<StopPlaceCsvModel> {
         .filter(stopPlaceCsvModel -> stopPlaceCsvModel.getStatus() == ACTIVE_STATUS).toList();
   }
 
-  private static List<StopPlaceCsvModel> removeDuplicatedVersions(List<StopPlaceCsvModel> stopPlaceCsvModels) {
-    return new ArrayList<>(new HashSet<>(stopPlaceCsvModels));
-  }
   private static List<StopPlaceCsvModel> removeWrongMeansOfTransportCode(List<StopPlaceCsvModel> stopPlaceCsvModels) {
     return stopPlaceCsvModels.stream()
-        .filter(stopPlaceCsvModel -> !stopPlaceCsvModel.getTransportationMeans().equals(UNKNOWN_MEANS_OF_TRANSPORT_CODE)).toList();
+        .filter(stopPlaceCsvModel -> !stopPlaceCsvModel.getTransportationMeans().equals(UNKNOWN_MEANS_OF_TRANSPORT_CODE))
+        .toList();
+  }
+
+  public List<StopPlaceCsvModel> mergeSequentialEqualsStopPlaceVersions(List<StopPlaceCsvModel> stopPlaceCsvModels,
+      List<Integer> mergedDidokNumbers) {
+    List<StopPlaceCsvModel> stopPlaceCsvModelListMerged = new ArrayList<>();
+    if (stopPlaceCsvModels.size() == 1){
+        return stopPlaceCsvModels;
+    }
+    if (stopPlaceCsvModels.size() > 1) {
+      stopPlaceCsvModels.sort(comparing(StopPlaceCsvModel::getValidFrom));
+       stopPlaceCsvModelListMerged = new ArrayList<>(List.of(stopPlaceCsvModels.get(0)));
+      for (int currentIndex = 1; currentIndex < stopPlaceCsvModels.size(); currentIndex++) {
+        final StopPlaceCsvModel previous = stopPlaceCsvModelListMerged.get(stopPlaceCsvModelListMerged.size() - 1);
+        final StopPlaceCsvModel current = stopPlaceCsvModels.get(currentIndex);
+        if (DateHelper.areDatesSequential(previous.getValidTo(), current.getValidFrom())
+            && current.equals(previous)) {
+          removeCurrentVersionIncreaseNextValidTo(previous, current);
+          mergedDidokNumbers.add(current.getDidokCode());
+        } else {
+          stopPlaceCsvModelListMerged.add(current);
+        }
+      }
+    }
+    return stopPlaceCsvModelListMerged;
+  }
+
+  public List<StopPlaceCsvModel> mergeEqualsStopPlaceVersions(List<StopPlaceCsvModel> stopPlaceCsvModels,
+      List<Integer> mergedDidokNumbers) {
+    List<StopPlaceCsvModel> stopPlaceCsvModelListMerged = new ArrayList<>();
+    if (stopPlaceCsvModels.size() == 1){
+      return stopPlaceCsvModels;
+    }
+    if (stopPlaceCsvModels.size() > 1) {
+      stopPlaceCsvModels.sort(comparing(StopPlaceCsvModel::getValidFrom));
+      stopPlaceCsvModelListMerged = new ArrayList<>(
+          List.of(stopPlaceCsvModels.get(0))
+      );
+      for (int currentIndex = 1; currentIndex < stopPlaceCsvModels.size(); currentIndex++) {
+        final StopPlaceCsvModel previous = stopPlaceCsvModelListMerged.get(stopPlaceCsvModelListMerged.size() - 1);
+        final StopPlaceCsvModel current = stopPlaceCsvModels.get(currentIndex);
+        if (current.getValidFrom().isEqual(previous.getValidFrom()) && current.getValidTo().isEqual(previous.getValidTo())
+            && current.equals(previous)) {
+          log.info("Found duplicated version with number {}", previous.getDidokCode());
+          log.info("Version-1 [{}]-[{}]", previous.getValidFrom(), previous.getValidTo());
+          log.info("Version-2 [{}]-[{}]", current.getValidFrom(), current.getValidTo());
+          mergedDidokNumbers.add(current.getDidokCode());
+        } else {
+          stopPlaceCsvModelListMerged.add(current);
+        }
+      }
+    }
+    return stopPlaceCsvModelListMerged;
+  }
+
+  private void removeCurrentVersionIncreaseNextValidTo(StopPlaceCsvModel previous,
+      StopPlaceCsvModel current) {
+    log.info("Found versions to merge with number {}", previous.getDidokCode());
+    log.info("Version-1 [{}]-[{}]", previous.getValidFrom(), previous.getValidTo());
+    log.info("Version-2 [{}]-[{}]", current.getValidFrom(), current.getValidTo());
+    previous.setValidTo(current.getValidTo());
+    log.info("Version merged [{}]-[{}]", previous.getValidFrom(), current.getValidTo());
   }
 
 }
