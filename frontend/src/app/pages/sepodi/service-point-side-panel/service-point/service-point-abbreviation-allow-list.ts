6b23f1f20ba875d044282c6d5bccf2f05638965e
atlas <TechSupport-ATLAS@sbb.ch>
2023-10-17 13:46:06 +0200

    ATLAS-1130 ref

---
+++ frontend/src/app/pages/sepodi/service-point-side-panel/service-point/service-point-abbreviation-allow-list.ts
@@ -0,0 +1,24 @@
+export class ServicePointAbbreviationAllowList {
+  public static readonly SBOIDS: string[] = [
+    'ch:1:sboid:100016',
+    'ch:1:sboid:100015',
+    'ch:1:sboid:100026',
+    'ch:1:sboid:100609',
+    'ch:1:sboid:100034',
+    'ch:1:sboid:100061',
+    'ch:1:sboid:100879',
+    'ch:1:sboid:100075',
+    'ch:1:sboid:100871',
+    'ch:1:sboid:100033',
+    'ch:1:sboid:100874',
+    'ch:1:sboid:100650',
+    'ch:1:sboid:100056',
+    'ch:1:sboid:100001',
+    'ch:1:sboid:100878',
+    'ch:1:sboid:100872',
+    'ch:1:sboid:100875',
+    'ch:1:sboid:100866',
+    'ch:1:sboid:100042',
+    'ch:1:sboid:100053',
+  ];
+}
