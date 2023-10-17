8519e2390fb4f179403ea718db0dd9dee7529531
atlas <19825793+iuccio@users.noreply.github.com>
2023-10-17 12:10:00 +0200

    ATLAS-875: Remove non used code.

--- frontend/src/app/pages/sepodi/service-point-side-panel/service-point/service-point-detail-form-group.ts
+++ frontend/src/app/pages/sepodi/service-point-side-panel/service-point/service-point-detail-form-group.ts
@@ -117,7 +117,6 @@ export class ServicePointFormGroupBuilder {
       [DateRangeValidator.fromGreaterThenTo('validFrom', 'validTo')],
     );
     this.initConditionalValidators(formGroup);
-    // this.initOperationgPoint(formGroup);
     return formGroup;
   }
 
@@ -318,21 +317,5 @@ export class ServicePointFormGroupBuilder {
       return form.value.operatingPointType! as OperatingPointType;
     }
     return undefined;
-  }
-
-  private static initOperationgPoint(formGroup: FormGroup<ServicePointDetailFormGroup>) {
-    formGroup.controls.operatingPointRouteNetwork.valueChanges.subscribe((value) => {
-      if (value) {
-        formGroup.controls.operatingPointKilometer.setValue(true);
-        formGroup.controls.operatingPointKilometer.disable();
-        formGroup.controls.operatingPointKilometerMaster.setValue(formGroup.controls.number.value);
-        formGroup.controls.operatingPointKilometerMaster.disable();
-      } else {
-        formGroup.controls.operatingPointKilometer.reset();
-        formGroup.controls.operatingPointKilometer.enable();
-        formGroup.controls.operatingPointKilometerMaster.reset();
-        formGroup.controls.operatingPointKilometerMaster.enable();
-      }
-    });
   }
 }
