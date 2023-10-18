ee9c8132dc10a62fddcefa47b0ee14aac81493c7
atlas <19825793+iuccio@users.noreply.github.com>
2023-10-18 19:42:05 +0200

    ATLAS-875: WIP implement backend check for bps and bpk.

---
+++ service-point-directory/src/test/java/ch/sbb/atlas/servicepointdirectory/service/TimelineTest.java
@@ -0,0 +1,115 @@
+package ch.sbb.atlas.servicepointdirectory.service;
+
+import ch.sbb.atlas.servicepointdirectory.ServicePointVersionsTimelineTestData;
+import ch.sbb.atlas.servicepointdirectory.entity.ServicePointVersion;
+import ch.sbb.atlas.servicepointdirectory.service.servicepoint.Timeline;
+import org.junit.jupiter.api.Test;
+import org.locationtech.jts.util.Assert;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TimelineTest {
+
+    @Test
+    void bpsTimelinesCombinationWithAndWithoutGaps() {
+        List<ServicePointVersion> servicePointVersionList = new ArrayList<>();
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel1());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel2());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel3());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel4());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel5());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel6());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel7());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel8());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel9());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel10());
+
+        Timeline timeline = new Timeline(servicePointVersionList);
+        List<Timeline.TimelineElement> mergedTimeline = timeline.getMergedTimeline();
+        Assert.equals(6, mergedTimeline.size());
+        Assert.equals(LocalDate.of(2010, 12, 11), mergedTimeline.get(0).getStartDate());
+        Assert.equals(LocalDate.of(2013, 8, 10), mergedTimeline.get(0).getEndDate());
+        Assert.equals(LocalDate.of(2013, 8, 14), mergedTimeline.get(1).getStartDate());
+        Assert.equals(LocalDate.of(2014, 8, 14), mergedTimeline.get(1).getEndDate());
+        Assert.equals(LocalDate.of(2014, 8, 18), mergedTimeline.get(2).getStartDate());
+        Assert.equals(LocalDate.of(2015, 8, 18), mergedTimeline.get(2).getEndDate());
+        Assert.equals(LocalDate.of(2015, 8, 20), mergedTimeline.get(3).getStartDate());
+        Assert.equals(LocalDate.of(2018, 8, 20), mergedTimeline.get(3).getEndDate());
+        Assert.equals(LocalDate.of(2018, 8, 25), mergedTimeline.get(4).getStartDate());
+        Assert.equals(LocalDate.of(2019, 8, 20), mergedTimeline.get(4).getEndDate());
+        Assert.equals(LocalDate.of(2019, 8, 25), mergedTimeline.get(5).getStartDate());
+        Assert.equals(LocalDate.of(2020, 8, 20), mergedTimeline.get(5).getEndDate());
+    }
+
+    @Test
+    void bpsTimelinesWithNull() {
+
+        Timeline timeline = new Timeline(null);
+        List<Timeline.TimelineElement> mergedTimeline = timeline.getMergedTimeline();
+        Assert.equals(0, mergedTimeline.size());
+    }
+
+    @Test
+    void bpsTimelinesWithEmptyList() {
+        List<ServicePointVersion> servicePointVersionList = new ArrayList<>();
+
+        Timeline timeline = new Timeline(servicePointVersionList);
+        List<Timeline.TimelineElement> mergedTimeline = timeline.getMergedTimeline();
+        Assert.equals(0, mergedTimeline.size());
+    }
+
+    @Test
+    void bpsTimelinesWithGapsBetween() {
+        List<ServicePointVersion> servicePointVersionList = new ArrayList<>();
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel1());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel3());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel5());
+
+        Timeline timeline = new Timeline(servicePointVersionList);
+        List<Timeline.TimelineElement> mergedTimeline = timeline.getMergedTimeline();
+        Assert.equals(3, mergedTimeline.size());
+        Assert.equals(LocalDate.of(2010, 12, 11), mergedTimeline.get(0).getStartDate());
+        Assert.equals(LocalDate.of(2011, 8, 10), mergedTimeline.get(0).getEndDate());
+        Assert.equals(LocalDate.of(2012, 8, 11), mergedTimeline.get(1).getStartDate());
+        Assert.equals(LocalDate.of(2013, 8, 10), mergedTimeline.get(1).getEndDate());
+        Assert.equals(LocalDate.of(2014, 8, 18), mergedTimeline.get(2).getStartDate());
+        Assert.equals(LocalDate.of(2015, 8, 18), mergedTimeline.get(2).getEndDate());
+    }
+
+    @Test
+    void bpsTimelinesWithGapAfterFirstTimeline() {
+        List<ServicePointVersion> servicePointVersionList = new ArrayList<>();
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel0());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel1());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel2());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel3());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel4());
+
+        Timeline timeline = new Timeline(servicePointVersionList);
+        List<Timeline.TimelineElement> mergedTimeline = timeline.getMergedTimeline();
+        Assert.equals(3, mergedTimeline.size());
+        Assert.equals(LocalDate.of(2008, 12, 11), mergedTimeline.get(0).getStartDate());
+        Assert.equals(LocalDate.of(2009, 8, 10), mergedTimeline.get(0).getEndDate());
+        Assert.equals(LocalDate.of(2010, 12, 11), mergedTimeline.get(1).getStartDate());
+        Assert.equals(LocalDate.of(2013, 8, 10), mergedTimeline.get(1).getEndDate());
+        Assert.equals(LocalDate.of(2013, 8, 14), mergedTimeline.get(2).getStartDate());
+        Assert.equals(LocalDate.of(2014, 8, 14), mergedTimeline.get(2).getEndDate());
+    }
+
+    @Test
+    void bpsTimelinesWithoutGapsBetween() {
+        List<ServicePointVersion> servicePointVersionList = new ArrayList<>();
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel1());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel2());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel3());
+
+        Timeline timeline = new Timeline(servicePointVersionList);
+        List<Timeline.TimelineElement> mergedTimeline = timeline.getMergedTimeline();
+        Assert.equals(1, mergedTimeline.size());
+        Assert.equals(LocalDate.of(2010, 12, 11), mergedTimeline.get(0).getStartDate());
+        Assert.equals(LocalDate.of(2013, 8, 10), mergedTimeline.get(0).getEndDate());
+    }
+
+}
---
+++ service-point-directory/src/test/java/ch/sbb/atlas/servicepointdirectory/service/servicepoint/ServicePointServiceTimelinesTest.java
@@ -0,0 +1,198 @@
+package ch.sbb.atlas.servicepointdirectory.service.servicepoint;
+
+import ch.sbb.atlas.servicepointdirectory.ServicePointVersionsTimelineTestData;
+import ch.sbb.atlas.servicepointdirectory.entity.ServicePointVersion;
+import ch.sbb.atlas.servicepointdirectory.repository.ServicePointSearchVersionRepository;
+import ch.sbb.atlas.servicepointdirectory.repository.ServicePointVersionRepository;
+import ch.sbb.atlas.versioning.service.VersionableService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.locationtech.jts.util.Assert;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServicePointServiceTimelinesTest {
+
+    private ServicePointService servicePointService;
+
+    @Mock
+    private ServicePointVersionRepository servicePointVersionRepositoryMock;
+
+    @Mock
+    private VersionableService versionableServiceMock;
+
+    @Mock
+    private ServicePointValidationService servicePointValidationService;
+
+    @Mock
+    private ServicePointSearchVersionRepository servicePointSearchVersionRepository;
+
+    @BeforeEach
+    void initMocksAndService() {
+        MockitoAnnotations.openMocks(this);
+        servicePointService = new ServicePointService(servicePointVersionRepositoryMock, versionableServiceMock,
+                servicePointValidationService, servicePointSearchVersionRepository);
+    }
+
+    @Test
+    void testScenarion1BpkIsEquallyLongAsOneBpsVersion() {
+        List<ServicePointVersion> servicePointVersionList = new ArrayList<>();
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel0());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel1());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel2());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel3());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel4());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel5());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel6());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel7());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel8());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel9());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel10());
+
+        ServicePointVersion servicePointVersion = ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel4();
+
+        boolean result = servicePointService.checkIfBpsCanBeAssignedToBpk(servicePointVersionList, servicePointVersion);
+        Assert.equals(true, result);
+    }
+
+    @Test
+    void testScenarion2BpkValidToIsLongerThanBpsVersionValidTo() {
+        List<ServicePointVersion> servicePointVersionList = new ArrayList<>();
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel0());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel1());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel2());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel3());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel4());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel5());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel6());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel7());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel8());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel9());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel10());
+
+        ServicePointVersion servicePointVersion = ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel4();
+        servicePointVersion.setValidTo(LocalDate.of(2014, 8, 18));
+
+        boolean result = servicePointService.checkIfBpsCanBeAssignedToBpk(servicePointVersionList, servicePointVersion);
+        Assert.equals(false, result);
+    }
+
+    @Test
+    void testScenarion3BpkValidFromIsBeforeThanBpsVersionValidFrom() {
+        List<ServicePointVersion> servicePointVersionList = new ArrayList<>();
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel0());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel1());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel2());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel3());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel4());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel5());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel6());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel7());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel8());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel9());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel10());
+
+        ServicePointVersion servicePointVersion = ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel4();
+        servicePointVersion.setValidFrom(LocalDate.of(2013, 8, 12));
+
+        boolean result = servicePointService.checkIfBpsCanBeAssignedToBpk(servicePointVersionList, servicePointVersion);
+        Assert.equals(false, result);
+    }
+
+    @Test
+    void testScenarion4BpkTimelineIsInsideOfBpsTimeline() {
+        List<ServicePointVersion> servicePointVersionList = new ArrayList<>();
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel0());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel1());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel2());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel3());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel4());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel5());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel6());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel7());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel8());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel9());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel10());
+
+        ServicePointVersion servicePointVersion = ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel4();
+        servicePointVersion.setValidFrom(LocalDate.of(2013, 8, 20));
+        servicePointVersion.setValidTo(LocalDate.of(2014, 8, 10));
+
+        boolean result = servicePointService.checkIfBpsCanBeAssignedToBpk(servicePointVersionList, servicePointVersion);
+        Assert.equals(true, result);
+    }
+
+    @Test
+    void testScenarion5BpkTimelineIsInsideOfBpsTimelines() {
+        List<ServicePointVersion> servicePointVersionList = new ArrayList<>();
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel0());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel1());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel2());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel3());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel4());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel5());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel6());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel7());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel8());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel9());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel10());
+
+        ServicePointVersion servicePointVersion = ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel2();
+        servicePointVersion.setValidFrom(LocalDate.of(2011, 1, 1));
+        servicePointVersion.setValidTo(LocalDate.of(2013, 1, 1));
+
+        boolean result = servicePointService.checkIfBpsCanBeAssignedToBpk(servicePointVersionList, servicePointVersion);
+        Assert.equals(true, result);
+    }
+
+    @Test
+    void testScenarion6BpsTimelineWithAGapWhereBpkTimeline() {
+        List<ServicePointVersion> servicePointVersionList = new ArrayList<>();
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel0());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel1());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel2());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel3());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel4());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel5());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel6());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel7());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel8());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel9());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel10());
+
+        ServicePointVersion servicePointVersion = ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel4();
+        servicePointVersion.setValidFrom(LocalDate.of(2013, 8, 1));
+        servicePointVersion.setValidTo(LocalDate.of(2014, 8, 1));
+
+        boolean result = servicePointService.checkIfBpsCanBeAssignedToBpk(servicePointVersionList, servicePointVersion);
+        Assert.equals(false, result);
+    }
+
+    @Test
+    void testScenarion7BpsTimelineWithAGapWhereBpkTimeline() {
+        List<ServicePointVersion> servicePointVersionList = new ArrayList<>();
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel0());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel1());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel2());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel3());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel4());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel5());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel6());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel7());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel8());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel9());
+        servicePointVersionList.add(ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel10());
+
+        ServicePointVersion servicePointVersion = ServicePointVersionsTimelineTestData.getAargauServicePointVersionModel4();
+        servicePointVersion.setValidFrom(LocalDate.of(2013, 8, 15));
+        servicePointVersion.setValidTo(LocalDate.of(2014, 8, 20));
+
+        boolean result = servicePointService.checkIfBpsCanBeAssignedToBpk(servicePointVersionList, servicePointVersion);
+        Assert.equals(false, result);
+    }
+
+}
