39cc2318bd065f642af7dedcc5b9718ab559677f
atlas <19825793+iuccio@users.noreply.github.com>
2023-10-31 22:19:51 +0100

    ATLAS-1358: Implement prm user rights backend, initial commit.

---
+++ base-atlas/src/main/java/ch/sbb/atlas/api/model/SboidAssociated.java
@@ -0,0 +1,9 @@
+package ch.sbb.atlas.api.model;
+
+import java.util.Set;
+
+public interface SboidAssociated {
+
+    Set<String> getSboids();
+
+}
--- prm-directory/src/main/java/ch/sbb/prm/directory/controller/InformationDeskController.java
+++ prm-directory/src/main/java/ch/sbb/prm/directory/controller/InformationDeskController.java
@@ -1,16 +1,17 @@
 package ch.sbb.prm.directory.controller;
 
-import ch.sbb.atlas.model.exception.NotFoundException.IdNotFoundException;
-import ch.sbb.prm.directory.api.InformationDeskApiV1;
 import ch.sbb.atlas.api.prm.model.informationdesk.CreateInformationDeskVersionModel;
 import ch.sbb.atlas.api.prm.model.informationdesk.ReadInformationDeskVersionModel;
+import ch.sbb.atlas.model.exception.NotFoundException.IdNotFoundException;
+import ch.sbb.prm.directory.api.InformationDeskApiV1;
 import ch.sbb.prm.directory.entity.InformationDeskVersion;
 import ch.sbb.prm.directory.mapper.InformationDeskVersionMapper;
 import ch.sbb.prm.directory.service.InformationDeskService;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -26,6 +27,11 @@ public class InformationDeskController implements InformationDeskApiV1 {
 
   @Override
   public ReadInformationDeskVersionModel createInformationDesk(CreateInformationDeskVersionModel model) {
+    // here I need to get all servicePontVersions, or more precisely sharedServicePoints and to pass this one to service.createInformationDesk
+    // out of sharedServicePoints I need to extract only for each version from String servicePoint, which looks like this {"servicePointSloid":"ch:1:sloid:6","sboids":["ch:1:sboid:100313"],"trafficPointSloids":[]}
+    // I need per each version only sboids ch:1:sboid:101698 and then to put it in ServicePointVersion, or some smaller Entity which implements CountryAndBusinessOrganisationAssociated and then I can pass it to CountryAndBusinessOrganisationBasedUserAdministrationService
+    // then there I can adjust check only for bo and without validFrom, validTo
+    // maybe I can also implement different interface CountryAndBusinessOrganisationAssociated and write different CountryAndBusinessOrganisationBasedUserAdministrationService, but I don't think it is necessary
     InformationDeskVersion informationDeskVersion = informationDeskService.createInformationDesk(
         InformationDeskVersionMapper.toEntity(model));
     return InformationDeskVersionMapper.toModel(informationDeskVersion);
