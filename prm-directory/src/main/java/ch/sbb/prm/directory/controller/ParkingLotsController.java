0bfe3d4ee6d8288f90bfe0bbedb7d8465120caa1
atlas <TechSupport-ATLAS@sbb.ch>
2023-11-06 21:29:37 +0100

    ATLAS-1358: Add user rights check on Parking Lot.

--- prm-directory/src/main/java/ch/sbb/prm/directory/controller/ParkingLotsController.java
+++ prm-directory/src/main/java/ch/sbb/prm/directory/controller/ParkingLotsController.java
@@ -1,16 +1,19 @@
 package ch.sbb.prm.directory.controller;
 
-import ch.sbb.atlas.model.exception.NotFoundException.IdNotFoundException;
-import ch.sbb.prm.directory.api.ParkingLotApiV1;
 import ch.sbb.atlas.api.prm.model.parkinglot.CreateParkingLotVersionModel;
 import ch.sbb.atlas.api.prm.model.parkinglot.ReadParkingLotVersionModel;
+import ch.sbb.atlas.model.exception.NotFoundException.IdNotFoundException;
+import ch.sbb.atlas.servicepoint.SharedServicePointVersionModel;
+import ch.sbb.prm.directory.api.ParkingLotApiV1;
 import ch.sbb.prm.directory.entity.ParkingLotVersion;
 import ch.sbb.prm.directory.mapper.ParkingLotVersionMapper;
 import ch.sbb.prm.directory.service.ParkingLotService;
-import java.util.List;
+import ch.sbb.prm.directory.service.SharedServicePointService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -19,6 +22,8 @@ public class ParkingLotsController implements ParkingLotApiV1 {
 
   private final ParkingLotService parkingLotService;
 
+  private final SharedServicePointService sharedServicePointService;
+
   @Override
   public List<ReadParkingLotVersionModel> getParkingLots() {
     return parkingLotService.getAllParkingLots().stream().map(ParkingLotVersionMapper::toModel).toList();
@@ -26,7 +31,9 @@ public class ParkingLotsController implements ParkingLotApiV1 {
 
   @Override
   public ReadParkingLotVersionModel createParkingLot(CreateParkingLotVersionModel model) {
-    ParkingLotVersion parkingLotVersion = parkingLotService.createParkingLot(ParkingLotVersionMapper.toEntity(model));
+    SharedServicePointVersionModel sharedServicePointVersionModel = sharedServicePointService.findServicePoint(model.getParentServicePointSloid()).orElseThrow();
+    ParkingLotVersion parkingLotVersion = parkingLotService.createParkingLot(ParkingLotVersionMapper.toEntity(model),
+            sharedServicePointVersionModel);
     return ParkingLotVersionMapper.toModel(parkingLotVersion);
   }
 
@@ -36,7 +43,8 @@ public class ParkingLotsController implements ParkingLotApiV1 {
         parkingLotService.getPlatformVersionById(id).orElseThrow(() -> new IdNotFoundException(id));
 
     ParkingLotVersion editedVersion = ParkingLotVersionMapper.toEntity(model);
-    parkingLotService.updateParkingLotVersion(parkingLotVersion, editedVersion);
+    SharedServicePointVersionModel sharedServicePointVersionModel = sharedServicePointService.findServicePoint(model.getParentServicePointSloid()).orElseThrow();
+    parkingLotService.updateParkingLotVersion(parkingLotVersion, editedVersion, sharedServicePointVersionModel);
 
     return parkingLotService.findAllByNumberOrderByValidFrom(parkingLotVersion.getNumber()).stream()
         .map(ParkingLotVersionMapper::toModel).toList();
