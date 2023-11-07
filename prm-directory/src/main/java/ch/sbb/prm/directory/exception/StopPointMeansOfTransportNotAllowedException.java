0b5bd5075f1de6d7241414a833cc6c7cd5e82657
atlas <TechSupport-ATLAS@sbb.ch>
2023-11-07 12:07:07 +0100

    ATLAS-1379: validate stop point: WIP

---
+++ prm-directory/src/main/java/ch/sbb/prm/directory/exception/StopPointMeansOfTransportNotAllowedException.java
@@ -0,0 +1,33 @@
+package ch.sbb.prm.directory.exception;
+
+import static ch.sbb.prm.directory.validation.PrmMeansOfTransportHelper.COMPLETE_MEANS_OF_TRANSPORT;
+import static ch.sbb.prm.directory.validation.PrmMeansOfTransportHelper.REDUCED_MEANS_OF_TRANSPORT;
+
+import ch.sbb.atlas.api.model.ErrorResponse;
+import ch.sbb.atlas.model.exception.AtlasException;
+import ch.sbb.atlas.servicepoint.enumeration.MeanOfTransport;
+import java.util.Set;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+@Getter
+public class StopPointMeansOfTransportNotAllowedException extends AtlasException {
+
+  private final Set<MeanOfTransport> meanOfTransports;
+
+  @Override
+  public ErrorResponse getErrorResponse() {
+    return ErrorResponse.builder()
+        .status(HttpStatus.BAD_REQUEST.value())
+        .message("Means of Transport combination not allowed!")
+        .error("""
+            The given Means of Transport combination %s is not allowed.
+            Allowed combination:
+            Reduced: %s
+            Complete: %s
+            """.formatted(meanOfTransports.toString(), REDUCED_MEANS_OF_TRANSPORT, COMPLETE_MEANS_OF_TRANSPORT))
+        .build();
+  }
+}
