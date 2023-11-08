4cee93984289f6c259385cb11118a681eba3fef3
atlas <19825793+iuccio@users.noreply.github.com>
2023-11-08 14:54:42 +0100

    ATLAS-1379: impl reduced vs complete

--- prm-directory/src/test/java/ch/sbb/prm/directory/service/InformationDeskServiceTest.java
+++ prm-directory/src/test/java/ch/sbb/prm/directory/service/InformationDeskServiceTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import ch.sbb.atlas.api.prm.enumeration.ReferencePointElementType;
 import ch.sbb.atlas.model.controller.IntegrationTest;
+import ch.sbb.atlas.servicepoint.enumeration.MeanOfTransport;
 import ch.sbb.prm.directory.InformationDeskTestData;
 import ch.sbb.prm.directory.ReferencePointTestData;
 import ch.sbb.prm.directory.StopPointTestData;
@@ -18,7 +19,7 @@ import ch.sbb.prm.directory.repository.ReferencePointRepository;
 import ch.sbb.prm.directory.repository.RelationRepository;
 import ch.sbb.prm.directory.repository.StopPointRepository;
 import java.util.List;
-import org.assertj.core.api.AbstractComparableAssert;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
@@ -104,8 +105,33 @@ class InformationDeskServiceTest {
         parentServicePointSloid);
     assertThat(relationVersions).hasSize(1);
     assertThat(relationVersions.get(0).getParentServicePointSloid()).isEqualTo(parentServicePointSloid);
-    AbstractComparableAssert<?, ReferencePointElementType> equalTo = assertThat(
-        relationVersions.get(0).getReferencePointElementType()).isEqualTo(ReferencePointElementType.INFORMATION_DESK);
+    assertThat(relationVersions.get(0).getReferencePointElementType()).isEqualTo(ReferencePointElementType.INFORMATION_DESK);
+  }
+  @Test
+  void shouldNotCreateToiletRelationWhenStopPointIsReduced() {
+    //given
+    String parentServicePointSloid = "ch:1:sloid:70000";
+    StopPointVersion stopPointVersion = StopPointTestData.getStopPointVersion();
+    stopPointVersion.setSloid(parentServicePointSloid);
+    stopPointVersion.setMeansOfTransport(Set.of(MeanOfTransport.BUS));
+    stopPointRepository.save(stopPointVersion);
+    ReferencePointVersion referencePointVersion = ReferencePointTestData.getReferencePointVersion();
+    referencePointVersion.setParentServicePointSloid(parentServicePointSloid);
+    referencePointRepository.save(referencePointVersion);
+    InformationDeskVersion informationDesk = InformationDeskTestData.getInformationDeskVersion();
+    informationDesk.setParentServicePointSloid(parentServicePointSloid);
+
+    //when
+    informationDeskService.createInformationDesk(informationDesk);
+
+    //then
+    List<InformationDeskVersion> informationDeskVersions = informationDeskRepository.findByParentServicePointSloid(
+        informationDesk.getParentServicePointSloid());
+    assertThat(informationDeskVersions).hasSize(1);
+    assertThat(informationDeskVersions.get(0).getParentServicePointSloid()).isEqualTo(informationDesk.getParentServicePointSloid());
+    List<RelationVersion> relationVersions = relationRepository.findAllByParentServicePointSloid(
+        parentServicePointSloid);
+    assertThat(relationVersions).isEmpty();
   }
 
 }
