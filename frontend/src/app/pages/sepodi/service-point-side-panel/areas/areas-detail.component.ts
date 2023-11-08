3a66d2c1456abf396f97188cd63b95ea45f931f8
atlas <TechSupport-ATLAS@sbb.ch>
2023-11-08 19:08:27 +0100

    ATLAS 1306: added condition for check if trafficpointarea

--- frontend/src/app/pages/sepodi/service-point-side-panel/areas/areas-detail.component.ts
+++ frontend/src/app/pages/sepodi/service-point-side-panel/areas/areas-detail.component.ts
@@ -1,8 +1,72 @@
-import { Component } from '@angular/core';
+//TODO kann gelöscht werden
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+import { ReadTrafficPointElementVersion, TrafficPointElementsService } from 'src/app/api';
+import { TableColumn } from 'src/app/core/components/table/table-column';
+import { TablePagination } from 'src/app/core/components/table/table-pagination';
+import { Pages } from 'src/app/pages/pages';
 
 @Component({
   selector: 'app-service-point-areas',
   templateUrl: './areas-detail.component.html',
   styleUrls: ['./areas-detail.component.scss'],
 })
-export class AreasDetailComponent {}
+export class AreasDetailComponent implements OnInit, OnDestroy {
+  tableColumns: TableColumn<ReadTrafficPointElementVersion>[] = [
+    { headerTitle: 'SEPODI.TRAFFIC_POINT_ELEMENTS.DESIGNATION', value: 'designation' },
+    { headerTitle: 'SEPODI.SERVICE_POINTS.SLOID', value: 'sloid' },
+    { headerTitle: 'COMMON.VALID_FROM', value: 'validFrom', formatAsDate: true },
+    { headerTitle: 'COMMON.VALID_TO', value: 'validTo', formatAsDate: true },
+  ];
+
+  trafficPointElementRows: ReadTrafficPointElementVersion[] = [];
+  totalCount$ = 0;
+  private ngUnsubscribe = new Subject<void>();
+
+  get servicePointNumber() {
+    return this.route.parent!.snapshot.params['id'];
+  }
+
+  constructor(
+    private trafficPointElementService: TrafficPointElementsService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {}
+
+  ngOnDestroy(): void {}
+
+  ngOnInit(): void {}
+
+  getOverview(pagination: TablePagination) {
+    this.trafficPointElementService
+      .getAreasOfServicePoint(this.servicePointNumber, pagination.page, pagination.size, [
+        pagination.sort ?? 'designation,asc',
+      ])
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe((container) => {
+        this.trafficPointElementRows = container.objects!;
+        this.totalCount$ = container.totalCount!;
+      });
+  }
+
+  newTrafficPointElement() {
+    this.router
+      .navigate([Pages.SEPODI.path, Pages.TRAFFIC_POINT_ELEMENTS.path, 'add'], {
+        state: { servicePointNumber: this.servicePointNumber },
+      })
+      .then();
+  }
+
+  editVersion($event: ReadTrafficPointElementVersion) {
+    this.router
+      .navigate([Pages.SEPODI.path, Pages.TRAFFIC_POINT_ELEMENTS.path, $event.sloid], {
+        state: { servicePointNumber: this.servicePointNumber },
+      })
+      .then();
+  }
+
+  closeSidePanel() {
+    this.router.navigate([Pages.SEPODI.path]).then();
+  }
+}
--- frontend/src/app/pages/sepodi/service-point-side-panel/traffic-point-elements/traffic-point-elements-table.component.ts
+++ frontend/src/app/pages/sepodi/service-point-side-panel/traffic-point-elements/traffic-point-elements-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TableColumn } from '../../../../core/components/table/table-column';
 import { ReadTrafficPointElementVersion, TrafficPointElementsService } from '../../../../api';
 import { TablePagination } from '../../../../core/components/table/table-pagination';
@@ -12,8 +12,8 @@ import { Subject } from 'rxjs';
   templateUrl: './traffic-point-elements-table.component.html',
   styleUrls: ['./traffic-point-elements-table.component.scss'],
 })
-export class TrafficPointElementsTableComponent implements OnDestroy {
-  tableColumns: TableColumn<ReadTrafficPointElementVersion>[] = [
+export class TrafficPointElementsTableComponent implements OnInit, OnDestroy {
+  tableColumnsPlatforms: TableColumn<ReadTrafficPointElementVersion>[] = [
     { headerTitle: 'SEPODI.TRAFFIC_POINT_ELEMENTS.DESIGNATION', value: 'designation' },
     { headerTitle: 'SEPODI.SERVICE_POINTS.SLOID', value: 'sloid' },
     {
@@ -24,8 +24,17 @@ export class TrafficPointElementsTableComponent implements OnDestroy {
     { headerTitle: 'COMMON.VALID_TO', value: 'validTo', formatAsDate: true },
   ];
 
+  tableColumnsAreas: TableColumn<ReadTrafficPointElementVersion>[] = [
+    { headerTitle: 'SEPODI.TRAFFIC_POINT_ELEMENTS.DESIGNATION', value: 'designation' },
+    { headerTitle: 'SEPODI.SERVICE_POINTS.SLOID', value: 'sloid' },
+    { headerTitle: 'COMMON.VALID_FROM', value: 'validFrom', formatAsDate: true },
+    { headerTitle: 'COMMON.VALID_TO', value: 'validTo', formatAsDate: true },
+  ];
+
   trafficPointElementRows: ReadTrafficPointElementVersion[] = [];
   totalCount$ = 0;
+  isTrafficPointArea = false;
+
   private ngUnsubscribe = new Subject<void>();
 
   constructor(
@@ -39,16 +48,17 @@ export class TrafficPointElementsTableComponent implements OnDestroy {
     this.ngUnsubscribe.complete();
   }
 
+  ngOnInit(): void {
+    //TODO mit hannes besprechen, ob nicht bessere lösung gibt
+    this.router.url.includes('/areas')
+      ? (this.isTrafficPointArea = true)
+      : (this.isTrafficPointArea = false);
+  }
+
   getOverview(pagination: TablePagination) {
-    this.trafficPointElementService
-      .getPlatformsOfServicePoint(this.servicePointNumber, pagination.page, pagination.size, [
-        pagination.sort ?? 'designation,asc',
-      ])
-      .pipe(takeUntil(this.ngUnsubscribe))
-      .subscribe((container) => {
-        this.trafficPointElementRows = container.objects!;
-        this.totalCount$ = container.totalCount!;
-      });
+    this.isTrafficPointArea
+      ? this.getAreasOfServicePoint(pagination)
+      : this.getPlatformsOfServicePoint(pagination);
   }
 
   newTrafficPointElement() {
@@ -56,7 +66,9 @@ export class TrafficPointElementsTableComponent implements OnDestroy {
       .navigate([Pages.SEPODI.path, Pages.TRAFFIC_POINT_ELEMENTS.path, 'add'], {
         state: { servicePointNumber: this.servicePointNumber },
       })
-      .then();
+      .then(() => {
+        this.trafficPointElementService.isTrafficPointArea.next(this.isTrafficPointArea);
+      });
   }
 
   editVersion($event: ReadTrafficPointElementVersion) {
@@ -73,5 +85,29 @@ export class TrafficPointElementsTableComponent implements OnDestroy {
 
   closeSidePanel() {
     this.router.navigate([Pages.SEPODI.path]).then();
+  }
+
+  getPlatformsOfServicePoint(pagination: TablePagination) {
+    this.trafficPointElementService
+      .getPlatformsOfServicePoint(this.servicePointNumber, pagination.page, pagination.size, [
+        pagination.sort ?? 'designation,asc',
+      ])
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe((container) => {
+        this.trafficPointElementRows = container.objects!;
+        this.totalCount$ = container.totalCount!;
+      });
+  }
+
+  getAreasOfServicePoint(pagination: TablePagination) {
+    this.trafficPointElementService
+      .getAreasOfServicePoint(this.servicePointNumber, pagination.page, pagination.size, [
+        pagination.sort ?? 'designation,asc',
+      ])
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe((container) => {
+        this.trafficPointElementRows = container.objects!;
+        this.totalCount$ = container.totalCount!;
+      });
   }
 }
