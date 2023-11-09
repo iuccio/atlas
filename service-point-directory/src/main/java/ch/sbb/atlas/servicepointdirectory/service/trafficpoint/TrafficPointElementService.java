5018a4cd7ef7eed822bda2534ab7f23b2933832e
atlas <19825793+iuccio@users.noreply.github.com>
2023-11-09 14:14:11 +0100

    ATLAS-1308: implement overview of loading points

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/service/trafficpoint/TrafficPointElementService.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/service/trafficpoint/TrafficPointElementService.java
@@ -2,7 +2,6 @@ package ch.sbb.atlas.servicepointdirectory.service.trafficpoint;
 
 import ch.sbb.atlas.api.model.Container;
 import ch.sbb.atlas.api.servicepoint.ReadTrafficPointElementVersionModel;
-import ch.sbb.atlas.model.DateRange;
 import ch.sbb.atlas.servicepoint.enumeration.TrafficPointElementType;
 import ch.sbb.atlas.servicepointdirectory.entity.ServicePointVersion;
 import ch.sbb.atlas.servicepointdirectory.entity.TrafficPointElementVersion;
@@ -11,15 +10,11 @@ import ch.sbb.atlas.servicepointdirectory.mapper.TrafficPointElementVersionMappe
 import ch.sbb.atlas.servicepointdirectory.model.search.TrafficPointElementSearchRestrictions;
 import ch.sbb.atlas.servicepointdirectory.repository.TrafficPointElementVersionRepository;
 import ch.sbb.atlas.servicepointdirectory.service.CrossValidationService;
+import ch.sbb.atlas.servicepointdirectory.service.OverviewService;
 import ch.sbb.atlas.versioning.consumer.ApplyVersioningDeleteByIdLongConsumer;
-import ch.sbb.atlas.versioning.model.Versionable;
 import ch.sbb.atlas.versioning.model.VersionedObject;
 import ch.sbb.atlas.versioning.service.VersionableService;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 import lombok.Getter;
@@ -131,59 +126,9 @@ public class TrafficPointElementService {
     List<ReadTrafficPointElementVersionModel> trafficPointElements = trafficPointElementVersions.stream()
         .map(TrafficPointElementVersionMapper::toModel).toList();
 
-    List<ReadTrafficPointElementVersionModel> displayableVersions = mergeVersionsForDisplay(trafficPointElements);
-    return Container.<ReadTrafficPointElementVersionModel>builder()
-        .objects(displayableVersions.stream()
-            .skip(Math.multiplyFull(pageable.getPageNumber(), pageable.getPageSize()))
-            .limit(pageable.getPageSize())
-            .toList())
-        .totalCount(displayableVersions.size())
-        .build();
-  }
-
-  private List<ReadTrafficPointElementVersionModel> mergeVersionsForDisplay(
-      List<ReadTrafficPointElementVersionModel> trafficPointElements) {
-    if (trafficPointElements.isEmpty()) {
-      return Collections.emptyList();
-    }
-
-    List<ReadTrafficPointElementVersionModel> result = new ArrayList<>();
-
-    List<ReadTrafficPointElementVersionModel> versions = new ArrayList<>();
-    Iterator<ReadTrafficPointElementVersionModel> iterator = trafficPointElements.iterator();
-    ReadTrafficPointElementVersionModel previous = iterator.next();
-    versions.add(previous);
-    ReadTrafficPointElementVersionModel current;
-
-    while (iterator.hasNext()) {
-      current = iterator.next();
-      if (previous.getSloid().equals(current.getSloid())) {
-        versions.add(current);
-      } else {
-        result.add(getDisplayModel(versions));
-        versions.clear();
-        versions.add(current);
-      }
-      previous = current;
-    }
-    result.add(getDisplayModel(versions));
-
-    return result;
-  }
-
-  private <T extends Versionable> T getDisplayModel(List<T> versions) {
-    List<T> sortedVersions = versions.stream().sorted(Comparator.comparing(T::getValidFrom)).toList();
-
-    T versionToShow = getPrioritizedVersion(sortedVersions);
-    versionToShow.setValidFrom(sortedVersions.get(0).getValidFrom());
-    versionToShow.setValidTo(sortedVersions.get(sortedVersions.size() - 1).getValidTo());
-    return versionToShow;
-  }
-
-  private <T extends Versionable> T getPrioritizedVersion(List<T> versions) {
-    Optional<T> validToday = versions.stream().filter(i -> DateRange.fromVersionable(i).contains(LocalDate.now())).findFirst();
-    Optional<T> validInFuture = versions.stream().filter(i -> i.getValidFrom().isAfter(LocalDate.now())).findFirst();
-    return validToday.orElse(validInFuture.orElse(versions.get(versions.size() - 1)));
+    List<ReadTrafficPointElementVersionModel> displayableVersions = OverviewService.mergeVersionsForDisplay(trafficPointElements,
+        (previous, current)->previous.getSloid().equals(current.getSloid()));
+    return OverviewService.toPagedContainer(displayableVersions, pageable);
   }
 
   public List<TrafficPointElementVersion> getTrafficPointElementsByServicePointNumber(Integer servicePointNumber,
