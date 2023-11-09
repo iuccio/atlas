19b80e249c3a1ea7fc6db006649a004a3a871270
atlas <TechSupport-ATLAS@sbb.ch>
2023-11-09 10:11:35 +0100

    ATLAS-878: ref

--- frontend/src/app/pages/sepodi/service-point-side-panel/traffic-point-elements/displayable-traffic-point.ts
+++
@@ -1,8 +0,0 @@
-import { CoordinatePair, TrafficPointElementType } from '../../../../api';
-
-export interface DisplayableTrafficPoint {
-  type: TrafficPointElementType;
-  sloid: string;
-  designation: string;
-  coordinates: CoordinatePair;
-}
