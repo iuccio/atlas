2921f47598f36cbbe86dd3577720a6e3768859c5
atlas <19825793+iuccio@users.noreply.github.com>
2023-11-13 14:11:26 +0100

    ATLAS-1433: add more validations

--- prm-directory/src/main/java/ch/sbb/prm/directory/exception/ServicePointNonSwissCountryNotAllowedException.java
+++ prm-directory/src/main/java/ch/sbb/prm/directory/exception/ServicePointNonSwissCountryNotAllowedException.java
@@ -2,23 +2,24 @@ package ch.sbb.prm.directory.exception;
 
 import ch.sbb.atlas.api.model.ErrorResponse;
 import ch.sbb.atlas.model.exception.AtlasException;
-import ch.sbb.prm.directory.entity.StopPointVersion;
+import ch.sbb.atlas.servicepoint.ServicePointNumber;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor
 @Getter
-public class ServicePointWithNotSwissCountryNotAllowedException extends AtlasException {
+public class ServicePointNonSwissCountryNotAllowedException extends AtlasException {
 
-  private final StopPointVersion stopPointVersion;
+  private final ServicePointNumber servicePointNumber;
 
   @Override
   public ErrorResponse getErrorResponse() {
     return ErrorResponse.builder()
         .status(HttpStatus.PRECONDITION_FAILED.value())
         .message("PRM does not allow to create StopPoints from non-Swiss ServicePoints!")
-        .error("The given SLOID [" + stopPointVersion.getSloid() + "], with " + stopPointVersion.getNumber()+ " has " + stopPointVersion.getNumber().getCountry() + " as its Country!")
+        .error("The given ServicePointNumber " + servicePointNumber.getNumber() + " has " + servicePointNumber.getCountry()
+            + " as its Country!")
         .build();
   }
 
