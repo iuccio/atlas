9b5b389a1f2b06cab144532e1e7d4525a9224765
atlas <19825793+iuccio@users.noreply.github.com>
2023-11-14 17:47:22 +0100

    ATLAS-1358: Improve PrmBusinessOrganisationBasedUserAdministrationService tests.

--- prm-directory/src/test/java/ch/sbb/prm/directory/security/ToiletUserAdministrationServiceTest.java
+++ prm-directory/src/test/java/ch/sbb/prm/directory/security/ToiletUserAdministrationServiceTest.java
@@ -1,198 +1,64 @@
 package ch.sbb.prm.directory.security;
 
-import ch.sbb.atlas.kafka.model.user.admin.ApplicationRole;
-import ch.sbb.atlas.kafka.model.user.admin.ApplicationType;
-import ch.sbb.atlas.kafka.model.user.admin.PermissionRestrictionType;
-import ch.sbb.atlas.kafka.model.user.admin.UserAdministrationModel;
-import ch.sbb.atlas.kafka.model.user.admin.UserAdministrationPermissionModel;
-import ch.sbb.atlas.kafka.model.user.admin.UserAdministrationPermissionRestrictionModel;
-import ch.sbb.atlas.model.controller.IntegrationTest;
 import ch.sbb.atlas.user.administration.security.UserPermissionHolder;
 import ch.sbb.prm.directory.ToiletTestData;
 import ch.sbb.prm.directory.entity.SharedServicePoint;
 import ch.sbb.prm.directory.repository.SharedServicePointRepository;
-import ch.sbb.prm.directory.service.SharedServicePointService;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
-import java.util.Optional;
-import java.util.Set;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
-
-@IntegrationTest
-public class ToiletUserAdministrationServiceTest {
-
-    @MockBean
-    private UserPermissionHolder userPermissionHolder;
-
-    private final SharedServicePointRepository sharedServicePointRepository;
-    private final SharedServicePointService sharedServicePointService;
-
-    private final PrmBusinessOrganisationBasedUserAdministrationService prmBOBasedUserAdministrationService;
+public class ToiletUserAdministrationServiceTest extends BaseUserAdministrationServiceTest {
 
     @Autowired
     public ToiletUserAdministrationServiceTest(SharedServicePointRepository sharedServicePointRepository,
-                                               SharedServicePointService sharedServicePointService,
                                                PrmBusinessOrganisationBasedUserAdministrationService prmBOBasedUserAdministrationService,
                                                UserPermissionHolder userPermissionHolder) {
-        this.sharedServicePointRepository = sharedServicePointRepository;
-        this.sharedServicePointService = sharedServicePointService;
-        this.prmBOBasedUserAdministrationService = prmBOBasedUserAdministrationService;
-        this.userPermissionHolder = userPermissionHolder;
+        super(sharedServicePointRepository, prmBOBasedUserAdministrationService, userPermissionHolder);
     }
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-        when(userPermissionHolder.isAdmin()).thenReturn(false);
-        when(userPermissionHolder.getCurrentUserSbbUid()).thenReturn("e123456");
-
+    @Override
+    protected SharedServicePoint getSharedServicePoint() {
         SharedServicePoint servicePoint = SharedServicePoint.builder()
                 .servicePoint("{\"servicePointSloid\":\"ch:1.sloid:12345\",\"sboids\":[\"ch:1:sboid:100001\",\"ch:1:sboid:100002\",\"ch:1:sboid:100003\",\"ch:1:sboid:100004\",\"ch:1:sboid:100005\"],"
                         + "\"trafficPointSloids\":[\"ch:1.sloid:12345:1\"]}")
                 .sloid("ch:1.sloid:12345")
                 .build();
-        sharedServicePointRepository.saveAndFlush(servicePoint);
-    }
-
-    @AfterEach
-    void cleanUp() {
-        sharedServicePointRepository.deleteAll();
+        return servicePoint;
     }
 
     @Test
-    void shouldAllowCreateToAdminUser() {
-        when(userPermissionHolder.isAdmin()).thenReturn(true);
-
-        boolean permissionsToCreate = prmBOBasedUserAdministrationService
-                .hasUserPermissionsForBusinessOrganisations(ToiletTestData.getToiletVersion(), ApplicationType.PRM);
-
-        assertThat(permissionsToCreate).isTrue();
+    void shouldAllowToiletCreateToAdminUser() {
+        allowCreateToAdminUser(ToiletTestData.getToiletVersion());
     }
 
     @Test
-    void shouldAllowCreateToSupervisorUser() {
-        when(userPermissionHolder.getCurrentUser()).thenReturn(Optional.of(UserAdministrationModel.builder()
-                .userId("e123456")
-                .permissions(Set.of(
-                        UserAdministrationPermissionModel.builder()
-                                .application(ApplicationType.PRM)
-                                .role(ApplicationRole.SUPERVISOR)
-                                .build()))
-                .build()));
-
-        boolean permissionsToCreate = prmBOBasedUserAdministrationService
-                .hasUserPermissionsForBusinessOrganisations(ToiletTestData.getToiletVersion(), ApplicationType.PRM);
-
-        assertThat(permissionsToCreate).isTrue();
+    void shouldAllowToiletCreateToSupervisorUser() {
+        allowCreateToSupervisorUser(ToiletTestData.getToiletVersion());
     }
 
     @Test
-    void shouldAllowCreateToSuperUser() {
-        when(userPermissionHolder.getCurrentUser()).thenReturn(Optional.of(UserAdministrationModel.builder()
-                .userId("e123456")
-                .permissions(Set.of(
-                        UserAdministrationPermissionModel.builder()
-                                .application(ApplicationType.PRM)
-                                .role(ApplicationRole.SUPER_USER)
-                                .build()))
-                .build()));
-
-        boolean permissionsToCreate = prmBOBasedUserAdministrationService
-                .hasUserPermissionsForBusinessOrganisations(ToiletTestData.getToiletVersion(), ApplicationType.PRM);
-
-        assertThat(permissionsToCreate).isTrue();
+    void shouldAllowToiletCreateToSuperUser() {
+        allowCreateToSuperUser(ToiletTestData.getToiletVersion());
     }
 
     @Test
-    void shouldNotAllowCreateToReaderUser() {
-        when(userPermissionHolder.getCurrentUser()).thenReturn(Optional.of(UserAdministrationModel.builder()
-                .userId("e123456")
-                .permissions(Set.of(
-                        UserAdministrationPermissionModel.builder()
-                                .application(ApplicationType.PRM)
-                                .role(ApplicationRole.READER)
-                                .build()))
-                .build()));
-
-        boolean permissionsToCreate = prmBOBasedUserAdministrationService
-                .hasUserPermissionsForBusinessOrganisations(ToiletTestData.getToiletVersion(), ApplicationType.PRM);
-
-        assertThat(permissionsToCreate).isFalse();
+    void shouldNotAllowToiletCreateToReaderUser() {
+        notAllowCreateToReaderUser(ToiletTestData.getToiletVersion());
     }
 
     @Test
-    void shouldAllowToCreateToWriterUserWithAppropriateBO() {
-        when(userPermissionHolder.getCurrentUser()).thenReturn(Optional.of(UserAdministrationModel.builder()
-                .userId("e123456")
-                .permissions(Set.of(
-                        UserAdministrationPermissionModel.builder()
-                                .application(ApplicationType.PRM)
-                                .role(ApplicationRole.WRITER)
-                                .restrictions(Set.of(UserAdministrationPermissionRestrictionModel.builder()
-                                                .value("ch:1:sboid:100001")
-                                                .restrictionType(PermissionRestrictionType.BUSINESS_ORGANISATION)
-                                                .build()))
-                                .build()))
-                .build()));
-
-        boolean permissionsToCreate = prmBOBasedUserAdministrationService
-                .hasUserPermissionsForBusinessOrganisations(ToiletTestData.getToiletVersion(), ApplicationType.PRM);
-
-        assertThat(permissionsToCreate).isTrue();
+    void shouldAllowToiletCreateToWriterUserWithAppropriateBO() {
+        allowCreateToWriterUserWithAppropriateBO(ToiletTestData.getToiletVersion());
     }
 
     @Test
-    void shouldNotAllowToCreateToWriterUserWithInappropriateBO() {
-        when(userPermissionHolder.getCurrentUser()).thenReturn(Optional.of(UserAdministrationModel.builder()
-                .userId("e123456")
-                .permissions(Set.of(
-                        UserAdministrationPermissionModel.builder()
-                                .application(ApplicationType.PRM)
-                                .role(ApplicationRole.WRITER)
-                                .restrictions(Set.of(UserAdministrationPermissionRestrictionModel.builder()
-                                        .value("ch:1:sboid:100011")
-                                        .restrictionType(PermissionRestrictionType.BUSINESS_ORGANISATION)
-                                        .build()))
-                                .build()))
-                .build()));
-
-        boolean permissionsToCreate = prmBOBasedUserAdministrationService
-                .hasUserPermissionsForBusinessOrganisations(ToiletTestData.getToiletVersion(), ApplicationType.PRM);
-
-        assertThat(permissionsToCreate).isFalse();
+    void shouldNotAllowToiletCreateToWriterUserWithInappropriateBO() {
+        notAllowCreateToWriterUserWithInappropriateBO(ToiletTestData.getToiletVersion());
     }
 
     @Test
-    void shouldAllowToCreateToWriterUserWithAppropriateBOs() {
-        when(userPermissionHolder.getCurrentUser()).thenReturn(Optional.of(UserAdministrationModel.builder()
-                .userId("e123456")
-                .permissions(Set.of(
-                        UserAdministrationPermissionModel.builder()
-                                .application(ApplicationType.PRM)
-                                .role(ApplicationRole.WRITER)
-                                .restrictions(Set.of(
-                                        UserAdministrationPermissionRestrictionModel.builder()
-                                        .value("ch:1:sboid:100005")
-                                        .restrictionType(PermissionRestrictionType.BUSINESS_ORGANISATION)
-                                        .build(),
-                                        UserAdministrationPermissionRestrictionModel.builder()
-                                        .value("ch:1:sboid:100001")
-                                        .restrictionType(PermissionRestrictionType.BUSINESS_ORGANISATION)
-                                        .build()))
-                                .build()))
-                .build()));
-
-        boolean permissionsToCreate = prmBOBasedUserAdministrationService
-                .hasUserPermissionsForBusinessOrganisations(ToiletTestData.getToiletVersion(), ApplicationType.PRM);
-
-        assertThat(permissionsToCreate).isTrue();
+    void shouldAllowToiletCreateToWriterUserWithAppropriateBOs() {
+        allowCreateToWriterUserWithAppropriateBOs(ToiletTestData.getToiletVersion());
     }
 
 }
