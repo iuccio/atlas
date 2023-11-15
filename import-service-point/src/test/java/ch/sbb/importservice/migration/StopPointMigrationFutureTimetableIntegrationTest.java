9502354eb29f5145e84b803ccf96c41aedf0c949
atlas <19825793+iuccio@users.noreply.github.com>
2023-11-15 11:46:57 +0100

    joho: Add migration tests (actual, future-timetable) for StopPoint

---
+++ import-service-point/src/test/java/ch/sbb/importservice/migration/StopPointMigrationFutureTimetableIntegrationTest.java
@@ -0,0 +1,76 @@
+package ch.sbb.importservice.migration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import ch.sbb.atlas.export.model.prm.StopPointVersionCsvModel;
+import ch.sbb.atlas.imports.prm.stoppoint.StopPointCsvModel;
+import ch.sbb.atlas.imports.prm.stoppoint.StopPointCsvModelContainer;
+import ch.sbb.atlas.imports.util.CsvReader;
+import ch.sbb.atlas.model.DateRange;
+import ch.sbb.atlas.model.controller.IntegrationTest;
+import ch.sbb.importservice.service.csv.StopPointCsvService;
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@IntegrationTest
+@Slf4j
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class StopPointMigrationFutureTimetableIntegrationTest {
+
+  private static final String DIDOK_STOP_PLACE_CSV_FILE = "PRM_STOP_PLACES_20231115022314.csv";
+  private static final String ATLAS_STOP_POINT_CSV_FILE = "future-timetable-stop-point-2023-11-15.csv";
+  private static final LocalDate FUTURE_TIMETABLE_DATE = LocalDate.of(2023, 12, 10);
+
+  private static final List<StopPointCsvModel> didokStopPointCsvLines = new ArrayList<>();
+  private static final List<StopPointVersionCsvModel> atlasStopPointCsvLines = new ArrayList<>();
+
+  private final StopPointCsvService stopPointCsvService;
+
+  @Autowired
+  public StopPointMigrationFutureTimetableIntegrationTest(StopPointCsvService stopPointCsvService) {
+    this.stopPointCsvService = stopPointCsvService;
+  }
+
+  @Test
+  @Order(1)
+  void shouldParseCsvCorrectly() throws IOException {
+    try (InputStream csvStream = this.getClass().getResourceAsStream(CsvReader.BASE_PATH + DIDOK_STOP_PLACE_CSV_FILE)) {
+      List<StopPointCsvModelContainer> stopPointCsvModelContainers = stopPointCsvService.mapToStopPointCsvModelContainers(
+          CsvReader.parseCsv(csvStream, StopPointCsvModel.class));
+      didokStopPointCsvLines.addAll(stopPointCsvModelContainers.stream()
+          .map(StopPointCsvModelContainer::getStopPointCsvModels)
+          .flatMap(Collection::stream)
+          .toList());
+
+    }
+    assertThat(didokStopPointCsvLines).isNotEmpty();
+
+    try (InputStream csvStream = this.getClass().getResourceAsStream(CsvReader.BASE_PATH + ATLAS_STOP_POINT_CSV_FILE)) {
+      atlasStopPointCsvLines.addAll(CsvReader.parseCsv(csvStream, StopPointVersionCsvModel.class));
+    }
+    assertThat(atlasStopPointCsvLines).isNotEmpty();
+  }
+
+  @Test
+  @Order(2)
+  void shouldHaveOnlyVersionsValidOnFutureTimetableDate() {
+    atlasStopPointCsvLines.forEach(atlasCsvLine -> assertThat(
+            DateRange.builder()
+                .from(CsvReader.dateFromString(atlasCsvLine.getValidFrom()))
+                .to(CsvReader.dateFromString(atlasCsvLine.getValidTo()))
+                .build()
+                .contains(FUTURE_TIMETABLE_DATE)
+        ).isTrue()
+    );
+  }
+}
