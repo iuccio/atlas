12e37cd7a1432ed079e95bf0bfbfc0f41f9dfd08
atlas <19825793+iuccio@users.noreply.github.com>
2023-11-16 13:40:57 +0100

    Fix Build

--- frontend/src/app/core/auth/guards/timetable-hearing-guard.spec.ts
+++ frontend/src/app/core/auth/guards/timetable-hearing-guard.spec.ts
@@ -1,13 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AuthService } from '../auth.service';
-import { Router } from '@angular/router';
 import { TimetableHearingGuard } from './timetable-hearing-guard.service';
 import { BehaviorSubject } from 'rxjs';
 
 describe('TimetableHearingGuard', () => {
   let guard: TimetableHearingGuard;
-  let router: Router;
 
   const authServiceSpy = jasmine.createSpyObj<AuthService>('authService', [
     'mayAccessTimetableHearing',
@@ -25,7 +23,6 @@ describe('TimetableHearingGuard', () => {
       ],
     });
     guard = TestBed.inject(TimetableHearingGuard);
-    router = TestBed.inject(Router);
   });
 
   it('should be created', () => {
