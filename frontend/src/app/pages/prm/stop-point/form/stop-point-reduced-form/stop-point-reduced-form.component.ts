471bb4089c1e3ecb5fdbc8b4478d178864d7f42a
atlas <19825793+iuccio@users.noreply.github.com>
2023-11-21 11:42:48 +0100

    ATLAS-1350: edit and create stopPoints WIP

--- frontend/src/app/pages/prm/stop-point/form/stop-point-reduced-form/stop-point-reduced-form.component.ts
+++ frontend/src/app/pages/prm/stop-point/form/stop-point-reduced-form/stop-point-reduced-form.component.ts
@@ -1,17 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { StopPointFormGroupBuilder } from '../stop-point-detail-form-group';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { StopPointDetailFormGroup } from '../stop-point-detail-form-group';
 import { MeanOfTransport } from '../../../../../api';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-stop-point-reduced-form',
   templateUrl: './stop-point-reduced-form.component.html',
   styleUrls: ['./stop-point-reduced-form.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StopPointReducedFormComponent implements OnInit {
-  @Input() form = StopPointFormGroupBuilder.buildEmptyReducedFormGroup();
+  @Input() form!: FormGroup<StopPointDetailFormGroup>;
   @Input() selectedMeansOfTransport!: MeanOfTransport[];
-
+  @Input() isNew = false;
   ngOnInit(): void {
-    this.form.controls['meansOfTransport'].setValue(this.selectedMeansOfTransport);
+    if (this.isNew) {
+      this.form.controls['meansOfTransport'].setValue(this.selectedMeansOfTransport);
+    }
   }
 }
