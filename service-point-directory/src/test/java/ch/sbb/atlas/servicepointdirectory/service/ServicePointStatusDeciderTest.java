0f042a4de1977cdda894b1873fcbf9a1db0bd265
atlas <19825793+iuccio@users.noreply.github.com>
2023-11-30 17:57:25 +0100

    ATLAS-1206: Fix more scenarios.

---
+++ service-point-directory/src/test/java/ch/sbb/atlas/servicepointdirectory/service/ServicePointStatusDeciderTest.java
@@ -0,0 +1,115 @@
+package ch.sbb.atlas.servicepointdirectory.service;
+
+import ch.sbb.atlas.servicepointdirectory.ServicePointTestData;
+import ch.sbb.atlas.servicepointdirectory.entity.ServicePointVersion;
+import ch.sbb.atlas.servicepointdirectory.service.servicepoint.ServicePointStatusDecider;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ServicePointStatusDeciderTest {
+
+    private final ServicePointStatusDecider servicePointStatusDecider = new ServicePointStatusDecider();
+
+    @Test
+    void testCheck() {
+        ServicePointVersion servicePointVersion = ServicePointTestData.getBern();
+        servicePointVersion.setValidFrom(LocalDate.of(2010, 12, 11));
+        servicePointVersion.setValidTo(LocalDate.of(2015, 12, 31));
+        ServicePointVersion servicePointVersion1 = ServicePointTestData.getBern();
+        servicePointVersion1.setValidFrom(LocalDate.of(2016, 1, 1));
+        servicePointVersion1.setValidTo(LocalDate.of(2019, 8, 10));
+        List<ServicePointVersion> servicePointVersionList = new ArrayList<>();
+        servicePointVersionList.add(servicePointVersion);
+        servicePointVersionList.add(servicePointVersion1);
+
+        ServicePointVersion newOne = ServicePointTestData.getBern();
+        newOne.setValidFrom(LocalDate.of(2015, 1, 1));
+        newOne.setValidTo(LocalDate.of(2019, 8, 10));
+
+        assertThat(servicePointStatusDecider.checkIfVersionIsIsolated(newOne, servicePointVersionList)).isFalse();
+    }
+
+    @Test
+    void testCheck1() {
+        ServicePointVersion servicePointVersion = ServicePointTestData.getBern();
+        servicePointVersion.setValidFrom(LocalDate.of(2010, 12, 11));
+        servicePointVersion.setValidTo(LocalDate.of(2015, 12, 31));
+        ServicePointVersion servicePointVersion1 = ServicePointTestData.getBern();
+        servicePointVersion1.setValidFrom(LocalDate.of(2016, 1, 1));
+        servicePointVersion1.setValidTo(LocalDate.of(2019, 8, 10));
+        List<ServicePointVersion> servicePointVersionList = new ArrayList<>();
+        servicePointVersionList.add(servicePointVersion);
+        servicePointVersionList.add(servicePointVersion1);
+
+        ServicePointVersion newOne = ServicePointTestData.getBern();
+        newOne.setValidFrom(LocalDate.of(2017, 1, 1));
+        newOne.setValidTo(LocalDate.of(2019, 8, 1));
+
+        assertThat(servicePointStatusDecider.checkIfVersionIsIsolated(newOne, servicePointVersionList)).isFalse();
+    }
+
+    @Test
+    void testCheck2() {
+        ServicePointVersion servicePointVersion = ServicePointTestData.getBern();
+        servicePointVersion.setValidFrom(LocalDate.of(2010, 12, 11));
+        servicePointVersion.setValidTo(LocalDate.of(2015, 12, 31));
+        ServicePointVersion servicePointVersion1 = ServicePointTestData.getBern();
+        servicePointVersion1.setValidFrom(LocalDate.of(2016, 1, 1));
+        servicePointVersion1.setValidTo(LocalDate.of(2019, 8, 10));
+        List<ServicePointVersion> servicePointVersionList = new ArrayList<>();
+        servicePointVersionList.add(servicePointVersion);
+        servicePointVersionList.add(servicePointVersion1);
+
+        ServicePointVersion newOne = ServicePointTestData.getBern();
+        newOne.setValidFrom(LocalDate.of(2015, 1, 1));
+        newOne.setValidTo(LocalDate.of(2019, 8, 1));
+
+        assertThat(servicePointStatusDecider.checkIfVersionIsIsolated(newOne, servicePointVersionList)).isFalse();
+    }
+
+    @Test
+    void testCheck3() {
+        ServicePointVersion servicePointVersion = ServicePointTestData.getBern();
+        servicePointVersion.setValidFrom(LocalDate.of(2010, 12, 11));
+        servicePointVersion.setValidTo(LocalDate.of(2015, 12, 31));
+        ServicePointVersion servicePointVersion1 = ServicePointTestData.getBern();
+        servicePointVersion1.setValidFrom(LocalDate.of(2016, 1, 1));
+        servicePointVersion1.setValidTo(LocalDate.of(2019, 8, 10));
+        List<ServicePointVersion> servicePointVersionList = new ArrayList<>();
+        servicePointVersionList.add(servicePointVersion);
+        servicePointVersionList.add(servicePointVersion1);
+
+        ServicePointVersion newOne = ServicePointTestData.getBern();
+        newOne.setValidFrom(LocalDate.of(2014, 1, 1));
+        newOne.setValidTo(LocalDate.of(2019, 8, 1));
+
+        assertThat(servicePointStatusDecider.checkIfVersionIsIsolated(newOne, servicePointVersionList)).isFalse();
+    }
+
+    @Test
+    void testCheck4() {
+        ServicePointVersion servicePointVersion = ServicePointTestData.getBern();
+        servicePointVersion.setValidFrom(LocalDate.of(2010, 12, 11));
+        servicePointVersion.setValidTo(LocalDate.of(2015, 12, 31));
+        ServicePointVersion servicePointVersion1 = ServicePointTestData.getBern();
+        servicePointVersion1.setValidFrom(LocalDate.of(2016, 1, 1));
+        servicePointVersion1.setValidTo(LocalDate.of(2019, 8, 10));
+        List<ServicePointVersion> servicePointVersionList = new ArrayList<>();
+        servicePointVersionList.add(servicePointVersion);
+        servicePointVersionList.add(servicePointVersion1);
+
+        ServicePointVersion newOne = ServicePointTestData.getBern();
+        newOne.setValidFrom(LocalDate.of(2020, 1, 1));
+        newOne.setValidTo(LocalDate.of(2021, 8, 1));
+
+        assertThat(servicePointStatusDecider.checkIfVersionIsIsolated(newOne, servicePointVersionList)).isTrue();
+    }
+
+
+
+}
