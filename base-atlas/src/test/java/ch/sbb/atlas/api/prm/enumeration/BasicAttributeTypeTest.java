b618869b12f640612404d0cd98d2f75e47ad88d4
atlas <19825793+iuccio@users.noreply.github.com>
2023-12-07 16:33:22 +0100

    Add another test

---
+++ base-atlas/src/test/java/ch/sbb/atlas/api/prm/enumeration/BasicAttributeTypeTest.java
@@ -0,0 +1,14 @@
+package ch.sbb.atlas.api.prm.enumeration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class BasicAttributeTypeTest {
+
+  @Test
+  void shouldGetTypeFromValue() {
+    BasicAttributeType basicAttributeType = BasicAttributeType.of(0);
+    assertThat(basicAttributeType).isEqualTo(BasicAttributeType.TO_BE_COMPLETED);
+  }
+}
