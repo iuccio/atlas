1b90c84d9c30eb98633c39c1300d8581bb50c5ac
atlas <TechSupport-ATLAS@sbb.ch>
2023-12-21 15:32:10 +0100

    ATLAS-866: Add constant for Europe Zurich zone id.

--- workflow/src/main/java/ch/sbb/workflow/WorkflowApplication.java
+++ workflow/src/main/java/ch/sbb/workflow/WorkflowApplication.java
@@ -1,16 +1,19 @@
 package ch.sbb.workflow;
 
 import ch.sbb.atlas.kafka.KafkaTruststorePreparation;
-import java.time.ZoneId;
-import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.ZoneId;
+import java.util.TimeZone;
+
+import static ch.sbb.atlas.api.AtlasApiConstants.ZURICH_ZONE_ID;
 
 @SpringBootApplication
 public class WorkflowApplication {
 
   public static void main(String[] args) {
-    TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("Europe/Zurich")));
+    TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of(ZURICH_ZONE_ID)));
     KafkaTruststorePreparation.setupTruststore();
     SpringApplication.run(WorkflowApplication.class, args);
   }
