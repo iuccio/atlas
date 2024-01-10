4c6cd8c9cc1c6a879777aa7d099e49db4263bff5
atlas <TechSupport-ATLAS@sbb.ch>
2024-01-10 14:46:02 +0100

    ATÃLAS-1449 fixed errors

--- export-service-point/src/main/java/ch/sbb/exportservice/service/ExportPlatformJobService.java
+++ export-service-point/src/main/java/ch/sbb/exportservice/service/ExportPlatformJobService.java
@@ -1,7 +1,7 @@
 package ch.sbb.exportservice.service;
 
-import static ch.sbb.exportservice.utils.JobDescriptionConstants.EXPORT_STOP_POINT_CSV_JOB_NAME;
-import static ch.sbb.exportservice.utils.JobDescriptionConstants.EXPORT_STOP_POINT_JSON_JOB_NAME;
+import static ch.sbb.exportservice.utils.JobDescriptionConstants.EXPORT_PLATFORM_CSV_JOB_NAME;
+import static ch.sbb.exportservice.utils.JobDescriptionConstants.EXPORT_PLATFORM_JSON_JOB_NAME;
 
 import ch.sbb.atlas.export.enumeration.ExportTypeBase;
 import ch.sbb.exportservice.model.PrmExportType;
@@ -16,9 +16,9 @@ public class ExportPlatformJobService extends BaseExportJobService {
 
     public ExportPlatformJobService(
         JobLauncher jobLauncher,
-        @Qualifier(EXPORT_STOP_POINT_CSV_JOB_NAME) Job exportStopPointCsvJob,
-        @Qualifier(EXPORT_STOP_POINT_JSON_JOB_NAME) Job exportStopPointJsonJob) {
-        super(jobLauncher, exportStopPointCsvJob, exportStopPointJsonJob);
+        @Qualifier(EXPORT_PLATFORM_CSV_JOB_NAME) Job exportPlatformCsvJob,
+        @Qualifier(EXPORT_PLATFORM_JSON_JOB_NAME) Job exportPlatformJsonJob) {
+        super(jobLauncher, exportPlatformCsvJob, exportPlatformJsonJob);
     }
 
     @Override
