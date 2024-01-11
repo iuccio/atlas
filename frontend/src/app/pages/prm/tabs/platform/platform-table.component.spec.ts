81aabac3cf8199fc60fb6f2a3ef4c669dd1b40a1
atlas <TechSupport-ATLAS@sbb.ch>
2024-01-11 15:43:29 +0100

    ATLAS-1353: fix build

--- frontend/src/app/pages/prm/tabs/platform/platform-table.component.spec.ts
+++ frontend/src/app/pages/prm/tabs/platform/platform-table.component.spec.ts
@@ -6,7 +6,6 @@ import { AppTestingModule } from '../../../../app.testing.module';
 import { ActivatedRoute } from '@angular/router';
 import { STOP_POINT } from '../../util/stop-point-test-data.spec';
 import { BERN_WYLEREGG } from '../../../../../test/data/service-point';
-import { TableComponent } from '../../../../core/components/table/table.component';
 
 describe('PlatformComponent', () => {
   let component: PlatformTableComponent;
