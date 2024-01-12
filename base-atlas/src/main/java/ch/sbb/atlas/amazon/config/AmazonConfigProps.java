3fd738e1cd858f1369778228fdb8d2d2aaf010e7
atlas <TechSupport-ATLAS@sbb.ch>
2024-01-12 11:48:43 +0100

    Fix build

--- base-atlas/src/main/java/ch/sbb/atlas/amazon/config/AmazonConfigProps.java
+++ base-atlas/src/main/java/ch/sbb/atlas/amazon/config/AmazonConfigProps.java
@@ -1,11 +1,15 @@
 package ch.sbb.atlas.amazon.config;
 
 import java.util.Map;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AmazonConfigProps {
     private String region;
 
