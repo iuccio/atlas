f3312470c5368d3d097db3c2cb8e7d51bb78f4bb
atlas <TechSupport-ATLAS@sbb.ch>
2024-01-23 07:46:04 +0100

    ATLAS-1449 added removeNewLine method

--- export-service-point/src/main/java/ch/sbb/exportservice/reader/PlatformVersionRowMapper.java
+++ export-service-point/src/main/java/ch/sbb/exportservice/reader/PlatformVersionRowMapper.java
@@ -18,32 +18,32 @@ public class PlatformVersionRowMapper extends BaseRowMapper implements RowMapper
     public PlatformVersion mapRow(ResultSet rs, int rowNum) throws SQLException {
         PlatformVersionBuilder<?,?> builder = PlatformVersion.builder();
         builder.id(rs.getLong("id"));
-        builder.sloid(rs.getString("sloid"));
-        builder.parentSloidServicePoint(rs.getString("parent_service_point_sloid"));
+        builder.sloid(removeNewLine(rs.getString("sloid")));
+        builder.parentSloidServicePoint(removeNewLine(rs.getString("parent_service_point_sloid")));
         builder.parentNumberServicePoint(ServicePointNumber.ofNumberWithoutCheckDigit(ServicePointNumber.removeCheckDigit(rs.getInt("number"))));
         builder.boardingDevice(
-                rs.getObject("boarding_device") != null ? BoardingDeviceAttributeType.valueOf(rs.getString("boarding_device")) : null);
-        builder.adviceAccessInfo(rs.getString("advice_access_info"));
-        builder.additionalInformation(rs.getString("additional_information"));
+                rs.getObject("boarding_device") != null ? BoardingDeviceAttributeType.valueOf(removeNewLine(rs.getString("boarding_device"))) : null);
+        builder.adviceAccessInfo(rs.getObject("boarding_device") != null ? removeNewLine(rs.getString("advice_access_info")) : null);
+        builder.additionalInformation(removeNewLine(rs.getString("additional_information")));
         builder.contrastingAreas(
-                rs.getObject("contrasting_areas") != null ? BooleanOptionalAttributeType.valueOf(rs.getString("contrasting_areas")) : null);
+                rs.getObject("contrasting_areas") != null ? BooleanOptionalAttributeType.valueOf(removeNewLine(rs.getString("contrasting_areas"))) : null);
         builder.dynamicAudio(
-                rs.getObject("dynamic_audio") != null ? BasicAttributeType.valueOf(rs.getString("dynamic_audio")) : null );
+                rs.getObject("dynamic_audio") != null ? BasicAttributeType.valueOf(removeNewLine(rs.getString("dynamic_audio"))) : null );
         builder.dynamicVisual(
-                rs.getObject("dynamic_visual") != null ? BasicAttributeType.valueOf(rs.getString("dynamic_visual")) : null);
+                rs.getObject("dynamic_visual") != null ? BasicAttributeType.valueOf(removeNewLine(rs.getString("dynamic_visual"))) : null);
         builder.height(rs.getObject("height") != null ? rs.getDouble("height") : null);
         builder.inclination(rs.getObject("inclination") != null ? rs.getDouble("inclination") : null);
         builder.inclinationLongitudinal(rs.getObject("inclination_longitudinal") != null ? rs.getDouble("inclination_longitudinal") : null);
         builder.inclinationWidth(rs.getObject("inclination_width") != null ? rs.getDouble("inclination_width") : null);
-        setInfoOpportunities(builder, rs.getString("info_opportunities"));
+        setInfoOpportunities(builder, removeNewLine(rs.getString("info_opportunities")));
         builder.levelAccessWheelchair(
-                rs.getObject("level_access_wheelchair") != null ? BasicAttributeType.valueOf(rs.getString("level_access_wheelchair")) : null);
+                rs.getObject("level_access_wheelchair") != null ? BasicAttributeType.valueOf(removeNewLine(rs.getString("level_access_wheelchair"))) : null);
         builder.partialElevation(rs.getBoolean("partial_elevation"));
         builder.superElevation(rs.getObject("superelevation") != null ? rs.getDouble("superelevation") : null);
         builder.tactileSystems(
-                rs.getObject("tactile_system") != null ? BooleanOptionalAttributeType.valueOf(rs.getString("tactile_system")) : null);
+                rs.getObject("tactile_system") != null ? BooleanOptionalAttributeType.valueOf(removeNewLine(rs.getString("tactile_system"))) : null);
         builder.vehicleAccess(
-                rs.getObject("vehicle_access") != null ? VehicleAccessAttributeType.valueOf(rs.getString("vehicle_access")) : null);
+                rs.getObject("vehicle_access") != null ? VehicleAccessAttributeType.valueOf(removeNewLine(rs.getString("vehicle_access"))) : null);
         builder.wheelchairAreaLength(
                 rs.getObject("wheelchair_area_length") != null ? rs.getDouble("wheelchair_area_length") : null);
         builder.wheelchairAreaWidth(
@@ -63,5 +63,9 @@ public class PlatformVersionRowMapper extends BaseRowMapper implements RowMapper
             platformVersionBuilder.infoOpportunities(infoOpportunities);
             platformVersionBuilder.infoOpportunitiesPipeList(RowMapperUtil.toPipedString(infoOpportunities));
         }
+    }
+
+    String removeNewLine(String text){
+        return text.replaceAll("\r\n|\r|\n", " ");
     }
 }
