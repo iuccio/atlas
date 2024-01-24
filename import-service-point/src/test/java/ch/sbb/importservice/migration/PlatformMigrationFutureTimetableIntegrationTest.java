9ea5c1da67ebfd118a562335a96eaa2b9ca01f36
atlas <TechSupport-ATLAS@sbb.ch>
2024-01-24 17:10:33 +0100

    ATLAS-1449 fixed wrong tests

--- import-service-point/src/test/java/ch/sbb/importservice/migration/PlatformMigrationFutureTimetableIntegrationTest.java
+++ import-service-point/src/test/java/ch/sbb/importservice/migration/PlatformMigrationFutureTimetableIntegrationTest.java
@@ -28,9 +28,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class PlatformMigrationFutureTimetableIntegrationTest {
 
-  private static final String DIDOK_STOP_PLACE_CSV_FILE = "PRM_PLATFORMS_20240123013648.csv";
-  private static final String ATLAS_STOP_POINT_CSV_FILE = "future-timetable-platform-2024-01-23.csv";
-  private static final LocalDate FUTURE_TIMETABLE_DATE = LocalDate.of(2024, 01, 23);
+  private static final String DIDOK_STOP_PLACE_CSV_FILE = "PRM_PLATFORMS_20240124011743.csv";
+  private static final String ATLAS_STOP_POINT_CSV_FILE = "future-timetable-platform-2024-01-24.csv";
+  private static final LocalDate FUTURE_TIMETABLE_DATE = LocalDate.of(2024, 01, 24);
 
   private static final List<PlatformCsvModel> didokPlatformCsvLines = new ArrayList<>();
   private static final List<PlatformVersionCsvModel> atlasPlatformCsvLines = new ArrayList<>();
