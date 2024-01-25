67e2fecba6d03652799b3f439ae62b5f040ad77e
atlas <19825793+iuccio@users.noreply.github.com>
2024-01-25 11:46:13 +0100

    ATLAS-1206: Add missing transactional.

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/service/servicepoint/ServicePointImportService.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/service/servicepoint/ServicePointImportService.java
@@ -27,6 +27,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -39,6 +40,7 @@ import java.util.stream.Collectors;
 @Service
 @Slf4j
 @RequiredArgsConstructor
+@Transactional
 public class ServicePointImportService extends BaseImportServicePointDirectoryService<ServicePointVersion> {
 
   private final ServicePointService servicePointService;
