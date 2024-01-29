94361d75b19cacf242c7a471c7fb74f79b1750ea
atlas <19825793+iuccio@users.noreply.github.com>
2024-01-29 15:34:15 +0100

    ATLAS-1511: splitting batch lists update

--- base-atlas/src/main/java/ch/sbb/atlas/api/location/SloidApiV1.java
+++ base-atlas/src/main/java/ch/sbb/atlas/api/location/SloidApiV1.java
@@ -2,7 +2,6 @@ package ch.sbb.atlas.api.location;
 
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +15,7 @@ public interface SloidApiV1 {
   @PostMapping("claim")
   ResponseEntity<String> claimSloid(@RequestBody @Valid ClaimSloidRequestModel claimSloidRequestModel);
 
-  @GetMapping
+  @PostMapping("sync")
   ResponseEntity<Void> sync();
 
 }
