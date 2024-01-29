7f58a673405702f5ea6d6be8dc673f564244b437
atlas <19825793+iuccio@users.noreply.github.com>
2024-01-29 16:56:50 +0100

    ATLAS-1511: fix location service after rebase

--- base-atlas/src/main/java/ch/sbb/atlas/api/location/ClaimSloidRequestModel.java
+++ base-atlas/src/main/java/ch/sbb/atlas/api/location/ClaimSloidRequestModel.java
@@ -1,24 +1,9 @@
 package ch.sbb.atlas.api.location;
 
-import ch.sbb.atlas.servicepoint.SloidValidation;
-import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record ClaimSloidRequestModel(@NotNull(message = "SloidType must not be null") SloidType sloidType,
                                      @NotBlank(message = "sloid must not be blank") String sloid) {
-
-  @AssertTrue(message = "Sloid not valid for provided sloidType")
-  public boolean isValidSloid() {
-    if (sloid == null) {
-      return false;
-    }
-    return switch (sloidType) {
-      case SERVICE_POINT -> SloidValidation.isSloidValid(sloid, SloidValidation.EXPECTED_COLONS_SERVICE_POINT);
-      case AREA, TOILET, REFERENCE_POINT, PARKING_LOT, INFO_DESK, TICKET_COUNTER ->
-          SloidValidation.isSloidValid(sloid, SloidValidation.EXPECTED_COLONS_AREA);
-      case PLATFORM -> SloidValidation.isSloidValid(sloid, SloidValidation.EXPECTED_COLONS_PLATFORM);
-    };
-  }
 
 }
