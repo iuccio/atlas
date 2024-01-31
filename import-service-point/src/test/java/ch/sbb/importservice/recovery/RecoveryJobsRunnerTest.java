ae905c4beeaeaa1614ef2d547786d65fbf2f312f
atlas <19825793+iuccio@users.noreply.github.com>
2024-01-31 17:53:02 +0100

    ATLAS-1511: rebase with master and cleanup

--- import-service-point/src/test/java/ch/sbb/importservice/recovery/RecoveryJobsRunnerTest.java
+++ import-service-point/src/test/java/ch/sbb/importservice/recovery/RecoveryJobsRunnerTest.java
@@ -270,24 +270,4 @@ class RecoveryJobsRunnerTest {
     verify(fileService).clearDir();
   }
 
-  @Test
-  void shouldRecoverImportReferencePointCsvJob() throws Exception {
-    //given
-    StepExecution stepExecution = new StepExecution("myStep", jobExecution);
-    stepExecution.setId(132L);
-    Map<String, JobParameter<?>> parameters = new HashMap<>();
-    parameters.put(EXECUTION_TYPE_PARAMETER, new JobParameter<>("BATCH", String.class));
-    when(jobParameters.getParameters()).thenReturn(parameters);
-    when(jobExecution.getStatus()).thenReturn(BatchStatus.STARTING);
-    when(jobExecution.getJobParameters()).thenReturn(jobParameters);
-    when(jobExecution.getStepExecutions()).thenReturn(List.of(stepExecution));
-    when(jobExplorer.getLastJobInstance(IMPORT_REFERENCE_POINT_CSV_JOB_NAME)).thenReturn(jobInstance);
-    when(jobExplorer.getLastJobExecution(jobInstance)).thenReturn(jobExecution);
-    when(jobLauncher.run(any(), any())).thenReturn(jobExecution);
-    //when
-    recoveryJobsRunner.run(new DefaultApplicationArguments());
-    //then
-    verify(jobLauncher).run(eq(importReferencePointCsvJob), any());
-    verify(fileService).clearDir();
-  }
 }
