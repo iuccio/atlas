0af3f819a64470cebf29e658ddab8c8dab8af505
atlas <19825793+iuccio@users.noreply.github.com>
2024-01-31 18:24:17 +0100

    ATLAS-1644: Fix for ATLAS-1674.

--- service-point-directory/src/test/java/ch/sbb/atlas/servicepointdirectory/controller/ServicePointStatusDeciderAllScenariosTest.java
+++ service-point-directory/src/test/java/ch/sbb/atlas/servicepointdirectory/controller/ServicePointStatusDeciderAllScenariosTest.java
@@ -1119,6 +1119,46 @@ class ServicePointStatusDeciderAllScenariosTest extends BaseControllerApiTest {
                 .andExpect(jsonPath("$[0].status", is(Status.DRAFT.toString())));
     }
 
+    /**
+     * Szenario 22: Haltestelle (stopPoint = true) in Status VALIDATED und No Geolocation, update mit Geolocation ändern in der Schweiz
+     * <p>
+     * NEU:       |________________Haltestelle A Hausen + Geolocation Switzerland____________|
+     * <p>
+     * IST:       |________________Haltestelle A Hausen + No Geolocation ____________________|
+     * Status:                      VALIDATED
+     * <p>
+     * RESULTAT:  |________________Haltestelle A Hausen + Geolocation Switzerland____________|
+     * Status:                      DRAFT
+     */
+    @Test
+    void scenario22WhenStopPointWithNoGeolocationAndUpdateStopPointWithNewSwissGeolocationThenStopPointDraft() throws Exception {
+        CreateServicePointVersionModel stopPoint1 = ServicePointTestData.getAargauServicePointVersionModel();
+        stopPoint1.setValidTo(LocalDate.of(2015, 12, 31));
+        stopPoint1.setDesignationOfficial("A Hausen");
+        stopPoint1.setServicePointGeolocation(null);
+        ReadServicePointVersionModel servicePointVersionModel = servicePointController.createServicePoint(
+                stopPoint1);
+        Long id = servicePointVersionModel.getId();
+        // Check that status for no geolocation is validated
+        assertThat(servicePointVersionModel.getStatus()).isEqualTo(Status.VALIDATED);
+
+        GeoReference geoReferenceSwitzerland = GeoReference.builder().country(Country.SWITZERLAND).build();
+        when(geoReferenceService.getGeoReference(any(), anyBoolean())).thenReturn(geoReferenceSwitzerland);
+        UpdateServicePointVersionModel stopPoint3 = ServicePointTestData.getAargauServicePointVersionModel();
+        stopPoint3.setServicePointGeolocation(ServicePointGeolocationMapper.toCreateModel(ServicePointTestData.getAargauServicePointGeolocation()));
+        stopPoint3.setValidTo(LocalDate.of(2015, 12, 31));
+        stopPoint3.setDesignationOfficial("A Hausen");
+
+        mvc.perform(put("/v1/service-points/" + id)
+                        .contentType(contentType)
+                        .content(mapper.writeValueAsString(stopPoint3)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0]." + ServicePointVersionModel.Fields.validFrom, is("2010-12-11")))
+                .andExpect(jsonPath("$[0]." + ServicePointVersionModel.Fields.validTo, is("2015-12-31")))
+                .andExpect(jsonPath("$[0].status", is(Status.DRAFT.toString())));
+    }
+
     @Test
     void whenStopPointWithFrenchGeolocationAndUpdateStopPointWithNewSwissGeolocationAndNewNameThenStopPointDraft() throws Exception {
         GeoReference geoReferenceFrance = GeoReference.builder().country(Country.FRANCE).build();
