1dc19c8a796adf5bd98ede8c0ae3f817d696aa8c
atlas <19825793+iuccio@users.noreply.github.com>
2024-02-01 09:00:06 +0100

    ATLAS-1595 fixed lint error

--- frontend/src/app/pages/sepodi/geography/geography.component.spec.ts
+++ frontend/src/app/pages/sepodi/geography/geography.component.spec.ts
@@ -1,4 +1,4 @@
-import {ComponentFixture, fakeAsync, TestBed, tick, waitForAsync} from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import {
   TranslateFakeLoader,
   TranslateLoader,
