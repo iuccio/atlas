2315da998f5749e78de866a045fe8801f37071ff
atlas <19825793+iuccio@users.noreply.github.com>
2024-02-05 11:08:23 +0100

    ATLAS-1678: Add latest files for reference point migration tests.

--- import-service-point/src/test/java/ch/sbb/importservice/migration/referencepoint/ReferencePointMigrationAcutalDateIntegrationTest.java
+++ import-service-point/src/test/java/ch/sbb/importservice/migration/referencepoint/ReferencePointMigrationAcutalDateIntegrationTest.java
@@ -28,9 +28,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ReferencePointMigrationAcutalDateIntegrationTest {
 
-    private static final String DIDOK_REFERENCE_POINT_CSV_FILE = "PRM_REFERENCE_POINTS_TEST_20240201013707.csv";
-    private static final String ATLAS_REFERENCE_POINT_CSV_FILE = "actual-date-reference-point-2024-02-04.csv";
-    private static final LocalDate ACTUAL_DATE = LocalDate.of(2024, 2, 4);
+    private static final String DIDOK_REFERENCE_POINT_CSV_FILE = "PRM_REFERENCE_POINTS_20240205013249.csv";
+    private static final String ATLAS_REFERENCE_POINT_CSV_FILE = "actual-date-reference-point-2024-02-05.csv";
+    private static final LocalDate ACTUAL_DATE = LocalDate.of(2024, 2, 5);
 
     private static final List<ReferencePointCsvModel> didokReferencePointCsvLines = new ArrayList<>();
     private static final List<ReferencePointVersionCsvModel> atlasReferencePointCsvLines = new ArrayList<>();
