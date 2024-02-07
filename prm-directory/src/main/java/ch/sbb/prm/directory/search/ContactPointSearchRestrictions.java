832af8323ad5994dac8dfa638320e99615622f29
atlas <19825793+iuccio@users.noreply.github.com>
2024-02-07 21:07:25 +0100

    ATLAS-1455: Add contact point type filter in search contactPoints.

--- prm-directory/src/main/java/ch/sbb/prm/directory/search/ContactPointSearchRestrictions.java
+++ prm-directory/src/main/java/ch/sbb/prm/directory/search/ContactPointSearchRestrictions.java
@@ -1,12 +1,51 @@
 package ch.sbb.prm.directory.search;
 
+import ch.sbb.atlas.searching.SpecificationBuilder;
+import ch.sbb.atlas.searching.specification.ValidOrEditionTimerangeSpecification;
+import ch.sbb.prm.directory.controller.model.ContactPointObjectRequestParams;
+import ch.sbb.prm.directory.entity.BasePrmEntityVersion;
 import ch.sbb.prm.directory.entity.ContactPointVersion;
+import ch.sbb.prm.directory.entity.ContactPointVersion_;
 import lombok.Getter;
+import lombok.Singular;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.List;
+import java.util.Optional;
 
 @Getter
 @ToString
 @SuperBuilder
-public class ContactPointSearchRestrictions extends BasePrmSearchRestrictions<ContactPointVersion> {
+public class ContactPointSearchRestrictions {
+
+    private final Pageable pageable;
+    private final ContactPointObjectRequestParams contactPointObjectRequestParams;
+
+    @Singular(ignoreNullCollections = true)
+    private List<String> searchCriterias;
+
+    public Specification<ContactPointVersion> getSpecification() {
+        return specBuilder().searchCriteriaSpecification(searchCriterias)
+                .and(specBuilder().validOnSpecification(Optional.ofNullable(contactPointObjectRequestParams.getValidOn())))
+                .and(specBuilder().inSpecification(contactPointObjectRequestParams.getServicePointNumbers(), BasePrmEntityVersion.Fields.number))
+                .and(specBuilder().inSpecification(contactPointObjectRequestParams.getSloids(), BasePrmEntityVersion.Fields.sloid))
+                .and(specBuilder().inSpecification(contactPointObjectRequestParams.getContactPointTypes(), ContactPointVersion.Fields.type))
+                .and(new ValidOrEditionTimerangeSpecification<>(
+                        contactPointObjectRequestParams.getFromDate(),
+                        contactPointObjectRequestParams.getToDate(),
+                        contactPointObjectRequestParams.getCreatedAfter(),
+                        contactPointObjectRequestParams.getModifiedAfter()));
+
+    }
+
+    protected SpecificationBuilder<ContactPointVersion> specBuilder() {
+        return SpecificationBuilder.<ContactPointVersion>builder()
+                .stringAttributes(List.of(BasePrmEntityVersion.Fields.number))
+                .validFromAttribute(ContactPointVersion_.validFrom)
+                .validToAttribute(ContactPointVersion_.validTo)
+                .build();
+    }
 }
