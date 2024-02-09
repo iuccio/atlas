8625dab3775b7bc8c4b4de5fa178ae51ef1a839f
atlas <TechSupport-ATLAS@sbb.ch>
2024-02-09 12:09:56 +0100

    ATLAS-1352: review

--- prm-directory/src/main/java/ch/sbb/prm/directory/service/ReferencePointService.java
+++ prm-directory/src/main/java/ch/sbb/prm/directory/service/ReferencePointService.java
@@ -107,8 +107,8 @@ public class ReferencePointService extends PrmVersionableService<ReferencePointV
   public ReferencePointVersion createReferencePoint(ReferencePointVersion referencePointVersion) {
     stopPointService.checkStopPointExists(referencePointVersion.getParentServicePointSloid());
     stopPointService.validateIsNotReduced(referencePointVersion.getParentServicePointSloid());
-    locationService.allocateSloid(referencePointVersion, SloidType.REFERENCE_POINT);
     referencePointValidationService.validatePreconditionBusinessRule(referencePointVersion);
+    locationService.allocateSloid(referencePointVersion, SloidType.REFERENCE_POINT);
 
     searchAndUpdatePlatformRelation(referencePointVersion.getParentServicePointSloid(), referencePointVersion.getSloid());
     searchAndUpdateToiletRelation(referencePointVersion.getParentServicePointSloid(), referencePointVersion.getSloid());
