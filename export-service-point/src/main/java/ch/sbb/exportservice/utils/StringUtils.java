c610f6b92364de826539dd91d29b218c69590d79
atlas <19825793+iuccio@users.noreply.github.com>
2024-02-12 17:03:45 +0100

    ATÃLAS-1698: csv centralize remove new line in field

--- export-service-point/src/main/java/ch/sbb/exportservice/utils/StringUtils.java
+++ export-service-point/src/main/java/ch/sbb/exportservice/utils/StringUtils.java
@@ -6,10 +6,15 @@ import lombok.experimental.UtilityClass;
 public class StringUtils {
 
   public static final String SEMICOLON = ";";
+  public static final String NEW_LINE = "\n";
   private static final String COLON = ":";
 
   public String replaceSemiColonWithColon(String value) {
-    return value != null ? value.replaceAll(SEMICOLON, COLON) : null;
+    return value != null ? value.replace(SEMICOLON, COLON) : null;
+  }
+
+  public String removeNewLine(String string) {
+    return string == null ? null : string.replaceAll("\r\n|\r|\n", " ");
   }
 
 }
