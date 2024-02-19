cca4e9e69cb5ff708d9a5017a9f3ef0f1522d475
atlas <19825793+iuccio@users.noreply.github.com>
2024-02-19 15:00:41 +0100

    ATLAS-1354: Fix warning in tests.

--- frontend/src/app/pages/prm/tabs/contact-point/contact-point-table.component.spec.ts
+++ frontend/src/app/pages/prm/tabs/contact-point/contact-point-table.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContactPointTableComponent } from './contact-point-table.component';
 import { STOP_POINT } from '../../util/stop-point-test-data.spec';
-import { MockAtlasButtonComponent } from '../../../../app.testing.mocks';
+import { MockAtlasButtonComponent, MockTableComponent } from '../../../../app.testing.mocks';
 import { AppTestingModule } from '../../../../app.testing.module';
 import { ActivatedRoute } from '@angular/router';
 import { BERN_WYLEREGG } from '../../../../../test/data/service-point';
@@ -16,7 +16,7 @@ describe('ContactPointTableComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ContactPointTableComponent, MockAtlasButtonComponent],
+      declarations: [ContactPointTableComponent, MockAtlasButtonComponent, MockTableComponent],
       imports: [AppTestingModule],
       providers: [{ provide: ActivatedRoute, useValue: activatedRouteMock }],
     });
