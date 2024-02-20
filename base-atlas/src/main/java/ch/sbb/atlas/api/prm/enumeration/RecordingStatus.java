2706c39e77fd43fe7ce63679ceebee3e4a36cb68
atlas <19825793+iuccio@users.noreply.github.com>
2024-02-20 17:35:19 +0100

    Increase coverage for frontend

--- base-atlas/src/main/java/ch/sbb/atlas/api/prm/enumeration/RecordingStatus.java
+++ base-atlas/src/main/java/ch/sbb/atlas/api/prm/enumeration/RecordingStatus.java
@@ -1,5 +1,8 @@
 package ch.sbb.atlas.api.prm.enumeration;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(enumAsRef = true)
 public enum RecordingStatus {
   COMPLETE,
   INCOMPLETE
