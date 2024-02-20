db820384704d4769b3edc30667f0d3b1bd251464
atlas <19825793+iuccio@users.noreply.github.com>
2024-02-20 17:35:19 +0100

    Increase coverage for frontend

--- frontend/src/app/pages/prm/tabs/contact-point/contact-point-table.component.spec.ts
+++ frontend/src/app/pages/prm/tabs/contact-point/contact-point-table.component.spec.ts
@@ -1,31 +1,82 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ContactPointTableComponent } from './contact-point-table.component';
-import { STOP_POINT } from '../../util/stop-point-test-data.spec';
-import { MockAtlasButtonComponent, MockTableComponent } from '../../../../app.testing.mocks';
-import { AppTestingModule } from '../../../../app.testing.module';
-import { ActivatedRoute } from '@angular/router';
-import { BERN_WYLEREGG } from '../../../../../test/data/service-point';
+import {ContactPointTableComponent} from './contact-point-table.component';
+import {STOP_POINT} from '../../util/stop-point-test-data.spec';
+import {MockAtlasButtonComponent, MockTableComponent} from '../../../../app.testing.mocks';
+import {AppTestingModule} from '../../../../app.testing.module';
+import {ActivatedRoute, Router} from '@angular/router';
+import {BERN_WYLEREGG} from '../../../../../test/data/service-point';
+import {of} from "rxjs";
+import {ContactPointOverview, ContactPointType, PersonWithReducedMobilityService, StandardAttributeType} from "../../../../api";
+
+const contactPointOverview: ContactPointOverview[] = [{
+  creationDate: '2024-01-22T13:52:30.598026',
+  creator: 'e524381',
+  editionDate: '2024-01-22T13:52:30.598026',
+  editor: 'e524381',
+  id: 1000,
+  sloid: 'ch:1:sloid:12345:1',
+  validFrom: new Date('2000-01-01'),
+  validTo: new Date('2000-12-31'),
+  etagVersion: 0,
+  parentServicePointSloid: 'ch:1:sloid:7000',
+  designation: 'designation',
+  additionalInformation: 'additional',
+  inductionLoop: StandardAttributeType.ToBeCompleted,
+  openingHours: 'openingHours',
+  wheelchairAccess: StandardAttributeType.ToBeCompleted,
+  type: ContactPointType.InformationDesk,
+  recordingStatus: "INCOMPLETE"
+}];
 
 describe('ContactPointTableComponent', () => {
   let component: ContactPointTableComponent;
   let fixture: ComponentFixture<ContactPointTableComponent>;
+  let router:Router;
+
+  const personWithReducedMobilityService = jasmine.createSpyObj('personWithReducedMobilityService', ['getContactPointOverview']);
+  personWithReducedMobilityService.getContactPointOverview.and.returnValue(of(contactPointOverview));
+
   const activatedRouteMock = {
-    parent: { snapshot: { data: { stopPoints: [STOP_POINT], servicePoints: [BERN_WYLEREGG] } } },
+    parent: {
+      snapshot: {
+        params: {stopPointSloid: STOP_POINT.sloid},
+        data: {stopPoints: [STOP_POINT], servicePoints: [BERN_WYLEREGG]}
+      }
+    },
   };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ContactPointTableComponent, MockAtlasButtonComponent, MockTableComponent],
       imports: [AppTestingModule],
-      providers: [{ provide: ActivatedRoute, useValue: activatedRouteMock }],
+      providers: [
+        { provide: ActivatedRoute, useValue: activatedRouteMock },
+        { provide: PersonWithReducedMobilityService, useValue: personWithReducedMobilityService }
+      ],
     });
     fixture = TestBed.createComponent(ContactPointTableComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    router = TestBed.inject(Router);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load table', () => {
+    component.getOverview({page: 0, size: 10});
+
+    expect(component.contactPoints.length).toBe(1);
+  });
+
+  it('should navigate on table click', () => {
+    spyOn(router, 'navigate').and.returnValue(Promise.resolve(true));
+
+    component.getOverview({page: 0, size: 10});
+
+    component.rowClicked(component.contactPoints[0]);
+    expect(router.navigate).toHaveBeenCalled();
   });
 });
--- frontend/src/app/pages/prm/tabs/reference-point/reference-point-table.component.spec.ts
+++ frontend/src/app/pages/prm/tabs/reference-point/reference-point-table.component.spec.ts
@@ -2,30 +2,86 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ReferencePointTableComponent } from './reference-point-table.component';
 import { AppTestingModule } from '../../../../app.testing.module';
-import { ActivatedRoute } from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import { MockAtlasButtonComponent, MockTableComponent } from '../../../../app.testing.mocks';
 import { STOP_POINT } from '../../util/stop-point-test-data.spec';
 import { BERN_WYLEREGG } from '../../../../../test/data/service-point';
+import {PersonWithReducedMobilityService, ReadReferencePointVersion} from "../../../../api";
+import {of} from "rxjs";
+
+const referencePointOverview: ReadReferencePointVersion[] = [
+  {
+    creationDate: '2024-01-22T13:52:30.598026',
+    creator: 'e524381',
+    editionDate: '2024-01-22T13:52:30.598026',
+    editor: 'e524381',
+    id: 1000,
+    sloid: 'ch:1:sloid:12345:1',
+    validFrom: new Date('2000-01-01'),
+    validTo: new Date('2000-12-31'),
+    etagVersion: 0,
+    parentServicePointSloid: 'ch:1:sloid:7000',
+    designation: 'designation',
+    additionalInformation: 'additional',
+    mainReferencePoint: true,
+    referencePointType: 'PLATFORM',
+    number: {
+      number: 8507000,
+      numberShort: 7000,
+      uicCountryCode: 85,
+      checkDigit: 3,
+    },
+  },
+];
 
 describe('ReferencePointTableComponent', () => {
   let component: ReferencePointTableComponent;
   let fixture: ComponentFixture<ReferencePointTableComponent>;
+  let router:Router;
+
+  const personWithReducedMobilityService = jasmine.createSpyObj('personWithReducedMobilityService', ['getReferencePointsOverview']);
+  personWithReducedMobilityService.getReferencePointsOverview.and.returnValue(of(referencePointOverview));
+
   const activatedRouteMock = {
-    parent: { snapshot: { data: { stopPoints: [STOP_POINT], servicePoints: [BERN_WYLEREGG] } } },
+    parent: {
+      snapshot: {
+        params: {stopPointSloid: STOP_POINT.sloid},
+        data: {stopPoints: [STOP_POINT], servicePoints: [BERN_WYLEREGG]}
+      }
+    },
   };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ReferencePointTableComponent, MockAtlasButtonComponent, MockTableComponent],
       imports: [AppTestingModule],
-      providers: [{ provide: ActivatedRoute, useValue: activatedRouteMock }],
+      providers: [
+        { provide: ActivatedRoute, useValue: activatedRouteMock },
+        { provide: PersonWithReducedMobilityService, useValue: personWithReducedMobilityService }
+      ],
     });
     fixture = TestBed.createComponent(ReferencePointTableComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    router = TestBed.inject(Router);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load table', () => {
+    component.getOverview({page: 0, size: 10});
+
+    expect(component.referencePoints.length).toBe(1);
+  });
+
+  it('should navigate on table click', () => {
+    spyOn(router, 'navigate').and.returnValue(Promise.resolve(true));
+
+    component.getOverview({page: 0, size: 10});
+
+    component.rowClicked(component.referencePoints[0]);
+    expect(router.navigate).toHaveBeenCalled();
   });
 });
