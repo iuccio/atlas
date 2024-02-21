faf3cfad750c96bc85f2629a02a7da56cc4d572d
atlas <TechSupport-ATLAS@sbb.ch>
2024-02-21 10:50:48 +0100

    ATLAS-1466: small ref

--- base-atlas/src/main/java/ch/sbb/atlas/api/prm/model/toilet/ToiletVersionModel.java
+++ base-atlas/src/main/java/ch/sbb/atlas/api/prm/model/toilet/ToiletVersionModel.java
@@ -1,13 +1,10 @@
 package ch.sbb.atlas.api.prm.model.toilet;
 
-import ch.sbb.atlas.api.AtlasFieldLengths;
 import ch.sbb.atlas.api.prm.enumeration.StandardAttributeType;
-import ch.sbb.atlas.api.prm.model.BasePrmVersionModel;
-import ch.sbb.atlas.api.prm.model.PrmApiConstants;
+import ch.sbb.atlas.api.prm.model.BasePrmStopPointChildVersionModel;
 import ch.sbb.atlas.validation.DatesValidator;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -22,22 +19,7 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 @FieldNameConstants
 @Schema(name = "ToiletVersion")
-public class ToiletVersionModel extends BasePrmVersionModel implements DatesValidator {
-
-  @Size(min = 1, max = AtlasFieldLengths.LENGTH_500)
-  @Schema(description = PrmApiConstants.PARENT_SLOID_DESCRIPTION, example = "ch:1:sloid:18771")
-  @NotNull
-  private String parentServicePointSloid;
-
-  @Schema(description = "Long designation of a location. Used primarily in customer information. "
-      + "Not all systems can process names of this length.", example = "Biel/Bienne Bözingenfeld/Champs-de-Boujean")
-  @NotNull
-  @Size(max = AtlasFieldLengths.LENGTH_50)
-  private String designation;
-
-  @Schema(description = "Additional Information")
-  @Size(max = AtlasFieldLengths.LENGTH_2000)
-  private String additionalInformation;
+public class ToiletVersionModel extends BasePrmStopPointChildVersionModel implements DatesValidator {
 
   @NotNull
   private StandardAttributeType wheelchairToilet;
