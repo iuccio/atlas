18916cbeb2997a96995e781d064f23c6e4ed60f0
atlas <TechSupport-ATLAS@sbb.ch>
2024-02-29 17:32:26 +0100

    ATLAS-1789: Add missing tests.

--- import-service-point/src/test/java/ch/sbb/importservice/service/csv/ContactPointCsvServiceTest.java
+++ import-service-point/src/test/java/ch/sbb/importservice/service/csv/ContactPointCsvServiceTest.java
@@ -1,4 +1,5 @@
 package ch.sbb.importservice.service.csv;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.MockitoAnnotations.openMocks;
 
@@ -7,12 +8,11 @@ import ch.sbb.atlas.imports.prm.contactpoint.ContactPointCsvModelContainer;
 import ch.sbb.atlas.testdata.prm.ContactPointCsvTestData;
 import ch.sbb.importservice.service.FileHelperService;
 import ch.sbb.importservice.service.JobHelperService;
+import java.time.LocalDate;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-
-import java.time.LocalDate;
-import java.util.List;
 
 public class ContactPointCsvServiceTest {
     private ContactPointCsvService contactPointCsvService;
@@ -98,4 +98,55 @@ public class ContactPointCsvServiceTest {
         assertThat(result.get(0).getCsvModels()).hasSize(1);
         assertThat(result.get(0).getCsvModels().get(0).getStatus()).isEqualTo(1);
     }
+
+    @Test
+    void shouldSetReplaceHighDateDataForContactPoints() {
+        // given
+        ContactPointCsvModel contactPointCsvModel1 = ContactPointCsvTestData.getCsvModel();
+        contactPointCsvModel1.setStatus(1);
+        ContactPointCsvModel contactPointCsvModel2 = ContactPointCsvTestData.getCsvModel();
+        contactPointCsvModel2.setValidFrom(contactPointCsvModel1.getValidTo().plusDays(1));
+        contactPointCsvModel2.setValidTo(LocalDate.of(2099, 12, 31));
+        contactPointCsvModel2.setStatus(1);
+        List<ContactPointCsvModel> csvModels = List.of(contactPointCsvModel1,contactPointCsvModel2);
+        LocalDate now = LocalDate.now();
+
+        // when
+        List<ContactPointCsvModelContainer> result = contactPointCsvService.mapToContactPointCsvModelContainers(csvModels);
+
+        //then
+        assertThat(result).hasSize(1);
+        assertThat(result.get(0).getCsvModels()).hasSize(1);
+        assertThat(result.get(0).getCsvModels().get(0).getValidFrom()).isEqualTo(contactPointCsvModel1.getValidFrom());
+        assertThat(result.get(0).getCsvModels().get(0).getValidTo()).isEqualTo(LocalDate.of(9999, 12, 31));
+        assertThat(result.get(0).getCsvModels().get(0).getModifiedAt().toLocalDate()).isEqualTo(now);
+    }
+
+
+    @Test
+    void shouldSetReplaceNewLineForContactPoints() {
+        // given
+        ContactPointCsvModel contactPointCsvModel1 = ContactPointCsvTestData.getCsvModel();
+        contactPointCsvModel1.setDescription("Desc $newline$ of $newline$ this model.");
+        contactPointCsvModel1.setStatus(1);
+        ContactPointCsvModel contactPointCsvModel2 = ContactPointCsvTestData.getCsvModel();
+        contactPointCsvModel2.setValidFrom(contactPointCsvModel1.getValidTo().plusDays(1));
+        contactPointCsvModel2.setDescription("Desc $newline$ of $newline$ this model.");
+        contactPointCsvModel2.setValidTo(LocalDate.of(2026, 12, 31));
+        contactPointCsvModel2.setStatus(1);
+        List<ContactPointCsvModel> csvModels = List.of(contactPointCsvModel1,contactPointCsvModel2);
+        LocalDate now = LocalDate.now();
+
+        // when
+        List<ContactPointCsvModelContainer> result = contactPointCsvService.mapToContactPointCsvModelContainers(csvModels);
+
+        //then
+        assertThat(result).hasSize(1);
+        assertThat(result.get(0).getCsvModels()).hasSize(1);
+        assertThat(result.get(0).getCsvModels().get(0).getValidFrom()).isEqualTo(contactPointCsvModel1.getValidFrom());
+        assertThat(result.get(0).getCsvModels().get(0).getValidTo()).isEqualTo(LocalDate.of(2026, 12, 31));
+        assertThat(result.get(0).getCsvModels().get(0).getDescription()).isEqualTo("Desc \r\n of \r\n this model.");
+        assertThat(result.get(0).getCsvModels().get(0).getModifiedAt().toLocalDate()).isEqualTo(now);
+    }
+
 }
--- import-service-point/src/test/java/ch/sbb/importservice/service/csv/TrafficPointCsvServiceTest.java
+++ import-service-point/src/test/java/ch/sbb/importservice/service/csv/TrafficPointCsvServiceTest.java
@@ -8,6 +8,7 @@ import ch.sbb.atlas.imports.servicepoint.trafficpoint.TrafficPointElementCsvMode
 import ch.sbb.importservice.service.FileHelperService;
 import ch.sbb.importservice.service.JobHelperService;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -74,6 +75,93 @@ class TrafficPointCsvServiceTest {
         LocalDate.of(2021, 1, 1));
     assertThat(trafficPointCsvModelContainers.get(1).getCsvModelList().get(0).getValidTo()).isEqualTo(
         LocalDate.of(2022, 12, 31));
+  }
+
+  @Test
+  void shouldSetReplaceHighDateDataForTrafficPoints() {
+    // given
+    List<TrafficPointElementCsvModel> csvModels = List.of(
+        TrafficPointElementCsvModel.builder()
+            .sloid("ch:1:sloid:123")
+            .servicePointNumber(8507000)
+            .validFrom(LocalDate.of(2020, 1, 1))
+            .validTo(LocalDate.of(2020, 12, 31))
+            .editedAt(LocalDateTime.of(2020,1,1,15,15))
+            .height(800.88)
+            .build(),
+        TrafficPointElementCsvModel.builder()
+            .sloid("ch:1:sloid:567")
+            .servicePointNumber(8507000)
+            .validFrom(LocalDate.of(2021, 1, 1))
+            .validTo(LocalDate.of(2099, 12, 31))
+            .editedAt(LocalDateTime.of(2020,1,1,15,15))
+            .height(500.88)
+            .build()
+    );
+    LocalDate now = LocalDate.now();
+
+    // when
+    List<TrafficPointCsvModelContainer> trafficPointCsvModelContainers =
+        trafficPointCsvService.mapToTrafficPointCsvModelContainers(
+            csvModels);
+
+    // then
+    assertThat(trafficPointCsvModelContainers).hasSize(2);
+    assertThat(trafficPointCsvModelContainers.get(0).getCsvModelList()).hasSize(1);
+    assertThat(trafficPointCsvModelContainers.get(0).getCsvModelList().get(0).getValidFrom()).isEqualTo(LocalDate.of(2020, 1, 1));
+    assertThat(trafficPointCsvModelContainers.get(0).getCsvModelList().get(0).getValidTo()).isEqualTo(LocalDate.of(2020, 12, 31));
+    assertThat(trafficPointCsvModelContainers.get(0).getCsvModelList().get(0).getEditedAt())
+        .isEqualTo(LocalDateTime.of(2020,1,1,15,15));
+    assertThat(trafficPointCsvModelContainers.get(1).getCsvModelList()).hasSize(1);
+    assertThat(trafficPointCsvModelContainers.get(1).getCsvModelList().get(0).getValidFrom()).isEqualTo(LocalDate.of(2021, 1, 1));
+    assertThat(trafficPointCsvModelContainers.get(1).getCsvModelList().get(0).getValidTo()).isEqualTo(LocalDate.of(9999, 12, 31));
+    assertThat(trafficPointCsvModelContainers.get(1).getCsvModelList().get(0).getEditedAt().toLocalDate())
+        .isEqualTo(now);
+  }
+
+  @Test
+  void shouldSetReplaceNewLineForTrafficPoints() {
+    // given
+    List<TrafficPointElementCsvModel> csvModels = List.of(
+        TrafficPointElementCsvModel.builder()
+            .sloid("ch:1:sloid:123")
+            .designationOperational("Desc $newline$ of $newline$ this model.")
+            .servicePointNumber(8507000)
+            .validFrom(LocalDate.of(2020, 1, 1))
+            .validTo(LocalDate.of(2020, 12, 31))
+            .editedAt(LocalDateTime.of(2020,1,1,15,15))
+            .height(800.88)
+            .build(),
+        TrafficPointElementCsvModel.builder()
+            .sloid("ch:1:sloid:567")
+            .designationOperational("Desc $newline$ of $newline$ this model.")
+            .servicePointNumber(8507000)
+            .validFrom(LocalDate.of(2021, 1, 1))
+            .validTo(LocalDate.of(2021, 12, 31))
+            .editedAt(LocalDateTime.of(2020,1,1,15,15))
+            .height(500.88)
+            .build()
+    );
+    LocalDate now = LocalDate.now();
+
+    // when
+    List<TrafficPointCsvModelContainer> trafficPointCsvModelContainers =
+        trafficPointCsvService.mapToTrafficPointCsvModelContainers(
+            csvModels);
+
+    // then
+    assertThat(trafficPointCsvModelContainers).hasSize(2);
+    assertThat(trafficPointCsvModelContainers.get(0).getCsvModelList()).hasSize(1);
+    assertThat(trafficPointCsvModelContainers.get(0).getCsvModelList().get(0).getValidFrom()).isEqualTo(LocalDate.of(2020, 1, 1));
+    assertThat(trafficPointCsvModelContainers.get(0).getCsvModelList().get(0).getValidTo()).isEqualTo(LocalDate.of(2020, 12, 31));
+    assertThat(trafficPointCsvModelContainers.get(0).getCsvModelList().get(0).getDesignationOperational()).isEqualTo("Desc \r\n of \r\n this model.");
+    assertThat(trafficPointCsvModelContainers.get(0).getCsvModelList().get(0).getEditedAt().toLocalDate()).isEqualTo(now);
+    assertThat(trafficPointCsvModelContainers.get(1).getCsvModelList()).hasSize(1);
+    assertThat(trafficPointCsvModelContainers.get(1).getCsvModelList().get(0).getValidFrom()).isEqualTo(LocalDate.of(2021, 1, 1));
+    assertThat(trafficPointCsvModelContainers.get(1).getCsvModelList().get(0).getValidTo()).isEqualTo(LocalDate.of(2021, 12, 31));
+    assertThat(trafficPointCsvModelContainers.get(1).getCsvModelList().get(0).getDesignationOperational()).isEqualTo("Desc \r\n"
+        + " of \r\n this model.");
+    assertThat(trafficPointCsvModelContainers.get(1).getCsvModelList().get(0).getEditedAt().toLocalDate()).isEqualTo(now);
   }
 
 }
