77b2d9e905f8a68f6c145a9940ca3afabea52771
atlas <19825793+iuccio@users.noreply.github.com>
2024-03-06 16:16:07 +0100

    ATLAS-1444: add migration tests

--- import-service-point/src/main/java/ch/sbb/importservice/entity/RelationKeyId.java
+++ import-service-point/src/main/java/ch/sbb/importservice/entity/RelationKeyId.java
@@ -5,11 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @EqualsAndHashCode
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
 public class RelationKeyId {
     private String sloid;
     private String rpSloid;
