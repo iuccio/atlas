97bd95f984dd57dc0d1d6253d3fb685e1d4353d8
atlas <TechSupport-ATLAS@sbb.ch>
2024-03-11 22:12:15 +0100

    ATLAS-1741: Add new servicePoint files.

--- service-point-directory/src/test/java/ch/sbb/atlas/servicepointdirectory/migration/servicepoints/ServicePointMigrationFutureTimetableDateIntegrationTest.java
+++ service-point-directory/src/test/java/ch/sbb/atlas/servicepointdirectory/migration/servicepoints/ServicePointMigrationFutureTimetableDateIntegrationTest.java
@@ -28,8 +28,10 @@ import org.junit.jupiter.api.TestMethodOrder;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
  class ServicePointMigrationFutureTimetableDateIntegrationTest {
 
-  private static final String DIDOK_CSV_FILE = "DIDOK3_DIENSTSTELLEN_FUTURE_TIMETABLE_V_2_20240213013909.csv";
-  private static final String ATLAS_CSV_FILE = "future_timetable-world-service_point-2024-02-13.csv";
+//  private static final String DIDOK_CSV_FILE = "DIDOK3_DIENSTSTELLEN_FUTURE_TIMETABLE_V_2_20240213013909.csv";
+  private static final String DIDOK_CSV_FILE = "DIDOK3_DIENSTSTELLEN_FUTURE_TIMETABLE_V_2_20240307015346.csv";
+//  private static final String ATLAS_CSV_FILE = "future_timetable-world-service_point-2024-02-13.csv";
+  private static final String ATLAS_CSV_FILE = "future_timetable-world-service_point-2024-03-07.csv";
   private static final LocalDate FUTURE_TIMETABLE_DATE = LocalDate.of(2024, 12, 15);
 
   private static final List<ServicePointAtlasCsvModel> atlasCsvLines = new ArrayList<>();
