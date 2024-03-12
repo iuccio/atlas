71f8966100e00a7c668558df51e0b15df363e061
atlas <19825793+iuccio@users.noreply.github.com>
2024-03-12 18:07:04 +0100

    ATLAS-1741: Add loadingPoint migration tests adjustments.

--- service-point-directory/src/test/java/ch/sbb/atlas/servicepointdirectory/migration/loadingpoints/LoadingPointMigrationActualDateIntegrationTest.java
+++ service-point-directory/src/test/java/ch/sbb/atlas/servicepointdirectory/migration/loadingpoints/LoadingPointMigrationActualDateIntegrationTest.java
@@ -11,12 +11,11 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-@Disabled
+//@Disabled
 @IntegrationTest
 @Slf4j
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -24,7 +23,7 @@ import org.junit.jupiter.api.TestMethodOrder;
 
   private static final String DIDOK_CSV_FILE = "DIDOK3_LADESTELLEN_20240312011855.csv";
   private static final String ATLAS_CSV_FILE = "actual_date-world-loading_point-2024-03-12.csv";
-  private static final LocalDate ACTUAL_DATE = LocalDate.of(2023, 9, 6);
+  public static final LocalDate ACTUAL_DATE = LocalDate.of(2024, 3, 12);
 
   private static final List<LoadingPointAtlasCsvModel> atlasCsvLines = new ArrayList<>();
   private static final List<LoadingPointDidokCsvModel> didokCsvLines = new ArrayList<>();
