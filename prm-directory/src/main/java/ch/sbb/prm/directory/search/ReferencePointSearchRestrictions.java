c9dace5bd19d59de1aeb7d86be3d0156bfb5a3e1
atlas <19825793+iuccio@users.noreply.github.com>
2024-03-12 15:45:32 +0100

    ATLAS-1815: add referencePointAttributeTypes to the filter

--- prm-directory/src/main/java/ch/sbb/prm/directory/search/ReferencePointSearchRestrictions.java
+++ prm-directory/src/main/java/ch/sbb/prm/directory/search/ReferencePointSearchRestrictions.java
@@ -1,13 +1,34 @@
 package ch.sbb.prm.directory.search;
 
+import ch.sbb.prm.directory.controller.model.ReferencePointRequestParams;
 import ch.sbb.prm.directory.entity.ReferencePointVersion;
+import ch.sbb.prm.directory.entity.ReferencePointVersion_;
+import java.util.List;
 import lombok.Getter;
+import lombok.Singular;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 
 @Getter
 @ToString
 @SuperBuilder
 public class ReferencePointSearchRestrictions extends BasePrmSearchRestrictions<ReferencePointVersion> {
+
+  private final Pageable pageable;
+  private final ReferencePointRequestParams referencePointRequestParams;
+
+  @Singular(ignoreNullCollections = true)
+  private List<String> searchCriterias;
+
+  @Override
+  public Specification<ReferencePointVersion> getSpecification() {
+    super.setPageable(pageable);
+    super.setPrmObjectRequestParams(referencePointRequestParams);
+    return Specification.allOf(super.getSpecification())
+        .and(specBuilder().enumSpecification(referencePointRequestParams.getReferencePointAttributeTypes(),
+            ReferencePointVersion_.referencePointType));
+  }
 
 }
