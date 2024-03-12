65ed13a79eba32c488eb10e141f52c0b0ea8c278
atlas <19825793+iuccio@users.noreply.github.com>
2024-03-12 15:35:28 +0100

    ATLAS-1810: sort by sloid

--- prm-directory/src/main/java/ch/sbb/prm/directory/api/ContactPointApiV1.java
+++ prm-directory/src/main/java/ch/sbb/prm/directory/api/ContactPointApiV1.java
@@ -9,6 +9,7 @@ import ch.sbb.atlas.imports.ItemImportResult;
 import ch.sbb.atlas.imports.prm.contactpoint.ContactPointImportRequestModel;
 import ch.sbb.prm.directory.controller.model.ContactPointObjectRequestParams;
 import ch.sbb.prm.directory.entity.BasePrmEntityVersion;
+import ch.sbb.prm.directory.entity.BasePrmEntityVersion.Fields;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -34,7 +35,7 @@ public interface ContactPointApiV1 {
   @GetMapping
   @PageableAsQueryParam
   Container<ReadContactPointVersionModel> getContactPoints(
-          @Parameter(hidden = true) @PageableDefault(sort = {BasePrmEntityVersion.Fields.number,
+          @Parameter(hidden = true) @PageableDefault(sort = {Fields.sloid,
                   BasePrmEntityVersion.Fields.validFrom}) Pageable pageable,
           @Valid @ParameterObject ContactPointObjectRequestParams contactPointObjectRequestParams);
 
