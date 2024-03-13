52f4925284e7e485b03fd70acbf4f9b4579cc3f2
atlas <TechSupport-ATLAS@sbb.ch>
2024-03-13 10:58:25 +0100

    ATLAS-1832 added exception and check

--- prm-directory/src/main/java/ch/sbb/prm/directory/controller/PlatformController.java
+++ prm-directory/src/main/java/ch/sbb/prm/directory/controller/PlatformController.java
@@ -10,6 +10,7 @@ import ch.sbb.atlas.model.exception.NotFoundException.IdNotFoundException;
 import ch.sbb.prm.directory.api.PlatformApiV1;
 import ch.sbb.prm.directory.controller.model.PrmObjectRequestParams;
 import ch.sbb.prm.directory.entity.PlatformVersion;
+import ch.sbb.prm.directory.exception.PlatformAlreadyExistsException;
 import ch.sbb.prm.directory.mapper.PlatformVersionMapper;
 import ch.sbb.prm.directory.search.PlatformSearchRestrictions;
 import ch.sbb.prm.directory.service.PlatformService;
@@ -45,6 +46,10 @@ public class PlatformController implements PlatformApiV1 {
 
   @Override
   public ReadPlatformVersionModel createPlatform(PlatformVersionModel model) {
+    boolean isPlatformExisting = platformService.isPlatformExisting(model.getSloid());
+    if (isPlatformExisting) {
+      throw new PlatformAlreadyExistsException(model.getSloid());
+    }
     PlatformVersion platformVersion = PlatformVersionMapper.toEntity(model);
     PlatformVersion savedVersion = platformService.createPlatformVersion(platformVersion);
     return PlatformVersionMapper.toModel(savedVersion);
