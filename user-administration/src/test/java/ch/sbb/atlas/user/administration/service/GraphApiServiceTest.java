32eba32769c69f89c3ee40b18614c51897606e0f
atlas <19825793+iuccio@users.noreply.github.com>
2024-03-18 11:41:11 +0100

    ATLAS-1745: migrate to new graph api

--- user-administration/src/test/java/ch/sbb/atlas/user/administration/service/GraphApiServiceTest.java
+++ user-administration/src/test/java/ch/sbb/atlas/user/administration/service/GraphApiServiceTest.java
@@ -2,133 +2,88 @@ package ch.sbb.atlas.user.administration.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-import ch.sbb.atlas.api.user.administration.UserModel;
-import ch.sbb.atlas.api.user.administration.enumeration.UserAccountStatus;
-import com.microsoft.graph.models.User;
-import com.microsoft.graph.requests.GraphServiceClient;
-import com.microsoft.graph.requests.UserCollectionPage;
-import com.microsoft.graph.requests.UserCollectionRequest;
-import com.microsoft.graph.requests.UserCollectionRequestBuilder;
-import com.microsoft.graph.requests.UserCollectionResponse;
-import java.util.ArrayList;
+import com.microsoft.graph.models.UserCollectionResponse;
+import com.microsoft.graph.serviceclient.GraphServiceClient;
+import com.microsoft.graph.users.UsersRequestBuilder;
+import com.microsoft.graph.users.UsersRequestBuilder.GetRequestConfiguration;
 import java.util.List;
-import okhttp3.Request;
+import java.util.function.Consumer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
- class GraphApiServiceTest {
+class GraphApiServiceTest {
+
+  @Mock
+  private GraphServiceClient graphClient;
+
+  @Mock
+  private UsersRequestBuilder usersRequestBuilder;
+
+  @Mock
+  private UserCollectionResponse userCollectionResponse;
 
   private GraphApiService graphApiService;
 
-  @Mock
-  private GraphServiceClient<Request> graphServiceClientMock;
-
-  @Mock
-  private UserCollectionRequestBuilder userCollectionRequestBuilderMock;
-
-  @Mock
-  private UserCollectionRequest userCollectionRequestMock;
-
-  @Mock
-  private UserCollectionPage userCollectionPageMock;
-
-  @Mock
-  private IGraphApiBatchRequestService graphApiBatchRequestService;
+  @Captor
+  private ArgumentCaptor<Consumer<GetRequestConfiguration>> getRequestConfigCaptor;
 
   @BeforeEach
   void setUp() {
     MockitoAnnotations.openMocks(this);
-    graphApiService = new GraphApiService(graphServiceClientMock, graphApiBatchRequestService);
+    graphApiService = new GraphApiService(graphClient);
+
+    when(graphClient.users()).thenReturn(usersRequestBuilder);
+    when(usersRequestBuilder.get(any())).thenReturn(userCollectionResponse);
   }
 
   @Test
-  void shouldSearchUsersByDisplayNameAndMail() {
-    doReturn(userCollectionRequestBuilderMock).when(graphServiceClientMock).users();
-    doReturn(userCollectionRequestMock).when(userCollectionRequestBuilderMock)
-                                       .buildRequest(anyList());
-    doReturn(userCollectionRequestMock).when(userCollectionRequestMock).top(anyInt());
-    doReturn(userCollectionRequestMock).when(userCollectionRequestMock).select(anyString());
-    doReturn(userCollectionPageMock).when(userCollectionRequestMock).get();
+  void shouldSearchUsers() {
+    graphApiService.searchUsers("U236171");
 
-    User user = new User();
-    user.onPremisesSamAccountName = "u236171";
-    user.accountEnabled = true;
-    User user2 = new User();
-    user2.onPremisesSamAccountName = "u123456";
-    user2.accountEnabled = null;
-    User user3 = new User();
-    user3.onPremisesSamAccountName = "u654321";
-    user3.accountEnabled = false;
+    GetRequestConfiguration configuration = verifyGetAndReturnConfiguration();
 
-    doReturn(List.of(user, user2, user3)).when(userCollectionPageMock).getCurrentPage();
+    String expectedSearchFilter = """
+        "onPremisesSamAccountName:U236171" OR "mail:U236171" OR "displayName:U236171"
+        """;
+    assertThat(configuration.queryParameters).isNotNull();
+    assertThat(configuration.queryParameters.search).isEqualTo(expectedSearchFilter);
+    assertThat(configuration.queryParameters.top).isEqualTo(10);
 
-    List<UserModel> result = graphApiService.searchUsers("test");
-
-    verify(userCollectionRequestMock, times(3)).get();
-    assertThat(result).hasSize(3);
-    assertThat(result.get(0).getUserId()).isEqualTo("u236171");
-    assertThat(result.get(0).getAccountStatus()).isEqualTo(UserAccountStatus.ACTIVE);
-
-    assertThat(result.get(1).getUserId()).isEqualTo("u123456");
-    assertThat(result.get(1).getAccountStatus()).isEqualTo(UserAccountStatus.INACTIVE);
-
-    assertThat(result.get(2).getUserId()).isEqualTo("u654321");
-    assertThat(result.get(2).getAccountStatus()).isEqualTo(UserAccountStatus.INACTIVE);
+    assertThat(configuration.headers).isNotNull();
+    assertThat(configuration.headers).hasSize(1);
+    assertThat(configuration.headers).containsKey("ConsistencyLevel");
   }
 
   @Test
-  void shouldResolveLdapUserDataFromUserIds() {
-    List<String> userIds = new ArrayList<>();
-    for (int i = 0; i < 18; i++) {
-      userIds.add("u1234" + i);
-    }
+  void shouldResolveUsers() {
+    graphApiService.resolveUsers(List.of("U236171", "e502999"));
 
-    doReturn(userCollectionRequestBuilderMock).when(graphServiceClientMock).users();
-    doReturn(userCollectionRequestMock).when(userCollectionRequestBuilderMock)
-                                       .buildRequest(anyList());
-    doReturn(userCollectionRequestMock).when(userCollectionRequestMock).select(anyString());
-    doReturn(userCollectionRequestMock).when(userCollectionRequestMock).count(true);
+    GetRequestConfiguration configuration = verifyGetAndReturnConfiguration();
 
-    doReturn("requestId").when(graphApiBatchRequestService).addBatchRequest(any(), any());
+    assertThat(configuration.queryParameters).isNotNull();
+    assertThat(configuration.queryParameters.filter).isEqualTo("onPremisesSamAccountName in ('U236171', 'e502999')");
+    assertThat(configuration.queryParameters.count).isEqualTo(true);
 
-    User user = new User();
-    user.onPremisesSamAccountName = "u236171";
-    user.accountEnabled = true;
-
-    UserCollectionResponseMockClass userCollectionPageMockClass = new UserCollectionResponseMockClass(
-        List.of(user));
-
-    doReturn(userCollectionPageMockClass).when(graphApiBatchRequestService)
-                                         .getDeserializedBody(any(), any(), anyString());
-
-    List<UserModel> userResult = graphApiService.resolveUsers(userIds);
-
-    assertThat(userResult).hasSize(18);
-    assertThat(userResult.get(0).getUserId()).isEqualTo("u236171");
-    assertThat(userResult.get(0).getAccountStatus()).isEqualTo(UserAccountStatus.ACTIVE);
-
-    doReturn(null).when(graphApiBatchRequestService).getDeserializedBody(any(), any(), anyString());
-
-    userResult = graphApiService.resolveUsers(userIds);
-
-    assertThat(userResult).hasSize(18);
-    assertThat(userResult.get(0).getUserId()).isEqualTo("u12340");
-    assertThat(userResult.get(0).getAccountStatus()).isEqualTo(UserAccountStatus.DELETED);
+    assertThat(configuration.headers).isNotNull();
+    assertThat(configuration.headers).hasSize(1);
+    assertThat(configuration.headers).containsKey("ConsistencyLevel");
   }
 
-  static class UserCollectionResponseMockClass extends UserCollectionResponse {
-     UserCollectionResponseMockClass(List<User> value){
-      super.value = value;
-    }
-  }
+  private GetRequestConfiguration verifyGetAndReturnConfiguration() {
+    verify(graphClient).users();
+    verify(usersRequestBuilder).get(getRequestConfigCaptor.capture());
 
+    GetRequestConfiguration getRequestConfiguration = usersRequestBuilder.new GetRequestConfiguration();
+    Consumer<GetRequestConfiguration> requestConfig = getRequestConfigCaptor.getValue();
+    requestConfig.accept(getRequestConfiguration);
+
+    return getRequestConfiguration;
+  }
 }
