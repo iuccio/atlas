4324c4b323bfa0e2de85bf2b6a13727a35daa161
atlas <TechSupport-ATLAS@sbb.ch>
2024-03-18 11:41:11 +0100

    ATLAS-1745: migrate to new graph api

--- user-administration/src/main/java/ch/sbb/atlas/user/administration/config/GraphClientConfig.java
+++ user-administration/src/main/java/ch/sbb/atlas/user/administration/config/GraphClientConfig.java
@@ -2,11 +2,8 @@ package ch.sbb.atlas.user.administration.config;
 
 import com.azure.identity.ClientSecretCredential;
 import com.azure.identity.ClientSecretCredentialBuilder;
-import com.microsoft.graph.authentication.TokenCredentialAuthProvider;
-import com.microsoft.graph.requests.GraphServiceClient;
-import java.util.List;
+import com.microsoft.graph.serviceclient.GraphServiceClient;
 import lombok.RequiredArgsConstructor;
-import okhttp3.Request;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,18 +16,13 @@ public class GraphClientConfig {
   private static final String SCOPE_GRAPH_API = "https://graph.microsoft.com/.default";
 
   @Bean
-  public GraphServiceClient<Request> initializeGraphServiceClient() {
+  public GraphServiceClient initializeGraphServiceClient() {
     final ClientSecretCredential clientSecretCredential = new ClientSecretCredentialBuilder()
         .clientId(azureConfig.getAppRegistrationId())
         .clientSecret(azureConfig.getAzureAdSecret())
         .tenantId(azureConfig.getTenantId())
         .build();
-    final TokenCredentialAuthProvider tokenCredentialAuthProvider = new TokenCredentialAuthProvider(
-        List.of(SCOPE_GRAPH_API), clientSecretCredential);
-    return GraphServiceClient
-        .builder()
-        .authenticationProvider(tokenCredentialAuthProvider)
-        .buildClient();
+    return new GraphServiceClient(clientSecretCredential, SCOPE_GRAPH_API);
   }
 
 }
