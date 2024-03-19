4fa7b922f00719f409e2881443c4e7a9297ae9b6
atlas <19825793+iuccio@users.noreply.github.com>
2024-03-19 16:10:10 +0100

    ATLAS-1848: Initial commit.

---
+++ import-service-point/src/test/java/ch/sbb/importservice/migration/contactpoint/ContactPointMappingEquality.java
@@ -0,0 +1,33 @@
+package ch.sbb.importservice.migration.contactpoint;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import ch.sbb.atlas.api.AtlasApiConstants;
+import ch.sbb.atlas.export.model.prm.ContactPointVersionCsvModel;
+import ch.sbb.atlas.imports.prm.contactpoint.ContactPointCsvModel;
+import ch.sbb.importservice.migration.MigrationUtil;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public record ContactPointMappingEquality(ContactPointCsvModel didokCsvLine, ContactPointVersionCsvModel atlasCsvLine) {
+
+    public void performCheck() {
+        assertThat(atlasCsvLine.getParentNumberServicePoint()).isEqualTo(MigrationUtil.removeCheckDigit(didokCsvLine.getDidokCode()));
+        assertThat(atlasCsvLine.getSloid()).isEqualTo(didokCsvLine.getSloid());
+        if (atlasCsvLine.getParentSloidServicePoint() != null && didokCsvLine.getDsSloid() != null) {
+            assertThat(atlasCsvLine.getParentSloidServicePoint()).isEqualTo(
+                    didokCsvLine.getDsSloid());
+        }
+        if (atlasCsvLine.getDesignation() != null && didokCsvLine.getDescription() != null) {
+            assertThat(atlasCsvLine.getDesignation()).isEqualTo((didokCsvLine.getDescription()));
+        }
+
+        assertThat(localDateFromString(atlasCsvLine.getCreationDate())).isEqualTo(didokCsvLine.getCreatedAt());
+        assertThat(localDateFromString(atlasCsvLine.getEditionDate())).isEqualTo(didokCsvLine.getModifiedAt());
+
+    }
+
+    public LocalDateTime localDateFromString(String string) {
+        return LocalDateTime.parse(string, DateTimeFormatter.ofPattern(AtlasApiConstants.DATE_TIME_FORMAT_PATTERN));
+    }
+}
---
+++ import-service-point/src/test/java/ch/sbb/importservice/migration/contactpoint/ContactPointMigrationActualDateIntegrationTest.java
@@ -0,0 +1,80 @@
+package ch.sbb.importservice.migration.contactpoint;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import ch.sbb.atlas.export.model.prm.ContactPointVersionCsvModel;
+import ch.sbb.atlas.imports.prm.contactpoint.ContactPointCsvModel;
+import ch.sbb.atlas.imports.prm.contactpoint.ContactPointCsvModelContainer;
+import ch.sbb.atlas.imports.util.CsvReader;
+import ch.sbb.atlas.model.DateRange;
+import ch.sbb.atlas.model.controller.IntegrationTest;
+import ch.sbb.importservice.service.csv.ContactPointCsvService;
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Disabled
+@IntegrationTest
+@Slf4j
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ContactPointMigrationActualDateIntegrationTest {
+
+    private static final String DIDOK_CSV_FILE = "PRM_TICKET_COUNTERS_20240319021046.csv";
+    private static final String DIDOK_CSV_FILE_INFO_DESK = "PRM_INFO_DESKS_20240319021037.csv";
+    private static final String ATLAS_CSV_FILE = "actual-date-contact_point-2024-03-19.csv";
+    private static final LocalDate ACTUAL_DATE = LocalDate.of(2024, 3, 19);
+
+    private static final List<ContactPointCsvModel> didokCsvLines = new ArrayList<>();
+    private static final List<ContactPointVersionCsvModel> atlasCsvLines = new ArrayList<>();
+
+    private final ContactPointCsvService contactPointCsvService;
+
+    @Autowired
+    public ContactPointMigrationActualDateIntegrationTest(ContactPointCsvService contactPointCsvService) {
+        this.contactPointCsvService = contactPointCsvService;
+    }
+
+    @Test
+    @Order(1)
+    void shouldParseCsvCorrectly() throws IOException {
+        try(InputStream csvStream = this.getClass().getResourceAsStream(CsvReader.BASE_PATH + DIDOK_CSV_FILE)) {
+            List<ContactPointCsvModelContainer> contactPointCsvModelContainers =
+                contactPointCsvService.mapToContactPointCsvModelContainers(
+                    CsvReader.parseCsv(csvStream, ContactPointCsvModel.class));
+                    didokCsvLines.addAll(contactPointCsvModelContainers.stream()
+                            .map(ContactPointCsvModelContainer::getCsvModels)
+                            .flatMap(Collection::stream)
+                            .toList());
+        }
+        assertThat(didokCsvLines).isNotEmpty();
+
+        try(InputStream csvStream = this.getClass().getResourceAsStream(CsvReader.BASE_PATH + ATLAS_CSV_FILE)) {
+            atlasCsvLines.addAll(CsvReader.parseCsv(csvStream, ContactPointVersionCsvModel.class));
+        }
+        assertThat(atlasCsvLines).isNotEmpty();
+    }
+
+    @Test
+    @Order(2)
+    void shouldHaveOnlyVersionsOnActualDate() {
+        atlasCsvLines.forEach(atlasCsvLine ->
+                assertThat(
+                DateRange.builder()
+                        .from(CsvReader.dateFromString(atlasCsvLine.getValidFrom()))
+                        .to(CsvReader.dateFromString(atlasCsvLine.getValidTo()))
+                        .build()
+                        .contains(ACTUAL_DATE)
+        ).isTrue());
+    }
+
+}
