4cec799bc04175c023c5e5dfac48391fd2d91447
atlas <TechSupport-ATLAS@sbb.ch>
2024-03-19 11:14:47 +0100

    ATLAS-1709: country not given

--- base-atlas/src/test/java/ch/sbb/atlas/api/servicepoint/CreateServicePointVersionModelTest.java
+++ base-atlas/src/test/java/ch/sbb/atlas/api/servicepoint/CreateServicePointVersionModelTest.java
@@ -2,17 +2,14 @@ package ch.sbb.atlas.api.servicepoint;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import ch.sbb.atlas.model.BaseValidatorTest;
 import ch.sbb.atlas.servicepoint.Country;
 import jakarta.validation.ConstraintViolation;
-import jakarta.validation.Validation;
-import jakarta.validation.Validator;
 import java.time.LocalDate;
 import java.util.Set;
 import org.junit.jupiter.api.Test;
 
-class CreateServicePointVersionModelTest {
-
-  private final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+class CreateServicePointVersionModelTest extends BaseValidatorTest {
 
   @Test
   void shouldNotBeValidWhenNumberShortGivenButCountryInAutomaticServicePointId() {
@@ -70,6 +67,24 @@ class CreateServicePointVersionModelTest {
 
     Set<ConstraintViolation<CreateServicePointVersionModel>> constraintViolations = validator.validate(servicePointVersionModel);
     assertThat(constraintViolations).isEmpty();
+  }
+
+  @Test
+  void shouldBeInValidWhenCountryAndNumberShortNull() {
+    CreateServicePointVersionModel servicePointVersionModel = CreateServicePointVersionModel.builder()
+        .numberShort(null)
+        .country(null)
+        .designationOfficial("Bern")
+        .businessOrganisation("ch:1:sboid:5846489645")
+        .validFrom(LocalDate.of(2022, 1, 1))
+        .validTo(LocalDate.of(2022, 12, 31))
+        .build();
+
+    Set<ConstraintViolation<CreateServicePointVersionModel>> constraintViolations = validator.validate(servicePointVersionModel);
+    assertThat(constraintViolations).hasSize(1);
+    ConstraintViolation<CreateServicePointVersionModel> constraintViolation = constraintViolations.iterator().next();
+    assertThat(constraintViolation.getPropertyPath().toString()).isEqualTo("country");
+    assertThat(constraintViolation.getMessageTemplate()).isEqualTo("{jakarta.validation.constraints.NotNull.message}");
   }
 
 }
