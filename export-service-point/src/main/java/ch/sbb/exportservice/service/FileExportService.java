10204260dce8e0edf4cab47297037fed2f0f9fa0
atlas <19825793+iuccio@users.noreply.github.com>
2024-03-20 18:06:02 +0100

    ATLAS-1841: pr review

--- export-service-point/src/main/java/ch/sbb/exportservice/service/FileExportService.java
+++ export-service-point/src/main/java/ch/sbb/exportservice/service/FileExportService.java
@@ -3,13 +3,9 @@ package ch.sbb.exportservice.service;
 import ch.sbb.atlas.amazon.service.AmazonBucket;
 import ch.sbb.atlas.amazon.service.AmazonFileStreamingService;
 import ch.sbb.atlas.amazon.service.AmazonService;
-import ch.sbb.atlas.amazon.service.FileService;
 import ch.sbb.atlas.export.enumeration.ExportFileName;
 import ch.sbb.atlas.export.enumeration.ExportTypeBase;
-import ch.sbb.exportservice.model.ExportExtensionFileType;
 import ch.sbb.exportservice.model.ExportFilePath;
-import java.time.Clock;
-import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.InputStreamResource;
@@ -22,29 +18,17 @@ public class FileExportService {
 
   private final AmazonFileStreamingService amazonFileStreamingService;
   private final AmazonService amazonService;
-  private final FileService fileService;
-  private final Clock clock;
 
-  public ExportFilePath createExportFilePath(ExportTypeBase exportTypeBase, ExportFileName exportFileName,
-      ExportExtensionFileType exportExtensionFileType) {
-    return ExportFilePath.builder()
-        .baseDir(exportFileName.getBaseDir())
-        .dir(exportTypeBase.getDir())
-        .prefix(exportTypeBase.getFileTypePrefix())
-        .fileName(exportFileName.getFileName())
-        .extension(exportExtensionFileType == null ? null : exportExtensionFileType.getExtension())
-        .systemDir(fileService.getDir())
-        .actualDate(LocalDate.now(clock))
-        .build();
+  public InputStreamResource streamJsonFile(ExportTypeBase exportTypeBase, ExportFileName exportFileName) {
+    ExportFilePath exportFilePath = new ExportFilePath(exportTypeBase, exportFileName);
+    logStreamingStart(exportFilePath.getFileToStream());
+    return amazonFileStreamingService.streamFileAndDecompress(AmazonBucket.EXPORT, exportFilePath.getFileToStream());
   }
 
-  public ExportFilePath createExportFilePath(ExportTypeBase exportTypeBase, ExportFileName exportFileName) {
-    return createExportFilePath(exportTypeBase, exportFileName, null);
-  }
-
-  public InputStreamResource streamJsonFile(String fileName) {
-    logStreamingStart(fileName);
-    return amazonFileStreamingService.streamFileAndDecompress(AmazonBucket.EXPORT, fileName);
+  public InputStreamResource streamLatestJsonFile(ExportTypeBase exportTypeBase, ExportFileName exportFileName) {
+    String latestUploadedFileName = getLatestUploadedFileName(exportTypeBase, exportFileName);
+    logStreamingStart(latestUploadedFileName);
+    return amazonFileStreamingService.streamFileAndDecompress(AmazonBucket.EXPORT, latestUploadedFileName);
   }
 
   public InputStreamResource streamGzipFile(String fileName) {
@@ -52,7 +36,8 @@ public class FileExportService {
     return amazonFileStreamingService.streamFile(AmazonBucket.EXPORT, fileName);
   }
 
-  public String getLatestUploadedFileName(ExportFilePath exportFilePath) {
+  public String getLatestUploadedFileName(ExportTypeBase exportTypeBase, ExportFileName exportFileName) {
+    ExportFilePath exportFilePath = new ExportFilePath(exportTypeBase, exportFileName);
     return amazonService.getLatestJsonUploadedObject(AmazonBucket.EXPORT, exportFilePath.s3BucketDirPath(),
         exportFilePath.getPrefix());
   }
