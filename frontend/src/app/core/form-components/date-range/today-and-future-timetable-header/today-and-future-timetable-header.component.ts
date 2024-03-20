4b96871c0a52f9f749456597c0c43db932ca7b5e
atlas <19825793+iuccio@users.noreply.github.com>
2024-03-20 17:04:14 +0100

    ATLAS-1870: Fix DateRange component issues

---
+++ frontend/src/app/core/form-components/date-range/today-and-future-timetable-header/today-and-future-timetable-header.component.ts
@@ -0,0 +1,32 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatDatepicker} from "@angular/material/datepicker";
+import moment, {Moment} from "moment/moment";
+import {TimetableYearChangeService} from "../../../../api";
+
+@Component({
+  selector: 'today-and-future-timetable-header',
+  templateUrl: 'today-and-future-timetable-header.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TodayAndFutureTimetableHeaderComponent {
+
+  constructor(private timetableYearChangeService: TimetableYearChangeService,
+              private datepicker: MatDatepicker<Moment>) {
+  }
+
+  selectToday() {
+    this.selectPredefinedDate(moment());
+  }
+
+  selectFutureTimetable() {
+    this.timetableYearChangeService.getNextTimetablesYearChange(1).subscribe(dates => {
+      this.selectPredefinedDate(moment(dates[0]));
+    })
+  }
+
+  selectPredefinedDate(date: Moment) {
+    this.datepicker.select(date.startOf('day'));
+    this.datepicker.close();
+  }
+
+}
