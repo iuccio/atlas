86f8a6e00186387a04c50711cb83838a563cd201
atlas <19825793+iuccio@users.noreply.github.com>
2024-03-21 10:24:28 +0100

    ATLAS-1872: Deny updates on revoked reference point

---
+++ prm-directory/src/main/java/ch/sbb/prm/directory/exception/ObjectRevokedException.java
@@ -0,0 +1,24 @@
+package ch.sbb.prm.directory.exception;
+
+import ch.sbb.atlas.api.model.ErrorResponse;
+import ch.sbb.atlas.model.exception.AtlasException;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+@Getter
+public class ObjectRevokedException extends AtlasException {
+
+  private final Class<?> clazz;
+  private final String sloid;
+
+  @Override
+  public ErrorResponse getErrorResponse() {
+    return ErrorResponse.builder()
+        .status(HttpStatus.PRECONDITION_FAILED.value())
+        .message("The " + getClazz().getSimpleName() + " with sloid " + getSloid() + " is revoked. Updates are not allowed.")
+        .build();
+  }
+
+}
