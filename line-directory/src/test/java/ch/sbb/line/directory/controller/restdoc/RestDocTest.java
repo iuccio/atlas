32d6f7ef25fbfd537c1c9b5437a7215d6a6f180a
atlas <TechSupport-ATLAS@sbb.ch>
2024-03-22 14:45:55 +0100

    ATLAS-1190: initial draft for custom doc

--- line-directory/src/test/java/ch/sbb/line/directory/controller/restdoc/RestDocTest.java
+++ line-directory/src/test/java/ch/sbb/line/directory/controller/restdoc/RestDocTest.java
@@ -1,16 +1,27 @@
-package ch.sbb.atlas.model.controller;
+package ch.sbb.line.directory.controller.restdoc;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import ch.sbb.atlas.api.lidi.LineVersionModel;
+import ch.sbb.atlas.business.organisation.service.SharedBusinessOrganisationService;
+import ch.sbb.atlas.model.controller.IntegrationTest;
+import ch.sbb.line.directory.LineTestData;
+import ch.sbb.line.directory.controller.LineController;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.nio.charset.StandardCharsets;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
@@ -26,7 +37,7 @@ import org.springframework.web.context.WebApplicationContext;
 @IntegrationTest
 @ExtendWith({RestDocumentationExtension.class})
 @AutoConfigureMockMvc(addFilters = false)
-public abstract class BaseControllerApiTest {
+public class RestDocTest {
 
   @Autowired
   protected MockMvc mvc;
@@ -36,6 +47,12 @@ public abstract class BaseControllerApiTest {
 
   @Autowired
   private WebApplicationContext context;
+
+  @MockBean
+  private SharedBusinessOrganisationService sharedBusinessOrganisationService;
+
+  @Autowired
+  private LineController lineController;
 
   protected final MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
       MediaType.APPLICATION_JSON.getSubtype(),
@@ -56,8 +73,9 @@ public abstract class BaseControllerApiTest {
             .and().snippets()
             .withDefaults(CliDocumentation.curlRequest(),
                 HttpDocumentation.httpRequest(),
-                new MethodAndPathSnippet(),
-                HttpDocumentation.httpResponse()))
+                HttpDocumentation.httpResponse(),
+            new MethodAndPathSnippet(),
+                new AtlasAutoDocSnippet()))
         .build();
   }
 
@@ -65,6 +83,22 @@ public abstract class BaseControllerApiTest {
     return MockMvcRestDocumentation.document("{class-name}/{method-name}",
         preprocessRequest(prettyPrint()),
         preprocessResponse(prettyPrint()));
+  }
+
+  @Test
+  void shouldGetLineOverview() throws Exception {
+    //given
+    LineVersionModel lineVersionModel = LineTestData.lineVersionModelBuilder().build();
+    lineController.createLineVersion(lineVersionModel);
+
+    //when
+    mvc.perform(get("/v1/lines")
+            .queryParam("page", "0")
+            .queryParam("size", "5")
+            .queryParam("sort", "swissLineNumber,asc"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.totalCount").value(1))
+        .andExpect(jsonPath("$.objects", hasSize(1)));
   }
 
 }
