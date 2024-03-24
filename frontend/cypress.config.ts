5a39a02208175266aba445b8240688f229f2bee6
atlas <19825793+iuccio@users.noreply.github.com>
2024-03-24 12:17:43 +0100

    ATLAS-1541: add cypress-mochawesome-reporter

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -3,8 +3,8 @@ import cypress_failed_log from 'cypress-failed-log/src/failed';
 import cypress_high_resolution from 'cypress-high-resolution';
 
 export default defineConfig({
-  videosFolder: 'cypress/videos',
-  screenshotsFolder: 'cypress/screenshots',
+  videosFolder: 'cypress/test-results/videos',
+  screenshotsFolder: 'cypress/test-results/screenshots',
   fixturesFolder: 'cypress/fixtures',
   viewportWidth: 1920,
   viewportHeight: 1080,
@@ -15,17 +15,31 @@ export default defineConfig({
   requestTimeout: 60000,
   responseTimeout: 60000,
   video: true,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir:'cypress/test-results/reports',
+    charts: true,
+    reportPageTitle: 'Atlas E2E Tests',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    debug: true,
+  },
+
   env: {
     resolution: 'high'
   },
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
+    async setupNodeEvents(on, config) {
       on('task', {
         failed: cypress_failed_log()
       });
       cypress_high_resolution(on, config);
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
+
     },
     baseUrl: 'http://localhost:4200'
   }
