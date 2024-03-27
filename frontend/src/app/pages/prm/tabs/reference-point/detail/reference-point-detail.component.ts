6b2a1662acbd0567b57afa4d4735180157e34188
atlas <TechSupport-ATLAS@sbb.ch>
2024-03-27 15:53:40 +0100

    ATLAS-1748 lint fixes

--- frontend/src/app/pages/prm/tabs/reference-point/detail/reference-point-detail.component.ts
+++ frontend/src/app/pages/prm/tabs/reference-point/detail/reference-point-detail.component.ts
@@ -10,7 +10,7 @@ import {
   PersonWithReducedMobilityService,
   ReadReferencePointVersion,
   ReadServicePointVersion,
-  ReferencePointVersion, StopPointVersion,
+  ReferencePointVersion,
 } from '../../../../../api';
 import {DetailHelperService, DetailWithCancelEdit} from "../../../../../core/detail/detail-helper.service";
 import {take} from "rxjs";
