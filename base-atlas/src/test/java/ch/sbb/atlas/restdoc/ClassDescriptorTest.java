f5b2d85b80ac831ae41e46973a3f6dadcb192cc3
atlas <19825793+iuccio@users.noreply.github.com>
2024-03-27 10:57:37 +0100

    ATLAS-1190: cleanup

--- base-atlas/src/test/java/ch/sbb/atlas/restdoc/ClassDescriptorTest.java
+++ base-atlas/src/test/java/ch/sbb/atlas/restdoc/ClassDescriptorTest.java
@@ -1,5 +1,7 @@
 package ch.sbb.atlas.restdoc;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import ch.sbb.atlas.api.lidi.LineVersionModel;
 import ch.sbb.atlas.api.servicepoint.CreateServicePointVersionModel;
 import org.junit.jupiter.api.Test;
@@ -7,17 +9,14 @@ import org.junit.jupiter.api.Test;
 class ClassDescriptorTest {
 
   @Test
-  void shouldDescibeType() {
+  void shouldDescribeLineVersionModel() {
     FieldDescriptors fieldDescriptors = new FieldDescriptors(LineVersionModel.class);
-    System.out.println(fieldDescriptors);
-
+    assertThat(fieldDescriptors.getFields()).hasSize(29);
   }
 
   @Test
-  void shouldDescibeservicepointType() {
-    // TODO: super/nested classes
+  void shouldDescribeCreateServicePointVersionModel() {
     FieldDescriptors fieldDescriptors = new FieldDescriptors(CreateServicePointVersionModel.class);
-    System.out.println(fieldDescriptors);
-
+    assertThat(fieldDescriptors.getFields()).hasSize(30);
   }
 }
