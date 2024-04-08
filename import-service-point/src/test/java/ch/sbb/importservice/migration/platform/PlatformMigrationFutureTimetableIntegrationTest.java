b223d954cacd69414756c2f46c457ae5399fab94
atlas <19825793+iuccio@users.noreply.github.com>
2024-04-08 10:58:14 +0200

    Platform Tests

--- import-service-point/src/test/java/ch/sbb/importservice/migration/platform/PlatformMigrationFutureTimetableIntegrationTest.java
+++ import-service-point/src/test/java/ch/sbb/importservice/migration/platform/PlatformMigrationFutureTimetableIntegrationTest.java
@@ -16,21 +16,19 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 
-@Disabled
 @IntegrationTest
 @Slf4j
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class PlatformMigrationFutureTimetableIntegrationTest {
 
   private static final String DIDOK_STOP_PLACE_CSV_FILE = "PRM_PLATFORMS_20240408013251.csv";
-  private static final String ATLAS_STOP_POINT_CSV_FILE = "future-timetable-platform-2024-03-12.csv";
+  private static final String ATLAS_STOP_POINT_CSV_FILE = "future-timetable-platform-2024-04-08.csv";
   private static final LocalDate FUTURE_TIMETABLE_DATE = LocalDate.of(2024, 12, 15);
 
   private static final List<PlatformCsvModel> didokPlatformCsvLines = new ArrayList<>();
