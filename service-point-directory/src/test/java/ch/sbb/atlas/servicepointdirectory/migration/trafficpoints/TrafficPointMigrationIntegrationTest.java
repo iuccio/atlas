c6b54ae1f4254d0ca74eee79f669ceed464ef05a
atlas <19825793+iuccio@users.noreply.github.com>
2024-04-08 09:45:28 +0200

    Add TraPo and LoPo

--- service-point-directory/src/test/java/ch/sbb/atlas/servicepointdirectory/migration/trafficpoints/TrafficPointMigrationIntegrationTest.java
+++ service-point-directory/src/test/java/ch/sbb/atlas/servicepointdirectory/migration/trafficpoints/TrafficPointMigrationIntegrationTest.java
@@ -28,7 +28,7 @@ import org.junit.jupiter.api.TestMethodOrder;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
  class TrafficPointMigrationIntegrationTest {
 
-  private static final String DIDOK_CSV_FILE = "DIDOK3_VERKEHRSPUNKTELEMENTE_ALL_V_1_20240312011007.csv";
+  private static final String DIDOK_CSV_FILE = "DIDOK3_VERKEHRSPUNKTELEMENTE_ALL_V_1_20240408011006.csv";
   private static final String ATLAS_CSV_FILE = "full-world-traffic_point-2024-03-12.csv";
 
   private static final List<TrafficPointAtlasCsvModel> trafficPointElementCsvModels = new ArrayList<>();
