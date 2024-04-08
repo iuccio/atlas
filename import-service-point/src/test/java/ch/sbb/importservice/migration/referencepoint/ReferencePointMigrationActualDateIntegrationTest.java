4d910519c9b52297ca7db227996402ccf6f1b07a
atlas <19825793+iuccio@users.noreply.github.com>
2024-04-08 10:44:32 +0200

    Reference Point Tests

--- import-service-point/src/test/java/ch/sbb/importservice/migration/referencepoint/ReferencePointMigrationActualDateIntegrationTest.java
+++ import-service-point/src/test/java/ch/sbb/importservice/migration/referencepoint/ReferencePointMigrationActualDateIntegrationTest.java
@@ -16,22 +16,20 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 
-@Disabled
 @IntegrationTest
 @Slf4j
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ReferencePointMigrationActualDateIntegrationTest {
 
     private static final String DIDOK_REFERENCE_POINT_CSV_FILE = "PRM_REFERENCE_POINTS_20240408013239.csv";
-    private static final String ATLAS_REFERENCE_POINT_CSV_FILE = "actual-date-reference_point-2024-03-12.csv";
-    static final LocalDate ACTUAL_DATE = LocalDate.of(2024, 3, 12);
+    private static final String ATLAS_REFERENCE_POINT_CSV_FILE = "actual-date-reference_point-2024-04-08.csv";
+    static final LocalDate ACTUAL_DATE = LocalDate.of(2024, 4, 8);
 
     private static final List<ReferencePointCsvModel> didokReferencePointCsvLines = new ArrayList<>();
     private static final List<ReferencePointVersionCsvModel> atlasReferencePointCsvLines = new ArrayList<>();
