8ac0e42f7985d9e992dc1a78561a00422e569354
atlas <TechSupport-ATLAS@sbb.ch>
2024-04-17 18:31:15 +0200

    ATLAS-1669: Add v2 implementation.

--- line-directory/src/main/java/ch/sbb/line/directory/controller/TimetableHearingStatementControllerV2.java
+++ line-directory/src/main/java/ch/sbb/line/directory/controller/TimetableHearingStatementControllerV2.java
@@ -2,6 +2,11 @@ package ch.sbb.line.directory.controller;
 
 import ch.sbb.atlas.api.timetable.hearing.TimetableHearingStatementApiV2;
 import ch.sbb.atlas.api.timetable.hearing.TimetableHearingStatementModel;
+import ch.sbb.line.directory.entity.TimetableHearingYear;
+import ch.sbb.line.directory.entity.TimetableHearingYear_;
+import ch.sbb.line.directory.exception.ForbiddenDueToHearingYearSettingsException;
+import ch.sbb.line.directory.service.hearing.TimetableHearingStatementService;
+import ch.sbb.line.directory.service.hearing.TimetableHearingYearService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,10 +17,19 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequiredArgsConstructor
 public class TimetableHearingStatementControllerV2 implements TimetableHearingStatementApiV2 {
+
+  private final TimetableHearingStatementService timetableHearingStatementService;
+  private final TimetableHearingYearService timetableHearingYearService;
+
   @Override
   public TimetableHearingStatementModel createStatementV2(TimetableHearingStatementModel statement,
       List<MultipartFile> documents) {
-    return null;
+    TimetableHearingYear hearingYear = timetableHearingYearService.getHearingYear(statement.getTimetableYear());
+    if (!hearingYear.isStatementCreatableInternal()) {
+      throw new ForbiddenDueToHearingYearSettingsException(hearingYear.getTimetableYear(),
+          TimetableHearingYear_.STATEMENT_CREATABLE_INTERNAL);
+    }
+    return timetableHearingStatementService.createHearingStatement(statement, documents);
   }
 
 }
