427f0e794b5e5610da5db0e299b0e37c1adc74be
atlas <19825793+iuccio@users.noreply.github.com>
2024-04-17 18:31:15 +0200

    ATLAS-1669: Add v2 implementation.

--- base-atlas/src/main/java/ch/sbb/atlas/api/timetable/hearing/TimetableHearingStatementApiV2.java
+++ base-atlas/src/main/java/ch/sbb/atlas/api/timetable/hearing/TimetableHearingStatementApiV2.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.multipart.MultipartFile;
 
-@Tag(name = "Timetable Hearing Statements")
+@Tag(name = "Timetable Hearing Statements v2")
 @RequestMapping("v2/timetable-hearing/statements")
 public interface TimetableHearingStatementApiV2 {
 
