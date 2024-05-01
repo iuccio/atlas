f4bb08f28a0f2c09faa8cc9c5128586f3cc9820d
atlas <19825793+iuccio@users.noreply.github.com>
2024-05-01 16:08:17 +0200

    ATLAS-1669: Few improvements in styling.

--- frontend/src/app/core/form-components/email-list/email-list.component.ts
+++ frontend/src/app/core/form-components/email-list/email-list.component.ts
@@ -1,11 +1,25 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, ContentChild, EventEmitter, Input, Output, TemplateRef} from '@angular/core';
+import {FieldExample} from "../text-field/field-example";
+import {AtlasFieldCustomError} from "../atlas-field-error/atlas-field-custom-error";
 
 @Component({
-  selector: 'app-email-list',
+  selector: 'atlas-email-list',
   templateUrl: './email-list.component.html',
   styleUrls: ['./email-list.component.scss']
 })
 export class EmailListComponent {
+  @Input() fieldLabel!: string;
+  @Input() infoIconTitle!: string;
+  @Input() infoIconLink!: string;
+  @Input() required!: boolean;
+  @Input() fieldExamples!: Array<FieldExample>;
+  @Input() customError!: AtlasFieldCustomError;
+  @ContentChild('customChildInputPostfixTemplate')
+    // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+  customChildInputPostfixTemplate!: TemplateRef<any>;
+  @ContentChild('customChildInputPrefixTemplate')
+    // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+  customChildInputPrefixTemplate!: TemplateRef<any>;
   @Input() existingEmails: string[] = [];
   email: string = '';
   emailList: string[] = [];
