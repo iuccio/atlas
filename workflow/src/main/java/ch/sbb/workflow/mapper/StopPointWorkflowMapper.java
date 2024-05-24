43e6d781b3d5533e74e9535db7dc9318cbfdca8a
atlas <19825793+iuccio@users.noreply.github.com>
2024-05-24 14:58:21 +0200

    ATLAS-1716: add specialistOffice examinants

--- workflow/src/main/java/ch/sbb/workflow/mapper/StopPointWorkflowMapper.java
+++ workflow/src/main/java/ch/sbb/workflow/mapper/StopPointWorkflowMapper.java
@@ -1,17 +1,20 @@
 package ch.sbb.workflow.mapper;
 
-import ch.sbb.atlas.api.workflow.StopPointWorkflowStartModel;
+import ch.sbb.atlas.api.workflow.ClientPersonModel;
+import ch.sbb.atlas.api.workflow.StopPointAddWorkflowModel;
 import ch.sbb.workflow.entity.StopPointWorkflow;
+import java.util.List;
 import java.util.stream.Collectors;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class StopPointWorkflowMapper {
 
-  public static StopPointWorkflowStartModel toModel(StopPointWorkflow entity) {
-    return StopPointWorkflowStartModel.builder()
+  public static StopPointAddWorkflowModel toModel(StopPointWorkflow entity) {
+    return StopPointAddWorkflowModel.builder()
         .sloid(entity.getSloid())
         .examinants(entity.getExaminants().stream().map(ClientPersonMapper::toModel).toList())
+        .ccEmails(entity.getCcEmails())
         .sboid(entity.getSboid())
         .designationOfficial(entity.getDesignationOfficial())
         .swissMunicipalityName(entity.getSwissMunicipalityName())
@@ -20,7 +23,7 @@ public class StopPointWorkflowMapper {
   }
 
   //startStopPointModel
-  public static StopPointWorkflow toEntity(StopPointWorkflowStartModel model) {
+  public static StopPointWorkflow toEntity(StopPointAddWorkflowModel model, List<ClientPersonModel> examinants) {
     StopPointWorkflow stopPointWorkflow = StopPointWorkflow.builder()
         .sloid(model.getSloid())
         .versionId(model.getVersionId())
@@ -29,6 +32,25 @@ public class StopPointWorkflowMapper {
         .designationOfficial(model.getDesignationOfficial())
         .startDate(model.getValidFrom())
         .endDate(model.getValidTo())
+        .ccEmails(model.getCcEmails())
+        .workflowComment(model.getWorkflowComment())
+        .build();
+    model.getExaminants().addAll(examinants);
+    stopPointWorkflow.setExaminants(
+        model.getExaminants().stream().map(clientPersonModel -> ClientPersonMapper.toEntity(clientPersonModel, stopPointWorkflow))
+            .collect(Collectors.toSet()));
+    return stopPointWorkflow;
+  }
+  public static StopPointWorkflow toEntity(StopPointAddWorkflowModel model) {
+    StopPointWorkflow stopPointWorkflow = StopPointWorkflow.builder()
+        .sloid(model.getSloid())
+        .versionId(model.getVersionId())
+        .sboid(model.getSboid())
+        .swissMunicipalityName(model.getSwissMunicipalityName())
+        .designationOfficial(model.getDesignationOfficial())
+        .startDate(model.getValidFrom())
+        .endDate(model.getValidTo())
+        .ccEmails(model.getCcEmails())
         .workflowComment(model.getWorkflowComment())
         .build();
     stopPointWorkflow.setExaminants(
