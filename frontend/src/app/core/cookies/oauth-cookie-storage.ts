e92f72859918003ba0f0a2777bc03429c0d18902
atlas <TechSupport-ATLAS@sbb.ch>
2024-05-27 14:33:21 +0200

    ATLAS-1847: use cookies for auth token storage

---
+++ frontend/src/app/core/cookies/oauth-cookie-storage.ts
@@ -0,0 +1,28 @@
+import {Injectable} from "@angular/core";
+import {OAuthStorage} from "angular-oauth2-oidc";
+import {CookieService} from "ngx-cookie-service";
+
+// See https://confluence.sbb.ch/x/MpfzpQ
+@Injectable()
+export class OAuthCookieStorage extends OAuthStorage {
+
+  constructor(private cookieService: CookieService) {
+    super();
+  }
+
+  getItem(key: string): string | null {
+    return this.cookieService.get(key);
+  }
+
+  removeItem(key: string): void {
+    this.cookieService.delete(key);
+  }
+
+  setItem(key: string, data: string): void {
+    this.cookieService.set(key, data, {
+      sameSite: 'Strict',
+      secure: true
+    });
+  }
+
+}
