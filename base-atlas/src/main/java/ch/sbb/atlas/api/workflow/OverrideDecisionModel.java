55db19fbc5094a85a7c6f4b5cc6339f72ba9d17a
atlas <19825793+iuccio@users.noreply.github.com>
2024-05-28 08:08:40 +0200

    ATLAS-1716: add override vote

---
+++ base-atlas/src/main/java/ch/sbb/atlas/api/workflow/OverrideDecisionModel.java
@@ -0,0 +1,34 @@
+package ch.sbb.atlas.api.workflow;
+
+import ch.sbb.atlas.api.AtlasCharacterSetsRegex;
+import ch.sbb.atlas.api.AtlasFieldLengths;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@SuperBuilder
+@Schema(name = "Decision")
+public class OverrideDecisionModel {
+
+  @Schema(description = "Override Examinant")
+  @NotNull
+  private ClientPersonModel overrideExaminant;
+
+  @Schema(description = "BAV Judgement", example = "true")
+  @NotNull
+  private Boolean fotJudgement;
+
+  @Schema(description = "Motivation", example = "I'm agree")
+  @Size(min = 1, max = AtlasFieldLengths.LENGTH_1500)
+  @Pattern(regexp = AtlasCharacterSetsRegex.ISO_8859_1)
+  private String fotMotivation;
+
+}
