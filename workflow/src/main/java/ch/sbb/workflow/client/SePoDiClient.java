c31abdb71187d7854284a20c3f45867ec9f42789
atlas <19825793+iuccio@users.noreply.github.com>
2024-06-03 13:33:47 +0200

    ATLAS-1585: improve addWorkflow

--- workflow/src/main/java/ch/sbb/workflow/client/SePoDiClient.java
+++ workflow/src/main/java/ch/sbb/workflow/client/SePoDiClient.java
@@ -4,7 +4,6 @@ import ch.sbb.atlas.api.client.TokenPassingFeignClientConfig;
 import ch.sbb.atlas.api.servicepoint.UpdateServicePointVersionModel;
 import ch.sbb.atlas.model.Status;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +14,7 @@ public interface SePoDiClient {
   String BASEPATH = "/service-point-directory/v1/service-points/";
 
   @PutMapping(value = BASEPATH + "status/{id}")
-  ResponseEntity<UpdateServicePointVersionModel> postServicePointsStatusUpdate(@PathVariable("id") Long id,
+  UpdateServicePointVersionModel postServicePointsStatusUpdate(@PathVariable("id") Long id,
       @RequestBody Status status);
 
 }
