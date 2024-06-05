89684b318f0d8f05a10133f60591e62d9c41e476
atlas <TechSupport-ATLAS@sbb.ch>
2024-06-05 15:40:37 +0200

    ATLAS-1582: Overview and detail routing setup

--- frontend/src/app/pages/sepodi/workflow/detail-page/stop-point-workflow-detail-resolver.service.ts
+++ frontend/src/app/pages/sepodi/workflow/detail-page/stop-point-workflow-detail-resolver.service.ts
@@ -1,32 +1,49 @@
 import {inject, Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, ResolveFn, Router} from '@angular/router';
-import {catchError, Observable, of} from 'rxjs';
-import {ReadStopPointWorkflow, StopPointWorkflowService} from "../../../../api";
+import {catchError, mergeMap, Observable, of} from 'rxjs';
+import {ReadServicePointVersion, ReadStopPointWorkflow, ServicePointsService, StopPointWorkflowService} from "../../../../api";
 import {Pages} from "../../../pages";
+import {map} from "rxjs/operators";
+
+export interface StopPointWorkflowDetailData {
+  workflow: ReadStopPointWorkflow,
+  version: ReadServicePointVersion
+}
 
 @Injectable({providedIn: 'root'})
 export class StopPointWorkflowDetailResolver {
 
   constructor(
     private readonly workflowService: StopPointWorkflowService,
+    private readonly servicePointService: ServicePointsService,
     private readonly router: Router,
   ) {
   }
 
-  resolve(route: ActivatedRouteSnapshot): Observable<ReadStopPointWorkflow | undefined> {
+  resolve(route: ActivatedRouteSnapshot): Observable<StopPointWorkflowDetailData | undefined> {
     const idParameter = parseInt(route.paramMap.get('id') || '0');
-    console.log("resolving")
-    return this.workflowService.getStopPointWorkflow(idParameter).pipe(
-      catchError(() => {
-        this.router.navigate([Pages.SEPODI.path], {
-          state: {notDismissSnackBar: true},
-        }).then()
-        return of(undefined);
-      }),
-    );
+    return this.workflowService.getStopPointWorkflow(idParameter)
+      .pipe(catchError(() => {
+          this.router.navigate([Pages.SEPODI.path], {
+            state: {notDismissSnackBar: true},
+          }).then()
+          return of(undefined);
+        }),
+        mergeMap(workflow => {
+          if (workflow) {
+            return this.servicePointService.getServicePointVersion(workflow.versionId).pipe(map(servicePointVersion => {
+              return {
+                workflow: workflow,
+                version: servicePointVersion
+              };
+            }));
+          }
+          return of();
+        })
+      );
   }
 }
 
-export const stopPointWorkflowDetailResolver: ResolveFn<ReadStopPointWorkflow | undefined> = (
+export const stopPointWorkflowDetailResolver: ResolveFn<StopPointWorkflowDetailData | undefined> = (
   route: ActivatedRouteSnapshot,
 ) => inject(StopPointWorkflowDetailResolver).resolve(route);
