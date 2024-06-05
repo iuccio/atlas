29e8b124952d0548f47c353bd1ff36239468c597
atlas <19825793+iuccio@users.noreply.github.com>
2024-06-05 10:42:41 +0200

    ATLAS-1722: fix person validation

--- base-atlas/src/main/java/ch/sbb/atlas/api/workflow/PersonModel.java
+++ base-atlas/src/main/java/ch/sbb/atlas/api/workflow/PersonModel.java
@@ -4,6 +4,7 @@ import ch.sbb.atlas.api.AtlasCharacterSetsRegex;
 import ch.sbb.atlas.api.AtlasFieldLengths;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.media.Schema.AccessMode;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
@@ -21,16 +22,19 @@ public class PersonModel {
 
   @Schema(description = "Firstname", example = "John")
   @Pattern(regexp = AtlasCharacterSetsRegex.ISO_8859_1)
+  @NotBlank
   @Size(min = 1, max = AtlasFieldLengths.LENGTH_50)
   private String firstName;
 
   @Schema(description = "Second", example = "Doe")
+  @NotBlank
   @Size(min = 1, max = AtlasFieldLengths.LENGTH_50)
   @Pattern(regexp = AtlasCharacterSetsRegex.ISO_8859_1)
   private String lastName;
 
   @Schema(description = "Person Function", example = "Officer")
   @Pattern(regexp = AtlasCharacterSetsRegex.ISO_8859_1)
+  @NotBlank
   @Size(min = 1, max = AtlasFieldLengths.LENGTH_50)
   private String personFunction;
 
