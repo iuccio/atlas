1ff14e1349939cd4287c079611056be843b93f04
atlas <19825793+iuccio@users.noreply.github.com>
2024-06-11 17:53:31 +0200

    ATLAS-1584: show all the necessary info

--- frontend/src/app/core/components/base-detail/user-edit-info/user-detail-info.component.ts
+++ frontend/src/app/core/components/base-detail/user-edit-info/user-detail-info.component.ts
@@ -14,6 +14,7 @@ import { map } from 'rxjs/operators';
 })
 export class UserDetailInfoComponent {
   @Input() short = false;
+  @Input() showEditor = true;
 
   private _record$: Observable<CreationEditionRecord | undefined> = of(undefined);
 
