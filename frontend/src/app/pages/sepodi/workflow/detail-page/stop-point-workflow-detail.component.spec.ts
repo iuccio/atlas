827864d70adbe0cc206ad743be51537c26491ad3
atlas <TechSupport-ATLAS@sbb.ch>
2024-06-12 15:59:19 +0200

    ATLAS-1584: add coverage

--- frontend/src/app/pages/sepodi/workflow/detail-page/stop-point-workflow-detail.component.spec.ts
+++ frontend/src/app/pages/sepodi/workflow/detail-page/stop-point-workflow-detail.component.spec.ts
@@ -4,7 +4,7 @@ import {AppTestingModule} from "../../../../app.testing.module";
 import {FormModule} from "../../../../core/module/form.module";
 import {ActivatedRoute} from "@angular/router";
 import {BERN_WYLEREGG} from "../../../../../test/data/service-point";
-import {ReadStopPointWorkflow} from "../../../../api";
+import {Country, MeanOfTransport, ReadServicePointVersion, ReadStopPointWorkflow, Status} from "../../../../api";
 import {StopPointWorkflowDetailFormComponent} from "../detail-form/stop-point-workflow-detail-form.component";
 import {StringListComponent} from "../../../../core/form-components/string-list/string-list.component";
 import {MockAtlasButtonComponent} from "../../../../app.testing.mocks";
@@ -18,10 +18,10 @@ import {AtlasSpacerComponent} from "../../../../core/components/spacer/atlas-spa
 import {StopPointWorkflowDetailData} from "./stop-point-workflow-detail-resolver.service";
 import {UserDetailInfoComponent} from "../../../../core/components/base-detail/user-edit-info/user-detail-info.component";
 
-const workflow:ReadStopPointWorkflow = {
+const workflow: ReadStopPointWorkflow = {
   versionId: 1000,
 };
-const workflowData: StopPointWorkflowDetailData={
+const workflowData: StopPointWorkflowDetailData = {
   workflow: workflow,
   servicePoint: [BERN_WYLEREGG]
 }
@@ -67,6 +67,70 @@ describe('StopPointWorkflowDetailComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should calculate old designation if version before was validated', () => {
+    const servicePoint: ReadServicePointVersion[] = [
+      {
+        sloid: 'ch:1:sloid:89008',
+        designationOfficial: 'Bern, Wyleregg 1',
+        businessOrganisation: 'ch:1:sboid:100626',
+        meansOfTransport: [MeanOfTransport.Bus],
+        status: Status.Validated,
+        validFrom: new Date('2014-12-14'),
+        validTo: new Date('2021-03-31'),
+        number: {number: 8589008, checkDigit: 7, uicCountryCode: 85, numberShort: 89008},
+        country: Country.Switzerland,
+        stopPoint: true,
+      },
+      {
+        sloid: 'ch:1:sloid:89008',
+        designationOfficial: 'Bern, Wyleregg 2',
+        businessOrganisation: 'ch:1:sboid:100626',
+        meansOfTransport: [MeanOfTransport.Bus],
+        status: Status.Draft,
+        validFrom: new Date('2021-04-01'),
+        validTo: new Date('2021-06-31'),
+        number: {number: 8589008, checkDigit: 7, uicCountryCode: 85, numberShort: 89008},
+        country: Country.Switzerland,
+        stopPoint: true,
+      }
+    ];
+
+    const result = component.getOldDesignation(servicePoint, 1);
+    expect(result).toBe('Bern, Wyleregg 1')
+  });
+
+  it('should calculate old designation if version before was not stoppoint', () => {
+    const servicePoint: ReadServicePointVersion[] = [
+      {
+        sloid: 'ch:1:sloid:89008',
+        designationOfficial: 'Bern, Wyleregg 1',
+        businessOrganisation: 'ch:1:sboid:100626',
+        meansOfTransport: [],
+        status: Status.Validated,
+        validFrom: new Date('2014-12-14'),
+        validTo: new Date('2021-03-31'),
+        number: {number: 8589008, checkDigit: 7, uicCountryCode: 85, numberShort: 89008},
+        country: Country.Switzerland,
+        stopPoint: false,
+      },
+      {
+        sloid: 'ch:1:sloid:89008',
+        designationOfficial: 'Bern, Wyleregg 2',
+        businessOrganisation: 'ch:1:sboid:100626',
+        meansOfTransport: [MeanOfTransport.Bus],
+        status: Status.Draft,
+        validFrom: new Date('2021-04-01'),
+        validTo: new Date('2021-06-31'),
+        number: {number: 8589008, checkDigit: 7, uicCountryCode: 85, numberShort: 89008},
+        country: Country.Switzerland,
+        stopPoint: true,
+      }
+    ];
+
+    const result = component.getOldDesignation(servicePoint, 1);
+    expect(result).toBe('-')
   });
 
 });
