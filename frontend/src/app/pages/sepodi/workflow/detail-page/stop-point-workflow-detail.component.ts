aa891aa1b96ce70074e51b2bad3ac727c27fe644
atlas <19825793+iuccio@users.noreply.github.com>
2024-06-14 13:38:39 +0200

    ATLAS-1722: add frontend for wf start

--- frontend/src/app/pages/sepodi/workflow/detail-page/stop-point-workflow-detail.component.ts
+++ frontend/src/app/pages/sepodi/workflow/detail-page/stop-point-workflow-detail.component.ts
@@ -1,21 +1,32 @@
-import {Component, OnInit} from '@angular/core';
-import {ReadServicePointVersion, ReadStopPointWorkflow, Status} from "../../../../api";
-import {FormGroup} from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import {
+  ReadServicePointVersion,
+  ReadStopPointWorkflow,
+  Status,
+  StopPointWorkflowService,
+  WorkflowStatus,
+} from '../../../../api';
+import { FormGroup } from '@angular/forms';
 import {
   StopPointWorkflowDetailFormGroup,
-  StopPointWorkflowDetailFormGroupBuilder
-} from "../detail-form/stop-point-workflow-detail-form-group";
-import {ActivatedRoute} from "@angular/router";
-import {StopPointWorkflowDetailData} from "./stop-point-workflow-detail-resolver.service";
+  StopPointWorkflowDetailFormGroupBuilder,
+} from '../detail-form/stop-point-workflow-detail-form-group';
+import { ActivatedRoute } from '@angular/router';
+import { StopPointWorkflowDetailData } from './stop-point-workflow-detail-resolver.service';
+import { NotificationService } from '../../../../core/notification/notification.service';
 
 @Component({
   selector: 'stop-point-workflow-detail',
   templateUrl: './stop-point-workflow-detail.component.html',
 })
 export class StopPointWorkflowDetailComponent implements OnInit {
+  protected readonly WorkflowStatus = WorkflowStatus;
 
-  constructor(private route: ActivatedRoute) {
-  }
+  constructor(
+    private route: ActivatedRoute,
+    private readonly stopPointWorkflowService: StopPointWorkflowService,
+    private readonly notificationService: NotificationService,
+  ) {}
 
   form!: FormGroup<StopPointWorkflowDetailFormGroup>;
   stopPoint!: ReadServicePointVersion;
@@ -26,17 +37,35 @@ export class StopPointWorkflowDetailComponent implements OnInit {
     const workflowData: StopPointWorkflowDetailData = this.route.snapshot.data.workflow;
     this.workflow = workflowData.workflow;
 
-    const indexOfVersionInReview = workflowData.servicePoint.findIndex(i => i.id === this.workflow.versionId)!;
+    const indexOfVersionInReview = workflowData.servicePoint.findIndex(
+      (i) => i.id === this.workflow.versionId,
+    )!;
     this.stopPoint = workflowData.servicePoint[indexOfVersionInReview];
-    this.oldDesignation = this.getOldDesignation(workflowData.servicePoint, indexOfVersionInReview)
+    this.oldDesignation = this.getOldDesignation(workflowData.servicePoint, indexOfVersionInReview);
 
     this.form = StopPointWorkflowDetailFormGroupBuilder.buildFormGroup(this.workflow);
     this.form.disable();
   }
 
-  getOldDesignation(servicePoint: ReadServicePointVersion[], indexOfVersionInReview: number): string {
+  getOldDesignation(
+    servicePoint: ReadServicePointVersion[],
+    indexOfVersionInReview: number,
+  ): string {
     const versionsBeforeInReview = servicePoint.slice(0, indexOfVersionInReview);
-    return versionsBeforeInReview.filter(i => i.stopPoint && i.status === Status.Validated).map(i => i.designationOfficial).at(-1) ?? '-';
+    return (
+      versionsBeforeInReview
+        .filter((i) => i.stopPoint && i.status === Status.Validated)
+        .map((i) => i.designationOfficial)
+        .at(-1) ?? '-'
+    );
   }
 
+  startWF() {
+    this.stopPointWorkflowService
+      .startStopPointWorkflow(this.workflow.id!)
+      .subscribe((startedWF) => {
+        this.workflow = startedWF;
+        this.notificationService.success('WORKFLOW.NOTIFICATION.START.SUCCESS');
+      });
+  }
 }
