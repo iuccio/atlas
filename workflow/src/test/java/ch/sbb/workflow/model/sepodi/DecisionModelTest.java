a1dccb54c138779360785174ed9f26ea8e498401
atlas <19825793+iuccio@users.noreply.github.com>
2024-06-26 15:24:51 +0200

    ATLAS-1798: motivation needed if no

---
+++ workflow/src/test/java/ch/sbb/workflow/model/sepodi/DecisionModelTest.java
@@ -0,0 +1,52 @@
+package ch.sbb.workflow.model.sepodi;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import ch.sbb.atlas.model.BaseValidatorTest;
+import ch.sbb.workflow.entity.JudgementType;
+import ch.sbb.workflow.model.sepodi.DecisionModel.DecisionModelBuilder;
+import jakarta.validation.ConstraintViolation;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+
+class DecisionModelTest extends BaseValidatorTest {
+
+  @Test
+  void shouldValidateWithJudgementYes() {
+    DecisionModel decision = getDecisionBuilder()
+        .judgement(JudgementType.YES)
+        .build();
+    Set<ConstraintViolation<DecisionModel>> constraintViolations = validator.validate(decision);
+    assertThat(constraintViolations).isEmpty();
+  }
+
+  @Test
+  void shouldValidateWithJudgementNoAndMotivation() {
+    DecisionModel decision = getDecisionBuilder()
+        .judgement(JudgementType.NO)
+        .motivation("No, please")
+        .build();
+    Set<ConstraintViolation<DecisionModel>> constraintViolations = validator.validate(decision);
+    assertThat(constraintViolations).isEmpty();
+  }
+
+  @Test
+  void shouldFailToValidateWithJudgementNoAndNoMotivation() {
+    DecisionModel decision = getDecisionBuilder()
+        .judgement(JudgementType.NO)
+        .build();
+    Set<ConstraintViolation<DecisionModel>> constraintViolations = validator.validate(decision);
+    assertThat(constraintViolations).hasSize(1);
+  }
+
+  private static DecisionModelBuilder<?, ?> getDecisionBuilder() {
+    return DecisionModel.builder()
+        .firstName("Daniel")
+        .lastName("Berlin")
+        .organisation("Regierung")
+        .personFunction("Cheffe")
+        .examinantMail("daniel@berlin.com")
+        .pinCode("1246");
+  }
+
+}
