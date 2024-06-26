c88a7e68292a91c6bd70917d26762c28e132fb04
atlas <TechSupport-ATLAS@sbb.ch>
2024-06-26 14:33:09 +0200

    ATLAS-2204: redact sensitive data with @Redacted annotation

--- workflow/src/main/java/ch/sbb/workflow/aop/RedactAspect.java
+++ workflow/src/main/java/ch/sbb/workflow/aop/RedactAspect.java
@@ -9,54 +9,80 @@ import ch.sbb.workflow.helper.StringHelper;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 @RequiredArgsConstructor
-public class StopPointWorkflowRedactAspect {
+public class RedactAspect {
+
+  private static final String ERROR_MSG = "You are redacting a wrong method!!";
 
   private final BusinessOrganisationBasedUserAdministrationService businessOrganisationBasedUserAdministrationService;
 
-  @Around("execution(* ch.sbb.workflow.repository.StopPointWorkflowRepository.find*(..))")
+  @Around("@annotation(ch.sbb.workflow.aop.Redacted)")
   public Object redactSensitiveData(final ProceedingJoinPoint joinPoint) throws Throwable {
-    return doRedactSensitiveData(joinPoint);
+    validateIsRedactedMethodSupported(joinPoint);
+    return redact(joinPoint);
   }
 
-  private Object doRedactSensitiveData(ProceedingJoinPoint joinPoint) throws Throwable {
+  Object redact(ProceedingJoinPoint joinPoint) throws Throwable {
     Object proceed = joinPoint.proceed();
+    if (proceed instanceof StopPointWorkflow) {
+      return redactStopPointSensitiveData(joinPoint);
+    }
+    if (proceed instanceof PageImpl) {
+      return redactStopPointPageSensitiveData(joinPoint);
+    }
+    throw new IllegalStateException(ERROR_MSG);
+  }
 
-    if(proceed instanceof PageImpl<?>){
+  private static void validateIsRedactedMethodSupported(ProceedingJoinPoint joinPoint) {
+    Class<?> redactedClassType = ((MethodSignature) joinPoint.getSignature()).getMethod().getAnnotation(Redacted.class)
+        .redactedClassType();
+    if (!StopPointWorkflow.class.equals(redactedClassType)) {
+      throw new IllegalStateException("Redaction for Class " + redactedClassType.getName() + " not supported!");
+    }
+  }
+
+  StopPointWorkflow redactStopPointSensitiveData(final ProceedingJoinPoint joinPoint) throws Throwable {
+    Object proceed = joinPoint.proceed();
+    if (proceed instanceof StopPointWorkflow stopPointWorkflow) {
+      if (!showConfidentialData(stopPointWorkflow.getSboid())) {
+        return redactData(stopPointWorkflow);
+      }
+      return stopPointWorkflow;
+    }
+    throw new IllegalStateException(ERROR_MSG);
+  }
+
+  PageImpl<StopPointWorkflow> redactStopPointPageSensitiveData(final ProceedingJoinPoint joinPoint) throws Throwable {
+    Object proceed = joinPoint.proceed();
+    if (proceed instanceof PageImpl) {
       final List<StopPointWorkflow> list = new ArrayList<>();
       try (Stream<StopPointWorkflow> stopPointWorkflowStream = ((PageImpl<StopPointWorkflow>) proceed).stream()) {
-        stopPointWorkflowStream.forEach( stopPointWorkflow -> {
-          if(!showConfidentialData(stopPointWorkflow.getSboid())){
-            list.add(redact(stopPointWorkflow));
-          }else {
+        stopPointWorkflowStream.forEach(stopPointWorkflow -> {
+          if (!showConfidentialData(stopPointWorkflow.getSboid())) {
+            list.add(redactData(stopPointWorkflow));
+          } else {
             list.add(stopPointWorkflow);
           }
         });
       }
-      return new PageImpl<>(list,((PageImpl<?>) proceed).getPageable(), ((PageImpl) proceed).getTotalElements());
+      return new PageImpl<>(list, ((PageImpl<?>) proceed).getPageable(), ((PageImpl) proceed).getTotalElements());
     }
-    if(proceed instanceof Optional<?>){
-      StopPointWorkflow stopPointWorkflow = ((Optional<StopPointWorkflow>) proceed).get();
-      if(!showConfidentialData(stopPointWorkflow.getSboid())){
-        return Optional.of(redact(stopPointWorkflow));
-      }
-    }
-    return proceed;
+    throw new IllegalStateException(ERROR_MSG);
   }
 
-  private StopPointWorkflow redact(StopPointWorkflow stopPointWorkflow){
+  private StopPointWorkflow redactData(StopPointWorkflow stopPointWorkflow) {
     List<String> mails = stopPointWorkflow.getCcEmails().stream().map(StringHelper::redactString).toList();
     Set<Person> examinantsRedacted = new HashSet<>();
     stopPointWorkflow.getExaminants().forEach(person -> examinantsRedacted.add(
@@ -71,7 +97,6 @@ public class StopPointWorkflowRedactAspect {
         .applicantMail(StringHelper.redactString(stopPointWorkflow.getApplicantMail()))
         .build();
   }
-
 
   boolean showConfidentialData(String sboid) {
     boolean hasPermission = businessOrganisationBasedUserAdministrationService
