f6985d3796af83d04a770d929f5036405e3d3250
atlas <19825793+iuccio@users.noreply.github.com>
2024-06-27 12:57:50 +0200

    ATLAS-1798: Update validator

--- frontend/src/app/pages/sepodi/workflow/detail-page/decision-detail/override/decision-override-form-group.ts
+++ frontend/src/app/pages/sepodi/workflow/detail-page/decision-detail/override/decision-override-form-group.ts
@@ -2,6 +2,7 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {JudgementType, ReadDecision} from "../../../../../../api";
 import {AtlasFieldLengthValidator} from "../../../../../../core/validation/field-lengths/atlas-field-length-validator";
 import {AtlasCharsetsValidator} from "../../../../../../core/validation/charsets/atlas-charsets-validator";
+import {DecisionFormGroupBuilder} from "../../decision-form/decision-form-group";
 
 export interface DecisionOverrideFormGroup {
   firstName: FormControl<string | null | undefined>;
@@ -11,12 +12,16 @@ export interface DecisionOverrideFormGroup {
 }
 
 export class DecisionOverrideFormGroupBuilder {
+
   static buildFormGroup(existingDecision?: ReadDecision) {
     return new FormGroup<DecisionOverrideFormGroup>({
-      firstName: new FormControl(existingDecision?.fotOverrider?.firstName, [Validators.required, AtlasFieldLengthValidator.length_50, AtlasCharsetsValidator.iso88591]),
-      lastName: new FormControl(existingDecision?.fotOverrider?.lastName, [Validators.required, AtlasFieldLengthValidator.length_50, AtlasCharsetsValidator.iso88591]),
-      fotJudgement: new FormControl(existingDecision?.fotJudgement, [Validators.required]),
-      fotMotivation: new FormControl(existingDecision?.fotMotivation, [AtlasFieldLengthValidator.comments, AtlasCharsetsValidator.iso88591]),
-    });
+        firstName: new FormControl(existingDecision?.fotOverrider?.firstName, [Validators.required, AtlasFieldLengthValidator.length_50, AtlasCharsetsValidator.iso88591]),
+        lastName: new FormControl(existingDecision?.fotOverrider?.lastName, [Validators.required, AtlasFieldLengthValidator.length_50, AtlasCharsetsValidator.iso88591]),
+        fotJudgement: new FormControl(existingDecision?.fotJudgement, [Validators.required]),
+        fotMotivation: new FormControl(existingDecision?.fotMotivation, [AtlasFieldLengthValidator.comments, AtlasCharsetsValidator.iso88591]),
+      },
+      {
+        validators: DecisionFormGroupBuilder.conditionallyRequired("fotJudgement","fotMotivation"),
+      });
   }
 }
