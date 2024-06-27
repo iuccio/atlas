486f84332c141c45611d267488b744a9ea3331f8
atlas <19825793+iuccio@users.noreply.github.com>
2024-06-27 16:14:11 +0200

    ATLAS-1798: finalize decision

--- frontend/src/app/pages/sepodi/workflow/detail-page/decision-detail/decision-detail-dialog.component.ts
+++ frontend/src/app/pages/sepodi/workflow/detail-page/decision-detail/decision-detail-dialog.component.ts
@@ -1,19 +1,19 @@
-import {Component, Inject, OnInit, ViewChild} from '@angular/core';
-import {FormsModule, ReactiveFormsModule,} from '@angular/forms';
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatStepperModule} from '@angular/material/stepper';
-import {TranslateModule} from '@ngx-translate/core';
-import {MAT_DIALOG_DATA, MatDialogClose, MatDialogRef} from '@angular/material/dialog';
-import {MatIconModule} from '@angular/material/icon';
-import {FormModule} from '../../../../../core/module/form.module';
-import {CoreModule} from '../../../../../core/module/core.module';
-import {ReadDecision, StopPointWorkflowService} from "../../../../../api";
-import {DecisionDetailDialogData} from "./decision-detail-dialog.service";
-import {DecisionOverrideComponent} from "./override/decision-override.component";
-import {DecisionFormComponent} from "../decision-form/decision-form.component";
-import {DecisionFormGroupBuilder} from "../decision-form/decision-form-group";
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatStepperModule } from '@angular/material/stepper';
+import { TranslateModule } from '@ngx-translate/core';
+import { MAT_DIALOG_DATA, MatDialogClose, MatDialogRef } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { FormModule } from '../../../../../core/module/form.module';
+import { CoreModule } from '../../../../../core/module/core.module';
+import { ReadDecision, StopPointWorkflowService } from '../../../../../api';
+import { DecisionDetailDialogData } from './decision-detail-dialog.service';
+import { DecisionOverrideComponent } from './override/decision-override.component';
+import { DecisionFormComponent } from '../decision-form/decision-form.component';
+import { DecisionFormGroupBuilder } from '../decision-form/decision-form-group';
 
 @Component({
   selector: 'decision-detail-dialog',
@@ -34,10 +34,8 @@ import {DecisionFormGroupBuilder} from "../decision-form/decision-form-group";
     DecisionFormComponent,
   ],
   templateUrl: './decision-detail-dialog.component.html',
-  styleUrl: './decision-detail-dialog.component.scss',
 })
 export class DecisionDetailDialogComponent implements OnInit {
-
   @ViewChild(DecisionOverrideComponent) decisionOverrideComponent!: DecisionOverrideComponent;
 
   existingDecision!: ReadDecision;
@@ -53,11 +51,13 @@ export class DecisionDetailDialogComponent implements OnInit {
   ngOnInit() {
     this.decisionForm.patchValue(this.decisionDetailDialogData.examinant.value);
     if (this.decisionDetailDialogData.examinant.controls.judgement.value) {
-      this.stopPointWorkflowService.getDecision(this.decisionDetailDialogData.examinant.controls.id.value!).subscribe(decision => {
-        this.existingDecision = decision;
-        this.decisionForm.controls.judgement.setValue(decision.judgement);
-        this.decisionForm.controls.motivation.setValue(decision.motivation);
-      });
+      this.stopPointWorkflowService
+        .getDecision(this.decisionDetailDialogData.examinant.controls.id.value!)
+        .subscribe((decision) => {
+          this.existingDecision = decision;
+          this.decisionForm.controls.judgement.setValue(decision.judgement);
+          this.decisionForm.controls.motivation.setValue(decision.motivation);
+        });
     }
     this.decisionForm.disable();
   }
