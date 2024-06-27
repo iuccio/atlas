574aa9545d533d0c8c9171c6ae15297df6ffa780
atlas <TechSupport-ATLAS@sbb.ch>
2024-06-27 16:08:12 +0200

    ATLAS-1798: Fix test

--- frontend/src/app/pages/sepodi/workflow/detail-page/decision-detail/override/decision-override.component.spec.ts
+++ frontend/src/app/pages/sepodi/workflow/detail-page/decision-detail/override/decision-override.component.spec.ts
@@ -1,14 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DecisionOverrideComponent } from './decision-override.component';
+import {AppTestingModule} from "../../../../../../app.testing.module";
 
-describe('DecisionDialogComponent', () => {
+describe('DecisionOverrideComponent', () => {
   let component: DecisionOverrideComponent;
   let fixture: ComponentFixture<DecisionOverrideComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DecisionOverrideComponent]
+      imports: [DecisionOverrideComponent, AppTestingModule]
     })
     .compileComponents();
 
