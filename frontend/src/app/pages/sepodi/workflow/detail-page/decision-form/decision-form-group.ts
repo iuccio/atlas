c37c10a8d6e928c3c74b1d9374f02f926f4a6a23
atlas <19825793+iuccio@users.noreply.github.com>
2024-06-27 16:14:11 +0200

    ATLAS-1798: finalize decision

--- frontend/src/app/pages/sepodi/workflow/detail-page/decision-form/decision-form-group.ts
+++ frontend/src/app/pages/sepodi/workflow/detail-page/decision-form/decision-form-group.ts
@@ -1,7 +1,7 @@
-import {AbstractControl, FormControl, FormGroup, ValidatorFn, Validators} from "@angular/forms";
-import {JudgementType, ReadDecision} from "../../../../../api";
-import {AtlasFieldLengthValidator} from "../../../../../core/validation/field-lengths/atlas-field-length-validator";
-import {AtlasCharsetsValidator} from "../../../../../core/validation/charsets/atlas-charsets-validator";
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { JudgementType, ReadDecision } from '../../../../../api';
+import { AtlasFieldLengthValidator } from '../../../../../core/validation/field-lengths/atlas-field-length-validator';
+import { AtlasCharsetsValidator } from '../../../../../core/validation/charsets/atlas-charsets-validator';
 
 export interface DecisionFormGroup {
   firstName: FormControl<string | null | undefined>;
@@ -13,21 +13,46 @@ export interface DecisionFormGroup {
 }
 
 export class DecisionFormGroupBuilder {
+  private static readonly _judgement = 'judgement';
+  private static readonly _motivation = 'motivation';
 
   static buildFormGroup(existingDecision?: ReadDecision) {
-    return new FormGroup<DecisionFormGroup>({
-        firstName: new FormControl(existingDecision?.examinant?.firstName, [Validators.required, AtlasFieldLengthValidator.length_50, AtlasCharsetsValidator.iso88591]),
-        lastName: new FormControl(existingDecision?.examinant?.lastName, [Validators.required, AtlasFieldLengthValidator.length_50, AtlasCharsetsValidator.iso88591]),
-        organisation: new FormControl(existingDecision?.examinant?.organisation, [Validators.required, AtlasFieldLengthValidator.length_50, AtlasCharsetsValidator.iso88591]),
-        personFunction: new FormControl(existingDecision?.examinant?.personFunction, [Validators.required, AtlasFieldLengthValidator.length_50, AtlasCharsetsValidator.iso88591]),
-        judgement: new FormControl(existingDecision?.judgement, [Validators.required]),
-        motivation: new FormControl(existingDecision?.motivation, [AtlasFieldLengthValidator.comments, AtlasCharsetsValidator.iso88591]),
+    return new FormGroup<DecisionFormGroup>(
+      {
+        firstName: new FormControl(existingDecision?.examinant?.firstName, [
+          Validators.required,
+          AtlasFieldLengthValidator.length_50,
+          AtlasCharsetsValidator.iso88591,
+        ]),
+        lastName: new FormControl(existingDecision?.examinant?.lastName, [
+          Validators.required,
+          AtlasFieldLengthValidator.length_50,
+          AtlasCharsetsValidator.iso88591,
+        ]),
+        organisation: new FormControl(existingDecision?.examinant?.organisation, [
+          Validators.required,
+          AtlasFieldLengthValidator.length_50,
+          AtlasCharsetsValidator.iso88591,
+        ]),
+        personFunction: new FormControl(existingDecision?.examinant?.personFunction, [
+          Validators.required,
+          AtlasFieldLengthValidator.length_50,
+          AtlasCharsetsValidator.iso88591,
+        ]),
+        [this._judgement]: new FormControl(existingDecision?.judgement, [Validators.required]),
+        [this._motivation]: new FormControl(existingDecision?.motivation, [
+          AtlasFieldLengthValidator.comments,
+          AtlasCharsetsValidator.iso88591,
+        ]),
       },
       {
-        validators: DecisionFormGroupBuilder.conditionallyRequired("judgement","motivation"),
-      });
+        validators: DecisionFormGroupBuilder.conditionallyRequired(
+          this._judgement,
+          this._motivation,
+        ),
+      },
+    );
   }
-
 
   static conditionallyRequired(judgementField: string, requiredField: string): ValidatorFn {
     return (c: AbstractControl): { [key: string]: boolean } | null => {
@@ -35,7 +60,7 @@ export class DecisionFormGroupBuilder {
       const required = c.get(requiredField)!;
 
       if (judgement.value === JudgementType.No && !required.value) {
-        required.setErrors({decision_comment_required: true});
+        required.setErrors({ decision_comment_required: true });
       } else {
         const errors = required.errors;
         delete errors?.decision_comment_required;
@@ -45,5 +70,4 @@ export class DecisionFormGroupBuilder {
       return null;
     };
   }
-
 }
--- frontend/src/app/pages/sepodi/workflow/detail-page/decision-form/decision-form.component.ts
+++ frontend/src/app/pages/sepodi/workflow/detail-page/decision-form/decision-form.component.ts
@@ -1,15 +1,15 @@
-import {Component, Input} from '@angular/core';
-import {ControlContainer, FormGroup, FormsModule, NgForm, ReactiveFormsModule,} from '@angular/forms';
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {TranslateModule} from '@ngx-translate/core';
-import {MatDialogClose} from '@angular/material/dialog';
-import {MatIconModule} from '@angular/material/icon';
-import {FormModule} from '../../../../../core/module/form.module';
-import {CoreModule} from '../../../../../core/module/core.module';
-import {DecisionFormGroup} from "./decision-form-group";
-import {JudgementType} from "../../../../../api";
+import { Component, Input } from '@angular/core';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { TranslateModule } from '@ngx-translate/core';
+import { MatDialogClose } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { FormModule } from '../../../../../core/module/form.module';
+import { CoreModule } from '../../../../../core/module/core.module';
+import { DecisionFormGroup } from './decision-form-group';
+import { JudgementType } from '../../../../../api';
 
 @Component({
   selector: 'decision-form',
@@ -28,12 +28,10 @@ import {JudgementType} from "../../../../../api";
   ],
   templateUrl: './decision-form.component.html',
   styleUrls: ['decision-form.component.scss'],
-  viewProviders: [{provide: ControlContainer, useExisting: NgForm}],
 })
 export class DecisionFormComponent {
-
   protected readonly JudgementType = JudgementType;
 
-  @Input() formGroup!: FormGroup<DecisionFormGroup>;
+  @Input() form!: FormGroup<DecisionFormGroup>;
   @Input() hasOverride = false;
 }
