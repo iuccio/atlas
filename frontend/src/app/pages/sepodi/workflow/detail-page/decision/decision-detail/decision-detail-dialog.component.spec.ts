79fb106934b28a49bf843d48bdc46af739f21f35
atlas <19825793+iuccio@users.noreply.github.com>
2024-06-28 10:55:41 +0200

    ATLAS-1798: add tests

--- frontend/src/app/pages/sepodi/workflow/detail-page/decision/decision-detail/decision-detail-dialog.component.spec.ts
+++ frontend/src/app/pages/sepodi/workflow/detail-page/decision/decision-detail/decision-detail-dialog.component.spec.ts
@@ -1,10 +1,12 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 
 import {DecisionDetailDialogComponent} from './decision-detail-dialog.component';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {DecisionDetailDialogData} from "./decision-detail-dialog.service";
 import {StopPointWorkflowDetailFormGroupBuilder} from "../../detail-form/stop-point-workflow-detail-form-group";
 import {AppTestingModule} from "../../../../../../app.testing.module";
+import {of} from "rxjs";
+import {JudgementType, ReadDecision, StopPointWorkflowService} from "../../../../../../api";
 
 const dialogRefSpy = jasmine.createSpyObj(['close']);
 const dialogData: DecisionDetailDialogData = {
@@ -14,25 +16,76 @@ const dialogData: DecisionDetailDialogData = {
   examinant: StopPointWorkflowDetailFormGroupBuilder.buildExaminantFormGroup()
 }
 
+const dialogDataWithExisitingExaminant: DecisionDetailDialogData = {
+  title: '',
+  message: '',
+  workflowId: 123,
+  examinant: StopPointWorkflowDetailFormGroupBuilder.buildExaminantFormGroup({
+    judgement: JudgementType.Yes,
+    organisation: 'Stadt Bern',
+    mail: 'stadt@bern.be'
+  })
+}
+
+const existingDecision: ReadDecision = {
+  judgement: JudgementType.Yes,
+  motivation: 'Yep boiii'
+}
+const stopPointWorkflowService = jasmine.createSpyObj('stopPointWorkflowService', {
+  getDecision: of(existingDecision)
+});
+
 describe('DecisionDetailDialogComponent', () => {
   let component: DecisionDetailDialogComponent;
   let fixture: ComponentFixture<DecisionDetailDialogComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(() => {
+    TestBed.configureTestingModule({
       imports: [DecisionDetailDialogComponent, AppTestingModule],
       providers: [
         {provide: MatDialogRef, useValue: dialogRefSpy},
-        {provide: MAT_DIALOG_DATA, useValue: dialogData}]
-    })
-      .compileComponents();
-
-    fixture = TestBed.createComponent(DecisionDetailDialogComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+        {provide: MAT_DIALOG_DATA, useValue: dialogData},
+        {provide: StopPointWorkflowService, useValue: stopPointWorkflowService},
+      ]
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('without existing decision', () => {
+    beforeEach(() => {
+      fixture = TestBed.createComponent(DecisionDetailDialogComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+
+      expect(component.existingDecision).toBeUndefined();
+      expect(component.decisionForm.disabled).toBeTrue();
+    });
+
+    it('should close dialog', () => {
+      component.close();
+
+      expect(dialogRefSpy.close).toHaveBeenCalled();
+    });
+  });
+
+  describe('with existing decision', () => {
+    beforeEach(() => {
+      TestBed.overrideProvider(MAT_DIALOG_DATA, {useValue: dialogDataWithExisitingExaminant});
+      fixture = TestBed.createComponent(DecisionDetailDialogComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should init', () => {
+      expect(component).toBeTruthy();
+
+      expect(stopPointWorkflowService.getDecision).toHaveBeenCalled();
+
+      expect(component.existingDecision).toBeDefined();
+      expect(component.decisionForm.controls.judgement.value).toEqual(JudgementType.Yes);
+    });
   });
 });
