6e5e941fdcb2870dd63766b81ba498ebde07ebcd
atlas <TechSupport-ATLAS@sbb.ch>
2024-07-09 21:06:05 +0200

    ATLAS-1729: Add some improvements in logging aspect for workflow splunk alerts.

--- workflow/src/main/java/ch/sbb/workflow/aop/LoggingAspect.java
+++ workflow/src/main/java/ch/sbb/workflow/aop/LoggingAspect.java
@@ -10,7 +10,6 @@ import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -20,51 +19,47 @@ public class LoggingAspect {
   private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
   private static final String ERROR_MARKER = "CRITICAL_WORKFLOW_ERROR";
 
-  @Autowired
-  private ObjectMapper objectMapper;
+  private final ObjectMapper objectMapper;
+
+  public LoggingAspect(ObjectMapper objectMapper) {
+    this.objectMapper = objectMapper;
+  }
 
   @Around("@annotation(methodLogged)")
   public Object logMethod(ProceedingJoinPoint joinPoint, MethodLogged methodLogged) throws Throwable {
-    String methodName = joinPoint.getSignature().getName();
-    String workflowType = methodLogged.workflowType();
-    boolean isCritical = methodLogged.critical();
-    Object[] args = joinPoint.getArgs();
+    var methodName = joinPoint.getSignature().getName();
+    var workflowType = methodLogged.workflowType();
+    var isCritical = methodLogged.critical();
 
-    MDC.put("methodName", methodName);
-    MDC.put("workflowType", workflowType);
-    MDC.put("isCritical", String.valueOf(isCritical));
+    try (var ignored = MDC.putCloseable("methodName", methodName);
+        var ignored2 = MDC.putCloseable("workflowType", workflowType);
+        var ignored3 = MDC.putCloseable("isCritical", String.valueOf(isCritical))) {
 
-    try {
-      Object result = joinPoint.proceed();
-      return result;
+      return joinPoint.proceed();
     } catch (Exception e) {
-      Map<String, Object> errorDetails = buildErrorDetails(methodName, workflowType, isCritical, args, e);
-      String jsonErrorDetails = objectMapper.writeValueAsString(errorDetails);
+      var errorDetails = buildErrorDetails(methodName, workflowType, isCritical, joinPoint.getArgs(), e);
+      var jsonErrorDetails = objectMapper.writeValueAsString(errorDetails);
       logger.error("{}: {}", ERROR_MARKER, jsonErrorDetails, e);
       throw e;
-    } finally {
-      MDC.remove("methodName");
-      MDC.remove("workflowType");
-      MDC.remove("isCritical");
     }
   }
 
   private Map<String, Object> buildErrorDetails(String methodName, String workflowType, boolean isCritical, Object[] args, Exception e) {
-    Map<String, Object> details = new HashMap<>();
+    var details = new HashMap<String, Object>();
     details.put("methodName", methodName);
     details.put("workflowType", workflowType);
     details.put("isCritical", isCritical);
     details.put("errorMessage", e.getMessage());
 
-    for (Object arg : args) {
-      if (arg instanceof StopPointAddWorkflowModel) {
-        StopPointAddWorkflowModel model = (StopPointAddWorkflowModel) arg;
-        details.put("workflowId", model.getId());
-        details.put("servicePointVersionId", model.getVersionId());
-        details.put("sloid", model.getSloid());
-      } else if (arg instanceof Long) {
-        Long id = (Long) arg;
-        details.put("workflowId", id);
+    for (var arg : args) {
+      switch (arg) {
+        case StopPointAddWorkflowModel model -> {
+          details.put("workflowId", model.getId());
+          details.put("servicePointVersionId", model.getVersionId());
+          details.put("sloid", model.getSloid());
+        }
+        case Long id -> details.put("workflowId", id);
+        default -> {}
       }
     }
 
