b7daf13b967f0948b987988f88976a9e2f56da0f
atlas <19825793+iuccio@users.noreply.github.com>
2024-07-11 14:25:02 +0200

    ATLAS-1830 added restart dialog

--- workflow/src/main/java/ch/sbb/workflow/model/sepodi/StopPointRestartWorkflowModel.java
+++ workflow/src/main/java/ch/sbb/workflow/model/sepodi/StopPointRestartWorkflowModel.java
@@ -21,9 +21,21 @@ import lombok.NoArgsConstructor;
 @Schema(name = "StopPointRestartWorkflow")
 public class StopPointRestartWorkflowModel extends BasePersonModel {
 
-  @Schema(description = "BAV Examinant Client")
-  @NotNull
-  private ClientPersonModel examinantBAVClient;
+//  @Schema(description = "BAV Examinant Client")
+//  @NotNull
+//  private ClientPersonModel examinantBAVClient;
+
+  @Schema(description = "Organisation", example = "ZVV Zürcher Verkehrsverbund")
+  @Pattern(regexp = AtlasCharacterSetsRegex.ISO_8859_1)
+  @NotBlank
+  @Size(min = 1, max = AtlasFieldLengths.LENGTH_255)
+  private String organisation;
+
+  @Pattern(regexp = AtlasCharacterSetsRegex.EMAIL_ADDRESS)
+  @Size(min = 1, max = AtlasFieldLengths.LENGTH_255)
+  @Schema(description = "mail", example = "mail@sbb.ch")
+  @NotBlank
+  private String mail;
 
   @Pattern(regexp = AtlasCharacterSetsRegex.ISO_8859_1)
   @Size(min = 1, max = AtlasFieldLengths.LENGTH_1500)
@@ -33,12 +45,6 @@ public class StopPointRestartWorkflowModel extends BasePersonModel {
   @NotNull
   @Size(min = 2, max = AtlasFieldLengths.LENGTH_30)
   @Schema(description = "Official designation of a location that must be used by all recipients"
-      , example = "Biel/Bienne Bözingenfeld/Champ", maxLength = 30)
-  private String newDesignationOfficial;
-
-  @Schema(description = "Organisation", example = "ZVV Zürcher Verkehrsverbund")
-  @Pattern(regexp = AtlasCharacterSetsRegex.ISO_8859_1)
-  @NotBlank
-  @Size(min = 1, max = AtlasFieldLengths.LENGTH_255)
-  private String organisation;
+          , example = "Biel/Bienne Bözingenfeld/Champ", maxLength = 30)
+  private String designationOfficial;
 }
