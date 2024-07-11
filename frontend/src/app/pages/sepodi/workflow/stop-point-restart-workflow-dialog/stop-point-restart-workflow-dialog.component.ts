515515bed0d9106e1b236ab9ff94c975225bcc58
atlas <19825793+iuccio@users.noreply.github.com>
2024-07-11 09:43:36 +0200

    ATLAS-1830 restart workflow

--- frontend/src/app/pages/sepodi/workflow/stop-point-restart-workflow-dialog/stop-point-restart-workflow-dialog.component.ts
+++ frontend/src/app/pages/sepodi/workflow/stop-point-restart-workflow-dialog/stop-point-restart-workflow-dialog.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup} from "@angular/forms";
+import {StopPointRestartWorkflowFormGroup} from "./stop-point-restart-workflow-form-group";
+import {
+  StopPointRejectWorkflowFormGroupBuilder
+} from "../stop-point-reject-workflow-dialog/stop-point-reject-workflow-form-group";
+import {DetailHelperService} from "../../../../core/detail/detail-helper.service";
+import {MatDialogRef} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-stop-point-restart-workflow-dialog',
   templateUrl: './stop-point-restart-workflow-dialog.component.html'
 })
-export class StopPointRestartWorkflowDialogComponent {
+export class StopPointRestartWorkflowDialogComponent implements OnInit {
 
+  formGroup!: FormGroup<StopPointRestartWorkflowFormGroup>;
+
+
+  constructor(
+    public dialogRef: MatDialogRef<StopPointRestartWorkflowDialogComponent>,
+    private detailHelperService: DetailHelperService,
+  ) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  closeDialog() {
+    this.detailHelperService.confirmLeaveDirtyForm(this.formGroup).subscribe((confirmed) => {
+      if (confirmed) {
+        this.dialogRef.close(true);
+      }
+    });
+  }
 }
