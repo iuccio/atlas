93706000d8f90e37170dbc5650b1d5c9e13a6698
atlas <TechSupport-ATLAS@sbb.ch>
2024-07-12 09:34:06 +0200

    Add custom file not found on s3 exception with link to doc

---
+++ base-atlas/src/main/java/ch/sbb/atlas/model/exception/FileNotFoundOnS3Exception.java
@@ -0,0 +1,26 @@
+package ch.sbb.atlas.model.exception;
+
+import ch.sbb.atlas.api.model.ErrorResponse;
+import java.util.TreeSet;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+public class FileNotFoundOnS3Exception extends AtlasException {
+
+  private final String filePath;
+
+  @Override
+  public ErrorResponse getErrorResponse() {
+    return ErrorResponse.builder()
+        .status(HttpStatus.NOT_FOUND.value())
+        .message("""
+            File not found on S3. The export might not have run yet.
+            Please consult the docs at: https://export-service-point.dev.app.sbb.ch/static/rest-api.html for data availability.
+            """)
+        .error("File " + filePath + " not found on atlas amazon s3 bucket.")
+        .details(new TreeSet<>())
+        .build();
+  }
+
+}
