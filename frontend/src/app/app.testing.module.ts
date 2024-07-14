6478838ec39f1621911921ed9d13ab96e1e8f87d
atlas <19825793+iuccio@users.noreply.github.com>
2024-07-14 04:02:00 +0000

    Update dependency prettier to v3.3.3

--- frontend/src/app/app.testing.module.ts
+++ frontend/src/app/app.testing.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {TranslateFakeLoader, TranslateLoader, TranslateModule, TranslatePipe} from '@ngx-translate/core';
+import {
+  TranslateFakeLoader,
+  TranslateLoader,
+  TranslateModule,
+  TranslatePipe,
+} from '@ngx-translate/core';
 import { DateModule } from './core/module/date.module';
 import { MaterialModule } from './core/module/material.module';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
@@ -35,9 +40,6 @@ const dialogMock = {
     RouterModule,
     TranslateModule,
   ],
-  providers: [
-    { provide: MatDialogRef, useValue: dialogMock },
-    { provide: TranslatePipe }
-  ],
+  providers: [{ provide: MatDialogRef, useValue: dialogMock }, { provide: TranslatePipe }],
 })
 export class AppTestingModule {}
