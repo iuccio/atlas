2097ef33b84e613d8a3412910623583213ac2548
atlas <19825793+iuccio@users.noreply.github.com>
2024-07-14 04:02:00 +0000

    Update dependency prettier to v3.3.3

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,8 +2,8 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-import {AuthConfig} from 'angular-oauth2-oidc';
-import {Environment} from './environment.model';
+import { AuthConfig } from 'angular-oauth2-oidc';
+import { Environment } from './environment.model';
 import packageJson from '../../package.json';
 
 // See https://confluence.sbb.ch/display/CLEW/Azure+AD
