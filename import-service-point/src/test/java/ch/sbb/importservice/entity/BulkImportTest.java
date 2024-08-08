0727bb9cad4c71e09aa7d42ecd9a07235468be1a
atlas <TechSupport-ATLAS@sbb.ch>
2024-08-08 15:38:35 +0200

    ATLAS-2016: Add bulk import entity

---
+++ import-service-point/src/test/java/ch/sbb/importservice/entity/BulkImportTest.java
@@ -0,0 +1,66 @@
+package ch.sbb.importservice.entity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import ch.sbb.atlas.kafka.model.user.admin.ApplicationType;
+import ch.sbb.atlas.model.BaseValidatorTest;
+import ch.sbb.importservice.model.BusinessObjectType;
+import ch.sbb.importservice.model.ImportType;
+import jakarta.validation.ConstraintViolation;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+
+class BulkImportTest extends BaseValidatorTest {
+
+  @Test
+  void shouldValidateBusinessObjectTypeBasedOnApplicationSepodi() {
+    BulkImport bulkImport = BulkImport.builder()
+        .application(ApplicationType.SEPODI)
+        .objectType(BusinessObjectType.SERVICE_POINT)
+        .importType(ImportType.UPDATE)
+        .creator("e123456")
+        .build();
+
+    Set<ConstraintViolation<BulkImport>> constraintViolations = validator.validate(bulkImport);
+    assertThat(constraintViolations).isEmpty();
+  }
+
+  @Test
+  void shouldValidateBusinessObjectTypeBasedOnApplicationPrm() {
+    BulkImport bulkImport = BulkImport.builder()
+        .application(ApplicationType.PRM)
+        .objectType(BusinessObjectType.CONTACT_POINT)
+        .importType(ImportType.UPDATE)
+        .creator("e123456")
+        .build();
+
+    Set<ConstraintViolation<BulkImport>> constraintViolations = validator.validate(bulkImport);
+    assertThat(constraintViolations).isEmpty();
+  }
+
+  @Test
+  void shouldValidateBusinessObjectTypeBasedOnApplicationSepodiAndFail() {
+    BulkImport bulkImport = BulkImport.builder()
+        .application(ApplicationType.SEPODI)
+        .objectType(BusinessObjectType.STOP_POINT)
+        .importType(ImportType.UPDATE)
+        .creator("e123456")
+        .build();
+
+    Set<ConstraintViolation<BulkImport>> constraintViolations = validator.validate(bulkImport);
+    assertThat(constraintViolations).hasSize(1);
+  }
+
+  @Test
+  void shouldValidateBusinessObjectTypeBasedOnApplicationPrmAndFail() {
+    BulkImport bulkImport = BulkImport.builder()
+        .application(ApplicationType.PRM)
+        .objectType(BusinessObjectType.LOADING_POINT)
+        .importType(ImportType.UPDATE)
+        .creator("e123456")
+        .build();
+
+    Set<ConstraintViolation<BulkImport>> constraintViolations = validator.validate(bulkImport);
+    assertThat(constraintViolations).hasSize(1);
+  }
+}
