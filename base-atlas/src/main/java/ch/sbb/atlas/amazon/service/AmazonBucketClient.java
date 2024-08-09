18eb03f173f8fb0f8b3e204f37bb8bbee05d2a39
atlas <19825793+iuccio@users.noreply.github.com>
2024-08-09 12:53:56 +0200

    ATLAS-2377: Use new SDK, add integration test

--- base-atlas/src/main/java/ch/sbb/atlas/amazon/service/AmazonBucketClient.java
+++ base-atlas/src/main/java/ch/sbb/atlas/amazon/service/AmazonBucketClient.java
@@ -1,14 +1,14 @@
 package ch.sbb.atlas.amazon.service;
 
 import ch.sbb.atlas.amazon.config.AmazonConfigProps.AmazonBucketConfig;
-import com.amazonaws.services.s3.AmazonS3;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Data
 @RequiredArgsConstructor
 public class AmazonBucketClient {
     private final AmazonBucket bucket;
-    private final AmazonS3 client;
+    private final S3Client client;
     private final AmazonBucketConfig amazonBucketConfig;
 }
--- line-directory/src/main/java/ch/sbb/line/directory/service/hearing/TimetableHearingPdfsAmazonService.java
+++ line-directory/src/main/java/ch/sbb/line/directory/service/hearing/TimetableHearingPdfsAmazonService.java
@@ -1,10 +1,8 @@
 package ch.sbb.line.directory.service.hearing;
 
-import ch.sbb.atlas.amazon.exception.FileException;
 import ch.sbb.atlas.amazon.service.AmazonBucket;
 import ch.sbb.atlas.amazon.service.AmazonService;
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,11 +18,7 @@ public class TimetableHearingPdfsAmazonService {
   public void uploadPdfFiles(List<File> files, String dirName) {
     log.info("Starting upload to S3 Bucket");
     files.forEach(file -> {
-      try {
-        amazonService.putFile(AmazonBucket.HEARING_DOCUMENT, file, dirName);
-      } catch (IOException e) {
-        throw new FileException("Error uploading file: " + file.getName() + " to bucket: " + AmazonBucket.HEARING_DOCUMENT, e);
-      }
+      amazonService.putFile(AmazonBucket.HEARING_DOCUMENT, file, dirName);
     });
     log.info("Upload complete.");
   }
