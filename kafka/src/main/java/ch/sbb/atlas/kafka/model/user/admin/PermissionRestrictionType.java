af69fa98491d532b7d1d0fa0df4c986f9d005a7a
atlas <TechSupport-ATLAS@sbb.ch>
2024-08-14 15:28:08 +0200

    ATLAS-2022: Add permission check to bulk import controller

--- kafka/src/main/java/ch/sbb/atlas/kafka/model/user/admin/PermissionRestrictionType.java
+++ kafka/src/main/java/ch/sbb/atlas/kafka/model/user/admin/PermissionRestrictionType.java
@@ -1,9 +1,7 @@
 package ch.sbb.atlas.kafka.model.user.admin;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Getter;
 
-@Getter
 @Schema(enumAsRef = true)
 public enum PermissionRestrictionType {
 
