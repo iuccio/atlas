a8f39a0ab59f47fb814f79caa8ce0e280ce009c9
atlas <TechSupport-ATLAS@sbb.ch>
2024-08-14 16:57:38 +0200

    ATLAS-2027: Add backend for files download.

--- import-service-point/src/main/java/ch/sbb/importservice/controller/BulkImportController.java
+++ import-service-point/src/main/java/ch/sbb/importservice/controller/BulkImportController.java
@@ -10,41 +10,25 @@ import ch.sbb.importservice.service.bulk.BulkImportService;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import ch.sbb.importservice.service.BulkImportService;
+import java.io.File;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@Tag(name = "Bulk Import")
-@RequestMapping("v1/import/bulk")
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-public class BulkImportController {
+public class BulkImportController implements BulkImportApiV1 {
 
   private final BulkImportService bulkImportService;
   private final FileService fileService;
 
-  @PostMapping("{application}/{objectType}/{importType}")
-  @ResponseStatus(HttpStatus.ACCEPTED)
-  @ApiResponses(value = {
-      @ApiResponse(responseCode = "202"),
-  })
-  @PreAuthorize("""
-      @bulkImportUserAdministrationService.hasPermissionsForBulkImport(#application)""")
-  public void startServicePointImportBatch(
-      @PathVariable ApplicationType application,
-      @PathVariable BusinessObjectType objectType,
-      @PathVariable ImportType importType,
-      @RequestParam MultipartFile file) {
+  @Override
+  public void startServicePointImportBatch(ApplicationType application, BusinessObjectType objectType,
+      ImportType importType, MultipartFile file) {
     log.info("Starting bulk import:");
     log.info("Application={}, BusinessObject={}, ImportType={}", application, objectType, importType);
     log.info("Uploaded file has size={}, uploadFileName={}, contentType={}",
@@ -60,6 +44,11 @@ public class BulkImportController {
         .build();
 
     bulkImportService.startBulkImport(bulkImport, fileService.getFileFromMultipart(file));
+  }
+
+  @Override
+  public File downloadTemplate(BusinessObjectType objectType, ImportType importType) {
+    return bulkImportService.downloadTemplate(objectType, importType);
   }
 
 }
