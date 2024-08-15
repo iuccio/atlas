07a6dc894347fba91411b8f526f5d4c1b4c26f91
atlas <TechSupport-ATLAS@sbb.ch>
2024-08-15 10:41:52 +0200

    ATLAS-2235: add logging

--- workflow/src/main/java/ch/sbb/workflow/service/sepodi/StopPointWorkflowTransitionService.java
+++ workflow/src/main/java/ch/sbb/workflow/service/sepodi/StopPointWorkflowTransitionService.java
@@ -1,6 +1,7 @@
 package ch.sbb.workflow.service.sepodi;
 
 import static ch.sbb.atlas.workflow.model.WorkflowStatus.REJECTED;
+import static ch.sbb.workflow.service.sepodi.StopPointWorkflowService.WORKFLOW_DURATION_IN_DAYS;
 
 import ch.sbb.atlas.api.servicepoint.ReadServicePointVersionModel;
 import ch.sbb.atlas.kafka.model.SwissCanton;
@@ -27,9 +28,11 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 @Transactional
@@ -40,6 +43,8 @@ public class StopPointWorkflowTransitionService {
   private final Examinants examinants;
   private final StopPointWorkflowNotificationService notificationService;
   private final StopPointWorkflowService stopPointWorkflowService;
+
+  static final int WORKFLOW_EXPIRATION_IN_DAYS = WORKFLOW_DURATION_IN_DAYS + 1;
 
   /**
    * Authorization for this method is delegated to ServicePointService#update()
@@ -61,7 +66,7 @@ public class StopPointWorkflowTransitionService {
 
     stopPointWorkflow.setStatus(WorkflowStatus.HEARING);
     stopPointWorkflow.setStartDate(LocalDate.now());
-    stopPointWorkflow.setEndDate(LocalDate.now().plusDays(StopPointWorkflowService.WORKFLOW_DURATION_IN_DAYS));
+    stopPointWorkflow.setEndDate(LocalDate.now().plusDays(WORKFLOW_DURATION_IN_DAYS));
     StopPointWorkflow workflow = stopPointWorkflowService.save(stopPointWorkflow);
     notificationService.sendStartStopPointWorkflowMail(workflow);
     return workflow;
@@ -138,18 +143,38 @@ public class StopPointWorkflowTransitionService {
   }
 
   public void endExpiredWorkflows() {
-    List<StopPointWorkflow> expiredWorkflow = stopPointWorkflowService.findExpiredWorkflow();
-    expiredWorkflow.forEach(stopPointWorkflow -> {
+    log.info("###### Start close expired workflows in Hearing ######");
+    List<StopPointWorkflow> expiredWorkflows = getExpiredWorkflows();
+    expiredWorkflows.forEach(stopPointWorkflow -> {
       Set<Decision> decisions = new HashSet<>(decisionService.findDecisionByWorkflowId(stopPointWorkflow.getId()));
       if (decisions.stream().noneMatch(Decision::hasWeightedJudgementTypeNo)) {
+        log.info("#### Found Expired workflow without JudgmentType.NO to close: [id:{},startDate:{},endDate:{},status:{}]",
+            stopPointWorkflow.getId(), stopPointWorkflow.getStartDate(), stopPointWorkflow.getEndDate(),
+            stopPointWorkflow.getStatus());
         List<Person> examiants = getExamiantsToVote(stopPointWorkflow, decisions);
         examiants.forEach(stopPointWorkflowService::voteExpiredWorkflowDecision);
+
         sePoDiClientService.updateStopPointStatusToValidatedAsAdmin(stopPointWorkflow);
         notificationService.sendApprovedStopPointWorkflowMail(stopPointWorkflow);
+        stopPointWorkflow.setEndDate(LocalDate.now());
         stopPointWorkflow.setStatus(WorkflowStatus.APPROVED);
         stopPointWorkflowService.save(stopPointWorkflow);
+        log.info("#### Expired workflow without JudgmentType.NO successfully closed: [id:{},startDate:{},endDate:{},status:{}]",
+            stopPointWorkflow.getId(), stopPointWorkflow.getStartDate(), stopPointWorkflow.getEndDate(),
+            stopPointWorkflow.getStatus());
       }
     });
+    log.info("###### End close expired workflows in Hearing! ######");
+  }
+
+  private List<StopPointWorkflow> getExpiredWorkflows() {
+    List<StopPointWorkflow> workflowsInHearing = stopPointWorkflowService.findWorkflowsInHearing();
+    log.info("## Found {} in Hearing...", workflowsInHearing.size());
+    List<StopPointWorkflow> expiredWorkflows = workflowsInHearing.stream()
+        .filter(stopPointWorkflow -> stopPointWorkflow.getStartDate().plusDays(WORKFLOW_EXPIRATION_IN_DAYS).equals(
+            LocalDate.now())).toList();
+    log.info("## Found {} workflow(s) expired ...", expiredWorkflows.size());
+    return expiredWorkflows;
   }
 
   private List<Person> getExamiantsToVote(StopPointWorkflow stopPointWorkflow, Set<Decision> decisions) {
@@ -158,6 +183,8 @@ public class StopPointWorkflowTransitionService {
     List<Person> overriderVotedPersonIds = new ArrayList<>(decisions.stream().map(Decision::getFotOverrider).toList());
     examiants.removeIf(examinantVotedPersonIds::contains);
     examiants.removeIf(overriderVotedPersonIds::contains);
+    examiants.forEach(person ->
+        log.info("### Found following examinant without vote: id:{}, mail:{}", person.getId(), person.getMail()));
     return examiants;
   }
 
