78f67551d5ae7e87015751e3e200c3ce5dff2eb2
atlas <19825793+iuccio@users.noreply.github.com>
2024-08-19 17:39:53 +0200

    ATLAS-2035: implement typing

--- base-atlas/src/main/java/ch/sbb/atlas/imports/bulk/ServicePointUpdateCsvModel.java
+++ base-atlas/src/main/java/ch/sbb/atlas/imports/bulk/ServicePointUpdateCsvModel.java
@@ -1,11 +1,18 @@
 package ch.sbb.atlas.imports.bulk;
 
+import ch.sbb.atlas.imports.bulk.ServicePointUpdateCsvModel.Fields;
+import ch.sbb.atlas.imports.servicepoint.deserializer.LocalDateDeserializer;
+import ch.sbb.atlas.imports.servicepoint.deserializer.PipedSetDeserializer;
+import ch.sbb.atlas.servicepoint.enumeration.Category;
+import ch.sbb.atlas.servicepoint.enumeration.MeanOfTransport;
 import ch.sbb.atlas.servicepoint.enumeration.OperatingPointTechnicalTimetableType;
 import ch.sbb.atlas.servicepoint.enumeration.OperatingPointTrafficPointType;
 import ch.sbb.atlas.servicepoint.enumeration.OperatingPointType;
 import ch.sbb.atlas.servicepoint.enumeration.StopPointType;
-import ch.sbb.atlas.imports.bulk.ServicePointUpdateCsvModel.Fields;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import java.time.LocalDate;
+import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -32,9 +39,11 @@ public class ServicePointUpdateCsvModel implements BulkImportContainer {
 
   private Integer number;
 
-  private String validFrom;
+  @JsonDeserialize(using = LocalDateDeserializer.class)
+  private LocalDate validFrom;
 
-  private String validTo;
+  @JsonDeserialize(using = LocalDateDeserializer.class)
+  private LocalDate validTo;
 
   private String designationOfficial;
 
@@ -44,15 +53,17 @@ public class ServicePointUpdateCsvModel implements BulkImportContainer {
 
   private boolean freightServicePoint;
 
+  // todo: muss weg
   private boolean borderPoint;
 
   private OperatingPointType operatingPointType;
 
   private OperatingPointTechnicalTimetableType operatingPointTechnicalTimetableType;
 
-  private String meansOfTransport;
+  @JsonDeserialize(using = PipedSetDeserializer.class)
+  private Set<MeanOfTransport> meansOfTransport;
 
-  private String categories;
+  private Set<Category> categories;
 
   private OperatingPointTrafficPointType operatingPointTrafficPointType;
 
