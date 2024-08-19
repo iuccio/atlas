9e81361f74f401e97dfad1ce5cefc15251e0f68a
atlas <TechSupport-ATLAS@sbb.ch>
2024-08-19 14:43:18 +0200

    ATLAS-2388: Fix frontend tests.

--- frontend/src/app/pages/sepodi/workflow/add-dialog/add-stop-point-workflow-dialog.service.spec.ts
+++ frontend/src/app/pages/sepodi/workflow/add-dialog/add-stop-point-workflow-dialog.service.spec.ts
@@ -4,6 +4,7 @@ import {TestBed} from '@angular/core/testing';
 import {TranslateModule} from '@ngx-translate/core';
 import {AddStopPointWorkflowDialogService,} from './add-stop-point-workflow-dialog.service';
 import {BERN_WYLEREGG} from "../../../../../test/data/service-point";
+import {DecisionType, JudgementType, StopPointPerson} from "../../../../api";
 
 describe('AddStopPointWorkflowDialogService', () => {
   let service: AddStopPointWorkflowDialogService;
@@ -20,9 +21,30 @@ describe('AddStopPointWorkflowDialogService', () => {
 
   it('should open new workflow', () => {
     dialogSpy.open.and.returnValue({ afterClosed: () => of(true) });
+    const examinants: StopPointPerson[] = [
+      {
+        firstName: 'Thomas',
+        lastName: 'Thomas',
+        organisation: 'sbb',
+        personFunction: 'PO',
+        mail: 'thomas.thomas@fake.com',
+        judgement: JudgementType.Yes,
+        decisionType: DecisionType.Voted,
+        id: 1,
+      },
+      {
+        firstName: 'Judith',
+        lastName: 'Judith',
+        organisation: 'sbb',
+        personFunction: 'PO',
+        mail: 'judith.judith@fake.com',
+        judgement: JudgementType.No,
+        decisionType: DecisionType.Voted,
+        id: 2,
+      }];
 
     service
-      .openDialog(BERN_WYLEREGG)
+      .openDialog(BERN_WYLEREGG, examinants)
       .subscribe((result) => expect(result).toBeTrue());
 
     expect(dialogSpy.open).toHaveBeenCalled();
