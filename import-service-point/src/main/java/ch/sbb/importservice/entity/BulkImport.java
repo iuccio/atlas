4ec24427ca433a43c38840295af8942a837b89a9
atlas <19825793+iuccio@users.noreply.github.com>
2024-08-20 19:50:14 +0200

    ATLAS-885 added detail view for bulk import

--- import-service-point/src/main/java/ch/sbb/importservice/entity/BulkImport.java
+++ import-service-point/src/main/java/ch/sbb/importservice/entity/BulkImport.java
@@ -18,7 +18,6 @@ import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 import lombok.AllArgsConstructor;
