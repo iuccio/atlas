12e459655a6be5c728d3a43b575d87d508ccd822
atlas <19825793+iuccio@users.noreply.github.com>
2024-08-21 15:23:06 +0200

    ATLAS-885 added formcontrol name to detailview

--- frontend/src/app/pages/user-administration/user/user-select/user-select.component.ts
+++ frontend/src/app/pages/user-administration/user/user-select/user-select.component.ts
@@ -19,6 +19,7 @@ export class UserSelectComponent {
   userSearchResults$: Observable<User[]> = of([]);
 
   searchUser(searchQuery: string): void {
+    console.log("string ", searchQuery)
     if (!searchQuery) {
       return;
     }
