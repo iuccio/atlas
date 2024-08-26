63cb66f3e58ec994dfd15c0ad2d4d3d1ed57b9a9
atlas <19825793+iuccio@users.noreply.github.com>
2024-08-26 06:00:53 +0200

    ATLAS-2027: Remove frontend changes.

--- frontend/src/app/pages/bulk-import/overview/bulk-import-overview.component.ts
+++ frontend/src/app/pages/bulk-import/overview/bulk-import-overview.component.ts
@@ -1,34 +1,8 @@
 import {Component} from "@angular/core";
-import {BulkImportService, BusinessObjectType, ImportType} from "../../../api";
-import {FileDownloadService} from "../../../core/components/file-upload/file/file-download.service";
 
 @Component({
   templateUrl: './bulk-import-overview.component.html'
 })
 export class BulkImportOverviewComponent {
-
-  selectedBusinessObjectType: keyof typeof BusinessObjectType = 'ServicePoint';
-  selectedImportType: keyof typeof ImportType = 'Create';
-
-  constructor(
-    private readonly bulkImportService: BulkImportService,
-  ) {}
-
-  downloadExcel() {
-    this.selectedBusinessObjectType = 'ServicePoint' as keyof typeof BusinessObjectType;
-    this.selectedImportType = 'Update' as keyof typeof ImportType;
-    if (this.selectedBusinessObjectType && this.selectedImportType) {
-      const businessObjectType = BusinessObjectType[this.selectedBusinessObjectType];
-      const importType = ImportType[this.selectedImportType];
-
-      const filename = `${this.selectedImportType.toLowerCase()}_${this.selectedBusinessObjectType.toLowerCase()}.xlsx`;
-
-      this.bulkImportService
-        .downloadTemplate(businessObjectType, importType)
-        .subscribe((response) => FileDownloadService.downloadFile(filename, response));
-    } else {
-      console.error('Please select both a Business Object Type and an Import Type.');
-    }
-  }
 
 }
