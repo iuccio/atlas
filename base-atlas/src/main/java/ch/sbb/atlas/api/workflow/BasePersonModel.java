e7c00415248e7a0367199ede726cf6d0edb194b6
atlas <19825793+iuccio@users.noreply.github.com>
2024-08-26 21:36:59 +0200

    ATLAS-2388: Changes according to PR comments.

--- base-atlas/src/main/java/ch/sbb/atlas/api/workflow/BasePersonModel.java
+++ base-atlas/src/main/java/ch/sbb/atlas/api/workflow/BasePersonModel.java
@@ -1,9 +1,11 @@
 package ch.sbb.atlas.api.workflow;
 
 import ch.sbb.atlas.api.AtlasCharacterSetsRegex;
+import ch.sbb.atlas.api.AtlasFieldLengths;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.media.Schema.AccessMode;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,10 +20,14 @@ public abstract class BasePersonModel {
 
   @Schema(description = "Firstname", example = "John")
   @Pattern(regexp = AtlasCharacterSetsRegex.ISO_8859_1)
+  @Size(min = 1, max = AtlasFieldLengths.LENGTH_50)
+//  @JsonDeserialize(using = StringTrimmerDeserializer.class)
   private String firstName;
 
   @Schema(description = "Second", example = "Doe")
   @Pattern(regexp = AtlasCharacterSetsRegex.ISO_8859_1)
+  @Size(min = 1, max = AtlasFieldLengths.LENGTH_50)
+//  @JsonDeserialize(using = StringTrimmerDeserializer.class)
   private String lastName;
 
   @Schema(description = "Object creation date", example = "01.01.2000", accessMode = AccessMode.READ_ONLY)
