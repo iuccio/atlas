eb3e82085463b9f6521017be788e12d63c6fab91
atlas <TechSupport-ATLAS@sbb.ch>
2024-08-28 16:07:06 +0200

    ATLAS-1090: add end point to get swiss SP with geo

---
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/mapper/ServicePointSwissWithGeoMapper.java
@@ -0,0 +1,15 @@
+package ch.sbb.atlas.servicepointdirectory.mapper;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ServicePointSwissWithGeoMapper {
+
+//  public static ServicePointSwissWithGeoModel toModel(ServicePointSwissWithGeoTransfer swissWithGeoTransfer) {
+//    return ServicePointSwissWithGeoModel.builder()
+//        .ids(swissWithGeoTransfer.getId())
+//        .sloid(swissWithGeoTransfer.getSloid())
+//        .build();
+//  }
+
+}
