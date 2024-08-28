3735bfc4ec1ebd21235585703e4f68b9d9bafe36
atlas <TechSupport-ATLAS@sbb.ch>
2024-08-28 19:16:28 +0200

    ATLAS-885 temp commit

--- import-service-point/src/main/java/ch/sbb/importservice/entity/BulkImportRequest.java
+++ import-service-point/src/main/java/ch/sbb/importservice/entity/BulkImportRequest.java
@@ -3,25 +3,39 @@ package ch.sbb.importservice.entity;
 import ch.sbb.atlas.kafka.model.user.admin.ApplicationType;
 import ch.sbb.importservice.model.BusinessObjectType;
 import ch.sbb.importservice.model.ImportType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.experimental.FieldNameConstants;
+import lombok.experimental.SuperBuilder;
 
 import java.util.List;
 
-@NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
-@ToString
-@Builder
+@NoArgsConstructor
+@Data
+@SuperBuilder
+@FieldNameConstants
+@Schema(name = "BulkImportRequest")
 public class BulkImportRequest {
+
+    @NotNull
     private ApplicationType applicationType;
+
+    @NotNull
     private BusinessObjectType objectType;
+
+    @NotNull
     private ImportType importType;
+
     private String inNameOf;
+
     private List<String> emails;
 }
