2a351ddb71024d9bac42caa2f2f6d0cc608a063d
atlas <19825793+iuccio@users.noreply.github.com>
2024-09-02 14:16:53 +0200

    ATLAS-885 fixed backend call

--- import-service-point/src/main/java/ch/sbb/importservice/entity/BulkImportRequest.java
+++ import-service-point/src/main/java/ch/sbb/importservice/entity/BulkImportRequest.java
@@ -1,21 +1,11 @@
 package ch.sbb.importservice.entity;
 
-import ch.sbb.atlas.kafka.model.user.admin.ApplicationType;
-import ch.sbb.importservice.model.BusinessObjectType;
-import ch.sbb.importservice.model.ImportType;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 import lombok.experimental.FieldNameConstants;
 import lombok.experimental.SuperBuilder;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -27,19 +17,8 @@ import java.util.List;
 @Schema(name = "BulkImportRequest")
 public class BulkImportRequest {
 
-    @NotNull
-    private ApplicationType applicationType;
-
-    @NotNull
-    private BusinessObjectType objectType;
-
-    @NotNull
-    private ImportType importType;
-
     private String inNameOf;
 
     private List<String> emails;
 
-    @NotNull
-    private MultipartFile file;
 }
