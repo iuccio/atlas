bc407e7bd77d921b39548b5be36dac26879f3072
atlas <TechSupport-ATLAS@sbb.ch>
2024-09-02 11:54:18 +0200

    ATLAS-2037: wrap up testing

--- import-service-point/src/main/java/ch/sbb/importservice/config/AsyncConfig.java
+++ import-service-point/src/main/java/ch/sbb/importservice/config/AsyncConfig.java
@@ -1,10 +1,12 @@
 package ch.sbb.importservice.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
 @EnableAsync
+@Profile("!integration-test")
 public class AsyncConfig {
 
 }
