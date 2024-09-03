6f13445c5d4bca9d0d9fff5d1c966c1acb1085f6
atlas <TechSupport-ATLAS@sbb.ch>
2024-09-03 15:03:37 +0200

    ATLAS-2037: Ignore additional columns after last

--- base-atlas/src/main/java/ch/sbb/atlas/imports/bulk/AtlasCsvReader.java
+++ base-atlas/src/main/java/ch/sbb/atlas/imports/bulk/AtlasCsvReader.java
@@ -1,6 +1,7 @@
 package ch.sbb.atlas.imports.bulk;
 
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvParser;
 import com.fasterxml.jackson.dataformat.csv.CsvParser.Feature;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import lombok.experimental.UtilityClass;
@@ -8,7 +9,9 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class AtlasCsvReader {
 
-  public static final CsvMapper CSV_MAPPER = new CsvMapper().enable(Feature.EMPTY_STRING_AS_NULL);
+  public static final CsvMapper CSV_MAPPER = new CsvMapper()
+      .enable(Feature.EMPTY_STRING_AS_NULL)
+      .enable(CsvParser.Feature.IGNORE_TRAILING_UNMAPPABLE);
 
   public static final char CSV_COLUMN_SEPARATOR = ';';
   public static final CsvSchema CSV_SCHEMA = CsvSchema.emptySchema()
