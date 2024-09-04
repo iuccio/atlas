7f2d8d1e5403b9b279d76df7c30e3fabb347ed1d
atlas <19825793+iuccio@users.noreply.github.com>
2024-09-04 16:31:40 +0200

    Improve e2e tests

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -49,7 +49,7 @@ module.exports = [{
         "@typescript-eslint/explicit-module-boundary-types": "off",
     },
 }, {
-  files: ["**/*.ts"],
+  files: ["cypress/**/*.ts"],
 
   languageOptions: {
     ecmaVersion: 5,
