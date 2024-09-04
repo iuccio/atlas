38f029f83e81abe911fe167054eb9a0f616decf6
atlas <TechSupport-ATLAS@sbb.ch>
2024-09-04 15:23:45 +0200

    ATLAS-2038: wip: log response for frontend visualisation

---
+++ import-service-point/src/main/java/ch/sbb/importservice/model/BulkImportResult.java
@@ -0,0 +1,20 @@
+package ch.sbb.importservice.model;
+
+import ch.sbb.atlas.imports.bulk.BulkImportLogEntry;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class BulkImportResult {
+
+  private ImportType importType;
+  private BusinessObjectType businessObjectType;
+  private String creator;
+  private LocalDateTime creationDate;
+  private String inNameOf;
+  private List<BulkImportLogEntry> logEntries;
+
+}
