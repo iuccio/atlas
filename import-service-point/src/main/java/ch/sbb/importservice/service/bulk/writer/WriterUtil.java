ad923f456dcc09cdd47dc5fb2846a3f1826d417f
atlas <19825793+iuccio@users.noreply.github.com>
2024-09-04 15:23:45 +0200

    ATLAS-2038: wip: log response for frontend visualisation

--- import-service-point/src/main/java/ch/sbb/importservice/service/bulk/writer/WriterUtil.java
+++ import-service-point/src/main/java/ch/sbb/importservice/service/bulk/writer/WriterUtil.java
@@ -28,6 +28,7 @@ public class WriterUtil {
           .filter(i -> i.getLineNumber() == updateContainer.getLineNumber()).findFirst().orElseThrow();
       updateContainer.setBulkImportLogEntry(BulkImportLogEntry.builder()
           .lineNumber(updateContainer.getLineNumber())
+          /* todo: check http status code of error response to determine new status VersioningNoChanges as "warning" */
           .status(correspondingResult.isSuccess() ? BulkImportStatus.SUCCESS : BulkImportStatus.DATA_EXECUTION_ERROR)
           .errors(correspondingResult.getErrors())
           .build());
