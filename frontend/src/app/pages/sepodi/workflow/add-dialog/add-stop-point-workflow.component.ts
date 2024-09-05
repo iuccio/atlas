266f502c7b0020250d4db66f04666390cc5afa06
atlas <19825793+iuccio@users.noreply.github.com>
2024-09-05 15:24:30 +0200

    ATLAS-2439: Add more improvements suggested in PR comments.

--- frontend/src/app/pages/sepodi/workflow/add-dialog/add-stop-point-workflow.component.ts
+++ frontend/src/app/pages/sepodi/workflow/add-dialog/add-stop-point-workflow.component.ts
@@ -2,7 +2,7 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {StopPointAddWorkflow, StopPointPerson, StopPointWorkflowService,} from '../../../../api';
 import {AddStopPointWorkflowDialogData} from './add-stop-point-workflow-dialog-data';
-import {FormGroup} from '@angular/forms';
+import {FormArray, FormGroup} from '@angular/forms';
 import {ValidationService} from '../../../../core/validation/validation.service';
 import {DetailHelperService} from '../../../../core/detail/detail-helper.service';
 import {NotificationService} from '../../../../core/notification/notification.service';
@@ -10,6 +10,7 @@ import {Router} from '@angular/router';
 import {Pages} from '../../../pages';
 import {UserService} from "../../../../core/auth/user/user.service";
 import {
+  ExaminantFormGroup,
   StopPointWorkflowDetailFormGroup,
   StopPointWorkflowDetailFormGroupBuilder
 } from "../detail-page/detail-form/stop-point-workflow-detail-form-group";
@@ -38,6 +39,20 @@ export class AddStopPointWorkflowComponent implements OnInit {
     this.form.controls.designationOfficial.setValue(this.data.stopPoint.designationOfficial)
   }
 
+  transformExaminants(examinants: FormArray<FormGroup<ExaminantFormGroup>>): StopPointPerson[] {
+    return examinants.controls
+      .filter(control => !control.disabled)
+      .map(examinant => {
+        if (examinant.controls.firstName.value === '') {
+          examinant.controls.firstName.setValue(null);
+        }
+        if (examinant.controls.lastName.value === '') {
+          examinant.controls.lastName.setValue(null);
+        }
+        return examinant.value as StopPointPerson;
+      });
+  }
+
   addWorkflow() {
     ValidationService.validateForm(this.form);
     if (this.form.valid) {
@@ -47,13 +62,7 @@ export class AddStopPointWorkflowComponent implements OnInit {
         sloid: this.data.stopPoint.sloid!,
         workflowComment: this.form.controls.workflowComment.value!,
         ccEmails: this.form.controls.ccEmails.value!,
-        examinants: this.form.controls.examinants.controls
-          .filter(control => !control.disabled)
-          .map(examinant => {
-            if (!examinant.value.firstName) examinant.value.firstName = null;
-            if (!examinant.value.lastName) examinant.value.lastName = null;
-            return examinant.value as StopPointPerson;
-          })
+        examinants: this.transformExaminants(this.form.controls.examinants as FormArray<FormGroup>)
       }
       this.form.disable();
       this.stopPointWorkflowService.addStopPointWorkflow(workflow).subscribe((createdWorkflow) => {
