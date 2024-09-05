0b3e0f16d6f0012c610f20cd7674b6581b5ca69a
atlas <TechSupport-ATLAS@sbb.ch>
2024-09-05 13:50:50 +0200

    ATLAS-885 add filter to search for user in atlas

--- frontend/src/app/api/model/pageableObject.ts
+++ frontend/src/app/api/model/pageableObject.ts
@@ -12,7 +12,7 @@
 import { SortObject } from './sortObject';
 
 
-export interface PageableObject {
+export interface PageableObject { 
     offset?: number;
     sort?: Array<SortObject>;
     unpaged?: boolean;
