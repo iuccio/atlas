3c464a461667fb246d42f90e8f1d591152394914
atlas <TechSupport-ATLAS@sbb.ch>
2024-09-11 11:41:33 +0200

    ATLAS-885 added test coverage

---
+++ user-administration/src/test/java/ch/sbb/atlas/user/administration/controller/UserInformationApiControllerTest.java
@@ -0,0 +1,120 @@
+package ch.sbb.atlas.user.administration.controller;
+
+import ch.sbb.atlas.api.user.administration.UserModel;
+import ch.sbb.atlas.api.user.administration.enumeration.UserAccountStatus;
+import ch.sbb.atlas.kafka.model.user.admin.ApplicationType;
+import ch.sbb.atlas.model.controller.BaseControllerApiTest;
+import ch.sbb.atlas.user.administration.service.GraphApiService;
+import ch.sbb.atlas.user.administration.service.UserAdministrationService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+class UserInformationApiControllerTest extends BaseControllerApiTest {
+
+    @Autowired
+    protected MockMvc mvc;
+
+    @MockBean
+    private GraphApiService graphApiService;
+
+    @MockBean
+    private UserAdministrationService userAdministrationService;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+
+    @AfterEach
+    void tearDown() {}
+
+
+    @Test
+    void shoudSearchUserInAD() throws Exception {
+        UserModel userModel = UserModel.builder()
+                        .sbbUserId("***REMOVED***")
+                        .firstName("***REMOVED***")
+                        .lastName("***REMOVED***")
+                        .mail("***REMOVED***")
+                        .accountStatus(UserAccountStatus.ACTIVE)
+                        .build();
+
+        UserModel userModel2 = UserModel.builder()
+                .sbbUserId("u239097")
+                .firstName("hans")
+                .lastName("müller")
+                .mail("hans.müller@sbb.ch")
+                .accountStatus(UserAccountStatus.ACTIVE)
+                .build();
+
+
+        when(graphApiService.searchUsers("testQuery")).thenReturn(List.of(userModel, userModel2));
+
+        mvc.perform(get("/v1/search")
+                .param("searchQuery", "testQuery")
+                .param("searchInAtlas", "false"))
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(status().isOk());
+
+        verify(graphApiService, times(1)).searchUsers("testQuery");
+        verifyNoInteractions(userAdministrationService);
+    }
+
+    @Test
+    void shouldSearchUserInAtlas() throws Exception {
+        UserModel userModel = UserModel.builder()
+                .sbbUserId("***REMOVED***")
+                .firstName("***REMOVED***")
+                .lastName("***REMOVED***")
+                .mail("***REMOVED***")
+                .accountStatus(UserAccountStatus.ACTIVE)
+                .build();
+
+        UserModel userModel2 = UserModel.builder()
+                .sbbUserId("u239097")
+                .firstName("hans")
+                .lastName("müller")
+                .mail("hans.müller@sbb.ch")
+                .accountStatus(UserAccountStatus.ACTIVE)
+                .build();
+
+        List<UserModel> userModels = Arrays.asList(userModel, userModel2);
+        when(graphApiService.searchUsers("testQuery")).thenReturn(userModels);
+
+        List<UserModel> filteredUsers = Collections.singletonList(userModel);
+
+        when(userAdministrationService.filterForUserInAtlas(userModels, ApplicationType.SEPODI))
+                .thenReturn(filteredUsers);
+
+
+        mvc.perform(get("/v1/search")
+                        .param("searchQuery", "testQuery")
+                        .param("searchInAtlas", "true")
+                        .param("applicationType", "SEPODI"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(1))
+                .andExpect(jsonPath("$[0].sbbUserId").value("***REMOVED***"));
+
+        verify(graphApiService, times(1)).searchUsers("testQuery");
+        verify(userAdministrationService, times(1)).filterForUserInAtlas(userModels, ApplicationType.SEPODI);
+
+    }
+}
