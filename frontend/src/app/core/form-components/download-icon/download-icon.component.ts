a69dda357ee9e9efb15d22ba78806e9c9c8519df
atlas <19825793+iuccio@users.noreply.github.com>
2024-09-11 22:54:38 +0200

    ATLAS-2027: Add more imporvements.

--- frontend/src/app/core/form-components/download-icon/download-icon.component.ts
+++ frontend/src/app/core/form-components/download-icon/download-icon.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'download-icon',
@@ -6,8 +6,12 @@ import { Component, Input } from '@angular/core';
 })
 export class DownloadIconComponent {
   @Input() readonly!: boolean;
+  @Input() disabled!: boolean;
 
   get fill(): string {
+    if (this.disabled) {
+      return '#d3d3d3';
+    }
     return this.readonly ? '#2B2B2B' : '#adb5bd';
   }
 }
