7bfb64b1d92eed1d2903783ae8cb66d8aca7f6c3
atlas <TechSupport-ATLAS@sbb.ch>
2024-09-11 17:46:10 +0200

    ATLAS-2038: wip: design, todo: paging, error-translations

--- frontend/src/app/pages/bulk-import/bulk-import.module.ts
+++ frontend/src/app/pages/bulk-import/bulk-import.module.ts
@@ -6,9 +6,10 @@ import { BulkImportRoutingModule } from './bulk-import-routing.module';
 import { FormModule } from '../../core/module/form.module';
 import { BulkImportLogComponent } from './log/bulk-import-log.component';
 import {UserAdministrationModule} from "../user-administration/user-administration.module";
+import { ParamsForTranslationPipe } from './log/params-for-translation.pipe';
 
 @NgModule({
   declarations: [BulkImportOverviewComponent, BulkImportLogComponent],
-    imports: [CoreModule, ColorModule, BulkImportRoutingModule, FormModule, UserAdministrationModule],
+    imports: [CoreModule, ColorModule, BulkImportRoutingModule, FormModule, UserAdministrationModule,ParamsForTranslationPipe],
 })
 export class BulkImportModule {}
---
+++ frontend/src/app/pages/bulk-import/log/params-for-translation.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ParamsForTranslationPipe } from './params-for-translation.pipe';
+
+describe('ParamsForTranslationPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ParamsForTranslationPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
