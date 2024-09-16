53b10838a68aba26a79af9fd7fd47486ce52c39d
atlas <19825793+iuccio@users.noreply.github.com>
2024-09-16 16:55:47 +0200

    ATLAS-2450: generate example template from csv model

---
+++ import-service-point/src/main/java/ch/sbb/importservice/service/bulk/template/BulkImportTemplateGenerator.java
@@ -0,0 +1,42 @@
+package ch.sbb.importservice.service.bulk.template;
+
+import static ch.sbb.importservice.service.bulk.template.ServicePointTemplateGenerator.getServicePointCsvTemplate;
+
+import ch.sbb.atlas.export.AtlasCsvMapper;
+import ch.sbb.atlas.export.CsvExportWriter;
+import ch.sbb.atlas.kafka.model.user.admin.ApplicationType;
+import ch.sbb.importservice.exception.BulkImportNotImplementedException;
+import ch.sbb.importservice.model.BulkImportConfig;
+import ch.sbb.importservice.model.BusinessObjectType;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import java.io.File;
+import java.util.List;
+import java.util.Objects;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class BulkImportTemplateGenerator {
+
+  public static final String CSV_EXTENSION = ".csv";
+
+  public static File generateCsvTemplate(BulkImportConfig importConfig) {
+    Object example = bulkImportExample(importConfig);
+    File csvFile = new File(importConfig.getTemplateFileName());
+    ObjectWriter objectWriter = new AtlasCsvMapper(example.getClass()).getObjectWriter();
+    return CsvExportWriter.writeToFile(csvFile, List.of(example), objectWriter);
+  }
+
+  private Object bulkImportExample(BulkImportConfig importConfig) {
+
+    BulkImportNotImplementedException bulkImportNotImplementedException = new BulkImportNotImplementedException(importConfig);
+
+    if (Objects.requireNonNull(importConfig.getApplication()) == ApplicationType.SEPODI) {
+      if (Objects.requireNonNull(importConfig.getObjectType()) == BusinessObjectType.SERVICE_POINT) {
+        return getServicePointCsvTemplate(importConfig);
+      }
+      throw bulkImportNotImplementedException;
+    }
+    throw bulkImportNotImplementedException;
+  }
+
+}
