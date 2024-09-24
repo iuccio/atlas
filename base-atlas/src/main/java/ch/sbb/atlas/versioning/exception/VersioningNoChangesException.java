b741d561fe99e08af5e83cf22c4ac6c9533905cf
atlas <19825793+iuccio@users.noreply.github.com>
2024-09-24 11:10:42 +0200

    ATLAS-2452 format code

--- base-atlas/src/main/java/ch/sbb/atlas/versioning/exception/VersioningNoChangesException.java
+++ base-atlas/src/main/java/ch/sbb/atlas/versioning/exception/VersioningNoChangesException.java
@@ -12,23 +12,24 @@ import static ch.sbb.atlas.api.model.ErrorResponse.DisplayInfo.builder;
 
 public class VersioningNoChangesException extends AtlasException {
 
-  private static final String NO_ENTITIES_WERE_MODIFIED_MESSAGE = "No entities were modified after versioning execution.";
+    private static final String NO_ENTITIES_WERE_MODIFIED_MESSAGE = "No entities were modified after versioning execution.";
 
-  @Override
-  public ErrorResponse getErrorResponse() {
-    return ErrorResponse.builder()
-            .status(HttpStatus.BAD_REQUEST.value())
-            .error(NO_ENTITIES_WERE_MODIFIED_MESSAGE)
-            .message(NO_ENTITIES_WERE_MODIFIED_MESSAGE)
-            .details(new TreeSet<>(getErrorDetails()))
-            .build();
-  }
-  private List<Detail> getErrorDetails() {
-    return List.of(Detail.builder()
-            .message(NO_ENTITIES_WERE_MODIFIED_MESSAGE)
-            .displayInfo(builder()
-                    .code("ERROR.WARNING.VERSIONING_NO_CHANGES")
-                    .build())
-            .build());
-  }
+    @Override
+    public ErrorResponse getErrorResponse() {
+        return ErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .error(NO_ENTITIES_WERE_MODIFIED_MESSAGE)
+                .message(NO_ENTITIES_WERE_MODIFIED_MESSAGE)
+                .details(new TreeSet<>(getErrorDetails()))
+                .build();
+    }
+
+    private List<Detail> getErrorDetails() {
+        return List.of(Detail.builder()
+                .message(NO_ENTITIES_WERE_MODIFIED_MESSAGE)
+                .displayInfo(builder()
+                        .code("ERROR.WARNING.VERSIONING_NO_CHANGES")
+                        .build())
+                .build());
+    }
 }
