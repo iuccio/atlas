a34e7b657908cf4b25b8c0f17ba471aa3e82a40f
atlas <19825793+iuccio@users.noreply.github.com>
2024-09-25 10:30:26 +0200

    ATLAS-2471 added valid and requestbody annotation to api

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/api/GeoReferenceApiV1.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/api/GeoReferenceApiV1.java
@@ -5,11 +5,13 @@ import ch.sbb.atlas.configuration.Role;
 import ch.sbb.atlas.geoupdate.job.model.GeoUpdateItemResultModel;
 import ch.sbb.atlas.servicepoint.CoordinatePair;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -18,7 +20,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public interface GeoReferenceApiV1 {
 
   @GetMapping("/reverse-geocode")
-  GeoReference getLocationInformation(CoordinatePair coordinatePair);
+  GeoReference getLocationInformation(@Valid @RequestBody CoordinatePair coordinatePair);
 
   @Secured(Role.SECURED_FOR_ATLAS_ADMIN)
   @ResponseStatus(HttpStatus.OK)
