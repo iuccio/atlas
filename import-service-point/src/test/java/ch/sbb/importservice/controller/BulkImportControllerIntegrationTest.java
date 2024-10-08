2bceeb1755442184a6ba9fd2529dcc62e693acf5
atlas <19825793+iuccio@users.noreply.github.com>
2024-10-08 11:59:04 +0200

    ATLAS-2393: Adjustments after rebase.

--- import-service-point/src/test/java/ch/sbb/importservice/controller/BulkImportControllerIntegrationTest.java
+++ import-service-point/src/test/java/ch/sbb/importservice/controller/BulkImportControllerIntegrationTest.java
@@ -21,6 +21,7 @@ import ch.sbb.atlas.kafka.model.user.admin.ApplicationType;
 import ch.sbb.atlas.model.controller.BaseControllerApiTest;
 import ch.sbb.importservice.ImportFiles;
 import ch.sbb.importservice.client.ServicePointBulkImportClient;
+import ch.sbb.importservice.client.TrafficPointBulkImportClient;
 import ch.sbb.importservice.entity.BulkImport;
 import ch.sbb.importservice.model.BulkImportConfig;
 import ch.sbb.importservice.model.BulkImportRequest;
@@ -36,7 +37,6 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -48,7 +48,7 @@ import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-class BulkImportControllerITest extends BaseControllerApiTest {
+class BulkImportControllerIntegrationTest extends BaseControllerApiTest {
 
   @Autowired
   private BulkImportRepository bulkImportRepository;
@@ -57,18 +57,12 @@ class BulkImportControllerITest extends BaseControllerApiTest {
   private ServicePointBulkImportClient servicePointBulkImportClient;
 
   @MockBean
+  private TrafficPointBulkImportClient trafficPointBulkImportClient;
+
+  @MockBean
   private AmazonService amazonService;
 
   private String todaysDirectory;
-
-  @BeforeEach
-  void setUp() {
-    todaysDirectory = "e123456/" + DateTimeFormatter.ofPattern(AtlasApiConstants.DATE_FORMAT_PATTERN).format(LocalDate.now())
-        + "/SEPODI/SERVICE_POINT/UPDATE";
-    when(amazonService.putFile(eq(AmazonBucket.BULK_IMPORT), any(File.class), anyString()))
-        .thenAnswer(i -> URI.create("https://atlas-bulk-import-dev-dev.s3.eu-central-1.amazonaws.com/" +
-            todaysDirectory + "/" + i.getArgument(1, File.class).getName()).toURL());
-  }
 
   @AfterEach
   void tearDown() {
@@ -77,6 +71,11 @@ class BulkImportControllerITest extends BaseControllerApiTest {
 
   @Test
   void shouldAcceptGenericBulkImportWithFile() throws Exception {
+    todaysDirectory = "e123456/" + DateTimeFormatter.ofPattern(AtlasApiConstants.DATE_FORMAT_PATTERN).format(LocalDate.now())
+        + "/SEPODI/SERVICE_POINT/UPDATE";
+    when(amazonService.putFile(eq(AmazonBucket.BULK_IMPORT), any(File.class), anyString()))
+        .thenAnswer(i -> URI.create("https://atlas-bulk-import-dev-dev.s3.eu-central-1.amazonaws.com/" +
+            todaysDirectory + "/" + i.getArgument(1, File.class).getName()).toURL());
     when(servicePointBulkImportClient.bulkImportUpdate(any())).thenReturn(
         List.of(BulkImportItemExecutionResult.builder()
             .lineNumber(1)
@@ -113,6 +112,50 @@ class BulkImportControllerITest extends BaseControllerApiTest {
     assertThat(bulkImport.getImportFileUrl()).isEqualTo(todaysDirectory + "/service-point-update.csv");
   }
 
+  @Test
+  void shouldAcceptTrafficPointUpdateBulkImportWithFile() throws Exception {
+    todaysDirectory = "e123456/" + DateTimeFormatter.ofPattern(AtlasApiConstants.DATE_FORMAT_PATTERN).format(LocalDate.now())
+        + "/SEPODI/TRAFFIC_POINT/UPDATE";
+    when(amazonService.putFile(eq(AmazonBucket.BULK_IMPORT), any(File.class), anyString()))
+        .thenAnswer(i -> URI.create("https://atlas-bulk-import-dev-dev.s3.eu-central-1.amazonaws.com/" +
+            todaysDirectory + "/" + i.getArgument(1, File.class).getName()).toURL());
+    when(trafficPointBulkImportClient.bulkImportUpdate(any())).thenReturn(
+        List.of(BulkImportItemExecutionResult.builder()
+            .lineNumber(1)
+            .build()));
+
+    File file = ImportFiles.getFileByPath("import-files/valid/traffic-point-update.csv");
+
+    BulkImportRequest bulkImportRequest = BulkImportRequest.builder()
+        .applicationType(ApplicationType.SEPODI)
+        .objectType(BusinessObjectType.TRAFFIC_POINT)
+        .importType(ImportType.UPDATE)
+        .inNameOf("Test Name")
+        .emails(List.of("test@example.com", "techsupport@atlas-sbb.ch"))
+        .build();
+
+    MockMultipartFile mockBulkImportRequest = new MockMultipartFile(
+        "bulkImportRequest",
+        "",
+        MediaType.APPLICATION_JSON_VALUE,
+        new ObjectMapper().writeValueAsBytes(bulkImportRequest));
+
+    mvc.perform(multipart("/v1/import/bulk")
+            .file(new MockMultipartFile("file", "traffic-point-update.csv", CSV_CONTENT_TYPE,
+                Files.readAllBytes(file.toPath())))
+            .file(mockBulkImportRequest)
+            .contentType(MediaType.MULTIPART_FORM_DATA_VALUE))
+        .andExpect(status().isAccepted());
+
+    verify(amazonService, times(2)).putFile(eq(AmazonBucket.BULK_IMPORT), any(File.class), eq(todaysDirectory));
+    verify(trafficPointBulkImportClient, atLeastOnce()).bulkImportUpdate(any());
+
+    assertThat(bulkImportRepository.count()).isEqualTo(1);
+    BulkImport bulkImport = bulkImportRepository.findAll().getFirst();
+    assertThat(bulkImport.getId()).isNotNull();
+    assertThat(bulkImport.getImportFileUrl()).isEqualTo(todaysDirectory + "/traffic-point-update.csv");
+  }
+
   @ParameterizedTest
   @MethodSource("getArgumentsImplementedTemplates")
   void shouldDownloadTemplateSuccessfully(BulkImportConfig importConfig) throws Exception {
@@ -146,7 +189,6 @@ class BulkImportControllerITest extends BaseControllerApiTest {
         Arguments.of(new BulkImportConfig(ApplicationType.SEPODI, BusinessObjectType.SERVICE_POINT, ImportType.TERMINATE)),
         Arguments.of(new BulkImportConfig(ApplicationType.SEPODI, BusinessObjectType.SERVICE_POINT, ImportType.CREATE)),
         Arguments.of(new BulkImportConfig(ApplicationType.SEPODI, BusinessObjectType.TRAFFIC_POINT, ImportType.CREATE)),
-        Arguments.of(new BulkImportConfig(ApplicationType.SEPODI, BusinessObjectType.TRAFFIC_POINT, ImportType.UPDATE)),
         Arguments.of(new BulkImportConfig(ApplicationType.SEPODI, BusinessObjectType.TRAFFIC_POINT, ImportType.TERMINATE)),
         Arguments.of(new BulkImportConfig(ApplicationType.SEPODI, BusinessObjectType.LOADING_POINT, ImportType.CREATE)),
         Arguments.of(new BulkImportConfig(ApplicationType.SEPODI, BusinessObjectType.LOADING_POINT, ImportType.UPDATE)),
@@ -156,7 +198,8 @@ class BulkImportControllerITest extends BaseControllerApiTest {
 
   static Stream<Arguments> getArgumentsImplementedTemplates() {
     return Stream.of(
-        Arguments.of(new BulkImportConfig(ApplicationType.SEPODI, BusinessObjectType.SERVICE_POINT, ImportType.UPDATE))
+        Arguments.of(new BulkImportConfig(ApplicationType.SEPODI, BusinessObjectType.SERVICE_POINT, ImportType.UPDATE)),
+        Arguments.of(new BulkImportConfig(ApplicationType.SEPODI, BusinessObjectType.TRAFFIC_POINT, ImportType.UPDATE))
     );
   }
 
