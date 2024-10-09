7335cf3201f335cfd51ef2691cac087618d93a75
atlas <19825793+iuccio@users.noreply.github.com>
2024-10-09 11:54:09 +0200

    ATLAS-2501 added exception test

---
+++ base-atlas/src/test/java/ch/sbb/atlas/exception/DateValidationExceptionTest.java
@@ -0,0 +1,20 @@
+package ch.sbb.atlas.exception;
+
+import ch.sbb.atlas.api.model.ErrorResponse;
+import ch.sbb.atlas.versioning.exception.DateValidationException;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DateValidationExceptionTest {
+    @Test
+    void shouldDisplayErrorMessage(){
+        // given
+        DateValidationException exception = new DateValidationException("ValidTo cannot be after 31.12.9999.");
+        // when & then
+        ErrorResponse errorResponse = exception.getErrorResponse();
+        assertThat(errorResponse.getStatus()).isEqualTo(400);
+        assertThat(errorResponse.getMessage()).isEqualTo("ValidTo cannot be after 31.12.9999.");
+        assertThat(errorResponse.getError()).isEqualTo("ValidTo cannot be after 31.12.9999.");
+    }
+}
