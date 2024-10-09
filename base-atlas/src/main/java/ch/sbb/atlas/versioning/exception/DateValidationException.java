5f85436ff4cfb9e11933657973ea5634604f575a
atlas <19825793+iuccio@users.noreply.github.com>
2024-10-09 09:02:01 +0200

    ATLAS-2501 changed from runtime to atlas exception

--- base-atlas/src/main/java/ch/sbb/atlas/versioning/exception/DateValidationException.java
+++ base-atlas/src/main/java/ch/sbb/atlas/versioning/exception/DateValidationException.java
@@ -1,10 +1,40 @@
 package ch.sbb.atlas.versioning.exception;
 
-public class DateValidationException extends RuntimeException {
+import ch.sbb.atlas.api.model.ErrorResponse;
+import ch.sbb.atlas.api.model.ErrorResponse.Detail;
+import ch.sbb.atlas.model.exception.AtlasException;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
-  private static final long serialVersionUID = 1L;
+import java.util.List;
+import java.util.TreeSet;
 
-  public DateValidationException(String message) {
-    super(message);
+import static ch.sbb.atlas.api.model.ErrorResponse.DisplayInfo.builder;
+
+@RequiredArgsConstructor
+@Getter
+public class DateValidationException extends AtlasException {
+
+  private final String message;
+
+
+  @Override
+  public ErrorResponse getErrorResponse() {
+    return ErrorResponse.builder()
+            .status(HttpStatus.BAD_REQUEST.value())
+            .message(message)
+            .error(message)
+            .details(new TreeSet<>(getErrorDetails()))
+            .build();
+  }
+
+  private List<Detail> getErrorDetails() {
+    return List.of(Detail.builder()
+            .message(message)
+            .displayInfo(builder()
+                    .code("VALIDATION.DATE_RANGE_ERROR")
+                    .build())
+            .build());
   }
 }
