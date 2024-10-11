aa2d6e29c966c99cc06b4a69b251523a9da12cc5
atlas <19825793+iuccio@users.noreply.github.com>
2024-10-11 10:33:41 +0200

    ATLAS-1896: Add changes according to PR comments.

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/exception/TerminationNotAllowedValidToNotWithinLastVersionRangeException.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/exception/TerminationNotAllowedValidToNotWithinLastVersionRangeException.java
@@ -1,8 +1,12 @@
 package ch.sbb.atlas.servicepointdirectory.exception;
 
 import ch.sbb.atlas.api.model.ErrorResponse;
+import ch.sbb.atlas.api.model.ErrorResponse.Detail;
+import ch.sbb.atlas.api.model.ErrorResponse.DisplayInfo;
 import ch.sbb.atlas.model.exception.AtlasException;
 import java.time.LocalDate;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
@@ -18,10 +22,29 @@ public class TerminationNotAllowedValidToNotWithinLastVersionRangeException exte
   public ErrorResponse getErrorResponse() {
     return ErrorResponse.builder()
         .status(HttpStatus.FORBIDDEN.value())
-        .message("Termination not allowed for sloid " + sloid + " since the date range for the last version is from "
-            + validFromCurrent + " until " + validToCurrent + ". "
-            + "And requested validTo value " + requestedValidTo + " is not within the range.")
+        .message(getErrorMessage())
         .error("Termination not allowed")
+        .details(getErrorDetails())
         .build();
   }
+
+  private String getErrorMessage() {
+    return "Termination not allowed for sloid " + sloid + " since the date range for the last version is from "
+        + validFromCurrent + " until " + validToCurrent + ". "
+        + "And requested validTo value " + requestedValidTo + " is not within the range.";
+  }
+
+  private SortedSet<Detail> getErrorDetails() {
+    TreeSet<Detail> errorDetails = new TreeSet<>();
+    errorDetails.add(Detail.builder()
+        .field("termination")
+        .message("Termination not allowed for non latest version.")
+        .displayInfo(DisplayInfo.builder()
+            .code("SEPODI.SERVICE_POINTS.TERMINATION_NOT_ALLOWED_FOR_NON_LATEST_VERSION")
+            .with("sloid", sloid)
+            .build())
+        .build());
+    return errorDetails;
+  }
+
 }
