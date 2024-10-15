fd8eedf59752d764dda1ad7439cc726aed8110d7
atlas <TechSupport-ATLAS@sbb.ch>
2024-10-15 11:11:00 +0200

    ATLAS improved pr

--- frontend/src/app/core/navigation-sepodi-prm/navigation-sepodi-prm.component.ts
+++ frontend/src/app/core/navigation-sepodi-prm/navigation-sepodi-prm.component.ts
@@ -1,6 +1,11 @@
 import {Component, Input} from '@angular/core';
 import {Router} from "@angular/router";
 
+export enum NavigationToIconType {
+  PRM = 'prm',
+  SEPODI = 'sepodi'
+}
+
 @Component({
   selector: 'app-navigation-sepodi-prm',
   templateUrl: './navigation-sepodi-prm.component.html'
@@ -8,8 +13,7 @@ import {Router} from "@angular/router";
 export class NavigationSepodiPrmComponent {
 
   @Input() url!: string[];
-  @Input() showSepodiIcon!: boolean;
-  @Input() showPrmIcon!: boolean;
+  @Input() icon!: NavigationToIconType;
 
   constructor(private router: Router) {}
 
--- frontend/src/app/pages/sepodi/traffic-point-elements/traffic-point-elements-detail.component.ts
+++ frontend/src/app/pages/sepodi/traffic-point-elements/traffic-point-elements-detail.component.ts
@@ -23,6 +23,7 @@ import {DetailFormComponent} from '../../../core/leave-guard/leave-dirty-form-gu
 import {GeographyFormGroup, GeographyFormGroupBuilder} from '../geography/geography-form-group';
 import {ValidityService} from "../validity/validity.service";
 import {PRM_DETAIL_TAB_LINK} from "../../prm/tabs/relation/tab/detail-with-relation-tab.component";
+import {NavigationToIconType} from "../../../core/navigation-sepodi-prm/navigation-sepodi-prm.component";
 
 interface AreaOption {
   sloid: string | undefined;
@@ -41,6 +42,7 @@ const NUMBER_COLONS_AREA = 0;
 export class TrafficPointElementsDetailComponent implements OnInit, OnDestroy, DetailFormComponent {
   readonly extractSloid = (option: AreaOption) => option.sloid;
   readonly displayExtractor = (option: AreaOption) => option.displayText;
+  readonly NavigationToIconType = NavigationToIconType;
 
   trafficPointVersions!: ReadTrafficPointElementVersion[];
   selectedVersion!: ReadTrafficPointElementVersion;
