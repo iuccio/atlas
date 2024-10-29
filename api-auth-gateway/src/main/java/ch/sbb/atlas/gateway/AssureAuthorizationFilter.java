f0915726afebe9fc9c455d8f3be237b4f2a40a23
atlas <TechSupport-ATLAS@sbb.ch>
2024-10-29 12:27:51 +0100

    Add functionality test

--- api-auth-gateway/src/main/java/ch/sbb/atlas/gateway/AssureAuthorizationFilter.java
+++ api-auth-gateway/src/main/java/ch/sbb/atlas/gateway/AssureAuthorizationFilter.java
@@ -5,13 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
-import org.springframework.security.oauth2.client.AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
-import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Mono;
 
 /**
  * Required dependencies:
@@ -29,12 +23,7 @@ import reactor.core.publisher.Mono;
 @Slf4j
 public class AssureAuthorizationFilter extends AbstractGatewayFilterFactory<Object> {
 
-  private final ReactiveClientRegistrationRepository clientRegistrations;
-  private final ReactiveOAuth2AuthorizedClientService clientService;
-
-  public GatewayFilter apply() {
-    return apply((Object) null);
-  }
+  private final TokenService tokenService;
 
   @Override
   public GatewayFilter apply(Object config) {
@@ -45,7 +34,7 @@ public class AssureAuthorizationFilter extends AbstractGatewayFilterFactory<Obje
    * GatewayFilter to add an Authorization Header, if currently not set
    */
   private GatewayFilter addAccessTokenIfNotPresent() {
-    return (exchange, chain) -> getClientCredentialAccessToken().map(accessToken -> {
+    return (exchange, chain) -> tokenService.getClientCredentialAccessToken().map(accessToken -> {
       if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
         exchange.getRequest()
                 .mutate()
@@ -54,40 +43,6 @@ public class AssureAuthorizationFilter extends AbstractGatewayFilterFactory<Obje
       }
       return exchange;
     }).defaultIfEmpty(exchange).flatMap(chain::filter);
-  }
-
-  private Mono<String> getClientCredentialAccessToken() {
-    ReactiveOAuth2AuthorizedClientManager manager = new AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(
-        clientRegistrations, clientService);
-    return manager.authorize(getAuthorizateRequest())
-                  .map(oAuth2AuthorizedClient -> oAuth2AuthorizedClient.getAccessToken()
-                                                                       .getTokenValue());
-  }
-
-  /**
-   * OAuth2AuthorizeRequest to execute.
-   * Configure your application with spring in application.yml as follows:
-   *
-   * spring:
-   *  security:
-   *    oauth2:
-   *      client:
-   *        registration:
-   *          sbb:
-   *            client-id: 39ff0746-6345-4204-afd3-3e114e5ab715
-   *            client-secret: super-secret-text
-   *            authorization-grant-type: client_credentials
-   *            scope: api://87e6e634-6ba1-4e7a-869d-3348b4c3eafc/.default
-   *         provider:
-   *          sbb:
-   *            token-uri: https://login.microsoftonline.com/2cda5d11-f0ac-46b3-967d-af1b2e1bd01a/oauth2/v2.0/token
-   *
-   * The registrationId has to match the path in your settings.
-   */
-  private static OAuth2AuthorizeRequest getAuthorizateRequest() {
-    return OAuth2AuthorizeRequest.withClientRegistrationId("sbb")
-                                 .principal("unauthenticated")
-                                 .build();
   }
 
 }
