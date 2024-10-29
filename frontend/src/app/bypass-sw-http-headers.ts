b0e07e2eda1f75c653641fb115e9082176b6b99c
atlas <19825793+iuccio@users.noreply.github.com>
2024-10-29 10:24:17 +0100

    Revert "Try with sw bypass"
    This reverts commit e956456f418f2cab8f216b1a697bde64d75052c4.

--- frontend/src/app/bypass-sw-http-headers.ts
+++
@@ -1,7 +0,0 @@
-import {HttpEvent, HttpHandlerFn, HttpRequest} from "@angular/common/http";
-import {Observable} from "rxjs";
-
-export function serviceWorkerBypassHeaders(request: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
-  const newRequest = request.clone({headers: request.headers.append('ngsw-bypass', 'true')});
-  return next(newRequest);
-}
