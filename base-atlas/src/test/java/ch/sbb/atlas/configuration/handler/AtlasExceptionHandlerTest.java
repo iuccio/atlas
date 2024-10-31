a01f5dedcf105df34dda909e684e1c1c0c9f75d4
atlas <TechSupport-ATLAS@sbb.ch>
2024-10-31 14:32:07 +0100

    ATLAS-2518: Revert Exception Handler change

--- base-atlas/src/test/java/ch/sbb/atlas/configuration/handler/AtlasExceptionHandlerTest.java
+++ base-atlas/src/test/java/ch/sbb/atlas/configuration/handler/AtlasExceptionHandlerTest.java
@@ -12,7 +12,6 @@ import ch.sbb.atlas.export.enumeration.ExportType;
 import ch.sbb.atlas.model.exception.SloidNotFoundException;
 import ch.sbb.atlas.servicepoint.enumeration.StopPointType;
 import ch.sbb.atlas.versioning.exception.VersioningNoChangesException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
@@ -34,7 +33,7 @@ import org.springframework.web.multipart.support.MissingServletRequestPartExcept
 
 public class AtlasExceptionHandlerTest {
 
-  private final AtlasExceptionHandler atlasExceptionHandler = new AtlasExceptionHandler(new ObjectMapper());
+  private final AtlasExceptionHandler atlasExceptionHandler = new AtlasExceptionHandler();
 
   @Test
   void shouldConvertMethodArgumentExceptionToErrorResponse() {
