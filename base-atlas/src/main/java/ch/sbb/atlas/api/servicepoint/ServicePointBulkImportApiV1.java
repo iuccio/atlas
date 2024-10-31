39a94b8662a3bf2fea0901e21c2f579a8308cdff
atlas <19825793+iuccio@users.noreply.github.com>
2024-10-31 10:57:00 +0100

    ATLAS-2518: Avoid checking bulkimport permissions in itemwriter per rest call

--- base-atlas/src/main/java/ch/sbb/atlas/api/servicepoint/ServicePointBulkImportApiV1.java
+++ base-atlas/src/main/java/ch/sbb/atlas/api/servicepoint/ServicePointBulkImportApiV1.java
@@ -4,11 +4,9 @@ import ch.sbb.atlas.imports.BulkImportItemExecutionResult;
 import ch.sbb.atlas.imports.bulk.BulkImportUpdateContainer;
 import ch.sbb.atlas.imports.bulk.ServicePointUpdateCsvModel;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.security.access.prepost.PreAuthorize;
+import java.util.List;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
-import java.util.List;
 
 @Tag(name = "Service Points Bulk Import")
 public interface ServicePointBulkImportApiV1 {
@@ -16,7 +14,5 @@ public interface ServicePointBulkImportApiV1 {
   String BASEPATH = "v1/service-points/bulk-import";
 
   @PostMapping(value = BASEPATH + "/update")
-  @PreAuthorize("@bulkImportUserAdministrationService.hasPermissionsForBulkImport(T(ch.sbb.atlas.kafka.model.user.admin"
-          + ".ApplicationType).SEPODI)")
   List<BulkImportItemExecutionResult> bulkImportUpdate(@RequestBody List<BulkImportUpdateContainer<ServicePointUpdateCsvModel>> bulkImportContainers);
 }
