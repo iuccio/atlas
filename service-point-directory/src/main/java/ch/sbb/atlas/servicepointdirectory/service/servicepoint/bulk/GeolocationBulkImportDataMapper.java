4121debc3a2fdc2741b30797a1275b2affbe8afa
atlas <TechSupport-ATLAS@sbb.ch>
2024-10-31 15:29:11 +0100

    ATLAS-2529: round coordinates on import

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/service/servicepoint/bulk/GeolocationBulkImportDataMapper.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/service/servicepoint/bulk/GeolocationBulkImportDataMapper.java
@@ -1,9 +1,16 @@
 package ch.sbb.atlas.servicepointdirectory.service.servicepoint.bulk;
 
+import static ch.sbb.atlas.api.AtlasApiConstants.ATLAS_LV_MAX_DIGITS;
+import static ch.sbb.atlas.api.AtlasApiConstants.ATLAS_WGS84_MAX_DIGITS;
+
 import ch.sbb.atlas.api.servicepoint.GeolocationBaseCreateModel;
-import ch.sbb.atlas.imports.bulk.UpdateGeolocationModel;
+import ch.sbb.atlas.api.servicepoint.SpatialReference;
 import ch.sbb.atlas.imports.bulk.BulkImportDataMapper;
+import ch.sbb.atlas.imports.bulk.UpdateGeolocationModel;
 import ch.sbb.atlas.servicepointdirectory.entity.geolocation.GeolocationBaseEntity;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Optional;
 
 public abstract class GeolocationBulkImportDataMapper extends BulkImportDataMapper {
 
@@ -24,16 +31,29 @@ public abstract class GeolocationBulkImportDataMapper extends BulkImportDataMapp
       UpdateGeolocationModel update) {
     GeolocationBaseCreateModel geolocationModel = new GeolocationBaseCreateModel();
 
-    applyValueWithDefault(update.getNorth(), currentGeolocation == null ? null : currentGeolocation.getNorth(),
-        geolocationModel::setNorth);
-    applyValueWithDefault(update.getEast(), currentGeolocation == null ? null : currentGeolocation.getEast(),
-        geolocationModel::setEast);
-    applyValueWithDefault(update.getSpatialReference(),
-        currentGeolocation == null ? null : currentGeolocation.getSpatialReference(), geolocationModel::setSpatialReference);
+    SpatialReference spatialReference = Optional.ofNullable(update.getSpatialReference())
+        .orElse(currentGeolocation == null ? null : currentGeolocation.getSpatialReference());
+    geolocationModel.setSpatialReference(spatialReference);
+
+    applyValueWithDefault(roundToSpatialReferencePrecision(update.getNorth(), spatialReference),
+        currentGeolocation == null ? null : currentGeolocation.getNorth(), geolocationModel::setNorth);
+    applyValueWithDefault(roundToSpatialReferencePrecision(update.getEast(), spatialReference),
+        currentGeolocation == null ? null : currentGeolocation.getEast(), geolocationModel::setEast);
 
     applyValueWithDefault(update.getHeight(), currentGeolocation == null ? null : currentGeolocation.getHeight(),
         geolocationModel::setHeight);
 
     return geolocationModel;
+  }
+
+  private static Double roundToSpatialReferencePrecision(Double value, SpatialReference spatialReference) {
+    if (value == null) {
+      return null;
+    }
+
+    int newScale = spatialReference == SpatialReference.LV95 ? ATLAS_LV_MAX_DIGITS : ATLAS_WGS84_MAX_DIGITS;
+    return BigDecimal.valueOf(value)
+        .setScale(newScale, RoundingMode.HALF_UP)
+        .doubleValue();
   }
 }
