068b0367939427f9423d3c7e9cc16b25307b3815
atlas <19825793+iuccio@users.noreply.github.com>
2024-11-04 15:10:01 +0100

    ATLAS-2518: adapt trafficpointelement bulk import

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/service/trafficpoint/bulk/TrafficPointElementBulkImportUpdate.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/service/trafficpoint/bulk/TrafficPointElementBulkImportUpdate.java
@@ -1,58 +1,26 @@
 package ch.sbb.atlas.servicepointdirectory.service.trafficpoint.bulk;
 
 import ch.sbb.atlas.api.servicepoint.CreateTrafficPointElementVersionModel;
-import ch.sbb.atlas.imports.bulk.AttributeNullingNotSupportedException;
+import ch.sbb.atlas.imports.bulk.BulkImportUpdateContainer;
 import ch.sbb.atlas.imports.model.TrafficPointUpdateCsvModel;
-import ch.sbb.atlas.imports.model.TrafficPointUpdateCsvModel.Fields;
 import ch.sbb.atlas.servicepointdirectory.entity.TrafficPointElementVersion;
 import ch.sbb.atlas.servicepointdirectory.service.servicepoint.bulk.GeolocationBulkImportDataMapper;
-import java.util.List;
-import lombok.experimental.UtilityClass;
 
-@UtilityClass
-public class TrafficPointElementBulkImportUpdate extends GeolocationBulkImportDataMapper {
+public class TrafficPointElementBulkImportUpdate extends GeolocationBulkImportDataMapper<TrafficPointUpdateCsvModel, TrafficPointElementVersion,
+    CreateTrafficPointElementVersionModel> {
 
-  public void applyNulling(List<String> attributesToNull, CreateTrafficPointElementVersionModel updateModel) {
-    for (String attributeToNull : attributesToNull) {
-      switch (attributeToNull) {
-        case Fields.designation -> updateModel.setDesignation(null);
-        case Fields.designationOperational -> updateModel.setDesignationOperational(null);
-        case Fields.length -> updateModel.setLength(null);
-        case Fields.boardingAreaHeight -> updateModel.setBoardingAreaHeight(null);
-        case Fields.compassDirection -> updateModel.setCompassDirection(null);
-        case Fields.height -> {
-          if (updateModel.getTrafficPointElementGeolocation() != null) {
-            updateModel.getTrafficPointElementGeolocation().setHeight(null);
-          }
-        }
-        case Fields.east, Fields.north, Fields.spatialReference -> updateModel.setTrafficPointElementGeolocation(null);
-        case Fields.parentSloid -> updateModel.setParentSloid(null);
-        default -> throw new AttributeNullingNotSupportedException(attributeToNull);
-      }
-    }
+  public static CreateTrafficPointElementVersionModel apply(BulkImportUpdateContainer<TrafficPointUpdateCsvModel> bulkImportContainer,
+      TrafficPointElementVersion currentVersion, CreateTrafficPointElementVersionModel updateModel) {
+    return new TrafficPointElementBulkImportUpdate().applyUpdate(bulkImportContainer, currentVersion, updateModel);
   }
 
-  public CreateTrafficPointElementVersionModel applyUpdateFromCsv(TrafficPointElementVersion currentVersion,
-      TrafficPointUpdateCsvModel update) {
-    CreateTrafficPointElementVersionModel updateModel = new CreateTrafficPointElementVersionModel();
+  @Override
+  protected void applySpecificUpdate(TrafficPointUpdateCsvModel update, TrafficPointElementVersion currentVersion,
+      CreateTrafficPointElementVersionModel updateModel) {
 
     setNonUpdatableValues(currentVersion, updateModel);
 
-    updateModel.setValidFrom(update.getValidFrom());
-    updateModel.setValidTo(update.getValidTo());
-
-    applyValueWithDefault(update.getDesignation(), currentVersion.getDesignation(), updateModel::setDesignation);
-    applyValueWithDefault(update.getDesignationOperational(), currentVersion.getDesignationOperational(),
-        updateModel::setDesignationOperational);
-
-    applyValueWithDefault(update.getLength(), currentVersion.getLength(), updateModel::setLength);
-    applyValueWithDefault(update.getBoardingAreaHeight(), currentVersion.getBoardingAreaHeight(),
-        updateModel::setBoardingAreaHeight);
-    applyValueWithDefault(update.getCompassDirection(), currentVersion.getCompassDirection(), updateModel::setCompassDirection);
-    applyValueWithDefault(update.getParentSloid(), currentVersion.getParentSloid(), updateModel::setParentSloid);
-
     updateModel.setTrafficPointElementGeolocation(applyGeolocationUpdate(currentVersion.getTrafficPointElementGeolocation(), update));
-    return updateModel;
   }
 
   private static void setNonUpdatableValues(TrafficPointElementVersion currentVersion,
