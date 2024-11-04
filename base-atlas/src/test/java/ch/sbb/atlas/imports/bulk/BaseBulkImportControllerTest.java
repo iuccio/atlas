ba34a4c46ec4d4cb17fdffd585b97f15b0752ec5
atlas <TechSupport-ATLAS@sbb.ch>
2024-11-04 10:27:08 +0100

    ATLAS-2518: Introduce ErrorResponseMapper

--- base-atlas/src/test/java/ch/sbb/atlas/imports/bulk/BaseBulkImportControllerTest.java
+++ base-atlas/src/test/java/ch/sbb/atlas/imports/bulk/BaseBulkImportControllerTest.java
@@ -8,30 +8,17 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 
-import ch.sbb.atlas.configuration.handler.AtlasExceptionHandler;
 import ch.sbb.atlas.imports.BulkImportItemExecutionResult;
 import ch.sbb.atlas.imports.model.ServicePointUpdateCsvModel;
-import ch.sbb.atlas.model.controller.IntegrationTest;
 import ch.sbb.atlas.model.exception.SloidNotFoundException;
 import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
-@IntegrationTest
 class BaseBulkImportControllerTest {
 
-  @Autowired
-  private AtlasExceptionHandler atlasExceptionHandler;
-
-  private BaseBulkImportController bulkImportController;
-
-  @BeforeEach
-  void setUp() {
-    bulkImportController = new BaseBulkImportController(atlasExceptionHandler);
-  }
+  private final BaseBulkImportController bulkImportController = new BaseBulkImportController();
 
   @Test
   void shouldUpdateWithInNameOf() {
