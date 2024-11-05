8d63f889ca374562c476d6dc33a13e856202a3a4
atlas <19825793+iuccio@users.noreply.github.com>
2024-11-05 14:33:29 +0100

    ATLAS-2518: integrate to defaultmapping

--- base-atlas/src/main/java/ch/sbb/atlas/imports/model/TrafficPointUpdateCsvModel.java
+++ base-atlas/src/main/java/ch/sbb/atlas/imports/model/TrafficPointUpdateCsvModel.java
@@ -3,7 +3,6 @@ package ch.sbb.atlas.imports.model;
 import ch.sbb.atlas.api.servicepoint.SpatialReference;
 import ch.sbb.atlas.deserializer.LocalDateDeserializer;
 import ch.sbb.atlas.imports.annotation.AdditionalDefaultMappings;
-import ch.sbb.atlas.imports.annotation.AdditionalDefaultMappings.AdditionalDefaultMapping;
 import ch.sbb.atlas.imports.bulk.BulkImportErrors;
 import ch.sbb.atlas.imports.bulk.BulkImportLogEntry.BulkImportError;
 import ch.sbb.atlas.imports.annotation.DefaultMapping;
@@ -35,10 +34,10 @@ import lombok.experimental.FieldNameConstants;
     Fields.designationOperational, Fields.length, Fields.boardingAreaHeight, Fields.compassDirection,
     Fields.east, Fields.north, Fields.spatialReference, Fields.height, Fields.parentSloid})
 @AdditionalDefaultMappings({
-    @AdditionalDefaultMapping(target = "id", current = "id"),
-    @AdditionalDefaultMapping(target = "etagVersion", current = "version"),
-    @AdditionalDefaultMapping(target = "sloid", current = "sloid"),
-    @AdditionalDefaultMapping(target = "numberWithoutCheckDigit", current = "servicePointNumber.value"),
+    @DefaultMapping(target = "id", current = "id"),
+    @DefaultMapping(target = "etagVersion", current = "version"),
+    @DefaultMapping(target = "sloid", current = "sloid"),
+    @DefaultMapping(target = "numberWithoutCheckDigit", current = "servicePointNumber.value"),
 })
 public class TrafficPointUpdateCsvModel implements Validatable<TrafficPointUpdateCsvModel>, UpdateGeolocationModel {
 
