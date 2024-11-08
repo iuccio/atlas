153d18ab2aafad2437deeaf8247c647c3a9f1370
atlas <TechSupport-ATLAS@sbb.ch>
2024-11-08 10:16:54 +0100

    Redact unauthorized useradministration requests

--- user-administration/src/main/java/ch/sbb/atlas/user/administration/controller/UserInformationApiController.java
+++ user-administration/src/main/java/ch/sbb/atlas/user/administration/controller/UserInformationApiController.java
@@ -2,8 +2,10 @@ package ch.sbb.atlas.user.administration.controller;
 
 import ch.sbb.atlas.api.user.administration.UserInformationApiV1;
 import ch.sbb.atlas.kafka.model.user.admin.ApplicationType;
+import ch.sbb.atlas.service.UserService;
 import ch.sbb.atlas.user.administration.service.GraphApiService;
 import ch.sbb.atlas.api.user.administration.UserModel;
+import java.util.Collections;
 import java.util.List;
 
 import ch.sbb.atlas.user.administration.service.UserAdministrationService;
@@ -24,6 +26,9 @@ public class UserInformationApiController implements UserInformationApiV1 {
 
   @Override
   public List<UserModel> searchUsersInAtlas(String searchQuery, ApplicationType applicationType) {
+    if (UserService.hasUnauthorizedRole()) {
+      return Collections.emptyList();
+    }
     List<UserModel> foundUsers = graphApiService.searchUsers(searchQuery);
     return administrationService.filterForPermittedUserInAtlas(foundUsers, applicationType);
   }
