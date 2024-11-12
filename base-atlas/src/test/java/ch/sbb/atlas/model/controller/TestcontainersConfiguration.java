3f642c800ea94f2efd624eb16f2f5e44f25e0b57
atlas <19825793+iuccio@users.noreply.github.com>
2024-11-12 13:41:37 +0100

    ATLAS-2136: lets now try on tekton

--- base-atlas/src/test/java/ch/sbb/atlas/model/controller/TestcontainersConfiguration.java
+++ base-atlas/src/test/java/ch/sbb/atlas/model/controller/TestcontainersConfiguration.java
@@ -1,38 +1,27 @@
 package ch.sbb.atlas.model.controller;
 
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 /**
  * <a
- * href="https://confluence.sbb.ch/display/CLEW/Advanced+Topics#AdvancedTopics-Knownissuesandtroubleshooting">Testcontainers
- * on Kubedock troubleshooting</a>
+ * href="https://confluence.sbb.ch/display/CLEW/Advanced+Topics#AdvancedTopics-Knownissuesandtroubleshooting">Testcontainers</a>
+ * on Kubedock troubleshooting
  */
-@TestConfiguration(proxyBeanMethods = false)
-@Testcontainers
+@Configuration
 public class TestcontainersConfiguration {
 
-  public static final String KUBEDOCK_RUNAS_USER_LABEL_NAME = "com.joyrex2001.kubedock.runas-user";
+  private static final String KUBEDOCK_RUNAS_USER_LABEL_NAME = "com.joyrex2001.kubedock.runas-user";
+  private static final String KUBEDOCK_RUNAS_USER = "999";
 
-  /**
-   * DB container with customization for Kubedock compatibility. How to find the correct user id?
-   * <ol>
-   *     <li>Start the application locally</li>
-   *     <li>learn from <a href="https://hub.docker.com/_/postgres">Postgres Container docs</a> the the default linux username is 'postgres'.</li>
-   *     <li>Shell into the Postgres Container</li>
-   *     <li>Get the uid of the linux user: <code>id -u postgres</code></li>
-   * </ol>
-   */
-  @Bean
+  @Bean(destroyMethod = "stop")
   @ServiceConnection
-  public PostgreSQLContainer<?> dbContainer() {
-    PostgreSQLContainer<?> container = new PostgreSQLContainer<>(DockerImageName.parse("postgres:16.4"));
-    container.withLabel(KUBEDOCK_RUNAS_USER_LABEL_NAME, "999");
-    return container;
+  public PostgreSQLContainer<?> postgreSQLContainer() {
+    return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16.4"))
+        .withLabel(KUBEDOCK_RUNAS_USER_LABEL_NAME, KUBEDOCK_RUNAS_USER);
   }
 
 }
