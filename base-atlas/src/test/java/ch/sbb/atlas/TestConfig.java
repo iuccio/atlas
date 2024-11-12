7b154fada57566af53f4f60db1f2ea3db6f520d2
atlas <19825793+iuccio@users.noreply.github.com>
2024-11-12 16:15:09 +0100

    ATLAS-2136: Integrate with lidi

--- base-atlas/src/test/java/ch/sbb/atlas/TestConfig.java
+++ base-atlas/src/test/java/ch/sbb/atlas/TestConfig.java
@@ -1,14 +1,11 @@
 package ch.sbb.atlas;
 
 import ch.sbb.atlas.business.organisation.repository.BusinessOrganisationVersionSharingDataAccessor;
-import ch.sbb.atlas.model.controller.TestcontainersConfiguration;
 import ch.sbb.atlas.transport.company.repository.TransportCompanySharingDataAccessor;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import(TestcontainersConfiguration.class)
 public class TestConfig {
 
   @MockBean
