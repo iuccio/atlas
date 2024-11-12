52159e05495589c1b4cc7dc04524fdef9049fbce
atlas <19825793+iuccio@users.noreply.github.com>
2024-11-12 16:15:09 +0100

    ATLAS-2136: Integrate with lidi

--- base-atlas/src/test/java/ch/sbb/atlas/model/controller/IntegrationTest.java
+++ base-atlas/src/test/java/ch/sbb/atlas/model/controller/IntegrationTest.java
@@ -6,6 +6,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -16,6 +17,7 @@ import org.springframework.test.context.ActiveProfiles;
 @WithAdminMockJwtAuthentication
 @ActiveProfiles("integration-test")
 @EmbeddedKafka
+@Import(TestcontainersConfiguration.class)
 public @interface IntegrationTest {
 
 }
