9eac988bdf3d9a816d01cbc6a8549d232910b61b
atlas <19825793+iuccio@users.noreply.github.com>
2024-11-18 11:06:44 +0100

    ATLAS-2136: upgrade postgres to 17.1

--- base-atlas/src/test/java/ch/sbb/atlas/model/controller/PostgreSQLTestContainer.java
+++ base-atlas/src/test/java/ch/sbb/atlas/model/controller/PostgreSQLTestContainer.java
@@ -15,7 +15,10 @@ public class PostgreSQLTestContainer {
   private static final String KUBEDOCK_RUNAS_USER_LABEL_NAME = "com.joyrex2001.kubedock.runas-user";
   private static final String KUBEDOCK_RUNAS_USER = "999";
 
-  private static final String POSTGRES_DOCKER_IMAGE = "postgres:16.4";
+  /**
+   * This Postgres Version should reflect the docker-compose.yml and the postgres version used on AWS
+   */
+  private static final String POSTGRES_DOCKER_IMAGE = "postgres:17.1";
 
   public static PostgreSQLContainer<?> create() {
     return new PostgreSQLContainer<>(DockerImageName.parse(POSTGRES_DOCKER_IMAGE))
