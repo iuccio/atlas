8d4844c2113eb5b2af6e31084204b0b45f19dddf
atlas <19825793+iuccio@users.noreply.github.com>
2024-11-19 16:11:26 +0100

    ATLAS-2392: impl update lineVersion frontend

--- frontend/src/app/pages/lidi/lines/detail/line-detail-form/line-detail-form.component.ts
+++ frontend/src/app/pages/lidi/lines/detail/line-detail-form/line-detail-form.component.ts
@@ -1,6 +1,16 @@
-import { Component, Input } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { LineType, PaymentType } from '../../../../../api';
+import {Component, Input} from '@angular/core';
+import {FormGroup} from '@angular/forms';
+import {LineConcessionType, LineType, MeanOfTransport, OfferCategory} from '../../../../../api';
+
+
+interface Category {
+  value: OfferCategory;
+}
+
+interface OfferCategoryGroup {
+  name: MeanOfTransport;
+  category: Category[];
+}
 
 @Component({
   selector: 'line-detail-form',
@@ -11,6 +21,50 @@ export class LineDetailFormComponent {
   @Input() form!: FormGroup;
   @Input() newRecord = false;
   @Input() boSboidRestriction: string[] = [];
+
+  readonly extractOfferCategory = (category: Category) => category.value;
+
   TYPE_OPTIONS = Object.values(LineType);
-  PAYMENT_TYPE_OPTIONS = Object.values(PaymentType);
+  OFFER_CATEGORY_OPTIONS = Object.values(OfferCategory);
+  LINE_CONCESSION_TYPE_OPTIONS = Object.values(LineConcessionType);
+  OFFER_CATEGORY_GROUP: OfferCategoryGroup[] = [
+    {
+      name: MeanOfTransport.Train,
+      category: [{value: 'IC'}, {value: 'EC'}, {value: 'EN'}, {value: 'IR'}, {value: 'RE'},
+        {value: 'R'}, {value: 'S'}, {value: 'SN'}, {value: 'PE'}, {value: 'EXT'}, {value: 'ATZ'},
+        {value: 'ICE'}, {value: 'TGV'}, {value: 'RJ'}, {value: 'TE2'}, {value: 'TER'}, {value: 'RB'}, {value: 'IRE'}]
+    },
+    {
+      name: MeanOfTransport.Tram,
+      category: [{value: 'T'}, {value: 'TN'}]
+    },
+    {
+      name: MeanOfTransport.Metro,
+      category: [{value: 'M'}]
+    },
+    {
+      name: MeanOfTransport.Bus,
+      category: [{value: 'CAX'}, {value: 'CAR'}, {value: 'EXB'}, {value: 'B'}, {value: 'BN'}, {value: 'RUB'}, {value: 'BP'}]
+    },
+    {
+      name: MeanOfTransport.CableRailway,
+      category: [{value: 'FUN'}]
+    },
+    {
+      name: MeanOfTransport.CableCar,
+      category: [{value: 'PB'}, {value: 'GB'}]
+    },
+    {
+      name: MeanOfTransport.Chairlift,
+      category: [{value: 'SL'}]
+    },
+    {
+      name: MeanOfTransport.Elevator,
+      category: [{value: 'ASC'}]
+    },
+    {
+      name: MeanOfTransport.Boat,
+      category: [{value: 'BAT'}, {value: 'FAE'}]
+    }
+  ];
 }
--- frontend/src/app/pages/lidi/lines/detail/line-detail.resolver.ts
+++ frontend/src/app/pages/lidi/lines/detail/line-detail.resolver.ts
@@ -1,8 +1,8 @@
-import { inject, Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, ResolveFn, Router } from '@angular/router';
-import { catchError, Observable, of } from 'rxjs';
-import { LinesService, LineVersion } from '../../../../api';
-import { Pages } from '../../../pages';
+import {inject, Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, ResolveFn, Router} from '@angular/router';
+import {catchError, Observable, of} from 'rxjs';
+import {LinesService, LineVersion, LineVersionV2} from '../../../../api';
+import {Pages} from '../../../pages';
 
 @Injectable({ providedIn: 'root' })
 export class LineDetailResolver {
@@ -11,19 +11,19 @@ export class LineDetailResolver {
     private readonly router: Router,
   ) {}
 
-  resolve(route: ActivatedRouteSnapshot): Observable<Array<LineVersion>> {
+  resolve(route: ActivatedRouteSnapshot): Observable<Array<LineVersionV2>> {
     const idParameter = route.paramMap.get('id') || '';
     return idParameter === 'add'
       ? of([])
-      : this.linesService.getLineVersions(idParameter).pipe(
-          catchError(() =>
-            this.router
-              .navigate([Pages.LIDI.path], {
-                state: { notDismissSnackBar: true },
-              })
-              .then(() => []),
-          ),
-        );
+      : this.linesService.getLineVersionsV2(idParameter).pipe(
+        catchError(() =>
+          this.router
+            .navigate([Pages.LIDI.path], {
+              state: {notDismissSnackBar: true},
+            })
+            .then(() => []),
+        ),
+      );
   }
 }
 
