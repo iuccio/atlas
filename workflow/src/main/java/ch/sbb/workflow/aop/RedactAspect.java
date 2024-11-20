6e0478985f1343d19db14fa5396dda69e440d1ba
atlas <19825793+iuccio@users.noreply.github.com>
2024-11-20 14:22:09 +0100

    ATLAS-2571: Complete ref

--- workflow/src/main/java/ch/sbb/workflow/aop/RedactAspect.java
+++ workflow/src/main/java/ch/sbb/workflow/aop/RedactAspect.java
@@ -1,20 +1,15 @@
 package ch.sbb.workflow.aop;
 
-import ch.sbb.atlas.kafka.model.user.admin.ApplicationType;
-import ch.sbb.atlas.service.UserService;
-import ch.sbb.atlas.user.administration.security.aspect.ParameterInfoReflectionResolver;
-import ch.sbb.atlas.user.administration.security.aspect.ParameterInfoReflectionResolver.ParameterInfo;
-import ch.sbb.atlas.user.administration.security.service.BusinessOrganisationBasedUserAdministrationService;
 import ch.sbb.atlas.versioning.convert.ReflectionHelper;
 import ch.sbb.workflow.helper.StringHelper;
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -29,7 +24,7 @@ import org.springframework.util.ReflectionUtils;
 @RequiredArgsConstructor
 public class RedactAspect {
 
-  private final BusinessOrganisationBasedUserAdministrationService businessOrganisationBasedUserAdministrationService;
+  private final RedactDecider redactDecider;
 
   @Around("@annotation(ch.sbb.workflow.aop.Redacted)")
   public Object redactSensitiveData(final ProceedingJoinPoint joinPoint) throws Throwable {
@@ -39,97 +34,104 @@ public class RedactAspect {
   Object redact(ProceedingJoinPoint joinPoint) throws Throwable {
     Object resultObject = joinPoint.proceed();
 
-    boolean redact = shouldRedact(joinPoint, resultObject);
-
-    if (!redact) {
+    boolean shouldRedact = redactDecider.shouldRedact(joinPoint, resultObject);
+    if (!shouldRedact) {
       return resultObject;
     }
 
+    return redactResult(resultObject);
+  }
+
+  private Object redactResult(Object resultObject) {
     if (resultObject instanceof Page<?> page) {
-      List<Object> redactedPage = page.getContent().stream().map(this::redactObject).toList();
+      List<Object> redactedPage = page.getContent().stream().map(pageItem -> new ObjectRedactor(pageItem).accept()).toList();
       return new PageImpl<>(redactedPage, page.getPageable(), page.getTotalElements());
     } else {
-      return this.redactObject(resultObject);
+      return new ObjectRedactor(resultObject).accept();
     }
   }
 
-  private boolean shouldRedact(ProceedingJoinPoint joinPoint, Object resultObject) {
-    Optional<ParameterInfo<RedactBySboid>> redactBySboidParameterInfo = ParameterInfoReflectionResolver.byAnnotation(joinPoint,
-        RedactBySboid.class);
-    if (redactBySboidParameterInfo.isPresent()) {
-      ApplicationType application = redactBySboidParameterInfo.get().getAnnotation().application();
-      String sboid = redactBySboidParameterInfo.get().getValueAsString();
+  @AllArgsConstructor
+  private static class ObjectRedactor {
 
-      return shouldRedactBySboid(sboid, application);
-    } else {
-      Optional<Field> redactBySboidField = Arrays.stream(resultObject.getClass().getDeclaredFields())
-          .filter(i -> i.isAnnotationPresent(RedactBySboid.class)).findFirst();
+    private Object object;
 
-      if (redactBySboidField.isPresent()) {
-        ReflectionUtils.makeAccessible(redactBySboidField.get());
-        String sboidField = (String) ReflectionUtils.getField(redactBySboidField.get(), resultObject);
-        return shouldRedactBySboid(sboidField, redactBySboidField.get().getAnnotation(RedactBySboid.class).application());
+    public Object accept() {
+      if (object == null) {
+        return null;
+      }
+      if (object.getClass().isAnnotationPresent(Redacted.class)) {
+
+        object = ReflectionHelper.copyObjectViaBuilder(object);
+
+        for (Field field : object.getClass().getDeclaredFields()) {
+          if (field.isAnnotationPresent(Redacted.class) && field.getType().isAnnotationPresent(Redacted.class)) {
+            Object fieldObject = ReflectionUtils.getField(field, object);
+            Object redactObject = new ObjectRedactor(fieldObject).accept();
+            ReflectionUtils.setField(field, object, redactObject);
+          } else if (field.isAnnotationPresent(Redacted.class)) {
+            new FieldRedactor(field, object).accept();
+          }
+        }
+        return object;
       } else {
-        return UserService.hasUnauthorizedRole();
+        throw new IllegalStateException("Class not configured for redact. Class name=" + object.getClass().getName());
       }
     }
+
   }
 
-  Object redactObject(Object object) {
-    if (object == null) {
-      return null;
+  private static class FieldRedactor {
+
+    private final Field field;
+    private final Object currentFieldValue;
+    private final Object object;
+    private final boolean showFirstChar;
+
+    private boolean redactPerformed = false;
+
+    public FieldRedactor(Field field, Object object) {
+      ReflectionUtils.makeAccessible(field);
+      this.currentFieldValue = ReflectionUtils.getField(field, object);
+      this.field = field;
+      this.object = object;
+
+      Redacted redacted = Objects.requireNonNull(field.getAnnotation(Redacted.class),
+          "Redacted field must be annotated with @Redacted");
+      this.showFirstChar = redacted.showFirstChar();
     }
-    if (object.getClass().isAnnotationPresent(Redacted.class)) {
 
-      object = ReflectionHelper.copyObjectViaBuilder(object);
+    public void accept() {
+      if (currentFieldValue instanceof String stringValue) {
+        performRedact(StringHelper.redactString(stringValue, showFirstChar));
+      }
 
-      for (Field field : object.getClass().getDeclaredFields()) {
-        if (field.isAnnotationPresent(Redacted.class) && field.getType().isAnnotationPresent(Redacted.class)) {
-          Object fieldObject = ReflectionUtils.getField(field, object);
-          Object redactObject = redactObject(fieldObject);
-          ReflectionUtils.setField(field, object, redactObject);
-        } else if (field.isAnnotationPresent(Redacted.class)) {
-          redactField(field, object);
+      if (field.getGenericType() instanceof ParameterizedType parameterizedType) {
+        Type actualTypeArgument = parameterizedType.getActualTypeArguments()[0];
+
+        if (actualTypeArgument.equals(String.class)) {
+          if (currentFieldValue instanceof List<?> list) {
+            List<String> redactedList = list.stream().map(i -> StringHelper.redactString((String) i, showFirstChar))
+                .toList();
+            performRedact(redactedList);
+          }
+        } else {
+          if (currentFieldValue instanceof Set<?> set) {
+            Set<Object> redactedSet = set.stream().map(i -> new ObjectRedactor(i).accept()).collect(Collectors.toSet());
+            performRedact(redactedSet);
+          }
         }
       }
-      return object;
-    } else {
-      throw new IllegalStateException("Class not configured for redact");
-    }
-  }
 
-
-  void redactField(Field field, Object object) {
-    ReflectionUtils.makeAccessible(field);
-    Object currentFieldValue = ReflectionUtils.getField(field, object);
-    Redacted redacted = field.getAnnotation(Redacted.class);
-
-    if (currentFieldValue instanceof String stringValue) {
-      ReflectionUtils.setField(field, object, StringHelper.redactString(stringValue, redacted.showFirstChar()));
-    }
-
-    if (field.getGenericType() instanceof ParameterizedType parameterizedType) {
-      Type actualTypeArgument = parameterizedType.getActualTypeArguments()[0];
-
-      if (actualTypeArgument.equals(String.class)) {
-        if (currentFieldValue instanceof List<?> list) {
-          List<String> redactedList = list.stream().map(i -> StringHelper.redactString((String) i, redacted.showFirstChar()))
-              .toList();
-          ReflectionUtils.setField(field, object, redactedList);
-        }
-      } else {
-        if (currentFieldValue instanceof Set<?> set) {
-          Set<Object> redactedSet = set.stream().map(this::redactObject).collect(Collectors.toSet());
-          ReflectionUtils.setField(field, object, redactedSet);
-        }
+      if (!redactPerformed) {
+        throw new IllegalStateException("Field not redacted. Field type not supported. Field name=" + field.getName());
       }
     }
-  }
 
-  private boolean shouldRedactBySboid(String sboid, ApplicationType application) {
-    boolean hasPermission = businessOrganisationBasedUserAdministrationService.hasUserPermissionsForBusinessOrganisation(sboid,application);
-    boolean isUnauthorized = UserService.hasUnauthorizedRole();
-    return !hasPermission || isUnauthorized;
+    private void performRedact(Object newValue) {
+      ReflectionUtils.setField(field, object, newValue);
+      redactPerformed = true;
+    }
   }
 
 }
