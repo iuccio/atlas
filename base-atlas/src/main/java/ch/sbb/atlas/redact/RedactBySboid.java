917af9240c2d19e4d4292373cbbec1608eac1145
atlas <19825793+iuccio@users.noreply.github.com>
2024-11-26 14:13:49 +0100

    ATLAS-2571: Add documentation

--- base-atlas/src/main/java/ch/sbb/atlas/redact/RedactBySboid.java
+++ base-atlas/src/main/java/ch/sbb/atlas/redact/RedactBySboid.java
@@ -6,6 +6,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marker annotation for {@link RedactAspect}.
+ * Mark a parameter or field with this annotation to check the users rights on a given BusinessOrganisation
+ */
 @Target({ElementType.PARAMETER, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RedactBySboid {
