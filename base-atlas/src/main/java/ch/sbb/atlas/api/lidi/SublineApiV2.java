8a6da1501f1855da09427f65315830bb156fafa7
atlas <19825793+iuccio@users.noreply.github.com>
2024-11-26 18:06:22 +0100

    ATLAS-2395: Implement line search and draft subline detail view

--- base-atlas/src/main/java/ch/sbb/atlas/api/lidi/SublineApiV2.java
+++ base-atlas/src/main/java/ch/sbb/atlas/api/lidi/SublineApiV2.java
@@ -13,4 +13,27 @@ public interface SublineApiV2 {
   @GetMapping("versions/{slnid}")
   List<SublineVersionModelV2> getSublineVersionV2(@PathVariable String slnid);
 
+//  @PostMapping("versions")
+//  @ResponseStatus(HttpStatus.CREATED)
+//  @ApiResponses(value = {
+//      @ApiResponse(responseCode = "201"),
+//      @ApiResponse(responseCode = "409", description = "Swiss number is not unique in time", content = @Content(schema =
+//      @Schema(implementation = ErrorResponse.class)))
+//  })
+//  SublineVersionModelV2 createSublineVersionV2(@RequestBody @Valid CreateLineVersionModelV2 newVersion);
+//
+//  @PostMapping({"versions/{id}"})
+//  @ApiResponses(value = {
+//      @ApiResponse(responseCode = "200"),
+//      @ApiResponse(responseCode = "409", description = "Swiss number is not unique in time", content = @Content(schema =
+//      @Schema(implementation = ErrorResponse.class))),
+//      @ApiResponse(responseCode = "412", description = ENTITY_ALREADY_UPDATED, content =
+//      @Content(schema = @Schema(implementation = ErrorResponse.class))),
+//      @ApiResponse(responseCode = "501", description = VERSIONING_NOT_IMPLEMENTED, content =
+//      @Content(schema = @Schema(implementation = ErrorResponse.class))),
+//      @ApiResponse(responseCode = "520", description = NO_ENTITIES_WERE_MODIFIED, content =
+//      @Content(schema = @Schema(implementation = ErrorResponse.class))),
+//  })
+//  List<SublineVersionModelV2> updateSublineVersion(@PathVariable Long id, @RequestBody @Valid UpdateLineVersionModelV2 newVersion);
+
 }
--- frontend/src/app/pages/lidi/sublines/detail/subline-detail-form-group.ts
+++ frontend/src/app/pages/lidi/sublines/detail/subline-detail-form-group.ts
@@ -1,6 +1,11 @@
 import {BaseDetailFormGroup} from '../../../../core/components/base-detail/base-detail-form-group';
-import {FormControl} from '@angular/forms';
-import {PaymentType, SublineType} from '../../../../api';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {PaymentType, SublineConcessionType, SublineType, SublineVersion, SublineVersionV2} from '../../../../api';
+import {AtlasFieldLengthValidator} from "../../../../core/validation/field-lengths/atlas-field-length-validator";
+import {WhitespaceValidator} from "../../../../core/validation/whitespace/whitespace-validator";
+import {AtlasCharsetsValidator} from "../../../../core/validation/charsets/atlas-charsets-validator";
+import {DateRangeValidator} from "../../../../core/validation/date-range/date-range-validator";
+import moment from 'moment';
 
 export interface SublineDetailFormGroup extends BaseDetailFormGroup {
   swissSublineNumber: FormControl<string | null>;
@@ -8,10 +13,63 @@ export interface SublineDetailFormGroup extends BaseDetailFormGroup {
   slnid: FormControl<string | null | undefined>;
   status: FormControl<string | null | undefined>;
   sublineType: FormControl<SublineType | null>;
-  paymentType: FormControl<PaymentType | null | undefined>;
+  sublineConcessionType: FormControl<SublineConcessionType | null>;
   businessOrganisation: FormControl<string | null>;
   number: FormControl<string | null | undefined>;
   longName: FormControl<string | null | undefined>;
   description: FormControl<string | null | undefined>;
   etagVersion: FormControl<number | null | undefined>;
+}
+
+export class SublineFormGroupBuilder {
+  static buildFormGroup(version: SublineVersionV2): FormGroup {
+    return new FormGroup<SublineDetailFormGroup>(
+      {
+        swissSublineNumber: new FormControl(version.swissSublineNumber, [
+          Validators.required,
+          AtlasFieldLengthValidator.length_50,
+          AtlasCharsetsValidator.sid4pt,
+        ]),
+        mainlineSlnid: new FormControl(version.mainlineSlnid, [Validators.required]),
+        slnid: new FormControl(version.slnid),
+        status: new FormControl(version.status),
+        sublineType: new FormControl(version.sublineType, [Validators.required]),
+        sublineConcessionType: new FormControl(version.sublineConcessionType),
+        businessOrganisation: new FormControl(version.businessOrganisation, [
+          Validators.required,
+          AtlasFieldLengthValidator.length_50,
+          WhitespaceValidator.blankOrEmptySpaceSurrounding,
+          AtlasCharsetsValidator.iso88591,
+        ]),
+        number: new FormControl(version.mainSwissLineNumber, [
+          AtlasFieldLengthValidator.length_50,
+          WhitespaceValidator.blankOrEmptySpaceSurrounding,
+          AtlasCharsetsValidator.iso88591,
+        ]),
+        longName: new FormControl(version.longName, [
+          AtlasFieldLengthValidator.length_255,
+          WhitespaceValidator.blankOrEmptySpaceSurrounding,
+          AtlasCharsetsValidator.iso88591,
+        ]),
+        description: new FormControl(version.description, [
+          AtlasFieldLengthValidator.length_255,
+          WhitespaceValidator.blankOrEmptySpaceSurrounding,
+          AtlasCharsetsValidator.iso88591,
+        ]),
+        validFrom: new FormControl(
+          version.validFrom ? moment(version.validFrom) : version.validFrom,
+          [Validators.required],
+        ),
+        validTo: new FormControl(version.validTo ? moment(version.validTo) : version.validTo, [
+          Validators.required,
+        ]),
+        etagVersion: new FormControl(version.etagVersion),
+        creationDate: new FormControl(version.creationDate),
+        editionDate: new FormControl(version.editionDate),
+        editor: new FormControl(version.editor),
+        creator: new FormControl(version.creator),
+      },
+      [DateRangeValidator.fromGreaterThenTo('validFrom', 'validTo')],
+    );
+  }
 }
