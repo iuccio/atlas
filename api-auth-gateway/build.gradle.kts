dc1a90877b32f6761b5852984c9c16ccb62d4031
atlas <19825793+iuccio@users.noreply.github.com>
2024-11-27 16:46:18 +0100

    ATLAS-2147: build with gradle api-auth-gateway

---
+++ api-auth-gateway/build.gradle.kts
@@ -0,0 +1,33 @@
+import java.util.*
+
+plugins {
+    id("org.openapi.generator") version "7.10.0"
+    id("buildlogic.java-conventions")
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    implementation("org.springframework.security:spring-security-oauth2-client")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-configuration-processor")//optional
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("com.squareup.okhttp3:mockwebserver")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.bootJar {
+    enabled = false
+}
+
+springBoot {
+    buildInfo {
+        properties {
+            additional.set(mapOf(
+                    "time" to "${Date()}"
+            ))
+        }
+    }
+}
