8cb336c7d32090ca9fc7da604cff2b48bd9d00f0
atlas <TechSupport-ATLAS@sbb.ch>
2024-11-27 17:14:56 +0100

    ATLAS-2395: Add validation for types

--- line-directory/src/test/java/ch/sbb/line/directory/validation/SublineValidationServiceTest.java
+++ line-directory/src/test/java/ch/sbb/line/directory/validation/SublineValidationServiceTest.java
@@ -6,16 +6,23 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
+import ch.sbb.atlas.api.lidi.enumaration.LineType;
+import ch.sbb.atlas.api.lidi.enumaration.SublineConcessionType;
+import ch.sbb.atlas.api.lidi.enumaration.SublineType;
 import ch.sbb.atlas.business.organisation.service.SharedBusinessOrganisationService;
 import ch.sbb.line.directory.LineTestData;
 import ch.sbb.line.directory.SublineTestData;
 import ch.sbb.line.directory.entity.LineVersion;
 import ch.sbb.line.directory.entity.SublineVersion;
+import ch.sbb.line.directory.exception.SlnidNotFoundException;
 import ch.sbb.line.directory.exception.SubLineAssignToLineConflictException;
+import ch.sbb.line.directory.exception.SublineConcessionException;
+import ch.sbb.line.directory.exception.SublineTypeMissmatchException;
 import ch.sbb.line.directory.repository.LineVersionRepository;
 import ch.sbb.line.directory.repository.SublineVersionRepository;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
@@ -23,7 +30,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
- class SublineValidationTest {
+ class SublineValidationServiceTest {
 
   @Mock
   private SublineVersionRepository sublineVersionRepository;
@@ -139,5 +146,66 @@ import org.mockito.MockitoAnnotations;
     //then
     assertThat(result).isTrue();
   }
+
+   @Test
+   void shouldThrowExceptionIfMainLineIsNotFound() {
+     //given
+     when(lineVersionRepository.findAllBySlnidOrderByValidFrom(any())).thenReturn(Collections.emptyList());
+
+     SublineVersion sublineVersion = SublineTestData.sublineVersion();
+     sublineVersion.setValidFrom(LocalDate.of(1999, 12, 31));
+     sublineVersion.setValidTo(LocalDate.of(2002, 1, 1));
+
+     //when
+     assertThatExceptionOfType(SlnidNotFoundException.class).isThrownBy(
+         () -> sublineValidationService.validatePreconditionSublineBusinessRules(sublineVersion));
+   }
+
+   @Test
+   void shouldThrowExceptionIfMainLineTypeDoesNotMatch() {
+     //given
+     LineVersion mainLineVersion = LineTestData.lineVersion();
+     mainLineVersion.setLineType(LineType.ORDERLY);
+     when(lineVersionRepository.findAllBySlnidOrderByValidFrom(any())).thenReturn(List.of(mainLineVersion));
+
+     SublineVersion sublineVersion = SublineTestData.sublineVersion();
+     sublineVersion.setSublineType(SublineType.TEMPORARY);
+
+     //when
+     assertThatExceptionOfType(SublineTypeMissmatchException.class).isThrownBy(
+         () -> sublineValidationService.validatePreconditionSublineBusinessRules(sublineVersion));
+   }
+
+   @Test
+   void shouldThrowExceptionIfConcessionTypeIsPresentOnOtherType() {
+     //given
+     LineVersion mainLineVersion = LineTestData.lineVersion();
+     mainLineVersion.setLineType(LineType.ORDERLY);
+     when(lineVersionRepository.findAllBySlnidOrderByValidFrom(any())).thenReturn(List.of(mainLineVersion));
+
+     SublineVersion sublineVersion = SublineTestData.sublineVersion();
+     sublineVersion.setSublineType(SublineType.TECHNICAL);
+     sublineVersion.setConcessionType(SublineConcessionType.CANTONALLY_APPROVED_LINE);
+
+     //when
+     assertThatExceptionOfType(SublineConcessionException.class).isThrownBy(
+         () -> sublineValidationService.validatePreconditionSublineBusinessRules(sublineVersion));
+   }
+
+   @Test
+   void shouldThrowExceptionIfConcessionTypeIsMissingOnConcessionType() {
+     //given
+     LineVersion mainLineVersion = LineTestData.lineVersion();
+     mainLineVersion.setLineType(LineType.ORDERLY);
+     when(lineVersionRepository.findAllBySlnidOrderByValidFrom(any())).thenReturn(List.of(mainLineVersion));
+
+     SublineVersion sublineVersion = SublineTestData.sublineVersion();
+     sublineVersion.setSublineType(SublineType.CONCESSION);
+     sublineVersion.setConcessionType(null);
+
+     //when
+     assertThatExceptionOfType(SublineConcessionException.class).isThrownBy(
+         () -> sublineValidationService.validatePreconditionSublineBusinessRules(sublineVersion));
+   }
 
 }
