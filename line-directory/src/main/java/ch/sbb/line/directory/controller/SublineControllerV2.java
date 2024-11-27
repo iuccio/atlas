5ea6befa86fa5190ea55973f31d409293945faef
atlas <19825793+iuccio@users.noreply.github.com>
2024-11-27 17:23:15 +0100

    ATLAS-2583: improve not found error handling

--- line-directory/src/main/java/ch/sbb/line/directory/controller/SublineControllerV2.java
+++ line-directory/src/main/java/ch/sbb/line/directory/controller/SublineControllerV2.java
@@ -4,6 +4,7 @@ import ch.sbb.atlas.api.lidi.ReadSublineVersionModelV2;
 import ch.sbb.atlas.api.lidi.SublineApiV2;
 import ch.sbb.line.directory.entity.LineVersion;
 import ch.sbb.line.directory.entity.SublineVersion;
+import ch.sbb.line.directory.exception.SlnidNotFoundException;
 import ch.sbb.line.directory.mapper.SublineMapper;
 import ch.sbb.line.directory.service.SublineService;
 import java.util.List;
@@ -21,6 +22,9 @@ public class SublineControllerV2 implements SublineApiV2 {
   @Override
   public List<ReadSublineVersionModelV2> getSublineVersionV2(String slnid) {
     List<SublineVersion> versions = sublineService.findSubline(slnid);
+    if (versions.isEmpty()) {
+      throw new SlnidNotFoundException(slnid);
+    }
     String lineSlnid = versions.getFirst().getMainlineSlnid();
     LineVersion lineVersion = sublineService.getMainLineVersion(lineSlnid);
     return versions.stream().map(sublineVersion -> SublineMapper.toModel(sublineVersion, lineVersion)).toList();
