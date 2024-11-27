5a37faea6a6a0a6218eae8fc76bac1c93ad03617
atlas <TechSupport-ATLAS@sbb.ch>
2024-11-27 11:29:25 +0100

    ATLAS-2395: remove base detail from subline

--- frontend/src/app/pages/lidi/sublines/detail/subline-detail.component.ts
+++ frontend/src/app/pages/lidi/sublines/detail/subline-detail.component.ts
@@ -1,100 +1,153 @@
 import {Component, OnInit} from '@angular/core';
 import {
+  ApplicationRole,
   ApplicationType,
   Line,
   LinesService,
   SublineConcessionType,
   SublinesService,
   SublineType,
-  SublineVersion, SublineVersionV2,
+  SublineVersion,
+  SublineVersionV2,
 } from '../../../../api';
-import {BaseDetailController} from '../../../../core/components/base-detail/base-detail-controller';
-import {catchError, Observable, of} from 'rxjs';
+import {catchError, EMPTY, Observable, of} from 'rxjs';
 import {DialogService} from '../../../../core/components/dialog/dialog.service';
 import {NotificationService} from '../../../../core/notification/notification.service';
 import {FormGroup} from '@angular/forms';
 import {ActivatedRoute, Router} from '@angular/router';
-import {Page} from '../../../../core/model/page';
 import {Pages} from '../../../pages';
 import {map} from 'rxjs/operators';
 import {ValidationService} from '../../../../core/validation/validation.service';
-import {SublineFormGroupBuilder} from './subline-detail-form-group';
+import {SublineFormGroup, SublineFormGroupBuilder} from './subline-form-group';
 import {ValidityService} from "../../../sepodi/validity/validity.service";
 import {PermissionService} from "../../../../core/auth/permission/permission.service";
+import {DetailFormComponent} from "../../../../core/leave-guard/leave-dirty-form-guard.service";
+import {VersionsHandlingService} from "../../../../core/versioning/versions-handling.service";
+import {DateRange} from "../../../../core/versioning/date-range";
+import {DetailHelperService, DetailWithCancelEdit} from "../../../../core/detail/detail-helper.service";
 
 @Component({
   templateUrl: './subline-detail.component.html',
   styleUrls: ['./subline-detail.component.scss'],
   providers: [ValidityService],
 })
-export class SublineDetailComponent extends BaseDetailController<SublineVersion> implements OnInit {
+export class SublineDetailComponent implements OnInit, DetailFormComponent, DetailWithCancelEdit {
+  protected readonly Pages = Pages;
+
   TYPE_OPTIONS = Object.values(SublineType);
-  CONCESSION_TYPE_OPTIONS= Object.values(SublineConcessionType);
+  CONCESSION_TYPE_OPTIONS = Object.values(SublineConcessionType);
 
   mainlines$: Observable<Line[]> = of([]);
 
   readonly mainlineSlnidFormControlName = 'mainlineSlnid';
 
+  form!: FormGroup<SublineFormGroup>;
+  isNew = false;
+  showVersionSwitch = false;
+  isSwitchVersionDisabled = false;
+  selectedVersionIndex!: number;
+
+  maxValidity!: DateRange;
+
+  versions!: SublineVersion[];
+  selectedVersion!: SublineVersion;
+
+  boSboidRestriction: string[] = [];
+
   constructor(
-    protected router: Router,
+    private router: Router,
     private sublinesService: SublinesService,
-    protected notificationService: NotificationService,
+    private notificationService: NotificationService,
     private validationService: ValidationService,
     private linesService: LinesService,
-    protected dialogService: DialogService,
-    protected permissionService: PermissionService,
-    protected activatedRoute: ActivatedRoute,
-    protected validityService: ValidityService,
+    private dialogService: DialogService,
+    private permissionService: PermissionService,
+    private activatedRoute: ActivatedRoute,
+    private validityService: ValidityService,
+    private detailHelperService: DetailHelperService,
   ) {
-    super(router, dialogService, notificationService, permissionService, activatedRoute, validityService);
   }
 
   ngOnInit() {
-    super.ngOnInit();
-    if (this.isExistingRecord()) {
+    this.versions = this.activatedRoute.snapshot.data.sublineDetail;
+    if (this.versions.length == 0) {
+      this.isNew = true;
+      this.form = SublineFormGroupBuilder.buildFormGroup();
+    } else {
+      this.isNew = false;
+      VersionsHandlingService.addVersionNumbers(this.versions);
+      this.maxValidity = VersionsHandlingService.getMaxValidity(this.versions);
+      this.selectedVersion = VersionsHandlingService.determineDefaultVersionByValidity(this.versions);
+      this.selectedVersionIndex = this.versions.indexOf(this.selectedVersion);
+
+      this.initSelectedVersion();
       this.mainlines$ = this.linesService
-        .getLine(this.record.mainlineSlnid)
+        .getLine(this.selectedVersion.mainlineSlnid)
         .pipe(map((value) => [value]));
+    }
+    this.initBoSboidRestriction();
+  }
+
+  private initSelectedVersion() {
+    this.showVersionSwitch = VersionsHandlingService.hasMultipleVersions(this.versions);
+    // this.form = SublineFormGroupBuilder.buildFormGroup(this.selectedVersion);
+    if (!this.isNew) {
+      this.form.disable();
     }
   }
 
-  getPageType(): Page {
-    return Pages.SUBLINES;
+  toggleEdit() {
+    if (this.form.enabled) {
+      this.detailHelperService.showCancelEditDialog(this);
+    } else {
+      this.isSwitchVersionDisabled = true;
+      this.validityService.initValidity(this.form);
+      this.form.enable({ emitEvent: false });
+    }
   }
 
-  getApplicationType(): ApplicationType {
-    return ApplicationType.Lidi;
+  initBoSboidRestriction() {
+    if (!this.isNew || this.permissionService.isAdmin) {
+      this.boSboidRestriction = [];
+    } else {
+      const permission = this.permissionService.getApplicationUserPermission(ApplicationType.Lidi);
+      if (permission.role === ApplicationRole.Writer) {
+        this.boSboidRestriction = PermissionService.getSboidRestrictions(permission);
+      } else {
+        this.boSboidRestriction = [];
+      }
+    }
   }
 
-  readRecord(): SublineVersion {
-    return this.activatedRoute.snapshot.data.sublineDetail;
+  switchVersion(newIndex: number) {
+    this.selectedVersionIndex = newIndex;
+    this.selectedVersion = this.versions[newIndex];
+    this.initSelectedVersion();
   }
 
-  getDetailHeading(record: SublineVersion): string {
-    return `${record.number ?? ''} - ${record.description ?? ''}`;
+  save() {
+    ValidationService.validateForm(this.form);
+    if (this.form.valid) {
+      const sublineVersion = this.form.value as unknown as SublineVersionV2;
+      this.form.disable();
+      if (this.isNew) {
+        this.createSubline(sublineVersion);
+      } else {
+        this.validityService.updateValidity(this.form);
+        this.validityService.validate().subscribe(confirmed => {
+          if (confirmed) {
+            this.form.disable();
+            this.updateSubline(this.selectedVersion.id!, sublineVersion);
+          }
+        });
+      }
+    }
   }
 
-  getDetailSubheading(record: SublineVersion): string {
-    return record.slnid!;
-  }
-
-  updateRecord(): void {
-    this.form.disable();
+  createSubline(sublineVersion:SublineVersion): void {
     this.sublinesService
-      .updateSublineVersion(this.getId(), this.form.value)
-      .pipe(catchError(this.handleError))
-      .subscribe(() => {
-        this.notificationService.success('LIDI.SUBLINE.NOTIFICATION.EDIT_SUCCESS');
-        this.router
-          .navigate([Pages.LIDI.path, Pages.SUBLINES.path, this.record.slnid])
-          .then(() => this.ngOnInit());
-      });
-  }
-
-  createRecord(): void {
-    this.sublinesService
-      .createSublineVersion(this.form.value)
-      .pipe(catchError(this.handleError))
+      .createSublineVersion(sublineVersion)
+      .pipe(catchError(this.handleError()))
       .subscribe((version) => {
         this.notificationService.success('LIDI.SUBLINE.NOTIFICATION.ADD_SUCCESS');
         this.router
@@ -103,38 +156,40 @@ export class SublineDetailComponent extends BaseDetailController<SublineVersion>
       });
   }
 
-  revokeRecord(): void {
-    const selectedRecord = this.getSelectedRecord();
-    if (selectedRecord.slnid) {
-      this.sublinesService.revokeSubline(selectedRecord.slnid).subscribe(() => {
+  updateSubline(id:number, sublineVersion:SublineVersion): void {
+    this.sublinesService
+      .updateSublineVersion(id, sublineVersion)
+      .pipe(catchError(this.handleError()))
+      .subscribe(() => {
+        this.notificationService.success('LIDI.SUBLINE.NOTIFICATION.EDIT_SUCCESS');
+        this.router
+          .navigate([Pages.LIDI.path, Pages.SUBLINES.path, sublineVersion.slnid])
+          .then(() => this.ngOnInit());
+      });
+  }
+
+  revoke(): void {
+    if (this.selectedVersion.slnid) {
+      this.sublinesService.revokeSubline(this.selectedVersion.slnid).subscribe(() => {
         this.notificationService.success('LIDI.SUBLINE.NOTIFICATION.REVOKE_SUCCESS');
         this.router
-          .navigate([Pages.LIDI.path, Pages.SUBLINES.path, selectedRecord.slnid])
+          .navigate([Pages.LIDI.path, Pages.SUBLINES.path, this.selectedVersion.slnid])
           .then(() => this.ngOnInit());
       });
     }
   }
 
-  deleteRecord(): void {
-    const selectedSublineVersion = this.getSelectedRecord();
-    if (selectedSublineVersion.slnid != null) {
-      this.sublinesService.deleteSublines(selectedSublineVersion.slnid).subscribe(() => {
+  delete(): void {
+    if (this.selectedVersion.slnid) {
+      this.sublinesService.deleteSublines(this.selectedVersion.slnid).subscribe(() => {
         this.notificationService.success('LIDI.SUBLINE.NOTIFICATION.DELETE_SUCCESS');
-        this.backToOverview();
+        this.back();
       });
     }
   }
 
-  getFormGroup(version: SublineVersion): FormGroup {
-    return SublineFormGroupBuilder.buildFormGroup(version as SublineVersionV2);
-  }
-
-  getValidation(inputForm: string) {
-    return this.validationService.getValidation(this.form?.controls[inputForm]?.errors);
-  }
-
-  getFormControlsToDisable(): string[] {
-    return [this.mainlineSlnidFormControlName];
+  back() {
+    this.router.navigate(['..'], {relativeTo: this.activatedRoute}).then();
   }
 
   searchMainlines(searchString: string) {
@@ -149,4 +204,12 @@ export class SublineDetailComponent extends BaseDetailController<SublineVersion>
       this.mainlineSlnidFormControlName,
     )?.value}`;
   }
+
+  private handleError() {
+    return () => {
+      this.form.enable();
+      return EMPTY;
+    };
+  }
+
 }
