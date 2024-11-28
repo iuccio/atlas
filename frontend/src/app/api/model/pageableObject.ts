ecfb288d696fabda3e3499efb238cea971048b83
atlas <TechSupport-ATLAS@sbb.ch>
2024-11-28 10:46:42 +0100

    ATLAS-2395: Use v2 create and update

--- frontend/src/app/api/model/pageableObject.ts
+++ frontend/src/app/api/model/pageableObject.ts
@@ -15,9 +15,9 @@ import { SortObject } from './sortObject';
 export interface PageableObject { 
     offset?: number;
     sort?: Array<SortObject>;
-    unpaged?: boolean;
     paged?: boolean;
     pageNumber?: number;
     pageSize?: number;
+    unpaged?: boolean;
 }
 
