c80b6b1382689bd944692701bc9173f7595e4e6f
atlas <19825793+iuccio@users.noreply.github.com>
2024-11-29 15:20:01 +0100

    ATLAS-2147: try to exec sonarqube

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,9 @@ subprojects {
             mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
         }
     }
+}
 
+allprojects {
     sonar {
         properties {
             property("sonar.projectName", "ATLAS - SKI Business Application")
