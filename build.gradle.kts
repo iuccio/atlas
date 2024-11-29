7a8744624494d254d1e25c9feb27f703f0a28152
atlas <19825793+iuccio@users.noreply.github.com>
2024-11-29 16:07:09 +0100

    ATLAS-2147: try to fix frontend sonarqube config

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,9 +26,14 @@ allprojects {
         properties {
             property("sonar.projectName", "ATLAS - SKI Business Application")
             property("sonar.projectKey", "ch.sbb.atlas:atlas")
+            property("sonar.sources", "src/main/java, src/app")
             property("sonar.dynamicAnalysis", "reuseReports")
             property("sonar.java.coveragePlugin", "jacoco")
             property("sonar.exclusions", "**/node_modules/**,**/src/app/api/**,**/*.spec.ts,**/*.module.ts,**/*.routes.ts,**/karma.conf.js,**/instana.js,**/polyfills.ts,**/cypress/**,**/db/migration/**/*")
+            property("sonar.test.inclusion", "**/*.spec.ts")
+            property("sonar.ts.tslint.configPath", "tslint.json")
+            property("sonar.ts.coverage.lcovReportPath", "coverage/atlas-frontend/lcov.info")
+            property("sonar.typescript.lcov.reportPaths", "${project.projectDir}/coverage/atlas-frontend/lcov.info")
         }
     }
 }
