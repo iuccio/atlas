1c309523cc5d1362f2f49794d46ca84fc293602e
atlas <19825793+iuccio@users.noreply.github.com>
2024-11-29 14:59:09 +0100

    ATLAS-2147: try to run sonarqube correctly

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,15 +4,7 @@ plugins {
     id("org.sonarqube") version "6.0.1.5171"
 }
 
-sonar {
-    properties {
-        property("sonar.projectName", "ATLAS - SKI Business Application")
-        property("sonar.projectKey", "ch.sbb.atlas:atlas")
-        property("sonar.dynamicAnalysis", "reuseReports")
-        property("sonar.java.coveragePlugin", "jacoco")
-        property("sonar.exclusions", "**/node_modules/**,**/src/app/api/**,**/*.spec.ts,**/*.module.ts,**/*.routes.ts,**/karma.conf.js,**/instana.js,**/polyfills.ts,**/cypress/**,**/db/migration/**/*")
-    }
-}
+
 
 subprojects {
     apply(plugin = "org.sonarqube")
@@ -25,6 +17,16 @@ subprojects {
         imports {
             mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
             mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+        }
+    }
+
+    sonar {
+        properties {
+            property("sonar.projectName", "ATLAS - SKI Business Application")
+            property("sonar.projectKey", "ch.sbb.atlas:atlas")
+            property("sonar.dynamicAnalysis", "reuseReports")
+            property("sonar.java.coveragePlugin", "jacoco")
+            property("sonar.exclusions", "**/node_modules/**,**/src/app/api/**,**/*.spec.ts,**/*.module.ts,**/*.routes.ts,**/karma.conf.js,**/instana.js,**/polyfills.ts,**/cypress/**,**/db/migration/**/*")
         }
     }
 }
