23214e2ccc47273547eebcde4293477786c4035b
atlas <19825793+iuccio@users.noreply.github.com>
2024-12-02 11:12:35 +0100

    ATLAS-2147: build user-admi-sec and workflow with gradle

---
+++ user-administration-security/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("org.openapi.generator") version "7.10.0"
+    id("buildlogic.java-conventions")
+}
+
+
+description= "Atlas User Administration Security Handler"
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation(project(":base-atlas"))
+    implementation("org.springframework.security:spring-security-oauth2-client")
+    implementation("org.springframework.security:spring-security-oauth2-authorization-server")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation(project(":kafka"))
+    implementation("org.springframework.kafka:spring-kafka")//get this dependency from :kafka use as api does not work
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation(project(":base-atlas", "test"))
+    testImplementation(project(":auto-rest-doc"))
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:postgresql")
+
+    testRuntimeOnly("org.postgresql:postgresql")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.bootJar {
+    enabled = false
+}
+
---
+++ workflow/build.gradle.kts
@@ -0,0 +1,53 @@
+import java.util.*
+
+plugins {
+    id("buildlogic.java-conventions")
+}
+
+description = "Atlas Scheduling Service"
+extra["shedlockVersion"] = "5.16.0"
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${property("springOpenapiUiVersion")}")
+    implementation("commons-codec:commons-codec")
+
+    implementation("org.flywaydb:flyway-database-postgresql")
+    implementation("org.flywaydb:flyway-core")
+
+    implementation(project(":base-atlas"))
+    implementation(project(":kafka"))
+    implementation("org.springframework.kafka:spring-kafka")//get this dependency from :kafka use as api does not work
+    implementation(project(":user-administration-security"))
+
+    runtimeOnly("org.postgresql:postgresql")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+    testImplementation("org.springframework.kafka:spring-kafka-test")//get this dependency from :kafka use as api does not work
+    testImplementation("org.mockito:mockito-inline:5.2.0")
+    testImplementation(project(":base-atlas", "test"))
+    testImplementation(project(":auto-rest-doc"))
+
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:postgresql")
+
+    testRuntimeOnly("org.postgresql:postgresql")
+
+}
+
+springBoot {
+    buildInfo {
+        properties {
+            additional.set(mapOf(
+                    "time" to "${Date()}"
+            ))
+        }
+    }
+}
