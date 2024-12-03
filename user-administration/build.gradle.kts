887c06a84996380fef67d1e698dcd35700bde9f2
atlas <19825793+iuccio@users.noreply.github.com>
2024-12-03 08:36:24 +0100

    ATLAS-2147: build user-administration with gradle

---
+++ user-administration/build.gradle.kts
@@ -0,0 +1,54 @@
+import java.util.*
+
+plugins {
+    id("buildlogic.java-conventions")
+}
+
+description = "Atlas User Administration"
+extra["microsoftGraphSdkVersion"] = "6.19.0"
+extra["azureIdentityVersion"] = "1.14.0"
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.security:spring-security-oauth2-authorization-server")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${property("springOpenapiUiVersion")}")
+    implementation("com.microsoft.graph:microsoft-graph:${property("microsoftGraphSdkVersion")}")
+    implementation("com.azure:azure-identity:${property("azureIdentityVersion")}")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    implementation(project(":base-atlas"))
+    implementation(project(":kafka"))
+    implementation("org.springframework.kafka:spring-kafka")
+    implementation("org.hibernate.orm:hibernate-jpamodelgen")
+    annotationProcessor("org.hibernate.orm:hibernate-jpamodelgen:6.6.3.Final")
+
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-database-postgresql")
+    runtimeOnly("org.postgresql:postgresql")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+    testImplementation(project(":base-atlas", "test"))
+    testImplementation(project(":auto-rest-doc"))
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.springframework.kafka:spring-kafka-test")
+
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:postgresql")
+
+    testRuntimeOnly("org.postgresql:postgresql")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.mockito:mockito-inline:5.2.0")
+}
+
+springBoot {
+    buildInfo {
+        properties {
+            additional.set(mapOf(
+                    "time" to "${Date()}"
+            ))
+        }
+    }
+}
