e31162fabc7d553b394c75f520bf276a2cb3ac81
atlas <TechSupport-ATLAS@sbb.ch>
2024-12-03 09:56:06 +0100

    ATLAS-2147: build gateway with gradle

---
+++ gateway/build.gradle.kts
@@ -0,0 +1,27 @@
+import java.util.*
+
+plugins {
+    id("buildlogic.java-conventions")
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-tracing")
+    implementation("io.micrometer:micrometer-tracing-bridge-brave")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    implementation("org.springframework.boot:spring-boot-configuration-processor")//optional
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+springBoot {
+    buildInfo {
+        properties {
+            additional.set(mapOf(
+                    "time" to "${Date()}"
+            ))
+        }
+    }
+}
