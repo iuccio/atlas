487657fbc5b87f6342b02d80f00f119b614fbf4e
atlas <19825793+iuccio@users.noreply.github.com>
2024-12-07 14:15:27 +0100

    ATLAS-2147: small ref

--- atlas-gradle-ci-plugin/src/main/kotlin/buildlogic.java-conventions.gradle.kts
+++ atlas-gradle-ci-plugin/src/main/kotlin/buildlogic.java-conventions.gradle.kts
@@ -91,9 +91,15 @@ tasks.withType<Javadoc> {
 tasks.test {
     finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
 }
+
 tasks.jacocoTestReport {
     dependsOn(tasks.test) // tests are required to run before generating the report
     reports {
         xml.required = true
     }
+}
+
+tasks.withType<Jar> {
+    enabled = true
+    archiveClassifier.set("")
 }
