a7a4f8e4cff2a65fa5aa4ef91eef21a23a191ca9
atlas <19825793+iuccio@users.noreply.github.com>
2024-12-10 13:17:14 +0100

    ATLAS-2040 fixed location-service issue

--- location/src/main/java/ch/sbb/atlas/location/service/SloidService.java
+++ location/src/main/java/ch/sbb/atlas/location/service/SloidService.java
@@ -6,6 +6,7 @@ import ch.sbb.atlas.servicepoint.Country;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,56 +15,69 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class SloidService {
 
-  private final SloidRepository sloidRepository;
+    private final SloidRepository sloidRepository;
 
-  public String generateNewSloid(String sloidPrefix, @NotNull SloidType sloidType) {
-    if (SloidType.SERVICE_POINT == sloidType) {
-      throw new IllegalArgumentException("This method is not allowed to generate sloid for ServicePoint.");
+    public String generateNewSloid(String sloidPrefix, @NotNull SloidType sloidType) {
+        if (SloidType.SERVICE_POINT == sloidType) {
+            throw new IllegalArgumentException("This method is not allowed to generate sloid for ServicePoint.");
+        }
+        String sloidToInsert = null;
+        do {
+            final Integer nextSeqValue = sloidRepository.getNextSeqValue(sloidType);
+            final String sloid = createFormattedSloid(sloidPrefix, nextSeqValue);
+            if (!sloidRepository.isSloidAllocated(sloid)) {
+                sloidToInsert = sloid;
+            }
+        } while (sloidToInsert == null);
+        sloidRepository.insertSloid(sloidToInsert, sloidType);
+        return sloidToInsert;
     }
-    String sloidToInsert = null;
-    do {
-      final Integer nextSeqValue = sloidRepository.getNextSeqValue(sloidType);
-      final String sloid = createFormattedSloid(sloidPrefix, nextSeqValue);
-      if (!sloidRepository.isSloidAllocated(sloid)) {
-        sloidToInsert = sloid;
-      }
-    } while (sloidToInsert == null);
-    sloidRepository.insertSloid(sloidToInsert, sloidType);
-    return sloidToInsert;
-  }
 
-  @Transactional
-  public String getNextAvailableServicePointSloid(Country country) {
-    final String nextAvailableSloid = sloidRepository.getNextAvailableSloid(country);
-    sloidRepository.insertSloid(nextAvailableSloid, SloidType.SERVICE_POINT);
-    sloidRepository.setAvailableSloidToClaimed(nextAvailableSloid);
-    return nextAvailableSloid;
-  }
-
-  @Transactional
-  public boolean claimAvailableServicePointSloid(String sloid) {
-    if (sloidRepository.isServicePointSloidAvailable(sloid)) {
-      sloidRepository.insertSloid(sloid, SloidType.SERVICE_POINT);
-      sloidRepository.setAvailableSloidToClaimed(sloid);
-      return true;
-    } else {
-      log.info("Could not claim Service Point sloid: {}", sloid);
-      return false;
+    public String getNextAvailableServicePointSloid(Country country) {
+        do {
+            try {
+                //was wenn es beim sql script "setToClaimed" fehler gibt aber insert korrekt ging
+                final String nextAvailableSloid = sloidRepository.getNextAvailableSloid(country);
+                sloidRepository.insertSloid(nextAvailableSloid, SloidType.SERVICE_POINT);
+                sloidRepository.setAvailableSloidToClaimed(nextAvailableSloid);
+                return nextAvailableSloid;
+            } catch (DuplicateKeyException e) {
+                //continue
+            }
+        } while (true);
     }
-  }
 
-  public boolean claimSloid(String sloid, @NotNull SloidType sloidType) {
-    if (!sloidRepository.isSloidAllocated(sloid)) {
-      sloidRepository.insertSloid(sloid, sloidType);
-      return true;
-    } else {
-      log.info("Could not claim {} sloid: {}", sloidType, sloid);
-      return false;
+    @Transactional
+    public boolean claimAvailableServicePointSloid(String sloid) {
+        if (sloidRepository.isServicePointSloidAvailable(sloid)) {
+            sloidRepository.insertSloid(sloid, SloidType.SERVICE_POINT);
+            sloidRepository.setAvailableSloidToClaimed(sloid);
+            return true;
+        } else {
+            log.info("Could not claim Service Point sloid: {}", sloid);
+            return false;
+        }
     }
-  }
 
-  private String createFormattedSloid(String sloidPrefix, Integer nextSeqValue) {
-    return sloidPrefix + ":" + nextSeqValue;
-  }
+    public boolean claimSloid(String sloid, @NotNull SloidType sloidType) {
+        if (!sloidRepository.isSloidAllocated(sloid)) {
+            sloidRepository.insertSloid(sloid, sloidType);
+            return true;
+        } else {
+            log.info("Could not claim {} sloid: {}", sloidType, sloid);
+            return false;
+        }
+    }
+
+    private String createFormattedSloid(String sloidPrefix, Integer nextSeqValue) {
+        return sloidPrefix + ":" + nextSeqValue;
+    }
+
+    private synchronized String generateAndSaveAvailableSloid(Country country) {
+        String nextAvailableSloid = sloidRepository.getNextAvailableSloid(country);
+        sloidRepository.insertSloid(nextAvailableSloid, SloidType.SERVICE_POINT);
+        sloidRepository.setAvailableSloidToClaimed(nextAvailableSloid);
+        return nextAvailableSloid;
+    }
 
 }
