3904dacfce5e44f792d93c9a643e41611beedb31
atlas <TechSupport-ATLAS@sbb.ch>
2024-12-12 16:23:00 +0100

    ATLAS-2147: fix versions gradle

--- user-administration/build.gradle.kts
+++ user-administration/build.gradle.kts
@@ -5,6 +5,9 @@ plugins {
     id("buildlogic.java-restdoc")
 }
 
+group = "ch.sbb.atlas"
+version = "2.372.0"
+
 description = "Atlas User Administration"
 extra["microsoftGraphSdkVersion"] = "6.19.0"
 extra["azureIdentityVersion"] = "1.14.0"
