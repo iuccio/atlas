c4f3b193724a5918a99c6a717f4e15a59b3653b8
atlas <TechSupport-ATLAS@sbb.ch>
2025-01-06 11:56:19 +0100

    [tekton-build-bot] updated version to 2.375.0

--- api-auth-gateway/build.gradle.kts
+++ api-auth-gateway/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.374.0"
+version = "2.375.0"
 
 dependencies {
     implementation(platform("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}"))
