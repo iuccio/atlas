bed3f57fe9daa35f065433f01bf8a86f6a949bf3
atlas <19825793+iuccio@users.noreply.github.com>
2025-01-07 09:12:17 +0100

    [tekton-build-bot] updated version to 2.377.0

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.376.0"
+version = "2.377.0"
 
 dependencies {
     implementation(platform("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}"))
