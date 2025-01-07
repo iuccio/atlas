2d0cb0ab21b442bd8075c31073e8f8c6d0b6dd0a
atlas <TechSupport-ATLAS@sbb.ch>
2025-01-07 15:37:45 +0100

    ATLAS-2666: fix sonar config

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -1,28 +1,5 @@
 import org.apache.tools.ant.taskdefs.condition.Os
 
-plugins {
-  id("org.sonarqube")
-}
-
-sonar {
-  properties {
-    property(
-      "sonar.exclusions",
-      "**/node_modules/**,**/src/app/api/**,**/*.spec.ts,**/*.module.ts,**/*.routes.ts,**/karma.conf.js,**/*.kts"
-    )
-    property("sonar.sources", "./")
-    property("sonar.language", "ts")
-    property("sonar.profile", "TsLint")
-    property("sonar.verbose", "true")
-    property("sonar.test.inclusion", "**/*.spec.ts")
-    property("sonar.ts.tslint.configPath", "tslint.json")
-    property("sonar.ts.coverage.lcovReportPath", "coverage/atlas-frontend/lcov.info")
-    property("sonar.typescript.lcov.reportPaths", "${project.projectDir}/coverage/atlas-frontend/lcov.info")
-    property("sonar.coverage.exclusions", "**/*.spec.ts,**/src/app/api/**,**/cypress/**,/**/*.module.ts")
-  }
-}
-
-
 task<Exec>("execNpmCi") {
   doFirst {
     println("[Angular] Run atlas npm ci")
