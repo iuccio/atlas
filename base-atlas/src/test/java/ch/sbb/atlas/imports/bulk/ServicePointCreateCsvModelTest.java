fcbecc24db237c4eb0a23062044c409419f5b18a
atlas <TechSupport-ATLAS@sbb.ch>
2025-01-13 07:08:34 +0100

    ATLAS-2629 clean coded, added tests

--- base-atlas/src/test/java/ch/sbb/atlas/imports/bulk/ServicePointCreateCsvModelTest.java
+++ base-atlas/src/test/java/ch/sbb/atlas/imports/bulk/ServicePointCreateCsvModelTest.java
@@ -1,6 +1,8 @@
 package ch.sbb.atlas.imports.bulk;
 
+import ch.sbb.atlas.api.servicepoint.SpatialReference;
 import ch.sbb.atlas.imports.model.create.ServicePointCreateCsvModel;
+import ch.sbb.atlas.servicepoint.enumeration.StopPointType;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -22,7 +24,7 @@ class ServicePointCreateCsvModelTest {
     }
 
     @Test
-    void shouldReportErrorsInServicePointUpdateModel() {
+    void shouldReportErrorsInServicePointCreateModel() {
         ServicePointCreateCsvModel bern = ServicePointCreateCsvModel.builder()
                 .build();
         assertThat(bern.validate()).hasSize(6);
@@ -63,4 +65,83 @@ class ServicePointCreateCsvModelTest {
                 .build();
         assertThat(bern.validate()).isEmpty();
     }
+
+    @Test
+    void shouldValidateNumberShort() {
+        ServicePointCreateCsvModel bern = ServicePointCreateCsvModel.builder()
+                .uicCountryCode(81)
+                .numberShort(44444444)
+                .validFrom(LocalDate.of(2021, 4, 1))
+                .validTo(LocalDate.of(2099, 12, 31))
+                .designationOfficial("Bern")
+                .businessOrganisation("abc")
+                .build();
+
+        assertThat(bern.validate()).hasSize(1);
+
+    }
+
+    @Test
+    void shouldValidateUicCountryCode() {
+        ServicePointCreateCsvModel bern = ServicePointCreateCsvModel.builder()
+                .uicCountryCode(35)
+                .numberShort(12345)
+                .validFrom(LocalDate.of(2021, 4, 1))
+                .validTo(LocalDate.of(2099, 12, 31))
+                .designationOfficial("Bern")
+                .businessOrganisation("abc")
+                .build();
+
+        assertThat(bern.validate()).hasSize(1);
+    }
+
+    @Test
+    void shouldValidateHeight() {
+        ServicePointCreateCsvModel bern = ServicePointCreateCsvModel.builder()
+                .uicCountryCode(85)
+                .validFrom(LocalDate.of(2021, 4, 1))
+                .validTo(LocalDate.of(2099, 12, 31))
+                .designationOfficial("Bern")
+                .businessOrganisation("abc")
+                .spatialReference(SpatialReference.LV95)
+                .north(1207167.52455)
+                .east(2601352.9171)
+                .height(100000000.0)
+                .build();
+
+        assertThat(bern.validate()).hasSize(1);
+    }
+
+    @Test
+    void shouldValidateGeography() {
+        ServicePointCreateCsvModel bern = ServicePointCreateCsvModel.builder()
+                .uicCountryCode(85)
+                .validFrom(LocalDate.of(2021, 4, 1))
+                .validTo(LocalDate.of(2099, 12, 31))
+                .designationOfficial("Bern")
+                .businessOrganisation("abc")
+                .spatialReference(null)
+                .north(1207167.52455)
+                .east(2601352.9171)
+                .height(100000000.0)
+                .build();
+
+        assertThat(bern.validate()).hasSize(1);
+    }
+
+    @Test
+    void shouldValidateStopPointType() {
+        ServicePointCreateCsvModel bern = ServicePointCreateCsvModel.builder()
+                .uicCountryCode(85)
+                .validFrom(LocalDate.of(2021, 4, 1))
+                .validTo(LocalDate.of(2099, 12, 31))
+                .designationOfficial("Bern")
+                .businessOrganisation("abc")
+                .stopPointType(StopPointType.ORDERLY)
+                .meansOfTransport(null)
+                .build();
+
+        assertThat(bern.validate()).hasSize(1);
+    }
+
 }
