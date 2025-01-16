6fe845c1122405a3cbab8a05adbfeeedbf66cb33
atlas <19825793+iuccio@users.noreply.github.com>
2025-01-16 17:15:16 +0100

    [tekton-build-bot] updated version to 2.386.1-SNAPSHOT

--- base-atlas/build.gradle.kts
+++ base-atlas/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.386.0"
+version = "2.386.1-SNAPSHOT"
 
 configurations {
     create("test") //used to create the base-atlas-test jar
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.386.0"
+version = "2.386.1-SNAPSHOT"
 
 subprojects {
         sonar {
