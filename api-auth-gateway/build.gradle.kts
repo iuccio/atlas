4aa6de859fdd32a62abe485131df0e2999cd2ae2
atlas <TechSupport-ATLAS@sbb.ch>
2025-01-20 14:20:24 +0100

    [tekton-build-bot] updated version to 2.396.0

--- api-auth-gateway/build.gradle.kts
+++ api-auth-gateway/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.395.0"
+version = "2.396.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
--- export-service-point/build.gradle.kts
+++ export-service-point/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.395.0"
+version = "2.396.0"
 
 description = "Atlas Export Service Point"
 
