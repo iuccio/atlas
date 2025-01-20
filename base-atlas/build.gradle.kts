a70a01c0d0a8a51eaa3946b7c9e59803b8c92701
atlas <19825793+iuccio@users.noreply.github.com>
2025-01-20 07:32:41 +0100

    [tekton-build-bot] updated version to 2.393.0

--- base-atlas/build.gradle.kts
+++ base-atlas/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.392.0"
+version = "2.393.0"
 
 configurations {
     create("test") //used to create the base-atlas-test jar
--- user-administration/build.gradle.kts
+++ user-administration/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.392.0"
+version = "2.393.0"
 
 description = "Atlas User Administration"
 extra["microsoftGraphSdkVersion"] = "6.25.1"
