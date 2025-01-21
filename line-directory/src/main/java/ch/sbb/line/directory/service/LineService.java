5726a7986f8aa204c69202be609ef9acbd868e11
atlas <TechSupport-ATLAS@sbb.ch>
2025-01-21 09:05:25 +0100

    ATLAS-2589: make sonar happy

--- line-directory/src/main/java/ch/sbb/line/directory/service/LineService.java
+++ line-directory/src/main/java/ch/sbb/line/directory/service/LineService.java
@@ -56,15 +56,17 @@ public class LineService {
   }
 
   @Transactional
-  @PreAuthorize("""
-      @businessOrganisationBasedUserAdministrationService.hasUserPermissionsToCreate(#businessObject, T(ch.sbb.atlas.kafka.model.user.admin.ApplicationType).LIDI)""")
+  @PreAuthorize(
+      """
+          @businessOrganisationBasedUserAdministrationService.hasUserPermissionsToCreate(#businessObject, T(ch.sbb.atlas.kafka.model.user.admin.ApplicationType).LIDI)""")
   public LineVersion create(LineVersion businessObject) {
     return save(businessObject);
   }
 
   @Transactional
-  @PreAuthorize("""
-      @businessOrganisationBasedUserAdministrationService.hasUserPermissionsToUpdate(#editedVersion, #currentVersions, T(ch.sbb.atlas.kafka.model.user.admin.ApplicationType).LIDI)""")
+  @PreAuthorize(
+      """
+          @businessOrganisationBasedUserAdministrationService.hasUserPermissionsToUpdate(#editedVersion, #currentVersions, T(ch.sbb.atlas.kafka.model.user.admin.ApplicationType).LIDI)""")
   public void update(LineVersion currentVersion, LineVersion editedVersion, List<LineVersion> currentVersions) {
     updateVersion(currentVersion, editedVersion);
   }
