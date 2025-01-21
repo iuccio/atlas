ec6154844013907cf4c7a0ffa5d9d53b4b9ba394
atlas <19825793+iuccio@users.noreply.github.com>
2025-01-21 15:49:10 +0100

    ATLAS-2617: cleanup tests

---
+++ base-atlas/src/test/java/ch/sbb/atlas/imports/bulk/BulkImportUpdateDataMapperTest.java
@@ -0,0 +1,110 @@
+package ch.sbb.atlas.imports.bulk;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import ch.sbb.atlas.api.servicepoint.UpdateServicePointVersionModel;
+import ch.sbb.atlas.imports.model.ServicePointUpdateCsvModel;
+import ch.sbb.atlas.imports.model.ServicePointUpdateCsvModel.Fields;
+import ch.sbb.atlas.model.entity.BaseEntity;
+import ch.sbb.atlas.servicepoint.enumeration.Category;
+import ch.sbb.atlas.servicepoint.enumeration.MeanOfTransport;
+import ch.sbb.atlas.servicepoint.enumeration.OperatingPointTechnicalTimetableType;
+import ch.sbb.atlas.servicepoint.enumeration.OperatingPointTrafficPointType;
+import ch.sbb.atlas.servicepoint.enumeration.OperatingPointType;
+import ch.sbb.atlas.servicepoint.enumeration.StopPointType;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import lombok.Builder;
+import org.junit.jupiter.api.Test;
+
+class BulkImportUpdateDataMapperTest {
+
+  private static class MockMapper extends
+      BulkImportUpdateDataMapper<ServicePointUpdateCsvModel, BaseEntity, UpdateServicePointVersionModel> {
+
+  }
+
+  @Builder
+  private static class MockEntity extends BaseEntity {
+
+    private LocalDate validFrom;
+    private LocalDate validTo;
+    private String designationOfficial;
+    private String designationLong;
+    private StopPointType stopPointType;
+    private boolean freightServicePoint;
+    private OperatingPointType operatingPointType;
+    private OperatingPointTechnicalTimetableType operatingPointTechnicalTimetableType;
+    private Set<MeanOfTransport> meansOfTransport;
+    private Set<Category> categories;
+    private OperatingPointTrafficPointType operatingPointTrafficPointType;
+    private String sortCodeOfDestinationStation;
+    private String businessOrganisation;
+    private Long id;
+  }
+
+  @Test
+  void shouldApplyDefaultMapping() {
+    BulkImportUpdateContainer<ServicePointUpdateCsvModel> container =
+        BulkImportUpdateContainer.<ServicePointUpdateCsvModel>builder()
+            .object(ServicePointUpdateCsvModel.builder()
+                .sloid("sloid")
+                .validFrom(LocalDate.of(2014, 12, 14))
+                .validTo(LocalDate.of(2021, 3, 31))
+                .designationLong("Bern, am Wyleregg")
+                .meansOfTransport(Set.of(MeanOfTransport.BUS))
+                .build())
+            .build();
+    MockEntity currentEntity = MockEntity.builder()
+        .categories(Collections.emptySet())
+        .designationOfficial("Bern, Wyleregg")
+        .build();
+
+    UpdateServicePointVersionModel result = new MockMapper().applyUpdate(container, currentEntity,
+        new UpdateServicePointVersionModel());
+    assertThat(result.getDesignationLong()).isEqualTo("Bern, am Wyleregg");
+    assertThat(result.getMeansOfTransport()).containsExactly(MeanOfTransport.BUS);
+    assertThat(result.getDesignationOfficial()).isEqualTo("Bern, Wyleregg");
+  }
+
+  @Test
+  void shouldApplyCopyFromCurrentVersion() {
+    BulkImportUpdateContainer<ServicePointUpdateCsvModel> container =
+        BulkImportUpdateContainer.<ServicePointUpdateCsvModel>builder()
+            .object(ServicePointUpdateCsvModel.builder()
+                .sloid("sloid")
+                .validFrom(LocalDate.of(2014, 12, 14))
+                .validTo(LocalDate.of(2021, 3, 31))
+                .designationLong("Bern, am Wyleregg")
+                .meansOfTransport(Set.of(MeanOfTransport.BUS))
+                .build())
+            .build();
+    MockEntity currentEntity = MockEntity.builder().id(4L).build();
+
+    UpdateServicePointVersionModel result = new MockMapper().applyUpdate(container, currentEntity,
+        new UpdateServicePointVersionModel());
+    assertThat(result.getId()).isEqualTo(4L);
+  }
+
+  @Test
+  void shouldApplyNullingAndThrowExceptionOnUnallowedProperty() {
+    BulkImportUpdateContainer<ServicePointUpdateCsvModel> container =
+        BulkImportUpdateContainer.<ServicePointUpdateCsvModel>builder()
+            .object(ServicePointUpdateCsvModel.builder()
+                .sloid("sloid")
+                .validFrom(LocalDate.of(2014, 12, 14))
+                .validTo(LocalDate.of(2021, 3, 31))
+                .meansOfTransport(Set.of(MeanOfTransport.BUS))
+                .build())
+            .attributesToNull(List.of(Fields.businessOrganisation))
+            .build();
+    MockEntity currentEntity = MockEntity.builder().build();
+
+    assertThatExceptionOfType(AttributeNullingNotSupportedException.class).isThrownBy(
+        () -> new MockMapper().applyUpdate(container, currentEntity, new UpdateServicePointVersionModel()));
+  }
+
+}
--- service-point-directory/src/test/java/ch/sbb/atlas/servicepointdirectory/service/servicepoint/bulk/ServicePointBulkImportUpdateTest.java
+++ service-point-directory/src/test/java/ch/sbb/atlas/servicepointdirectory/service/servicepoint/bulk/ServicePointBulkImportUpdateTest.java
@@ -1,10 +1,8 @@
 package ch.sbb.atlas.servicepointdirectory.service.servicepoint.bulk;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import ch.sbb.atlas.api.servicepoint.UpdateServicePointVersionModel;
-import ch.sbb.atlas.imports.bulk.AttributeNullingNotSupportedException;
 import ch.sbb.atlas.imports.bulk.BulkImportUpdateContainer;
 import ch.sbb.atlas.imports.model.ServicePointUpdateCsvModel;
 import ch.sbb.atlas.imports.model.ServicePointUpdateCsvModel.Fields;
@@ -13,53 +11,11 @@ import ch.sbb.atlas.servicepoint.enumeration.MeanOfTransport;
 import ch.sbb.atlas.servicepointdirectory.entity.ServicePointVersion;
 import ch.sbb.atlas.servicepointdirectory.entity.geolocation.ServicePointGeolocation;
 import java.time.LocalDate;
-import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 class ServicePointBulkImportUpdateTest {
-
-  @Test
-  void shouldApplyDefaultMapping() {
-    BulkImportUpdateContainer<ServicePointUpdateCsvModel> container =
-        BulkImportUpdateContainer.<ServicePointUpdateCsvModel>builder()
-            .object(ServicePointUpdateCsvModel.builder()
-                .sloid("sloid")
-                .validFrom(LocalDate.of(2014, 12, 14))
-                .validTo(LocalDate.of(2021, 3, 31))
-                .designationLong("Bern, am Wyleregg")
-                .meansOfTransport(Set.of(MeanOfTransport.BUS))
-                .build())
-            .build();
-    ServicePointVersion currentEntity = ServicePointVersion.builder()
-        .categories(Collections.emptySet())
-        .designationOfficial("Bern, Wyleregg")
-        .build();
-
-    UpdateServicePointVersionModel result = ServicePointBulkImportUpdate.apply(container, currentEntity);
-    assertThat(result.getDesignationLong()).isEqualTo("Bern, am Wyleregg");
-    assertThat(result.getMeansOfTransport()).containsExactly(MeanOfTransport.BUS);
-    assertThat(result.getDesignationOfficial()).isEqualTo("Bern, Wyleregg");
-  }
-
-  @Test
-  void shouldApplyCopyFromCurrentVersion() {
-    BulkImportUpdateContainer<ServicePointUpdateCsvModel> container =
-        BulkImportUpdateContainer.<ServicePointUpdateCsvModel>builder()
-            .object(ServicePointUpdateCsvModel.builder()
-                .sloid("sloid")
-                .validFrom(LocalDate.of(2014, 12, 14))
-                .validTo(LocalDate.of(2021, 3, 31))
-                .designationLong("Bern, am Wyleregg")
-                .meansOfTransport(Set.of(MeanOfTransport.BUS))
-                .build())
-            .build();
-    ServicePointVersion currentEntity = ServicePointVersion.builder().id(4L).build();
-
-    UpdateServicePointVersionModel result = ServicePointBulkImportUpdate.apply(container, currentEntity);
-    assertThat(result.getId()).isEqualTo(4L);
-  }
 
   @Test
   void shouldApplyNullingAfterDefaultMapping() {
@@ -88,24 +44,6 @@ class ServicePointBulkImportUpdateTest {
     assertThat(result.getServicePointGeolocation()).isNull();
     assertThat(result.getCategories()).isEmpty();
     assertThat(result.getDesignationLong()).isNull();
-  }
-
-  @Test
-  void shouldApplyNullingAndThrowExceptionOnUnallowedProperty() {
-    BulkImportUpdateContainer<ServicePointUpdateCsvModel> container =
-        BulkImportUpdateContainer.<ServicePointUpdateCsvModel>builder()
-            .object(ServicePointUpdateCsvModel.builder()
-                .sloid("sloid")
-                .validFrom(LocalDate.of(2014, 12, 14))
-                .validTo(LocalDate.of(2021, 3, 31))
-                .meansOfTransport(Set.of(MeanOfTransport.BUS))
-                .build())
-            .attributesToNull(List.of(Fields.businessOrganisation))
-            .build();
-    ServicePointVersion currentEntity = ServicePointVersion.builder().build();
-
-    assertThatExceptionOfType(AttributeNullingNotSupportedException.class).isThrownBy(
-        () -> ServicePointBulkImportUpdate.apply(container, currentEntity));
   }
 
 }
