f189ca807e12b5bc18baba050464301ca4ecbf63
atlas <19825793+iuccio@users.noreply.github.com>
2025-01-27 06:01:37 +0100

    [tekton-build-bot] updated version to 2.416.0

--- import-service-point/build.gradle.kts
+++ import-service-point/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.415.0"
+version = "2.416.0"
 
 description = "Atlas Import Service Point"
 
--- kafka/build.gradle.kts
+++ kafka/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.415.0"
+version = "2.416.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
