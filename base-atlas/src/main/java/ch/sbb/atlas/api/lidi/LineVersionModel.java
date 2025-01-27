c218283525b1315a1e2e38022222ef34250bd3dc
atlas <TechSupport-ATLAS@sbb.ch>
2025-01-27 15:42:10 +0100

    ATLAS-2399: Use constants

--- base-atlas/src/main/java/ch/sbb/atlas/api/lidi/LineVersionModel.java
+++ base-atlas/src/main/java/ch/sbb/atlas/api/lidi/LineVersionModel.java
@@ -35,7 +35,7 @@ public class LineVersionModel extends BaseLineVersionModel {
 
   @Schema(description = "SwissLineNumber", example = "b1.L1")
   @NotBlank
-  @Size(min = 1, max = 50)
+  @Size(min = 1, max = AtlasFieldLengths.LENGTH_50)
   @Pattern(regexp = AtlasCharacterSetsRegex.SID4PT)
   private String swissLineNumber;
 
