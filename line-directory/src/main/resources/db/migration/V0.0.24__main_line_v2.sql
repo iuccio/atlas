ae527c67e9c4e7cbdd61115c80189c389e56fc5a
atlas <TechSupport-ATLAS@sbb.ch>
2025-01-27 14:06:21 +0100

    ATLAS-2399: V1 dont return lines without swissLineNumber

--- line-directory/src/main/resources/db/migration/V0.0.24__main_line_v2.sql
+++ line-directory/src/main/resources/db/migration/V0.0.24__main_line_v2.sql
@@ -2,3 +2,15 @@ ALTER TABLE line_version
     ALTER COLUMN description set NOT NULL;
 ALTER TABLE line_version
     ALTER COLUMN number set NOT NULL;
+
+-- Orderly => concessionType mandatory => EK
+update line_version
+set concession_type = 'FEDERALLY_LICENSED_OR_APPROVED_LINE'
+where line_type = 'ORDERLY'
+  and concession_type is null;
+
+-- !Orderly => swissLineNumber and concessionType have to be null
+update line_version
+set swiss_line_number = null,
+    concession_type = null
+where line_type != 'ORDERLY';
