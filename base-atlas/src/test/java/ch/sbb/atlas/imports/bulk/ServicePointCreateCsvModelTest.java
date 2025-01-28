ed04163cd7b184d7cf43218666e14f5d5c4e3c0c
atlas <19825793+iuccio@users.noreply.github.com>
2025-01-28 13:13:37 +0100

    ATLAS-2636: work on test coverage

--- base-atlas/src/test/java/ch/sbb/atlas/imports/bulk/ServicePointCreateCsvModelTest.java
+++ base-atlas/src/test/java/ch/sbb/atlas/imports/bulk/ServicePointCreateCsvModelTest.java
@@ -1,147 +1,172 @@
 package ch.sbb.atlas.imports.bulk;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import ch.sbb.atlas.api.servicepoint.SpatialReference;
 import ch.sbb.atlas.imports.model.create.ServicePointCreateCsvModel;
 import ch.sbb.atlas.servicepoint.enumeration.StopPointType;
-import org.junit.jupiter.api.Test;
-
 import java.time.LocalDate;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
 
 class ServicePointCreateCsvModelTest {
 
-    @Test
-    void shouldBeValidServicePointCreateModelSwitzerland() {
-        ServicePointCreateCsvModel bern = ServicePointCreateCsvModel.builder()
-                .uicCountryCode(85)
-                .validFrom(LocalDate.of(2021, 4, 1))
-                .validTo(LocalDate.of(2099, 12, 31))
-                .designationOfficial("Bern")
-                .businessOrganisation("abc")
-                .build();
-        assertThat(bern.validate()).isEmpty();
-    }
+  @Test
+  void shouldBeValidServicePointCreateModelSwitzerland() {
+    ServicePointCreateCsvModel bern = ServicePointCreateCsvModel.builder()
+        .uicCountryCode(85)
+        .validFrom(LocalDate.of(2021, 4, 1))
+        .validTo(LocalDate.of(2099, 12, 31))
+        .designationOfficial("Bern")
+        .businessOrganisation("abc")
+        .build();
+    assertThat(bern.validate()).isEmpty();
+  }
 
-    @Test
-    void shouldReportErrorsInServicePointCreateModel() {
-        ServicePointCreateCsvModel bern = ServicePointCreateCsvModel.builder()
-                .build();
-        assertThat(bern.validate()).hasSize(6);
-    }
+  @Test
+  void shouldReportErrorsInServicePointCreateModel() {
+    ServicePointCreateCsvModel bern = ServicePointCreateCsvModel.builder()
+        .build();
+    assertThat(bern.validate()).hasSize(6);
+  }
 
-    @Test
-    void shouldReportInvalidServicePointMissingBO() {
-        ServicePointCreateCsvModel bern = ServicePointCreateCsvModel.builder()
-                .uicCountryCode(85)
-                .validFrom(LocalDate.of(2021, 4, 1))
-                .validTo(LocalDate.of(2099, 12, 31))
-                .designationOfficial("Bern")
-                .build();
-        assertThat(bern.validate()).hasSize(1);
-    }
+  @Test
+  void shouldReportInvalidServicePointMissingBO() {
+    ServicePointCreateCsvModel bern = ServicePointCreateCsvModel.builder()
+        .uicCountryCode(85)
+        .validFrom(LocalDate.of(2021, 4, 1))
+        .validTo(LocalDate.of(2099, 12, 31))
+        .designationOfficial("Bern")
+        .build();
+    assertThat(bern.validate()).hasSize(1);
+  }
 
-    @Test
-    void shouldReportInvalidServicePointMissingNumberShort() {
-        ServicePointCreateCsvModel bern = ServicePointCreateCsvModel.builder()
-                .uicCountryCode(81)
-                .validFrom(LocalDate.of(2021, 4, 1))
-                .validTo(LocalDate.of(2099, 12, 31))
-                .designationOfficial("Bern")
-                .businessOrganisation("abc")
-                .build();
-        assertThat(bern.validate()).hasSize(1);
-    }
+  @Test
+  void shouldReportInvalidServicePointMissingNumberShort() {
+    ServicePointCreateCsvModel bern = ServicePointCreateCsvModel.builder()
+        .uicCountryCode(81)
+        .validFrom(LocalDate.of(2021, 4, 1))
+        .validTo(LocalDate.of(2099, 12, 31))
+        .designationOfficial("Bern")
+        .businessOrganisation("abc")
+        .build();
+    assertThat(bern.validate()).hasSize(1);
+  }
 
-    @Test
-    void shouldBeValidServicePointCreateModel() {
-        ServicePointCreateCsvModel bern = ServicePointCreateCsvModel.builder()
-                .uicCountryCode(81)
-                .numberShort(12345)
-                .validFrom(LocalDate.of(2021, 4, 1))
-                .validTo(LocalDate.of(2099, 12, 31))
-                .designationOfficial("Bern")
-                .businessOrganisation("abc")
-                .build();
-        assertThat(bern.validate()).isEmpty();
-    }
+  @Test
+  void shouldBeValidServicePointCreateModel() {
+    ServicePointCreateCsvModel bern = ServicePointCreateCsvModel.builder()
+        .uicCountryCode(81)
+        .numberShort(12345)
+        .validFrom(LocalDate.of(2021, 4, 1))
+        .validTo(LocalDate.of(2099, 12, 31))
+        .designationOfficial("Bern")
+        .businessOrganisation("abc")
+        .build();
+    assertThat(bern.validate()).isEmpty();
+  }
 
-    @Test
-    void shouldValidateNumberShort() {
-        ServicePointCreateCsvModel bern = ServicePointCreateCsvModel.builder()
-                .uicCountryCode(81)
-                .numberShort(44444444)
-                .validFrom(LocalDate.of(2021, 4, 1))
-                .validTo(LocalDate.of(2099, 12, 31))
-                .designationOfficial("Bern")
-                .businessOrganisation("abc")
-                .build();
+  @Test
+  void shouldValidateNumberShort() {
+    ServicePointCreateCsvModel bern = ServicePointCreateCsvModel.builder()
+        .uicCountryCode(81)
+        .numberShort(44444444)
+        .validFrom(LocalDate.of(2021, 4, 1))
+        .validTo(LocalDate.of(2099, 12, 31))
+        .designationOfficial("Bern")
+        .businessOrganisation("abc")
+        .build();
 
-        assertThat(bern.validate()).hasSize(1);
+    assertThat(bern.validate()).hasSize(1);
 
-    }
+  }
 
-    @Test
-    void shouldValidateUicCountryCode() {
-        ServicePointCreateCsvModel bern = ServicePointCreateCsvModel.builder()
-                .uicCountryCode(35)
-                .numberShort(12345)
-                .validFrom(LocalDate.of(2021, 4, 1))
-                .validTo(LocalDate.of(2099, 12, 31))
-                .designationOfficial("Bern")
-                .businessOrganisation("abc")
-                .build();
+  @Test
+  void shouldValidateUicCountryCode() {
+    ServicePointCreateCsvModel bern = ServicePointCreateCsvModel.builder()
+        .uicCountryCode(35)
+        .numberShort(12345)
+        .validFrom(LocalDate.of(2021, 4, 1))
+        .validTo(LocalDate.of(2099, 12, 31))
+        .designationOfficial("Bern")
+        .businessOrganisation("abc")
+        .build();
 
-        assertThat(bern.validate()).hasSize(1);
-    }
+    assertThat(bern.validate()).hasSize(1);
+  }
 
-    @Test
-    void shouldValidateHeight() {
-        ServicePointCreateCsvModel bern = ServicePointCreateCsvModel.builder()
-                .uicCountryCode(85)
-                .validFrom(LocalDate.of(2021, 4, 1))
-                .validTo(LocalDate.of(2099, 12, 31))
-                .designationOfficial("Bern")
-                .businessOrganisation("abc")
-                .spatialReference(SpatialReference.LV95)
-                .north(1207167.52455)
-                .east(2601352.9171)
-                .height(100000000.0)
-                .build();
+  @Test
+  void shouldValidateHeight() {
+    ServicePointCreateCsvModel bern = ServicePointCreateCsvModel.builder()
+        .uicCountryCode(85)
+        .validFrom(LocalDate.of(2021, 4, 1))
+        .validTo(LocalDate.of(2099, 12, 31))
+        .designationOfficial("Bern")
+        .businessOrganisation("abc")
+        .spatialReference(SpatialReference.LV95)
+        .north(1207167.52455)
+        .east(2601352.9171)
+        .height(100000000.0)
+        .build();
 
-        assertThat(bern.validate()).hasSize(1);
-    }
+    assertThat(bern.validate()).hasSize(1);
+  }
 
-    @Test
-    void shouldValidateGeography() {
-        ServicePointCreateCsvModel bern = ServicePointCreateCsvModel.builder()
-                .uicCountryCode(85)
-                .validFrom(LocalDate.of(2021, 4, 1))
-                .validTo(LocalDate.of(2099, 12, 31))
-                .designationOfficial("Bern")
-                .businessOrganisation("abc")
-                .spatialReference(null)
-                .north(1207167.52455)
-                .east(2601352.9171)
-                .height(500.0)
-                .build();
+  @Test
+  void shouldValidateGeography() {
+    ServicePointCreateCsvModel bern = ServicePointCreateCsvModel.builder()
+        .uicCountryCode(85)
+        .validFrom(LocalDate.of(2021, 4, 1))
+        .validTo(LocalDate.of(2099, 12, 31))
+        .designationOfficial("Bern")
+        .businessOrganisation("abc")
+        .spatialReference(null)
+        .north(1207167.52455)
+        .east(2601352.9171)
+        .height(500.0)
+        .build();
 
-        assertThat(bern.validate()).hasSize(1);
-    }
+    assertThat(bern.validate()).hasSize(1);
+  }
 
-    @Test
-    void shouldValidateStopPointType() {
-        ServicePointCreateCsvModel bern = ServicePointCreateCsvModel.builder()
-                .uicCountryCode(85)
-                .validFrom(LocalDate.of(2021, 4, 1))
-                .validTo(LocalDate.of(2099, 12, 31))
-                .designationOfficial("Bern")
-                .businessOrganisation("abc")
-                .stopPointType(StopPointType.ORDERLY)
-                .meansOfTransport(null)
-                .build();
+  @Test
+  void shouldValidateStopPointType() {
+    ServicePointCreateCsvModel bern = ServicePointCreateCsvModel.builder()
+        .uicCountryCode(85)
+        .validFrom(LocalDate.of(2021, 4, 1))
+        .validTo(LocalDate.of(2099, 12, 31))
+        .designationOfficial("Bern")
+        .businessOrganisation("abc")
+        .stopPointType(StopPointType.ORDERLY)
+        .meansOfTransport(null)
+        .build();
 
-        assertThat(bern.validate()).hasSize(1);
-    }
+    assertThat(bern.validate()).hasSize(1);
+  }
+
+  @Test
+  void checkUniqueFields() {
+    ServicePointCreateCsvModel model = ServicePointCreateCsvModel.builder().build();
+    assertThat(model.uniqueFields()).hasSize(1);
+    assertThat(model.uniqueFields().get(0).getField()).isEqualTo("number");
+    assertThat(model.uniqueFields().get(0).getFieldValueExtractor()).isNotNull();
+  }
+
+  @Test
+  void shouldReturnNullOnGetNumberIfCountryNotValid() {
+    ServicePointCreateCsvModel model = ServicePointCreateCsvModel.builder().uicCountryCode(150).build();
+    assertThat(model.getNumber()).isNull();
+  }
+
+  @Test
+  void shouldReturnNullOnGetNumberIfNumberShortNotValid() {
+    ServicePointCreateCsvModel model = ServicePointCreateCsvModel.builder().uicCountryCode(85).build();
+    assertThat(model.getNumber()).isNull();
+  }
+
+  @Test
+  void shouldReturnNumber() {
+    ServicePointCreateCsvModel model = ServicePointCreateCsvModel.builder().uicCountryCode(85).numberShort(1).build();
+    assertThat(model.getNumber()).isEqualTo("8500001");
+  }
 
 }
