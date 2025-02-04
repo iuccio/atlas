c27bae7a305a3f46bf6416371bf701c200b24048
atlas <19825793+iuccio@users.noreply.github.com>
2025-02-04 11:29:48 +0100

    ATLAS-2530: Configure service

---
+++ export-service/src/main/java/ch/sbb/exportservice/service/ExportTransportCompanyJobService.java
@@ -0,0 +1,31 @@
+package ch.sbb.exportservice.service;
+
+import static ch.sbb.exportservice.utils.JobDescriptionConstants.EXPORT_STOP_POINT_CSV_JOB_NAME;
+import static ch.sbb.exportservice.utils.JobDescriptionConstants.EXPORT_STOP_POINT_JSON_JOB_NAME;
+import static ch.sbb.exportservice.utils.JobDescriptionConstants.EXPORT_TRANSPORT_COMPANY_CSV_JOB_NAME;
+import static ch.sbb.exportservice.utils.JobDescriptionConstants.EXPORT_TRANSPORT_COMPANY_JSON_JOB_NAME;
+
+import ch.sbb.atlas.export.enumeration.ExportTypeBase;
+import ch.sbb.exportservice.model.PrmExportType;
+import java.util.List;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExportTransportCompanyJobService extends BaseExportJobService {
+
+  public ExportTransportCompanyJobService(
+      JobLauncher jobLauncher,
+      @Qualifier(EXPORT_TRANSPORT_COMPANY_CSV_JOB_NAME) Job exportTransportCompanyCsvJob,
+      @Qualifier(EXPORT_TRANSPORT_COMPANY_JSON_JOB_NAME) Job exportTransportCompanyJsonJob) {
+    super(jobLauncher, exportTransportCompanyCsvJob, exportTransportCompanyJsonJob);
+  }
+
+  @Override
+  protected List<ExportTypeBase> getExportTypes() {
+    return List.of(PrmExportType.FULL);
+  }
+
+}
