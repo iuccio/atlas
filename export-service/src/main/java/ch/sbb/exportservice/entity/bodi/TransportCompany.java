33cc507ee09740c943c8e8b21cc0ebfe363ddd96
atlas <19825793+iuccio@users.noreply.github.com>
2025-02-04 11:06:57 +0100

    ATLAS-2530: Configure batch job

---
+++ export-service/src/main/java/ch/sbb/exportservice/entity/bodi/TransportCompany.java
@@ -0,0 +1,54 @@
+package ch.sbb.exportservice.entity.bodi;
+
+import ch.sbb.atlas.api.bodi.enumeration.TransportCompanyStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldNameConstants;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldNameConstants
+public class TransportCompany {
+
+  private Long id;
+
+  private String number;
+
+  private String abbreviation;
+
+  private String description;
+
+  private String businessRegisterName;
+
+  private TransportCompanyStatus transportCompanyStatus;
+
+  private String businessRegisterNumber;
+
+  private String enterpriseId;
+
+  private String ricsCode;
+
+  private String businessOrganisationNumbers;
+
+  private String comment;
+
+  private LocalDateTime creationDate;
+
+  private LocalDateTime editionDate;
+
+}
