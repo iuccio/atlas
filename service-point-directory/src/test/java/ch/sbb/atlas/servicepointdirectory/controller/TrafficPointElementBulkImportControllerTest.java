95088324a2d9485258c423c4ab9b45c1d0675846
atlas <19825793+iuccio@users.noreply.github.com>
2025-02-06 16:48:59 +0100

    ATLAS-2394: Implement test scenarios

--- service-point-directory/src/test/java/ch/sbb/atlas/servicepointdirectory/controller/TrafficPointElementBulkImportControllerTest.java
+++ service-point-directory/src/test/java/ch/sbb/atlas/servicepointdirectory/controller/TrafficPointElementBulkImportControllerTest.java
@@ -10,6 +10,7 @@ import ch.sbb.atlas.configuration.handler.AtlasExceptionHandler;
 import ch.sbb.atlas.imports.BulkImportItemExecutionResult;
 import ch.sbb.atlas.imports.bulk.BulkImportUpdateContainer;
 import ch.sbb.atlas.imports.model.TrafficPointUpdateCsvModel;
+import ch.sbb.atlas.imports.model.create.TrafficPointCreateCsvModel;
 import ch.sbb.atlas.model.exception.SloidNotFoundException;
 import ch.sbb.atlas.servicepointdirectory.service.trafficpoint.bulk.TrafficPointElementBulkImportService;
 import java.util.List;
@@ -85,6 +86,44 @@ class TrafficPointElementBulkImportControllerTest {
     verify(trafficPointElementBulkImportService).updateTrafficPoint(updateContainer);
     assertThat(bulkImportItemExecutionResults).hasSize(1).first()
         .extracting(BulkImportItemExecutionResult::isSuccess).isEqualTo(false);
+  }
+
+  @Test
+  void shouldBulkImportCreate() {
+    BulkImportUpdateContainer<TrafficPointCreateCsvModel> updateContainer =
+        BulkImportUpdateContainer.<TrafficPointCreateCsvModel>builder()
+            .object(TrafficPointCreateCsvModel.builder()
+                .sloid("ch:1:sloid:89008:123:123")
+                .build())
+            .build();
+
+    List<BulkImportItemExecutionResult> bulkImportItemExecutionResults =
+        trafficPointElementBulkImportController.bulkImportCreate(List.of(updateContainer));
+
+    verify(trafficPointElementBulkImportService, never()).createTrafficPointByUserName("username", updateContainer);
+    verify(trafficPointElementBulkImportService).createTrafficPoint(updateContainer);
+    assertThat(bulkImportItemExecutionResults).hasSize(1).first()
+        .extracting(BulkImportItemExecutionResult::isSuccess).isEqualTo(true);
+  }
+
+  @Test
+  void shouldBulkImportCreateWithUsername() {
+    String username = "e123456";
+    BulkImportUpdateContainer<TrafficPointCreateCsvModel> updateContainer =
+        BulkImportUpdateContainer.<TrafficPointCreateCsvModel>builder()
+            .object(TrafficPointCreateCsvModel.builder()
+                .sloid("ch:1:sloid:89008:123:123")
+                .build())
+            .inNameOf(username)
+            .build();
+
+    List<BulkImportItemExecutionResult> bulkImportItemExecutionResults =
+        trafficPointElementBulkImportController.bulkImportCreate(List.of(updateContainer));
+
+    verify(trafficPointElementBulkImportService).createTrafficPointByUserName(username, updateContainer);
+    verify(trafficPointElementBulkImportService, never()).createTrafficPoint(updateContainer);
+    assertThat(bulkImportItemExecutionResults).hasSize(1).first()
+        .extracting(BulkImportItemExecutionResult::isSuccess).isEqualTo(true);
   }
 
 }
