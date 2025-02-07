b585e424e93844545f9dc29d1050b7e5628ec6bc
atlas <19825793+iuccio@users.noreply.github.com>
2025-02-07 12:14:47 +0100

    [tekton-build-bot] updated version to 2.432.0

--- mail/build.gradle.kts
+++ mail/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.431.0"
+version = "2.432.0"
 
 description = "Atlas Mail Service"
 
--- workflow/build.gradle.kts
+++ workflow/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.431.0"
+version = "2.432.0"
 
 description = "Atlas Scheduling Service"
 extra["shedlockVersion"] = "5.16.0"
