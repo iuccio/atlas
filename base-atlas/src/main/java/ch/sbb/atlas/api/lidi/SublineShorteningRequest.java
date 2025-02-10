6657d5ee85dd0b83288f3c4dce0df1b9fbd18768
atlas <19825793+iuccio@users.noreply.github.com>
2025-02-10 14:21:02 +0100

    ATLAS-1372 removed unused annotations

--- base-atlas/src/main/java/ch/sbb/atlas/api/lidi/SublineShorteningRequest.java
+++ base-atlas/src/main/java/ch/sbb/atlas/api/lidi/SublineShorteningRequest.java
@@ -1,20 +1,14 @@
 package ch.sbb.atlas.api.lidi;
 
 import ch.sbb.atlas.model.DateRange;
-import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.experimental.FieldNameConstants;
-import lombok.experimental.SuperBuilder;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@SuperBuilder
-@FieldNameConstants
-@Schema(name = "")
 public class SublineShorteningRequest {
 
   DateRange mainlineValidity;
