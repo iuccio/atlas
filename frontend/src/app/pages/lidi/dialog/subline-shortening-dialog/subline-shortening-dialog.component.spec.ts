f5551172e48a98559caf246bcf9f0d01ebd9a0eb
atlas <19825793+iuccio@users.noreply.github.com>
2025-02-10 14:12:59 +0100

    ATLAS-1372 removed unused exceptions and files, fixed failing test

--- frontend/src/app/pages/lidi/dialog/subline-shortening-dialog/subline-shortening-dialog.component.spec.ts
+++ frontend/src/app/pages/lidi/dialog/subline-shortening-dialog/subline-shortening-dialog.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SublineShorteningDialogComponent } from './subline-shortening-dialog.component';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { TranslatePipe } from '@ngx-translate/core';
+import { AppTestingModule } from '../../../../app.testing.module';
 
 describe('SublineShorteningDialogComponent', () => {
   let component: SublineShorteningDialogComponent;
@@ -8,9 +11,22 @@ describe('SublineShorteningDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [SublineShorteningDialogComponent]
-    })
-    .compileComponents();
+      declarations: [SublineShorteningDialogComponent],
+      imports: [AppTestingModule],
+      providers: [
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {
+            isAllowed: true,
+            affectedSublines: {
+              allowedSublines: [],
+              notAllowedSublines: [],
+            },
+          },
+        },
+        { provide: TranslatePipe },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SublineShorteningDialogComponent);
     component = fixture.componentInstance;
