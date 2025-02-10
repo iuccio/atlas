185952716f3f4938be11d1f55805e442285aa696
atlas <TechSupport-ATLAS@sbb.ch>
2025-02-10 08:55:01 +0100

    [tekton-build-bot] updated version to 2.434.0

--- mail/build.gradle.kts
+++ mail/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.433.0"
+version = "2.434.0"
 
 description = "Atlas Mail Service"
 
--- user-administration/build.gradle.kts
+++ user-administration/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.433.0"
+version = "2.434.0"
 
 description = "Atlas User Administration"
 extra["microsoftGraphSdkVersion"] = "6.27.0"
