0620213ea71fa99d1cc41871ab4c2ad9d79bf695
atlas <19825793+iuccio@users.noreply.github.com>
2025-02-12 14:21:02 +0100

    ATLAS-1372 made getter in dialog, used link component

--- frontend/src/app/pages/lidi/dialog/subline-shortening-dialog/subline-shortening-dialog.component.ts
+++ frontend/src/app/pages/lidi/dialog/subline-shortening-dialog/subline-shortening-dialog.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { AffectedSublines } from '../../../../api';
+import { Router } from '@angular/router';
+import { Pages } from '../../../pages';
 
 @Component({
   selector: 'app-subline-shortening-dialog',
@@ -9,8 +11,37 @@ import { AffectedSublines } from '../../../../api';
 export class SublineShorteningDialogComponent {
   public readonly SUBLINES_URL = '/line-directory/sublines/';
 
+  public get isAllowedOnly() {
+    return (
+      this.data.affectedSublines.notAllowedSublines?.length! === 0 &&
+      this.data.affectedSublines.allowedSublines?.length! > 0
+    );
+  }
+
+  public get isAllowedAndNotAllowed() {
+    return (
+      this.data.affectedSublines.notAllowedSublines?.length! > 0 &&
+      this.data.affectedSublines.allowedSublines?.length! > 0
+    );
+  }
+
+  public get isNotAllowedOnly() {
+    return (
+      this.data.affectedSublines.notAllowedSublines?.length! > 0 &&
+      this.data.affectedSublines.allowedSublines?.length! === 0
+    );
+  }
+
   constructor(
     @Inject(MAT_DIALOG_DATA)
-    public data: { isAllowed: boolean; affectedSublines: AffectedSublines }
+    public data: { isAllowed: boolean; affectedSublines: AffectedSublines },
+    private router: Router
   ) {}
+
+  openNewTabOfSubline(slnid: string) {
+    const url = this.router.serializeUrl(
+      this.router.createUrlTree([Pages.LIDI.path, Pages.SUBLINES.path, slnid])
+    );
+    window.open(url, '_blank');
+  }
 }
