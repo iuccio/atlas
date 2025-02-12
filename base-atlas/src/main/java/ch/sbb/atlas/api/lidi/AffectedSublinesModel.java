695fa3db014e2de9e2fab807b45ed3914eea8018
atlas <19825793+iuccio@users.noreply.github.com>
2025-02-12 14:41:50 +0100

    ATLAS-1372 added new attributes to affectedSublines

--- base-atlas/src/main/java/ch/sbb/atlas/api/lidi/AffectedSublinesModel.java
+++ base-atlas/src/main/java/ch/sbb/atlas/api/lidi/AffectedSublinesModel.java
@@ -3,15 +3,20 @@ package ch.sbb.atlas.api.lidi;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Builder
 public class AffectedSublinesModel {
 
   private List<String> allowedSublines = new ArrayList<>();
   private List<String> notAllowedSublines = new ArrayList<>();
+
+  private boolean isZeroAffectedSublines;
+  private boolean isAllowedToShort;
 
 }
