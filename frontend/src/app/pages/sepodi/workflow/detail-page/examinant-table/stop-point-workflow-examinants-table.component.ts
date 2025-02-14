f82f03ac00726f523edeb416652de4cb633e40e8
atlas <19825793+iuccio@users.noreply.github.com>
2025-02-14 13:09:05 +0100

    ATLAS-2365: remove unused imports

--- frontend/src/app/pages/sepodi/workflow/detail-page/examinant-table/stop-point-workflow-examinants-table.component.ts
+++ frontend/src/app/pages/sepodi/workflow/detail-page/examinant-table/stop-point-workflow-examinants-table.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {ControlContainer, FormArray, FormGroup, NgForm} from '@angular/forms';
 import { WorkflowStatus } from 'src/app/api';
 import {ExaminantFormGroup, StopPointWorkflowDetailFormGroupBuilder} from '../detail-form/stop-point-workflow-detail-form-group';
