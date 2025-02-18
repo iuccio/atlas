9a9a60a2698f7e408dd09caafe63d8658ca8f50e
atlas <19825793+iuccio@users.noreply.github.com>
2025-02-18 09:40:18 +0100

    ATLAS-2365: Add more tests for default examinant handling

--- workflow/src/main/resources/db/migration/V0.0.13__default_examinant.sql
+++ workflow/src/main/resources/db/migration/V0.0.13__default_examinant.sql
@@ -1,1 +1,2 @@
 ALTER TABLE person add column default_examinant boolean not null default false;
+update person set default_examinant = true where "function" = 'Verkehrsplanung' or "function" = 'Fachstelle atlas';
