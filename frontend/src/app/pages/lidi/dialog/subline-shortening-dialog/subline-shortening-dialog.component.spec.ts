5971aa62316e331e786db91a47773854aded066f
atlas <19825793+iuccio@users.noreply.github.com>
2025-02-18 10:21:28 +0100

    ATLAS-1372 fixed frontend tests

--- frontend/src/app/pages/lidi/dialog/subline-shortening-dialog/subline-shortening-dialog.component.spec.ts
+++ frontend/src/app/pages/lidi/dialog/subline-shortening-dialog/subline-shortening-dialog.component.spec.ts
@@ -4,12 +4,20 @@ import { SublineShorteningDialogComponent } from './subline-shortening-dialog.co
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { TranslatePipe } from '@ngx-translate/core';
 import { AppTestingModule } from '../../../../app.testing.module';
+import { Router } from '@angular/router';
+import { Pages } from '../../../pages';
 
 describe('SublineShorteningDialogComponent', () => {
   let component: SublineShorteningDialogComponent;
   let fixture: ComponentFixture<SublineShorteningDialogComponent>;
+  let router: jasmine.SpyObj<Router>;
 
   beforeEach(async () => {
+    const routerSpy = jasmine.createSpyObj('Router', [
+      'createUrlTree',
+      'serializeUrl',
+    ]);
+
     await TestBed.configureTestingModule({
       declarations: [SublineShorteningDialogComponent],
       imports: [AppTestingModule],
@@ -25,15 +33,35 @@ describe('SublineShorteningDialogComponent', () => {
           },
         },
         { provide: TranslatePipe },
+        { provide: Router, useValue: routerSpy },
       ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SublineShorteningDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should open new tab with correct url', () => {
+    const slnid = '123';
+
+    const expectedUrl = '/line-directory/sublines/123';
+    router.serializeUrl.and.returnValue(expectedUrl);
+
+    spyOn(window, 'open');
+
+    component.openNewTabOfSubline(slnid);
+
+    expect(router.createUrlTree).toHaveBeenCalledWith([
+      Pages.LIDI.path,
+      Pages.SUBLINES.path,
+      slnid,
+    ]);
+    expect(window.open).toHaveBeenCalledWith(expectedUrl, '_blank');
   });
 });
