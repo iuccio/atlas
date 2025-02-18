e02189afdddaa514c9d5f709b8506ff045be3f3f
atlas <TechSupport-ATLAS@sbb.ch>
2025-02-18 04:00:48 +0000

    fix(deps): update dependency software.amazon.awssdk:s3 to v2.30.22

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 group = "ch.sbb.atlas"
 version = "2.447.0"
 
-extra["awsS3Version"] = "2.30.21"
+extra["awsS3Version"] = "2.30.22"
 extra["swaggerCoreVersion"] = "2.2.28"
 extra["openapiStarterCommonVersion"] = "2.8.5"
 //Geo Data Libs
