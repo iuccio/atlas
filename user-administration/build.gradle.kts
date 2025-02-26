18762e63dcaf5d9718e19970c4c6d5dcd86f13e8
atlas <19825793+iuccio@users.noreply.github.com>
2025-02-26 04:01:54 +0000

    fix(deps): update gradle dependencies

--- user-administration/build.gradle.kts
+++ user-administration/build.gradle.kts
@@ -9,7 +9,7 @@ group = "ch.sbb.atlas"
 version = "2.461.0"
 
 description = "Atlas User Administration"
-extra["microsoftGraphSdkVersion"] = "6.29.0"
+extra["microsoftGraphSdkVersion"] = "6.30.0"
 extra["azureIdentityVersion"] = "1.15.3"
 
 dependencies {
