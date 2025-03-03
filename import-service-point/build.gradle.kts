1ecac10971a491fbaa24bde8e71de30c841bba81
atlas <19825793+iuccio@users.noreply.github.com>
2025-03-03 09:03:23 +0100

    [tekton-build-bot] updated version to 2.474.0

--- import-service-point/build.gradle.kts
+++ import-service-point/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.473.0"
+version = "2.474.0"
 
 description = "Atlas Import Service Point"
 
--- service-point-directory/build.gradle.kts
+++ service-point-directory/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.473.0"
+version = "2.474.0"
 
 description = "ServicePointDirectory"
 extra["shedlockVersion"] = "5.16.0"
