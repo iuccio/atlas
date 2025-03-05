9dc4ca2300c54d1c1299595de53ae3b3d09532c2
atlas <TechSupport-ATLAS@sbb.ch>
2025-03-05 17:30:15 +0100

    ATLAS-2410: add v2 streaming controller

--- export-service/src/main/java/ch/sbb/exportservice/controller/FileStreamingControllerApiV2.java
+++ export-service/src/main/java/ch/sbb/exportservice/controller/FileStreamingControllerApiV2.java
@@ -3,15 +3,10 @@ package ch.sbb.exportservice.controller;
 import ch.sbb.atlas.api.controller.GzipFileDownloadHttpHeader;
 import static ch.sbb.atlas.api.controller.GzipFileDownloadHttpHeader.extractFileNameFromS3ObjectName;
 import ch.sbb.atlas.api.model.ErrorResponse;
-import ch.sbb.atlas.export.enumeration.ExportFileName;
-import ch.sbb.atlas.export.enumeration.ExportTypeBase;
-import ch.sbb.atlas.model.exception.BadRequestException;
-import ch.sbb.exportservice.exception.NotAllowedExportFileExceptionV1;
-import ch.sbb.exportservice.model.ExportFilePathV1;
-import ch.sbb.exportservice.model.PrmBatchExportFileName;
-import ch.sbb.exportservice.model.PrmExportType;
-import ch.sbb.exportservice.model.SePoDiBatchExportFileName;
-import ch.sbb.exportservice.model.SePoDiExportType;
+import ch.sbb.exportservice.exception.NotAllowedExportFileExceptionV2;
+import ch.sbb.exportservice.model.ExportFilePathV2;
+import ch.sbb.exportservice.model.ExportObjectV2;
+import ch.sbb.exportservice.model.ExportTypeV2;
 import ch.sbb.exportservice.service.FileExportService;
 import io.micrometer.tracing.annotation.NewSpan;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -33,17 +28,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Export File Streaming V1")
-@RequestMapping(value = {"v1/export", "v1/export/prm"})
+@Tag(name = "Export File Streaming V2")
+@RequestMapping("v2/export")
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@Deprecated(forRemoval = true)
-public class FileStreamingControllerApiV1 {
+public class FileStreamingControllerApiV2 {
 
   private final FileExportService fileExportService;
 
-  @GetMapping(value = "json/{exportFileName}/{exportType}", produces = MediaType.APPLICATION_JSON_VALUE)
+  @GetMapping(value = "json/{exportObject}/{exportType}", produces = MediaType.APPLICATION_JSON_VALUE)
   @ApiResponses(value = {
       @ApiResponse(responseCode = "200", description = "Returns the today generated file as Stream"),
       @ApiResponse(responseCode = "404", description = "No file found for today date", content = @Content(schema =
@@ -52,15 +46,15 @@ public class FileStreamingControllerApiV1 {
   @NewSpan
   @Async
   public CompletableFuture<ResponseEntity<InputStreamResource>> streamExportJsonFile(
-      @PathVariable ExportFileName exportFileName,
-      @PathVariable ExportTypeBase exportType) {
-    isExportSupported(exportFileName, exportType);
-    final InputStreamResource body = fileExportService.streamJsonFile(new ExportFilePathV1(exportType, exportFileName));
+      @PathVariable ExportObjectV2 exportObject,
+      @PathVariable ExportTypeV2 exportType) {
+    isExportSupported(exportObject, exportType);
+    final InputStreamResource body = fileExportService.streamJsonFile(ExportFilePathV2.buildV2(exportObject, exportType));
     return CompletableFuture.supplyAsync(() ->
         ResponseEntity.status(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON).body(body));
   }
 
-  @GetMapping(value = "json/latest/{exportFileName}/{exportType}", produces = MediaType.APPLICATION_JSON_VALUE)
+  @GetMapping(value = "json/latest/{exportObject}/{exportType}", produces = MediaType.APPLICATION_JSON_VALUE)
   @ApiResponses(value = {
       @ApiResponse(responseCode = "200", description = "Returns the today generated file as Stream"),
       @ApiResponse(responseCode = "404", description = "No generated files found", content = @Content(schema =
@@ -69,15 +63,15 @@ public class FileStreamingControllerApiV1 {
   @NewSpan
   @Async
   public CompletableFuture<ResponseEntity<InputStreamResource>> streamLatestExportJsonFile(
-      @PathVariable ExportFileName exportFileName,
-      @PathVariable ExportTypeBase exportType) {
-    isExportSupported(exportFileName, exportType);
-    final InputStreamResource body = fileExportService.streamLatestJsonFile(new ExportFilePathV1(exportType, exportFileName));
+      @PathVariable ExportObjectV2 exportObject,
+      @PathVariable ExportTypeV2 exportType) {
+    isExportSupported(exportObject, exportType);
+    final InputStreamResource body = fileExportService.streamLatestJsonFile(ExportFilePathV2.buildV2(exportObject, exportType));
     return CompletableFuture.supplyAsync(() ->
         ResponseEntity.status(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON).body(body));
   }
 
-  @GetMapping(value = "download-gzip-json/{exportFileName}/{exportType}")
+  @GetMapping(value = "download-gzip-json/{exportObject}/{exportType}")
   @ApiResponses(value = {
       @ApiResponse(responseCode = "200", description = "Returns the today generated file"),
       @ApiResponse(responseCode = "404", description = "No file found for today date", content = @Content(schema =
@@ -86,16 +80,16 @@ public class FileStreamingControllerApiV1 {
   @NewSpan
   @Async
   public CompletableFuture<ResponseEntity<InputStreamResource>> streamExportGzFile(
-      @PathVariable ExportFileName exportFileName,
-      @PathVariable ExportTypeBase exportType) {
-    isExportSupported(exportFileName, exportType);
-    final ExportFilePathV1 exportFilePath = new ExportFilePathV1(exportType, exportFileName);
-    final HttpHeaders headers = GzipFileDownloadHttpHeader.getHeaders(exportFilePath.actualDateFileName());
+      @PathVariable ExportObjectV2 exportObject,
+      @PathVariable ExportTypeV2 exportType) {
+    isExportSupported(exportObject, exportType);
+    final ExportFilePathV2 exportFilePath = ExportFilePathV2.buildV2(exportObject, exportType);
+    final HttpHeaders headers = GzipFileDownloadHttpHeader.getHeaders(exportFilePath.fileName());
     final InputStreamResource body = fileExportService.streamGzipFile(exportFilePath.fileToStream());
     return CompletableFuture.supplyAsync(() -> ResponseEntity.ok().headers(headers).body(body));
   }
 
-  @GetMapping(value = "download-gzip-json/latest/{exportFileName}/{exportType}")
+  @GetMapping(value = "download-gzip-json/latest/{exportObject}/{exportType}")
   @ApiResponses(value = {
       @ApiResponse(responseCode = "200", description = "Returns the latest generated file"),
       @ApiResponse(responseCode = "404", description = "No generated files found", content = @Content(schema =
@@ -104,30 +98,20 @@ public class FileStreamingControllerApiV1 {
   @NewSpan
   @Async
   public CompletableFuture<ResponseEntity<InputStreamResource>> streamLatestExportGzFile(
-      @PathVariable ExportFileName exportFileName,
-      @PathVariable ExportTypeBase exportType) {
-    isExportSupported(exportFileName, exportType);
+      @PathVariable ExportObjectV2 exportObject,
+      @PathVariable ExportTypeV2 exportType) {
+    isExportSupported(exportObject, exportType);
     final String latestUploadedFileName = fileExportService.getLatestUploadedFileName(
-        new ExportFilePathV1(exportType, exportFileName));
+        ExportFilePathV2.buildV2(exportObject, exportType));
     final HttpHeaders headers = GzipFileDownloadHttpHeader.getHeaders(extractFileNameFromS3ObjectName(latestUploadedFileName));
     final InputStreamResource body = fileExportService.streamGzipFile(latestUploadedFileName);
     return CompletableFuture.supplyAsync(() -> ResponseEntity.ok().headers(headers).body(body));
   }
 
-  private static void isExportSupported(ExportFileName exportFileName, ExportTypeBase exportType) {
-    switch (exportFileName) {
-      case SePoDiBatchExportFileName e -> {
-        if (!(exportType instanceof SePoDiExportType)) {
-          throw new NotAllowedExportFileExceptionV1(e, SePoDiExportType.class);
-        }
-      }
-      case PrmBatchExportFileName e -> {
-        if (!(exportType instanceof PrmExportType)) {
-          throw new NotAllowedExportFileExceptionV1(e, PrmExportType.class);
-        }
-      }
-      default -> throw new BadRequestException("Unsupported export type " + exportType);
+  private static void isExportSupported(ExportObjectV2 exportObject, ExportTypeV2 exportType) {
+    if (!exportObject.isSupportedExportType(exportType)) {
+      throw new NotAllowedExportFileExceptionV2(exportObject, exportType);
     }
   }
-  // todo: spring cannot map pathvariables to interface types
+
 }
