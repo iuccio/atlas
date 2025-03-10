d6b0fc9857afcc2ed1f2a7e7de8e3959f3cd750e
atlas <19825793+iuccio@users.noreply.github.com>
2025-03-10 15:09:12 +0100

    ATLAS-2792: fix geolocation job

--- scheduling/src/main/java/ch/sbb/scheduling/client/BulkImportServiceBatchClient.java
+++ scheduling/src/main/java/ch/sbb/scheduling/client/BulkImportServiceBatchClient.java
@@ -5,9 +5,9 @@ import feign.Response;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "importServicePointBatch", url = "${atlas.client.gateway.url}", configuration = OAuthFeignConfig.class)
-public interface ImportServicePointBatchClient {
+@FeignClient(name = "bulkImportServiceBatch", url = "${atlas.client.gateway.url}", configuration = OAuthFeignConfig.class)
+public interface BulkImportServiceBatchClient {
 
-  @PostMapping(value = "/import-service-point/v1/service-point-job/update-geo")
+  @PostMapping(value = "/bulk-import-service/v1/service-point-job/update-geo")
   Response triggerUpdateGeolocationServicePointJob();
 }
