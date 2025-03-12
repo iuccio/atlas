bfd66936af86e874ad7d1fbf1fa5a952b7f82cdf
atlas <TechSupport-ATLAS@sbb.ch>
2025-03-12 14:00:18 +0100

    ATLAS-2398: Orderly line validity validation

--- line-directory/src/main/java/ch/sbb/line/directory/validation/LineValidationService.java
+++ line-directory/src/main/java/ch/sbb/line/directory/validation/LineValidationService.java
@@ -7,11 +7,11 @@ import ch.sbb.atlas.model.Status;
 import ch.sbb.line.directory.entity.LineVersion;
 import ch.sbb.line.directory.exception.LineConflictException;
 import ch.sbb.line.directory.exception.LineTypeOrderlyException;
+import ch.sbb.line.directory.exception.OrderlyLineValidityException;
 import ch.sbb.line.directory.exception.RevokedException;
 import ch.sbb.line.directory.exception.TemporaryLineValidationException;
 import ch.sbb.line.directory.repository.LineVersionRepository;
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,6 +21,7 @@ import org.springframework.stereotype.Service;
 public class LineValidationService {
 
   private static final int TEMPORARY_LINE_MAX_VALIDITY_IN_DAYS = 14;
+  private static final int ORDERLY_LINE_MIN_VALIDITY_IN_DAYS = 15;
 
   private final LineVersionRepository lineVersionRepository;
   private final CoverageValidationService coverageValidationService;
@@ -34,6 +35,7 @@ public class LineValidationService {
   public void validateLineAfterVersioningBusinessRule(LineVersion lineVersion) {
     List<LineVersion> savedLine = lineVersionRepository.findAllBySlnidOrderByValidFrom(lineVersion.getSlnid());
     validateTemporaryLinesDuration(savedLine);
+    validateOrderlyLinesDuration(savedLine);
 
     coverageValidationService.validateLineSublineCoverage(lineVersion);
   }
@@ -63,6 +65,19 @@ public class LineValidationService {
       long validityInDays = new DateRange(minValidFrom, maxValidTo).getValidityInDays();
       if (validityInDays > TEMPORARY_LINE_MAX_VALIDITY_IN_DAYS) {
         throw new TemporaryLineValidationException(minValidFrom, maxValidTo);
+      }
+    }
+  }
+
+  void validateOrderlyLinesDuration(List<LineVersion> savedLine) {
+    if (LineType.ORDERLY.equals(savedLine.getFirst().getLineType())) {
+
+      LocalDate minValidFrom = savedLine.getFirst().getValidFrom();
+      LocalDate maxValidTo = savedLine.getLast().getValidTo();
+
+      long validityInDays = new DateRange(minValidFrom, maxValidTo).getValidityInDays();
+      if (validityInDays < ORDERLY_LINE_MIN_VALIDITY_IN_DAYS) {
+        throw new OrderlyLineValidityException(minValidFrom, maxValidTo);
       }
     }
   }
