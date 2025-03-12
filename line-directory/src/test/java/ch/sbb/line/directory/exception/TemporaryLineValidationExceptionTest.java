7fe1b404eeec4ae098d19bb88140ce6f667e2fd7
atlas <TechSupport-ATLAS@sbb.ch>
2025-03-12 10:56:51 +0100

    ATLAS-2398: Finish temp line validation

--- line-directory/src/test/java/ch/sbb/line/directory/exception/TemporaryLineValidationExceptionTest.java
+++ line-directory/src/test/java/ch/sbb/line/directory/exception/TemporaryLineValidationExceptionTest.java
@@ -2,93 +2,20 @@ package ch.sbb.line.directory.exception;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import ch.sbb.atlas.api.model.ErrorResponse.Parameter;
-import ch.sbb.line.directory.LineTestData;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 
- class TemporaryLineValidationExceptionTest {
+class TemporaryLineValidationExceptionTest {
 
   @Test
-  void testGetErrorResponseWithMultipleRelatingVersions() {
-    try {
-      throw new TemporaryLineValidationException(
-          List.of(
-              LineTestData.lineVersionBuilder()
-                  .validFrom(LocalDate.of(2021, 10, 2))
-                  .validTo(LocalDate.of(2022, 2, 1))
-                  .build(),
-              LineTestData.lineVersionBuilder()
-                  .validFrom(LocalDate.of(2021, 1, 1))
-                  .validTo(LocalDate.of(2021, 5, 1))
-                  .build(),
-              LineTestData.lineVersionBuilder()
-                  .validFrom(LocalDate.of(2021, 5, 2))
-                  .validTo(LocalDate.of(2021, 10, 1))
-                  .build()
-          )
-      );
-    } catch (TemporaryLineValidationException exception) {
-      assertThat(exception.getErrorResponse().getStatus()).isEqualTo(422);
-      assertThat(exception.getErrorResponse().getMessage()).isEqualTo(
-          "Business rule validation failed");
-      assertThat(exception.getErrorResponse().getDetails()).hasSize(3);
-      assertThat(exception.getErrorResponse().getDetails().first().getField()).isEqualTo("validTo");
-      assertThat(exception.getErrorResponse().getDetails().first().getMessage()).isEqualTo(
-          "Temporary version from 01.01.2021 to 01.05.2021 is a part of relating temporary versions, which together exceed maximum validity of 12 months");
-      assertThat(
-          exception.getErrorResponse().getDetails().first().getDisplayInfo().getCode()).isEqualTo(
-          "LIDI.LINE.RELATING_TEMPORARY_VERSIONS_EXCEED_MAX_VALIDITY");
-      assertThat(exception.getErrorResponse()
-          .getDetails()
-          .first()
-          .getDisplayInfo()
-          .getParameters()).usingRecursiveComparison().isEqualTo(
-          List.of(
-              new Parameter("validFrom",
-                  LocalDate.of(2021, 1, 1).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))),
-              new Parameter("validTo",
-                  LocalDate.of(2021, 5, 1).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")))
-          )
-      );
-    }
-  }
+  void shouldHaveCorrectDisplayMessage() {
+    TemporaryLineValidationException temporaryLineValidationException = new TemporaryLineValidationException(
+        LocalDate.of(2020, 1, 1), LocalDate.of(2020, 1, 16));
 
-  @Test
-  void testGetErrorResponseWithOneVersion() {
-    try {
-      throw new TemporaryLineValidationException(
-          List.of(
-              LineTestData.lineVersionBuilder().validFrom(LocalDate.of(2021, 1, 1))
-                  .validTo(LocalDate.of(2022, 5, 1)).build()
-          )
-      );
-    } catch (TemporaryLineValidationException exception) {
-      assertThat(exception.getErrorResponse().getStatus()).isEqualTo(422);
-      assertThat(exception.getErrorResponse().getMessage()).isEqualTo(
-          "Business rule validation failed");
-      assertThat(exception.getErrorResponse().getDetails()).hasSize(1);
-      assertThat(exception.getErrorResponse().getDetails().first().getField()).isEqualTo("validTo");
-      assertThat(exception.getErrorResponse().getDetails().first().getMessage()).isEqualTo(
-          "Temporary version from 01.01.2021 to 01.05.2022 exceeds maximum validity of 12 months");
-      assertThat(
-          exception.getErrorResponse().getDetails().first().getDisplayInfo().getCode()).isEqualTo(
-          "LIDI.LINE.TEMPORARY_VERSION_EXCEEDS_MAX_VALIDITY");
-      assertThat(exception.getErrorResponse()
-          .getDetails()
-          .first()
-          .getDisplayInfo()
-          .getParameters()).usingRecursiveComparison().isEqualTo(
-          List.of(
-              new Parameter("validFrom",
-                  LocalDate.of(2021, 1, 1).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))),
-              new Parameter("validTo",
-                  LocalDate.of(2022, 5, 1).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")))
-          )
-      );
-    }
+    assertThat(temporaryLineValidationException.getErrorResponse().getDetails().first().getDisplayInfo().getCode())
+        .isEqualTo("LIDI.LINE.TEMPORARY_VERSION_EXCEEDS_MAX_VALIDITY");
+    assertThat(temporaryLineValidationException.getErrorResponse().getDetails().first().getDisplayInfo().getParameters())
+        .hasSize(2);
   }
 
 }
--- line-directory/src/test/java/ch/sbb/line/directory/validation/LineValidationServiceTest.java
+++ line-directory/src/test/java/ch/sbb/line/directory/validation/LineValidationServiceTest.java
@@ -11,14 +11,12 @@ import static org.mockito.Mockito.when;
 import ch.sbb.atlas.api.lidi.enumaration.LineConcessionType;
 import ch.sbb.atlas.api.lidi.enumaration.LineType;
 import ch.sbb.atlas.business.organisation.service.SharedBusinessOrganisationService;
-import ch.sbb.atlas.model.Status;
 import ch.sbb.line.directory.LineTestData;
 import ch.sbb.line.directory.SublineTestData;
 import ch.sbb.line.directory.entity.LineVersion;
 import ch.sbb.line.directory.entity.SublineVersion;
 import ch.sbb.line.directory.exception.LineConflictException;
 import ch.sbb.line.directory.exception.LineTypeOrderlyException;
-import ch.sbb.line.directory.exception.RevokedException;
 import ch.sbb.line.directory.exception.TemporaryLineValidationException;
 import ch.sbb.line.directory.repository.LineVersionRepository;
 import ch.sbb.line.directory.repository.SublineVersionRepository;
@@ -68,18 +66,16 @@ class LineValidationServiceTest {
   }
 
   @Test
-  void shouldNotSaveTemporaryLineWithValidityGreaterThan12Months() {
+  void shouldNotSaveTemporaryLineWithValidityGreaterThan14Days() {
     // Given
     LineVersion lineVersion = LineTestData.lineVersionBuilder()
         .lineType(LineType.TEMPORARY)
         .validFrom(LocalDate.of(2021, 10, 15))
-        .validTo(LocalDate.of(2022, 10, 16))
+        .validTo(LocalDate.of(2021, 10, 31))
         .build();
-    when(lineVersionRepository.findAllBySlnidOrderByValidFrom(lineVersion.getSlnid())).thenReturn(
-        List.of());
     // When
     assertThatExceptionOfType(TemporaryLineValidationException.class).isThrownBy(
-        () -> lineValidationService.validateTemporaryLinesDuration(lineVersion));
+        () -> lineValidationService.validateTemporaryLinesDuration(List.of(lineVersion)));
   }
 
   @Test
@@ -88,182 +84,11 @@ class LineValidationServiceTest {
     LineVersion lineVersion = LineTestData.lineVersionBuilder()
         .lineType(LineType.TEMPORARY)
         .validFrom(LocalDate.of(2021, 10, 1))
-        .validTo(LocalDate.of(2022, 10, 1))
+        .validTo(LocalDate.of(2021, 10, 14))
         .build();
-    when(lineVersionRepository.findAllBySlnidOrderByValidFrom(lineVersion.getSlnid())).thenReturn(
-        List.of());
     // When & Then
     assertThatNoException().isThrownBy(
-        () -> lineValidationService.validateTemporaryLinesDuration(lineVersion));
-  }
-
-  @Test
-  void shouldNotSaveWith2ExistentTemporaryVersionsWhichRelateIncomingVersionWhenValidityLongerThan12() {
-    // Given
-    List<LineVersion> versions = List.of(
-        LineTestData.lineVersionBuilder().lineType(LineType.TEMPORARY).id(1L)
-            .validFrom(LocalDate.of(2021, 1, 1))
-            .validTo(LocalDate.of(2021, 3, 31)).build(),
-        LineTestData.lineVersionBuilder().lineType(LineType.TEMPORARY).id(2L)
-            .validFrom(LocalDate.of(2021, 9, 1))
-            .validTo(LocalDate.of(2022, 2, 1)).build());
-    LineVersion lineVersion = LineTestData.lineVersionBuilder()
-        .lineType(LineType.TEMPORARY)
-        .validFrom(LocalDate.of(2021, 4, 1))
-        .validTo(LocalDate.of(2021, 8, 31))
-        .build();
-    when(lineVersionRepository.findAllBySlnidOrderByValidFrom(lineVersion.getSlnid())).thenReturn(
-        versions);
-    // When
-    assertThatExceptionOfType(TemporaryLineValidationException.class).isThrownBy(
-        () -> lineValidationService.validateTemporaryLinesDuration(lineVersion));
-  }
-
-  @Test
-  void shouldSaveTemporaryLineWith1ExistentTemporaryVersionWhichRelatesWhenValidityIsLessThan12() {
-    // Given
-    List<LineVersion> versions = List.of(
-        LineTestData.lineVersionBuilder()
-            .id(1L)
-            .lineType(LineType.TEMPORARY)
-            .validFrom(LocalDate.of(2021, 1, 1))
-            .validTo(LocalDate.of(2021, 3, 31))
-            .build());
-    LineVersion lineVersion = LineTestData.lineVersionBuilder()
-        .lineType(LineType.TEMPORARY)
-        .validFrom(LocalDate.of(2021, 4, 1))
-        .validTo(LocalDate.of(2021, 7, 31))
-        .build();
-    when(lineVersionRepository.findAllBySlnidOrderByValidFrom(lineVersion.getSlnid())).thenReturn(
-        versions);
-    // When & Then
-    assertThatNoException().isThrownBy(
-        () -> lineValidationService.validateTemporaryLinesDuration(lineVersion));
-  }
-
-  @Test
-  void shouldThrowExceptionOn2RelatingTemporaryAnd1RelatingNotTemporary() {
-    // Given
-    List<LineVersion> versions = List.of(
-        LineTestData.lineVersionBuilder()
-            .lineType(LineType.TEMPORARY)
-            .id(1L)
-            .validFrom(LocalDate.of(2021, 1, 1))
-            .validTo(LocalDate.of(2021, 3, 31))
-            .build(),
-        LineTestData.lineVersionBuilder()
-            .lineType(LineType.TEMPORARY)
-            .id(2L)
-            .validFrom(LocalDate.of(2021, 4, 1))
-            .validTo(LocalDate.of(2021, 8, 31))
-            .build(),
-        LineTestData.lineVersionBuilder().id(3L).validFrom(LocalDate.of(2022, 3, 1))
-            .validTo(LocalDate.of(2022, 3, 31)).build());
-    LineVersion lineVersion = LineTestData.lineVersionBuilder()
-        .lineType(LineType.TEMPORARY)
-        .validFrom(LocalDate.of(2021, 9, 1))
-        .validTo(LocalDate.of(2022, 2, 28))
-        .build();
-    when(lineVersionRepository.findAllBySlnidOrderByValidFrom(lineVersion.getSlnid())).thenReturn(
-        versions);
-    // When
-    assertThatExceptionOfType(TemporaryLineValidationException.class).isThrownBy(
-        () -> lineValidationService.validateTemporaryLinesDuration(lineVersion));
-  }
-
-  @Test
-  void shouldSaveOn2RelatingTemporaryAnd1RelatingNotTemporary() {
-    // Given
-    List<LineVersion> versions = List.of(
-        LineTestData.lineVersionBuilder()
-            .id(1L)
-            .lineType(LineType.TEMPORARY)
-            .validFrom(LocalDate.of(2021, 1, 1))
-            .validTo(LocalDate.of(2021, 3, 31))
-            .build(),
-        LineTestData.lineVersionBuilder()
-            .id(2L)
-            .lineType(LineType.TEMPORARY)
-            .validFrom(LocalDate.of(2021, 4, 1))
-            .validTo(LocalDate.of(2021, 8, 31))
-            .build(),
-        LineTestData.lineVersionBuilder().id(3L).validFrom(LocalDate.of(2021, 10, 1))
-            .validTo(LocalDate.of(2022, 3, 31)).build());
-    LineVersion lineVersion = LineTestData.lineVersionBuilder()
-        .lineType(LineType.TEMPORARY)
-        .validFrom(LocalDate.of(2021, 9, 1))
-        .validTo(LocalDate.of(2021, 9, 30))
-        .build();
-    when(lineVersionRepository.findAllBySlnidOrderByValidFrom(lineVersion.getSlnid())).thenReturn(
-        versions);
-    // When & Then
-    assertThatNoException().isThrownBy(
-        () -> lineValidationService.validateTemporaryLinesDuration(lineVersion));
-  }
-
-  @Test
-  void shouldSaveOn1ExistingTemporaryWithGap() {
-    // Given
-    List<LineVersion> versions = List.of(
-        LineTestData.lineVersionBuilder()
-            .id(1L)
-            .lineType(LineType.TEMPORARY)
-            .validFrom(LocalDate.of(2021, 1, 1))
-            .validTo(LocalDate.of(2021, 3, 31))
-            .build());
-    LineVersion lineVersion = LineTestData.lineVersionBuilder()
-        .lineType(LineType.TEMPORARY)
-        .validFrom(LocalDate.of(2021, 4, 2))
-        .validTo(LocalDate.of(2021, 8, 31))
-        .build();
-    when(lineVersionRepository.findAllBySlnidOrderByValidFrom(lineVersion.getSlnid())).thenReturn(
-        versions);
-    // When & Then
-    assertThatNoException().isThrownBy(
-        () -> lineValidationService.validateTemporaryLinesDuration(lineVersion));
-  }
-
-  @Test
-  void shouldThrowExceptionOnUpdateWhenRelatingTemporaryVersionsLongerThan12Months() {
-    // Given
-    List<LineVersion> versions = List.of(
-        LineTestData.lineVersionBuilder().id(1L).lineType(LineType.TEMPORARY)
-            .validFrom(LocalDate.of(2021, 1, 1))
-            .validTo(LocalDate.of(2021, 3, 31)).build(),
-        LineTestData.lineVersionBuilder().id(2L).lineType(LineType.TEMPORARY)
-            .validFrom(LocalDate.of(2021, 8, 1))
-            .validTo(LocalDate.of(2021, 8, 31)).build());
-    LineVersion lineVersion = versions.get(1);
-    lineVersion.setValidFrom(LocalDate.of(2021, 4, 1));
-    lineVersion.setValidTo(LocalDate.of(2022, 2, 1));
-    when(lineVersionRepository.findAllBySlnidOrderByValidFrom(lineVersion.getSlnid())).thenReturn(
-        versions);
-    // When
-    assertThatExceptionOfType(TemporaryLineValidationException.class).isThrownBy(
-        () -> lineValidationService.validateTemporaryLinesDuration(lineVersion));
-  }
-
-  @Test
-  void shouldThrowExceptionOn2VersionsGap1VersionAndNewVersion() {
-    // Given
-    List<LineVersion> versions = List.of(
-        LineTestData.lineVersionBuilder().id(1L).lineType(LineType.TEMPORARY)
-            .validFrom(LocalDate.of(2021, 1, 1))
-            .validTo(LocalDate.of(2021, 4, 1)).build(),
-        LineTestData.lineVersionBuilder().id(2L).lineType(LineType.TEMPORARY)
-            .validFrom(LocalDate.of(2021, 4, 2))
-            .validTo(LocalDate.of(2021, 9, 2)).build(),
-        LineTestData.lineVersionBuilder().id(3L).lineType(LineType.TEMPORARY)
-            .validFrom(LocalDate.of(2021, 10, 2))
-            .validTo(LocalDate.of(2022, 2, 2)).build());
-    LineVersion lineVersion = LineTestData.lineVersionBuilder().lineType(LineType.TEMPORARY)
-        .validFrom(LocalDate.of(2022, 2, 3))
-        .validTo(LocalDate.of(2022, 11, 3)).build();
-    when(lineVersionRepository.findAllBySlnidOrderByValidFrom(lineVersion.getSlnid())).thenReturn(
-        versions);
-    // When
-    assertThatExceptionOfType(TemporaryLineValidationException.class).isThrownBy(
-        () -> lineValidationService.validateTemporaryLinesDuration(lineVersion));
+        () -> lineValidationService.validateTemporaryLinesDuration(List.of(lineVersion)));
   }
 
   @Test
