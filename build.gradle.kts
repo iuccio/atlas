1c26b9fbf914314c4dca3ff8938f28ce9ed18e78
atlas <19825793+iuccio@users.noreply.github.com>
2025-03-14 04:00:38 +0000

    Update GRADLE Dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 group = "ch.sbb.atlas"
 version = "2.499.0"
 
-extra["awsS3Version"] = "2.30.38"
+extra["awsS3Version"] = "2.31.0"
 extra["swaggerCoreVersion"] = "2.2.29"
 extra["openapiStarterCommonVersion"] = "2.8.5"
 //Geo Data Libs
