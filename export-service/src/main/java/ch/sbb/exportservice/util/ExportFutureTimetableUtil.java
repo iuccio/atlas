c68c0cbce999d7673cf8de1e287f5fef2627fa65
atlas <19825793+iuccio@users.noreply.github.com>
2025-03-18 17:52:31 +0100

    ATLAS-2793: TimetableYears Export sql logic

---
+++ export-service/src/main/java/ch/sbb/exportservice/util/ExportFutureTimetableUtil.java
@@ -0,0 +1,21 @@
+package ch.sbb.exportservice.util;
+
+import ch.sbb.atlas.model.DateRange;
+import ch.sbb.atlas.model.FutureTimetableHelper;
+import java.time.LocalDate;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ExportFutureTimetableUtil {
+
+  public static DateRange getTimetableYearsDateRange() {
+    return getTimetableYearsDateRange(LocalDate.now());
+  }
+
+  static DateRange getTimetableYearsDateRange(LocalDate referenceDate) {
+    LocalDate yearsStart = FutureTimetableHelper.getTimetableYearChangeDateToExportData(referenceDate.minusYears(1));
+    LocalDate yearsEnd = FutureTimetableHelper.getTimetableYearChangeDateToExportData(referenceDate.plusYears(1)).minusDays(1);
+    return new DateRange(yearsStart, yearsEnd);
+  }
+
+}
