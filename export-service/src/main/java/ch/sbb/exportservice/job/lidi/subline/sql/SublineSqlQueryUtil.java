c546c534f74e2d54d38c9cbc27ce7799d4831a88
atlas <TechSupport-ATLAS@sbb.ch>
2025-03-19 17:13:18 +0100

    ATLAS-2410: wip: export-service: restructuring

---
+++ export-service/src/main/java/ch/sbb/exportservice/job/lidi/subline/sql/SublineSqlQueryUtil.java
@@ -0,0 +1,39 @@
+package ch.sbb.exportservice.job.lidi.subline.sql;
+
+import static ch.sbb.exportservice.model.ExportTypeV2.ACTUAL;
+import static ch.sbb.exportservice.model.ExportTypeV2.FULL;
+
+import ch.sbb.atlas.model.FutureTimetableHelper;
+import ch.sbb.atlas.versioning.date.DateHelper;
+import ch.sbb.exportservice.job.SqlQueryUtil;
+import ch.sbb.exportservice.model.ExportTypeV2;
+import java.time.LocalDate;
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@UtilityClass
+public class SublineSqlQueryUtil extends SqlQueryUtil {
+
+  private static final String SELECT_STATEMENT = """
+      SELECT sv.*
+      FROM subline_version as sv
+      """;
+  private static final String ORDER_BY_STATEMENT = "ORDER BY sv.slnid, sv.valid_from";
+
+  public String getSqlQuery(ExportTypeV2 exportTypeV2) {
+    final String today = DateHelper.getDateAsSqlString(LocalDate.now());
+    String additionalWhereClause = "";
+    if (exportTypeV2 != FULL) {
+      final String date = exportTypeV2 == ACTUAL ? today :
+          DateHelper.getDateAsSqlString(FutureTimetableHelper.getTimetableYearChangeDateToExportData(LocalDate.now()));
+      additionalWhereClause = "WHERE '%s' >= sv.valid_from AND '%s' <= sv.valid_to".formatted(date, date);
+    }
+
+    final String sqlQuery = buildSqlQuery(SELECT_STATEMENT, additionalWhereClause, ORDER_BY_STATEMENT);
+    log.info("Execution SQL query:");
+    log.info(sqlQuery);
+    return sqlQuery;
+  }
+
+}
---
+++ export-service/src/main/java/ch/sbb/exportservice/job/lidi/ttfn/service/ExportTimetableFieldNumberJobService.java
@@ -0,0 +1,34 @@
+package ch.sbb.exportservice.job.lidi.ttfn.service;
+
+import static ch.sbb.exportservice.util.JobDescriptionConstant.EXPORT_TTFN_CSV_JOB_NAME;
+import static ch.sbb.exportservice.util.JobDescriptionConstant.EXPORT_TTFN_JSON_JOB_NAME;
+
+import ch.sbb.exportservice.job.BaseExportJobService;
+import ch.sbb.exportservice.model.ExportTypeV2;
+import java.util.List;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExportTimetableFieldNumberJobService extends BaseExportJobService {
+
+  public ExportTimetableFieldNumberJobService(
+      JobLauncher jobLauncher,
+      @Qualifier(EXPORT_TTFN_CSV_JOB_NAME) Job exportLoadingPointCsvJob,
+      @Qualifier(EXPORT_TTFN_JSON_JOB_NAME) Job exportLoadingPointJsonJob
+  ) {
+    super(jobLauncher, exportLoadingPointCsvJob, exportLoadingPointJsonJob);
+  }
+
+  @Override
+  protected List<JobParams> getExportTypes() {
+    return List.of(
+        new JobParams(ExportTypeV2.FULL),
+        new JobParams(ExportTypeV2.ACTUAL),
+        new JobParams(ExportTypeV2.FUTURE_TIMETABLE)
+    );
+  }
+
+}
