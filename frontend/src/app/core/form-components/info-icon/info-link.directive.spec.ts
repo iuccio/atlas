05eddc81717210afca1786ab306b527d5a27305f
atlas <19825793+iuccio@users.noreply.github.com>
2025-03-20 09:59:24 +0100

    ATLAS-2670: fix tests

--- frontend/src/app/core/form-components/info-icon/info-link.directive.spec.ts
+++ frontend/src/app/core/form-components/info-icon/info-link.directive.spec.ts
@@ -6,7 +6,12 @@ import { TranslateService } from '@ngx-translate/core';
 import { of } from 'rxjs';
 import SpyObj = jasmine.SpyObj;
 
-@Component({ template: `<span infoLink infoLinkTranslationKey="TEST_TRANSLATION_KEY">Test</span>` })
+@Component({
+  imports: [InfoLinkDirective],
+  template: `<span infoLink infoLinkTranslationKey="TEST_TRANSLATION_KEY"
+    >Test</span
+  >`,
+})
 class TestComponent {}
 
 describe('InfoLinkDirective', () => {
@@ -14,12 +19,17 @@ describe('InfoLinkDirective', () => {
   let translateServiceSpy: SpyObj<TranslateService>;
 
   beforeEach(() => {
-    translateServiceSpy = jasmine.createSpyObj<TranslateService>('TranslateServiceMock', ['get']);
-    translateServiceSpy.get = jasmine.createSpy().and.returnValue(of('https://atlas.test.ch'));
+    translateServiceSpy = jasmine.createSpyObj<TranslateService>(
+      'TranslateServiceMock',
+      ['get']
+    );
+    translateServiceSpy.get = jasmine
+      .createSpy()
+      .and.returnValue(of('https://atlas.test.ch'));
     fixture = TestBed.configureTestingModule({
-    imports: [InfoLinkDirective, TestComponent],
-    providers: [{ provide: TranslateService, useValue: translateServiceSpy }],
-}).createComponent(TestComponent);
+      imports: [InfoLinkDirective, TestComponent],
+      providers: [{ provide: TranslateService, useValue: translateServiceSpy }],
+    }).createComponent(TestComponent);
 
     fixture.detectChanges();
   });
@@ -29,7 +39,9 @@ describe('InfoLinkDirective', () => {
       By.directive(InfoLinkDirective)
     );
     expect(elementsWithInfoLinkDirective).toHaveSize(1);
-    expect(elementsWithInfoLinkDirective[0].classes['atlas-info-link']).toBeTrue();
+    expect(
+      elementsWithInfoLinkDirective[0].classes['atlas-info-link']
+    ).toBeTrue();
   });
 
   it('should handle click event', () => {
@@ -42,8 +54,13 @@ describe('InfoLinkDirective', () => {
     spyOn(console, 'error');
     elementsWithInfoLinkDirective[0].nativeElement.click();
     fixture.detectChanges();
-    expect(translateServiceSpy.get).toHaveBeenCalledOnceWith('TEST_TRANSLATION_KEY');
-    expect(window.open).toHaveBeenCalledOnceWith('https://atlas.test.ch', '_blank');
+    expect(translateServiceSpy.get).toHaveBeenCalledOnceWith(
+      'TEST_TRANSLATION_KEY'
+    );
+    expect(window.open).toHaveBeenCalledOnceWith(
+      'https://atlas.test.ch',
+      '_blank'
+    );
     expect(console.error).not.toHaveBeenCalled();
   });
 });
