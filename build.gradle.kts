69498b04be4b1f36c0318db92219b3abb0f07d10
atlas <19825793+iuccio@users.noreply.github.com>
2025-03-24 09:22:27 +0100

    [tekton-build-bot] updated version to 2.522.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.521.0"
+version = "2.522.0"
 
 extra["awsS3Version"] = "2.31.6"
 extra["swaggerCoreVersion"] = "2.2.29"
--- scheduling/build.gradle.kts
+++ scheduling/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.521.0"
+version = "2.522.0"
 
 description = "Atlas Scheduling Service"
 extra["shedlockVersion"] = "6.3.0"
