83611b6c9d2ec2468750f504e4cac0cb8cfd9114
atlas <19825793+iuccio@users.noreply.github.com>
2025-03-25 09:10:22 +0100

    ATLAS-2574: mock auth

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,15 +2,15 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-import { AuthConfig } from 'angular-oauth2-oidc';
-import { Environment } from './environment.model';
+import {AuthConfig} from 'angular-oauth2-oidc';
+import {Environment} from './environment.model';
 import packageJson from '../../package.json';
 
 // See https://confluence.sbb.ch/display/CLEW/Azure+AD
 const authConfig: AuthConfig = {
   // This is the issuer URL for the SBB Azure AD organization
   issuer:
-    'https://login.microsoftonline.com/2cda5d11-f0ac-46b3-967d-af1b2e1bd01a/v2.0',
+    'http://localhost:8080/2cda5d11-f0ac-46b3-967d-af1b2e1bd01a/v2.0',
   // This is required, since Azure AD uses different domains in their issuer configuration
   strictDiscoveryDocumentValidation: false,
   clientId: '18746f30-7978-48b5-b19b-0f871fb12e67',
