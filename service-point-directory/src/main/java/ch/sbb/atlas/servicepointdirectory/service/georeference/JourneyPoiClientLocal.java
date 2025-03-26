651f26ffd6700b76b13cc5799375953b0dd4bf0e
atlas <19825793+iuccio@users.noreply.github.com>
2025-03-26 15:31:25 +0100

    ATLAS-2475: mocks for servicepoint

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/service/georeference/JourneyPoiClientLocal.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/service/georeference/JourneyPoiClientLocal.java
@@ -3,7 +3,7 @@ package ch.sbb.atlas.servicepointdirectory.service.georeference;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Profile;
 
-@Profile("local")
+@Profile("github")
 @FeignClient(name = "journeyPoiClient", url = "${journeyPoi.client.url}")
 public interface JourneyPoiClientLocal extends JourneyPoiClientBase {
 
