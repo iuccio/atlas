21a695b32423f3c1f5859319e4aed153e7c7bee2
atlas <19825793+iuccio@users.noreply.github.com>
2025-03-27 10:01:23 +0100

    [tekton-build-bot] updated version to 2.525.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.524.0"
+version = "2.525.0"
 
 extra["awsS3Version"] = "2.31.8"
 extra["swaggerCoreVersion"] = "2.2.29"
--- export-service/build.gradle.kts
+++ export-service/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.524.0"
+version = "2.525.0"
 
 description = "Atlas Export Service"
 
