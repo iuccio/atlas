9170ff09c9e2b401e9959648915018c8ca9bf37a
atlas <19825793+iuccio@users.noreply.github.com>
2025-04-02 15:00:56 +0200

    [tekton-build-bot] updated version to 2.530.0

--- business-organisation-directory/build.gradle.kts
+++ business-organisation-directory/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.529.0"
+version = "2.530.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
--- user-administration-security/build.gradle.kts
+++ user-administration-security/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.529.0"
+version = "2.530.0"
 
 description= "Atlas User Administration Security Handler"
 
