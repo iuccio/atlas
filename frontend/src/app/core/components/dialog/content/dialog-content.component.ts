0b276fd59a6429c4ddf870e510192275b1822c9b
atlas <TechSupport-ATLAS@sbb.ch>
2025-04-03 15:33:19 +0200

    fix tests

--- frontend/src/app/core/components/dialog/content/dialog-content.component.ts
+++ frontend/src/app/core/components/dialog/content/dialog-content.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { CdkScrollable } from '@angular/cdk/scrolling';
 import { MatDialogContent } from '@angular/material/dialog';
 
 @Component({
-    selector: 'atlas-dialog-content',
-    templateUrl: './dialog-content.component.html',
-    styleUrls: ['dialog-content.component.scss'],
-    imports: [CdkScrollable, MatDialogContent]
+  selector: 'atlas-dialog-content',
+  templateUrl: './dialog-content.component.html',
+  styleUrls: ['dialog-content.component.scss'],
+  imports: [MatDialogContent],
 })
 export class DialogContentComponent {}
