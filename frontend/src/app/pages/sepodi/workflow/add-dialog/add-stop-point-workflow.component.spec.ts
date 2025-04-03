78206f69418dc82c2c5c0d4b2ac2f04cec62631e
atlas <19825793+iuccio@users.noreply.github.com>
2025-04-03 15:33:19 +0200

    fix tests

--- frontend/src/app/pages/sepodi/workflow/add-dialog/add-stop-point-workflow.component.spec.ts
+++ frontend/src/app/pages/sepodi/workflow/add-dialog/add-stop-point-workflow.component.spec.ts
@@ -1,111 +1,72 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import {AddStopPointWorkflowComponent} from './add-stop-point-workflow.component';
-import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
-import {TranslatePipe} from '@ngx-translate/core';
-import {WorkflowFormComponent} from "../../../../core/workflow/workflow-form/workflow-form.component";
-import {WorkflowCheckFormComponent} from "../../../../core/workflow/workflow-check-form/workflow-check-form.component";
-import {CommentComponent} from "../../../../core/form-components/comment/comment.component";
-import {ErrorNotificationComponent} from "../../../../core/notification/error/error-notification.component";
-import {adminUserServiceMock, MockAtlasButtonComponent} from "../../../../app.testing.mocks";
-import {AppTestingModule} from "../../../../app.testing.module";
-import {NotificationService} from "../../../../core/notification/notification.service";
-import {FormModule} from "../../../../core/module/form.module";
-import {AddStopPointWorkflowDialogData} from "./add-stop-point-workflow-dialog-data";
-import {BERN_WYLEREGG} from "../../../../../test/data/service-point";
-import {StringListComponent} from "../../../../core/form-components/string-list/string-list.component";
-import {DisplayDatePipe} from "../../../../core/pipe/display-date.pipe";
-import {SplitServicePointNumberPipe} from "../../../../core/search-service-point/split-service-point-number.pipe";
-import {DetailPageContentComponent} from "../../../../core/components/detail-page-content/detail-page-content.component";
-import {DetailPageContainerComponent} from "../../../../core/components/detail-page-container/detail-page-container.component";
-import {DetailFooterComponent} from "../../../../core/components/detail-footer/detail-footer.component";
-import {DetailHelperService} from "../../../../core/detail/detail-helper.service";
-import {of} from "rxjs";
+import { AddStopPointWorkflowComponent } from './add-stop-point-workflow.component';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { TranslateModule, TranslatePipe } from '@ngx-translate/core';
+import { adminUserServiceMock } from '../../../../app.testing.mocks';
+import { NotificationService } from '../../../../core/notification/notification.service';
+import { AddStopPointWorkflowDialogData } from './add-stop-point-workflow-dialog-data';
+import { BERN_WYLEREGG } from '../../../../../test/data/service-point';
+import { DetailHelperService } from '../../../../core/detail/detail-helper.service';
+import { of } from 'rxjs';
 import {
   DecisionType,
   JudgementType,
-  ReadServicePointVersion,
   ReadStopPointWorkflow,
-  StopPointWorkflowService
-} from "../../../../api";
-import {Router} from "@angular/router";
-import {AtlasSpacerComponent} from "../../../../core/components/spacer/atlas-spacer.component";
-import {UserService} from "../../../../core/auth/user/user.service";
-import {DialogFooterComponent} from "../../../../core/components/dialog/footer/dialog-footer.component";
-import {DialogContentComponent} from "../../../../core/components/dialog/content/dialog-content.component";
-import {DialogCloseComponent} from "../../../../core/components/dialog/close/dialog-close.component";
-import {ValidationService} from "../../../../core/validation/validation.service";
+  StopPointWorkflowService,
+} from '../../../../api';
+import { Router } from '@angular/router';
+import { UserService } from '../../../../core/auth/user/user.service';
+import { ValidationService } from '../../../../core/validation/validation.service';
 import {
   ExaminantFormGroup,
-  StopPointWorkflowDetailFormGroup, StopPointWorkflowDetailFormGroupBuilder
-} from "../detail-page/detail-form/stop-point-workflow-detail-form-group";
-import {FormArray, FormControl, FormGroup} from "@angular/forms";
-import {Component, Input} from "@angular/core";
-import {
-  StopPointWorkflowExaminantsTableComponent
-} from "../detail-page/examinant-table/stop-point-workflow-examinants-table.component";
+  StopPointWorkflowDetailFormGroup,
+  StopPointWorkflowDetailFormGroupBuilder,
+} from '../detail-page/detail-form/stop-point-workflow-detail-form-group';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { BoSelectionDisplayPipe } from '../../../../core/form-components/bo-select/bo-selection-display.pipe';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 const workflow: ReadStopPointWorkflow = {
   versionId: 1,
   sloid: 'ch:1:sloid:8000',
-  workflowComment: "No comment"
+  workflowComment: 'No comment',
 };
 const dialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['close']);
-const notificationServiceSpy = jasmine.createSpyObj('NotificationService', ['success']);
+const notificationServiceSpy = jasmine.createSpyObj('NotificationService', [
+  'success',
+]);
 const router = jasmine.createSpyObj({
   navigate: Promise.resolve(),
 });
 const detailHelperService = jasmine.createSpyObj({
   confirmLeaveDirtyForm: of(true),
 });
-const stopPointWorkflowService = jasmine.createSpyObj('StopPointWorkflowService', {
-  addStopPointWorkflow: of(workflow)
-});
+const stopPointWorkflowService = jasmine.createSpyObj(
+  'StopPointWorkflowService',
+  {
+    addStopPointWorkflow: of(workflow),
+    getExaminants: of(workflow),
+  }
+);
 
 const workflowDialogData: AddStopPointWorkflowDialogData = {
   title: '',
   message: '',
   stopPoint: BERN_WYLEREGG,
-}
-
-@Component({
-  selector: 'stop-point-workflow-detail-form',
-  template: '<p>Mock AddStopPointWorkflowDetailForm Component</p>',
-  imports: [AppTestingModule, FormModule]
-})
-export class MockStopPointWorkflowDetailFormComponent {
-  @Input() stopPoint!: ReadServicePointVersion;
-  @Input() oldDesignation?: string;
-  @Input() form!: FormGroup<StopPointWorkflowDetailFormGroup>;
-  @Input() currentWorkflow?: ReadStopPointWorkflow;
-}
+};
 
 describe('AddStopPointWorkflowComponent', () => {
   let component: AddStopPointWorkflowComponent;
   let fixture: ComponentFixture<AddStopPointWorkflowComponent>;
 
   beforeEach(async () => {
-    TestBed.configureTestingModule({
-      imports: [AppTestingModule, FormModule, AddStopPointWorkflowComponent,
-        WorkflowFormComponent,
-        WorkflowCheckFormComponent,
-        StopPointWorkflowExaminantsTableComponent,
-        CommentComponent,
-        ErrorNotificationComponent,
-        MockAtlasButtonComponent,
-        MockStopPointWorkflowDetailFormComponent,
-        StringListComponent,
-        MockAtlasButtonComponent,
-        DisplayDatePipe,
-        SplitServicePointNumberPipe,
-        DetailPageContentComponent,
-        DetailPageContainerComponent,
-        DetailFooterComponent,
-        AtlasSpacerComponent,
-        DialogCloseComponent,
-        DialogFooterComponent,
-        DialogContentComponent],
+    await TestBed.configureTestingModule({
+      imports: [AddStopPointWorkflowComponent, TranslateModule.forRoot()],
       providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
         {
           provide: MAT_DIALOG_DATA,
           useValue: workflowDialogData,
@@ -113,14 +74,16 @@ describe('AddStopPointWorkflowComponent', () => {
         { provide: MatDialogRef, useValue: dialogRefSpy },
         { provide: NotificationService, useValue: notificationServiceSpy },
         { provide: DetailHelperService, useValue: detailHelperService },
-        { provide: StopPointWorkflowService, useValue: stopPointWorkflowService },
+        {
+          provide: StopPointWorkflowService,
+          useValue: stopPointWorkflowService,
+        },
         { provide: UserService, useValue: adminUserServiceMock },
         { provide: Router, useValue: router },
         { provide: TranslatePipe },
+        { provide: BoSelectionDisplayPipe },
       ],
-    })
-      .compileComponents()
-      .then();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AddStopPointWorkflowComponent);
     component = fixture.componentInstance;
@@ -141,7 +104,9 @@ describe('AddStopPointWorkflowComponent', () => {
   it('should add workflow via service', () => {
     spyOn(ValidationService, 'validateForm').and.callThrough();
 
-    component.form.controls.examinants.push(StopPointWorkflowDetailFormGroupBuilder.buildExaminantFormGroup());
+    component.form.controls.examinants.push(
+      StopPointWorkflowDetailFormGroupBuilder.buildExaminantFormGroup()
+    );
     const firstExaminant = component.form.controls.examinants.at(0);
     firstExaminant.controls.firstName.setValue('');
     firstExaminant.controls.lastName.setValue('');
@@ -171,10 +136,12 @@ describe('AddStopPointWorkflowComponent', () => {
       judgementIcon: new FormControl<string | null>(null),
       judgement: new FormControl<JudgementType | null>(null),
       decisionType: new FormControl<DecisionType | null>(null),
-      defaultExaminant: new FormControl(false)
+      defaultExaminant: new FormControl(false),
     });
 
-    const formArray = new FormArray<FormGroup<ExaminantFormGroup>>([examinantFormGroup]);
+    const formArray = new FormArray<FormGroup<ExaminantFormGroup>>([
+      examinantFormGroup,
+    ]);
     component.form = new FormGroup<StopPointWorkflowDetailFormGroup>({
       ccEmails: new FormControl<Array<string> | null>(null),
       workflowComment: new FormControl<string | null>('Workflow comment 1'),
@@ -184,17 +151,18 @@ describe('AddStopPointWorkflowComponent', () => {
 
     component.addWorkflow();
 
-    expect(stopPointWorkflowService.addStopPointWorkflow).toHaveBeenCalledWith(jasmine.objectContaining({
-      examinants: [
-        jasmine.objectContaining({
-          firstName: null,
-          lastName: null,
-        })
-      ]
-    }));
+    expect(stopPointWorkflowService.addStopPointWorkflow).toHaveBeenCalledWith(
+      jasmine.objectContaining({
+        examinants: [
+          jasmine.objectContaining({
+            firstName: null,
+            lastName: null,
+          }),
+        ],
+      })
+    );
 
     expect(notificationServiceSpy.success).toHaveBeenCalled();
     expect(dialogRefSpy.close).toHaveBeenCalled();
   });
-
 });
