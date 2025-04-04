132cf8846bd3e05eba0074f68e786f03f1f53d2f
atlas <TechSupport-ATLAS@sbb.ch>
2025-04-04 10:32:06 +0200

    ATLAS-2832: info opportunities to be completed

--- prm-directory/src/test/java/ch/sbb/prm/directory/mapper/PlatformVersionMapperTest.java
+++ prm-directory/src/test/java/ch/sbb/prm/directory/mapper/PlatformVersionMapperTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import ch.sbb.atlas.api.prm.enumeration.BasicAttributeType;
 import ch.sbb.atlas.api.prm.enumeration.BoardingDeviceAttributeType;
 import ch.sbb.atlas.api.prm.enumeration.BooleanOptionalAttributeType;
+import ch.sbb.atlas.api.prm.enumeration.InfoOpportunityAttributeType;
 import ch.sbb.atlas.api.prm.enumeration.VehicleAccessAttributeType;
 import ch.sbb.prm.directory.entity.PlatformVersion;
 import org.junit.jupiter.api.Test;
@@ -18,6 +19,7 @@ class PlatformVersionMapperTest {
 
     assertThat(platformVersion.getTactileSystem()).isEqualTo(BooleanOptionalAttributeType.TO_BE_COMPLETED);
     assertThat(platformVersion.getVehicleAccess()).isEqualTo(VehicleAccessAttributeType.TO_BE_COMPLETED);
+    assertThat(platformVersion.getInfoOpportunities()).containsExactly(InfoOpportunityAttributeType.TO_BE_COMPLETED);
   }
 
   @Test
