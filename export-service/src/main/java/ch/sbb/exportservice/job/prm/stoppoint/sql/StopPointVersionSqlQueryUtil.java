c811efc6dfc043c9625f55a1029a545375daeaa0
atlas <19825793+iuccio@users.noreply.github.com>
2025-04-10 14:17:03 +0200

    ATLAS-2427: fix lint

--- export-service/src/main/java/ch/sbb/exportservice/job/prm/stoppoint/sql/StopPointVersionSqlQueryUtil.java
+++ export-service/src/main/java/ch/sbb/exportservice/job/prm/stoppoint/sql/StopPointVersionSqlQueryUtil.java
@@ -10,12 +10,13 @@ import lombok.extern.slf4j.Slf4j;
 public class StopPointVersionSqlQueryUtil extends SqlQueryUtil {
 
   private static final String SELECT_STATEMENT = """
-      SELECT spv.id, string_agg(spvmot.means_of_transport, '|') as list_of_transports, spv.*
+      SELECT spv.id, string_agg(spvmot.means_of_transport, '|') as list_of_transports, spv.*, ro.recording_obligation
       FROM stop_point_version spv
         LEFT JOIN stop_point_version_means_of_transport spvmot on spv.id = spvmot.stop_point_version_id
+        LEFT JOIN recording_obligation ro on ro.sloid = spv.sloid
       """;
   private static final String WHERE_STATEMENT = "WHERE '%s' between spv.valid_from and spv.valid_to";
-  private static final String GROUP_BY_STATEMENT = "GROUP BY spv.id";
+  private static final String GROUP_BY_STATEMENT = "GROUP BY spv.id, ro.recording_obligation";
 
   public String getSqlQuery(ExportTypeV2 exportTypeV2) {
     final String sqlQuery = buildSqlQuery(SELECT_STATEMENT, getWhereClause(exportTypeV2, WHERE_STATEMENT), GROUP_BY_STATEMENT);
