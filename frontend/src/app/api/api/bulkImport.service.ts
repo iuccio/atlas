5b599f2f157067e2dce6eef9e45da36b12b05b3c
atlas <19825793+iuccio@users.noreply.github.com>
2025-04-15 12:44:48 +0200

    ATLAS-2524 make apiHelper object oriented

--- frontend/src/app/api/api/bulkImport.service.ts
+++ frontend/src/app/api/api/bulkImport.service.ts
@@ -4,7 +4,7 @@ import {catchError, Observable} from 'rxjs';
 
 import {ApplicationType, BulkImportRequest, BulkImportResult, BusinessObjectType, ImportType} from '../model/models';
 import {environment} from "../../../environments/environment";
-import {createFormdata, DEFAULT_HTTP_HEADERS, handleError, validateParams} from "./util/api-helper";
+import {ApiHelper} from "./util/api-helper";
 
 
 @Injectable({
@@ -12,28 +12,27 @@ import {createFormdata, DEFAULT_HTTP_HEADERS, handleError, validateParams} from 
 })
 export class BulkImportService {
 
-  constructor(protected httpClient: HttpClient) {
-  }
+  constructor(protected httpClient: HttpClient) {}
 
   public downloadTemplate(
     applicationType: ApplicationType,
     objectType: BusinessObjectType,
     importType: ImportType,
   ): Observable<Blob> {
-    validateParams({applicationType, objectType, importType});
+    ApiHelper.validateParams({applicationType, objectType, importType});
 
     const url = `${environment.atlasApiUrl}/bulk-import-service/v1/import/bulk/template/${encodeURIComponent(applicationType)}/${encodeURIComponent(String(objectType))}/${encodeURIComponent(importType)}`;
 
     return this.httpClient.get(url, {
       responseType: "blob",
-      headers: DEFAULT_HTTP_HEADERS
+      headers: ApiHelper.DEFAULT_HTTP_HEADERS
     }).pipe(
-      catchError(handleError)
+      catchError(ApiHelper.handleError)
     );
   }
 
   public getBulkImportResults(id: number): Observable<BulkImportResult> {
-    validateParams({id})
+    ApiHelper.validateParams({id})
 
     if (id === null || id === undefined) {
       throw new Error('Required parameter id was null or undefined when calling getBulkImportResults.');
@@ -41,20 +40,20 @@ export class BulkImportService {
     const url = `${environment.atlasApiUrl}/bulk-import-service/v1/import/bulk/${encodeURIComponent(id)}`;
 
     return this.httpClient.get<BulkImportResult>(url, {
-      headers: DEFAULT_HTTP_HEADERS
+      headers: ApiHelper.DEFAULT_HTTP_HEADERS
     }).pipe(
-      catchError(handleError)
+      catchError(ApiHelper.handleError)
     );
   }
 
 
   public startBulkImport(bulkImportRequest: BulkImportRequest, file: Blob): Observable<any> {
-    validateParams({bulkImportRequest, file});
+    ApiHelper.validateParams({bulkImportRequest, file});
 
     const url = `${environment.atlasApiUrl}/bulk-import-service/v1/import/bulk`;
 
-    return this.httpClient.post(url, createFormdata({bulkImportRequest, file})).pipe(
-      catchError(handleError)
+    return this.httpClient.post(url, ApiHelper.createFormData({bulkImportRequest, file})).pipe(
+      catchError(ApiHelper.handleError)
     );
   }
 }
