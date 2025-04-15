66f411b7b71e3866253f402b82619f9ea15cd4ad
atlas <TechSupport-ATLAS@sbb.ch>
2025-04-15 12:44:48 +0200

    ATLAS-2524 make apiHelper object oriented

--- frontend/src/app/api/api/util/api-helper.ts
+++ frontend/src/app/api/api/util/api-helper.ts
@@ -1,37 +1,39 @@
 import {HttpHeaders} from "@angular/common/http";
 import {throwError} from "rxjs";
 
-export const DEFAULT_HTTP_HEADERS = new HttpHeaders({
-  'Accept': '*/*'
-});
+export class ApiHelper {
 
-
-export function handleError() {
-  return throwError(() => 'Unexpected Error occured!');
-}
-
-export function validateParams(params: { [key: string]: any }): void {
-  Object.keys(params).forEach(key => {
-    if (params[key] === null || params[key] === undefined) {
-      throw new Error(`Required parameter '${key}' is null or undefined.`);
-    }
+  public static readonly DEFAULT_HTTP_HEADERS: HttpHeaders = new HttpHeaders({
+    'Accept': '*/*'
   });
-}
 
-export function createBlob(param: any) {
-  return new Blob([JSON.stringify(param)], {type: 'application/json'})
-}
+  public static handleError() {
+    return throwError(() => 'Unexpected Error occured!');
+  }
 
-export function createFormdata(params: { [key: string]: any }){
-  let formData: FormData = new FormData();
+  public static validateParams(params: { [key: string]: any }): void {
+    Object.keys(params).forEach(key => {
+      if (params[key] === null || params[key] === undefined) {
+        throw new Error(`Required parameter '${key}' is null or undefined.`);
+      }
+    });
+  }
 
-  Object.keys(params).forEach(key => {
-    if(params[key] === 'file') {
-      formData.append(key, params[key]);
-    }
-    else {
-      formData.append(key, createBlob(params[key]));
-    }
-  });
-  return formData;
+  public static createBlob(param: any): Blob {
+    return new Blob([JSON.stringify(param)], { type: 'application/json' });
+  }
+
+  public static createFormData(params: { [key: string]: any }): FormData {
+    let formData: FormData = new FormData();
+
+    Object.keys(params).forEach(key => {
+      if(key === 'file') {
+        formData.append(key, params[key]);
+      }
+      else {
+        formData.append(key, ApiHelper.createBlob(params[key]));
+      }
+    });
+    return formData;
+  }
 }
