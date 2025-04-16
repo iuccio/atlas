e6bfcb4cdf3003d21d8acbade20a9d04c2650a49
atlas <19825793+iuccio@users.noreply.github.com>
2025-04-16 11:37:03 +0200

    ATLAS-2670: migrate standalone to lazy loading

--- frontend/src/app/pages/bulk-import/bulk-import-routing.module.ts
+++ frontend/src/app/pages/bulk-import/bulk-import-routing.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BulkImportOverviewComponent } from './overview/bulk-import-overview.component';
-import { canLeaveDirtyForm } from '../../core/leave-guard/leave-dirty-form-guard.service';
-import { BulkImportLogComponent } from './log/bulk-import-log.component';
 
-const routes: Routes = [
+import { canLeaveDirtyForm } from '../../core/leave-guard/leave-dirty-form-guard.service';
+
+export const routes: Routes = [
   {
     path: '',
-    component: BulkImportOverviewComponent,
+    loadComponent: () =>
+      import('./overview/bulk-import-overview.component').then(
+        (m) => m.BulkImportOverviewComponent
+      ),
     canDeactivate: [canLeaveDirtyForm],
   },
   {
     path: ':id',
-    component: BulkImportLogComponent,
+    loadComponent: () =>
+      import('./log/bulk-import-log.component').then(
+        (m) => m.BulkImportLogComponent
+      ),
   },
 ];
 
--- frontend/src/app/pages/lidi/lidi.routing.module.ts
+++ frontend/src/app/pages/lidi/lidi.routing.module.ts
@@ -1,21 +1,22 @@
-import {RouterModule, Routes} from '@angular/router';
-import {NgModule} from '@angular/core';
-import {LidiOverviewComponent} from './overview/lidi-overview.component';
-import {LineDetailComponent} from './lines/detail/line-detail.component';
-import {lineResolver} from './lines/detail/line-detail.resolver';
-import {Pages} from '../pages';
-import {SublineDetailComponent} from './sublines/detail/subline-detail.component';
-import {sublineResolver} from './sublines/detail/subline-detail.resolver';
-import {LinesComponent} from './lines/lines.component';
-import {LidiWorkflowOverviewComponent} from './workflow/overview/lidi-workflow-overview.component';
-import {lineVersionSnapshotResolver} from './workflow/detail/line-version-snapshot.resolver';
-import {LineVersionSnapshotDetailComponent} from './workflow/detail/line-version-snapshot-detail.component';
-import {canLeaveDirtyForm} from '../../core/leave-guard/leave-dirty-form-guard.service';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
 
-const routes: Routes = [
+import { lineResolver } from './lines/detail/line-detail.resolver';
+import { Pages } from '../pages';
+
+import { sublineResolver } from './sublines/detail/subline-detail.resolver';
+
+import { lineVersionSnapshotResolver } from './workflow/detail/line-version-snapshot.resolver';
+
+import { canLeaveDirtyForm } from '../../core/leave-guard/leave-dirty-form-guard.service';
+
+export const routes: Routes = [
   {
     path: Pages.LINES.path + '/:id',
-    component: LineDetailComponent,
+    loadComponent: () =>
+      import('./lines/detail/line-detail.component').then(
+        (m) => m.LineDetailComponent
+      ),
     canDeactivate: [canLeaveDirtyForm],
     resolve: {
       lineDetail: lineResolver,
@@ -24,7 +25,10 @@ const routes: Routes = [
   },
   {
     path: Pages.SUBLINES.path + '/:id',
-    component: SublineDetailComponent,
+    loadComponent: () =>
+      import('./sublines/detail/subline-detail.component').then(
+        (m) => m.SublineDetailComponent
+      ),
     canDeactivate: [canLeaveDirtyForm],
     resolve: {
       sublineDetail: sublineResolver,
@@ -33,7 +37,10 @@ const routes: Routes = [
   },
   {
     path: Pages.WORKFLOWS.path + '/:id',
-    component: LineVersionSnapshotDetailComponent,
+    loadComponent: () =>
+      import('./workflow/detail/line-version-snapshot-detail.component').then(
+        (m) => m.LineVersionSnapshotDetailComponent
+      ),
     resolve: {
       lineVersionSnapshot: lineVersionSnapshotResolver,
     },
@@ -41,15 +48,22 @@ const routes: Routes = [
   },
   {
     path: '',
-    component: LidiOverviewComponent,
+    loadComponent: () =>
+      import('./overview/lidi-overview.component').then(
+        (m) => m.LidiOverviewComponent
+      ),
     children: [
       {
         path: Pages.LINES.path,
-        component: LinesComponent,
+        loadComponent: () =>
+          import('./lines/lines.component').then((m) => m.LinesComponent),
       },
       {
         path: Pages.WORKFLOWS.path,
-        component: LidiWorkflowOverviewComponent,
+        loadComponent: () =>
+          import('./workflow/overview/lidi-workflow-overview.component').then(
+            (m) => m.LidiWorkflowOverviewComponent
+          ),
       },
       { path: '**', redirectTo: Pages.LINES.path },
     ],
