9b30cb9982fc883bb72f9f580a20044305303c4c
atlas <19825793+iuccio@users.noreply.github.com>
2025-04-22 11:19:32 +0200

    ATLAS-2105: finish generation of valid spec.yaml and change backend internal urls

--- scheduling/src/main/java/ch/sbb/scheduling/client/LiDiClient.java
+++ scheduling/src/main/java/ch/sbb/scheduling/client/LiDiClient.java
@@ -9,31 +9,34 @@ import org.springframework.web.bind.annotation.PostMapping;
 @FeignClient(name = "lidiClient", url = "${atlas.client.gateway.url}", configuration = OAuthFeignConfig.class)
 public interface LiDiClient {
 
-  @PostMapping(value = "/line-directory/v1/lines/export-csv/full", produces = MediaType.APPLICATION_JSON_VALUE)
+  @PostMapping(value = "/line-directory/internal/lines/export-csv/full", produces = MediaType.APPLICATION_JSON_VALUE)
   Response putLiDiLineExportFull();
 
-  @PostMapping(value = "/line-directory/v1/lines/export-csv/actual", produces = MediaType.APPLICATION_JSON_VALUE)
+  @PostMapping(value = "/line-directory/internal/lines/export-csv/actual", produces = MediaType.APPLICATION_JSON_VALUE)
   Response putLiDiLineExportActual();
 
-  @PostMapping(value = "/line-directory/v1/lines/export-csv/timetable-year-change", produces = MediaType.APPLICATION_JSON_VALUE)
+  @PostMapping(value = "/line-directory/internal/lines/export-csv/timetable-year-change", produces =
+      MediaType.APPLICATION_JSON_VALUE)
   Response putLiDiLineExportNextTimetableVersions();
 
-  @PostMapping(value = "/line-directory/v1/sublines/export-csv/full", produces = MediaType.APPLICATION_JSON_VALUE)
+  @PostMapping(value = "/line-directory/internal/sublines/export-csv/full", produces = MediaType.APPLICATION_JSON_VALUE)
   Response putLiDiSublineExportFull();
 
-  @PostMapping(value = "/line-directory/v1/sublines/export-csv/actual", produces = MediaType.APPLICATION_JSON_VALUE)
+  @PostMapping(value = "/line-directory/internal/sublines/export-csv/actual", produces = MediaType.APPLICATION_JSON_VALUE)
   Response putLiDiSublineExportActual();
 
-  @PostMapping(value = "/line-directory/v1/sublines/export-csv/timetable-year-change", produces = MediaType.APPLICATION_JSON_VALUE)
+  @PostMapping(value = "/line-directory/internal/sublines/export-csv/timetable-year-change", produces =
+      MediaType.APPLICATION_JSON_VALUE)
   Response putLiDiSublineExportNextTimetableVersions();
 
-  @PostMapping(value = "/line-directory/v1/field-numbers/export-csv/full", produces = MediaType.APPLICATION_JSON_VALUE)
+  @PostMapping(value = "/line-directory/internal/field-numbers/export-csv/full", produces = MediaType.APPLICATION_JSON_VALUE)
   Response putLiDiTimetableFieldNumberExportFull();
 
-  @PostMapping(value = "/line-directory/v1/field-numbers/export-csv/actual", produces = MediaType.APPLICATION_JSON_VALUE)
+  @PostMapping(value = "/line-directory/internal/field-numbers/export-csv/actual", produces = MediaType.APPLICATION_JSON_VALUE)
   Response putLiDiTimetableFieldNumberExportActual();
 
-  @PostMapping(value = "/line-directory/v1/field-numbers/export-csv/timetable-year-change", produces = MediaType.APPLICATION_JSON_VALUE)
+  @PostMapping(value = "/line-directory/internal/field-numbers/export-csv/timetable-year-change", produces =
+      MediaType.APPLICATION_JSON_VALUE)
   Response putLiDiTimetableFieldNumberExportNextTimetableVersions();
 
 }
