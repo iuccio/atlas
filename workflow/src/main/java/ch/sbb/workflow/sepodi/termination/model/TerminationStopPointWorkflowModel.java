2e121358649bbaeaa37dc9f0d3de828154321b19
atlas <19825793+iuccio@users.noreply.github.com>
2025-04-22 17:05:10 +0200

    ATLAS-2713: adc basic structure (WIP)

---
+++ workflow/src/main/java/ch/sbb/workflow/sepodi/termination/model/TerminationStopPointWorkflowModel.java
@@ -0,0 +1,77 @@
+package ch.sbb.workflow.sepodi.termination.model;
+
+import ch.sbb.atlas.api.AtlasFieldLengths;
+import ch.sbb.atlas.api.workflow.ClientPersonModel;
+import ch.sbb.workflow.sepodi.termination.entity.TerminationWorkflowStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.AccessMode;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@AllArgsConstructor
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@Schema(name = "TerminationStopPointAddWorkflow")
+public class TerminationStopPointWorkflowModel {
+
+  @Schema(description = "Workflow id", accessMode = AccessMode.READ_ONLY)
+  private Long id;
+
+  @Schema(description = "Service Point version id")
+  @NotNull
+  private Long versionId;
+
+  @Size(min = 1, max = AtlasFieldLengths.LENGTH_255)
+  @NotEmpty
+  @Schema(description = "Applicant mail", example = "me@you.ch")
+  private String applicantMail;
+
+  @Size(min = 1, max = AtlasFieldLengths.LENGTH_500)
+  @Schema(description = "Unique code for locations that is used in customer information. The structure is described in the "
+      + "“Swiss Location ID” specification, chapter 4.2. The document is available here. "
+      + "https://transportdatamanagement.ch/standards/", example = "ch:1:sloid:18771")
+  @NotNull
+  private String sloid;
+
+  @NotNull
+  @Schema(description = "Service Point version SBOID")
+  private String sboid;
+
+  @Schema(description = "Termination Workflow Status", accessMode = AccessMode.READ_ONLY)
+  private TerminationWorkflowStatus status;
+
+  @Schema(description = "Termination Date defined by Business Organisation managing the Stop Point")
+  private LocalDateTime boTerminationDate;
+
+  @Schema(description = "Termination Date defined by Info Plus")
+  private LocalDateTime infoPlusTerminationDate;
+
+  @Schema(description = "Termination Date defined by NOVA")
+  private LocalDateTime novaTerminationDate;
+
+  @Schema(description = "Info Plus Examinant")
+  private ClientPersonModel infoPlusExaminant;
+
+  @Schema(description = "Nova Examinant")
+  private ClientPersonModel novaExaminant;
+
+  @Schema(description = "Object creation date", example = "01.01.2000")
+  private LocalDateTime creationDate;
+
+  @Schema(description = "User creator", example = "u123456", accessMode = AccessMode.READ_ONLY)
+  private String creator;
+
+  @Schema(description = "Last edition date", example = "01.01.2000")
+  private LocalDateTime editionDate;
+
+  @Schema(description = "Editor", example = "u123456", accessMode = AccessMode.READ_ONLY)
+  private String editor;
+
+}
---
+++ workflow/src/main/java/ch/sbb/workflow/sepodi/termination/service/TerminationStopPointWorkflowService.java
@@ -0,0 +1,32 @@
+package ch.sbb.workflow.sepodi.termination.service;
+
+import ch.sbb.atlas.model.exception.NotFoundException.IdNotFoundException;
+import ch.sbb.atlas.redact.Redacted;
+import ch.sbb.workflow.sepodi.termination.entity.TerminationStopPointWorkflow;
+import ch.sbb.workflow.sepodi.termination.entity.TerminationWorkflowStatus;
+import ch.sbb.workflow.sepodi.termination.mapper.TerminationStopPointWorkflowMapper;
+import ch.sbb.workflow.sepodi.termination.model.StartTerminationStopPointWorkflowModel;
+import ch.sbb.workflow.sepodi.termination.repository.TerminationStopPointWorkflowRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class TerminationStopPointWorkflowService {
+
+  private final TerminationStopPointWorkflowRepository repository;
+
+  public TerminationStopPointWorkflow startTerminationWorkflow(StartTerminationStopPointWorkflowModel model) {
+    TerminationStopPointWorkflow terminationStopPointWorkflow = TerminationStopPointWorkflowMapper.toEntityStart(model);
+    terminationStopPointWorkflow.setStatus(TerminationWorkflowStatus.STARTED);
+    terminationStopPointWorkflow.setNovaTerminationDate(terminationStopPointWorkflow.getBoTerminationDate());
+    terminationStopPointWorkflow.setInfoPlusTerminationDate(terminationStopPointWorkflow.getBoTerminationDate());
+    return repository.save(terminationStopPointWorkflow);
+  }
+
+  @Redacted
+  public TerminationStopPointWorkflow getTerminationWorkflow(Long id) {
+    return repository.findById(id).orElseThrow(() -> new IdNotFoundException(id));
+  }
+
+}
