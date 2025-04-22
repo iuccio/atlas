874e37dc5f48d6915b2a35cc2b30172f01165a54
atlas <19825793+iuccio@users.noreply.github.com>
2025-04-22 04:00:56 +0000

    Update GRADLE Dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 group = "ch.sbb.atlas"
 version = "2.560.0"
 
-extra["awsS3Version"] = "2.31.23"
+extra["awsS3Version"] = "2.31.26"
 
 extra["swaggerCoreVersion"] = "2.2.30"
 extra["openapiStarterCommonVersion"] = "2.8.6"
