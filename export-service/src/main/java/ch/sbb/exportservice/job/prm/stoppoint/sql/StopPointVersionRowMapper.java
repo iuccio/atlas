ee3d3f127a8f9ccffba8729e6b76f04af17e4efb
atlas <TechSupport-ATLAS@sbb.ch>
2025-04-28 09:49:32 +0200

    ATLAS-2878: Remove from export

--- export-service/src/main/java/ch/sbb/exportservice/job/prm/stoppoint/sql/StopPointVersionRowMapper.java
+++ export-service/src/main/java/ch/sbb/exportservice/job/prm/stoppoint/sql/StopPointVersionRowMapper.java
@@ -68,10 +68,6 @@ public class StopPointVersionRowMapper implements RowMapper<StopPointVersion> {
             BooleanOptionalAttributeType.valueOf(rs.getString("ticket_machine")) : null);
     setMeansOfTransport(builder, rs.getString("list_of_transports"));
 
-    builder.recordingObligation(
-        rs.getObject("recording_obligation") == null ? true : rs.getBoolean("recording_obligation")
-    );
-
     builder.validFrom(rs.getObject("valid_from", LocalDate.class));
     builder.validTo(rs.getObject("valid_to", LocalDate.class));
     builder.creationDate(rs.getObject("creation_date", LocalDateTime.class));
