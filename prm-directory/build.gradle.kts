1a2322882ea90d70a526542354cafddbac12345b
atlas <19825793+iuccio@users.noreply.github.com>
2025-05-01 17:31:17 +0200

    [tekton-build-bot] updated version to 2.583.0

--- prm-directory/build.gradle.kts
+++ prm-directory/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.582.0"
+version = "2.583.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
--- workflow/build.gradle.kts
+++ workflow/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.582.0"
+version = "2.583.0"
 
 description = "Atlas Scheduling Service"
 extra["shedlockVersion"] = "5.16.0"
