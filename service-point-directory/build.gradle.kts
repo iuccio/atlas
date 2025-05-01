a0f510dd1ac42170d67d60d630f596bc45e29ca9
atlas <19825793+iuccio@users.noreply.github.com>
2025-05-01 17:31:17 +0200

    [tekton-build-bot] updated version to 2.583.0

--- service-point-directory/build.gradle.kts
+++ service-point-directory/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.582.0"
+version = "2.583.0"
 
 description = "ServicePointDirectory"
 extra["shedlockVersion"] = "5.16.0"
--- user-administration/build.gradle.kts
+++ user-administration/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.582.0"
+version = "2.583.0"
 
 description = "Atlas User Administration"
 extra["microsoftGraphSdkVersion"] = "6.36.0"
