e9ca0cbc25c90b341f8d1e06690c0e06a1e1f2ed
atlas <19825793+iuccio@users.noreply.github.com>
2025-05-02 09:15:38 +0200

    [tekton-build-bot] updated version to 2.586.0

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.585.0"
+version = "2.586.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
--- service-point-directory/build.gradle.kts
+++ service-point-directory/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.585.0"
+version = "2.586.0"
 
 description = "ServicePointDirectory"
 extra["shedlockVersion"] = "5.16.0"
