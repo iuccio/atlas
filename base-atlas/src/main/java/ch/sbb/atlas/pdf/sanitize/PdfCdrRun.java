3ff9b1d7ac00b9ab2028ebf34487487debb59e66
atlas <TechSupport-ATLAS@sbb.ch>
2025-05-06 16:55:46 +0200

    ATLAS-2872: Improve CDR Api

--- base-atlas/src/main/java/ch/sbb/atlas/pdf/sanitize/PdfCdrRun.java
+++ base-atlas/src/main/java/ch/sbb/atlas/pdf/sanitize/PdfCdrRun.java
@@ -3,16 +3,12 @@ package ch.sbb.atlas.pdf.sanitize;
 import java.io.IOException;
 import java.io.OutputStream;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.pdfbox.Loader;
-import org.apache.pdfbox.io.RandomAccessRead;
 import org.apache.pdfbox.pdmodel.PDDocument;
 
 @Slf4j
 class PdfCdrRun {
 
-  PdfCdrResult sanitize(RandomAccessRead inputStream, OutputStream outputStream) throws IOException {
-    PDDocument doc = Loader.loadPDF(inputStream);
-
+  PdfCdrResult sanitize(PDDocument doc, OutputStream outputStream) throws IOException {
     PdfCdrResult result = new PdfCdrResult();
     new PdfCdrNamesSanitization(result).sanitizeNamed(doc, doc.getDocumentCatalog().getNames());
     new PdfCdrDocumentCatalogSanitization(result).sanitize(doc.getDocumentCatalog());
