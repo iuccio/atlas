e618b10e5ca3128571509b788089af7e3494a556
atlas <19825793+iuccio@users.noreply.github.com>
2025-05-08 07:02:45 +0200

    [tekton-build-bot] updated version to 2.601.0

--- auto-rest-doc/build.gradle.kts
+++ auto-rest-doc/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.600.0"
+version = "2.601.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")//need it?
--- line-directory/build.gradle.kts
+++ line-directory/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.600.0"
+version = "2.601.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
