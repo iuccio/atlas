b343b75ce6cf122eacf85ab88bbd55b33be17dcc
atlas <TechSupport-ATLAS@sbb.ch>
2025-05-08 10:15:22 +0200

    ATLAS-2902: refresh obligation on search

--- frontend/src/app/core/prm-recording-obligation/prm-recording-obligation.component.ts
+++ frontend/src/app/core/prm-recording-obligation/prm-recording-obligation.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { ApplicationType } from '../../api';
 import { PermissionService } from '../auth/permission/permission.service';
 import { PersonWithReducedMobilityService } from '../../api/service/prm/person-with-reduced-mobility.service';
@@ -11,7 +11,7 @@ import { AtlasSlideToggleComponent } from '../form-components/atlas-slide-toggle
   templateUrl: './prm-recording-obligation.component.html',
   imports: [TranslatePipe, AtlasSlideToggleComponent],
 })
-export class PrmRecordingObligationComponent implements OnInit {
+export class PrmRecordingObligationComponent implements OnInit, OnChanges {
   recordingObligation = true;
   isPrmSupervisor = false;
 
@@ -29,6 +29,14 @@ export class PrmRecordingObligationComponent implements OnInit {
       ApplicationType.Prm
     );
 
+    this.initCurrentRecordingObligation();
+  }
+
+  ngOnChanges() {
+    this.initCurrentRecordingObligation();
+  }
+
+  private initCurrentRecordingObligation() {
     this.personWithReducedMobilityService
       .getRecordingObligation(this.sloid)
       .subscribe(
