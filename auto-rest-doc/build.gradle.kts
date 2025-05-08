e741f655287ae7bce392eb077d32d4a35c7eb406
atlas <19825793+iuccio@users.noreply.github.com>
2025-05-08 15:34:58 +0200

    [tekton-build-bot] updated version to 2.611.0

--- auto-rest-doc/build.gradle.kts
+++ auto-rest-doc/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.610.0"
+version = "2.611.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")//need it?
--- user-administration/build.gradle.kts
+++ user-administration/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.610.0"
+version = "2.611.0"
 
 description = "Atlas User Administration"
 extra["microsoftGraphSdkVersion"] = "6.37.0"
