bbf08017ac7ba741e4f304a93380a60fef87bbe5
atlas <TechSupport-ATLAS@sbb.ch>
2025-05-11 04:00:32 +0000

    Update GRADLE Dependencies

--- service-point-directory/build.gradle.kts
+++ service-point-directory/build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
     implementation("org.flywaydb:flyway-core")
 
     implementation("com.google.protobuf:protobuf-java:4.30.2")
-    annotationProcessor("org.hibernate.orm:hibernate-jpamodelgen:6.6.13.Final")
+    annotationProcessor("org.hibernate.orm:hibernate-jpamodelgen:6.6.14.Final")
 
     runtimeOnly("org.postgresql:postgresql")
 
