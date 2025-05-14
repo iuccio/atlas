b476503a5be7fcfc30589ebec18e767101ae9f2b
atlas <TechSupport-ATLAS@sbb.ch>
2025-05-14 09:58:00 +0200

    [tekton-build-bot] updated version to 2.625.0

--- auto-rest-doc/build.gradle.kts
+++ auto-rest-doc/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.624.0"
+version = "2.625.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")//need it?
--- base-atlas/build.gradle.kts
+++ base-atlas/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.624.0"
+version = "2.625.0"
 
 configurations {
     create("test") //used to create the base-atlas-test jar
