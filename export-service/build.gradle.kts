2dadcc1d3e66ce94733278cd57d546c2ec8f2b51
atlas <19825793+iuccio@users.noreply.github.com>
2025-05-19 07:47:39 +0200

    [tekton-build-bot] updated version to 2.630.0

--- export-service/build.gradle.kts
+++ export-service/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.629.0"
+version = "2.630.0"
 
 description = "Atlas Export Service"
 
--- service-point-directory/build.gradle.kts
+++ service-point-directory/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.629.0"
+version = "2.630.0"
 
 description = "ServicePointDirectory"
 extra["shedlockVersion"] = "5.16.0"
