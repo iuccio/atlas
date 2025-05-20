7cd41200ebec99c2f452191bb28d9589721259ef
atlas <TechSupport-ATLAS@sbb.ch>
2025-05-20 11:55:45 +0200

    ATLAS-2763 try to make sonar happy

--- base-atlas/src/test/java/ch/sbb/atlas/api/user/administration/workflow/InfoPlusTerminationVotePermissionRestrictionModelTest.java
+++ base-atlas/src/test/java/ch/sbb/atlas/api/user/administration/workflow/InfoPlusTerminationVotePermissionRestrictionModelTest.java
@@ -1,7 +1,9 @@
 package ch.sbb.atlas.api.user.administration.workflow;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import ch.sbb.atlas.kafka.model.user.admin.PermissionRestrictionType;
 import org.junit.jupiter.api.Test;
@@ -15,5 +17,22 @@ class InfoPlusTerminationVotePermissionRestrictionModelTest {
     assertNull(model.getValue());
     assertEquals("null", model.getValueAsString());
     assertEquals(PermissionRestrictionType.INFO_PLUS_TERMINATION_VOTE, model.getType());
+  }
+
+  @Test
+  void shouldSetValueTrue() {
+    InfoPlusTerminationVotePermissionRestrictionModel model = new InfoPlusTerminationVotePermissionRestrictionModel(true);
+
+    assertTrue(model.getValue());
+    assertEquals("true", model.getValueAsString());
+  }
+
+  @Test
+  void shouldSetValueFalse() {
+    InfoPlusTerminationVotePermissionRestrictionModel model = new InfoPlusTerminationVotePermissionRestrictionModel();
+    model.setValueAsString("false");
+
+    assertFalse(model.getValue());
+    assertEquals("false", model.getValueAsString());
   }
 }
