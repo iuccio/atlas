001f136df551a1cc99077e2162a6e063ec4e1fdc
atlas <19825793+iuccio@users.noreply.github.com>
2025-05-20 11:55:45 +0200

    ATLAS-2763 try to make sonar happy

--- base-atlas/src/test/java/ch/sbb/atlas/api/user/administration/workflow/NovaTerminationVotePermissionRestrictionModelTest.java
+++ base-atlas/src/test/java/ch/sbb/atlas/api/user/administration/workflow/NovaTerminationVotePermissionRestrictionModelTest.java
@@ -1,7 +1,9 @@
 package ch.sbb.atlas.api.user.administration.workflow;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import ch.sbb.atlas.kafka.model.user.admin.PermissionRestrictionType;
 import org.junit.jupiter.api.Test;
@@ -16,4 +18,22 @@ class NovaTerminationVotePermissionRestrictionModelTest {
     assertEquals("null", model.getValueAsString());
     assertEquals(PermissionRestrictionType.NOVA_TERMINATION_VOTE, model.getType());
   }
+
+  @Test
+  void shouldSetValueTrue() {
+    NovaTerminationVotePermissionRestrictionModel model = new NovaTerminationVotePermissionRestrictionModel(true);
+
+    assertTrue(model.getValue());
+    assertEquals("true", model.getValueAsString());
+  }
+
+  @Test
+  void shouldSetValueFalse() {
+    NovaTerminationVotePermissionRestrictionModel model = new NovaTerminationVotePermissionRestrictionModel();
+    model.setValueAsString("false");
+
+    assertFalse(model.getValue());
+    assertEquals("false", model.getValueAsString());
+  }
+
 }
