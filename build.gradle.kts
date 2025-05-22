d4604fd70733f769e1a3767b5673c6a15adf52a3
atlas <19825793+iuccio@users.noreply.github.com>
2025-05-22 15:32:39 +0200

    [tekton-build-bot] updated version to 2.643.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.642.0"
+version = "2.643.0"
 
 extra["awsS3Version"] = "2.31.47"
 
--- business-organisation-directory/build.gradle.kts
+++ business-organisation-directory/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.642.0"
+version = "2.643.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
