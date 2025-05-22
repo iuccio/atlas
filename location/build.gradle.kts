63a63fd143bc9d395fe0b2436c0521c72c77ef24
atlas <TechSupport-ATLAS@sbb.ch>
2025-05-22 15:32:39 +0200

    [tekton-build-bot] updated version to 2.643.0

--- location/build.gradle.kts
+++ location/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.642.0"
+version = "2.643.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
--- user-administration/build.gradle.kts
+++ user-administration/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.642.0"
+version = "2.643.0"
 
 description = "Atlas User Administration"
 extra["microsoftGraphSdkVersion"] = "6.39.0"
