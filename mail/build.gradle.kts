ec613ae74dbaa4909c5a9804e8d432b67e1634a9
atlas <19825793+iuccio@users.noreply.github.com>
2025-05-26 15:22:23 +0200

    [tekton-build-bot] updated version to 2.646.0

--- mail/build.gradle.kts
+++ mail/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.645.0"
+version = "2.646.0"
 
 description = "Atlas Mail Service"
 
--- user-administration-security/build.gradle.kts
+++ user-administration-security/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.645.0"
+version = "2.646.0"
 
 description= "Atlas User Administration Security Handler"
 
