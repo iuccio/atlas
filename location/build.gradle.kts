195b24b828c12515afc6e63be2a17aafa16e315c
atlas <19825793+iuccio@users.noreply.github.com>
2025-05-26 07:37:48 +0200

    [tekton-build-bot] updated version to 2.644.0

--- location/build.gradle.kts
+++ location/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.643.0"
+version = "2.644.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
--- workflow/build.gradle.kts
+++ workflow/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.643.0"
+version = "2.644.0"
 
 description = "Atlas Scheduling Service"
 extra["shedlockVersion"] = "5.16.0"
