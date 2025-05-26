2286cff7232b7b50d8eb6e2ae8949de41ce80cf0
atlas <19825793+iuccio@users.noreply.github.com>
2025-05-26 10:52:33 +0200

    [tekton-build-bot] updated version to 2.645.0

--- auto-rest-doc/build.gradle.kts
+++ auto-rest-doc/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.644.0"
+version = "2.645.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")//need it?
--- business-organisation-directory/build.gradle.kts
+++ business-organisation-directory/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.644.0"
+version = "2.645.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
