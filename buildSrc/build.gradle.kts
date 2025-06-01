d728da18c49f0869884e780dfcd19efc3d1ac0b1
atlas <19825793+iuccio@users.noreply.github.com>
2025-06-01 04:00:32 +0000

    fix(deps): update gradle dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,5 +17,5 @@ repositories {
 dependencies {
     implementation("org.asciidoctor.jvm.convert:org.asciidoctor.jvm.convert.gradle.plugin:4.0.4")
     implementation("io.spring.dependency-management:io.spring.dependency-management.gradle.plugin:1.1.7")
-    implementation("org.springframework.boot:org.springframework.boot.gradle.plugin:3.4.5")
+    implementation("org.springframework.boot:org.springframework.boot.gradle.plugin:3.5.0")
 }
