e1c03ed22b38ff95995c30daafbafab400af2409
atlas <19825793+iuccio@users.noreply.github.com>
2025-06-03 13:44:09 +0200

    [tekton-build-bot] updated version to 2.652.0

--- mail/build.gradle.kts
+++ mail/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.651.0"
+version = "2.652.0"
 
 description = "Atlas Mail Service"
 
--- user-administration/build.gradle.kts
+++ user-administration/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.651.0"
+version = "2.652.0"
 
 description = "Atlas User Administration"
 extra["microsoftGraphSdkVersion"] = "6.39.0"
