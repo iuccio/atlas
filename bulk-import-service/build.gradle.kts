5e9efb80077737588b69a4decd4342fd41d1a3bd
atlas <TechSupport-ATLAS@sbb.ch>
2025-06-23 07:35:09 +0200

    [tekton-build-bot] updated version to 2.686.0

--- bulk-import-service/build.gradle.kts
+++ bulk-import-service/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.685.0"
+version = "2.686.0"
 
 description = "Atlas Bulk Import Service"
 
--- user-administration/build.gradle.kts
+++ user-administration/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.685.0"
+version = "2.686.0"
 
 description = "Atlas User Administration"
 extra["microsoftGraphSdkVersion"] = "6.42.1"
