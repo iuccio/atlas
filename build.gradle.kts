657c24fa4b4db8e0b0384956db6e42adbd74fe01
atlas <19825793+iuccio@users.noreply.github.com>
2025-06-28 04:00:30 +0000

    fix(deps): update dependency software.amazon.awssdk:s3 to v2.31.73

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 group = "ch.sbb.atlas"
 version = "2.697.0"
 
-extra["awsS3Version"] = "2.31.71"
+extra["awsS3Version"] = "2.31.73"
 
 extra["swaggerCoreVersion"] = "2.2.34"
 extra["openapiStarterCommonVersion"] = "2.8.9"
