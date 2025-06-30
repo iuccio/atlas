195428e5116dbe539340dca29b63e387299e0a32
atlas <19825793+iuccio@users.noreply.github.com>
2025-06-30 16:45:43 +0200

    [tekton-build-bot] updated version to 2.702.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.701.0"
+version = "2.702.0"
 
 extra["awsS3Version"] = "2.31.73"
 
--- kafka/build.gradle.kts
+++ kafka/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.701.0"
+version = "2.702.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
