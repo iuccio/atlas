0b6ea0c76c26452c259b6c33e80d671f0c48afe2
atlas <19825793+iuccio@users.noreply.github.com>
2025-06-30 13:09:10 +0200

    ATLAS-2714: remove unused company attribute

--- business-organisation-directory/src/main/java/ch/sbb/business/organisation/directory/entity/Company.java
+++ business-organisation-directory/src/main/java/ch/sbb/business/organisation/directory/entity/Company.java
@@ -4,14 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldNameConstants;
 import lombok.experimental.SuperBuilder;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 @Data
 @SuperBuilder
@@ -26,8 +23,6 @@ public class Company {
 
   private String name;
 
-  private String nameAscii;
-
   private String url;
 
   @Column(columnDefinition = "DATE")
@@ -41,31 +36,5 @@ public class Company {
   private String freeText;
 
   private String countryCodeIso;
-
-  private boolean passengerFlag;
-
-  private boolean freightFlag;
-
-  private boolean infrastructureFlag;
-
-  private boolean otherCompanyFlag;
-
-  private boolean neEntityFlag;
-
-  private boolean ceEntityFlag;
-
-  @Column(columnDefinition = "TIMESTAMP")
-  private LocalDateTime addDate;
-
-  @Column(columnDefinition = "TIMESTAMP")
-  private LocalDateTime modifiedDate;
-
-  @CreationTimestamp
-  @Column(columnDefinition = "TIMESTAMP", updatable = false)
-  private LocalDateTime creationDate;
-
-  @UpdateTimestamp
-  @Column(columnDefinition = "TIMESTAMP")
-  private LocalDateTime editionDate;
 
 }
