73880cb005fcb43bb62f36e47dba25674c220f98
atlas <19825793+iuccio@users.noreply.github.com>
2025-07-07 16:49:31 +0200

    ATLAS-2964 add sector group api test

--- base-atlas/src/main/java/ch/sbb/atlas/api/servicepoint/sector/UpdateSectorGroupVersionModel.java
+++ base-atlas/src/main/java/ch/sbb/atlas/api/servicepoint/sector/UpdateSectorGroupVersionModel.java
@@ -1,9 +1,11 @@
 package ch.sbb.atlas.api.servicepoint.sector;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @SuperBuilder
+@NoArgsConstructor
 @Schema(name = "UpdateSectorGroupVersion")
 public class UpdateSectorGroupVersionModel extends AbstractSectorVersion {
 
