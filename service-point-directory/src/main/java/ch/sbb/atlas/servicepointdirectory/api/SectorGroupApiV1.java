4845ada5bdbc19995477cf17e5add03735b9f469
atlas <TechSupport-ATLAS@sbb.ch>
2025-07-07 16:49:31 +0200

    ATLAS-2964 add sector group api test

--- service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/api/SectorGroupApiV1.java
+++ service-point-directory/src/main/java/ch/sbb/atlas/servicepointdirectory/api/SectorGroupApiV1.java
@@ -7,6 +7,7 @@ import ch.sbb.atlas.api.servicepoint.sector.UpdateSectorGroupVersionModel;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Tag(name = "Sector Groups")
 @RequestMapping("v1/sector-groups")
@@ -29,10 +31,12 @@ public interface SectorGroupApiV1 {
   @GetMapping("versions/{id}")
   ReadSectorGroupVersionModel getSectorGroupVersion(@PathVariable Long id);
 
+  @ResponseStatus(HttpStatus.CREATED)
   @PostMapping
   ReadSectorGroupVersionModel createSectorGroupVersion(
       @Valid @RequestBody CreateSectorGroupVersionModel createSectorGroupVersionModel);
 
+  @ResponseStatus(HttpStatus.OK)
   @PutMapping(path = "{id}")
   List<SectorGroupVersionModel> updateSectorGroupVersion(
       @PathVariable Long id,
