eac303ccc5fb18c8f5f4feaec624093fe3489618
atlas <TechSupport-ATLAS@sbb.ch>
2025-07-08 17:04:43 +0200

    Use upgraded okhttp version as well

--- api-auth-gateway/build.gradle.kts
+++ api-auth-gateway/build.gradle.kts
@@ -15,7 +15,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-configuration-processor")//optional
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("com.squareup.okhttp3:mockwebserver:5.1.0")
+    testImplementation("com.squareup.okhttp3:mockwebserver:${property("okhttpVersion")}")
+    testImplementation("com.squareup.okhttp3:okhttp:${property("okhttpVersion")}")
 
 }
 
