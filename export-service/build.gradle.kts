a7ae4a37d73f382e1977a124f4a5430cb45cc74e
atlas <19825793+iuccio@users.noreply.github.com>
2025-07-09 08:43:23 +0200

    [tekton-build-bot] updated version to 2.713.0

--- export-service/build.gradle.kts
+++ export-service/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.712.0"
+version = "2.713.0"
 
 description = "Atlas Export Service"
 
--- user-administration-security/build.gradle.kts
+++ user-administration-security/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.712.0"
+version = "2.713.0"
 
 description= "Atlas User Administration Security Handler"
 
