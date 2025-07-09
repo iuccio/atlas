4d3a5ed85c86e3ecce797fff3d81a4a481ee2ed8
atlas <TechSupport-ATLAS@sbb.ch>
2025-07-09 04:00:39 +0000

    Update dependency com.microsoft.graph:microsoft-graph to v6.45.0

--- user-administration/build.gradle.kts
+++ user-administration/build.gradle.kts
@@ -9,7 +9,7 @@ group = "ch.sbb.atlas"
 version = "2.712.0"
 
 description = "Atlas User Administration"
-extra["microsoftGraphSdkVersion"] = "6.44.0"
+extra["microsoftGraphSdkVersion"] = "6.45.0"
 extra["azureIdentityVersion"] = "1.16.2"
 
 dependencies {
