bd0db0b1e8f96f35e564d57e0d0f01e059f5b680
atlas <TechSupport-ATLAS@sbb.ch>
2025-07-10 17:10:42 +0200

    ATLAS-3013 make sonar happy

---
+++ base-atlas/src/test/java/ch/sbb/atlas/helper/TerminationHelperTest.java
@@ -0,0 +1,39 @@
+package ch.sbb.atlas.helper;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import ch.sbb.atlas.exception.TerminationNotAllowedValidToNotWithinLastVersionRangeException;
+import ch.sbb.atlas.model.DateRange;
+import java.time.LocalDate;
+import org.junit.jupiter.api.Test;
+
+class TerminationHelperTest {
+
+  @Test
+  void doesNotThrowIfValidToWithinRange() {
+    String sloid = "test-id";
+    LocalDate from = LocalDate.of(2024, 1, 1);
+    LocalDate to = LocalDate.of(2024, 12, 31);
+    DateRange dateRange = new DateRange(from, to);
+    LocalDate validTo = LocalDate.of(2024, 6, 1);
+
+    assertDoesNotThrow(() ->
+        TerminationHelper.isValidToInLastVersionRange(sloid, dateRange, validTo)
+    );
+  }
+
+  @Test
+  void throwsIfValidToOutsideRange() {
+    String sloid = "test-id";
+    LocalDate from = LocalDate.of(2024, 1, 1);
+    LocalDate to = LocalDate.of(2024, 12, 31);
+    DateRange dateRange = new DateRange(from, to);
+    LocalDate validTo = LocalDate.of(2025, 1, 1);
+
+    assertThrows(TerminationNotAllowedValidToNotWithinLastVersionRangeException.class, () ->
+        TerminationHelper.isValidToInLastVersionRange(sloid, dateRange, validTo)
+    );
+  }
+
+}
