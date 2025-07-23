2277ba77d5befcef6b28e55ca265e38b55c6d7fb
atlas <19825793+iuccio@users.noreply.github.com>
2025-07-23 16:19:00 +0200

    [tekton-build-bot] updated version to 2.741.0

--- prm-directory/build.gradle.kts
+++ prm-directory/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.740.0"
+version = "2.741.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
--- user-administration-security/build.gradle.kts
+++ user-administration-security/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.740.0"
+version = "2.741.0"
 
 description= "Atlas User Administration Security Handler"
 
