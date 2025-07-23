3728a4d60984a7a4f8cfb2bc0a23c531268169e1
atlas <19825793+iuccio@users.noreply.github.com>
2025-07-23 16:46:46 +0200

    [tekton-build-bot] updated version to 2.742.0

--- bulk-import-service/build.gradle.kts
+++ bulk-import-service/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.741.0"
+version = "2.742.0"
 
 description = "Atlas Bulk Import Service"
 
--- service-point-directory/build.gradle.kts
+++ service-point-directory/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.741.0"
+version = "2.742.0"
 
 description = "ServicePointDirectory"
 extra["shedlockVersion"] = "5.16.0"
