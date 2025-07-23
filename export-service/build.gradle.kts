22dfddef4285a355d9865cf68575f418f802c57f
atlas <19825793+iuccio@users.noreply.github.com>
2025-07-23 09:11:03 +0200

    [tekton-build-bot] updated version to 2.740.0

--- export-service/build.gradle.kts
+++ export-service/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.739.0"
+version = "2.740.0"
 
 description = "Atlas Export Service"
 
--- mail/build.gradle.kts
+++ mail/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.739.0"
+version = "2.740.0"
 
 description = "Atlas Mail Service"
 
