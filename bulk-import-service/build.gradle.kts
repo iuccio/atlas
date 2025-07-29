146b70a74f1427f82ee67c0375768df5547c5039
atlas <19825793+iuccio@users.noreply.github.com>
2025-07-29 18:14:19 +0200

    [tekton-build-bot] updated version to 2.754.0

--- bulk-import-service/build.gradle.kts
+++ bulk-import-service/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.753.0"
+version = "2.754.0"
 
 description = "Atlas Bulk Import Service"
 
--- user-administration-security/build.gradle.kts
+++ user-administration-security/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.753.0"
+version = "2.754.0"
 
 description= "Atlas User Administration Security Handler"
 
