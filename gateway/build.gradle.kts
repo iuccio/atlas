23063c6188f890c0a7bfbf8084b984d4a634318b
atlas <TechSupport-ATLAS@sbb.ch>
2025-07-31 16:44:06 +0200

    [tekton-build-bot] updated version to 2.756.0

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.755.0"
+version = "2.756.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
--- user-administration-security/build.gradle.kts
+++ user-administration-security/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.755.0"
+version = "2.756.0"
 
 description= "Atlas User Administration Security Handler"
 
