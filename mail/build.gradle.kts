21d8ff7752c85e0387c91bc349f02bee8229e69c
atlas <TechSupport-ATLAS@sbb.ch>
2025-08-04 13:08:08 +0200

    [tekton-build-bot] updated version to 2.759.0

--- mail/build.gradle.kts
+++ mail/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.758.0"
+version = "2.759.0"
 
 description = "Atlas Mail Service"
 
--- prm-directory/build.gradle.kts
+++ prm-directory/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.758.0"
+version = "2.759.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
