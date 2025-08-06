243d61b3c25a5a9ac6c8b86e5a1bc43f35aef2dc
atlas <19825793+iuccio@users.noreply.github.com>
2025-08-06 09:06:03 +0200

    [tekton-build-bot] updated version to 2.762.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.761.0"
+version = "2.762.0"
 
 extra["awsS3Version"] = "2.32.16"
 
--- user-administration/build.gradle.kts
+++ user-administration/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.761.0"
+version = "2.762.0"
 
 description = "Atlas User Administration"
 extra["microsoftGraphSdkVersion"] = "6.49.0"
