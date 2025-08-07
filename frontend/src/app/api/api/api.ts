23da1dc215b6b6904ab171e5127f69aa9a0a71fa
atlas <19825793+iuccio@users.noreply.github.com>
2025-08-07 13:47:28 +0200

    ATLAS-2909: rebase fixes

--- frontend/src/app/api/api/api.ts
+++ frontend/src/app/api/api/api.ts
@@ -1,16 +1,8 @@
-import { GeoDataService } from './geoData.service';
-import { LoadingPointsService } from './loadingPoints.service';
 import { PersonWithReducedMobilityService } from './personWithReducedMobility.service';
-import { ServicePointsService } from './servicePoints.service';
 import { StopPointWorkflowService } from './stopPointWorkflow.service';
-import { TrafficPointElementsService } from './trafficPointElements.service';
 import { WorkflowService } from './workflow.service';
 
-export * from './geoData.service';
-export * from './loadingPoints.service';
 export * from './personWithReducedMobility.service';
-export * from './servicePoints.service';
 export * from './stopPointWorkflow.service';
-export * from './trafficPointElements.service';
 export * from './workflow.service';
-export const APIS = [GeoDataService, LoadingPointsService, PersonWithReducedMobilityService, ServicePointsService, StopPointWorkflowService, TrafficPointElementsService, WorkflowService];
+export const APIS = [PersonWithReducedMobilityService, StopPointWorkflowService, WorkflowService];
