08019cc2002faa19cf6df6a4ce6573f4b5726ced
atlas <19825793+iuccio@users.noreply.github.com>
2025-08-11 09:28:34 +0200

    ATLAS-2906: fix scheduling internal calls

--- scheduling/src/main/java/ch/sbb/scheduling/client/BoDiClient.java
+++ scheduling/src/main/java/ch/sbb/scheduling/client/BoDiClient.java
@@ -9,10 +9,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 @FeignClient(name = "bodiClient", url = "${atlas.client.gateway.url}", configuration = OAuthFeignConfig.class)
 public interface BoDiClient {
 
-  @PostMapping(value = "/business-organisation-directory/v1/companies/loadFromCRD", produces = MediaType.APPLICATION_JSON_VALUE)
+  @PostMapping(value = "/business-organisation-directory/internal/companies/loadFromCRD", produces =
+      MediaType.APPLICATION_JSON_VALUE)
   Response postLoadCompaniesFromCRD();
 
-  @PostMapping(value = "/business-organisation-directory/v1/transport-companies/loadFromBAV", produces =
+  @PostMapping(value = "/business-organisation-directory/internal/transport-companies/loadFromBAV", produces =
       MediaType.APPLICATION_JSON_VALUE)
   Response postLoadTransportCompaniesFromBav();
 
