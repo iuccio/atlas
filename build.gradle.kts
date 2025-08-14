0113879b2c83bac2909164ce5163a32509f7faf3
atlas <TechSupport-ATLAS@sbb.ch>
2025-08-14 16:32:08 +0200

    [tekton-build-bot] updated version to 2.782.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.781.0"
+version = "2.782.0"
 
 extra["awsS3Version"] = "2.32.22"
 
--- user-administration/build.gradle.kts
+++ user-administration/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.781.0"
+version = "2.782.0"
 
 description = "Atlas User Administration"
 extra["microsoftGraphSdkVersion"] = "6.50.0"
