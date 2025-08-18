52de363320a5496da4426c2fb9b48ea93d3215cb
atlas <TechSupport-ATLAS@sbb.ch>
2025-08-18 12:07:08 +0200

    [tekton-build-bot] updated version to 2.787.0

--- base-atlas/build.gradle.kts
+++ base-atlas/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.786.0"
+version = "2.787.0"
 
 configurations {
     create("test") //used to create the base-atlas-test jar
--- user-administration-security/build.gradle.kts
+++ user-administration-security/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.786.0"
+version = "2.787.0"
 
 description= "Atlas User Administration Security Handler"
 
