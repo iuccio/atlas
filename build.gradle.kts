2d96685b834b84a9c46ec181a76a42d4ac83053c
atlas <19825793+iuccio@users.noreply.github.com>
2025-08-20 16:32:52 +0200

    [tekton-build-bot] updated version to 2.797.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.796.0"
+version = "2.797.0"
 
 extra["awsS3Version"] = "2.32.26"
 
--- location/build.gradle.kts
+++ location/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.796.0"
+version = "2.797.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
