9a502840c50421763f144ea059f00516e6e32ce5
atlas <19825793+iuccio@users.noreply.github.com>
2025-08-21 04:00:35 +0000

    Update GRADLE Dependencies

--- scheduling/build.gradle.kts
+++ scheduling/build.gradle.kts
@@ -9,7 +9,7 @@ group = "ch.sbb.atlas"
 version = "2.797.0"
 
 description = "Atlas Scheduling Service"
-extra["shedlockVersion"] = "6.9.2"
+extra["shedlockVersion"] = "6.10.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
