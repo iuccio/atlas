c32fe578792460ffbbb03254608e3a61d5e68a3a
atlas <TechSupport-ATLAS@sbb.ch>
2025-08-28 14:39:26 +0200

    ATLAS-2555 test sonar

--- frontend/components/form/atlas-label-field/atlas-label-field.component.spec.ts
+++ frontend/components/form/atlas-label-field/atlas-label-field.component.spec.ts
@@ -4,6 +4,11 @@ import { AtlasLabelFieldComponent } from './atlas-label-field.component';
 import { TranslatePipe } from '@ngx-translate/core';
 import { translateServiceProvider } from '../../../src/app/app.testing.mocks';
 import { provideHttpClient } from '@angular/common/http';
+import { NgClass } from '@angular/common';
+import { InfoIconComponent } from '@atlas/form';
+import { InfoLinkDirective } from '@atlas/form/info-icon/info-link.directive';
+import { FieldExample } from '../../../src/app/core/form-components/text-field/field-example';
+import { By } from '@angular/platform-browser';
 
 describe('AtlasLabelFieldComponent', () => {
   let component: AtlasLabelFieldComponent;
@@ -11,7 +16,13 @@ describe('AtlasLabelFieldComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AtlasLabelFieldComponent],
+      imports: [
+        AtlasLabelFieldComponent,
+        NgClass,
+        InfoIconComponent,
+        InfoLinkDirective,
+        TranslatePipe,
+      ],
       providers: [TranslatePipe, translateServiceProvider, provideHttpClient()],
     }).compileComponents();
 
@@ -22,5 +33,43 @@ describe('AtlasLabelFieldComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should translate without arg', () => {
+    const fieldExample: FieldExample = {
+      label: 'label',
+      translate: true,
+      numberOfChars: 2,
+    };
+    expect(component.translate(fieldExample)).toEqual('label');
+  });
+
+  it('should translate with arg', () => {
+    const fieldExample: FieldExample = {
+      label: 'hallo',
+      translate: true,
+      numberOfChars: 2,
+      arg: { key: 'key', value: 'value' },
+    };
+    expect(component.translate(fieldExample)).toEqual('hallo');
+  });
+
+  it('should return only label', () => {
+    const fieldExample: FieldExample = {
+      label: '',
+    };
+    expect(component.translate(fieldExample)).toEqual('');
+  });
+
+  it('should translate without arg', () => {
+    const fieldExample: FieldExample = {
+      label: 'hallo',
+      translate: true,
+      numberOfChars: 2,
+    };
+    component.fieldExamples = [fieldExample];
+    fixture.detectChanges();
+    const element = fixture.debugElement.query(By.css('.font-regular-sm'));
+    expect(element.nativeElement.textContent).toEqual('hallo');
   });
 });
--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 });
 
 module.exports = [{
-  ignores: ["projects/**/*", "**node_modules/**/*", "src/app/api/**/*", "cypress.config.ts"],
+  ignores: ["projects/**/*", "**node_modules/**/*", "src/app/api/**/*", "cypress.config.ts", "**/*.ts.html"],
 }, ...compat.extends(
   "eslint:recommended",
   "plugin:@typescript-eslint/recommended",
