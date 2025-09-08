97147565625c8a7d8829fe6056cc205873ce3be7
atlas <TechSupport-ATLAS@sbb.ch>
2025-09-08 14:10:54 +0200

    ATLAS-3131: Document deprecated endpoints and APIs

--- base-atlas/src/main/java/ch/sbb/atlas/api/lidi/LineApiV1.java
+++ base-atlas/src/main/java/ch/sbb/atlas/api/lidi/LineApiV1.java
@@ -1,6 +1,7 @@
 package ch.sbb.atlas.api.lidi;
 
 import ch.sbb.atlas.api.model.Container;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -18,15 +19,18 @@ public interface LineApiV1 {
 
   @GetMapping
   @PageableAsQueryParam
+  @Operation(deprecated = true, summary = "Use /v2/lines/versions instead. Will be removed by 09.10.2025.")
   Container<LineModel> getLines(@Parameter(hidden = true) Pageable pageable,
       @Valid @ParameterObject LineRequestParams lineRequestParams);
 
   @GetMapping("{slnid}")
+  @Operation(deprecated = true, summary = "Use /v2/lines/versions/{slnid} instead. Will be removed by 09.10.2025.")
   LineModel getLine(@PathVariable String slnid);
 
   /**
    * @deprecated
    */
+  @Operation(deprecated = true, summary = "Use /v2/lines/versions/{slnid} instead. Will be removed by 09.10.2025.")
   @Deprecated(forRemoval = true, since = "2.328.0")
   @GetMapping("versions/{slnid}")
   List<LineVersionModel> getLineVersions(@PathVariable String slnid);
