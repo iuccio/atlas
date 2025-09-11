02bd2b7d28e7b3f78e5c083a66bf053e86e5db50
atlas <19825793+iuccio@users.noreply.github.com>
2025-09-11 13:30:01 +0200

    [tekton-build-bot] updated version to 2.835.0

--- base-atlas/build.gradle.kts
+++ base-atlas/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.834.0"
+version = "2.835.0"
 
 configurations {
     create("test") //used to create the base-atlas-test jar
--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.834.0"
+version = "2.835.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
