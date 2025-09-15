eeaf0a1955add04960d25712d7eb1e4d3fcb2186
atlas <TechSupport-ATLAS@sbb.ch>
2025-09-15 13:46:28 +0200

    [tekton-build-bot] updated version to 2.839.0

--- bulk-import-service/build.gradle.kts
+++ bulk-import-service/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.838.0"
+version = "2.839.0"
 
 description = "Atlas Bulk Import Service"
 
--- user-administration-security/build.gradle.kts
+++ user-administration-security/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.838.0"
+version = "2.839.0"
 
 description= "Atlas User Administration Security Handler"
 
