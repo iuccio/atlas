a14a49617d1220ddeaa8185e9012bcfff4a3b855
atlas <TechSupport-ATLAS@sbb.ch>
2025-09-15 09:12:07 +0200

    ATLAS-3085: Sector Json Export

--- scheduling/src/main/java/ch/sbb/scheduling/client/ExportServiceBatchClient.java
+++ scheduling/src/main/java/ch/sbb/scheduling/client/ExportServiceBatchClient.java
@@ -19,7 +19,7 @@ public interface ExportServiceBatchClient {
   @PostMapping(value = "sepodi/loading-point-batch", produces = MediaType.APPLICATION_JSON_VALUE)
   Response exportLoadingPointBatch();
 
-  @PostMapping(value = "sepodi/sector", produces = MediaType.APPLICATION_JSON_VALUE)
+  @PostMapping(value = "sepodi/sector-batch", produces = MediaType.APPLICATION_JSON_VALUE)
   Response exportSectorBatch();
 
   @PostMapping(value = "prm/stop-point-batch", produces = MediaType.APPLICATION_JSON_VALUE)
