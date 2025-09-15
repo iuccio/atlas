00ace83a48022aa411e10882f86f3545f5b2b5ca
atlas <19825793+iuccio@users.noreply.github.com>
2025-09-15 09:13:48 +0200

    [tekton-build-bot] updated version to 2.838.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.837.0"
+version = "2.838.0"
 
 extra["awsS3Version"] = "2.33.9"
 
--- user-administration-security/build.gradle.kts
+++ user-administration-security/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.837.0"
+version = "2.838.0"
 
 description= "Atlas User Administration Security Handler"
 
