58b80abcf8e9b38927f4d345eb2848d6c2f8307a
atlas <19825793+iuccio@users.noreply.github.com>
2025-09-19 08:49:06 +0200

    [tekton-build-bot] updated version to 2.850.0

--- export-service/build.gradle.kts
+++ export-service/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.849.0"
+version = "2.850.0"
 
 description = "Atlas Export Service"
 
--- location/build.gradle.kts
+++ location/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.849.0"
+version = "2.850.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
