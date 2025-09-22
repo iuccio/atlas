191969508b41e5c1f49e85a4b59d1a2ec17bd204
atlas <19825793+iuccio@users.noreply.github.com>
2025-09-22 14:34:18 +0200

    ATLAS-2988: show sector tabs only if servicePoint has mot train

--- frontend/src/app/pages/sepodi/traffic-point-elements/traffic-point-elements-side-panel.component.spec.ts
+++ frontend/src/app/pages/sepodi/traffic-point-elements/traffic-point-elements-side-panel.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TrafficPointElementsSidePanelComponent } from './traffic-point-elements-side-panel.component';
 import { ActivatedRoute } from '@angular/router';
 import { AppTestingModule } from '../../../app.testing.module';
@@ -8,17 +8,12 @@ import { TranslatePipe } from '@ngx-translate/core';
 import { BERN_WYLEREGG } from '../../../../test/data/service-point';
 import { BERN_WYLEREGG_TRAFFIC_POINTS } from '../../../../test/data/traffic-point-element';
 import { DetailPageContainerComponent } from '../../../core/components/detail-page-container/detail-page-container.component';
-import { ServicePointService } from '../../../api/service/sepodi/service-point.service';
 import { SectorMapService } from '../map/sector-map.service';
 
 describe('TrafficPointElementsSidePanelComponent', () => {
   let component: TrafficPointElementsSidePanelComponent;
   let fixture: ComponentFixture<TrafficPointElementsSidePanelComponent>;
 
-  const servicePointService = jasmine.createSpyObj(['getServicePointVersions']);
-  servicePointService.getServicePointVersions.and.returnValue(
-    of([BERN_WYLEREGG])
-  );
   const sectorMapService = jasmine.createSpyObj<SectorMapService>([
     'displaySectorsOnMap',
     'clearDisplayedSectors',
@@ -33,6 +28,7 @@ describe('TrafficPointElementsSidePanelComponent', () => {
       },
       data: of({
         trafficPoint: [BERN_WYLEREGG_TRAFFIC_POINTS[0]],
+        servicePoint: [BERN_WYLEREGG],
         isTrafficPointArea: false,
       }),
     };
@@ -48,13 +44,9 @@ describe('TrafficPointElementsSidePanelComponent', () => {
     expect(component.selectedVersion.designationOperational).toEqual('1');
     expect(component.maxValidity.validFrom).toEqual(new Date('2019-07-22'));
     expect(component.maxValidity.validTo).toEqual(new Date('2099-12-31'));
+
+    expect(component.servicePointName).toBe('Bern, Wyleregg');
   });
-
-  it('should init selected servicepoint', fakeAsync(() => {
-    expect(component.servicePointName).toBeTruthy();
-
-    expect(servicePointService.getServicePointVersions).toHaveBeenCalled();
-  }));
 
   function setupTestBed(activatedRoute: ActivatedRouteMockType) {
     return TestBed.configureTestingModule({
@@ -65,7 +57,6 @@ describe('TrafficPointElementsSidePanelComponent', () => {
       ],
       providers: [
         { provide: ActivatedRoute, useValue: activatedRoute },
-        { provide: ServicePointService, useValue: servicePointService },
         { provide: SectorMapService, useValue: sectorMapService },
         TranslatePipe,
       ],
