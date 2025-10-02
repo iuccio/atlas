bad6e0b52d3a31bf2c55b840667d9a4462cc496e
atlas <19825793+iuccio@users.noreply.github.com>
2025-10-02 17:16:08 +0200

    ATLAS-2995 sector group detail, add, edit, view done

--- base-atlas/src/main/java/ch/sbb/atlas/api/servicepoint/sector/CreateSectorGroupVersionModel.java
+++ base-atlas/src/main/java/ch/sbb/atlas/api/servicepoint/sector/CreateSectorGroupVersionModel.java
@@ -2,6 +2,7 @@ package ch.sbb.atlas.api.servicepoint.sector;
 
 import ch.sbb.atlas.model.IdCheckable;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.util.Set;
 import lombok.AllArgsConstructor;
@@ -21,6 +22,7 @@ import lombok.experimental.SuperBuilder;
 public class CreateSectorGroupVersionModel extends SectorGroupVersionModel implements IdCheckable {
 
   @Size(min = 2)
+  @NotNull
   @Schema(description = "Sector sloid's related to the sector group")
   private Set<String> sectorSloids;
 
