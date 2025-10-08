aaf30e96d968b4f08159a61dea14ed551288bc56
atlas <TechSupport-ATLAS@sbb.ch>
2025-10-08 15:49:56 +0200

    Delete dependent data

--- line-directory/src/main/resources/db/migration/V0.0.28__delete_lidi_v1.sql
+++ line-directory/src/main/resources/db/migration/V0.0.28__delete_lidi_v1.sql
@@ -1,3 +1,5 @@
+delete from line_version_workflow where line_version_id in
+                                        (select id from line_version where line_type!='ORDERLY' and (swiss_line_number is not null or concession_type is not null));
 delete from line_version where line_type!='ORDERLY' and (swiss_line_number is not null or concession_type is not null);
 
 -- LineVersion
