ad511b4e7f0a2646ac85e082a0d0d9f180f8800a
atlas <TechSupport-ATLAS@sbb.ch>
2025-10-14 07:45:06 +0200

    [tekton-build-bot] updated version to 2.900.0

--- bulk-import-service/build.gradle.kts
+++ bulk-import-service/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.899.0"
+version = "2.900.0"
 
 description = "Atlas Bulk Import Service"
 
--- service-point-directory/build.gradle.kts
+++ service-point-directory/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.899.0"
+version = "2.900.0"
 
 description = "ServicePointDirectory"
 
