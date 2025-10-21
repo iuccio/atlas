1c7aa47ff27eebdc80303d66a7bf3e83c18ce69d
atlas <TechSupport-ATLAS@sbb.ch>
2025-10-21 10:08:30 +0200

    [tekton-build-bot] updated version to 2.910.0

--- base-atlas/build.gradle.kts
+++ base-atlas/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.909.0"
+version = "2.910.0"
 
 configurations {
     create("test") //used to create the base-atlas-test jar
--- workflow/build.gradle.kts
+++ workflow/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.909.0"
+version = "2.910.0"
 
 description = "Atlas Workflow Service"
 
