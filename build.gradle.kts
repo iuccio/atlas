24d2b7a1bfeb85d6e7952bfc4f1a84cd5a637c73
atlas <TechSupport-ATLAS@sbb.ch>
2025-11-12 08:52:39 +0100

    [tekton-build-bot] updated version to 2.947.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.946.0"
+version = "2.947.0"
 
 subprojects {
     sonar {
--- service-point-directory/build.gradle.kts
+++ service-point-directory/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.946.0"
+version = "2.947.0"
 
 description = "ServicePointDirectory"
 
