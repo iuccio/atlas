d1300bd715fda3d11176cdbb0f1d3290f87196f5
atlas <19825793+iuccio@users.noreply.github.com>
2025-11-14 09:24:05 +0100

    [tekton-build-bot] updated version to 2.951.0

--- kafka/build.gradle.kts
+++ kafka/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.950.0"
+version = "2.951.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
--- service-point-directory/build.gradle.kts
+++ service-point-directory/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.950.0"
+version = "2.951.0"
 
 description = "ServicePointDirectory"
 
