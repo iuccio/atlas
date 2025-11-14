39791adc109b38ecc5ff4a67d7271b7382cdbeda
atlas <19825793+iuccio@users.noreply.github.com>
2025-11-14 10:27:59 +0100

    [tekton-build-bot] updated version to 2.952.0

--- service-point-directory/build.gradle.kts
+++ service-point-directory/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.951.0"
+version = "2.952.0"
 
 description = "ServicePointDirectory"
 
--- user-administration/build.gradle.kts
+++ user-administration/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.951.0"
+version = "2.952.0"
 
 description = "Atlas User Administration"
 
