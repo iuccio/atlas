d7a9a40bb2d956b7853da93b0590a485f6df2607
atlas <19825793+iuccio@users.noreply.github.com>
2025-11-14 15:24:09 +0100

    [tekton-build-bot] updated version to 2.954.0

--- bulk-import-service/build.gradle.kts
+++ bulk-import-service/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.953.0"
+version = "2.954.0"
 
 description = "Atlas Bulk Import Service"
 
--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.953.0"
+version = "2.954.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
