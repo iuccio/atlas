750d15f1bea657cdb5582a21dc7e575410430355
atlas <19825793+iuccio@users.noreply.github.com>
2025-11-20 16:20:00 +0100

    [tekton-build-bot] updated version to 2.967.0

--- user-administration-security/build.gradle.kts
+++ user-administration-security/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.966.0"
+version = "2.967.0"
 
 description= "Atlas User Administration Security Handler"
 
--- workflow/build.gradle.kts
+++ workflow/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.966.0"
+version = "2.967.0"
 
 description = "Atlas Workflow Service"
 
