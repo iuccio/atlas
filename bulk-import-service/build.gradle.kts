74aeab138385296a42fb0aedf18e4bc7a84462a2
atlas <TechSupport-ATLAS@sbb.ch>
2025-11-24 08:12:09 +0100

    [tekton-build-bot] updated version to 2.970.0

--- bulk-import-service/build.gradle.kts
+++ bulk-import-service/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.969.0"
+version = "2.970.0"
 
 description = "Atlas Bulk Import Service"
 
--- business-organisation-directory/build.gradle.kts
+++ business-organisation-directory/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.969.0"
+version = "2.970.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
