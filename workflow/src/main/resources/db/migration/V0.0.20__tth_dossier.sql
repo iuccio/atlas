80ca5af92d1d06ef5fb35a05df398f292ce75eb8
atlas <TechSupport-ATLAS@sbb.ch>
2025-11-24 11:30:31 +0100

    ATLAS-3215: update db after challenge

--- workflow/src/main/resources/db/migration/V0.0.20__tth_dossier.sql
+++ workflow/src/main/resources/db/migration/V0.0.20__tth_dossier.sql
@@ -15,18 +15,29 @@ create table tth_dossier
     editor                VARCHAR(50)  NOT NULL
 );
 
+CREATE TABLE tth_dossier_statement_ids
+(
+    tth_dossier_id BIGINT NOT NULL,
+    statement_ids  BIGINT NOT NULL,
+
+    CONSTRAINT fk_tth_dossier_statement_ids_tth_dossier_id
+        FOREIGN KEY (tth_dossier_id)
+            REFERENCES tth_dossier (id)
+);
+
 CREATE SEQUENCE tth_dossier_question_seq START WITH 1000 INCREMENT BY 1;
 create table tth_dossier_question
 (
     id               BIGINT      NOT NULL PRIMARY KEY,
     tth_dossier_id   BIGINT      NOT NULL,
+    question         VARCHAR(5000),
     answer_to_canton VARCHAR(5000),
     creation_date    TIMESTAMP   NOT NULL,
     creator          VARCHAR(50) NOT NULL,
     edition_date     TIMESTAMP   NOT NULL,
     editor           VARCHAR(50) NOT NULL,
 
-    CONSTRAINT fk_follow_up_workflow
+    CONSTRAINT fk_tth_dossier_question_tth_dossier_id
         FOREIGN KEY (tth_dossier_id)
             REFERENCES tth_dossier (id)
 );
