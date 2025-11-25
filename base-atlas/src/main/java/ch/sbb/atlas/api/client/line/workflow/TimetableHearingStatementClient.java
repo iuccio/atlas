f8118e8536e56e37c153ba9c5c1f8003dd02e154
atlas <TechSupport-ATLAS@sbb.ch>
2025-11-25 14:48:54 +0100

    ATLAS-3215: improve coverage

--- base-atlas/src/main/java/ch/sbb/atlas/api/client/line/workflow/TimetableHearingStatementClient.java
+++ base-atlas/src/main/java/ch/sbb/atlas/api/client/line/workflow/TimetableHearingStatementClient.java
@@ -1,7 +1,6 @@
 package ch.sbb.atlas.api.client.line.workflow;
 
 import ch.sbb.atlas.api.client.TokenPassingFeignClientConfig;
-import ch.sbb.atlas.api.lidi.workflow.LineWorkflowApi;
 import ch.sbb.atlas.api.timetable.hearing.TimetableHearingStatementApiInternal;
 import org.springframework.cloud.openfeign.FeignClient;
 
