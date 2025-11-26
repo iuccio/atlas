bce423e4972559e231f622bb9c764b5fa66d216b
atlas <19825793+iuccio@users.noreply.github.com>
2025-11-26 15:51:54 +0100

    ATLAS-3218: use-case send to bo

---
+++ workflow/src/main/java/ch/sbb/workflow/module/lidi/tth/mail/TthDossierNotificationService.java
@@ -0,0 +1,32 @@
+package ch.sbb.workflow.module.lidi.tth.mail;
+
+import ch.sbb.atlas.kafka.model.mail.MailNotification;
+import ch.sbb.workflow.mail.MailProducerService;
+import ch.sbb.workflow.module.lidi.tth.entity.TthDossierQuestion;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class TthDossierNotificationService {
+
+  private final MailProducerService mailProducerService;
+
+  public void notifyBoAboutNewQuestion(TthDossierQuestion question) {
+    // Send actual Mail with ATLAS-3232
+    MailNotification mailNotification = MailNotification.builder()
+        .to(List.of(question.getTthDossier().getBoContactMail()))
+        .build();
+    mailProducerService.produceMailNotification(mailNotification);
+  }
+
+  public void notifyCantonAboutNewAnswer(TthDossierQuestion question) {
+    // Send actual Mail with ATLAS-3232
+    MailNotification mailNotification = MailNotification.builder()
+        .to(List.of()) // An welche Kantonsmail sollen wir die Notification schicken?
+        .build();
+    mailProducerService.produceMailNotification(mailNotification);
+  }
+
+}
