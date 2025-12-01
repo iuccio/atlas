a91b99280ec85c43ee3d712466bb86a9abe1fc22
atlas <19825793+iuccio@users.noreply.github.com>
2025-12-01 09:40:10 +0100

    ATLAS-3200: set editor adn edition date to data correction

--- business-organisation-directory/src/main/resources/db/migration/V0.0.9__shorten_validity_for_already_revoked.sql
+++ business-organisation-directory/src/main/resources/db/migration/V0.0.9__shorten_validity_for_already_revoked.sql
@@ -1,6 +1,8 @@
 -- Shorten only the last version of SubLine Versions in status Revoked
 UPDATE business_organisation_version
-set valid_to = valid_from
+set valid_to     = valid_from,
+    editor       = '7087e562-e073-42b1-b5eb-f6b83700733e',
+    edition_date = now()
 where business_organisation_version.status = 'REVOKED' and id in (SELECT DISTINCT ON (sboid) id
                                                  FROM business_organisation_version
                                                  ORDER BY sboid, valid_from DESC);
