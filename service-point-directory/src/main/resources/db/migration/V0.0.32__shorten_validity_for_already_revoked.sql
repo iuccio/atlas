fcd393ffdffe5e8796d737cc7edffa3a31bdce3e
atlas <TechSupport-ATLAS@sbb.ch>
2025-12-01 09:40:10 +0100

    ATLAS-3200: set editor adn edition date to data correction

--- service-point-directory/src/main/resources/db/migration/V0.0.32__shorten_validity_for_already_revoked.sql
+++ service-point-directory/src/main/resources/db/migration/V0.0.32__shorten_validity_for_already_revoked.sql
@@ -1,6 +1,8 @@
 -- Shorten only the last version of SubLine Versions in status Revoked
 UPDATE traffic_point_element_version
-set valid_to = valid_from
+set valid_to     = valid_from,
+    editor       = '7087e562-e073-42b1-b5eb-f6b83700733e',
+    edition_date = now()
 where traffic_point_element_version.status = 'REVOKED' and id in (SELECT DISTINCT ON (sloid) id
                                                  FROM traffic_point_element_version
                                                  ORDER BY sloid, valid_from DESC);
