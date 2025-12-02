941350c3a4367f12473dc1692fff1ee5858d2d20
atlas <19825793+iuccio@users.noreply.github.com>
2025-12-02 16:41:59 +0100

    [tekton-build-bot] updated version to 2.986.0

--- base-atlas/build.gradle.kts
+++ base-atlas/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.985.0"
+version = "2.986.0"
 
 configurations {
     create("test") //used to create the base-atlas-test jar
--- export-service/build.gradle.kts
+++ export-service/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.985.0"
+version = "2.986.0"
 
 description = "Atlas Export Service"
 
