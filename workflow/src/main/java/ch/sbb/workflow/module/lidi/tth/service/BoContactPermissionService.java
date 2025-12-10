eb51fe2225f24e41520f6fc6c2c0b9f249abce22
atlas <TechSupport-ATLAS@sbb.ch>
2025-12-10 11:18:23 +0100

    ATLAS-3216 add permission check for bo contact

---
+++ workflow/src/main/java/ch/sbb/workflow/module/lidi/tth/service/BoContactPermissionService.java
@@ -0,0 +1,41 @@
+package ch.sbb.workflow.module.lidi.tth.service;
+
+import ch.sbb.atlas.api.client.user.administration.UserAdministrationClient;
+import ch.sbb.atlas.api.model.ErrorResponse.Parameter;
+import ch.sbb.atlas.api.user.administration.PermissionModel;
+import ch.sbb.atlas.api.user.administration.UserModel;
+import ch.sbb.atlas.kafka.model.user.admin.ApplicationType;
+import ch.sbb.atlas.kafka.model.user.admin.PermissionRestrictionType;
+import ch.sbb.atlas.model.exception.SimpleAtlasException;
+import java.util.Collection;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BoContactPermissionService {
+
+  private final UserAdministrationClient userAdministrationClient;
+
+  public void checkPermissionForBoContactMail(String mail) {
+    UserModel user = userAdministrationClient.getUserByMail(mail);
+
+    List<PermissionModel> permissions =
+        user.getPermissions().stream()
+            .filter(permissionModel -> permissionModel.getApplication().equals(ApplicationType.TIMETABLE_HEARING)).toList();
+
+    boolean hasPermission = permissions.stream().map(PermissionModel::getPermissionRestrictions).flatMap(Collection::stream)
+        .filter(i -> i.getType() == PermissionRestrictionType.TRANSPORT_COMPANY_DOSSIER_ANSWER)
+        .anyMatch(i -> Boolean.parseBoolean(i.getValueAsString()));
+
+    if (!hasPermission) {
+      throw SimpleAtlasException.builder()
+          .status(HttpStatus.PRECONDITION_FAILED)
+          .messageAndError("User with mail " + mail + " has no permission to be BO contact")
+          .displayCode("TTH.ERROR.NOT_ALLOWED_BO_CONTACT", List.of(new Parameter("mail", mail)))
+          .build();
+    }
+  }
+}
