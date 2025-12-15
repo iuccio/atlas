def41c62fa2c193ba49f823f717c1891869dbc0d
atlas <19825793+iuccio@users.noreply.github.com>
2025-12-15 14:13:54 +0100

    [tekton-build-bot] updated version to 2.1009.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1008.0"
+version = "2.1009.0"
 
 subprojects {
     sonar {
--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1008.0"
+version = "2.1009.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
