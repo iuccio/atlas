fbf04769bb464d7add7ae9046b881d839eff37bb
atlas <19825793+iuccio@users.noreply.github.com>
2026-01-06 15:28:26 +0100

    pr review: read form errors directly inside field-error component

--- frontend/src/app/core/form-components/atlas-field-error/atlas-field-error.component.ts
+++ frontend/src/app/core/form-components/atlas-field-error/atlas-field-error.component.ts
@@ -1,18 +1,39 @@
-import { Component, computed, inject, input } from '@angular/core';
+import { Component, effect, inject, input } from '@angular/core';
 import { AbstractControl, ValidationErrors } from '@angular/forms';
 import { ValidationService } from '../../validation/validation.service';
 import { TranslatePipe } from '@ngx-translate/core';
+import { map } from 'rxjs/operators';
+import { EMPTY, merge, Observable, of, startWith } from 'rxjs';
+import { ValidationError } from '../../validation/validation-error';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'atlas-field-error',
   templateUrl: './atlas-field-error.component.html',
   styleUrls: ['./atlas-field-error.component.scss'],
-  imports: [TranslatePipe],
+  imports: [TranslatePipe, AsyncPipe],
 })
 export class AtlasFieldErrorComponent {
   readonly control = input.required<AbstractControl | null>();
-  readonly ctrlErrors = input<ValidationErrors | null>();
-  protected readonly errors = computed(() => this.getErrors(this.ctrlErrors()));
+
+  protected errors$: Observable<ValidationError[]> = of();
+
+  constructor() {
+    effect(() => {
+      const ctrl = this.control();
+      if (!ctrl) {
+        this.errors$ = of([]);
+        return;
+      }
+      this.errors$ = merge(
+        ctrl.valueChanges,
+        ctrl.parent?.statusChanges ?? EMPTY
+      ).pipe(
+        startWith(this.getErrors(ctrl.errors)),
+        map(() => this.getErrors(ctrl.errors))
+      );
+    });
+  }
 
   private readonly validationService = inject(ValidationService);
 
