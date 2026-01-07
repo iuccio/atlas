d30de500325ccac97227a68f3e32f4800d861235
atlas <TechSupport-ATLAS@sbb.ch>
2026-01-07 11:38:48 +0100

    fix ngSrc warnings

--- frontend/src/app/pages/tth/overview-tab/overview-tab-heading/overview-tab-heading.component.ts
+++ frontend/src/app/pages/tth/overview-tab/overview-tab-heading/overview-tab-heading.component.ts
@@ -1,15 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { HearingStatus, TimetableHearingYear } from '../../../../api';
 import { Pages } from '../../../pages';
-
 import { DisplayDatePipe } from '../../../../core/pipe/display-date.pipe';
 import { TranslatePipe } from '@ngx-translate/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'atlas-timetable-hearing-overview-tab-heading',
   templateUrl: './overview-tab-heading.component.html',
   styleUrls: ['./overview-tab-heading.component.scss'],
-  imports: [DisplayDatePipe, TranslatePipe],
+  imports: [DisplayDatePipe, TranslatePipe, NgOptimizedImage],
 })
 export class OverviewTabHeadingComponent {
   @Input() cantonShort!: string;
