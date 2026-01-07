297018e5c3f842b955c68354c691cbc3817f2891
atlas <TechSupport-ATLAS@sbb.ch>
2026-01-07 11:55:16 +0100

    [tekton-build-bot] updated version to 2.1020.0

--- export-service/build.gradle.kts
+++ export-service/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1019.0"
+version = "2.1020.0"
 
 description = "Atlas Export Service"
 
--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1019.0"
+version = "2.1020.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
