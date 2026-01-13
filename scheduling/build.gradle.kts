f0d9f48b239a5d3be3cec2a8bf178c5f9f9da9c4
atlas <19825793+iuccio@users.noreply.github.com>
2026-01-13 13:14:17 +0100

    [tekton-build-bot] updated version to 2.1034.0

--- scheduling/build.gradle.kts
+++ scheduling/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1033.0"
+version = "2.1034.0"
 
 description = "Atlas Scheduling Service"
 
--- service-point-directory/build.gradle.kts
+++ service-point-directory/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1033.0"
+version = "2.1034.0"
 
 description = "ServicePointDirectory"
 
