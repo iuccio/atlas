e49a9b95eac5e679dff4cdd82f1c508202b2d0ec
atlas <TechSupport-ATLAS@sbb.ch>
2026-01-19 08:51:05 +0100

    [tekton-build-bot] updated version to 2.1041.0

--- export-service/build.gradle.kts
+++ export-service/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1040.0"
+version = "2.1041.0"
 
 description = "Atlas Export Service"
 
--- user-administration-security/build.gradle.kts
+++ user-administration-security/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1040.0"
+version = "2.1041.0"
 
 description= "Atlas User Administration Security Handler"
 
