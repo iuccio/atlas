5d91194d66d2855d3a128e9cd695e4a055f02758
atlas <19825793+iuccio@users.noreply.github.com>
2026-01-26 18:35:03 +0100

    ATLAS add json starter to and update spring cloud dependencies

--- base-atlas/build.gradle.kts
+++ base-atlas/build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("org.springframework.boot:spring-boot-starter-flyway")
+    implementation("org.springframework.boot:spring-boot-starter-json")
 
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-security-oauth2-client")
