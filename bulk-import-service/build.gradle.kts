e9c17b6c8f6e3d5f4f9be1643bc9fdc37c6f3e00
atlas <19825793+iuccio@users.noreply.github.com>
2026-01-26 12:46:11 +0100

    [tekton-build-bot] updated version to 2.1063.0

--- bulk-import-service/build.gradle.kts
+++ bulk-import-service/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1062.0"
+version = "2.1063.0"
 
 description = "Atlas Bulk Import Service"
 
--- service-point-directory/build.gradle.kts
+++ service-point-directory/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1062.0"
+version = "2.1063.0"
 
 description = "ServicePointDirectory"
 
