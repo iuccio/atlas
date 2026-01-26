cc687e81f768f90c846a84599534af099001b5c5
atlas <TechSupport-ATLAS@sbb.ch>
2026-01-26 09:43:09 +0100

    Make fx users mail usablÃe
    Update test
    
    be more consistent

--- user-administration/src/main/java/ch/sbb/atlas/user/administration/module/userinformation/mapper/GraphApiUserMapper.java
+++ user-administration/src/main/java/ch/sbb/atlas/user/administration/module/userinformation/mapper/GraphApiUserMapper.java
@@ -4,6 +4,7 @@ import ch.sbb.atlas.api.user.administration.UserModel;
 import ch.sbb.atlas.api.user.administration.enumeration.UserAccountStatus;
 import com.microsoft.graph.models.User;
 import java.util.Collections;
+import java.util.Optional;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
@@ -14,7 +15,7 @@ public class GraphApiUserMapper {
         .sbbUserId(user.getOnPremisesSamAccountName())
         .lastName(user.getSurname())
         .firstName(user.getGivenName())
-        .mail(user.getMail())
+        .mail(Optional.ofNullable(user.getMail()).orElse(user.getUserPrincipalName()))
         .accountStatus(UserAccountStatus.getUserAccountStatusFromBoolean(user.getAccountEnabled()))
         .displayName(user.getDisplayName())
         .permissions(Collections.emptySet())
