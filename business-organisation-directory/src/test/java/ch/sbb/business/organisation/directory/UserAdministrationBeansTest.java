7449145acb74d2405015544475c79aa7ac8de1ce
atlas <TechSupport-ATLAS@sbb.ch>
2026-01-27 17:13:27 +0100

    Make bodi compile and run

--- business-organisation-directory/src/test/java/ch/sbb/business/organisation/directory/UserAdministrationBeansTest.java
+++ business-organisation-directory/src/test/java/ch/sbb/business/organisation/directory/UserAdministrationBeansTest.java
@@ -6,7 +6,7 @@ import ch.sbb.atlas.model.controller.IntegrationTest;
 import ch.sbb.atlas.user.administration.security.UserAdministrationLoader;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.context.ApplicationContext;
 
 @IntegrationTest
