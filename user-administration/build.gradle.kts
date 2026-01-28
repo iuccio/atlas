ec5143b71410b8a79240c19c225a9204f15064a4
atlas <TechSupport-ATLAS@sbb.ch>
2026-01-28 10:38:58 +0100

    make user administration compile and run

--- user-administration/build.gradle.kts
+++ user-administration/build.gradle.kts
@@ -13,18 +13,20 @@ description = "Atlas User Administration"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.security:spring-security-oauth2-authorization-server")
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springframework.boot:spring-boot-starter-security-oauth2-authorization-server")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-flyway")
     implementation(libs.microsoft.graph)
     implementation(libs.azure.identity)
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
     implementation(project(":base-atlas"))
     implementation(project(":kafka"))
-    implementation("org.springframework.kafka:spring-kafka")
-    implementation("org.hibernate.orm:hibernate-jpamodelgen")
-    annotationProcessor("org.hibernate.orm:hibernate-jpamodelgen")
+    implementation(project(":kafka"))
+    implementation("org.springframework.boot:spring-boot-starter-kafka")
+    implementation("org.hibernate.orm:hibernate-processor")
+    annotationProcessor("org.hibernate.orm:hibernate-processor")
 
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-database-postgresql")
@@ -34,10 +36,11 @@ dependencies {
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
     testImplementation(project(":base-atlas", "test"))
     testImplementation(project(":auto-rest-doc"))
-    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-security-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
-    testImplementation("org.testcontainers:postgresql")
+    testImplementation("org.testcontainers:testcontainers-postgresql")
 
     testRuntimeOnly("org.postgresql:postgresql")
     testImplementation(libs.mockito.inline)
--- user-administration/src/test/java/ch/sbb/atlas/user/administration/ApimYamlExtractionTest.java
+++ user-administration/src/test/java/ch/sbb/atlas/user/administration/ApimYamlExtractionTest.java
@@ -12,41 +12,41 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 @IntegrationTest
 @AutoConfigureMockMvc(addFilters = false)
 @Slf4j
- class ApimYamlExtractionTest {
+class ApimYamlExtractionTest {
 
-    @Autowired
-    private MockMvc mvc;
+  @Autowired
+  private MockMvc mvc;
 
-    @Value("${info.app.name}")
-    private String appName;
+  @Value("${info.app.name}")
+  private String appName;
 
-    @Test
-    void shouldProvideApiYaml() throws Exception {
-        MvcResult mvcResult = mvc.perform(get("/v3/api-docs.yaml"))
-            .andExpect(status().isOk())
-            .andReturn();
-        Path specYamlFile = Paths.get("..", "apim-configuration",
-            "src/main/resources/apis/", appName, "spec.yaml");
+  @Test
+  void shouldProvideApiYaml() throws Exception {
+    MvcResult mvcResult = mvc.perform(get("/v3/api-docs.yaml"))
+        .andExpect(status().isOk())
+        .andReturn();
+    Path specYamlFile = Paths.get("..", "apim-configuration",
+        "src/main/resources/apis/", appName, "spec.yaml");
 
-        Path parentDir = specYamlFile.getParent();
-        assertThat(parentDir).isNotNull();
-        if (!Files.exists(parentDir)) {
-            Files.createDirectories(parentDir);
-        }
-
-        log.info("Exporting OpenAPI spec.yaml to {}", specYamlFile.toAbsolutePath().normalize());
-
-        byte[] specYamlAsBytes = mvcResult.getResponse().getContentAsByteArray();
-        assertThat(specYamlAsBytes).isNotEmpty();
-
-        Files.write(specYamlFile, specYamlAsBytes);
+    Path parentDir = specYamlFile.getParent();
+    assertThat(parentDir).isNotNull();
+    if (!Files.exists(parentDir)) {
+      Files.createDirectories(parentDir);
     }
+
+    log.info("Exporting OpenAPI spec.yaml to {}", specYamlFile.toAbsolutePath().normalize());
+
+    byte[] specYamlAsBytes = mvcResult.getResponse().getContentAsByteArray();
+    assertThat(specYamlAsBytes).isNotEmpty();
+
+    Files.write(specYamlFile, specYamlAsBytes);
+  }
 
 }
