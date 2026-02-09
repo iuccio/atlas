8295686ca1ac7101e5f552f9ba2942a130ed129a
atlas <19825793+iuccio@users.noreply.github.com>
2026-02-09 15:57:58 +0100

    ATLAS-3325: allow creation of dossier without question

---
+++ frontend/src/app/pages/tth/dossier/detail/dossier-detail-form-group.spec.ts
@@ -0,0 +1,19 @@
+import { DossierFormGroupBuilder } from './dossier-detail-form-group';
+
+describe('DossierDetailFormGroup', () => {
+  it('should map form question to TthDossier correctly when empty', () => {
+    const formGroup = DossierFormGroupBuilder.buildFormGroup();
+    const dossier = DossierFormGroupBuilder.getDossier(formGroup);
+    expect(dossier.questions.length).toBe(0);
+  });
+
+  it('should map form question to TthDossier correctly when filled', () => {
+    const formGroup = DossierFormGroupBuilder.buildFormGroup();
+    formGroup.controls.question.setValue('What is the status?');
+    const dossierQuestions =
+      DossierFormGroupBuilder.getDossier(formGroup).questions;
+    expect(dossierQuestions.length).toBe(1);
+    expect(dossierQuestions[0].question).toEqual('What is the status?');
+    expect(dossierQuestions[0].answerToCanton).toBeNull();
+  });
+});
