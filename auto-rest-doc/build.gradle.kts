54532e0d19b283963a9d1e4bd459512fea993ff3
atlas <TechSupport-ATLAS@sbb.ch>
2026-02-18 18:01:52 +0100

    [tekton-build-bot] updated version to 2.1112.0

--- auto-rest-doc/build.gradle.kts
+++ auto-rest-doc/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1111.0"
+version = "2.1112.0"
 
 dependencies {
     // Spring Boot Starters
--- mail/build.gradle.kts
+++ mail/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1111.0"
+version = "2.1112.0"
 
 description = "Atlas Mail Service"
 
