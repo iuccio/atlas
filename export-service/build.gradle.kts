f4ab63f4dc2b7d846f2c36d1e1cd15f7e1b83e79
atlas <19825793+iuccio@users.noreply.github.com>
2026-02-19 11:12:09 +0100

    [tekton-build-bot] updated version to 2.1115.0

--- export-service/build.gradle.kts
+++ export-service/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1114.0"
+version = "2.1115.0"
 
 description = "Atlas Export Service"
 
--- prm-directory/build.gradle.kts
+++ prm-directory/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1114.0"
+version = "2.1115.0"
 
 dependencies {
     // Spring Boot Starters
