5d0d348acf31376ebeba808b33ffba6307c8e4a2
atlas <19825793+iuccio@users.noreply.github.com>
2026-02-20 09:50:03 +0100

    ATLAS-3223 fix small issues

--- frontend/src/app/core/components/side-nav/side-nav.component.ts
+++ frontend/src/app/core/components/side-nav/side-nav.component.ts
@@ -45,6 +45,8 @@ export class SideNavComponent {
         })
       )
       .subscribe(([event, pages]) => {
+        console.log('Router event:', event);
+        console.log('Enabled pages:', pages);
         this.setActivePage(event.url, pages);
       });
   }
