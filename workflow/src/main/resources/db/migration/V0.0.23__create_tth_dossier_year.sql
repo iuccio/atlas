835127e6a35984132c088fd5741ddb6b346aa283
atlas <19825793+iuccio@users.noreply.github.com>
2026-03-03 11:30:39 +0100

    fix failing flyway migration

--- workflow/src/main/resources/db/migration/V0.0.23__create_tth_dossier_year.sql
+++ workflow/src/main/resources/db/migration/V0.0.23__create_tth_dossier_year.sql
@@ -1,11 +1,10 @@
-
 CREATE TABLE tth_dossier_year
 (
     timetable_year               BIGINT      NOT NULL PRIMARY KEY,
     hearing_status               VARCHAR(50) NOT NULL
 );
 
-alter table tth_dossier
-    add column timetable_year BIGINT NOT NULL
-        constraint fk_timetable_year
-            references tth_dossier_year(timetable_year);
+ALTER TABLE tth_dossier
+    ADD COLUMN timetable_year BIGINT
+        CONSTRAINT fk_timetable_year
+            REFERENCES tth_dossier_year(timetable_year);
