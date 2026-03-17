a2b45b40b72affdf6da12d46f71478e75096bc29
atlas <19825793+iuccio@users.noreply.github.com>
2026-03-17 17:39:26 +0100

    ATLAS-3343: remove tick-async

--- frontend/src/app/pages/user-administration/client-credential/detail/create/user-administration-client-create.component.spec.ts
+++ frontend/src/app/pages/user-administration/client-credential/detail/create/user-administration-client-create.component.spec.ts
@@ -2,18 +2,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { beforeEach, describe, expect, it, type Mocked, vi } from 'vitest';
 import { UserAdministrationClientCreateComponent } from './user-administration-client-create.component';
 import { NotificationService } from '../../../../../core/notification/notification.service';
-import { TranslatePipe } from '@ngx-translate/core';
 import { of } from 'rxjs';
-import { Router, RouterModule } from '@angular/router';
+import { provideRouter, Router } from '@angular/router';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { DetailPageContainerComponent } from '../../../../../core/components/detail-page-container/detail-page-container.component';
-import { DetailFooterComponent } from '../../../../../core/components/detail-footer/detail-footer.component';
-import { DetailPageContentComponent } from '../../../../../core/components/detail-page-content/detail-page-content.component';
 import { ClientCredentialAdministrationService } from '../../../../../api/service/user-administration/client-credential-administration.service';
 import { translateServiceProvider } from '../../../../../app.testing.mocks';
-import { provideHttpClient } from '@angular/common/http';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
-import { tickAsync } from '../../../../../../test/tick-async';
 
 describe('UserAdministrationClientCreateComponent', () => {
   let component: UserAdministrationClientCreateComponent;
@@ -24,7 +17,7 @@ describe('UserAdministrationClientCreateComponent', () => {
   >;
   let notificationService: Mocked<Pick<NotificationService, 'success'>>;
 
-  beforeEach(async () => {
+  beforeEach(() => {
     clientCredentialAdministrationService = {
       createClientCredential: vi.fn(),
     };
@@ -32,18 +25,10 @@ describe('UserAdministrationClientCreateComponent', () => {
       success: vi.fn(),
     };
 
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterModule.forRoot([]),
-        UserAdministrationClientCreateComponent,
-        DetailPageContainerComponent,
-        DetailPageContentComponent,
-        DetailFooterComponent,
-      ],
+    TestBed.configureTestingModule({
       providers: [
-        provideHttpClient(),
-        provideHttpClientTesting(),
         translateServiceProvider,
+        provideRouter([]),
         {
           provide: ClientCredentialAdministrationService,
           useValue: clientCredentialAdministrationService,
@@ -52,7 +37,6 @@ describe('UserAdministrationClientCreateComponent', () => {
           provide: NotificationService,
           useValue: notificationService,
         },
-        TranslatePipe,
         {
           provide: MAT_DIALOG_DATA,
           useValue: { user: undefined },
@@ -66,7 +50,7 @@ describe('UserAdministrationClientCreateComponent', () => {
           },
         },
       ],
-    }).compileComponents();
+    });
 
     fixture = TestBed.createComponent(UserAdministrationClientCreateComponent);
     component = fixture.componentInstance;
@@ -90,11 +74,12 @@ describe('UserAdministrationClientCreateComponent', () => {
     vi.spyOn(router, 'navigate').mockResolvedValue(true);
 
     component.create();
+
     expect(
       clientCredentialAdministrationService.createClientCredential
     ).toHaveBeenCalledTimes(1);
     expect(router.navigate).toHaveBeenCalledTimes(1);
-    await tickAsync(1000);
+    await fixture.whenStable();
     expect(notificationService.success).toHaveBeenCalledExactlyOnceWith(
       'USER_ADMIN.NOTIFICATIONS.ADD_SUCCESS'
     );
