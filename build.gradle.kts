4ec614fc418685adcf8870cbfccb5839808468db
atlas <19825793+iuccio@users.noreply.github.com>
2026-03-17 09:08:58 +0100

    [tekton-build-bot] updated version to 2.1166.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1165.0"
+version = "2.1166.0"
 
 subprojects {
     sonar {
--- scheduling/build.gradle.kts
+++ scheduling/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1165.0"
+version = "2.1166.0"
 
 description = "Atlas Scheduling Service"
 
