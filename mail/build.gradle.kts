20fce94848ba0f0aee724a0186ffc0d07ed83046
atlas <TechSupport-ATLAS@sbb.ch>
2026-03-18 10:50:07 +0100

    atlas

--- mail/build.gradle.kts
+++ mail/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1168.0"
+version = "2.1169.0"
 
 description = "Atlas Mail Service"
 
--- service-point-directory/build.gradle.kts
+++ service-point-directory/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1168.0"
+version = "2.1169.0"
 
 description = "ServicePointDirectory"
 
