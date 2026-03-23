a0084bcab7ea0aead2ed1077a2c2d46f3cd743d7
atlas <19825793+iuccio@users.noreply.github.com>
2026-03-23 10:43:38 +0100

    [tekton-build-bot] updated version to 2.1174.0

--- api-auth-gateway/build.gradle.kts
+++ api-auth-gateway/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1173.0"
+version = "2.1174.0"
 
 dependencies {
     // Spring Boot Starters
--- user-administration/build.gradle.kts
+++ user-administration/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ch.sbb.atlas"
-version = "2.1173.0"
+version = "2.1174.0"
 
 description = "Atlas User Administration"
 
